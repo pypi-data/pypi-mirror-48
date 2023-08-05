39a0056a9b4c659314fba73b14f24c73599ac37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:53 +0000

    Add package dask-ml/1.0.0/dask_ml-1.0.0-py3-none-any.whl

---
+++ packages/dask-ml/dask_ml-1.0.0-py3-none-any.whl/dask_ml/_compat.py
@@ -0,0 +1,25 @@
+import contextlib
+from collections.abc import Mapping  # noqa
+
+import dask
+import dask.array as da
+import packaging.version
+import pandas
+import sklearn
+
+SK_VERSION = packaging.version.parse(sklearn.__version__)
+DASK_VERSION = packaging.version.parse(dask.__version__)
+PANDAS_VERSION = packaging.version.parse(pandas.__version__)
+
+SK_022 = SK_VERSION >= packaging.version.parse("0.22.dev0")
+
+
+@contextlib.contextmanager
+def dummy_context(*args, **kwargs):
+    yield
+
+
+if DASK_VERSION < packaging.version.parse("1.1.0"):
+    blockwise = da.atop
+else:
+    blockwise = da.blockwise
