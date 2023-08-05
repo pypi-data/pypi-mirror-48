1bce892193e690228c54a55ef40e21adae819f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:54 +0000

    Add package aws-cdk.aws-wafregional/0.35.0/aws_cdk.aws_wafregional-0.35.0-py3-none-any.whl

---
+++ packages/aws-cdk.aws-wafregional/aws_cdk.aws_wafregional-0.35.0-py3-none-any.whl/aws_cdk/aws_wafregional/_jsii/__init__.py
@@ -0,0 +1,15 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.cdk
+__all__ = []
+
+publication.publish()
