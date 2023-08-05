f393a5aaad676b4e97478c445c30a43bc1ec4c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package sapitts/0.1/sapitts-0.1.tar.gz

---
+++ packages/sapitts/sapitts-0.1.tar.gz/sapitts-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import platform
+
+install_requires = []
+if platform.system() == 'Windows':
+    install_requires += [
+        'comtypes',
+    ]
+
+setup(name='sapitts',
+      version='0.01',
+      description='A simple TTS wrapper',
+      url='http://github.com/DeepHorizons/tts',
+      author='Joshua Milas',
+      author_email='josh.milas@gmail.com',
+      license='MIT',
+      packages=['tts'],
+      install_requires=install_requires,
+      zip_safe=False)
