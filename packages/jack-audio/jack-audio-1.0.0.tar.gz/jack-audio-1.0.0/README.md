09042fe230cda2bffe6868e844bb81c11fe14756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package jack-audio/1.0.0/jack-audio-1.0.0.tar.gz

---
+++ packages/jack-audio/jack-audio-1.0.0.tar.gz/jack-audio-1.0.0/README.md
@@ -0,0 +1,4 @@
+# jack-audio
+ A Python package for stationary audio noise reduction.
+
+[J.A.C.K.](https://github.com/cooperbarth/Joint-Audio-Correction-Kit)
---
+++ packages/jack-audio/jack-audio-1.0.0.tar.gz/jack-audio-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="jack-audio",
+    version="1.0.0",
+    author="Cooper Barth",
+    author_email="cooperfbarth@gmail.com",
+    description="A Python package for stationary audio noise reduction.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/cooperbarth/jack-audio",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'librosa',
+        'pathlib',
+        'pysndfx'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+'''
+PACKAGING:
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/ --skip-existing*
+'''
