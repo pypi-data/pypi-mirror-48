ac506e63a97f8cc4f75fa744750dc80a8e2a54b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:56 +0000

    Add package aimmo/0.22.11b4347/aimmo-0.22.11b4347-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-0.22.11b4347-py2.py3-none-any.whl/aimmo-game/authentication.py
@@ -0,0 +1,16 @@
+import os
+
+import kubernetes
+import requests
+
+
+async def initialize_game_token(communicator):
+    """Get game token and store it somewhere accessible."""
+    if os.environ["WORKER"] == "kubernetes":
+        api = kubernetes.client.CoreV1Api()
+        game_id = os.environ.get("GAME_ID")
+
+        secret = api.read_namespaced_secret(f"game-{game_id}-token", "default")
+        os.environ["TOKEN"] = secret.string_data["token"]
+
+    await communicator.patch_token({"token": os.environ["TOKEN"]})
