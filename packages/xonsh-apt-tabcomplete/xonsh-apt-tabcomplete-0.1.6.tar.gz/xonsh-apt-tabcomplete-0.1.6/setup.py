003e5f8ce07401659a96bf79493265fce299cd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:19 +0000

    Add package xonsh-apt-tabcomplete/0.1.6/xonsh-apt-tabcomplete-0.1.6.tar.gz

---
+++ packages/xonsh-apt-tabcomplete/xonsh-apt-tabcomplete-0.1.6.tar.gz/xonsh-apt-tabcomplete-0.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name="xonsh-apt-tabcomplete",
+    version="0.1.6",
+    license="BSD",
+    url="https://github.com/DangerOnTheRanger/xonsh-apt-tabcomplete",
+    download_url="https://github.com/DangerOnTheRanger/xonsh-apt-tabcomplete/tarball/v0.1.6",
+    description="APT tabcomplete support for the Xonsh shell",
+    long_description=long_description,
+    author="Kermit Alexander II",
+    author_email="tuxfreak@tuxfamily.org",
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python"
+    ]
+)
