3431b5dec24f40f614d9e5c2d6e46d141877022e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:29 +0000

    Add package green-example/1.0.0/green-example-1.0.0.tar.gz

---
+++ packages/green-example/green-example-1.0.0.tar.gz/green-example-1.0.0/setup.py
@@ -0,0 +1,18 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='green-example',
+    version='1.0.0',
+    description='A sample Python hello world lambda function for AWS Greengrass.',
+    license='MIT',
+    author='Halim Qarroum',
+    author_email='qarroumh@amazon.lu',
+    install_requires=['arrow'],
+    keywords=['greengrass', 'hello-world', 'pip']
+)
