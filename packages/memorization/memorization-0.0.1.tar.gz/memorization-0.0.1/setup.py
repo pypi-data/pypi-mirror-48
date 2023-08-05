461d2cc480d002c80e211f65404755321d0747e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:12 +0000

    Add package memorization/0.0.1/memorization-0.0.1.tar.gz

---
+++ packages/memorization/memorization-0.0.1.tar.gz/memorization-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="memorization",
+    version="0.0.1",
+    author="Modern LLC",
+    author_email="modernllc@outlook.com",
+    description="Memory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ciela-io/memorization",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
