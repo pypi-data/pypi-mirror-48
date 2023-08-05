4abe25a34f025d37bab56fb91b34ec85d862801a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package autolens/0.22.1/autolens-0.22.1.tar.gz

---
+++ packages/autolens/autolens-0.22.1.tar.gz/autolens-0.22.1/test/integration/tests/model_mapper/align_phi.py
@@ -0,0 +1,48 @@
+import os
+
+import autolens.pipeline.phase.phase_imaging
+from autofit import conf
+from autofit.optimize import non_linear as nl
+from autolens.model.galaxy import galaxy_model as gm
+from autolens.pipeline.phase import phase as ph
+from autolens.pipeline import pipeline as pl
+from autolens.model.profiles import light_profiles as lp, mass_profiles as mp
+from test.integration import integration_util
+from test.simulation import simulation_util
+
+test_type = 'model_mapper'
+test_name = "align_phi"
+
+test_path = '{}/../../'.format(os.path.dirname(os.path.realpath(__file__)))
+output_path = test_path + 'output/'
+config_path = test_path + 'config'
+conf.instance = conf.Config(config_path=config_path, output_path=output_path)
+
+
+def pipeline():
+
+    integration_util.reset_paths(test_name=test_name, output_path=output_path)
+    ccd_data = simulation_util.load_test_ccd_data(data_type='lens_only_dev_vaucouleurs', data_resolution='LSST')
+    pipeline = make_pipeline(test_name=test_name)
+    pipeline.run(data=ccd_data)
+
+def make_pipeline(test_name):
+
+    class MMPhase(autolens.pipeline.phase.phase_imaging.LensPlanePhase):
+        pass
+
+    phase1 = MMPhase(
+        phase_name='phase_1', phase_folders=[test_type, test_name],
+        lens_galaxies=dict(lens=gm.GalaxyModel(redshift=0.5,light_0=lp.EllipticalSersic, light_1=lp.EllipticalSersic,
+                                                mass=mp.EllipticalIsothermal, align_phi=True)),
+        optimizer_class=nl.MultiNest)
+
+    phase1.optimizer.const_efficiency_mode = True
+    phase1.optimizer.n_live_points = 20
+    phase1.optimizer.sampling_efficiency = 0.8
+
+    return pl.PipelineImaging(test_name, phase1)
+
+
+if __name__ == "__main__":
+    pipeline()
