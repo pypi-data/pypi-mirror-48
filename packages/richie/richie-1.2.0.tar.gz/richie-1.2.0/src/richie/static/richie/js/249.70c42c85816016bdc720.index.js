0b71f913328007b1f15c083625f5527bfdb30808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:41 +0000

    Add package richie/1.2.0/richie-1.2.0.tar.gz

---
+++ packages/richie/richie-1.2.0.tar.gz/richie-1.2.0/src/richie/static/richie/js/249.70c42c85816016bdc720.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[249],{445:function(n,w){}}]);
+//# sourceMappingURL=249.70c42c85816016bdc720.index.js.map
---
+++ packages/richie/richie-1.2.0.tar.gz/richie-1.2.0/src/richie/static/richie/js/250.70c42c85816016bdc720.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[250],{446:function(n,w){}}]);
+//# sourceMappingURL=250.70c42c85816016bdc720.index.js.map
