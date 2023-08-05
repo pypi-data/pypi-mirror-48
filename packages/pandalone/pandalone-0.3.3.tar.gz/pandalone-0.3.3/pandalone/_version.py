c9ac48a4853481dc03439387dd70f4c3121acdaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package pandalone/0.3.3/pandalone-0.3.3.tar.gz

---
+++ packages/pandalone/pandalone-0.3.3.tar.gz/pandalone-0.3.3/pandalone/_version.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Copyright 2014-2019 European Commission (JRC);
+# Licensed under the EUPL (the 'Licence');
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
+"""Authoritative project's version-info"""
+__version__ = "0.3.3"  # PEP 440 versioning. Also update README.rst's.
+__updated__ = "2019-06-21 20:54:25"
+version = __version__
