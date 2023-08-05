f088f34a73253f29b8463c18faec09469c76aed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package PyOpenWorm/0.10.1/PyOpenWorm-0.10.1-py3-none-any.whl

---
+++ packages/PyOpenWorm/PyOpenWorm-0.10.1-py3-none-any.whl/PyOpenWorm/contextDataObject.py
@@ -0,0 +1,15 @@
+from rdflib.term import URIRef
+from PyOpenWorm.dataObject import DataObject, ObjectProperty, This
+from .context_common import CONTEXT_IMPORTS
+
+
+class ContextDataObject(DataObject):
+    """ Represents a context """
+    class_context = 'http://openworm.org/schema'
+    rdf_type = URIRef('http://openworm.org/schema/Context')
+    imports = ObjectProperty(value_type=This,
+                             multiple=True,
+                             link=CONTEXT_IMPORTS)
+
+
+__yarom_mapped_classes__ = (ContextDataObject,)
