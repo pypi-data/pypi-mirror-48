47b7164ed8ce5afdde7e119579886e02d8ca0e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:50 +0000

    Add package sfini/0.1.0/sfini-0.1.0.tar.gz

---
+++ packages/sfini/sfini-0.1.0.tar.gz/sfini-0.1.0/src/sfini/execution/_execution.py
@@ -0,0 +1,303 @@
+# --- 80 characters -----------------------------------------------------------
+# Created by: Laurie 2018/07/11
+
+"""State-machine execution."""
+
+import time
+import json
+import datetime
+import typing as T
+import logging as lg
+
+from .. import _util
+from . import history
+
+_logger = lg.getLogger(__name__)
+_default = _util.DefaultParameter()
+
+
+class Execution:
+    """A state-machine execution.
+
+    Args:
+        name: name of execution
+        state_machine_arn: executed state-machine ARN
+        execution_input: execution input (must be JSON-serialisable)
+        arn: execution ARN (if known: provided when execution is posted to
+            AWS SFN)
+        session: session to use for AWS communication
+    """
+
+    _wait_sleep_time = 3.0
+    _not_provided = object()
+
+    def __init__(
+            self,
+            name: str,
+            state_machine_arn: str,
+            execution_input: _util.JSONable = _default,
+            arn: str = None,
+            *,
+            session: _util.AWSSession = None):
+        self.name = name
+        self.state_machine_arn = state_machine_arn
+        self.execution_input = execution_input
+        self.arn = arn
+        self.session = session or _util.AWSSession()
+
+        self._status = None
+        self._start_date = None
+        self._stop_date = None
+        self._output = _default
+
+    def __str__(self):
+        status_str = (" [%s]" % self._status) if self._status else ""
+        return "%s%s" % (self.name, status_str)
+
+    __repr__ = _util.easy_repr
+
+    @classmethod
+    def from_arn(
+            cls,
+            arn: str,
+            *,
+            session: _util.AWSSession = None
+    ) -> "Execution":
+        """Construct an ``Execution`` from an existing execution.
+
+        Queries AWS Step Functions for the execution with the given ARN
+
+        Args:
+            arn: existing execution ARN
+            session: session to use for AWS communication
+
+        Returns:
+            described execution
+        """
+
+        session = session or _util.AWSSession()
+        resp = session.sfn.describe_execution(executionArn=arn)
+        assert resp["executionArn"] == arn
+        execution_input = _default
+        if "input" in resp:
+            execution_input = json.loads(resp["input"])
+        self = cls(
+            resp["name"],
+            resp["stateMachineArn"],
+            execution_input=execution_input,
+            arn=arn,
+            session=session)
+        self._status = resp["status"]
+        self._start_date = resp["startDate"]
+        self._stop_date = resp.get("stopDate")
+        if "output" in resp:
+            self._output = json.loads(resp["output"])
+        return self
+
+    @classmethod
+    def from_execution_list_item(
+            cls,
+            item: T.Dict[str, _util.JSONable],
+            *,
+            session: _util.AWSSession = None
+    ) -> "Execution":
+        """Construct an ``Execution`` from a response list-item.
+
+        Args:
+            item: execution list item
+            session: session to use for AWS communication
+
+        Returns:
+            described execution
+        """
+
+        self = cls(
+            item["name"],
+            item["stateMachineArn"],
+            cls._not_provided,
+            arn=item["executionArn"],
+            session=session)
+        self._status = item["status"]
+        self._start_date = item["startDate"]
+        self._stop_date = item.get("stopDate")
+        return self
+
+    @property
+    def status(self) -> str:
+        """Execution status."""
+        if self._status in (None, "RUNNING"):
+            self._update()
+        return self._status
+
+    @property
+    def start_date(self) -> datetime.datetime:
+        """Execution start time."""
+        if self._start_date is None:
+            self._update()
+        return self._start_date
+
+    @property
+    def stop_date(self) -> datetime.datetime:
+        """Execution stop time.
+
+        Raises:
+            RuntimeError: if execution is not yet finished
+        """
+
+        if self._stop_date is None:
+            self._update()
+            self._raise_unfinished()
+        return self._stop_date
+
+    @property
+    def output(self) -> _util.JSONable:
+        """Output of execution.
+
+        Raises:
+            RuntimeError: if execution is not yet finished, or execution
+                failed
+        """
+
+        if self._output == _default:
+            self._update()
+            self._raise_unfinished()
+            self._raise_on_failure()
+        return self._output
+
+    def _update(self):
+        """Update execution information from AWS.
+
+        Raises:
+            RuntimeError: if execution ARN is not known (must be provided)
+        """
+
+        status_known = self._status not in (None, "RUNNING")
+        input_known = self.execution_input != self._not_provided
+        if status_known and input_known:
+            _logger.debug("Execution finished: update is unnecessary")
+            return
+        self._raise_no_arn()
+        resp = self.session.sfn.describe_execution(executionArn=self.arn)
+        assert resp["executionArn"] == self.arn
+        self._status = resp["status"]
+        self._start_date = resp["startDate"]
+        self._stop_date = resp.get("stopDate")
+        if "input" in resp:
+            input_ = json.loads(resp["input"])
+            if self.execution_input == self._not_provided:
+                self.execution_input = input_
+            else:
+                assert self.execution_input == input_
+        if "output" in resp:
+            self._output = json.loads(resp["output"])
+
+    def _raise_on_failure(self):
+        """Raise ``RuntimeError`` on execution failure."""
+        failed = self._status not in ("RUNNING", "SUCCEEDED")
+        if failed:
+            raise RuntimeError("Execution '%s' %s" % (self, self._status))
+
+    def _raise_unfinished(self):
+        """Raise ``RuntimeError`` when requiring execution to be finished."""
+        if self._status == "RUNNING":
+            raise RuntimeError("Execution '%s' not yet finished" % self)
+
+    def _raise_no_arn(self):
+        """Raise ``RuntimeError`` when ARN is not provided."""
+        if self.arn is None:
+            raise RuntimeError("Execution '%s' ARN is unknown" % self)
+
+    def start(self):
+        """Start this state-machine execution.
+
+        Sets the ``arn`` attribute.
+        """
+
+        _util.assert_valid_name(self.name)
+        input_ = self.execution_input
+        if input_ == _default:
+            input_ = {}
+        resp = self.session.sfn.start_execution(
+            stateMachineArn=self.state_machine_arn,
+            name=self.name,
+            input=json.dumps(input_))
+        self.arn = resp["executionArn"]
+        self._status = "RUNNING"
+        self._start_date = resp["startDate"]
+
+    def wait(self, raise_on_failure: bool = True, timeout: float = None):
+        """Wait for execution to finish.
+
+        Args:
+            raise_on_failure: raise error when execution fails
+            timeout: time to wait for execution to finish (seconds),
+                default: no time-out
+
+        Raises:
+            RuntimeError: if execution fails, or if time-out is reached
+                before execution finishes
+        """
+
+        t = time.time()
+        while True:
+            self._update()
+            if self._status != "RUNNING":
+                break
+            if timeout is not None and time.time() - t > timeout:
+                raise RuntimeError("Time-out waiting on execution '%s'" % self)
+            time.sleep(self._wait_sleep_time)
+        if raise_on_failure:
+            self._raise_on_failure()
+
+    def stop(self, error_code: str = _default, details: str = _default):
+        """Stop an existing execution.
+
+        Args:
+            error_code: stop reason identification
+            details: stop reason
+
+        Raises:
+            RuntimeError: if execution is already finished
+        """
+
+        kw = {}
+        if error_code != _default:
+            kw["error"] = error_code
+        if details != _default:
+            kw["cause"] = details
+        self._raise_no_arn()
+        resp = self.session.sfn.stop_execution(executionArn=self.arn, **kw)
+        self._stop_date = resp["stopDate"]
+        _logger.info("Execution stopped on %s" % resp["stopDate"])
+
+    def get_history(self) -> T.List[history.Event]:
+        """List the execution history.
+
+        Returns:
+            history of execution events
+        """
+
+        self._raise_no_arn()
+        resp = _util.collect_paginated(
+            self.session.sfn.get_execution_history,
+            executionArn=self.arn)
+        return history.parse_history(resp["events"])
+
+    def format_history(self) -> str:
+        """Format the execution history for printing.
+
+        Returns:
+            history formatted
+        """
+
+        events = self.get_history()
+        lines = []
+        for event in events:
+            ds = event.details_str
+            line = ("%s:\n  %s" % (event, ds)) if ds else str(event)
+            lines.append(line)
+        self._update()
+        if self._output != _default:
+            line = "Output: %s" % json.dumps(self._output)
+            lines.append(line)
+        return "\n".join(lines)
---
+++ packages/sfini/sfini-0.1.0.tar.gz/sfini-0.1.0/src/sfini/execution/history.py
@@ -0,0 +1,513 @@
+# --- 80 characters -----------------------------------------------------------
+# Created by: Laurie 2018/08/09
+
+"""State-machine execution history events.
+
+Use ``sfini.execution.Execution.format_history`` for nice history
+printing.
+"""
+
+import json
+import typing as T
+import logging as lg
+
+from .. import _util
+
+_logger = lg.getLogger(__name__)
+_default = _util.DefaultParameter()
+_type_keys = {
+    "ActivityFailed": "activityFailedEventDetails",
+    "ActivityScheduleFailed": "activityScheduleFailedEventDetails",
+    "ActivityScheduled": "activityScheduledEventDetails",
+    "ActivityStarted": "activityStartedEventDetails",
+    "ActivitySucceeded": "activitySucceededEventDetails",
+    "ActivityTimedOut": "activityTimedOutEventDetails",
+    "ChoiceStateEntered": "stateEnteredEventDetails",
+    "ChoiceStateExited": "stateExitedEventDetails",
+    "ExecutionFailed": "executionFailedEventDetails",
+    "ExecutionStarted": "executionStartedEventDetails",
+    "ExecutionSucceeded": "executionSucceededEventDetails",
+    "ExecutionAborted": "executionAbortedEventDetails",
+    "ExecutionTimedOut": "executionTimedOutEventDetails",
+    "FailStateEntered": "stateEnteredEventDetails",
+    "LambdaFunctionFailed": "lambdaFunctionFailedEventDetails",
+    "LambdaFunctionScheduleFailed": "lambdaFunctionScheduleFailedEventDetails",
+    "LambdaFunctionScheduled": "lambdaFunctionScheduledEventDetails",
+    "LambdaFunctionStartFailed": "lambdaFunctionStartFailedEventDetails",
+    # "LambdaFunctionStarted": "lambdaFunctionStartedEventDetails",
+    "LambdaFunctionSucceeded": "lambdaFunctionSucceededEventDetails",
+    "LambdaFunctionTimedOut": "lambdaFunctionTimedOutEventDetails",
+    "SucceedStateEntered": "stateEnteredEventDetails",
+    "SucceedStateExited": "stateExitedEventDetails",
+    # "TaskStateAborted": "stateAbortedEventDetails",
+    "TaskStateEntered": "stateEnteredEventDetails",
+    "TaskStateExited": "stateExitedEventDetails",
+    "PassStateEntered": "stateEnteredEventDetails",
+    "PassStateExited": "stateExitedEventDetails",
+    # "ParallelStateAborted": "stateAbortedEventDetails",
+    "ParallelStateEntered": "stateEnteredEventDetails",
+    "ParallelStateExited": "stateExitedEventDetails",
+    "ParallelStateFailed": "stateFailedEventDetails",
+    # "ParallelStateStarted": "stateStartedEventDetails",
+    # "ParallelStateSucceeded": "stateSucceededEventDetails",
+    # "WaitStateAborted": "stateAbortedEventDetails",
+    "WaitStateEntered": "stateEnteredEventDetails",
+    "WaitStateExited": "stateExitedEventDetails"}
+
+
+class Event:
+    """An execution history event.
+
+    Args:
+        timestamp (datetime.datetime): event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        previous_event_id: identifying index of causal event
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type: str,
+            event_id: int,
+            previous_event_id: int = None):
+        self.timestamp = timestamp
+        self.event_type = event_type
+        self.event_id = event_id
+        self.previous_event_id = previous_event_id
+
+    def __str__(self):
+        fmt = "%s [%s] @ %s"
+        return fmt % (self.event_type, self.event_id, self.timestamp)
+
+    __repr__ = _util.easy_repr
+
+    @staticmethod
+    def _get_args(
+            history_event: T.Dict[str, _util.JSONable]
+    ) -> T.Tuple[tuple, T.Dict[str, T.Any], T.Dict[str, _util.JSONable]]:
+        """Get initialisation arguments by parsing history event.
+
+        Args:
+            history_event: execution history event, provided by AWS API
+
+        Returns:
+            initialisation positional and keyword arguments, and event details
+        """
+
+        # _logger.debug("history_event: %s" % history_event)
+        timestamp = history_event["timestamp"]
+        event_type = history_event["type"]
+        event_id = history_event["id"]
+        args = (timestamp, event_type, event_id)
+        kwargs = {}
+        if "previousEventId" in history_event:
+            kwargs["previous_event_id"] = history_event["previousEventId"]
+        details = history_event.get(_type_keys.get(event_type), {})
+        return args, kwargs, details
+
+    @classmethod
+    def from_history_event(
+            cls,
+            history_event: T.Dict[str, _util.JSONable]
+    ) -> "Event":
+        """Parse an history event.
+
+        Args:
+            history_event: execution history event date, provided by AWS API
+
+        Returns:
+            Event: constructed execution history event
+        """
+
+        args, kwargs, _ = cls._get_args(history_event)
+        return cls(*args, **kwargs)
+
+    @_util.cached_property
+    def details_str(self) -> str:
+        """Format the event details.
+
+        Returns:
+            str: event details, formatted as string
+        """
+
+        return ""
+
+
+class Failed(Event):
+    """An execution history failure event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        previous_event_id: identifying index of causal event
+        error: error type
+        cause: failure details
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=None,
+            error: str = None,
+            cause: str = None):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.error = error
+        self.cause = cause
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        if "error" in details:
+            kwargs["error"] = details["error"]
+        if "cause" in details:
+            kwargs["cause"] = details["cause"]
+        return args, kwargs, details
+
+    @_util.cached_property
+    def details_str(self):
+        return "error: %s" % self.error
+
+
+class LambdaFunctionScheduled(Event):
+    """An execution history AWS Lambda task-schedule event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        resource: AWS Lambda function ARN
+        previous_event_id: identifying index of causal event
+        task_input: task input
+        timeout: time-out (seconds) of task execution
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            resource: str,
+            previous_event_id=None,
+            task_input: _util.JSONable = _default,
+            timeout: int = None):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.resource = resource
+        self.task_input = task_input
+        self.timeout = timeout
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        args += (details["resource"],)
+        if "input" in details:
+            kwargs["task_input"] = json.loads(details["input"])
+        if "timeoutInSeconds" in details:
+            kwargs["timeout"] = details["timeoutInSeconds"]
+        return args, kwargs, details
+
+    @_util.cached_property
+    def details_str(self):
+        return "resource: %s" % self.resource
+
+
+class ActivityScheduled(LambdaFunctionScheduled):
+    """An execution history activity task-schedule event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        resource: AWS Lambda function ARN
+        previous_event_id: identifying index of causal event
+        task_input: task input
+        timeout: time-out (seconds) of task execution
+        heartbeat: heartbeat time-out (seconds)
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            resource,
+            previous_event_id=None,
+            task_input=_default,
+            timeout=None,
+            heartbeat: int = None):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            resource,
+            previous_event_id=previous_event_id,
+            task_input=task_input,
+            timeout=timeout)
+        self.heartbeat = heartbeat
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        if "heartbeatInSeconds" in details:
+            kwargs["heartbeat"] = details["heartbeatInSeconds"]
+        return args, kwargs, details
+
+
+class ActivityStarted(Event):
+    """An execution history activity task-start event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        worker_name: name of activity worker executing activity task
+        previous_event_id: identifying index of causal event
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            worker_name: str,
+            previous_event_id=None):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.worker_name = worker_name
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        args += (details["workerName"],)
+        return args, kwargs, details
+
+    @_util.cached_property
+    def details_str(self):
+        return "worker: %s" % self.worker_name
+
+
+class ObjectSucceeded(Event):
+    """An execution history succeed event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        previous_event_id: identifying index of causal event
+        output: output of state/execution
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=None,
+            output: _util.JSONable = _default):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.output = output
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        if "output" in details:
+            kwargs["output"] = json.loads(details["output"])
+        return args, kwargs, details
+
+
+class ExecutionStarted(Event):
+    """An execution history execution-start event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        previous_event_id: identifying index of causal event
+        execution_input: execution input
+        role_arn: execution AWS IAM role ARN
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=None,
+            execution_input: _util.JSONable = _default,
+            role_arn: str = None):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.execution_input = execution_input
+        self.role_arn = role_arn
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        if "input" in details:
+            kwargs["execution_input"] = json.loads(details["input"])
+        if "roleArn" in details:
+            kwargs["role_arn"] = details["roleArn"]
+        return args, kwargs, details
+
+
+class StateEntered(Event):
+    """An execution history state-enter event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        state_name: state name
+        previous_event_id: identifying index of causal event
+        state_input: state input
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            state_name: str,
+            previous_event_id=None,
+            state_input: _util.JSONable = _default):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.state_name = state_name
+        self.state_input = state_input
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        args += (details["name"],)
+        if "input" in details:
+            kwargs["state_input"] = json.loads(details["input"])
+        return args, kwargs, details
+
+    @_util.cached_property
+    def details_str(self):
+        return "name: %s" % self.state_name
+
+
+class StateExited(Event):
+    """An execution history state-exit event.
+
+    Args:
+        timestamp: event time-stamp
+        event_type: type of event
+        event_id: identifying index of event
+        state_name: state name
+        previous_event_id: identifying index of causal event
+        output: state output
+    """
+
+    def __init__(
+            self,
+            timestamp,
+            event_type,
+            event_id,
+            state_name: str,
+            previous_event_id=None,
+            output: _util.JSONable = _default):
+        super().__init__(
+            timestamp,
+            event_type,
+            event_id,
+            previous_event_id=previous_event_id)
+        self.state_name = state_name
+        self.output = output
+
+    @classmethod
+    def _get_args(cls, history_event):
+        args, kwargs, details = super()._get_args(history_event)
+        args += (details["name"],)
+        if "output" in details:
+            kwargs["output"] = json.loads(details["output"])
+        return args, kwargs, details
+
+    @_util.cached_property
+    def details_str(self):
+        return "name: %s" % self.state_name
+
+
+_type_classes = {
+    "ActivityFailed": Failed,
+    "ActivityScheduleFailed": Failed,
+    "ActivityScheduled": ActivityScheduled,
+    "ActivityStarted": ActivityStarted,
+    "ActivitySucceeded": ObjectSucceeded,
+    "ActivityTimedOut": Failed,
+    "ChoiceStateEntered": StateEntered,
+    "ChoiceStateExited": StateExited,
+    "ExecutionFailed": Failed,
+    "ExecutionStarted": ExecutionStarted,
+    "ExecutionSucceeded": ObjectSucceeded,
+    "ExecutionAborted": Failed,
+    "ExecutionTimedOut": Failed,
+    "FailStateEntered": StateEntered,
+    "LambdaFunctionFailed": Failed,
+    "LambdaFunctionScheduleFailed": Failed,
+    "LambdaFunctionScheduled": LambdaFunctionScheduled,
+    "LambdaFunctionStartFailed": Failed,
+    "LambdaFunctionStarted": Event,
+    "LambdaFunctionSucceeded": ObjectSucceeded,
+    "LambdaFunctionTimedOut": Failed,
+    "SucceedStateEntered": StateEntered,
+    "SucceedStateExited": StateExited,
+    "TaskStateAborted": Event,
+    "TaskStateEntered": StateEntered,
+    "TaskStateExited": StateExited,
+    "PassStateEntered": StateEntered,
+    "PassStateExited": StateExited,
+    "ParallelStateAborted": Event,
+    "ParallelStateEntered": StateEntered,
+    "ParallelStateExited": StateExited,
+    "ParallelStateFailed": Failed,
+    "ParallelStateStarted": Event,
+    "ParallelStateSucceeded": Event,
+    "WaitStateAborted": Event,
+    "WaitStateEntered": StateEntered,
+    "WaitStateExited": StateExited}
+
+
+def parse_history(
+        history_events: T.List[T.Dict[str, _util.JSONable]]
+) -> T.List[Event]:
+    """List the execution history.
+
+    Args:
+        history_events: history events as provided by AWS API
+
+    Returns:
+        history of execution events
+    """
+
+    events = []
+    for history_event in history_events:
+        eclass = _type_classes[history_event["type"]]
+        event = eclass.from_history_event(history_event)
+        events.append(event)
+    return events
