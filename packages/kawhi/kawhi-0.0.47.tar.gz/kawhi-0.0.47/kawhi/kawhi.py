3460f6a4ef8030408aa1039fab2e6efdfc2fbd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:21 +0000

    Add package kawhi/0.0.47/kawhi-0.0.47.tar.gz

---
+++ packages/kawhi/kawhi-0.0.47.tar.gz/kawhi-0.0.47/kawhi/kawhi.py
@@ -0,0 +1,30 @@
+"""
+Kawhi package test pypi package publishing
+"""
+from .support_modules import get_joke
+
+
+def say_hello(name=None):
+    """ dummy function"""
+    if name:
+        return f"Hello, Kawhi - This is {name}."
+
+    return "Hello, Kawhi!"
+
+
+def kawhi_joke(search_term=None):
+    """
+    Gets a joke and appends a Kawhi laugh to it.
+    """
+
+    if search_term:
+        # TODO get joke about a specific topic
+        pass
+
+    returned_joke = get_joke()
+
+    if not returned_joke["success"]:
+        return "That's all folks!"
+
+    # return joke:
+    return f"{returned_joke['joke']}\n\nAha....ha, ha, ha, ha"
