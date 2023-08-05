f1fdb1b1da501b57bee0885883ef9f163a9cb94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:02 +0000

    Add package pycrow/0.1.0/pycrow-0.1.0-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/pycrow/pycrow-0.1.0-cp36-cp36m-manylinux1_x86_64.whl/pycrow/__init__.py
@@ -0,0 +1,43 @@
+import pycrow
+import pycrow.libcrow
+
+from pycrow.libcrow import *
+
+crowker = None
+
+def spin():
+	while 1:
+		pycrow.libcrow.onestep()
+
+def diagnostic(pack=False, live=False):
+	if pack:
+		pycrow.libcrow.diagnostic()
+
+	if live:
+		pycrow.libcrow.live_diagnostic()
+
+def incoming_handler(func):
+	pycrow.libcrow.set_incoming_handler(func)
+
+subs = dict()
+subs_init = False
+
+def subscribe_handler_impl(packet):
+	thm = packet.theme()
+	subs[thm](packet)
+
+def subscribe(theme, handler, **kwargs):
+	subs[theme] = handler
+
+	if not subs_init:
+		pycrow.libcrow.set_subscribe_handler(subscribe_handler_impl)
+
+	return pycrow.libcrow.subscribe(crowker, theme, **kwargs)
+
+
+def use_environment_crowker():
+	crowker = pycrow.libcrow.environment_crowker()
+
+def set_crowker(cker):
+	global crowker
+	crowker = pycrow.libcrow.compile_address(cker)
