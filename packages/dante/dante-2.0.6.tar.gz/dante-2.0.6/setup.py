4603ae5c5b583ad5e9182c8f9c1071c367dfd35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package dante/2.0.6/dante-2.0.6.tar.gz

---
+++ packages/dante/dante-2.0.6.tar.gz/dante-2.0.6/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup, find_packages
+
+
+README_FILE = 'README.md'
+DESCRIPTION = LONG_DESCRIPTION = 'Python dependency management utility'
+if os.path.isfile(README_FILE):
+    with open(README_FILE, 'r', encoding='utf-8') as readme_file:
+        LONG_DESCRIPTION = readme_file.read()
+
+VERSION = os.getenv('TRAVIS_TAG', os.getenv('VERSION', '0.0.1+local-build'))
+
+setup(
+    name='dante',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    platforms=['Windows', 'POSIX', 'MacOs'],
+    maintainer='Seven Bridges Genomics Inc.',
+    maintainer_email='quicksilver.machine@gmail.com',
+    url='https://github.com/sbg/dante',
+    license='Apache Software License 2.0',
+    install_requires=[],
+    extras_require={},
+    packages=find_packages(exclude=['tests*']),
+    keywords=[
+        'lock', 'dependency', 'pip',
+        'requirement', 'sbg', 'sevenbridges'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dante = dante.__main__:main'
+        ]
+    }
+)
