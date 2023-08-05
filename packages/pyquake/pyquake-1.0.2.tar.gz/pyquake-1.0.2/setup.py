20d25f434b151672c253f7e4e19e595d12cf72e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:20 +0000

    Add package pyquake/1.0.2/pyquake-1.0.2.tar.gz

---
+++ packages/pyquake/pyquake-1.0.2.tar.gz/pyquake-1.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="pyquake",
+    version="1.0.2",
+    author="Cooper Barth",
+    author_email="cooperfbarth@gmail.com",
+    description="A package enabling the synthesis of seismic audio.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/cooperbarth/PyQuake",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
