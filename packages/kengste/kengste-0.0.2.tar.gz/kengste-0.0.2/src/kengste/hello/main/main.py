12f8a7a70f18ae658117545856a8628a7793ab2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:52 +0000

    Add package kengste/0.0.2/kengste-0.0.2.tar.gz

---
+++ packages/kengste/kengste-0.0.2.tar.gz/kengste-0.0.2/src/kengste/hello/main/main.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import sys
+
+from kengste.hello.greet.greet import greet
+
+
+if __name__ == '__main__':
+  greetees = sys.argv[1:] or ['world']
+  for greetee in greetees:
+    print(greet(greetee))
+
+
+class Class1:
+
+  def show():
+    multi_line_string = """This is the first line.
+    Class 1.
+    and this is the second line
+    and this is the third line"""
+    print(multi_line_string)
+
+def show2():
+  multi_line_string = """This is the first line.
+  Class 1.
+  and this is the second line
+  and this is the third line"""
+  print(multi_line_string)
