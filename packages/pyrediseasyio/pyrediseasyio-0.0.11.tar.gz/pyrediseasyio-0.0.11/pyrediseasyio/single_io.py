40e9d75afa394f75317ece49b499f84824e1a26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:17 +0000

    Add package pyrediseasyio/0.0.11/pyrediseasyio-0.0.11.tar.gz

---
+++ packages/pyrediseasyio/pyrediseasyio-0.0.11.tar.gz/pyrediseasyio-0.0.11/pyrediseasyio/single_io.py
@@ -0,0 +1,142 @@
+from pyrediseasyio.abstract_reader_writer import AbstractReaderWriter
+import threading
+from str2bool import str2bool
+import json
+
+lock = threading.Lock()
+
+
+class SingleIO:
+    def __init__(self, name: str, addr: str, default: object, units: str = None, reader: AbstractReaderWriter = None):
+        self.name = name
+        self.addr = addr
+        self._reader_writer = reader
+        self.default = default
+        self.units = units
+
+    def __and__(self, other):
+        if hasattr(other, 'value'):
+            return self.value and other.value
+        return self.value and other
+
+    def __add__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value + other
+
+    def __sub__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value - other
+
+    def __mul__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value * other
+
+    def __truediv__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value / other
+
+    def __floordiv__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value // other
+
+    def __eq__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value == other
+
+    def __ne__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value != other
+
+    def __get__(self, instance, owner):
+        self.read()
+        return self
+
+    def __set__(self, obj, value):
+        self.write(value)
+
+    def __str__(self):
+        return f'[{type(self).__name__}] {self.name} = {self.value} {self.units}'
+
+    @property
+    def value(self):
+        return self.read()
+
+    @staticmethod
+    def _convert_type(value):
+        return value
+
+    def publish(self, value, channel: str = None):
+        value = self._convert_type(value)
+        data = json.dumps({self.addr: value})
+        self._reader_writer.publish(data, channel)
+
+    def read(self):
+        if self._reader_writer is None:
+            return None
+        with lock:
+            val = self._reader_writer.read(self.addr)
+            if val is None:
+                val = self.default
+            val = self._convert_type(val)
+            return val
+
+    def write(self, value):
+        if self._reader_writer is None:
+            return
+        with lock:
+            value = self._convert_type(value)
+            self._reader_writer.write(self.addr, value)
+
+
+class BooleanIO(SingleIO):
+    def __init__(self, name: str, addr: str, default: bool = False, units: str = None, reader: AbstractReaderWriter = None):
+        super().__init__(name, addr, default, units, reader)
+
+    @staticmethod
+    def _convert_type(value):
+        if isinstance(value, str):
+            return str2bool(value)
+        return bool(value)
+
+    def __bool__(self):
+        return self.value
+    
+    @property
+    def value(self) -> bool:
+        return super().value
+
+
+class IntIO(SingleIO):
+    def __init__(self, name: str, addr: str, default: int = 0, units: str = None, reader: AbstractReaderWriter = None):
+        super().__init__(name, addr, default, units, reader)
+
+    @staticmethod
+    def _convert_type(value):
+        return int(value)
+
+
+class FloatIO(SingleIO):
+    def __init__(self, name: str, addr: str, default: float = 0, units: str = None, reader: AbstractReaderWriter = None):
+        super().__init__(name, addr, default, units, reader)
+
+    @staticmethod
+    def _convert_type(value):
+        return float(value)
+
+
+class StringIO(SingleIO):
+    def __init__(self, name: str, addr: str, default: str = '', units: str = None, reader: AbstractReaderWriter = None):
+        super().__init__(name, addr, default, units, reader)
+
+    @staticmethod
+    def _convert_type(value):
+        return str(value)
+
+
