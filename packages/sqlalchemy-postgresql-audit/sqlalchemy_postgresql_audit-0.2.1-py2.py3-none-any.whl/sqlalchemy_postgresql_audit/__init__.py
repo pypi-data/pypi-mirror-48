e886e53834e2c7ec9c4285e69a994db762aabaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package sqlalchemy-postgresql-audit/0.2.1/sqlalchemy_postgresql_audit-0.2.1-py2.py3-none-any.whl

---
+++ packages/sqlalchemy-postgresql-audit/sqlalchemy_postgresql_audit-0.2.1-py2.py3-none-any.whl/sqlalchemy_postgresql_audit/__init__.py
@@ -0,0 +1,45 @@
+__version__ = "0.2.1"
+
+
+def install_audit_triggers(metadata, engine=None):
+    audit_table_ddl = [
+        t.info["audit.create_ddl"]
+        for t in metadata.tables.values()
+        if t.info.get("audit.is_audit_table")
+    ]
+
+    engine = engine or metadata.bind
+
+    if engine:
+        for ddl in audit_table_ddl:
+            engine.execute(ddl)
+    else:
+        return "; ".join(audit_table_ddl)
+
+
+def uninstall_audit_triggers(metadata, engine=None):
+    audit_table_ddl = [
+        t.info["audit.drop_ddl"]
+        for t in metadata.tables.values()
+        if t.info.get("audit.is_audit_table")
+    ]
+
+    engine = engine or metadata.bind
+
+    if engine:
+        for ddl in audit_table_ddl:
+            engine.execute(ddl)
+    else:
+        return ";\n ".join(audit_table_ddl)
+
+
+def set_session_var_stmt(**kwargs):
+    format_str = "set local {} = {}"
+    stmts = []
+    for key, value in kwargs.items():
+        stmts.append(format_str.format('"audit.{}"'.format(key), "'{}'".format(value)))
+    return "; ".join(stmts) + ";"
+
+
+def set_session_vars(engine, **kwargs):
+    engine.execute(set_session_var_stmt(**kwargs))
