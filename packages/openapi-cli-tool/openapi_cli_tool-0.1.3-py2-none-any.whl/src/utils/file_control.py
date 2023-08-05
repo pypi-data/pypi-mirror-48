3991d77eb98d20e1149f682fe30967a5f32264f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:43 +0000

    Add package openapi-cli-tool/0.1.3/openapi_cli_tool-0.1.3-py2-none-any.whl

---
+++ packages/openapi-cli-tool/openapi_cli_tool-0.1.3-py2-none-any.whl/src/utils/file_control.py
@@ -0,0 +1,31 @@
+import os
+import json
+import yaml
+
+
+def load_file(file_path):
+    try:
+        r = open(file_path, 'r')
+        content = r.read()
+        r.close()
+        return content
+    except:
+        return ''
+
+
+def load_dict_from_file(file_path):
+    try:
+        r = open(file_path, 'r')
+        content = r.read()
+        r.close()
+        _, file_extension = os.path.splitext(file_path)
+        if not os.path.exists(file_path):
+            raise Exception('Faild not found.')
+        if file_extension == '.json':
+            return json.loads(content)
+        elif file_extension in ['.yaml', '.yml']:
+            return yaml.safe_load(content)
+        else:
+            raise Exception('Unknown extension.')
+    except Exception as e:
+        raise Exception('File Read Error (' + file_path + '): ' + str(e))
