b0207b1d73353d13aff82fb13619ce5c39bbd4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:50 +0000

    Add package maup/0.6/maup-0.6-py3-none-any.whl

---
+++ packages/maup/maup-0.6-py3-none-any.whl/maup/__init__.py
@@ -0,0 +1,18 @@
+from .adjacencies import adjacencies
+from .assign import assign
+from .indexed_geometries import IndexedGeometries
+from .intersections import intersections, prorate
+from .repair import close_gaps, resolve_overlaps
+from .normalize import normalize
+
+__version__ = "0.6"
+__all__ = [
+    "assign",
+    "intersections",
+    "prorate",
+    "adjacencies",
+    "close_gaps",
+    "resolve_overlaps",
+    "IndexedGeometries",
+    "normalize",
+]
