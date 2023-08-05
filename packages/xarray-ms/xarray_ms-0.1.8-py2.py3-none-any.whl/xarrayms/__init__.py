835dac29179d8fe4d5917d49e29e5cada5db6078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:28 +0000

    Add package xarray-ms/0.1.8/xarray_ms-0.1.8-py2.py3-none-any.whl

---
+++ packages/xarray-ms/xarray_ms-0.1.8-py2.py3-none-any.whl/xarrayms/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__author__ = """Simon Perkins"""
+__email__ = 'sperkins@ska.ac.za'
+__version__ = '0.1.8'
+
+from xarrayms.xarray_ms import (xds_from_table,  # noqa
+                                xds_to_table,    # noqa
+                                xds_from_ms,     # noqa
+                                TableProxy)      # noqa
