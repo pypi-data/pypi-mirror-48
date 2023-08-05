8126ad598a0ab45bcb85e205ab3f99db75eebf6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:29 +0000

    Add package git-joke-cli/1.1.0/git-joke-cli-1.1.0.tar.gz

---
+++ packages/git-joke-cli/git-joke-cli-1.1.0.tar.gz/git-joke-cli-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+tests_require = ['pytest']
+
+setup(name='git-joke-cli',
+      version='1.1.0',
+      description='Get git joke in command line',
+      long_description=open("README.rst").read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/vitakrya/git-joke-cli',
+      author='Vita Kr',
+      license='MIT',
+      packages=['gitjoke'],
+      scripts=['bin/git-joke'],
+      install_requires=[
+          'requests',
+          'docopt',
+      ],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'responses'],
+      zip_safe=False)
