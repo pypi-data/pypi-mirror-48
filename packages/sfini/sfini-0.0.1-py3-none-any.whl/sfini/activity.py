8f0f2f3a2819d6833adcedee240f92a85b488911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:48 +0000

    Add package sfini/0.0.1/sfini-0.0.1-py3-none-any.whl

---
+++ packages/sfini/sfini-0.0.1-py3-none-any.whl/sfini/activity.py
@@ -0,0 +1,334 @@
+# --- 80 characters -----------------------------------------------------------
+# Created by: Laurie 2018/07/12
+
+"""Activity interfacing.
+
+Activities are separate from state-machines, and are used as
+implementations of 'Task' states. Activities are registered separately.
+"""
+
+import inspect
+import typing as T
+import logging as lg
+import functools as ft
+
+from . import _util
+from . import task_resource as sfini_task_resource
+
+_logger = lg.getLogger(__name__)
+
+
+class Activity(sfini_task_resource.TaskResource):
+    """Activity execution.
+
+    Note that activity names must be unique (within a region). It's
+    recommended to put your code's title and version in the activity name.
+    ``Activities`` makes this straight-forward.
+
+    An activity is attached to state-machine tasks, and is called when that
+    task is executed. A worker registers itself able to run some
+    activities using their names.
+
+    Args:
+        name: name of activity
+        session: session to use for AWS communication
+    """
+
+    service = "activity"
+
+    def register(self):
+        """Register activity with AWS SFN."""
+        _logger.debug("Registering activity '%s' on SFN" % self)
+        _util.assert_valid_name(self.name)
+        resp = self.session.sfn.create_activity(name=self.name)
+        assert resp["activityArn"] == self.arn
+        fmt = "Activity '%s' registered with ARN '%s' at %s"
+        _logger.info(fmt % (self, self.arn, resp["creationDate"]))
+
+    def is_registered(self) -> bool:
+        """See if this activity is registered with AWS SFN.
+
+        Returns:
+            if this activity is registered
+        """
+
+        _logger.debug("Testing for registration of '%s' on SFN" % self)
+        resp = _util.collect_paginated(self.session.sfn.list_activities)
+        arns = {sm["activityArn"] for sm in resp["activities"]}
+        return self.arn in arns
+
+    def deregister(self):
+        """Remove activity from AWS SFN."""
+        _logger.info("Deleting activity '%s' from SFN" % self)
+        self.session.sfn.delete_activity(activityArn=self.arn)
+
+
+class CallableActivity(Activity):
+    """Activity execution defined by a callable.
+
+    Note that activity names must be unique (within a region). It's
+    recommended to put your application's name and version in the activity
+    name. ``ActivityRegistration`` makes this straight-forward.
+
+    An activity is attached to state-machine tasks, and is called when that
+    task is executed. A worker registers itself able to run an activity
+    using the registered activity name.
+
+    Args:
+        name: name of activity
+        fn: function to run activity
+        heartbeat: seconds between heartbeat during activity running
+        session: session to use for AWS communication
+    """
+
+    def __init__(self, name, fn: T.Callable, heartbeat=20, *, session=None):
+        super().__init__(name, session=session)
+        self.fn = fn
+        self.heartbeat = heartbeat
+
+    def __call__(self, task_input: _util.JSONable, *args, **kwargs):
+        return self.fn(task_input, *args, **kwargs)
+
+    @classmethod
+    def decorate(
+            cls,
+            name: str,
+            heartbeat: int = 20,
+            *,
+            session: _util.AWSSession = None
+    ) -> T.Callable[[T.Callable], "CallableActivity"]:
+        """Decorate a callable as an activity implementation.
+
+        Args:
+            name: name of activity
+            heartbeat: seconds between heartbeat during activity running
+            session: session to use for AWS communication
+        """
+
+        def wrap(fn: T.Callable):
+            activity = cls(name, fn, heartbeat=heartbeat, session=session)
+            return ft.update_wrapper(activity, fn)
+        return wrap
+
+    def call_with(self, task_input: _util.JSONable) -> _util.JSONable:
+        """Call with task-input context.
+
+        Args:
+            task_input: task input
+
+        Returns:
+            function return-value
+        """
+
+        return self.fn(task_input)
+
+
+class SmartCallableActivity(CallableActivity):
+    """Activity execution defined by a callable, processing input.
+
+    The arguments to ``fn`` are extracted from the input provided by AWS
+    Step Functions.
+
+    Note that activity names must be unique (within a region). It's
+    recommended to put your application's name and version in the activity
+    name. ``ActivityRegistration`` makes this straight-forward.
+
+    An activity is attached to state-machine tasks, and is called when that
+    task is executed. A worker registers itself able to run an activity
+    using the registered activity name.
+
+    Args:
+        name: name of activity
+        fn: function to run activity
+        heartbeat: seconds between heartbeat during activity running
+        session: session to use for AWS communication
+
+    Attributes:
+        sig: function signature
+    """
+
+    def __init__(self, name, fn: T.Callable, heartbeat=20, *, session=None):
+        super().__init__(name, fn, heartbeat=heartbeat, session=session)
+        self.sig: inspect.Signature = inspect.Signature.from_callable(fn)
+
+    def __call__(self, *args, **kwargs):
+        return self.fn(*args, **kwargs)
+
+    def _get_input_from(
+            self,
+            task_input: T.Dict[str, _util.JSONable]
+    ) -> T.Dict[str, _util.JSONable]:
+        """Parse task input for execution input.
+
+        Does not perform input validation: ``fn(**kwargs)`` in
+        ``call_with`` will do that anyway.
+
+        Args:
+            task_input: task input
+
+        Returns:
+            activity input
+        """
+
+        kinds = {n: p.kind for n, p in self.sig.parameters.items()}
+        if any(k == inspect.Parameter.VAR_KEYWORD for k in kinds.values()):
+            return task_input
+
+        var_pos = inspect.Parameter.VAR_POSITIONAL
+        kwargs = {}
+        for name, arg_val in task_input.items():
+            if kinds.get(name, var_pos) != var_pos:
+                kwargs[name] = arg_val
+        return kwargs
+
+    def call_with(self, task_input: T.Dict[str, _util.JSONable]):
+        kwargs = self._get_input_from(task_input)
+        return self.fn(**kwargs)
+
+
+class ActivityRegistration:
+    """Activities registration.
+
+    Provides convenience for grouping activities, generating activity
+    names, bulk-registering activities, and activity function decoration.
+
+    An activity is attached to state-machine tasks, and is called when that
+    task is executed. A worker registers itself able to run an activity
+    using the registered activity name.
+
+    Args:
+        prefix: prefix for activity names
+        session: session to use for AWS communication
+
+    Attributes:
+        activities: registered activities
+
+    Example:
+        >>> activities = ActivityRegistration(prefix="foo")
+        >>> @activities.activity(name="MyActivity")
+        >>> def fn(data):
+        ...     print("hi")
+        >>> print(fn.name)
+        fooMyActivity
+    """
+
+    _activity_class = CallableActivity
+    _smart_activity_class = SmartCallableActivity
+
+    def __init__(self, prefix: str = "", *, session: _util.AWSSession = None):
+        self.prefix = prefix
+        self.session = session or _util.AWSSession()
+        self.activities: T.Dict[str, Activity] = {}
+
+    def __str__(self):
+        return "'%s' activities" % self.prefix
+
+    __repr__ = _util.easy_repr
+
+    def add_activity(self, activity: Activity):
+        """Add an activity to the group.
+
+        Args:
+            activity: activity to add
+
+        Raises:
+            ValueError: if activity name already in-use in group
+        """
+
+        if activity.name in self.activities:
+            raise ValueError("Activity '%s' already in group" % activity.name)
+        self.activities[activity.name] = activity
+
+    def _activity(
+            self,
+            activity_cls: T.Type[CallableActivity],
+            name: str = None,
+            heartbeat: int = 20
+    ) -> T.Callable[[T.Callable], CallableActivity]:
+        """Activity function decorator.
+
+        Args:
+            activity_cls: activity class
+            name: name of activity, default: function name
+            heartbeat: seconds between heartbeat during activity running
+        """
+
+        def wrap(fn):
+            suff = fn.__name__ if name is None else name
+            activity = activity_cls.decorate(
+                self.prefix + suff,
+                heartbeat=heartbeat,
+                session=self.session)(fn)
+            self.add_activity(activity)
+            return activity
+        return wrap
+
+    def activity(
+            self,
+            name: str = None,
+            heartbeat: int = 20
+    ) -> T.Callable[[T.Callable], CallableActivity]:
+        """Activity function decorator.
+
+        The decorated function will be passed one argument: the input to
+        the task state that executes the activity.
+
+        Args:
+            name: name of activity, default: function name
+            heartbeat: seconds between heartbeat during activity running
+        """
+
+        return self._activity(
+            self._activity_class,
+            name=name,
+            heartbeat=heartbeat)
+
+    def smart_activity(
+            self,
+            name: str = None,
+            heartbeat: int = 20
+    ) -> T.Callable[[T.Callable], SmartCallableActivity]:
+        """Smart activity function decorator.
+
+        The decorated function will be passed values to its parameters
+        from the input to the task state that executes the activity.
+
+        Args:
+            name: name of activity, default: function name
+            heartbeat: seconds between heartbeat during activity running
+        """
+
+        return self._activity(
+            self._smart_activity_class,
+            name=name,
+            heartbeat=heartbeat)
+
+    def register(self):
+        """Add registered activities to AWS SFN."""
+        for activity in self.activities.values():
+            activity.register()
+
+    def _list_activities(self) -> T.List[T.Tuple[str, str, str]]:
+        """List activities in SFN."""
+        resp = _util.collect_paginated(self.session.sfn.list_activities)
+        acts = []
+        for act in resp["activities"]:
+            prefix = act["name"][:len(self.prefix)]
+            if prefix != self.prefix and act["name"] not in self.activities:
+                continue
+            acts.append((act["name"], act["activityArn"], act["creationDate"]))
+        return acts
+
+    def _deregister_activities(
+            self,
+            activity_items: T.Sequence[T.Tuple[str, str, str]]):
+        """Deregister activities."""
+        _logger.info("Deregistering %d activities" % len(activity_items))
+        for act in activity_items:
+            _logger.debug("Deregistering '%s'" % act[0])
+            self.session.sfn.delete_activity(activityArn=act[1])
+
+    def deregister(self):
+        """Remove activities in AWS SFN."""
+        acts = self._list_activities()
+        self._deregister_activities(acts)
---
+++ packages/sfini/sfini-0.0.1-py3-none-any.whl/sfini/state_machine.py
@@ -0,0 +1,269 @@
+# --- 80 characters -----------------------------------------------------------
+# Created by: Laurie 2018/07/11
+
+"""State-machine interfacing.
+
+A state-machine defines the logic for a workflow of an application. It
+is comprised of states (ie stages), and executions of which will run
+the workflow over some given data.
+"""
+
+import json
+import uuid
+import datetime
+import typing as T
+import logging as lg
+
+from . import _util
+from . import execution as sfini_execution
+from . import state as sfini_state
+
+_logger = lg.getLogger(__name__)
+_default = _util.DefaultParameter()
+
+
+class StateMachine:
+    """State machine structure for AWS Step Functions.
+
+    Args:
+        name: name of state-machine
+        states: state-machine states
+        start_state: name of start state
+        comment: description of state-maching
+        timeout: execution time-out (seconds)
+        session: session to use for AWS communication
+    """
+
+    _execution_class = sfini_execution.Execution
+
+    def __init__(
+            self,
+            name: str,
+            states: T.Dict[str, sfini_state.State],
+            start_state: str,
+            comment: str = _default,
+            timeout: int = _default,
+            *,
+            session: _util.AWSSession = None):
+        self.name = name
+        self.states = states
+        self.start_state = start_state
+        self.comment = comment
+        self.timeout = timeout
+        self.session = session or _util.AWSSession()
+
+    def __str__(self):
+        return "'%s' (%d states)" % (self.name, len(self.states))
+
+    __repr__ = _util.easy_repr
+
+    @_util.cached_property
+    def arn(self) -> str:
+        """State-machine generated ARN."""
+        region = self.session.region
+        account = self.session.account_id
+        fmt = "arn:aws:states:%s:%s:stateMachine:%s"
+        return fmt % (region, account, self.name)
+
+    @_util.cached_property
+    def default_role_arn(self) -> str:
+        """``sfini``-generated state-machine IAM role ARN."""
+        return "arn:aws:iam::%s:role/sfiniGenerated" % self.session.account_id
+
+    def to_dict(self) -> T.Dict[str, _util.JSONable]:
+        """Convert this state-machine to a definition dictionary.
+
+        Returns:
+            definition
+        """
+
+        _logger.debug("Converting '%s' to dictionary" % self)
+        state_defns = {n: s.to_dict() for n, s in self.states.items()}
+        defn = {"StartAt": self.start_state, "States": state_defns}
+        if self.comment != _default:
+            defn["Comment"] = self.comment
+        if self.timeout != _default:
+            defn["TimeoutSeconds"] = self.timeout
+        return defn
+
+    def is_registered(self) -> bool:
+        """See if this state-machine is registered with AWS SFN.
+
+        Returns:
+            if this state-machine is registered
+        """
+
+        _logger.debug("Testing for registration of '%s' on SFN" % self)
+        resp = _util.collect_paginated(self.session.sfn.list_state_machines)
+        arns = {sm["stateMachineArn"] for sm in resp["stateMachines"]}
+        return self.arn in arns
+
+    def _sfn_create(self, role_arn: str):
+        """Create this state-machine in AWS SFN.
+
+        Args:
+            role_arn: state-machine IAM role ARN
+        """
+
+        _logger.info("Creating '%s' on SFN" % self)
+        resp = self.session.sfn.create_state_machine(
+            name=self.name,
+            definition=json.dumps(self.to_dict(), indent=4),
+            roleArn=role_arn)
+        assert resp["stateMachineArn"] == self.arn
+        fmt = "State-machine '%s' registered with ARN '%s' at %s"
+        _logger.info(fmt % (self, self.arn, resp["creationDate"]))
+
+    def _sfn_update(self, role_arn: str = None):
+        """Update this state-machine in AWS SFN.
+
+        Args:
+            role_arn: state-machine IAM role ARN to update to
+        """
+
+        _logger.info("Updating '%s' on SFN" % self)
+        kwargs = {} if role_arn is None else {"roleArn": role_arn}
+        resp = self.session.sfn.update_state_machine(
+            definition=json.dumps(self.to_dict(), indent=4),
+            stateMachineArn=self.arn,
+            **kwargs)
+        _logger.info("'%s' updated at %s" % (self, resp["updateDate"]))
+
+    def register(self, role_arn: str = None, allow_update: bool = False):
+        """Register state-machine with AWS SFN.
+
+        Args:
+            role_arn: state-machine IAM role ARN
+            allow_update: allow overwriting of an existing state-machine with
+                the same name
+        """
+
+        _util.assert_valid_name(self.name)
+        if allow_update and self.is_registered():
+            self._sfn_update(role_arn)
+        else:
+            self._sfn_create(role_arn or self.default_role_arn)
+
+    def deregister(self):
+        """Remove state-machine from AWS SFN."""
+        _logger.info("Deleting state-machine '%s' from SFN" % self)
+        self.session.sfn.delete_state_machine(stateMachineArn=self.arn)
+
+    def start_execution(
+            self,
+            execution_input: _util.JSONable
+    ) -> _execution_class:
+        """Start an execution.
+
+        Args:
+            execution_input: input to first state in state-machine
+
+        Returns:
+            started execution
+        """
+
+        fmt = "Starting execution of '%s' with: %s"
+        _logger.info(fmt % (self, execution_input))
+
+        _now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        name = self.name + "_" + _now + "_" + str(uuid.uuid4())[:8]
+        execution = self._execution_class(
+            name,
+            self.arn,
+            execution_input,
+            session=self.session)
+        execution.start()
+        return execution
+
+    def _build_executions(
+            self,
+            items: T.List[T.Dict[str, _util.JSONable]]
+    ) -> T.List[_execution_class]:
+        """Build executions from response list-items.
+
+        This state-machine is manually attached to the ``state_machine``
+        attribute of the resultant executions here.
+
+        Args:
+            items: execution list-items
+
+        Returns:
+            constructed executions
+        """
+
+        executions = []
+        for item in items:
+            assert item["stateMachineArn"] == self.arn
+            execution = self._execution_class.from_execution_list_item(
+                item,
+                session=self.session)
+            execution.state_machine = self
+            executions.append(execution)
+
+            fmt = "Found execution '%s' with stop-date: %s"
+            _logger.debug(fmt % (execution, item.get("stopDate")))
+        return executions
+
+    def list_executions(self, status: str = None) -> T.List[_execution_class]:
+        """List all executions of this state-machine.
+
+        This state-machine is manually attached to the ``state_machine``
+        attribute of the resultant executions here.
+
+        Args:
+            status: only list executions with this status. Choose from
+                'RUNNING', 'SUCCEEDED', 'FAILED', 'TIMED_OUT' or 'ABORTED'
+
+        Returns:
+            executions of this state-machine
+        """
+
+        _s = " with status '%s'" % status if status else ""
+        _logger.info("Listing executions of '%s'" % self + _s)
+
+        kwargs = {"stateMachineArn": self.arn}
+        if status is not None:
+            kwargs["statusFilter"] = status
+        fn = self.session.sfn.list_executions
+        resp = _util.collect_paginated(fn, **kwargs)
+        return self._build_executions(resp["executions"])
+
+
+def construct_state_machine(
+        name: str,
+        start_state: sfini_state.State,
+        comment: str = _default,
+        timeout: int = _default,
+        *,
+        session: _util.AWSSession = None
+) -> StateMachine:
+    """Construct a state-machine from the starting state.
+
+    Make sure to construct the state-machine after all states have been
+    defined: subsequent states will need to be added to the state-machine
+    manually.
+
+    Only states referenced by the provided first state (and their children)
+    will be in the state-machine definition. Add states via an impossible
+    choice rule to include them in the definition.
+
+    Args:
+        name: name of state-machine
+        start_state: starting state of state-machine
+        comment: description of state-maching
+        timeout: execution time-out (seconds)
+        session: session to use for AWS communication
+
+    Returns:
+        constructed state-machine
+    """
+
+    states = {}
+    start_state.add_to(states)
+    return StateMachine(
+        name,
+        states,
+        start_state.name,
+        comment=comment,
+        timeout=timeout,
+        session=session)
