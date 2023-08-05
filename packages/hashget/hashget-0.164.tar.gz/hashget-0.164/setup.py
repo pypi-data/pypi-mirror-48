06a57045268ddb93c9815e623bbabf8d85b272fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:30 +0000

    Add package hashget/0.164/hashget-0.164.tar.gz

---
+++ packages/hashget/hashget-0.164.tar.gz/hashget-0.164/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+import os
+import sys
+
+# check if we run under python 3+
+if sys.version_info.major < 3 or (sys.version_info.major == 3 and sys.version_info.minor < 4):
+    raise ValueError('Need python 3.4 or higher')
+
+
+# all this magic just to import one symbol __version__ without loading whole module itself
+import types
+import importlib.machinery
+loader = importlib.machinery.SourceFileLoader('ver', 'hashget/version.py')
+vermod = types.ModuleType(loader.name)
+loader.exec_module(vermod)
+
+sys.path.insert(0, '.')
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='hashget',
+    version=vermod.__version__,
+    packages=['hashget', 'hashget.heuristics.hint', 'hashget.heuristics.debian'],
+    scripts=['bin/hashget', 'bin/hashget-admin'],
+
+    install_requires=['patool','filetype','filelock','setuptools', 'requests'],
+
+    url='https://gitlab.com/yaroslaff/hashget',
+    license='MIT',
+    author='Yaroslav Polyakov',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author_email='yaroslaff@gmail.com',
+    description='deduplication tool for archiving data with extremely high ratio',
+
+    extras_require={
+        'plugins': ["hashget-kernel_org"],
+    },
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.4',
+    ],
+)
