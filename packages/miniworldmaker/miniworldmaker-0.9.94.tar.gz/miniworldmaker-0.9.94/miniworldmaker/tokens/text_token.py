73fe67f23af736179bc3de14e19151081917f5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:28 +0000

    Add package miniworldmaker/0.9.94/miniworldmaker-0.9.94.tar.gz

---
+++ packages/miniworldmaker/miniworldmaker-0.9.94.tar.gz/miniworldmaker-0.9.94/miniworldmaker/tokens/text_token.py
@@ -0,0 +1,16 @@
+from miniworldmaker.tokens import token
+
+
+class TextToken(token.Token):
+    def __init__(self, position = None, text = ""):
+        super().__init__(position)
+        self.costume.fill_color=(0,0,0,0)
+        self.costume.font_size = 80
+        self.costume.text = text
+        self.costume.is_scaled = True
+
+    def set_text(self, text):
+        self.costume.text = text
+
+    def get_text(self):
+        return self.costume.text
