97bfc8cfe81d29709524145595ad7093dcd983ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:28 +0000

    Add package xklearn/0.0.5/xklearn-0.0.5.tar.gz

---
+++ packages/xklearn/xklearn-0.0.5.tar.gz/xklearn-0.0.5/xklearn/__init__.py
@@ -0,0 +1,12 @@
+'''
+-------------------------------------------------------
+    extrakit-learn
+
+    Author: Simon Larsson <larssonsimon0@gmail.com>
+
+    License: MIT
+-------------------------------------------------------
+'''
+
+__all__ = ['models',
+           'preprocessing']
