e73aa3dea6f2ed89303108f4afdc1d2188b38404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package chewie/0.0.18/chewie-0.0.18.tar.gz

---
+++ packages/chewie/chewie-0.0.18.tar.gz/chewie-0.0.18/run_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# TODO: must be run from chewie root
+pip3 install .
+if [ -z "${TRAVIS_PYTHON_VERSION}" ]; then
+    PYTYPE_TARGET_VERSION=3.6
+else
+    PYTYPE_TARGET_VERSION=$TRAVIS_PYTHON_VERSION
+fi
+
+echo "=============== Running UnitTests ================="
+
+PYTHONPATH=./ pytest -v --cov=chewie --cov-report term --cov-report=xml:coverage.xml test/test_*.py || exit 1
+
+if [ "${PYTYPE}" != "false" ] ; then
+echo "=============== Running PyType ===================="
+pytype -V$PYTYPE_TARGET_VERSION chewie/*py || exit 1
+fi
+
+cd test/codecheck
+echo "=============== Running Pylint ===================="
+./pylint.sh || exit 1
+
+exit 0
