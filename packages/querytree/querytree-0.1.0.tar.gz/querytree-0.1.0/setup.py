73dd491bfb739db4fe3d572649c37df06b3d5f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:45 +0000

    Add package querytree/0.1.0/querytree-0.1.0.tar.gz

---
+++ packages/querytree/querytree-0.1.0.tar.gz/querytree-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="querytree",
+    version="0.1.0",
+    author="Quinn Freedman",
+    author_email="quinnfreedman@gmail.com",
+    description="A data structure for quickly dealing with nested data in different formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/QuinnFreedman/QueryTree",
+    packages=[],  # setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
