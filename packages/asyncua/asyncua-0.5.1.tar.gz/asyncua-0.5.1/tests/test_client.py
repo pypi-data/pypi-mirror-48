8a161d7c9c333412dbc09d5bf7d732cc8cbe69ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:09 +0000

    Add package asyncua/0.5.1/asyncua-0.5.1.tar.gz

---
+++ packages/asyncua/asyncua-0.5.1.tar.gz/asyncua-0.5.1/tests/test_client.py
@@ -0,0 +1,89 @@
+
+import logging
+import pytest
+
+from asyncua import Client
+from asyncua import ua
+
+_logger = logging.getLogger(__name__)
+pytestmark = pytest.mark.asyncio
+
+
+async def test_service_fault(server, admin_client):
+    request = ua.ReadRequest()
+    request.TypeId = ua.FourByteNodeId(999)  # bad type!
+    with pytest.raises(ua.UaStatusCodeError):
+        await admin_client.uaclient.protocol.send_request(request)
+
+
+async def test_objects_anonymous(server, client):
+    objects = client.get_objects_node()
+    with pytest.raises(ua.UaStatusCodeError):
+        await objects.set_attribute(ua.AttributeIds.WriteMask, ua.DataValue(999))
+    with pytest.raises(ua.UaStatusCodeError):
+        await objects.add_folder(3, 'MyFolder')
+
+
+async def test_folder_anonymous(server, admin_client, client):
+    objects = admin_client.get_objects_node()
+    f = await objects.add_folder(3, 'MyFolderRO')
+    f_ro = client.get_node(f.nodeid)
+    assert f == f_ro
+    with pytest.raises(ua.UaStatusCodeError):
+        await f_ro.add_folder(3, 'MyFolder2')
+
+
+async def test_variable_anonymous(server, admin_client, client):
+    objects = admin_client.get_objects_node()
+    v = await objects.add_variable(3, 'MyROVariable', 6)
+    await v.set_value(4)  # this should work
+    v_ro = client.get_node(v.nodeid)
+    with pytest.raises(ua.UaStatusCodeError):
+        await v_ro.set_value(2)
+    assert await v_ro.get_value() == 4
+    await v.set_writable(True)
+    await v_ro.set_value(2)  # now it should work
+    assert await v_ro.get_value() == 2
+    await v.set_writable(False)
+    with pytest.raises(ua.UaStatusCodeError):
+        await v_ro.set_value(9)
+    assert await v_ro.get_value() == 2
+
+
+async def test_context_manager(server):
+    """Context manager calls connect() and disconnect()"""
+    state = [0]
+
+    async def increment_state(*args, **kwargs):
+        state[0] += 1
+
+    # create client and replace instance methods with dummy methods
+    client = Client('opc.tcp://dummy_address:10000')
+    client.connect = increment_state.__get__(client)
+    client.disconnect = increment_state.__get__(client)
+
+    assert state[0] == 0
+    async with client:
+        # test if client connected
+        assert state[0] == 1
+    # test if client disconnected
+    assert state[0] == 2
+
+
+async def test_enumstrings_getvalue(server, client):
+    """
+    The real exception is server side, but is detected by using a client.
+    All due the server trace is also visible on the console.
+    The client only 'sees' an TimeoutError
+    """
+    nenumstrings = client.get_node(ua.ObjectIds.AxisScaleEnumeration_EnumStrings)
+    value = ua.Variant(await nenumstrings.get_value())
+
+
+async def test_custom_enum_struct(server, client):
+    await client.load_type_definitions()
+    ns = await client.get_namespace_index('http://yourorganisation.org/struct_enum_example/')
+    myvar = client.get_node(ua.NodeId(6009, ns))
+    val = await myvar.get_value()
+    assert 242 == val.IntVal1
+    assert ua.ExampleEnum.EnumVal2 == val.EnumVal
---
+++ packages/asyncua/asyncua-0.5.1.tar.gz/asyncua-0.5.1/tests/test_sync.py
@@ -0,0 +1,52 @@
+import time
+
+import pytest
+
+from asyncua.sync import Client, start_thread_loop, stop_thread_loop, Server
+from asyncua import ua
+
+
+@pytest.fixture
+def server():
+    s = Server()
+    s.set_endpoint('opc.tcp://0.0.0.0:8840/freeopcua/server/')
+    uri = "http://examples.freeopcua.github.io"
+    idx = s.register_namespace(uri)
+    myobj = s.nodes.objects.add_object(idx, "MyObject")
+    myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+    mysin = myobj.add_variable(idx, "MySin", 0, ua.VariantType.Float)
+    s.start()
+    yield s
+    s.stop()
+
+
+@pytest.fixture
+def tloop():
+    t_loop = start_thread_loop()
+    yield t_loop
+    stop_thread_loop()
+
+
+@pytest.fixture
+def client(tloop, server):
+    c = Client("opc.tcp://localhost:8840/freeopcua/server")
+    c.connect()
+    yield c
+    c.disconnect()
+
+
+def test_sync_client(client):
+    client.load_type_definitions()
+    uri = "http://examples.freeopcua.github.io"
+    idx = client.get_namespace_index(uri)
+    myvar = client.nodes.root.get_child(["0:Objects", f"{idx}:MyObject", f"{idx}:MyVariable"])
+    assert myvar.get_value() == 6.7
+
+
+def test_sync_get_node(client):
+    node  = client.get_node(85)
+    assert node == client.nodes.objects
+    nodes = node.get_children()
+    assert len(nodes) == 2
+    assert nodes[0] == client.nodes.server
+
