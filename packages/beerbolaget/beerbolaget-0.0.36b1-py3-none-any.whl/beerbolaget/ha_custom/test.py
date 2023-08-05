26a008f2d503debd479c9056567e5bbd0e84a311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:25 +0000

    Add package beerbolaget/0.0.36b1/beerbolaget-0.0.36b1-py3-none-any.whl

---
+++ packages/beerbolaget/beerbolaget-0.0.36b1-py3-none-any.whl/beerbolaget/ha_custom/test.py
@@ -0,0 +1,46 @@
+﻿import asyncio
+import json
+import common
+
+class beer_handler():
+    def __init__(self, api_key, ratebeer, untappd):
+        self.api_key = api_key
+        self.ratebeer = ratebeer
+        self.untappd = untappd
+        self.beers = []
+        self.release = None
+
+    async def update_new_beers(self):
+        beer_available = await common.get_beverage(self.api_key, 'Öl')
+        if len(beer_available) > 0:
+            self.release = beer_available[-1]['SellStartDate']
+        for beer_item in beer_available:
+            if beer_item['SellStartDate'] == self.release:
+                new_beer = beer('beer', beer_item['ProducerName'], beer_item['ProductNameBold'], beer_item['ProductNameThin'],
+                                beer_item['Type'], beer_item['Price'], beer_item['Country'])
+                self.beers.append(new_beer)
+    
+    async def get_beers(self):
+        beers = []
+        for beer in self.beers:
+            beers.append(beer.__dict__)
+        return json.dumps(beers)
+
+    async def get_release(self):
+        return self.release
+
+class beer():
+    def __init__(self, beverage_type, brewery, name, detailed_name, type, price, country):
+        self.beverage_type = beverage_type
+        self.brewery = brewery
+        self.name = name
+        self.detailed_name = detailed_name
+        self.type = type
+        self.price = price
+        self.country = country
+        self.rating = None
+
+if __name__ == '__main__':
+    beer_handle = beer_handler('8d15733674534d50b8a41889fe2a11f8', None, None)
+    asyncio.run(beer_handle.update_new_beers())
+    print(asyncio.run(beer_handle.get_beers()))
---
+++ packages/beerbolaget/beerbolaget-0.0.36b1-py3-none-any.whl/pybeerbolaget/ha_custom/beer.py
@@ -0,0 +1,40 @@
+﻿import json
+from pybeerbolaget.ha_custom import common
+
+class beer_handler():
+    def __init__(self, api_key, show_beer):
+        self.api_key = api_key
+        self.show_beer = show_beer
+        self.beers = []
+        self.release = None
+
+    async def update_new_beers(self):
+        if not self.release:
+            beer_available = await common.get_beverage(self.api_key, 'Öl')
+        if len(beer_available) > 0:
+            self.release = beer_available[-1]['SellStartDate']
+        for beer_item in beer_available:
+            if beer_item['SellStartDate'] == self.release:
+                new_beer = beer('beer', beer_item['ProducerName'], beer_item['ProductNameBold'], beer_item['ProductNameThin'],
+                                beer_item['Type'], beer_item['Price'], beer_item['Country'])
+                self.beers.append(new_beer)
+    
+    async def get_beers(self):
+        beers = []
+        for beer in self.beers:
+            beers.append(beer.__dict__)
+        return json.dumps(beers)
+
+    async def get_release(self):
+        return self.release
+
+class beer():
+    def __init__(self, beverage_type, brewery, name, detailed_name, type, price, country):
+        self.beverage_type = beverage_type
+        self.brewery = brewery
+        self.name = name
+        self.detailed_name = detailed_name
+        self.type = type
+        self.price = price
+        self.country = country
+        self.rating = None
