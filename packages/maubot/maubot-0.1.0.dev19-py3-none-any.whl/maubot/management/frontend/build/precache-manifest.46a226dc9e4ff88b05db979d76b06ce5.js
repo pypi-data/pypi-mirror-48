7af8a298a9033d2ed62eac96443cd89e340fbe91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:03 +0000

    Add package maubot/0.1.0.dev19/maubot-0.1.0.dev19-py3-none-any.whl

---
+++ packages/maubot/maubot-0.1.0.dev19-py3-none-any.whl/maubot/management/frontend/build/precache-manifest.46a226dc9e4ff88b05db979d76b06ce5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "23719c10ec0948af5e65",
+    "url": "./static/css/main.3ce1c750.chunk.css"
+  },
+  {
+    "revision": "23719c10ec0948af5e65",
+    "url": "./static/js/main.23719c10.chunk.js"
+  },
+  {
+    "revision": "1b0221374c2c08a658f1",
+    "url": "./static/js/1.1b022137.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "./static/media/raleway-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "d724dad2e61905f488d048e51d45ae3e",
+    "url": "./static/media/raleway-light.d724dad2.woff2"
+  },
+  {
+    "revision": "77d77f36bed0a452984832f6b5f22e3f",
+    "url": "./static/media/raleway-bold.77d77f36.woff2"
+  },
+  {
+    "revision": "24310bd06e2537b92a92f33dabdc038b",
+    "url": "./static/media/firacode-regular.24310bd0.woff2"
+  },
+  {
+    "revision": "9ecee2ce95b0de731b1498b48e71c737",
+    "url": "./static/media/firacode-bold.9ecee2ce.woff2"
+  },
+  {
+    "revision": "1851431378ea0e1c6a63c7261ee65d9e",
+    "url": "./static/media/plus.18514313.svg"
+  },
+  {
+    "revision": "b6b6c4db699010301b02593710377f70",
+    "url": "./static/media/chevron-right.b6b6c4db.svg"
+  },
+  {
+    "revision": "7e77da69cc71663ae51ce4b08b52e0d3",
+    "url": "./static/media/chevron-left.7e77da69.svg"
+  },
+  {
+    "revision": "ebe1c3a69ddb3b046edbb1f11f689d78",
+    "url": "./static/media/sort-down.ebe1c3a6.svg"
+  },
+  {
+    "revision": "4bff71677abd84948fb3335d9ecf4388",
+    "url": "./static/media/sort-up.4bff7167.svg"
+  },
+  {
+    "revision": "a57b766207fe6c113be29df03f171a6d",
+    "url": "./static/media/upload.a57b7662.svg"
+  },
+  {
+    "revision": "efdcd694bf8d1fc9783b7d1a15a28a78",
+    "url": "./index.html"
+  }
+];
