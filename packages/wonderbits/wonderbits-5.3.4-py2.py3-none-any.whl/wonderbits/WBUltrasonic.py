6d616240cab42cec496ed2bfe2bbf7d9f763d4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:22 +0000

    Add package wonderbits/5.3.4/wonderbits-5.3.4-py2.py3-none-any.whl

---
+++ packages/wonderbits/wonderbits-5.3.4-py2.py3-none-any.whl/wonderbits/WBUltrasonic.py
@@ -0,0 +1,23 @@
+from .wbits import Wonderbits
+
+
+
+
+
+class Ultrasonic(Wonderbits):
+    def __init__(self, index = 1):
+        Wonderbits.__init__(self)
+        self.index = index
+
+    
+    def register_distance(self, cb):
+        self._register_event('ultrasonic{}'.format(self.index), 'distance', cb)
+    
+    def get_distance(self):
+        """
+        该函数用于获取超声波检测的距离值，单位：cm:rtype: float
+        """
+
+        command = 'ultrasonic{}.get_distance()'.format(self.index)
+        return self._get_command(command)
+    
