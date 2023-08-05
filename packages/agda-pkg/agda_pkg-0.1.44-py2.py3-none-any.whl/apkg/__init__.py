4437912198bda9845f641635d771463056a14c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:31 +0000

    Add package agda-pkg/0.1.44/agda_pkg-0.1.44-py2.py3-none-any.whl

---
+++ packages/agda-pkg/agda_pkg-0.1.44-py2.py3-none-any.whl/apkg/__init__.py
@@ -0,0 +1,37 @@
+'''
+  apkg
+  ~~~~
+
+  A package manager for Agda.
+
+'''
+
+# ----------------------------------------------------------------------------
+
+import click
+
+from distlib.index  import PackageIndex
+from natsort        import natsorted
+
+from .__version__   import __version__
+
+# ----------------------------------------------------------------------------
+
+try:
+  index    = PackageIndex()
+  info     = index.search('agda-pkg')
+  versions = natsorted([e["version"] for e in info if e["name"] == "agda-pkg"])
+
+
+  if len(versions) > 0:
+    lastversion = versions[-1]
+    msg = "You are using agda-pkg version {cversion}, however version {lversion} is available.\n" + \
+          "You should consider upgrading via the 'pip install --upgrade agda-pkg' command."
+    msg = msg.format(cversion = __version__ , lversion = lastversion)
+    orden = [lastversion, __version__]
+    if orden != natsorted(orden):
+      click.echo(click.style(msg, fg='yellow', bold=True))
+except:
+  pass
+
+
