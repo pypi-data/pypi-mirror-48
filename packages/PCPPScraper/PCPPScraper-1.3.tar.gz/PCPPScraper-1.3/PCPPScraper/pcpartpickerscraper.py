2cb106e056c8a9a8ca26ecbcbf9e39e03e6dd001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package PCPPScraper/1.3/PCPPScraper-1.3.tar.gz

---
+++ packages/PCPPScraper/PCPPScraper-1.3.tar.gz/PCPPScraper-1.3/PCPPScraper/pcpartpickerscraper.py
@@ -0,0 +1,38 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+class Component():
+    def __init__(self, componentType, name, link, price):
+        self.type = componentType
+        self.name = name
+        self.link = link
+        self.price = price
+
+def getComponents(URL : str):
+    page = get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    components = soup.find_all(attrs = {'class' : 'tr__product'})
+    componentList = list()
+    for component in components:
+        componentLink = component.find(attrs = {'class' : 'td__name'}).find('a')
+        if componentLink == None:
+            break
+        componentType = component.find(attrs = {'class' : 'td__component'}).find('a').get_text()
+        price = component.find(attrs = {'class' : 'td__price'}).find('a')
+        if price == None:
+            price = 'unknown'
+            if component.find(attrs = {'class' : 'td__price'}).get_text() != None:
+                price = component.find(attrs = {'class' : 'td__price'}).get_text()[5:]
+        else:
+            price = price.get_text()
+        name = componentLink.get_text()
+        link = 'https://pcpartpicker.com' + componentLink.get('href')
+        newComponent = Component(componentType, name, link, price)
+        componentList.append(newComponent)
+        
+
+    return componentList
+
+l = getComponents('https://pcpartpicker.com/list/cm8wRJ')
+for x in l:
+    print(x.price)
