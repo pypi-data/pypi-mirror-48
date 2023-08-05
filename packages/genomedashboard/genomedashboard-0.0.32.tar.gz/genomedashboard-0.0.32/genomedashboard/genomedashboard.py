a74845b39af0d49a42b3294e0b91bc6dd34bab5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package genomedashboard/0.0.32/genomedashboard-0.0.32.tar.gz

---
+++ packages/genomedashboard/genomedashboard-0.0.32.tar.gz/genomedashboard-0.0.32/genomedashboard/genomedashboard.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""genomedashboard root module."""
+
+from .hpt import hpt as hpt
+from .ui import ui as ui
+
+
+def hello():
+    print("Hello from genomedashboard.genomedashboard.py.")
