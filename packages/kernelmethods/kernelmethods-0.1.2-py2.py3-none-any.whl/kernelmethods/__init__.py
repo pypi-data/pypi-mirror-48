c4b51adf92e2e3b94f9946d304434fd4f52446a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package kernelmethods/0.1.2/kernelmethods-0.1.2-py2.py3-none-any.whl

---
+++ packages/kernelmethods/kernelmethods-0.1.2-py2.py3-none-any.whl/kernelmethods/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for kernelmethods."""
+
+__all__ = ['KernelMatrix',
+           'BaseKernelFunction', 'KernelMethodsException', 'KMAccessError',
+           'PolyKernel', 'GaussianKernel', 'LaplacianKernel', 'LinearKernel',
+           'KernelBucket', 'KernelSet']
+
+from kernelmethods.base import BaseKernelFunction, KernelMatrix, \
+    KernelMethodsException, KMAccessError, KMSetAdditionError, KernelSet
+from kernelmethods.numeric_kernels import PolyKernel, GaussianKernel, LaplacianKernel, \
+    LinearKernel
+from kernelmethods.sampling import KernelBucket
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+__author__ = """Pradeep Reddy Raamana"""
+__email__ = 'raamana@gmail.com'
