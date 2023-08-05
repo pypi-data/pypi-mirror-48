4b8dd06b8bdaad73b17169a16bfbe15389db80d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:24 +0000

    Add package rocore/1.0.7/rocore-1.0.7.tar.gz

---
+++ packages/rocore/rocore-1.0.7.tar.gz/rocore-1.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='rocore',
+      version='1.0.7',
+      description='Core classes and functions, reusable in any kind of Python application',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent'
+      ],
+      url='https://github.com/RobertoPrevato/rocore',
+      author='RobertoPrevato',
+      author_email='roberto.prevato@gmail.com',
+      keywords='core utilities',
+      license='MIT',
+      packages=['rocore', 'rocore.typesutils'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
