10f1df255412c75555278cb37ff03c123d10b858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:48 +0000

    Add package attr-descriptions/0.1.1/attr-descriptions-0.1.1.tar.gz

---
+++ packages/attr-descriptions/attr-descriptions-0.1.1.tar.gz/attr-descriptions-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='attr-descriptions',
+    version='0.1.1',
+    description='A mini-module that helps to add descriptions to attrs attributes.',
+    long_description=readme,
+    author='Deniz Bozyigit',
+    author_email='deniz195@gmail.com',
+    url='https://github.com/deniz195/attr-descriptions',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires = ['attr','cattrs','logging','functools','typing','math','pytest']
+)
