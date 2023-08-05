824a1fe4ca14c0bfeeef16cdb3ddd5feb36711e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:56 +0000

    Add package aymara/0.1.2/aymara-0.1.2.tar.gz

---
+++ packages/aymara/aymara-0.1.2.tar.gz/aymara-0.1.2/aymara/lima.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import pyconll
+import requests
+import sys
+
+class Lima:
+    """
+    A simple wrapper around the LIMA REST server
+    """
+
+    def __init__(self, host='localhost', port='8080'):
+        self.host = host
+        self.port = port
+
+    def  analyzeText(self, text, lang='eng', pipeline='deep'):
+        url = 'http://{}:{}/?lang={}&pipeline={}'.format(self.host,
+                                                         self.port,
+                                                         lang,
+                                                         pipeline)
+        answer = requests.post(url, data = text)
+        if answer.status_code == 200:
+            str = '\n'.join(json.loads(answer.text)['tokens'])
+            return pyconll.load_from_string(str)
+        else:
+            print('Got HTTP error {}: {}'.format(answer.status_code,
+                                                 answer.text),
+                  file=sys.stderr)
+            return None
+
+
+
