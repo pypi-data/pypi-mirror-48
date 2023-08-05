904cbff3a230b500765a6d8f66ca7c4ca8015ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:32 +0000

    Add package huawei-modem-api-client/1.0.6/huawei-modem-api-client-1.0.6.tar.gz

---
+++ packages/huawei-modem-api-client/huawei-modem-api-client-1.0.6.tar.gz/huawei-modem-api-client-1.0.6/huaweisms/api/device.py
@@ -0,0 +1,29 @@
+from huaweisms.api.common import (
+    post_to_url,
+    get_from_url,
+    ApiCtx,
+)
+
+
+def information(ctx: ApiCtx) -> dict:
+    url = "{}/device/information".format(ctx.api_base_url)
+    return get_from_url(url, ctx)
+
+
+def basic_information(ctx: ApiCtx) -> dict:
+    url = "{}/device/information".format(ctx.api_base_url)
+    return get_from_url(url, ctx)
+
+
+def reboot(ctx: ApiCtx) -> dict:
+    """
+    Reboots the modem.
+    """
+
+    url = '{}/device/control'.format(ctx.api_base_url)
+    headers = {
+        '__RequestVerificationToken': ctx.token,
+    }
+
+    payload = '<?xml version: "1.0" encoding="UTF-8"?><request><Control>1</Control></request>'
+    return post_to_url(url, payload, ctx, additional_headers=headers)
