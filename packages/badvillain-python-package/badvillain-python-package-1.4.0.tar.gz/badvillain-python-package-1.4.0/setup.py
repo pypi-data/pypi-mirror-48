c1c0fa4126236d3c7e9d17caee7f9b519e1d378d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:29 +0000

    Add package badvillain-python-package/1.4.0/badvillain-python-package-1.4.0.tar.gz

---
+++ packages/badvillain-python-package/badvillain-python-package-1.4.0.tar.gz/badvillain-python-package-1.4.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open("readme.md") as f:
+        r = f.read()
+    return r
+
+
+setup(
+    name = "badvillain-python-package",
+    version = "1.4.0",
+    description = "Hello idk what to say",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    url = "https://www.thevillainer.tk/",
+    author = "Badvillain",
+    author_email = "idk@gmail.com",
+    license = "MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages = ["badvillain_greeting"],
+    setup_requires = ['wheel']
+)
