47b2d80d15aaeaf6a72fc59ab456c15cff53ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:07 +0000

    Add package jupytext/1.1.7/jupytext-1.1.7.tar.gz

---
+++ packages/jupytext/jupytext-1.1.7.tar.gz/jupytext-1.1.7/packages/labextension/lib/index.d.ts
@@ -0,0 +1,7 @@
+import { JupyterLabPlugin } from "@jupyterlab/application";
+import "../style/index.css";
+/**
+ * Initialization data for the jupyterlab-jupytext extension.
+ */
+declare const extension: JupyterLabPlugin<void>;
+export default extension;
