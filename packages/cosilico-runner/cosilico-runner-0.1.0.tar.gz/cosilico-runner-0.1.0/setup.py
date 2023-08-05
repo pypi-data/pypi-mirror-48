4e11978c05a3ebf79242b964bb850987e5371b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:50 +0000

    Add package cosilico-runner/0.1.0/cosilico-runner-0.1.0.tar.gz

---
+++ packages/cosilico-runner/cosilico-runner-0.1.0.tar.gz/cosilico-runner-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['cosilico_runner']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['cosilico-runner = cosilico_runner:cosilico_runner.main']}
+
+setup_kwargs = {
+    'name': 'cosilico-runner',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Erik Storrs',
+    'author_email': 'epstorrs@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
