06b9100959958f55107e29976dfeb932c57437d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:47 +0000

    Add package django-kakao-zipcode/0.1.1/django-kakao-zipcode-0.1.1.tar.gz

---
+++ packages/django-kakao-zipcode/django-kakao-zipcode-0.1.1.tar.gz/django-kakao-zipcode-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+README = open(os.path.join(ROOT_DIR, 'README.md')).read()
+
+setup(
+    name='django-kakao-zipcode',
+    version='0.1.1',
+    description='Kakao zipcode service widget for Django',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Lee HanYeong',
+    author_email='dev@lhy.kr',
+    license='MIT',
+    packages=[
+        'kakao_zipcode',
+    ],
+    url='https://github.com/LeeHanYeong/django-kakao-zipcode',
+    zip_safe=True,
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python',
+    ]
+)
