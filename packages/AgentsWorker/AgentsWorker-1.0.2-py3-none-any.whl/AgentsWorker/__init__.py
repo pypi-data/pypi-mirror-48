b0ce8c7cfe3600d4dc4bbcda83f29ed856b0cb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package AgentsWorker/1.0.2/AgentsWorker-1.0.2-py3-none-any.whl

---
+++ packages/AgentsWorker/AgentsWorker-1.0.2-py3-none-any.whl/AgentsWorker/__init__.py
@@ -0,0 +1,9 @@
+from .loggs import Loggs
+from .kafkaBase import KafkaBase
+from .agent import Agent
+from .options import Options
+name = 'AgentsWorker'
+
+__all__ = ['Manager', 'Agent', 'Options']
+
+__version__ = "1.0.0"
