948969c50b42a6fb0ad41660c239e0205e7af897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:46 +0000

    Add package eo-learn-core/0.5.0/eo-learn-core-0.5.0.tar.gz

---
+++ packages/eo-learn-core/eo-learn-core-0.5.0.tar.gz/eo-learn-core-0.5.0/eolearn/core/__init__.py
@@ -0,0 +1,17 @@
+"""
+The following objects and functions are the core of eo-learn package
+"""
+
+from sentinelhub import BBox, CRS
+
+from .constants import FeatureType, FeatureTypeSet, FileFormat, OverwritePermission
+from .eodata import EOPatch
+from .eotask import EOTask, CompositeTask
+from .eoworkflow import EOWorkflow, LinearWorkflow, Dependency, WorkflowResults
+from .eoexecution import EOExecutor
+
+from .core_tasks import CopyTask, DeepCopyTask, SaveToDisk, LoadFromDisk, AddFeature, RemoveFeature, RenameFeature
+from .utilities import deep_eq, negate_mask, constant_pad, get_common_timestamps, bgr_to_rgb, FeatureParser
+
+
+__version__ = '0.5.0'
