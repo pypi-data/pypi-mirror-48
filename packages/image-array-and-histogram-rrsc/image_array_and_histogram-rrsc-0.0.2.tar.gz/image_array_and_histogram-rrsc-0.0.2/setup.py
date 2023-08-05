38a55af1cdcb26e0d10a27362f58521e871d77c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package image-array-and-histogram-rrsc/0.0.2/image_array_and_histogram-rrsc-0.0.2.tar.gz

---
+++ packages/image-array-and-histogram-rrsc/image_array_and_histogram-rrsc-0.0.2.tar.gz/image_array_and_histogram-rrsc-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='image_array_and_histogram-rrsc',
+    version='0.0.2',
+    author='Rishi Raj Singh Chauhan',
+    author_email='rishirschauhan@gmail.com',
+    description='Helps in getting array and histogram of a greyscale image. Also, image from the array.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/rishi-chauhan/my-packages.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
