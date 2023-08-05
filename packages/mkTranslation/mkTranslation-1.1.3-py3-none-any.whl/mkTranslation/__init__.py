6f566c4b8d3bc18f4c5e1e974bdc70f6afb6172a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:39 +0000

    Add package mkTranslation/1.1.3/mkTranslation-1.1.3-py3-none-any.whl

---
+++ packages/mkTranslation/mkTranslation-1.1.3-py3-none-any.whl/mkTranslation/__init__.py
@@ -0,0 +1,7 @@
+"""Command line tools translated using Google and i18ns.com"""
+__all__ = 'mkTranslator',
+__version__ = '1.1.3'
+
+
+from mkTranslation.translator  import mkTranslator
+from mkTranslation.constants import LANGCODES, LANGUAGES
