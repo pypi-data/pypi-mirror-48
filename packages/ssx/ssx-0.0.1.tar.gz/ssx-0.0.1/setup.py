f7df2bef68a9bfe09832c37bd7026eeb504d6059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:38 +0000

    Add package ssx/0.0.1/ssx-0.0.1.tar.gz

---
+++ packages/ssx/ssx-0.0.1.tar.gz/ssx-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ssx',
+      version='0.0.1',
+      description='Synchrotron serial crystallography tools',
+      author='Jerome Kieffer',
+      author_email='jerome.kieffer@esrf.fr',
+      url='https://github.com/kif/ssx',
+      packages=['ssx'],
+     )
