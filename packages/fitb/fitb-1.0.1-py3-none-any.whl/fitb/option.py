293687dc38a89793ed5bd0074fd584346b14c407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:40 +0000

    Add package fitb/1.0.1/fitb-1.0.1-py3-none-any.whl

---
+++ packages/fitb/fitb-1.0.1-py3-none-any.whl/fitb/option.py
@@ -0,0 +1,27 @@
+class Option:
+    """Description of a single configurable option.
+
+    Args:
+        name: The name of the option, i.e. its key in the config.
+        description: Description of the option.
+        default: Default value of the option.
+    """
+    def __init__(self, name, description, default):
+        self._name = name
+        self._description = description
+        self._default = default
+
+    @property
+    def name(self):
+        "The name of the option."
+        return self._name
+
+    @property
+    def description(self):
+        "The description of the option."
+        return self._description
+
+    @property
+    def default(self):
+        "The default value of the option."
+        return self._default
