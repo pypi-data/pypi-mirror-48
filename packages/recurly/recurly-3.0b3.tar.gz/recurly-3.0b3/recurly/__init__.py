6ed043cde6d76773fafd3b0f48dce54d89de3799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:40 +0000

    Add package recurly/3.0b3/recurly-3.0b3.tar.gz

---
+++ packages/recurly/recurly-3.0b3.tar.gz/recurly-3.0b3/recurly/__init__.py
@@ -0,0 +1,31 @@
+name = "recurly"
+
+import os
+from os.path import dirname, basename, isfile
+import glob
+
+# Running in strict mode will throw exceptions
+# when API responses don't line up with the client's expectations.
+# The client's default behavior is to try to recover from these
+# errors. This is used to catch bugs in testing.
+# You do not want to enable this for production.
+STRICT_MODE = os.getenv("RECURLY_STRICT_MODE", "FALSE").upper() == "TRUE"
+
+
+class RecurlyError(Exception):
+    pass
+
+
+class ApiError(RecurlyError):
+    def __init__(self, message, error):
+        super(ApiError, self).__init__(message)
+        self.error = error
+
+
+class NetworkError(RecurlyError):
+    pass
+
+
+from . import errors
+from .client import Client
+from .resource import Resource
