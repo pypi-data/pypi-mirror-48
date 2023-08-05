96fc111b1759e0d4458340b37ccace4b249f0dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:32 +0000

    Add package census-data-downloader/0.0.9/census-data-downloader-0.0.9.tar.gz

---
+++ packages/census-data-downloader/census-data-downloader-0.0.9.tar.gz/census-data-downloader-0.0.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+setup(
+    name='census-data-downloader',
+    version='0.0.9',
+    description="Download Census data and reformat it for humans",
+    author='Los Angeles Times Data Desk',
+    author_email='datadesk@latimes.com',
+    url='http://www.github.com/datadesk/census-data-downloader',
+    license="MIT",
+    packages=("census_data_downloader",),
+    install_requires=(
+        "pandas",
+        "us",
+        "census",
+        "click"
+    ),
+    entry_points="""
+        [console_scripts]
+        censusdatadownloader=census_data_downloader.cli:cmd
+    """,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
