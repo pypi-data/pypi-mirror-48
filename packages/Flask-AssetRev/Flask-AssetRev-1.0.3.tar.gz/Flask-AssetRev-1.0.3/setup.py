204df0e259a735894fc9773d597ca74b9e2e0555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package Flask-AssetRev/1.0.3/Flask-AssetRev-1.0.3.tar.gz

---
+++ packages/Flask-AssetRev/Flask-AssetRev-1.0.3.tar.gz/Flask-AssetRev-1.0.3/setup.py
@@ -0,0 +1,55 @@
+from setuptools import find_packages, setup
+
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
+
+
+def get_requirements(filename):
+    try:
+        from pip._internal.download import PipSession
+        session = PipSession()
+    except ImportError:
+        from pip.download import PipSession
+        session = PipSession()
+    except Exception:
+        session = None
+
+    reqs = parse_requirements(filename, session=session)
+
+    return [str(r.req) for r in reqs]
+
+
+def get_meta():
+    mod_locals = {}
+    file_name = "flask_assetrev/__about__.py"
+
+    with open(file_name) as fp:
+        mod = compile(fp.read(), file_name, "exec")
+
+        exec(mod, mod_locals, mod_locals)
+
+    return dict(
+        (k, v) for k, v in mod_locals.items() if k in mod_locals["__all__"]
+    )
+
+
+meta = get_meta()
+
+
+setup_args = dict(
+    name="Flask-AssetRev",
+    version=meta["version"],
+    maintainer=meta["maintainer"],
+    maintainer_email=meta["maintainer_email"],
+    description=meta["description"],
+    url=meta["url"],
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+    tests_require=get_requirements("requirements_dev.txt"),
+)
+
+
+if __name__ == "__main__":
+    setup(**setup_args)
