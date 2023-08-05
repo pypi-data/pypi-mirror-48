9bed6c085c92ba568b56878fdb237d598c044461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:21 +0000

    Add package suresms/1.0/suresms-1.0.tar.gz

---
+++ packages/suresms/suresms-1.0.tar.gz/suresms-1.0/README.txt
@@ -0,0 +1,15 @@
+A Python wrapper around the SureSMS SMS gateway<https://www.suresms.com/>. 
+It is not a free SMS gateway that can be used for spamming. 
+
+You will need a paid account at SureSMS to be able to use the gateway.
+
+SureSMS is an international SMS gateway on pay-as-you-go principle. 
+
+See a couple of example use cases in example.py that should have come
+along with this file. Apart from what is shown in examples.py, this also
+supports status callback URLs, delayed sending and flash messages.
+
+For more info on parameters, take a look at the API documentation found at:
+<https://developer.suresms.com>.
+
+
---
+++ packages/suresms/suresms-1.0.tar.gz/suresms-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='suresms',
+    packages=['suresms'],
+    version='1.0',
+    author='Glen Mandsberg',
+    author_email='glen.mandsberg@suresms.com',
+    url='https://suresms.com',
+    description='A Python wrapper around the SureSMS SMS gateway <https://www.suresms.com>' \
+        '',
+    classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: Telephony',
+    ]
+)
