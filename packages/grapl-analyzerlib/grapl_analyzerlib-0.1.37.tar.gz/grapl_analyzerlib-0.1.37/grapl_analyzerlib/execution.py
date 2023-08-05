4b57b0ef37883c1be9f45c57800205d26e9ad0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:14:03 +0000

    Add package grapl-analyzerlib/0.1.37/grapl_analyzerlib-0.1.37.tar.gz

---
+++ packages/grapl-analyzerlib/grapl_analyzerlib-0.1.37.tar.gz/grapl_analyzerlib-0.1.37/grapl_analyzerlib/execution.py
@@ -0,0 +1,20 @@
+import json
+
+from grapl_analyzerlib.entities import NodeView
+
+
+class ExecutionHit(object):
+    def __init__(self, analyzer_name: str, node_view: NodeView, risk_score: int = 50):
+        node_dict = node_view.to_adjacency_list()
+        self.analyzer_name = analyzer_name
+        self.nodes = json.dumps(node_dict['nodes'])
+        self.edges = json.dumps(node_dict['edges'])
+        self.risk_score = risk_score
+
+
+class ExecutionComplete(object):
+    pass
+
+
+class ExecutionFailed(object):
+    pass
