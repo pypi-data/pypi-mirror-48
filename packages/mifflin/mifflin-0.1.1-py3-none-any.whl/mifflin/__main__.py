9f8e1639f6eaac58ad25f74c8095feacd5eca433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:25 +0000

    Add package mifflin/0.1.1/mifflin-0.1.1-py3-none-any.whl

---
+++ packages/mifflin/mifflin-0.1.1-py3-none-any.whl/mifflin/__main__.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+
+def main():
+    """TODO: implement this"""
+    URL = 'https://en.wikipedia.org/wiki/The_Office_(American_TV_series)'
+    webbrowser.open_new_tab(URL)
+    print('more mifflin to come...')
+
+
+if __name__ == "__main__":
+    main()
