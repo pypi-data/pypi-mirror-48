5dce846f13d920ad6be6bac1f717cdb4663019a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:35 +0000

    Add package nanome/0.2.4/nanome-0.2.4.tar.gz

---
+++ packages/nanome/nanome-0.2.4.tar.gz/nanome-0.2.4/nanome/util/quaternion.py
@@ -0,0 +1,44 @@
+import math
+#placeholder quaternion
+
+class Quaternion(object):
+    def __init__(self, x=0, y=0, z=0, w=0):
+        self._x = float(x)
+        self._y = float(y)
+        self._z = float(z)
+        self._w = float(w)
+    
+    def __str__(self):
+        s = ' '.join([str(self._x), str(self._y), str(self._z), str(self._w)])
+        return s
+
+    def set(self, x, y, z, w):
+        self._x = float(x)
+        self._y = float(y)
+        self._z = float(z)
+        self._w = float(w)
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def z(self):
+        return self._z
+
+    @property
+    def w(self):
+        return self._w
+
+    def _inverse_handedness(self):
+        self._y *= -1.0
+        self._z *= -1.0
+        return self
+
+    @classmethod
+    def _get_inversed_handedness(cls, value):
+        return Quaternion(value.x, -value.y, -value.z, value.w)
