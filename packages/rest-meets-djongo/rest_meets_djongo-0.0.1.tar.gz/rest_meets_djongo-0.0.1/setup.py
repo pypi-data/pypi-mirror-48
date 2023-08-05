be45184716e7212b151e43889fd50fa3e80924ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package rest-meets-djongo/0.0.1/rest_meets_djongo-0.0.1.tar.gz

---
+++ packages/rest-meets-djongo/rest_meets_djongo-0.0.1.tar.gz/rest_meets_djongo-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+from rest_meets_djongo import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='rest_meets_djongo',
+    version=__version__,
+    packages=['rest_meets_djongo'],
+
+    install_requires=[
+        'Django',
+        'djangorestframework<4',
+        'djongo',
+    ],
+
+    author='Kalum J. Ost',
+    author_email='kalumost@gmail.com',
+    description=(
+        "Allows for automatic serialization of Djongo fields w/ Django REST"
+    ),
+    long_description=readme(),
+    keywords="mongodb djongo rest_framework rest apis fields",
+    url="https://gitlab.com/SomeoneInParticular/rest_meets_djongo",
+    long_description_content_type="text/markdown",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+)
