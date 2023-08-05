27475491af2a7bdfce8751442f28e7a96033a435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:48 +0000

    Add package couchbase/3.0.0a2/couchbase-3.0.0a2-cp37-cp37m-win32.whl

---
+++ packages/couchbase/couchbase-3.0.0a2-cp37-cp37m-win32.whl/couchbase/durability.py
@@ -0,0 +1,44 @@
+from enum import Enum, IntEnum
+
+import couchbase_core._libcouchbase as _LCB
+from typing import *
+import couchbase.options
+from .options import Cardinal, OptionBlock
+
+
+class Durability(IntEnum):
+    MAJORITY_AND_PERSIST_ACTIVE = _LCB.LCB_DURABILITYLEVEL_MAJORITY_AND_PERSIST_ON_MASTER
+    NONE = _LCB.LCB_DURABILITYLEVEL_NONE
+    MAJORITY = _LCB.LCB_DURABILITYLEVEL_MAJORITY
+    PERSIST_TO_MAJORITY = _LCB.LCB_DURABILITYLEVEL_PERSIST_TO_MAJORITY
+
+
+class ReplicateTo(Cardinal):
+    Value = couchbase.options.Value
+
+
+class PersistTo(Cardinal):
+    Value = couchbase.options.Value
+
+
+T = TypeVar('T', bound=OptionBlock)
+
+
+class ClientDurableOption(object):
+    def dur_client(self,  # type: T
+                   replicate_to,  # type: couchbase.options.Value
+                   persist_to,  # type: couchbase.options.Value
+                   ):
+        # type: (...)->T.ClientDurable
+        self['replicate_to'] = replicate_to
+        self['persist_to'] = persist_to
+        return self
+
+
+class ServerDurableOption(object):
+    def dur_server(self,  # type: T
+                   level,  # type: Durability
+                   ):
+        # type: (...)->T.ServerDurable
+        self['durability_level'] = level
+        return self
