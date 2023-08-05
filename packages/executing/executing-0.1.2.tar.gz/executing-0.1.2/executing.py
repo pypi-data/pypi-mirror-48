f10f3acce8f4c29860fc1acd1a032b10e2eeb868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:29 +0000

    Add package executing/0.1.2/executing-0.1.2.tar.gz

---
+++ packages/executing/executing-0.1.2.tar.gz/executing-0.1.2/executing.py
@@ -0,0 +1,451 @@
+import __future__
+import ast
+import dis
+import functools
+import inspect
+import io
+import linecache
+import sys
+from collections import defaultdict, namedtuple, Sized
+from contextlib import contextmanager
+from itertools import islice
+from lib2to3.pgen2.tokenize import cookie_re as encoding_pattern
+from operator import attrgetter
+from threading import RLock
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:
+    # noinspection PyUnresolvedReferences
+    from functools import lru_cache
+    # noinspection PyUnresolvedReferences
+    from tokenize import detect_encoding
+
+    cache = lru_cache()
+    text_type = str
+else:
+    from lib2to3.pgen2.tokenize import detect_encoding
+
+
+    def cache(func):
+        d = {}
+
+        @functools.wraps(func)
+        def wrapper(*args):
+            if args in d:
+                return d[args]
+            result = d[args] = func(*args)
+            return result
+
+        return wrapper
+
+
+    # noinspection PyUnresolvedReferences
+    text_type = unicode
+try:
+    # noinspection PyUnresolvedReferences
+    get_instructions = dis.get_instructions
+except AttributeError:
+    Instruction = namedtuple('Instruction', 'offset argval opname')
+
+
+    def to_int(c):
+        if isinstance(c, int):
+            return c
+        else:
+            return ord(c)
+
+
+    from dis import HAVE_ARGUMENT, EXTENDED_ARG, hasconst, opname
+
+    # Based on dis.disassemble from 2.7
+    # Left as similar as possible for easy diff
+
+    def get_instructions(co):
+        code = co.co_code
+        n = len(code)
+        i = 0
+        extended_arg = 0
+        while i < n:
+            offset = i
+            c = code[i]
+            op = to_int(c)
+            argval = None
+            i = i + 1
+            if op >= HAVE_ARGUMENT:
+                oparg = to_int(code[i]) + to_int(code[i + 1]) * 256 + extended_arg
+                extended_arg = 0
+                i = i + 2
+                if op == EXTENDED_ARG:
+                    extended_arg = oparg * 65536
+
+                if op in hasconst:
+                    argval = co.co_consts[oparg]
+            yield Instruction(offset, argval, opname[op])
+
+
+class NotOneValueFound(Exception):
+    pass
+
+
+def only(it):
+    if isinstance(it, Sized):
+        if len(it) != 1:
+            raise NotOneValueFound('Expected one value, found %s' % len(it))
+        # noinspection PyTypeChecker
+        return list(it)[0]
+
+    lst = tuple(islice(it, 2))
+    if len(lst) == 0:
+        raise NotOneValueFound('Expected one value, found 0')
+    if len(lst) > 1:
+        raise NotOneValueFound('Expected one value, found several')
+    return lst[0]
+
+
+class Source(object):
+    def __init__(self, filename, text):
+        self.filename = filename
+
+        if not isinstance(text, text_type):
+            text = self.decode_source(text)
+        self.text = text
+
+        if PY3:
+            ast_text = text
+        else:
+            # In python 2 it's a syntax error to parse unicode
+            # with an encoding declaration, so we remove it but
+            # leave empty lines in its place to keep line numbers the same
+            ast_text = ''.join([
+                '\n' if i < 2 and encoding_pattern.match(line)
+                else line
+                for i, line in enumerate(text.splitlines(True))
+            ])
+
+        self.nodes_by_line = defaultdict(list)
+        self.tree = None
+        self.qualnames = {}
+
+        if text:
+            try:
+                self.tree = ast.parse(ast_text, filename=filename)
+            except SyntaxError:
+                pass
+            else:
+                for node in ast.walk(self.tree):
+                    for child in ast.iter_child_nodes(node):
+                        child.parent = node
+                    if hasattr(node, 'lineno'):
+                        self.nodes_by_line[node.lineno].append(node)
+
+                visitor = QualnameVisitor()
+                visitor.visit(self.tree)
+                self.qualnames = visitor.qualnames
+
+    @classmethod
+    def for_frame(cls, frame):
+        return cls.for_filename(frame.f_code.co_filename, frame.f_globals or {})
+
+    @classmethod
+    def for_filename(cls, filename, module_globals=None):
+        source_cache = cls._class_local('__source_cache', {})
+        try:
+            return source_cache[filename]
+        except KeyError:
+            pass
+
+        lines = linecache.getlines(filename, module_globals)
+        result = source_cache[filename] = cls(filename, ''.join(lines))
+        return result
+
+    @classmethod
+    def lazycache(cls, frame):
+        if hasattr(linecache, 'lazycache'):
+            linecache.lazycache(frame.f_code.co_filename, frame.f_globals)
+
+    @classmethod
+    def executing(cls, frame):
+        key = (frame.f_code, frame.f_lasti)
+        executing_cache = cls._class_local('__executing_cache', {})
+
+        try:
+            args = executing_cache[key]
+        except KeyError:
+            source = cls.for_frame(frame)
+            node = stmts = None
+            if source.tree:
+                stmts = source.statements_at_line(frame.f_lineno)
+                try:
+                    node = CallFinder(frame, stmts, source.tree).result
+                except Exception:
+                    pass
+                else:
+                    new_stmts = {statement_containing_node(node)}
+                    assert new_stmts <= stmts
+                    stmts = new_stmts
+
+            args = source, node, stmts
+            executing_cache[key] = args
+
+        return Executing(frame, *args)
+
+    @classmethod
+    def _class_local(cls, name, default):
+        """
+        Returns an attribute directly associated with this class
+        (as opposed to subclasses), setting default if necessary
+        """
+        # classes have a mappingproxy preventing us from using setdefault
+        result = cls.__dict__.get(name, default)
+        setattr(cls, name, result)
+        return result
+
+    @cache
+    def statements_at_line(self, lineno):
+        return {
+            statement_containing_node(node)
+            for node in
+            self.nodes_by_line[lineno]
+        }
+
+    @cache
+    def asttokens(self):
+        """
+        Returns an ASTTokens object for getting the source of specific AST nodes.
+
+        See http://asttokens.readthedocs.io/en/latest/api-index.html
+        """
+        from asttokens import ASTTokens  # must be installed separately
+        return ASTTokens(
+            self.text,
+            tree=self.tree,
+            filename=self.filename,
+        )
+
+    @staticmethod
+    def decode_source(source):
+        if isinstance(source, bytes):
+            encoding, _ = detect_encoding(io.BytesIO(source).readline)
+            source = source.decode(encoding)
+        return source
+
+    def code_qualname(self, code):
+        assert code.co_filename == self.filename
+        return self.qualnames.get((code.co_name, code.co_firstlineno), code.co_name)
+
+
+class Executing(object):
+    def __init__(self, frame, source, node, stmts):
+        self.frame = frame
+        self.source = source
+        self.node = node
+        self.statements = stmts
+
+    def code_qualname(self):
+        return self.source.code_qualname(self.frame.f_code)
+
+    def text(self):
+        return self.source.asttokens().get_text(self.node)
+
+    def text_range(self):
+        return self.source.asttokens().get_text_range(self.node)
+
+
+class QualnameVisitor(ast.NodeVisitor):
+    def __init__(self):
+        super(QualnameVisitor, self).__init__()
+        self.stack = []
+        self.qualnames = {}
+
+    def visit_FunctionDef(self, node, name=None):
+        name = name or node.name
+        self.stack.append(name)
+        self.qualnames.setdefault((name, node.lineno), ".".join(self.stack))
+
+        self.stack.append('<locals>')
+        if isinstance(node, ast.Lambda):
+            children = [node.body]
+        else:
+            children = node.body
+        for child in children:
+            self.visit(child)
+        self.stack.pop()
+        self.stack.pop()
+
+        # Find lambdas in the function definition outside the body,
+        # e.g. decorators or default arguments
+        # Based on iter_child_nodes
+        for field, child in ast.iter_fields(node):
+            if field == 'body':
+                continue
+            if isinstance(child, ast.AST):
+                self.visit(child)
+            elif isinstance(child, list):
+                for grandchild in child:
+                    if isinstance(grandchild, ast.AST):
+                        self.visit(grandchild)
+
+    def visit_Lambda(self, node):
+        self.visit_FunctionDef(node, '<lambda>')
+
+    def visit_ClassDef(self, node):
+        self.stack.append(node.name)
+        self.generic_visit(node)
+        self.stack.pop()
+
+
+future_flags = sum(
+    getattr(__future__, fname).compiler_flag
+    for fname in __future__.all_feature_names
+)
+
+
+def compile_similar_to(source, matching_code):
+    return compile(
+        source,
+        matching_code.co_filename,
+        'exec',
+        flags=future_flags & matching_code.co_flags,
+        dont_inherit=True,
+    )
+
+
+sentinel = 'io8urthglkjdghvljusketgIYRFYUVGHFRTBGVHKGF78678957647698'
+
+
+class CallFinder(object):
+    def __init__(self, frame, stmts, tree):
+        self.frame = frame
+        self.tree = tree
+        call_instruction_index = only(
+            i
+            for i, instruction in enumerate(
+                _call_instructions(self.compile_instructions()))
+            if instruction.offset == self.frame.f_lasti
+        )
+
+        calls = {
+            node
+            for stmt in stmts
+            for node in ast.walk(stmt)
+            if isinstance(node, ast.Call)
+        }
+
+        self.result = only(self.matching_calls(calls, call_instruction_index))
+
+    def matching_calls(self, calls, call_instruction_index):
+        for i, call in enumerate(calls):
+            with add_sentinel_kwargs(call):
+                ast.fix_missing_locations(call)
+                instructions = self.compile_instructions()
+
+            indices = [
+                i
+                for i, instruction in enumerate(instructions)
+                if instruction.argval == sentinel
+            ]
+            if not indices:
+                continue
+            arg_index = only(indices)
+            new_instruction = _call_instructions(instructions[arg_index:])[0]
+
+            call_instructions = _call_instructions(instructions)
+            new_instruction_index = only(
+                i
+                for i, instruction in enumerate(call_instructions)
+                if instruction is new_instruction
+            )
+
+            if new_instruction_index == call_instruction_index:
+                yield call
+
+    def compile_instructions(self):
+        module_code = compile_similar_to(self.tree, self.frame.f_code)
+        code = only(find_codes(module_code, self.frame.f_code))
+        return list(get_instructions(code))
+
+
+lock = RLock()
+
+
+@contextmanager
+def tweak_list(lst):
+    with lock:
+        original = lst[:]
+        try:
+            yield
+        finally:
+            lst[:] = original
+
+
+if sys.version_info[:2] >= (3, 5):
+    @contextmanager
+    def add_sentinel_kwargs(call):
+        keyword = ast.keyword(arg=None, value=ast.Str(s=sentinel))
+        with lock:
+            with tweak_list(call.keywords):
+                call.keywords.append(keyword)
+                yield
+else:
+    @contextmanager
+    def add_sentinel_kwargs(call):
+        with lock:
+            original = call.kwargs
+            call.kwargs = ast.Str(s=sentinel)
+            try:
+                yield
+            finally:
+                call.kwargs = original
+
+
+def _call_instructions(instructions):
+    return [
+        instruction
+        for instruction in instructions
+        if instruction.opname.startswith(('CALL_FUNCTION', 'CALL_METHOD'))
+    ]
+
+
+def find_codes(root_code, matching):
+    def matches(c):
+        return all(
+            f(c) == f(matching)
+            for f in [
+                attrgetter('co_firstlineno'),
+                attrgetter('co_name'),
+                code_names,
+            ]
+        )
+
+    code_options = []
+    if matches(root_code):
+        code_options.append(root_code)
+
+    def finder(code):
+        for const in code.co_consts:
+            if not inspect.iscode(const):
+                continue
+
+            if matches(const):
+                code_options.append(const)
+            finder(const)
+
+    finder(root_code)
+    return code_options
+
+
+def code_names(code):
+    return frozenset().union(
+        code.co_names,
+        code.co_varnames,
+        code.co_freevars,
+        code.co_cellvars,
+    )
+
+
+@cache
+def statement_containing_node(node):
+    while not isinstance(node, ast.stmt):
+        node = node.parent
+    return node
