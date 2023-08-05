905573d14eff3125cf61532fba6e14b292a03dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package aws-cdk.aws-autoscaling/0.35.0/aws-cdk.aws-autoscaling-0.35.0.tar.gz

---
+++ packages/aws-cdk.aws-autoscaling/aws-cdk.aws-autoscaling-0.35.0.tar.gz/aws-cdk.aws-autoscaling-0.35.0/setup.py
@@ -0,0 +1,50 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-autoscaling",
+    "version": "0.35.0",
+    "description": "The CDK Construct Library for AWS::AutoScaling",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_autoscaling",
+        "aws_cdk.aws_autoscaling._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_autoscaling._jsii": [
+            "aws-autoscaling@0.35.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_autoscaling": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.11.3",
+        "publication>=0.0.3",
+        "aws-cdk.aws-autoscaling-common~=0.35.0",
+        "aws-cdk.aws-cloudwatch~=0.35.0",
+        "aws-cdk.aws-ec2~=0.35.0",
+        "aws-cdk.aws-elasticloadbalancing~=0.35.0",
+        "aws-cdk.aws-elasticloadbalancingv2~=0.35.0",
+        "aws-cdk.aws-iam~=0.35.0",
+        "aws-cdk.aws-sns~=0.35.0",
+        "aws-cdk.cdk~=0.35.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
