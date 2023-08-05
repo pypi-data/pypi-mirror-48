92928c0ee6cc41e1972f55a124b748aa473f254e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:42 +0000

    Add package resolwe-bio/20.0.0/resolwe-bio-20.0.0.tar.gz

---
+++ packages/resolwe-bio/resolwe-bio-20.0.0.tar.gz/resolwe-bio-20.0.0/resolwe_bio/docker_images/rnaseq/packages-manual/r-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+. /var/cache/build/packages-manual/common.sh
+
+# XXX: This is unverifiable and in WITHOUT HTTPS and thus may compromise the whole image.
+# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
+sed -e 's/^#.*$//g' -e '/^$/d' /var/cache/build/packages-r-bioconductor.txt | \
+    Rscript --slave --no-save --no-restore-history \
+        -e "source('http://www.bioconductor.org/biocLite.R'); biocLite(readLines('stdin'))"
+
+# XXX: This is unverifiable and thus may compromise the whole image.
+# XXX: Use notary (https://github.com/ropenscilabs/notary) when ready.
+Rscript --slave --no-save --no-restore-history -e " \
+  library(devtools); \
+  install_github('jkokosar/RNASeqT'); \
+  install_github('jvrakor/Subread_to_DEXSeq', subdir = 'loadSubread'); \
+  install_github('genialis/shRNAde'); \
+"
