8d7ad60c99e6212704c2f324105c66b7da8bf738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:27 +0000

    Add package coinmetrics/0.1.0/coinmetrics-0.1.0-py2.py3-none-any.whl

---
+++ packages/coinmetrics/coinmetrics-0.1.0-py2.py3-none-any.whl/coinmetrics/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+Coin Metrics API Module
+"""
+from coinmetrics.api import Community
+
+from coinmetrics.api import (
+	Error,
+	InvalidAssetError,
+	InvalidDataTypeError,
+	InvalidTimeRangeError
+)
+__all__ = ['api']
