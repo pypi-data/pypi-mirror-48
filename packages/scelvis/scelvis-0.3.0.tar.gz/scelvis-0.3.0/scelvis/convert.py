b5ba482cc8a9eb8d1b8b24e9b010e694dcfd2d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:04 +0000

    Add package scelvis/0.3.0/scelvis-0.3.0.tar.gz

---
+++ packages/scelvis/scelvis-0.3.0.tar.gz/scelvis-0.3.0/scelvis/convert.py
@@ -0,0 +1,226 @@
+"""Conversion from single-cell pipeline output to SCelVis HDF5 files.
+
+Currently, only conversion from the Cell-Ranger is supported.
+"""
+
+import contextlib
+import logging
+import os
+
+import anndata
+import attr
+import scanpy.api as sc
+import pandas as pd
+from logzero import logger
+
+from .exceptions import ScelVisException
+
+
+@contextlib.contextmanager
+def with_log_level(logger, level):
+    """Set log level to warning temporarily."""
+    old_level = logger.level
+    logger.setLevel(level)
+    yield
+    logger.setLevel(old_level)
+
+
+class CellRangerConverter:
+    """Conversion of cell ranger data to SCelVis HDF5 file."""
+
+    def __init__(self, args):
+        #: Arguments as parsed by ``argparse.ArgumentParser``.`
+        self.args = args
+
+    def run(self):
+        """Perform the conversion."""
+        logger.info("Starting conversion from CellRanger output to SCelVis HDF5")
+        logger.info("Settings: %s", vars(self.args))
+        tsne = self._load_tsne()
+        pca = self._load_pca()
+        clustering = self._load_clustering()
+        diffexp = self._load_diffexp()
+        ad = self._load_expression(clustering, tsne, pca, diffexp)
+
+        if self.args.split_species:
+            ad = CellRangerConverter._split_species(ad)
+        if not self.args.use_raw:
+            ad = CellRangerConverter._normalize_filter_dge(ad)
+
+        self._write_output(ad)
+        logger.info("All done. Have a nice day!")
+
+    def _load_tsne(self):
+        tsne_file = os.path.join(
+            self.args.indir, "analysis", "tsne", "2_components", "projection.csv"
+        )
+        if not os.path.isfile(tsne_file):
+            raise ScelVisException("cannot find tSNE output at %s" % tsne_file)  # pragma nocover
+        else:
+            logging.info("Reading tSNE output from %s", tsne_file)
+            return pd.read_csv(tsne_file, header=0, index_col=0)
+
+    def _load_pca(self):
+        pca_file = os.path.join(
+            self.args.indir, "analysis", "pca", "10_components", "projection.csv"
+        )
+        if not os.path.isfile(pca_file):
+            raise ScelVisException("cannot find PCA output at %s" % pca_file)  # pragma nocover
+        else:
+            logger.info("Reading PCA output from %s", pca_file)
+            return pd.read_csv(pca_file, header=0, index_col=0)
+
+    def _load_clustering(self):
+        clustering_file = os.path.join(
+            self.args.indir, "analysis", "clustering", "graphclust", "clusters.csv"
+        )
+        if not os.path.isfile(clustering_file):
+            raise ScelVisException(
+                "cannot find clustering output at %s " % clustering_file
+            )  # pragma nocover
+        else:
+            logger.info("Reading clustering output from %s", clustering_file)
+            clustering = pd.read_csv(clustering_file, header=0)
+            nc = pd.DataFrame({"nclust": clustering["Cluster"].value_counts()})
+            clustering = clustering.set_index("Cluster")
+            clustering["nclust"] = nc
+            clustering["Cluster"] = "Cluster_" + clustering.index.astype(str)
+            clustering = clustering.set_index("Barcode")["Cluster"]
+            return clustering.astype("category")
+
+    def _load_diffexp(self):
+        diffexp_file = os.path.join(
+            self.args.indir, "analysis", "diffexp", "graphclust", "differential_expression.csv"
+        )
+        if not os.path.isfile(diffexp_file):
+            raise ScelVisException(
+                "cannot find differential expression output at " + diffexp_file
+            )  # pragma nocover
+        else:
+            logger.info("Reading differential expression output from %s", diffexp_file)
+            diffexp = pd.read_csv(diffexp_file, header=0, index_col=[0, 1])
+            diffexp.columns = (
+                diffexp.columns.str.replace("Cluster ", "Cluster_")
+                .str.split(n=1, expand=True)
+                .rename(["Cluster", "Obs"])
+            )
+            diffexp = diffexp.stack(level=0).reset_index()
+            diffexp.columns = ["GeneID", "gene", "Cluster", "p_adj", "log2_fc", "mean_counts"]
+            return diffexp
+
+    def _load_expression(self, clustering, tsne, pca, diffexp):
+        expression_file = os.path.join(self.args.indir, "filtered_feature_bc_matrix.h5")
+        if not os.path.isfile(expression_file):
+            raise ScelVisException(
+                "cannot find expression file at %s" % expression_file
+            )  # pragma nocover
+        else:
+            logger.info("Reading gene expression from %s", expression_file)
+            with with_log_level(anndata.utils.logger, logging.WARN):
+                ad = sc.read_10x_h5(expression_file)
+            ad.var_names_make_unique()
+            logger.info("Combining meta data")
+            # TODO: do we need to make variable names unique here or can we suppress the warning
+            # TODO: with ``with_log_level(anndata.utils.logger, logging.WARN)``?
+            ad.obs["cluster"] = clustering
+            ad.obs["n_counts"] = ad.X.sum(1).A1
+            ad.obs["n_genes"] = (ad.X > 0).sum(1).A1
+            logger.info("Adding coordinates")
+            ad.obsm["X_tsne"] = tsne.values
+            ad.obsm["X_pca"] = pca.values
+            logger.info("Saving top %d markers per cluster", self.args.nmarkers)
+            markers = (
+                diffexp[(diffexp["p_adj"] < 0.05) & (diffexp["log2_fc"] > 0)]
+                .drop("GeneID", axis=1)
+                .sort_values(["Cluster", "p_adj"])
+                .groupby("Cluster")
+                .head(self.args.nmarkers)
+            )
+            for col in markers.columns:
+                ad.uns["marker_" + col] = markers[col]
+            return ad
+
+    def _split_species(ad):
+        logger.info("Determining species mixing")
+        species = ad.var_names.str.split("_", n=1).str[0]
+        for sp in species.unique():
+            ad.obs["nUMI_" + sp] = ad.X[:, species == sp].sum(1).A1
+        cols = "nUMI_" + species.unique()
+        ratio = ad.obs[cols].divide(ad.obs[cols].sum(axis=1), axis=0)
+        ad.obs["species"] = (ratio > 0.95).idxmax(axis=1).str.split("_").str[1]
+        ad.obs["species"][ratio.max(axis=1) < 0.95] = "other"
+        return ad
+
+    def _normalize_filter_dge(ad):
+        logger.info("Normalizing and filtering DGE")
+        # TODO: do we need to make variable names unique here or can we suppress the warning
+        # TODO: with ``with_log_level(anndata.utils.logger, logging.WARN)``?
+        sc.pp.filter_cells(ad, min_genes=100)
+        sc.pp.filter_genes(ad, min_cells=5)
+        sc.pp.normalize_per_cell(ad, counts_per_cell_after=1.0e4)
+        sc.pp.log1p(ad)
+        return ad
+
+    def _write_output(self, ad):
+        out_file = os.path.join(self.args.outdir, "data.h5ad")
+        logger.info("Saving anndata object to %s", out_file)
+        # TODO: explicitely set column types to get rid of warning?
+        ad.write(out_file)
+
+
+@attr.s(auto_attribs=True)
+class Config:
+    """Configuration for the converter."""
+
+    #: Input directory.
+    indir: str
+    #: Output directory.
+    outdir: str
+    #: Top N markers to save.
+    nmarkers: int = 10
+    #: Whether to split by species.
+    split_species: bool = False
+    #: Use raw signal.
+    use_raw: bool = False
+
+
+def run(args, _parser=None):
+    """Main entry point after argument parsing."""
+    # TODO: detect pipeline output
+    return CellRangerConverter(args).run()
+
+
+def setup_argparse(parser):
+    """Setup argparse sub parser."""
+    parser.add_argument(
+        "-i",
+        "--input-dir",
+        required=True,
+        dest="indir",
+        help="path to input/pipeline output directory",
+    )
+    parser.add_argument("-o", "--output-dir", required=True, dest="outdir", help="output directory")
+    parser.add_argument(
+        "--use_raw",
+        dest="use_raw",
+        default=False,
+        action="store_true",
+        help="Do not normalize DGE (use raw counts)",
+    )
+    parser.add_argument(
+        "--split_species",
+        dest="split_species",
+        default=False,
+        action="store_true",
+        help="Split species",
+    )
+    parser.add_argument(
+        "--nmarkers",
+        dest="nmarkers",
+        default=10,
+        type=int,
+        help="Save top n markers per cluster [10]",
+    )
+    parser.add_argument(
+        "--verbose", default=False, action="store_true", help="Enable verbose output"
+    )
---
+++ packages/scelvis/scelvis-0.3.0.tar.gz/scelvis-0.3.0/scelvis/store.py
@@ -0,0 +1,153 @@
+"""Access to all data outside of the session.
+
+This module uses the functions from ``data`` to load the data from the appropriate location
+and memoizes the loaded data in the Flask cacche.  This module is aware of the Flask cache and
+thus also of the Dash app.
+
+Note well that the behaviour of iRODS is special because of how ticket access is implemented.
+We have to get direct access to the collection for which we have a ticket.  Then we need to
+perform a linear search for the collection's data objects and only THEN can we open them.
+"""
+
+import urllib.parse
+
+import fs.path
+import s3fs
+from irods.exception import CAT_SQL_ERR, DoesNotExist
+from logzero import logger
+
+from scelvis.data import redacted_urlunparse
+from . import data, settings
+from .exceptions import ScelVisException
+from .cache import cache
+
+
+@cache.memoize()
+def does_exist(url, path, *more_components):
+    """Return whether the given path exists behind the given URL."""
+    logger.info(
+        "Checking whether %s exists in %s",
+        fs.path.join(path, *more_components),
+        redacted_urlunparse(url),
+    )
+    if url.scheme in data.PYFS_SCHEMES:
+        curr_fs = data.make_fs(url)
+        result = curr_fs.exists(fs.path.join(path, *more_components))
+        return result
+    elif url.scheme == "s3":
+        anon = url.username is None and url.password is None
+        s3 = s3fs.S3FileSystem(anon=anon, key=url.username, secret=url.password)
+        return s3.exists("%s/%s" % (url.hostname, path))
+    elif url.scheme.startswith("irods"):
+        path_full = fs.path.join(url.path, path, *more_components)
+        with data.create_irods_session(url) as irods_session:
+            path_collection = fs.path.dirname(path_full)
+            name = fs.path.basename(path_full)
+            try:
+                collection = irods_session.collections.get(path_collection)
+                for data_object in collection.data_objects:
+                    if data_object.name == name:
+                        return True
+            except (CAT_SQL_ERR, DoesNotExist):
+                pass  # swallow
+            logger.info("=> False")
+    else:
+        raise ScelVisException("Invalid URL scheme: %s" % url.scheme)
+
+
+@cache.memoize()
+def glob_data_sets(url):
+    """Return list of all data sets behind the given ``url``."""
+    result = []
+    if url.scheme in data.PYFS_SCHEMES:
+        curr_fs = data.make_fs(url)
+        for match in curr_fs.glob(fs.path.join("*", settings.ABOUT_FILENAME)):
+            match_path = fs.path.basename(match.path)
+            logger.info("Found data set %s at %s" % (match_path, data.redacted_urlunparse(url)))
+            result.append(url._replace(path=fs.path.join(url.path, match.path)))
+    elif url.scheme == "s3":
+        anon = url.username is None and url.password is None
+        s3 = s3fs.S3FileSystem(anon=anon, key=url.username, secret=url.password)
+        if url.path:
+            pattern = "%s/%s/*/%s" % (url.hostname, url.path, settings.ABOUT_FILENAME)
+        else:
+            pattern = "%s/*/%s" % (url.hostname, settings.ABOUT_FILENAME)
+        for match in s3.glob(pattern):
+            result.append(url._replace(path=match.split("/", 1)[1]))
+    elif url.scheme.startswith("irods"):
+        with data.create_irods_session(url) as irods_session:
+            # Get pointed-to collection.
+            collection = irods_session.collections.get(url.path)
+            for sub_coll in collection.subcollections:
+                for data_obj in sub_coll.data_objects:
+                    if data_obj.name == settings.ABOUT_FILENAME:
+                        result.append(
+                            url._replace(path=fs.path.join(url.path, sub_coll.name, data_obj.name))
+                        )
+    else:
+        raise ScelVisException("Invalid URL scheme: %s" % url.scheme)
+    return result
+
+
+@cache.memoize()
+def load_all_metadata():
+    """Load all meta data information from ``settings.DATA_SOURCES``.
+
+    If ``data_source`` itself contains a file called ``about.md``, it is assumed that only one dataset is available.
+    Otherwise, all sub directories will be scanned for ``about.md`` and one entry is returned for each dataset.
+    """
+    result = []
+
+    if settings.FAKE_DATA:
+        result = [data.fake_data().metadata]
+
+    for url in settings.DATA_SOURCES:
+        if does_exist(url, settings.ABOUT_FILENAME):
+            logger.info("Loading single dataset from data source %s", data.redacted_urlunparse(url))
+            identifier = fs.path.basename(url.path)
+            collection = url._replace(path=fs.path.dirname(url.path))
+            result.append(data.load_metadata(collection, identifier))
+        else:
+            lst = []
+            for match in glob_data_sets(url):
+                identifier = fs.path.basename(fs.path.dirname(match.path))
+                lst.append(data.load_metadata(url, identifier))
+            if lst:
+                logger.info("Loaded %d data sets from data directory.", len(lst))
+            else:
+                logger.warn(
+                    "No data sets found in data directory %s", data.redacted_urlunparse(url)
+                )
+            result += lst
+    return result
+
+
+def _load(identifier, load_func):
+    urls = settings.DATA_SOURCES
+    if settings.UPLOAD_DIR:
+        urls += [urllib.parse.urlparse("file://%s" % settings.UPLOAD_DIR)]
+    for url in urls:
+        if fs.path.basename(url.path) == identifier:
+            url = url._replace(path=fs.path.dirname(url.path))
+            return load_func(url, identifier)
+        else:
+            if does_exist(url, identifier, settings.ABOUT_FILENAME):
+                return load_func(url, identifier)
+
+
+@cache.memoize()
+def load_metadata(identifier):
+    """Load metadata for the given identifier from data or upload directory."""
+    if identifier == data.FAKE_DATA_ID:
+        return data.fake_data().metadata
+    else:
+        return _load(identifier, data.load_metadata)
+
+
+@cache.memoize()
+def load_data(identifier):
+    """Load data for the given identifier from data or upload directory."""
+    if identifier == data.FAKE_DATA_ID:
+        return data.fake_data()
+    else:
+        return _load(identifier, data.load_data)
