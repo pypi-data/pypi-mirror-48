d2de98706457c259deaf4bacd86df3e132b4544e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package piny/0.5.2/piny-0.5.2.tar.gz

---
+++ packages/piny/piny-0.5.2.tar.gz/piny-0.5.2/piny/errors.py
@@ -0,0 +1,48 @@
+from typing import Any
+
+
+class PinyErrorMixin:
+    """
+    Mixin class to wrap and format original exception
+    """
+
+    msg_template: str
+
+    def __init__(self, origin: Exception = None, **context: Any) -> None:
+        """
+        Mixing for wrapping original exception
+
+        :param origin: original exception,
+               may be helpful when special method invocation needed
+        :param context: mapping used for exception message formatting
+        """
+        self.origin = origin
+        self.context = context or None
+        super().__init__()
+
+    def __str__(self) -> str:
+        return self.msg_template.format(**self.context or {})
+
+
+class ConfigError(PinyErrorMixin, Exception):
+    """
+    Base class for Piny exceptions
+    """
+
+    pass
+
+
+class LoadingError(ConfigError):
+    """
+    Exception for reading or parsing configuration file errors
+    """
+
+    msg_template = "Loading YAML file failed: {reason}"
+
+
+class ValidationError(ConfigError):
+    """
+    Exception for data validation errors
+    """
+
+    msg_template = "Validation failed: {reason}"
