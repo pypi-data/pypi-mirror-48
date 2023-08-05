83b99e7a92ba7840205295fc76c6c57ac5a2d844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:02 +0000

    Add package Jimpass/0.0.1a0/Jimpass-0.0.1a0-py3-none-any.whl

---
+++ packages/Jimpass/Jimpass-0.0.1a0-py3-none-any.whl/jimpass/config.py
@@ -0,0 +1,14 @@
+DEFAULTS = {
+    'managers': [],
+    'timeout': -1,
+    'auto_lock': False,
+    'clipboard_timeout': 5,
+    'copy_command': 'xclip',
+    'danger_mode': False,
+    'keybindings': {
+        'type_all': 'alt+1',
+        'type_user': 'alt+2',
+        'type_pass': 'alt+3'
+    },
+    'message': 'Default config loaded'
+}
