7bff33a5495d5cbdb9cc60cf55c5e1b2628ed78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package tf-encrypted/0.5.6rc1/tf-encrypted-0.5.6rc1.tar.gz

---
+++ packages/tf-encrypted/tf-encrypted-0.5.6rc1.tar.gz/tf-encrypted-0.5.6rc1/tf_encrypted/keras/__init__.py
@@ -0,0 +1,15 @@
+"""Higher-level layer abstractions built on TF Encrypted."""
+from __future__ import absolute_import
+
+from tf_encrypted.keras import engine
+from tf_encrypted.keras import models
+from tf_encrypted.keras import layers
+from tf_encrypted.keras.models import Sequential
+
+
+__all__ = [
+    'engine',
+    'models',
+    'layers',
+    'Sequential',
+]
---
+++ packages/tf-encrypted/tf-encrypted-0.5.6rc1.tar.gz/tf-encrypted-0.5.6rc1/tf_encrypted/protocol/pond/__init__.py
@@ -0,0 +1,25 @@
+"""The Pond protocol."""
+from __future__ import absolute_import
+
+from .pond import Pond
+from .pond import PondTensor, PondPublicTensor, PondPrivateTensor, PondMaskedTensor
+from .pond import TFEVariable, TFETensor, TFEInputter
+from .pond import _type
+from .pond import AdditiveFIFOQueue
+from .triple_sources import OnlineTripleSource, QueuedOnlineTripleSource
+
+__all__ = [
+    "Pond",
+    "PondPublicTensor",
+    "PondTensor",
+    "PondPublicTensor",
+    "PondPrivateTensor",
+    "PondMaskedTensor",
+    "TFEVariable",
+    "TFETensor",
+    "TFEInputter",
+    "_type",
+    "OnlineTripleSource",
+    "QueuedOnlineTripleSource",
+    "AdditiveFIFOQueue",
+]
