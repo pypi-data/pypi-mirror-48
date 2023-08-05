505d3b49e3ebd713aa96bbdb4edea7d1b6fa82f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package kts-cli/0.0.14/kts_cli-0.0.14.tar.gz

---
+++ packages/kts-cli/kts_cli-0.0.14.tar.gz/kts_cli-0.0.14/kts_cli/examples.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+VALID_EXAMPLES = ['titanic']
+
+
+def download_example(args):
+    name = args.name
+    if os.path.exists(name):
+        raise OSError(f'Path ./{name} already exists.')
+    download_name = ".kts_examples_" + str(int(time.time()))
+    os.system(f"git clone https://github.com/konodyuk/kts-examples {download_name} &&"
+              f" mv {download_name}/{name} . && rm -rf {download_name} && cd {name} && ./init.sh")
