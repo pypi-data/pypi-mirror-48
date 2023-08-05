19de82308bb8586f53a4860d6b5ba592d9cc7a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:27 +0000

    Add package beerbolaget/0.0.36b3/beerbolaget-0.0.36b3.tar.gz

---
+++ packages/beerbolaget/beerbolaget-0.0.36b3.tar.gz/beerbolaget-0.0.36b3/beerbolaget/ha_custom/beer.py
@@ -0,0 +1,49 @@
+﻿from datetime import datetime
+import json
+from beerbolaget.ha_custom import common
+
+
+class beer_handler():
+    def __init__(self, api_key, ratebeer, untappd):
+        self.api_key = api_key
+        self.ratebeer = ratebeer
+        self.untappd = untappd
+        self.beers = {}
+        self.release = None
+
+    async def update_new_beers(self):
+        self.release = await common.get_latest_release(self.api_key)
+
+        if self.release:
+            beer_available = await common.get_beverage(self.api_key, self.release)
+            for beer_item in beer_available:
+                new_beer = beer(beer_item['ProducerName'],
+                                beer_item['ProductNameBold'],
+                                beer_item['ProductNameThin'],
+                                beer_item['Type'],
+                                beer_item['Price'],
+                                beer_item['Country'])
+                self.beers[beer_item['ProductId']] = new_beer
+
+    async def get_beers(self):
+        beers = []
+        for beer in self.beers:
+            beers.append(self.beers[beer].__dict__)
+        return json.dumps(beers, ensure_ascii=False)
+
+    async def get_release(self):
+        return self.release.split('T')[0]
+
+
+class beer():
+    def __init__(self, brewery, name, detailed_name, type, price, country, show_availability=False):
+        self.availability_local = 0
+        self.availability_web = 0
+        self.brewery = brewery
+        self.name = name
+        self.detailed_name = detailed_name
+        self.type = type
+        self.price = price
+        self.country = country
+        self.rating = None
+        self.show_availability = show_availability
---
+++ packages/beerbolaget/beerbolaget-0.0.36b3.tar.gz/beerbolaget-0.0.36b3/beerbolaget/ha_custom/common.py
@@ -0,0 +1,52 @@
+﻿from datetime import date, timedelta
+import urllib.parse
+
+import requests
+
+
+async def get_latest_release(api_key, type='Öl'):
+    SellStartDateFrom = date.today() - timedelta(days=31)
+    SellStartDateTo = date.today() + timedelta(days=31)
+
+    latest = None
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'SubCategory': type,
+        'AssortmentText': 'Små partier',
+        'SellStartDateFrom': SellStartDateFrom,
+        'SellStartDateTo': SellStartDateTo,
+    })
+    beverages = await make_request(api_key, params)
+    if len(beverages) > 0:
+        latest = beverages[-1]['SellStartDate']
+    return latest
+
+
+async def get_beverage(api_key, release_date, type='Öl'):
+    params = urllib.parse.urlencode({
+        # Request parameters
+        'SubCategory': type,
+        'AssortmentText': 'Små partier',
+        'SellStartDateFrom': release_date,
+        'SellStartDateTo': release_date,
+    })
+    beverages = await make_request(api_key, params)
+    return beverages
+
+
+async def make_request(api_key, params):
+    headers = {
+        'Ocp-Apim-Subscription-Key': api_key,
+        'content-type': 'application/json'
+    }
+
+    all_beverages = []
+    try:
+        all_beverages = requests.get(
+            'https://api-extern.systembolaget.se/product/v1/product/search?%s' % params,
+            headers=headers, verify=True).json()['Hits']
+        all_beverages = sorted(all_beverages, key=lambda k: k['SellStartDate'])
+    except Exception as e:
+        print("Could not fetch data from api ({})".format(e))
+
+    return all_beverages
