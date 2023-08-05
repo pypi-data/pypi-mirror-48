86ee1d48fd493bf5ecced38ba406338293dac4fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package BxTorch/0.3.3/BxTorch-0.3.3-py3-none-any.whl

---
+++ packages/BxTorch/BxTorch-0.3.3-py3-none-any.whl/bxtorch/nn/training/__init__.py
@@ -0,0 +1,11 @@
+#
+#  nn/training/__init__.py
+#  bxtorch
+#
+#  Created by Oliver Borchert on May 20, 2019.
+#  Copyright (c) 2019 Oliver Borchert. All rights reserved.
+#
+
+from .hogwild import Hogwild
+from .supervised import SupervisedTrainer
+from .trainer import BaseTrainer
