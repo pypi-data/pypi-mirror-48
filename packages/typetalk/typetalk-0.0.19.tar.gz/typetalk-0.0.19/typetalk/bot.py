31fd4676a6eb480f41fc270c407c0095dfa74bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package typetalk/0.0.19/typetalk-0.0.19.tar.gz

---
+++ packages/typetalk/typetalk-0.0.19.tar.gz/typetalk-0.0.19/typetalk/bot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Internal Imports
+from .client import Client
+
+
+class Bot(Client):
+    """Represents a Typetalk bot"""
+
+    def __init__(self, token=None, run_async=False):
+        super().__init__(token=token, run_async=run_async)
+        self.is_bot = True
+        self.run_async = run_async
+        self.http.simple_login
