754240a57c8cb462fba041279aab48703f5aac32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package way2smswrapper/0.1.1/way2smswrapper-0.1.1-py3-none-any.whl

---
+++ packages/way2smswrapper/way2smswrapper-0.1.1-py3-none-any.whl/way2smswrapper/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.1'
+__doc__ = 'A Way2Sms Module\nRequirement : Selenium Module.\n\t\tFirefox Browser.\n\t\tGeckodriver.\n\t\tXvfb - Installation.\n\t\tXvfbwrapper Module.\nRuntime - 30sec approx.\nUsage   : import w2swrapper\nMethods : from w2swrapper import wrapper\n\t\twrapper.login()\n\t\twrapper.sms(Message)\n\nTested In Ubuntu-17.04\n\nCreated By $implic@'
