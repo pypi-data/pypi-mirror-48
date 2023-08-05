276098c41f902721e1197c0507c50d67e15ed8b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:46 +0000

    Add package hiddenalphabet-finance-nlp-model1/0.0.2/hiddenalphabet-finance-nlp-model1-0.0.2.tar.gz

---
+++ packages/hiddenalphabet-finance-nlp-model1/hiddenalphabet-finance-nlp-model1-0.0.2.tar.gz/hiddenalphabet-finance-nlp-model1-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hiddenalphabet-finance-nlp-model1",
+    version="0.0.2",
+    author="Donaldo Celaj",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description='long description',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
