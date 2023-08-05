1310d341cc864da66eb016097b41acf0f52eab93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package agora-community-sdk/0.1.10/agora-community-sdk-0.1.10.tar.gz

---
+++ packages/agora-community-sdk/agora-community-sdk-0.1.10.tar.gz/agora-community-sdk-0.1.10/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['agora_community_sdk']
+
+package_data = \
+{'': ['*'], 'agora_community_sdk': ['frontend/*', 'frontend/.vscode/*']}
+
+install_requires = \
+['nest_asyncio>=1.0,<2.0',
+ 'pillow>=6.0,<7.0',
+ 'pyppeteer>=0.0.25,<0.0.26',
+ 'selenium>=3.141,<4.0',
+ 'websockets==6.0']
+
+setup_kwargs = {
+    'name': 'agora-community-sdk',
+    'version': '0.1.10',
+    'description': 'An SDK allowing the use of Agora SDK in python',
+    'long_description': None,
+    'author': 'samyak-jain',
+    'author_email': 'samtan106@gmail.com',
+    'url': 'https://github.com/samyak-jain/AgoraPython',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
