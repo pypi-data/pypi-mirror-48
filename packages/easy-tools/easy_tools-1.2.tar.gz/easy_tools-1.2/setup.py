22a578e37063b7ca9809a856b34cb1b540f6798a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:20 +0000

    Add package easy-tools/1.2/easy_tools-1.2.tar.gz

---
+++ packages/easy-tools/easy_tools-1.2.tar.gz/easy_tools-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="easy_tools",
+    version="1.2",
+    author="Jayanth Sai",
+    long_description=long_description,
+    author_email="jayanthsai1998@gmail.com",
+    description="easy_tools is a package that helps to perform operations on iterables or datatypes with less time complexity",
+    url="https://github.com/jayanthsai1998/EasyTools",
+    license='MIT',
+    packages=['easy_tools'],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
