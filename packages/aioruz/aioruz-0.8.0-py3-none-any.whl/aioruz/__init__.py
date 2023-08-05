aee6310af779039c54a1dd3690c36f815d99d499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:45 +0000

    Add package aioruz/0.8.0/aioruz-0.8.0-py3-none-any.whl

---
+++ packages/aioruz/aioruz-0.8.0-py3-none-any.whl/aioruz/__init__.py
@@ -0,0 +1,11 @@
+from .api import (Lesson,
+                  is_hse_email,
+                  is_student_email,
+                  person_type_from_email,
+                  search, student_info,
+                  schedule,
+                  student_schedule,
+                  VERIFY_SSL)
+
+name = "aioruz"
+__version__ = (0.8)
