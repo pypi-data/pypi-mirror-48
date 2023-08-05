a505e5945db5e7ea93b2264e4d2a2dc26dae4f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:55 +0000

    Add package sinethesizer/0.1.0/sinethesizer-0.1.0.tar.gz

---
+++ packages/sinethesizer/sinethesizer-0.1.0.tar.gz/sinethesizer-0.1.0/sinethesizer/synth/__init__.py
@@ -0,0 +1,12 @@
+"""
+Synthesize sound.
+
+Author: Nikolay Lysenko
+"""
+
+
+from . import timbres, utils, waves
+from .synth import synthesize
+
+
+__all__ = ['timbres', 'utils', 'waves', 'synthesize']
