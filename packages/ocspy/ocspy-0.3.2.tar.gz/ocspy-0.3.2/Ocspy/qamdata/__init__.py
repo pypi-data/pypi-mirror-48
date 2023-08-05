10097746c97ca406c530639de2130b51ceaef81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:08 +0000

    Add package ocspy/0.3.2/ocspy-0.3.2.tar.gz

---
+++ packages/ocspy/ocspy-0.3.2.tar.gz/ocspy-0.3.2/Ocspy/qamdata/__init__.py
@@ -0,0 +1,16 @@
+from ..ReceiverDsp.dsp_tools import cal_symbols_qam
+from ..ReceiverDsp.dsp_tools import cal_scaling_factor_qam
+
+
+# import numpy as np
+# QPSK = cal_symbols_qam(4)/np.sqrt(cal_scaling_factor_qam((4)))
+#
+#
+
+
+
+
+
+
+
+
