e9bd906f67b22c5f57e710459b257769806f6878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package dival/0.1.1/dival-0.1.1.tar.gz

---
+++ packages/dival/dival-0.1.1.tar.gz/dival-0.1.1/docs/dival.datasets.rst
@@ -0,0 +1,18 @@
+dival.datasets package
+======================
+
+Submodules
+----------
+
+.. toctree::
+
+   dival.datasets.dataset
+   dival.datasets.standard
+
+Module contents
+---------------
+
+.. automodule:: dival.datasets
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ packages/dival/dival-0.1.1.tar.gz/dival-0.1.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='dival',
+      version='0.1.1',
+      description='Deep Inversion Validation Library',
+      url='https://gitlab.informatik.uni-bremen.de/dival/dival',
+      author='Johannes Leuschner',
+      author_email='jleuschn@uni-bremen.de',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy>=1.10',
+          'pandas',
+          'odl',
+          'scikit-image',
+          'scikit-learn',
+          'hyperopt',
+          'pydicom',
+          'tqdm',
+          'matplotlib'
+      ],
+      zip_safe=False)
