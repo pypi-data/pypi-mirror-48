fa1ea82a954f39dc66d4e84a03229218a45df96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:25 +0000

    Add package robotframework-confluentkafkalibrary/0.3.0/robotframework_confluentkafkalibrary-0.3.0-py3-none-any.whl

---
+++ packages/robotframework-confluentkafkalibrary/robotframework_confluentkafkalibrary-0.3.0-py3-none-any.whl/ConfluentKafkaLibrary/examples.py
@@ -0,0 +1,76 @@
+###Producer:
+
+from confluent_kafka import Producer
+
+
+p = Producer({'bootstrap.servers': 'mybroker1,mybroker2'})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+for data in some_data_source:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message, the delivery report callback
+    # will be triggered from poll() above, or flush() below, when the message has
+    # been successfully delivered or failed permanently.
+    p.produce('mytopic', data.encode('utf-8'), callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
+
+
+#########################################
+###AvroProducer
+
+from confluent_kafka import avro
+from confluent_kafka.avro import AvroProducer
+
+
+value_schema_str = """
+{
+   "namespace": "my.test",
+   "name": "value",
+   "type": "record",
+   "fields" : [
+     {
+       "name" : "name",
+       "type" : "string"
+     }
+   ]
+}
+"""
+
+key_schema_str = """
+{
+   "namespace": "my.test",
+   "name": "key",
+   "type": "record",
+   "fields" : [
+     {
+       "name" : "name",
+       "type" : "string"
+     }
+   ]
+}
+"""
+
+value_schema = avro.loads(value_schema_str)
+key_schema = avro.loads(key_schema_str)
+value = {"name": "Value"}
+key = {"name": "Key"}
+
+avroProducer = AvroProducer({
+    'bootstrap.servers': 'mybroker,mybroker2',
+    'schema.registry.url': 'http://schem_registry_host:port'
+    }, default_key_schema=key_schema, default_value_schema=value_schema)
+
+avroProducer.produce(topic='my_topic', value=value, key=key)
+avroProducer.flush()
