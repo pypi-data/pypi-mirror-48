e1854898a9170b2c32f726396010e0d08b059abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:09 +0000

    Add package hiddenalphabet-model/0.0.8/hiddenalphabet_model-0.0.8.tar.gz

---
+++ packages/hiddenalphabet-model/hiddenalphabet_model-0.0.8.tar.gz/hiddenalphabet_model-0.0.8/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hiddenalphabet_model",
+    version="0.0.8",
+    author="Donaldo Celaj",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description='long description',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
