55a7aceb283167cbe74d71845c49040defad0534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package bocadillo/0.16.2/bocadillo-0.16.2.tar.gz

---
+++ packages/bocadillo/bocadillo-0.16.2.tar.gz/bocadillo-0.16.2/bocadillo/__init__.py
@@ -0,0 +1,20 @@
+from .applications import App
+from .compat import ExpectedAsync
+from .config import SettingsError, configure, settings
+from .errors import HTTPError
+from .injection import discover_providers, provider, useprovider
+from .middleware import Middleware
+from .plugins import plugin
+from .recipes import Recipe
+from .redirection import Redirect
+from .request import ClientDisconnect, Request
+from .response import Response
+from .routing import Router
+from .sse import server_event
+from .staticfiles import static
+from .templates import Templates
+from .testing import LiveServer, create_client
+from .views import view
+from .websockets import WebSocket, WebSocketDisconnect
+
+__version__ = "0.16.2"
---
+++ packages/bocadillo/bocadillo-0.16.2.tar.gz/bocadillo-0.16.2/tests/conftest.py
@@ -0,0 +1,61 @@
+import typing
+
+import pytest
+
+from bocadillo import App, configure, create_client, Templates, Recipe, settings
+
+
+APP_CLASSES = [App, lambda: Recipe("tacos")]
+
+
+@pytest.fixture(params=APP_CLASSES, name="raw_app")
+def fixture_raw_app(request) -> App:
+    settings._clear()
+    cls = request.param
+    return cls()
+
+
+@pytest.fixture(name="app")
+def fixture_app(raw_app: App) -> App:
+    configure(raw_app)
+    return raw_app
+
+
+@pytest.fixture
+def client(app):
+    return create_client(app)
+
+
+@pytest.fixture(name="templates")
+def fixture_templates():
+    return Templates()
+
+
+class TemplateWrapper(typing.NamedTuple):
+    name: str
+    context: dict
+    rendered: str
+    root: str
+
+
+def create_template(
+    templates: Templates, tmpdir_factory, dirname: str
+) -> TemplateWrapper:
+    templates_dir = tmpdir_factory.mktemp(dirname)
+
+    template = templates_dir.join("hello.html")
+    template.write("<h1>Hello, {{ name }}!</h1>")
+
+    templates.directory = str(templates_dir)
+
+    return TemplateWrapper(
+        name="hello.html",
+        context={"name": "Bocadillo"},
+        rendered="<h1>Hello, Bocadillo!</h1>",
+        root=str(templates_dir),
+    )
+
+
+@pytest.fixture
+def template_file(templates: Templates, tmpdir_factory) -> TemplateWrapper:
+    return create_template(templates, tmpdir_factory, dirname="templates")
