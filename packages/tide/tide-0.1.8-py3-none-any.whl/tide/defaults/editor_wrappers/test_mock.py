d5be13d268d068189472ad6577051a3f01dde678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:50 +0000

    Add package tide/0.1.8/tide-0.1.8-py3-none-any.whl

---
+++ packages/tide/tide-0.1.8-py3-none-any.whl/tide/defaults/editor_wrappers/test_mock.py
@@ -0,0 +1,16 @@
+from editor_base import editor_base
+
+class test_mock(editor_base):
+
+    @staticmethod
+    def set_dictionary_value(parent_keys, value):
+        pass
+
+    def set_editor_dictionary(self, config_dictionary):
+        pass
+
+    def get_current_buffer_name(self):
+        pass
+
+    def get_current_buffer_line(self):
+        pass
---
+++ packages/tide/tide-0.1.8-py3-none-any.whl/tide/tide.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import inspect
+import traceback
+sys.path.insert(0, os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+import lib_paths
+from command_handler import CommandHandler
+from config_command import ConfigCommand
+from config_command_item import ConfigCommandItem
+from logging_decorator import logging
+
+@logging
+class Tide(object):
+
+    _startup_commands = ''
+    _command_handler = None
+
+    def start(self, commands):
+        try:
+            self._startup_commands = commands
+            self._command_handler = CommandHandler()
+            self._command_handler.spawn_process(commands)
+        except Exception as ex:
+            print("error in Tide.start(): " + str(ex))
+            print(traceback.format_exc())
+
+    def stop(self):
+        self._command_handler.close_command_handler()
+        del self._command_handler
+
+    def run_config_command(self, command, buffer_name='', event_input_args_name=''):
+        config_command_item = ConfigCommandItem()
+        config_command_item.command = command
+        config_command_item.buffer_name = buffer_name
+        config_command_item.event_input_args_name = event_input_args_name
+        ConfigCommand().run_config_command(config_command_item)
