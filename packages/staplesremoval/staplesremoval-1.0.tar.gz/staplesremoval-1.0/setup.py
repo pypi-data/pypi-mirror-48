adb2d4891e7175c7c28c22fa47bc9db82c7fb845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:12 +0000

    Add package staplesremoval/1.0/staplesremoval-1.0.tar.gz

---
+++ packages/staplesremoval/staplesremoval-1.0.tar.gz/staplesremoval-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='staplesremoval',
+    version='1.0',
+    packages=['staplesremoval'],
+    url='https://github.com/mmunar97',
+    license='mit',
+    author='marcmunar',
+    author_email='marc.munar1@estudiant.uib.es',
+    description='Algorithm for staple detection and mask generation'
+)
