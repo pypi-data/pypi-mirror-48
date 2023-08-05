4d58a2c6067479dd5dcc89afd62d617c9fcf5ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:52 +0000

    Add package logdna/1.4.2/logdna-1.4.2.tar.gz

---
+++ packages/logdna/logdna-1.4.2.tar.gz/logdna-1.4.2/logdna/configs.py
@@ -0,0 +1,7 @@
+defaults = {
+    'DEFAULT_REQUEST_TIMEOUT': 30,
+    'MAX_LINE_LENGTH': 32000,
+    'FLUSH_INTERVAL': 5,
+    'FLUSH_BYTE_LIMIT': 2 * 1024 * 1024,
+    'LOGDNA_URL': 'https://logs.logdna.com/logs/ingest'
+}
