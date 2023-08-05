5eaa3cb3fed8cd4f3f97628be5a855671fb3b0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:35 +0000

    Add package agsconfig/0.1.5/agsconfig-0.1.5-py2.py3-none-any.whl

---
+++ packages/agsconfig/agsconfig-0.1.5-py2.py3-none-any.whl/agsconfig/services/ogc_metadata_extension_mixin.py
@@ -0,0 +1,143 @@
+"""This module contains the WMS Server extension class"""
+
+# Python 2/3 compatibility
+# pylint: disable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+from future.builtins.disabled import *
+from future.builtins import *
+from future.standard_library import install_aliases
+install_aliases()
+# pylint: enable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
+
+from ..editing.edit_prop import EditorProperty
+
+
+class OGCMetadataExtensionMixin(object):
+
+    _keyword_key = "keyword"
+
+    abstract = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='abstract']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    access_constraints = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='accessConstraints']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    city = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='city']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    country = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='country']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    keyword = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name, _keyword_key: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='{1}']/Value".format(extension_name, _keyword_key)
+                        }
+                    ]
+                }
+            }
+        })
+
+    fees = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='fees']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    name = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='name']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    title = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='title']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    address = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='address']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
---
+++ packages/agsconfig/agsconfig-0.1.5-py2.py3-none-any.whl/agsconfig/services/wms_server_extension.py
@@ -0,0 +1,163 @@
+"""This module contains the WMS Server extension class"""
+
+# Python 2/3 compatibility
+# pylint: disable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+from future.builtins.disabled import *
+from future.builtins import *
+from future.standard_library import install_aliases
+install_aliases()
+# pylint: enable=wildcard-import,unused-wildcard-import,wrong-import-order,wrong-import-position
+
+from enum import Enum
+from ..editing.edit_prop import EditorProperty
+from .extension_base import ExtensionBase
+from .ogc_metadata_extension_mixin import OGCMetadataExtensionMixin
+from .custom_get_capabilities_extension_mixin import CustomGetCapabilitiesExtensionMixin
+
+class WMSServerExtension(OGCMetadataExtensionMixin, CustomGetCapabilitiesExtensionMixin, ExtensionBase):
+    """ WMS server extension properties for arcGIS services """
+
+
+    class Capability(Enum):
+        get_capabilities = "GetCapabilities"
+        get_map = "GetMap"
+        get_feature_info = "GetFeatureInfo"
+        get_styles = "GetStyles"
+        get_legend_graphic = "GetLegendGraphic"
+        get_schema_extension = "GetSchemaExtension"
+
+    def __init__(self, editor):
+        super().__init__(editor, "WMSServer")
+
+    inherit_layer_names = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='inheritLayerNames']/Value".format(extension_name)
+                        }
+                    ],
+                    "conversions": [{
+                        "id": "boolToString"
+                    }]
+                }
+            }
+        })
+
+    path_to_custom_sld_file = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='pathToCustomSLDFile']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    additional_spatial_ref_sys = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='ListSupportedCRS']/Value".format(extension_name),
+                            "parentPath":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray".format(extension_name),
+                            "tag": "PropertySetProperty",
+                            "attributes": {
+                                "{http://www.w3.org/2001/XMLSchema-instance}type": "typens:PropertySetProperty"
+                            },
+                            "children": [
+                                {
+                                    "tag": "Key",
+                                    "value": "ListSupportedCRS"
+                                },
+                                {
+                                    "tag": "Value",
+                                    "attributes": {
+                                        "{http://www.w3.org/2001/XMLSchema-instance}type": "xs:string"
+                                    }
+                                }
+                            ]
+                        }
+                    ],
+                    "conversions": [{
+                        "id": "stringToCsv"
+                    }]
+                }
+            }
+        })
+
+    reaspect = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='Reaspect']/Value".format(extension_name)
+                        }
+                    ],
+                    "conversions": [{
+                        "id": "boolToString"
+                    }]
+                }
+            }
+        })
+
+    post_code = EditorProperty(
+        {
+            "constraints": {
+                "int": True,
+                "min": 1
+            },
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='postCode']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    address_type = EditorProperty(
+        {
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='addressType']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
+
+    name = EditorProperty(
+        {
+            "constraints": {
+                "notEmpty": True
+            },
+            "formats": {
+                "sddraft": {
+                    "paths": [
+                        {
+                            "path":
+                            lambda extension_name: "./Configurations/SVCConfiguration/Definition/Extensions/SVCExtension[TypeName='{0}']/Props/PropertyArray/PropertySetProperty[Key='name']/Value".format(extension_name)
+                        }
+                    ]
+                }
+            }
+        })
