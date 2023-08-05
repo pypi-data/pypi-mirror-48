1885b241ac4a73e6182f084428c6efa617dbffb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package itk-cuberille/2.0.1/itk_cuberille-2.0.1-cp37-cp37m-win_amd64.whl

---
+++ packages/itk-cuberille/itk_cuberille-2.0.1-cp37-cp37m-win_amd64.whl/itk/Configuration/CuberilleConfig.py
@@ -0,0 +1,8 @@
+depends = ('ITKPyBase', 'ITKMesh', 'ITKImageGradient', 'ITKImageFunction', 'ITKCommon', )
+templates = (
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIF2MF2', True, 'itk::Image< float,2 >, itk::Mesh< float,2 >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterID2MD2', True, 'itk::Image< double,2 >, itk::Mesh< double,2 >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterIF3MF3', True, 'itk::Image< float,3 >, itk::Mesh< float,3 >'),
+  ('CuberilleImageToMeshFilter', 'itk::CuberilleImageToMeshFilter', 'itkCuberilleImageToMeshFilterID3MD3', True, 'itk::Image< double,3 >, itk::Mesh< double,3 >'),
+)
+snake_case_functions = ('cuberille_image_to_mesh_filter', 'mesh_source', 'image_to_mesh_filter', )
