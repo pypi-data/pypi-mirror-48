873b727af0e3eff42991f4bcdbe990d77a300b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:52 +0000

    Add package hostsed/0.3.0/hostsed-0.3.0.tar.gz

---
+++ packages/hostsed/hostsed-0.3.0.tar.gz/hostsed-0.3.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+long_description = ""
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    pass
+
+sdict = {
+    'name': 'hostsed',
+    'version': "0.3.0",
+    'packages': ['hosts'],
+    'zip_safe': False,
+    'author': 'lichun',
+    'url': 'https://github.com/socrateslee/hostsed',
+    'scripts': ['hostsed'],
+    'long_description': long_description,
+    'classifiers': [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python']
+}
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**sdict)
