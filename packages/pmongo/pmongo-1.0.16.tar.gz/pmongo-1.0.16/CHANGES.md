f0bcb5182e109da4842ba12fa0f3263ae6936c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:24 +0000

    Add package pmongo/1.0.16/pmongo-1.0.16.tar.gz

---
+++ packages/pmongo/pmongo-1.0.16.tar.gz/pmongo-1.0.16/CHANGES.md
@@ -0,0 +1,38 @@
+CHANGES
+===============
+1.0.0
+
+- first version
+
+
+1.0.6
+
+- support "create, update"
+
+1.0.8
+
+- fix find() with dictionary param
+
+1.0.9
+
+- suport values()
+
+1.0.10
+
+- suport Meta ordering
+
+1.0.11
+
+- remove __eq__ of Document
+
+1.0.12
+
+- support Python 3.x
+
+1.0.14
+
+- support update_fields when save a document
+
+1.0.16
+
+- fix $lte date bug
---
+++ packages/pmongo/pmongo-1.0.16.tar.gz/pmongo-1.0.16/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os, io
+
+__version__ = '1.0.16'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = io.open(os.path.join(here, 'README.md'), encoding='UTF-8').read()
+CHANGES = io.open(os.path.join(here, 'CHANGES.md'), encoding='UTF-8').read()
+setup(name="pmongo",
+      version=__version__,
+      keywords=('mongo', 'django', 'orm', 'nosql'),
+      description="A small Python MongoDB Document-Based access engine.",
+      long_description=README + '\n\n\n' + CHANGES,
+      long_description_content_type = "text/markdown",
+      url='https://github.com/sintrb/pmongo/',
+      author="trb",
+      author_email="sintrb@gmail.com",
+      packages=['pmongo'],
+      install_requires=['pymongo', 'six'],
+      zip_safe=False
+      )
