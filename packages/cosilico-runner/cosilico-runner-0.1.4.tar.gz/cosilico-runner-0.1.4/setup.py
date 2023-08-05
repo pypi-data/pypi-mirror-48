564f2176242cef19be56ae89040a1941f8cb2f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package cosilico-runner/0.1.4/cosilico-runner-0.1.4.tar.gz

---
+++ packages/cosilico-runner/cosilico-runner-0.1.4.tar.gz/cosilico-runner-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cosilico-runner",
+    version="0.1.4",
+    author="Erik Storrs",
+    author_email="epstorrs@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+#    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'cosilico-runner=cosilico_runner.cosilico_runner:main',
+        ],
+    },
+)
