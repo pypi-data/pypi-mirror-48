520d6c200a27ad31f090bd9042d4aa96a5e07710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package orz/0.1.0/orz-0.1.0-py2.py3-none-any.whl

---
+++ packages/orz/orz-0.1.0-py2.py3-none-any.whl/orz/__init__.py
@@ -0,0 +1,12 @@
+from .result import Result, Ok, Err, as_result, catch, resultify, first_ok
+from .exceptions import InvalidValueError
+
+__all__ = [
+    'Result',
+    'Ok',
+    'Err',
+    'as_result'
+    'catch',
+    'resultify',
+    'first_ok'
+]
