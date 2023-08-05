bcf3e4898bfbe0ae5611d63732b8f2aa01c30047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:36 +0000

    Add package videocloud/2.0/videocloud-2.0.tar.gz

---
+++ packages/videocloud/videocloud-2.0.tar.gz/videocloud-2.0/README.md
@@ -0,0 +1,19 @@
+# VideoCloud
+[![GitHub](https://img.shields.io/github/license/paramt/videocloud.svg)](LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/videocloud.svg)](https://pypi.org/project/VideoCloud/)
+
+Generate word clouds from YouTube video captions
+
+## Installation
+
+```
+pip install videocloud
+```
+
+## Usage
+
+```
+videocloud <link to youtube video> [filepath]
+```
+If no filepath is given, the wordcloud will be generated in `./videocloud.png`
+
---
+++ packages/videocloud/videocloud-2.0.tar.gz/videocloud-2.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="videocloud",
+    version="2.0",
+    url="https://github.com/paramt/VideoCloud",
+    license="MIT",
+    author="Param Thakkar",
+    author_email="contact@param.me",
+    description="A command line tool that generates word clouds from YouTube video captions",
+    long_description=long_description,
+	long_description_content_type="text/markdown",
+    packages=["videocloud"],
+    install_requires=[
+        "setuptools",
+        "pillow==6.0.0",
+        "wordcloud==1.5.0",
+        "youtube-transcript-api==0.1.4"
+    ],
+    python_requires=">= 3.6",
+	entry_points={
+        'console_scripts': [
+            'videocloud=videocloud.main:main'
+        ]
+    }
+)
