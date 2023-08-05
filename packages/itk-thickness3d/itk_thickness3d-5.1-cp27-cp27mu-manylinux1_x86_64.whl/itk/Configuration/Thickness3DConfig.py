57a0dd9d488dea31e0626e85abf814993a20f779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:37 +0000

    Add package itk-thickness3d/5.1/itk_thickness3d-5.1-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/itk-thickness3d/itk_thickness3d-5.1-cp27-cp27mu-manylinux1_x86_64.whl/itk/Configuration/Thickness3DConfig.py
@@ -0,0 +1,12 @@
+depends = ('ITKPyBase', 'ITKImageSources', 'ITKImageIntensity', 'ITKImageFilterBase', 'ITKDistanceMap', 'ITKCommon', )
+templates = (
+  ('BinaryThinningImageFilter3D', 'itk::BinaryThinningImageFilter3D', 'itkBinaryThinningImageFilter3DISS3ISS3', True, 'itk::Image< signed short,3 >, itk::Image< signed short,3 >'),
+  ('BinaryThinningImageFilter3D', 'itk::BinaryThinningImageFilter3D', 'itkBinaryThinningImageFilter3DIUC3IUC3', True, 'itk::Image< unsigned char,3 >, itk::Image< unsigned char,3 >'),
+  ('BinaryThinningImageFilter3D', 'itk::BinaryThinningImageFilter3D', 'itkBinaryThinningImageFilter3DIUS3IUS3', True, 'itk::Image< unsigned short,3 >, itk::Image< unsigned short,3 >'),
+  ('BinaryThinningImageFilter3D', 'itk::BinaryThinningImageFilter3D', 'itkBinaryThinningImageFilter3DIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('MedialThicknessImageFilter3D', 'itk::MedialThicknessImageFilter3D', 'itkMedialThicknessImageFilter3DISS3IF3', True, 'itk::Image< signed short,3 >, itk::Image< float,3 >'),
+  ('MedialThicknessImageFilter3D', 'itk::MedialThicknessImageFilter3D', 'itkMedialThicknessImageFilter3DIUC3IF3', True, 'itk::Image< unsigned char,3 >, itk::Image< float,3 >'),
+  ('MedialThicknessImageFilter3D', 'itk::MedialThicknessImageFilter3D', 'itkMedialThicknessImageFilter3DIUS3IF3', True, 'itk::Image< unsigned short,3 >, itk::Image< float,3 >'),
+  ('MedialThicknessImageFilter3D', 'itk::MedialThicknessImageFilter3D', 'itkMedialThicknessImageFilter3DIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+)
+snake_case_functions = ('binary_thinning_image_filter3_d', 'medial_thickness_image_filter3_d', )
