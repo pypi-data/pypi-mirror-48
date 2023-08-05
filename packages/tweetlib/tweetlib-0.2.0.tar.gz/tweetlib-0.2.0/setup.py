af0610c2fd7cbc49e1144638adae52ef534d7805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:18 +0000

    Add package tweetlib/0.2.0/tweetlib-0.2.0.tar.gz

---
+++ packages/tweetlib/tweetlib-0.2.0.tar.gz/tweetlib-0.2.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tweetlib",
+    version="0.2.0",
+    author="Wyatt Ferguson",
+    author_email="wyattf@gmail.com",
+    description="A python twitter scraper with no-api keys required.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wyattferguson/tweetlib",
+    packages=['tweetlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
