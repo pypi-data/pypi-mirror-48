24f6690e9f9a5a15ae3b8eb93eab35a6f268f3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:31 +0000

    Add package burdock/0.0.7/burdock-0.0.7-py3-none-any.whl

---
+++ packages/burdock/burdock-0.0.7-py3-none-any.whl/burdock/__main__.py
@@ -0,0 +1,83 @@
+import argparse
+from io import TextIOWrapper
+from os import path
+
+import pandas as pd
+from jinja2 import Environment, PackageLoader, Template
+from pandas import DataFrame, Series
+from pandas.core.dtypes.common import is_integer_dtype, is_float_dtype, is_string_dtype, is_bool_dtype
+
+from burdock import Burdock
+from burdock.expander import statistics_expander
+from burdock.matcher.common import numeric_matcher
+from burdock.util.subprocess import run_daikon
+
+# todo: use `constant` flag in decls to avoid output in traces.
+
+# todo: mark synthetic/derived/latent variables in decls var flags.
+# todo: use valid-values for categorical data.
+
+# todo: invent our own new flags...?
+
+parser = argparse.ArgumentParser(description='Produce .dtrace and .decls for a given CSV/TSV file.')
+parser.add_argument('input_file',
+                    metavar='path',
+                    type=argparse.FileType('r', encoding='utf-8'))
+parser.add_argument('--out-decls',
+                    dest='output_decls_file',
+                    metavar='path',
+                    type=argparse.FileType('w+', encoding='utf-8'))
+parser.add_argument('--out-dtrace',
+                    dest='output_dtrace_file',
+                    metavar='path',
+                    type=argparse.FileType('w+', encoding='utf-8'))
+
+
+def main(args):
+    input_file: TextIOWrapper = args.input_file
+    input_path = path.normpath(input_file.name)
+    input_dirname, input_filename = path.split(input_path)
+
+    df: DataFrame = pd.read_csv(args.input_file)
+    name = path.splitext(input_filename)[0]
+
+    b = Burdock(name, df)
+    # b.add_matcher(numeric_matcher)
+    # b.add_expander(statistics_expander)
+
+    b.match()
+    b.expand()
+
+    print("\nVariables:\n{}\n".format(b.variables))
+    print("\nTraces:\n{}\n".format(b.traces))
+    print("\nLatent Variables:\n{}\n".format(b.latent_variables))
+    print("\nLatent Traces:\n{}\n".format(b.latent_traces))
+
+    # Decls
+    output_decls_file = args.output_decls_file
+    if not output_decls_file:
+        output_decls_path = path.join(input_dirname, name + '.decls')
+        output_decls_file = open(output_decls_path, 'w+')
+
+    b.write_decls(output_decls_file)
+    output_decls_file.flush()
+
+    # Dtrace
+    output_dtrace_file = args.output_dtrace_file
+    if not output_dtrace_file:
+        output_dtrace_path = path.join(input_dirname, name + '.dtrace')
+        output_dtrace_file = open(output_dtrace_path, 'w+')
+
+    b.write_dtrace(output_dtrace_file)
+    output_dtrace_file.flush()
+
+    run_daikon(path.abspath(output_decls_file.name), path.abspath(output_dtrace_file.name))
+
+
+def entry():
+    args = parser.parse_args()
+    main(args)
+
+
+if __name__ == '__main__':
+    entry()
