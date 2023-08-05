f7601676414ae55269564c4b5c3b9fd6d2e66947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:55:48 +0000

    Add package mxmulpy.pkg/0.1.0/mxmulpy.pkg-0.1.0.tar.gz

---
+++ packages/mxmulpy.pkg/mxmulpy.pkg-0.1.0.tar.gz/mxmulpy.pkg-0.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description=fh.read()
+
+setuptools.setup(
+    name="mxmulpy.pkg",
+    version="0.1.0",
+    author="fufuheihei"
+)
