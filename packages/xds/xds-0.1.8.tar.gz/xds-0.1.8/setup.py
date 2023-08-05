fdb7ce2acbc583e0df7fcbdfe08bea269d705210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:25 +0000

    Add package xds/0.1.8/xds-0.1.8.tar.gz

---
+++ packages/xds/xds-0.1.8.tar.gz/xds-0.1.8/setup.py
@@ -0,0 +1,27 @@
+﻿#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: Xuan Ma
+# Mail: xuan.ma1@northwestern.edu
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "xds",
+    version = "0.1.8",
+    keywords = ("xds", "NOMAD","data loading"),
+    description = "Loading xds structure",
+    long_description = "Python codes for loading xds data structure",
+    license = "MIT Licence",
+
+    url = "https://github.com/xuanma/xds",    
+    author = "Xuan Ma",
+    author_email = "xuan.ma1@northwestern.edu",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["scipy"] 
+)
