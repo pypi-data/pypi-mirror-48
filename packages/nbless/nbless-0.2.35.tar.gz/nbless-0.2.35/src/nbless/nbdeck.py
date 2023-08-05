79a8f35e9e0732b5b06a60c8c5a07bb57ab1016d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:30 +0000

    Add package nbless/0.2.35/nbless-0.2.35.tar.gz

---
+++ packages/nbless/nbless-0.2.35.tar.gz/nbless-0.2.35/src/nbless/nbdeck.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python
+import nbformat
+
+
+def nbdeck(in_file: str) -> nbformat.notebooknode.NotebookNode:
+    """ Set up a Jupyter notebook to be viewed as or converted into slides.
+
+    Sets `slide_type` to ``slide`` for markdown cells that start with headers.
+    After running ``nbdeck``, you can
+    - view Jupyter notebooks on nbviewer or with the RISE extension
+    - create an HTML slideshow with ``nbconv`` or ``jupyter nbconvert``.
+
+    :param in_file: The name of the input Jupyter notebook file.
+    :return: An ``nbformat.NotebookNode`` object with edited slideshow metadata.
+    """
+    nb = nbformat.read(in_file, as_version=4)
+    for n, cell in enumerate(nb.cells):
+        if cell.cell_type == "markdown" and cell.source.startswith("#"):
+            nb.cells[n].metadata.slideshow = {"slide_type": "slide"}
+    return nb
---
+++ packages/nbless/nbless-0.2.35.tar.gz/nbless-0.2.35/src/nbless/nbuild.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from typing import List
+from pathlib import Path
+
+from nbformat.v4 import new_notebook, new_code_cell, new_markdown_cell
+from nbformat.notebooknode import NotebookNode
+
+
+def nbuild(in_files: List[str]) -> NotebookNode:
+    """Create an unexecuted Jupyter notebook from markdown and code files.
+
+    :param in_files: A list of source file names.
+    :return: An unexecuted ``nbformat.NotebookNode`` object.
+    """
+    nb = new_notebook()
+    nb.cells = [
+        new_code_cell(Path(name).read_text())
+        if name.endswith((".py", ".R"))
+        else new_markdown_cell(Path(name).read_text())
+        for name in in_files
+    ]
+
+    return nb
