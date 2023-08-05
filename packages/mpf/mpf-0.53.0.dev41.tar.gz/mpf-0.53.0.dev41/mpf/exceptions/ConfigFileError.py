cbcd97fb41707d6b9548093ca86e2f8027745e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package mpf/0.53.0.dev41/mpf-0.53.0.dev41.tar.gz

---
+++ packages/mpf/mpf-0.53.0.dev41.tar.gz/mpf-0.53.0.dev41/mpf/exceptions/ConfigFileError.py
@@ -0,0 +1,24 @@
+"""Config file error in a MPF config file."""
+
+
+class ConfigFileError(AssertionError):
+
+    """Error in a config file found."""
+
+    def __init__(self, message, error_no, logger_name, context=None):
+        """Initialise exception."""
+        self._logger_name = logger_name
+        self._error_no = error_no
+        self._context = context
+        super().__init__(message)
+
+    def __str__(self):
+        """Return nice string."""
+        error_slug = "CFE-{}-{}".format(self._logger_name, self._error_no)
+        error_url = "https://docs.missionpinball.org/logs/{}.html".format(error_slug)
+        if self._context:
+            return "Config File Error in {}: {} Context: {} Error Code: {} ({})".format(
+                self._logger_name, super().__str__(), self._context, error_slug, error_url)
+        else:
+            return "Config File Error in {}: {} Error Code: {} ({})".format(
+                self._logger_name, super().__str__(), error_slug, error_url)
