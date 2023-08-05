3206c5a2ce3d8c2a6ef5a2350fc7ee4425b5a8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:00 +0000

    Add package easysql2pd/0.0.2.6/easysql2pd-0.0.2.6.tar.gz

---
+++ packages/easysql2pd/easysql2pd-0.0.2.6.tar.gz/easysql2pd-0.0.2.6/easysql2pd/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.0.2.6"
+
+from .esqlk import *
---
+++ packages/easysql2pd/easysql2pd-0.0.2.6.tar.gz/easysql2pd-0.0.2.6/easysql2pd/test_0.py
@@ -0,0 +1,84 @@
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 23:06:55 2019
+
+@author: Steven
+"""
+
+import pandas as pd
+import numpy as np
+import easysql2pd as es
+import sqlite3
+
+#sqlite_db_file = 'D:\\test.db'
+
+#es.engine = sqlite3.connect(sqlite_db_file)
+
+def SQL(st=es.st_tables):
+    return es.SQL(globals(),st)
+
+aa = pd.DataFrame(np.arange(12))
+
+bb = pd.DataFrame(np.arange(3))
+
+
+cc = SQL('''
+         copy ['aa'] to fixed_table
+         ''')
+
+cc = SQL('''
+         select name from fixed_table
+         ''')
+print(cc)
+
+cc = SQL('''
+         select 'aa1' as tbl,count(*) as cnt from aa
+           union all
+         select 'bb1' as tbl,count(*) as cnt from bb
+       '''
+       )
+
+print(cc)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
