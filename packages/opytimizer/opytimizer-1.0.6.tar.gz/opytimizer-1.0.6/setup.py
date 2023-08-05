39156e5d918357f049ad0783d0c2adaaf9b0e1c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:39 +0000

    Add package opytimizer/1.0.6/opytimizer-1.0.6.tar.gz

---
+++ packages/opytimizer/opytimizer-1.0.6.tar.gz/opytimizer-1.0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+setup(name='opytimizer',
+      version='1.0.6',
+      description='Meta-Inspired Python Optimizer',
+      author='Gustavo Rosa',
+      author_email='gth.rosa@uol.com.br',
+      url='https://github.com/gugarosa/opytimizer',
+      license='MIT',
+      install_requires=['coverage>=4.5.2',
+                        'numpy>=1.13.3',
+                        'pylint>=1.7.4',
+                        'pytest>=3.2.3'
+                       ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                   ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
