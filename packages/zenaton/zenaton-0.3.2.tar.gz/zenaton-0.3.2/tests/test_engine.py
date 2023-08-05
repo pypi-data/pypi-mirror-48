de86d8b66d542f3e7c68e04b41fca3fa0a7d0efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:57 +0000

    Add package zenaton/0.3.2/zenaton-0.3.2.tar.gz

---
+++ packages/zenaton/zenaton-0.3.2.tar.gz/zenaton-0.3.2/tests/test_engine.py
@@ -0,0 +1,28 @@
+import pytest
+import pytest_mock
+
+from zenaton.exceptions import InvalidArgumentError
+
+
+class FakeTask:
+    pass
+
+
+@pytest.mark.usefixtures("engine")
+def test_check_argument(engine):
+    with pytest.raises(InvalidArgumentError):
+        engine.execute([FakeTask])
+
+
+@pytest.mark.usefixtures("engine", "task0", "sequential_workflow")
+def test_valid_job(engine, sequential_workflow, task0):
+    assert not engine.valid_job(FakeTask)
+    assert engine.valid_job(sequential_workflow)
+    assert engine.valid_job(task0)
+
+
+@pytest.mark.usefixtures("client", "engine", "task0")
+def test_dispatch_task(client, engine, task0, mocker):
+    mocker.spy(client, "start_task")
+    task0.dispatch()
+    assert client.start_task.call_count == 1
