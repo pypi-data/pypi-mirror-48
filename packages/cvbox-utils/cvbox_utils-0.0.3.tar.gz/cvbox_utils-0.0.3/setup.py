304d1812228f971a1342b086008b775411e87d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package cvbox-utils/0.0.3/cvbox_utils-0.0.3.tar.gz

---
+++ packages/cvbox-utils/cvbox_utils-0.0.3.tar.gz/cvbox_utils-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='cvbox_utils',
+        version='0.0.3',
+        description='bbox class that easy to cope with',
+        author='Cowhisper',
+        author_email='1187203155@qq.com',
+        url='https://github.com/iHateTa11B0y',
+        )
+
+
