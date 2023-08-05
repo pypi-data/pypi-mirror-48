13a7df31b1f20a9103a9863326ad9439417e7429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:28 +0000

    Add package aiida-export-migration-tests/0.5.1/aiida-export-migration-tests-0.5.1.tar.gz

---
+++ packages/aiida-export-migration-tests/aiida-export-migration-tests-0.5.1.tar.gz/aiida-export-migration-tests-0.5.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aiida-export-migration-tests",
+    version="0.5.1",
+    author="The AiiDA team",
+    author_email="developers@aiida.net",
+    description="Export migrations tests for AiiDA",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aiidateam/aiida-export-migration-tests",
+    packages=find_packages(),
+    license="MIT Licence",
+    include_package_data=True)
