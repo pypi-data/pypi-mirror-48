b4a4649d77507c5621c67b9722daa208aac11827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:22 +0000

    Add package begoneads/0.0.8/begoneads-0.0.8-py3-none-any.whl

---
+++ packages/begoneads/begoneads-0.0.8-py3-none-any.whl/begoneads/collectors/base_collector.py
@@ -0,0 +1,53 @@
+import requests
+import re
+from tqdm import tqdm
+
+
+class BaseCollector(object):
+    """A base class for all collectors
+
+    Attributes:
+        sources: list
+    """
+
+    def __init__(self, sources):
+        self.sources = sources
+
+    def try_get_sources(self, sources):
+        """Try and get each file"""
+
+        filtered = []
+
+        for source in tqdm(sources):
+            with open(source) as _file:
+                filtered.append(_file.read())
+
+        return filtered
+
+    def fix_ips(self, hosts):
+        """Replace all IP addresses with 0.0.0.0"""
+
+        hosts = re.sub(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',
+                       '0.0.0.0', hosts, 0, re.MULTILINE)
+
+        return hosts
+
+    def filter_hosts(self, hosts):
+        """Only keep meaningful lines"""
+
+        hosts = hosts.split('\n')
+        hosts = [line for line in hosts
+                 if line.strip() != '' and not line.startswith('#')]
+        hosts = '\n'.join(hosts)
+
+        return hosts
+
+    def get_result(self):
+        """Get usable result"""
+
+        sources = self.try_get_sources(self.sources)
+        sources = '\n'.join(sources)
+        hosts = self.fix_ips(sources)
+        hosts = self.filter_hosts(hosts)
+
+        return hosts
---
+++ packages/begoneads/begoneads-0.0.8-py3-none-any.whl/begoneads/hostsmanager_test.py
@@ -0,0 +1,54 @@
+from begoneads.hostsmanager import HostsManager, start_marker, end_marker
+from unittest.mock import patch, mock_open
+
+without_begoneads = '''
+127.0.0.1 localhost whatever-hostname
+0.0.0.0 some.local
+'''
+
+with_begoneads = f'''
+127.0.0.1 localhost whatever-hostname
+0.0.0.0 some.local
+
+{start_marker}
+0.0.0.0 inserted.by.begoneads
+{end_marker}
+'''
+
+
+def test_has_begoneads():
+    with patch('builtins.open', mock_open(read_data=without_begoneads)) as mock_file:
+        hosts_manager = HostsManager('/etc/hosts')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
+
+        assert hosts_manager.has_begoneads() is None
+
+    with patch('builtins.open', mock_open(read_data=with_begoneads)) as mock_file:
+        hosts_manager = HostsManager('/etc/hosts')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
+
+        assert hosts_manager.has_begoneads()
+
+
+def test_apply_hosts():
+    with patch('builtins.open', mock_open(read_data=without_begoneads)) as mock_file:
+        hosts_manager = HostsManager('/etc/hosts')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
+
+        assert hosts_manager.has_begoneads() is None
+
+        hosts_manager.apply_hosts('0.0.0.0 some.test')
+
+        assert hosts_manager.has_begoneads()
+
+
+def test_remove_begoneads():
+    with patch('builtins.open', mock_open(read_data=with_begoneads)) as mock_file:
+        hosts_manager = HostsManager('/etc/hosts')
+        mock_file.assert_called_with('/etc/hosts', 'r', encoding='utf-8')
+
+        assert hosts_manager.has_begoneads()
+
+        hosts_manager.remove_begoneads()
+
+        assert hosts_manager.has_begoneads() is None
