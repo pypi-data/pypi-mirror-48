82c46f9d9effcb10050d2e8ecf63c8b04c85b655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package kts-cli/0.0.15/kts_cli-0.0.15.tar.gz

---
+++ packages/kts-cli/kts_cli-0.0.15.tar.gz/kts_cli-0.0.15/kts_cli/__init__.py
@@ -0,0 +1,28 @@
+import argparse
+import sys
+sys.path.insert(0, '.')
+
+
+from .file_system import build_file_system, create_config
+from .examples import download_example, VALID_EXAMPLES
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(func=parser.print_help)
+    subparsers = parser.add_subparsers(dest="subcommand",
+                                       title='subcommands',
+                                       description='valid subcommands')
+    init_parser = subparsers.add_parser('init')
+    init_parser.set_defaults(func=build_file_system)
+    example_parser = subparsers.add_parser('example')
+    example_parser.add_argument('name', type=str, help=f'Name of example, one of {VALID_EXAMPLES}')
+    example_parser.set_defaults(func=download_example)
+    args = parser.parse_args(sys.argv[1:])
+    args.func(args)
+    # if args.subcommand == 'init':
+    #     build_file_system()
+    # elif args.subcommand == 'example':
+    #
+    # else:
+    #     parser.print_help()
