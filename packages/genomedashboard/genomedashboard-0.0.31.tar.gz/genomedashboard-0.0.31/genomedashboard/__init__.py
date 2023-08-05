7eeb475a76b65a2adc0bba93b3713f9860414356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package genomedashboard/0.0.31/genomedashboard-0.0.31.tar.gz

---
+++ packages/genomedashboard/genomedashboard-0.0.31.tar.gz/genomedashboard-0.0.31/genomedashboard/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Genome Dashboard"""
+
+
+from os import path
+from io import open
+from . import genomedashboard
+
+
+# Get the current version from a single source of truth.
+# with open('../VERSION') as version_file:
+#     current_version = str(version_file.read())
+#     print(current_version)
+
+
+# -->>> !!!! IMPORTANT: BUMP THE VERSION WITH EVERY COMMIT USING SEMVER CONVENTIONS  <Major.minor.patch> !!!! <<<--
+# Get the current version from a single source of truth.
+# here = path.abspath(path.dirname(__file__))
+# with open(path.join(here, '../VERSION.md'), encoding='utf-8') as e:
+#     current_version = str(e.read())
+    # print(current_version)
+
+
+# -->>> !!!! IMPORTANT: BUMP THE VERSION WITH EVERY COMMIT USING SEMVER CONVENTIONS  <Major.minor.patch> !!!! <<<--
+# __version__ =  str(current_version)
+
+__version__ = '0.0.31'
+__author__ = 'Zilong Li, Ran Sun, Thomas C. Bishop'
+__email__ = 'genome.dashboard@gmail.com'
