5eef01285c46bc24bf43a73f0b9cd8a6e0de3902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:07 +0000

    Add package pySpeechAlien/1.3/pySpeechAlien-1.3-py2-none-any.whl

---
+++ packages/pySpeechAlien/pySpeechAlien-1.3-py2-none-any.whl/pySpeechAlien/__init__.py
@@ -0,0 +1,228 @@
+import parselmouth
+from parselmouth.praat import call, run_file
+import glob
+import pandas as pd
+import numpy as np
+import scipy
+from scipy.stats import binom
+from scipy.stats import ks_2samp
+from scipy.stats import ttest_ind
+import os
+
+## return number of syllables in speech
+def syllables(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        return(objects[1].strip().split()[0])
+    except:
+        z3=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return number of pauses in speech
+def pauses(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        return(objects[1].strip().split()[1])
+    except:
+        z3=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return syllables per second of actual speaking
+def speech_rate(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        syllables = float(objects[1].strip().split()[0]);
+        speaking_duration = float(objects[1].strip().split()[4])
+        return(str(syllables/speaking_duration))
+    except:
+        z3=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return num of seconds of actual speaking
+def speaking_duration(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        return(objects[1].strip().split()[4])
+    except:
+        z4=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return num of seconds of the whole audio file
+def total_duration(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        return(objects[1].strip().split()[5])
+    except:
+        z4=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return ratio of speaking duration to total duration
+def speaking_to_total(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        speaking = float(objects[1].strip().split()[4])
+        total = float(objects[1].strip().split()[5])
+        return(str(speaking / total))
+    except:
+        z4=0
+        return ("Try again the sound of the audio was not clear")
+    return;
+
+## return gender of speaker
+def gender(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=float(z2[8]) # will be the integer number 10
+        z4=float(z2[7]) # will be the floating point number 8.3
+        if z4<=114:
+            g=101
+            j=3.4
+        elif z4>114 and z4<=135:
+            g=128
+            j=4.35
+        elif z4>135 and z4<=163:
+            g=142
+            j=4.85
+        elif z4>163 and z4<=197:
+            g=182
+            j=2.7
+        elif z4>197 and z4<=226:
+            g=213
+            j=4.5
+        elif z4>226:
+            g=239
+            j=5.3
+        else:
+            print("Voice not recognized")
+            exit()
+        def teset(a,b,c,d):
+            d1=np.random.wald(a, 1, 1000)
+            d2=np.random.wald(b,1,1000)
+            d3=ks_2samp(d1, d2)
+            c1=np.random.normal(a,c,1000)
+            c2=np.random.normal(b,d,1000)
+            c3=ttest_ind(c1,c2)
+            y=([d3[0],d3[1],abs(c3[0]),c3[1]])
+            return y
+        nn=0
+        mm=teset(g,j,z4,z3)
+        while (mm[3]>0.05 and mm[0]>0.04 or nn<5):
+            mm=teset(g,j,z4,z3)
+            nn=nn+1
+        nnn=nn
+        if mm[3]<=0.09:
+            mmm=mm[3]
+        else:
+            mmm=0.35
+        if z4>97 and z4<=114:
+            return("Male")
+        elif z4>114 and z4<=135:
+            return("Male")
+        elif z4>135 and z4<=163:
+            return("Male")
+        elif z4>163 and z4<=197:
+            return("Female")
+        elif z4>197 and z4<=226:
+            return("Female")
+        elif z4>226 and z4<=245:
+            return("Female")
+        else:
+            return("Voice not recognized")
+    except:
+        return ("Try again the sound of the audio was not clear")
+
+## return mood of speaker
+def mood(m,p):
+    sound=p+"/"+m+".wav"
+    sourcerun=p+"/myspsolution.praat"
+    path=p+"/"
+    try:
+        objects= run_file(sourcerun, -20, 2, 0.3, "yes",sound,path, 80, 400, 0.01, capture_output=True)
+        z1=str( objects[1]) # This will print the info from the textgrid object, and objects[1] is a parselmouth.Data object with a TextGrid inside
+        z2=z1.strip().split()
+        z3=float(z2[8]) # will be the integer number 10
+        z4=float(z2[7]) # will be the floating point number 8.3
+        if z4<=114:
+            g=101
+            j=3.4
+        elif z4>114 and z4<=135:
+            g=128
+            j=4.35
+        elif z4>135 and z4<=163:
+            g=142
+            j=4.85
+        elif z4>163 and z4<=197:
+            g=182
+            j=2.7
+        elif z4>197 and z4<=226:
+            g=213
+            j=4.5
+        elif z4>226:
+            g=239
+            j=5.3
+        else:
+            print("Voice not recognized")
+            exit()
+        def teset(a,b,c,d):
+            d1=np.random.wald(a, 1, 1000)
+            d2=np.random.wald(b,1,1000)
+            d3=ks_2samp(d1, d2)
+            c1=np.random.normal(a,c,1000)
+            c2=np.random.normal(b,d,1000)
+            c3=ttest_ind(c1,c2)
+            y=([d3[0],d3[1],abs(c3[0]),c3[1]])
+            return y
+        nn=0
+        mm=teset(g,j,z4,z3)
+        while (mm[3]>0.05 and mm[0]>0.04 or nn<5):
+            mm=teset(g,j,z4,z3)
+            nn=nn+1
+        nnn=nn
+        if mm[3]<=0.09:
+            mmm=mm[3]
+        else:
+            mmm=0.35
+        if z4>97 and z4<=114:
+            return("No_emotion")
+        elif z4>114 and z4<=135:
+            return("Reading")
+        elif z4>135 and z4<=163:
+            return("Passionate")
+        elif z4>163 and z4<=197:
+            return("No_emotion")
+        elif z4>197 and z4<=226:
+            return("Reading")
+        elif z4>226 and z4<=245:
+            return("Passionate")
+        else:
+            return("Voice not recognized")
+    except:
+        return ("Try again the sound of the audio was not clear")
