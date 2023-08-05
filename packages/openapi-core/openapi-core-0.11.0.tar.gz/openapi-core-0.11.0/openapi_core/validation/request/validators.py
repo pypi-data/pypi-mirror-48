355b134d0d95c37824d06c1cc5cfffcbdd41fe19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:16 +0000

    Add package openapi-core/0.11.0/openapi-core-0.11.0.tar.gz

---
+++ packages/openapi-core/openapi-core-0.11.0.tar.gz/openapi-core-0.11.0/openapi_core/validation/request/validators.py
@@ -0,0 +1,94 @@
+"""OpenAPI core validation request validators module"""
+from six import iteritems
+
+from openapi_core.schema.exceptions import OpenAPIMappingError
+from openapi_core.schema.parameters.exceptions import MissingParameter
+from openapi_core.validation.request.models import (
+    RequestParameters, RequestValidationResult,
+)
+from openapi_core.validation.util import get_operation_pattern
+
+
+class RequestValidator(object):
+
+    def __init__(self, spec, custom_formatters=None):
+        self.spec = spec
+        self.custom_formatters = custom_formatters
+
+    def validate(self, request):
+        try:
+            server = self.spec.get_server(request.full_url_pattern)
+        # don't process if server errors
+        except OpenAPIMappingError as exc:
+            return RequestValidationResult([exc, ], None, None)
+
+        operation_pattern = get_operation_pattern(
+            server.default_url, request.full_url_pattern
+        )
+
+        try:
+            path = self.spec[operation_pattern]
+        # don't process if operation errors
+        except OpenAPIMappingError as exc:
+            return RequestValidationResult([exc, ], None, None)
+
+        path_params, path_params_errors = self._get_parameters(request, path)
+
+        try:
+            operation = self.spec.get_operation(
+                operation_pattern, request.method)
+        # don't process if operation errors
+        except OpenAPIMappingError as exc:
+            return RequestValidationResult([exc, ], None, None)
+
+        op_params, op_params_errors = self._get_parameters(request, operation)
+        body, body_errors = self._get_body(request, operation)
+
+        errors = path_params_errors + op_params_errors + body_errors
+        return RequestValidationResult(errors, body, path_params + op_params)
+
+    def _get_parameters(self, request, operation):
+        errors = []
+
+        parameters = RequestParameters()
+        for param_name, param in iteritems(operation.parameters):
+            try:
+                raw_value = param.get_value(request)
+            except MissingParameter:
+                continue
+            except OpenAPIMappingError as exc:
+                errors.append(exc)
+                continue
+
+            try:
+                value = param.unmarshal(raw_value, self.custom_formatters)
+            except OpenAPIMappingError as exc:
+                errors.append(exc)
+            else:
+                parameters[param.location.value][param_name] = value
+
+        return parameters, errors
+
+    def _get_body(self, request, operation):
+        errors = []
+
+        if operation.request_body is None:
+            return None, errors
+
+        body = None
+        try:
+            media_type = operation.request_body[request.mimetype]
+        except OpenAPIMappingError as exc:
+            errors.append(exc)
+        else:
+            try:
+                raw_body = operation.request_body.get_value(request)
+            except OpenAPIMappingError as exc:
+                errors.append(exc)
+            else:
+                try:
+                    body = media_type.unmarshal(raw_body, self.custom_formatters)
+                except OpenAPIMappingError as exc:
+                    errors.append(exc)
+
+        return body, errors
