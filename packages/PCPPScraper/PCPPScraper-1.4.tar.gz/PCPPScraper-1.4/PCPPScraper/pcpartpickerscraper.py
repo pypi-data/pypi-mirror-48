2b2c4bf033f1a002ad2a76affda584abd4311cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package PCPPScraper/1.4/PCPPScraper-1.4.tar.gz

---
+++ packages/PCPPScraper/PCPPScraper-1.4.tar.gz/PCPPScraper-1.4/PCPPScraper/pcpartpickerscraper.py
@@ -0,0 +1,60 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+class Component():
+    def __init__(self, componentType, name, link, price):
+        self.type = componentType
+        self.name = name
+        self.link = link
+        self.price = price
+
+def getComponents(URL : str):
+    page = get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    components = soup.find_all(attrs = {'class' : 'tr__product'})
+    componentList = list()
+    for component in components:
+        componentLink = component.find(attrs = {'class' : 'td__name'}).find('a')
+        if componentLink == None:
+            break
+        componentType = component.find(attrs = {'class' : 'td__component'}).find('a').get_text()
+        price = component.find(attrs = {'class' : 'td__price'}).find('a')
+        if price == None:
+            price = 'unknown'
+            if component.find(attrs = {'class' : 'td__price'}).get_text() != None:
+                price = component.find(attrs = {'class' : 'td__price'}).get_text()[5:]
+        else:
+            price = price.get_text()
+        name = componentLink.get_text()
+        link = 'https://pcpartpicker.com' + componentLink.get('href')
+        newComponent = Component(componentType, name, link, price)
+        componentList.append(newComponent)
+        
+
+    return componentList
+
+def searchComponents(resultCount, query):
+    query = query.replace('+', '%2B')
+    URL = 'https://pcpartpicker.com/search/?q=' + query
+    page = get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    components = soup.find_all(attrs = {'class' : 'search_results--contentWrapper'})
+    componentList = list()
+    if components == None:
+        return componentList
+    i = 0
+    
+    for component in components:
+        nameAndLink = component.find(attrs = {'class' : 'search_results--link'})
+        name = nameAndLink.find('a').get_text()
+        link = 'https://pcpartpicker.com' + nameAndLink.find('a').get('href')
+        price = component.find(attrs = {'class' : 'search_results--price'}).find('a').get_text()
+        if price == None:
+            price = 'unknown'
+        newComponent = Component('unknown', name, link, price)
+        componentList.append(newComponent)
+        i+=1
+        if i >= resultCount:
+            break
+
+    return componentList
---
+++ packages/PCPPScraper/PCPPScraper-1.4.tar.gz/PCPPScraper-1.4/README.md
@@ -0,0 +1,44 @@
+# PCPPScraper
+A simple scraper to get some information from PCPartPicker, using requests and BeautifulSoup4
+
+### Github Link:
+https://github.com/A1phyte/PCPPScraper
+
+### PyPI Link: 
+https://pypi.org/project/PCPPScraper/
+
+## To Do:
+- ~~Make a Component class with type of component, name, price, and PCPP Link and return it in getComponents~~
+- ~~Fetch Custom Prices~~
+- ~~Make pip installable?~~
+- ~~Implement Search Method for part searching~~
+- Get component type in searchComponents
+- Suggest more in Issues
+
+## Documentation:
+
+### Component
+
+`self.type` = Type of component (CPU, GPU, etc.)
+
+`self.name` = Name of component (2200G, 2080 Ti, etc.)
+
+`self.link` = Link of component (https://pcpartpicker.com/product/RkJtt6/amd-ryzen-3-2200g-35ghz-quad-core-processor-yd2200c5fbbox)
+
+`self.price` = Price of component. Price is 'unknown' if there is no price or there is a custom price.
+
+### getComponents
+
+`getComponents(URL : str)`
+
+Gets components from PCPartPicker List URL
+
+Returns a list of Component objects.
+
+### searchComponents
+
+`searchComponents(resultCount, query)`
+
+Searches for components in PCPartPicker product search
+
+Returns a list of Component objects 
