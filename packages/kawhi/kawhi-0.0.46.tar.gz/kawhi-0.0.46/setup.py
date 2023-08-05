5e5f479be156148643245b859dce1ac5d8e1f96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package kawhi/0.0.46/kawhi-0.0.46.tar.gz

---
+++ packages/kawhi/kawhi-0.0.46.tar.gz/kawhi-0.0.46/setup.py
@@ -0,0 +1,26 @@
+"""
+setup module
+"""
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="kawhi",
+    version="0.0.46",
+    description="A package inspired by Kawhi Leonard of the Toronto Raptors",
+    packages=["kawhi", "kawhi.support_modules"],
+    # py_modules=["kawhi", "support_modules.get_jokes"],
+    # package_dir={"": "kawhi"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
