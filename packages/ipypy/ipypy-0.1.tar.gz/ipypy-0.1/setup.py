421a717c29925aed90e7c5b22c10430a3f291192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package ipypy/0.1/ipypy-0.1.tar.gz

---
+++ packages/ipypy/ipypy-0.1.tar.gz/ipypy-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipypy',
+    version='0.1',
+    url='',
+    description='ipypy notebook format',
+    packages=find_packages(),
+    install_requires=[],
+)
