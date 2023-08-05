d69359aac560a77da436dc4115639435dffd032f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package ck/1.9.9/ck-1.9.9.tar.gz

---
+++ packages/ck/ck-1.9.9.tar.gz/ck-1.9.9/bin/ck.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem Set default path by detecting the path to this script
+set ck_path=%~dp0\..
+
+rem Check if CK_ROOT is defined and used it, otherwise use auto-detected path
+IF "%CK_ROOT%"=="" set CK_ROOT=%ck_path%
+
+rem Load kernel module (either GIT/local installation or as package)
+IF EXIST %CK_ROOT%\ck\kernel.py (
+ python -W ignore::DeprecationWarning %CK_ROOT%\ck\kernel.py %*
+) ELSE (
+ python -W ignore::DeprecationWarning -W ignore::RuntimeWarning -m ck.kernel %*
+)
