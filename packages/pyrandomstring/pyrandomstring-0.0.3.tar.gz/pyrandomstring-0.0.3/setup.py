4307c52f65f9eb7472165586fb40ecfcd48d9cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:22 +0000

    Add package pyrandomstring/0.0.3/pyrandomstring-0.0.3.tar.gz

---
+++ packages/pyrandomstring/pyrandomstring-0.0.3.tar.gz/pyrandomstring-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pyrandomstring",
+    version = "0.0.3",
+    author = "Lakhya Jyoti Nath (ljnath)",
+    author_email = "ljnath@ljnath.com",
+    description = "PyRandomString is a python library to generate N random list of string of M length. Ofcourse you can configure N and M",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ljnath/PyRandomString",
+    packages = setuptools.find_packages(),
+    license='MIT',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
