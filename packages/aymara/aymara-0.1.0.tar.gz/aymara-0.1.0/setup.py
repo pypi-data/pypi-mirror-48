a42a2344a04205c2906630263b3ee46f3622888b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:46 +0000

    Add package aymara/0.1.0/aymara-0.1.0.tar.gz

---
+++ packages/aymara/aymara-0.1.0.tar.gz/aymara-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['aymara']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyconll>=2.0,<3.0', 'requests>=2.22,<3.0']
+
+setup_kwargs = {
+    'name': 'aymara',
+    'version': '0.1.0',
+    'description': 'Python bindings to the LIMA linguistic analyzer',
+    'long_description': None,
+    'author': 'Gael de Chalendar',
+    'author_email': 'gael.de-chalendar@cea.fr',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
