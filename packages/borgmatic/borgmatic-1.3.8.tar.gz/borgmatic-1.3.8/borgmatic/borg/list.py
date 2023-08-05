131ab4937c5b2495c854ce5940165083dc597d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package borgmatic/1.3.8/borgmatic-1.3.8.tar.gz

---
+++ packages/borgmatic/borgmatic-1.3.8.tar.gz/borgmatic-1.3.8/borgmatic/borg/list.py
@@ -0,0 +1,27 @@
+import logging
+
+from borgmatic.execute import execute_command
+
+logger = logging.getLogger(__name__)
+
+
+def list_archives(
+    repository, storage_config, archive=None, local_path='borg', remote_path=None, json=False
+):
+    '''
+    Given a local or remote repository path and a storage config dict, display the output of listing
+    Borg archives in the repository or return JSON output. Or, if an archive name is given, listing
+    the files in that archive.
+    '''
+    lock_wait = storage_config.get('lock_wait', None)
+
+    full_command = (
+        (local_path, 'list', '::'.join((repository, archive)) if archive else repository)
+        + (('--remote-path', remote_path) if remote_path else ())
+        + (('--lock-wait', str(lock_wait)) if lock_wait else ())
+        + (('--info',) if logger.getEffectiveLevel() == logging.INFO and not json else ())
+        + (('--debug', '--show-rc') if logger.isEnabledFor(logging.DEBUG) and not json else ())
+        + (('--json',) if json else ())
+    )
+
+    return execute_command(full_command, output_log_level=None if json else logging.WARNING)
