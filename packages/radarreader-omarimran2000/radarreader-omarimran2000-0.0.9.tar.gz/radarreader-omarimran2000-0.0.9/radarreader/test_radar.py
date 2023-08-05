2fb72fedf236916b12d9bd815c3155c7eb45c939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:32 +0000

    Add package radarreader-omarimran2000/0.0.9/radarreader-omarimran2000-0.0.9.tar.gz

---
+++ packages/radarreader-omarimran2000/radarreader-omarimran2000-0.0.9.tar.gz/radarreader-omarimran2000-0.0.9/radarreader/test_radar.py
@@ -0,0 +1,19 @@
+import unittest
+import raderreader.radar
+
+class testradar(unittest.TestCase):
+    
+    def test_dat(self):
+        
+        file=radar.dat_reader("xethru_datafloat_20190614_141837.dat",180,"converted")
+        self.assertEqual(file,1)
+    
+    def test_raw(self):
+        
+        file2=radar.raw_reader("xethru_datafloat_20190614_141837.dat","converted_2")
+        self.assertEqual(file2,1)
+        
+
+if __name__=='__main__':
+    
+    unittest.main()
---
+++ packages/radarreader-omarimran2000/radarreader-omarimran2000-0.0.9.tar.gz/radarreader-omarimran2000-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="radarreader-omarimran2000",
+    version="0.0.9",
+    author="Omar Imran",
+    author_email="omarimran@cmail.carleton.ca",
+    description="Convert .dat file into a CSV file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/omarimran2000/radarreader",
+    packages=['radarreader'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    data_files = [('xethru_datafloat_20190614_141837.dat', ['radarreader/xethru_datafloat_20190614_141837.dat'])],
+ 
+)
