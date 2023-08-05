629c45c0cb774473a6e9629572d5b8880e58d849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:50 +0000

    Add package scrooge-cache/0.1.2/scrooge_cache-0.1.2.tar.gz

---
+++ packages/scrooge-cache/scrooge_cache-0.1.2.tar.gz/scrooge_cache-0.1.2/scrooge/exception.py
@@ -0,0 +1,12 @@
+class ScroogeException(Exception):
+    """
+    This exception is used only in base classes
+    """
+    pass
+
+
+class ScroogeClientException(Exception):
+    """
+    This exceptions is used only in Client module
+    """
+    pass
