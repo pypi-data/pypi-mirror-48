77293be0a1bc7b6ccf4ec5870621c7bf802fecbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:01 +0000

    Add package django-ok-seo/0.7.2/django-ok-seo-0.7.2.tar.gz

---
+++ packages/django-ok-seo/django-ok-seo-0.7.2.tar.gz/django-ok-seo-0.7.2/seo/admin/inline.py
@@ -0,0 +1,17 @@
+from .mixins import AdminRichTextFieldMixin
+from .utils import get_admin_inline_base_class
+from ..models.instance_based import ModelInstanceSeo
+
+__all__ = (
+    'ModelInstanceSeoInline',
+)
+
+
+class ModelInstanceSeoInline(
+    AdminRichTextFieldMixin,
+    get_admin_inline_base_class()
+):
+    classes = ['collapse']
+    extra = 1
+    model = ModelInstanceSeo
+    max_num = 1
