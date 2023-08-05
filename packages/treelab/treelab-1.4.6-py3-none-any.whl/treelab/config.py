e79b46b841914d98c65ab3f482aeb707e0d237dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:17:03 +0000

    Add package treelab/1.4.6/treelab-1.4.6-py3-none-any.whl

---
+++ packages/treelab/treelab-1.4.6-py3-none-any.whl/treelab/config.py
@@ -0,0 +1,37 @@
+# # local
+# workspace_id = 'wspa05f5415628b8ede'
+# # core 1
+# core_id_1 = 'crea05da4426080f029'
+# view_id_1 = 'viwb5aa1318b086d7ba'
+# table_id_1 = 'tblb5aa1316c9073c53'
+# column_id_1 = 'colb5a9d42f5f8344e3'
+# column_id_2 = 'colb5a9d4304e8516ba'
+# column_name_1 = 'text'
+# column_name_2 = 'datetime'
+# view_name_1 = 'Default View'
+# row_id_1 = 'rowb5aa133b46844d91'
+# core_name_1 = '1'
+# view_id_2 = 'viwb5aa131a9a8299fe'
+# table_id_2 = 'tblb5aa1319a68f9c11'
+
+# local
+workspace_id = 'wspb5b4e84ec98a0088'
+# core 1
+core_id_1 = 'creb5b4e84f728e2b3b'
+view_id_1 = 'viwb5b4e85210059dd7'
+table_id_1 = 'tblb5b4e8504c88702d'
+column_id_1 = 'colb5a9d42f5f8344e3'
+column_id_2 = 'colb5a9d4304e8516ba'
+column_name_1 = 'text'
+column_name_2 = 'datetime'
+view_name_1 = 'Default View'
+row_id_1 = 'rowb5a9d428de0d0f17'
+core_name_1 = '1'
+view_id_2 = 'viwb5b4e853d20c2a0d'
+table_id_2 = 'tblb5b4e852f208b496'
+
+
+sleep_number = 5
+sleep_time = 0.1
+create_sleep_time = 0.3
+update_sleep_time = 0.3
