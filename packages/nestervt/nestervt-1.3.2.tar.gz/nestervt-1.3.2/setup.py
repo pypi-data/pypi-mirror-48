d0165b7e9a6658796a64356aeecb90d691173c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:09 +0000

    Add package nestervt/1.3.2/nestervt-1.3.2.tar.gz

---
+++ packages/nestervt/nestervt-1.3.2.tar.gz/nestervt-1.3.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name         = 'nestervt',
+	version      = '1.3.2',
+	py_modules   = ['nester'],
+	author       = 'Vlad Tofan',
+	author_email = 'vlad.tofan8@gmail.com',
+	url          = 'http://www.headfirstlabs.com',
+	description  = 'A simple printer of nested lists',
+	)
