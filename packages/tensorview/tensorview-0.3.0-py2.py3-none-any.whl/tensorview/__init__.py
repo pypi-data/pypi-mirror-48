daa5f405bec691c73e55dac10f71a5126fd599c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:41 +0000

    Add package tensorview/0.3.0/tensorview-0.3.0-py2.py3-none-any.whl

---
+++ packages/tensorview/tensorview-0.3.0-py2.py3-none-any.whl/tensorview/__init__.py
@@ -0,0 +1,18 @@
+from tensorview import train
+from tensorview import model
+
+__version__ = '0.2.0'
+__author__ = 'JinQing Lee'
+
+
+def _hello():
+    print("""
+------------------------------------------------------------------------------------
+      Linora
+--------------------
+      Version      : --  {}  --
+      Author       : --  {}  --
+      License      : Apache-2.0
+      Homepage     : https://github.com/Hourout/linora
+      Description  : Simple and efficient tools for data mining and data analysis.
+------------------------------------------------------------------------------------""".format(__version__, __author__))
---
+++ packages/tensorview/tensorview-0.3.0-py2.py3-none-any.whl/tensorview/train/__init__.py
@@ -0,0 +1,5 @@
+from tensorview.train._general_plot import *
+from tensorview.train._keras_callbacks_PlotMetricsOnBatch import *
+from tensorview.train._keras_callbacks_PlotMetricsOnEpoch import *
+
+__beefly_version__ = '0.8.0'
