e16ec4bc832a6b498d9b622f12cd0f1c41319f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:07 +0000

    Add package xbstrap/0.8/xbstrap-0.8.tar.gz

---
+++ packages/xbstrap/xbstrap-0.8.tar.gz/xbstrap-0.8/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(name='xbstrap',
+	version='0.8',
+	scripts=['scripts/xbstrap'],
+	install_requires=[
+		'colorama',
+		'pyyaml'
+	],
+
+	# Package metadata.
+	author='Alexander van der Grinten',
+	author_email='alexander.vandergrinten@gmail.com',
+	license='MIT',
+	url='https://github.com/managarm/xbstrap'
+)
+
