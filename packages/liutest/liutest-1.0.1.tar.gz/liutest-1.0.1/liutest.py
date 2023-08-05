19ea72a90dd5fd2505b5a3f8e01de44c3f1065aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:47 +0000

    Add package liutest/1.0.1/liutest-1.0.1.tar.gz

---
+++ packages/liutest/liutest-1.0.1.tar.gz/liutest-1.0.1/liutest.py
@@ -0,0 +1,17 @@
+'''此模块包含了学习《Head First Python》所写的示例代码
+   可能一些函数可以复用
+                               laoliu'''
+def print_lol(the_list, level):
+    '''使用递归函数实现复杂列表数据打印的函数'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            if level>=0:
+                print_lol(each_item, level+1)
+            else:
+                print_lol(each_item, level)
+        else:
+            if level<0:
+                tabnum = 0
+            else:
+                tabnum = level
+            print("\t"*tabnum,each_item)
---
+++ packages/liutest/liutest-1.0.1.tar.gz/liutest-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'liutest',
+    version     = '1.0.1',
+    py_modules  = ['liutest'],
+    author      = 'blazeliu',
+    author_email= 'spiritliu@126.com',
+    url         = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists.',
+    )
