bcb65f09ad073548efd577fbd9e844c3bdc9f44b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package wanakana-python/0.3.2/wanakana-python-0.3.2.tar.gz

---
+++ packages/wanakana-python/wanakana-python-0.3.2.tar.gz/wanakana-python-0.3.2/wanakana/common.py
@@ -0,0 +1,43 @@
+from typing import Iterable
+from .utils import (
+    is_empty,
+    is_char_hiragana,
+    is_char_katakana,
+    is_char_japanese,
+    is_char_kana,
+    is_char_kanji,
+    is_char_romaji,
+)
+
+
+def is_hiragana(input: str = "") -> bool:
+    """Tests if `input` is Hiragana"""
+    return (not is_empty(input)) and all(is_char_hiragana(char) for char in input)
+
+
+def is_katakana(input: str = "") -> bool:
+    """Tests if `input` is Katakana"""
+    return (not is_empty(input)) and all(is_char_katakana(char) for char in input)
+
+
+def is_japanese(input: str = "", augmented: Iterable = None) -> bool:
+    """Tests if `input` includes only Kanji, Kana, zenkaku numbers, and
+    JA punctuation/symbols"""
+    return (not is_empty(input)) and all(
+        is_char_japanese(char) or (augmented and char in augmented) for char in input
+    )
+
+
+def is_kana(input: str = "") -> bool:
+    """Tests if `input` is Kana (Katakana and/or Hiragana)"""
+    return (not is_empty(input)) and all(is_char_kana(char) for char in input)
+
+
+def is_kanji(input: str = "") -> bool:
+    """Tests if `input` is Kanji (Japanese CJK Ideographs)"""
+    return (not is_empty(input)) and all(is_char_kanji(char) for char in input)
+
+
+def is_mixed(input: str = "", ignore_kanji: bool = True):
+    """Tests if `input` contains a mix of Romaji *and* Kana, ignoring Kanji by default"""
+    return any(is_char_kana(char) for char in input) and any(is_char_romaji)
