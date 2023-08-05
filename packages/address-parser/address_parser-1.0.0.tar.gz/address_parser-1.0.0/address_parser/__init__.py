4822f50638c2c1571ff5738bc753c1c839f7b8db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:13 +0000

    Add package address-parser/1.0.0/address_parser-1.0.0.tar.gz

---
+++ packages/address-parser/address_parser-1.0.0.tar.gz/address_parser-1.0.0/address_parser/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Civic Knowledge. This file is licensed under the terms of the
+# Revised BSD License, included in this distribution as LICENSE
+
+"""
+A Basic parser for US postal addresses.
+"""
+
+from .parser import *
---
+++ packages/address-parser/address_parser-1.0.0.tar.gz/address_parser-1.0.0/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as f:
+    readme = f.read()
+
+packages = [
+    'address_parser',
+]
+
+package_data = {"": ['support/*.csv']}
+
+
+requires = [
+]
+
+classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='address_parser',
+    version='1.0.0',
+    description='Address parser',
+    long_description=readme,
+    packages=packages,
+    package_data=package_data,
+    install_requires=requires,
+    author='Eric Busboom',
+    author_email='eric@sandiegodata.org',
+    url='https://github.com/CivicKnowledge/address_parser',
+    license='MIT',
+    classifiers=classifiers,
+)
