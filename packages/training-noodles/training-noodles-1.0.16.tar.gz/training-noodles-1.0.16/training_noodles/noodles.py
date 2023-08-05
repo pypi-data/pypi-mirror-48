e2c8fde1d25a5fb5556ad67d0c630e02abc4e651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package training-noodles/1.0.16/training-noodles-1.0.16.tar.gz

---
+++ packages/training-noodles/training-noodles-1.0.16.tar.gz/training-noodles-1.0.16/training_noodles/noodles.py
@@ -0,0 +1,34 @@
+import sys
+
+from training_noodles.arguments import parse_args
+from training_noodles.logging_utils import init_logging
+from training_noodles.runner import Runner
+from training_noodles.spec import read_user_spec
+
+
+def main():
+    # Parse the arguments
+    args = parse_args()
+
+    # Initialize logging
+    init_logging(args)
+
+    # Read the spec
+    user_spec = read_user_spec(args)
+
+    # Build the runner
+    runner = Runner(args.type, user_spec, verbose=args.verbose)
+
+    # Start the runner
+    runner.run()
+
+    # Return the success code
+    return 0
+
+
+if __name__ == '__main__':
+    # Run the main method
+    status = main()
+
+    # Exit with the status
+    sys.exit(status)
