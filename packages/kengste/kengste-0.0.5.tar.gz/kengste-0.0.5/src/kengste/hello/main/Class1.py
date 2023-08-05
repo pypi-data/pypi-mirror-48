c1705879b2a5378120082115b9f41a3a2ae59827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:40:12 +0000

    Add package kengste/0.0.5/kengste-0.0.5.tar.gz

---
+++ packages/kengste/kengste-0.0.5.tar.gz/kengste-0.0.5/src/kengste/hello/main/Class1.py
@@ -0,0 +1,16 @@
+class Class1:
+
+  def show(self):
+    multi_line_string = """This is the first line.
+    Class 1.
+    and this is the second line
+    and this is the third line"""
+    print(multi_line_string)
+
+
+def showMain():
+    multi_line_string = """This is the first line.
+    Class 1.
+    and this is the second line
+    and this is the third line"""
+    print(multi_line_string)
