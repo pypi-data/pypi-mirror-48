07044a0114f9238449c5d7a20d111d1c0520b58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:43 +0000

    Add package fdn-kg-loader/0.2.0/fdn-kg-loader-0.2.0.tar.gz

---
+++ packages/fdn-kg-loader/fdn-kg-loader-0.2.0.tar.gz/fdn-kg-loader-0.2.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='fdn-kg-loader',
+    version='0.2.0',
+    packages=setuptools.find_packages(),
+    license='Restricted License, FDN license',
+    long_description=open('README.txt').read(),
+    entry_points={
+        'console_scripts': ['fdnloader=fdnloader:main']
+    },
+    install_requires=[
+        'grpcio==1.16.0',
+        'protobuf==3.6.1',
+        'grakn',
+        'grakn-client',
+        'click',
+        'six==1.11.0'
+    ]
+)
