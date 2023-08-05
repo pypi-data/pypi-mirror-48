039830ba4f89d5b06a4c6bf93a93cf7908e2ceaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:08 +0000

    Add package mkTranslation/1.2.3/mkTranslation-1.2.3.tar.gz

---
+++ packages/mkTranslation/mkTranslation-1.2.3.tar.gz/mkTranslation-1.2.3/mkTranslation/__init__.py
@@ -0,0 +1,7 @@
+"""Command line tools translated using Google、youdao and i18ns.com"""
+__all__ = 'mkTranslator',
+__version__ = '1.2.3'
+
+
+from mkTranslation.translator  import mkTranslator
+from mkTranslation.constants import LANGCODES, LANGUAGES
