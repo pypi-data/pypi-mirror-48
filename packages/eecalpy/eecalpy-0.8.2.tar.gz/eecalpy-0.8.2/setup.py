652c0d691d9a1d5a25eea3f649f77d267bac5976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:49 +0000

    Add package eecalpy/0.8.2/eecalpy-0.8.2.tar.gz

---
+++ packages/eecalpy/eecalpy-0.8.2.tar.gz/eecalpy-0.8.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import codecs
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='eecalpy',
+      version='0.8.2',
+      description='collection of classes for simple to complex electrical calculations',
+      long_description=long_description,
+      url='http://github.com/wese3112/eecalpy.git',
+      author='Sebastian Werner',
+      author_email='wese3112@startmail.com',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: OS Independent",
+      ],
+)
