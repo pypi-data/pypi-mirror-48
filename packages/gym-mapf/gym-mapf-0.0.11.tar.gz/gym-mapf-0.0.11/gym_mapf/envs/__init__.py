061268c40fd01df88fa8da396eba695d90d3ea6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:09 +0000

    Add package gym-mapf/0.0.11/gym-mapf-0.0.11.tar.gz

---
+++ packages/gym-mapf/gym-mapf-0.0.11.tar.gz/gym-mapf-0.0.11/gym_mapf/envs/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+MAPS_PATH = os.path.abspath(os.path.join(__file__, '../../maps/'))
+
+
+def map_name_to_files(map_name, scen_id):
+    map_file_path = os.path.join(MAPS_PATH, map_name, '{}.map'.format(map_name))
+    scen_file_path = os.path.join(MAPS_PATH, map_name, '{}-even-{}.scen'.format(map_name, scen_id))
+
+    return map_file_path, scen_file_path
+
+
+UP = 'UP'
+RIGHT = 'RIGHT'
+DOWN = 'DOWN'
+LEFT = 'LEFT'
+STAY = 'STAY'
+POSSIBILITIES = {
+    UP: (RIGHT, LEFT),
+    DOWN: (LEFT, RIGHT),
+    LEFT: (UP, DOWN),
+    RIGHT: (DOWN, UP),
+    STAY: (STAY, STAY)
+}
+ACTIONS = [UP, RIGHT, DOWN, LEFT, STAY]
