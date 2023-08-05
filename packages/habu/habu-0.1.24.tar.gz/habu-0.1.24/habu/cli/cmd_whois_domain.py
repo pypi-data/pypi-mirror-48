b4af0387257d9b81728f953cba474f6bc3fbdb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:49:47 +0000

    Add package habu/0.1.24/habu-0.1.24.tar.gz

---
+++ packages/habu/habu-0.1.24.tar.gz/habu-0.1.24/habu/cli/cmd_whois_domain.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import json
+import warnings
+
+import click
+
+# next lines because the pypi package python-whois uses the namespace 'whois' and
+# the Debian python3-pywhois uses the namespace 'pywhois' at least on 2018-11-03
+try:
+    import pywhois as whois
+except ModuleNotFoundError:
+    import whois
+
+
+def remove_duplicates(data):
+    for key in data.keys():
+        if isinstance(data[key], list):
+            if hasattr(data[key][0], 'lower'):
+                data[key] = list(set([ item.lower() for item in data[key] ]))
+            else:
+                data[key] = data[key][0]
+
+    if isinstance(data['domain_name'], list):
+        data['domain_name'] = data['domain_name'][0]
+
+    return data
+
+
+@click.command()
+@click.argument('domain')
+def cmd_whois_domain(domain):
+    """Simple whois client to check domain names.
+
+    Example:
+
+    \b
+    $ habu.whois.domain portantier.com
+    {
+        "domain_name": "portantier.com",
+        "registrar": "Amazon Registrar, Inc.",
+        "whois_server": "whois.registrar.amazon.com",
+        ...
+    """
+
+    warnings.filterwarnings("ignore")
+
+    data = whois.whois(domain)
+    data = remove_duplicates(data)
+
+    print(json.dumps(data, indent=4, default=str))
+
+if __name__ == '__main__':
+    cmd_whois_domain()
---
+++ packages/habu/habu-0.1.24.tar.gz/habu-0.1.24/habu/lib/shodan.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import logging
+import os
+import os.path
+
+import requests
+import requests_cache
+
+from pathlib import Path
+
+def shodan_get_result(ip, api_key, no_cache=False, verbose=False):
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    if not no_cache:
+        homedir = Path(os.path.expanduser('~'))
+        requests_cache.install_cache(str(homedir / '.habu_requests_cache'), expire_after=3600)
+
+    url = 'https://api.shodan.io/shodan/host/{}?key={}'.format(ip, api_key)
+
+    r = requests.get(url)
+
+    if r.status_code not in [200, 404]:
+        logging.error(str(r))
+        return {}
+
+    if r.status_code == 404:
+        return {}
+
+    data = r.json()
+
+    return data
+
+
+def shodan_query(query, api_key, no_cache=False, verbose=False):
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    if not no_cache:
+        homedir = Path(os.path.expanduser('~'))
+        requests_cache.install_cache(str(homedir / '.habu_requests_cache'), expire_after=3600)
+
+    url = 'https://api.shodan.io/shodan/host/search?key={}&query={}'.format(api_key, query)
+    print(url)
+
+    r = requests.get(url)
+
+    if r.status_code not in [200, 404]:
+        logging.error(str(r))
+        return {}
+
+    if r.status_code == 404:
+        return {}
+
+    data = r.json()
+
+    return data
