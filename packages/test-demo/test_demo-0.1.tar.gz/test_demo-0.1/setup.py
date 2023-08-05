5b87d3fb264a367338981bc7ef201d37a09f99f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:15:47 +0000

    Add package test-demo/0.1/test_demo-0.1.tar.gz

---
+++ packages/test-demo/test_demo-0.1.tar.gz/test_demo-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='test_demo',
+      version='0.1',
+      author='haoqihan',
+      author_email='2263310007@qq.com',
+      license='MIT',
+      url='http://haoqihan.github.io'
+)
