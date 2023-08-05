810aece00587f0c6eb7ca623c340f4a8fc1e8c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package dataprocessor-client/0.0.2/dataprocessor_client-0.0.2.tar.gz

---
+++ packages/dataprocessor-client/dataprocessor_client-0.0.2.tar.gz/dataprocessor_client-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dataprocessor_client',
+      version='0.0.2',
+      description='InterSCity DataProcessor client',
+      url='http://github.com/DylanGuedes/dataprocessor_client',
+      author='Dylan Guedes',
+      author_email='djmgguedes@gmail.com',
+      license='MIT',
+      packages=['dataprocessor_client'],
+      zip_safe=False)
