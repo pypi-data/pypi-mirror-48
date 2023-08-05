466a7a015afece0c74a0886e6c48b09581bfb8f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:23 +0000

    Add package toad/0.0.51/toad-0.0.51.tar.gz

---
+++ packages/toad/toad-0.0.51.tar.gz/toad-0.0.51/toad/__init__.py
@@ -0,0 +1,8 @@
+from .merge import merge, DTMerge, ChiMerge, StepMerge, QuantileMerge, KMeansMerge
+from .detector import detect
+from .stats import quality, IV, VIF, WOE, KS, KS_bucket, entropy, entropy_cond, gini, gini_cond
+from .selection import select
+from .scorecard import ScoreCard
+from .version import __version__
+
+VERSION = __version__
