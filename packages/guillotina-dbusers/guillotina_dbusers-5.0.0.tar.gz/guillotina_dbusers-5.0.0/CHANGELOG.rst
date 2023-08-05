a001dd16bef16fc818d16d3bfd257df2d4741188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:07 +0000

    Add package guillotina-dbusers/5.0.0/guillotina_dbusers-5.0.0.tar.gz

---
+++ packages/guillotina-dbusers/guillotina_dbusers-5.0.0.tar.gz/guillotina_dbusers-5.0.0/CHANGELOG.rst
@@ -0,0 +1,64 @@
+5.0.0 (2019-06-21)
+------------------
+
+- Guillotina > 5 only
+
+- Update API to be compat with plone api
+  [jordic]
+
+
+2.0.5 (2019-06-18)
+------------------
+
+- bump
+
+
+2.0.4 (2019-06-18)
+------------------
+
+- restrict g version
+  [vangheem]
+
+
+2.0.3 (2019-04-26)
+------------------
+
+- perms
+
+
+2.0.2 (2018-09-25)
+------------------
+
+- Remove login endpoint because guillotina already offers it
+  [bloodbare]
+
+
+2.0.1 (2018-07-19)
+------------------
+
+- Support both `username` and `login` as parameter for the `@login` endpoint
+  [ebrehault]
+
+
+2.0.0 (2018-07-19)
+------------------
+
+- Work with guillotina 4
+  [vangheem]
+
+
+1.0.1 (2017-10-08)
+------------------
+
+- Fix issues with user definition on latest guillotina
+  [vangheem]
+
+- Automatically configure
+  [vangheem]
+
+
+1.0.0 (2017-04-11)
+------------------
+
+- initial move to guillotina
+  [vangheem]
---
+++ packages/guillotina-dbusers/guillotina_dbusers-5.0.0.tar.gz/guillotina_dbusers-5.0.0/guillotina_dbusers/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from guillotina import configure
+from guillotina.i18n import MessageFactory
+
+_ = MessageFactory('guillotina_dbusers')
+
+
+app_settings = {
+    "auth_user_identifiers": [
+        "guillotina_dbusers.users.DBUserIdentifier"
+    ]
+}
+
+configure.permission("guillotina.NotAuthenticated", "")
+configure.permission("guillotina.Authenticated", "")
+configure.permission("guillotina.AddUser", title="Add plone user")
+configure.permission("guillotina.AddGroup", title="Add plone group")
+configure.grant(permission="guillotina.AccessContent",
+                role="guillotina.Anonymous")
+configure.grant(permission="guillotina.NotAuthenticated",
+                role="guillotina.Anonymous")
+configure.grant(permission="guillotina.Authenticated",
+                role="guillotina.Authenticated")
+configure.grant(permission="guillotina.AddUser",
+                role="guillotina.ContainerAdmin")
+configure.grant(permission="guillotina.AddGroup",
+                role="guillotina.ContainerAdmin")
+
+
+def includeme(root, settings):
+    configure.scan('guillotina_dbusers.content.users')
+    configure.scan('guillotina_dbusers.content.groups')
+    configure.scan('guillotina_dbusers.install')
+    configure.scan('guillotina_dbusers.services')
+    configure.scan('guillotina_dbusers.subscribers')
+    configure.scan('guillotina_dbusers.permissions')
+    configure.scan("guillotina_dbusers.serializers")
