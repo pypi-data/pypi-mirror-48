6a4f9db7708612e8b80278ee442ae008ed0729b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:02 +0000

    Add package jupyterlab/1.0.0a10/jupyterlab-1.0.0a10-py2.py3-none-any.whl

---
+++ packages/jupyterlab/jupyterlab-1.0.0a10-py2.py3-none-any.whl/jupyterlab-1.0.0a10.data/data/share/jupyter/lab/static/10.16abb2aab9321292a3d5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[10],{10:function(n,w){}}]);
+//# sourceMappingURL=10.16abb2aab9321292a3d5.js.map
---
+++ packages/jupyterlab/jupyterlab-1.0.0a10-py2.py3-none-any.whl/jupyterlab/static/styles.18f0f746a72dcda8f3bb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[2],{"9fgM":function(n,w,o){}}]);
+//# sourceMappingURL=styles.18f0f746a72dcda8f3bb.js.map
