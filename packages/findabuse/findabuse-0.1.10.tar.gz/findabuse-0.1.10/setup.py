3d4e961761b6de84e953c4b8b7db9a71f70f7ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package findabuse/0.1.10/findabuse-0.1.10.tar.gz

---
+++ packages/findabuse/findabuse-0.1.10.tar.gz/findabuse-0.1.10/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['findabuse']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bottle>=0.12.16,<0.13.0',
+ 'dnslib>=0.9.10,<0.10.0',
+ 'redis>=3.2,<4.0',
+ 'requests>=2.22,<3.0',
+ 'tornado>=6.0,<7.0']
+
+entry_points = \
+{'console_scripts': ['findabuse = findabuse.cli:main',
+                     'findabusedns = findabuse.dns:main',
+                     'findabusehttp = findabuse.http:main']}
+
+setup_kwargs = {
+    'name': 'findabuse',
+    'version': '0.1.10',
+    'description': 'findabuse.email is a simple API for looking up abuse contacts for network spaces',
+    'long_description': None,
+    'author': 'Matthew Gall',
+    'author_email': 'git@matthewgall.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
