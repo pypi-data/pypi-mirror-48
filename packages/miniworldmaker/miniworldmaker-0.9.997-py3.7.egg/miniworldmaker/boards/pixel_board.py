a9c7d3f6622d4c0cadec71eed3a908d1280839f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:24 +0000

    Add package miniworldmaker/0.9.997/miniworldmaker-0.9.997-py3.7.egg

---
+++ packages/miniworldmaker/miniworldmaker-0.9.997-py3.7.egg/miniworldmaker/boards/pixel_board.py
@@ -0,0 +1,46 @@
+from miniworldmaker.boards import board as bd
+from miniworldmaker.tokens import token as tk
+
+
+class PixelBoard(bd.Board):
+
+    def __init__(self, columns=40, rows=40):
+        super().__init__(columns=columns, rows=rows)
+        self.default_actor_speed = 3
+        self.speed = 1
+
+    def add_to_board(self, token: tk.Token, position) -> tk.Token:
+        super().add_to_board(token, position)
+        if token.size == (0, 0):
+            token.size = (30, 30)
+        return token
+
+    def remove_from_board(self, token: tk.Token):
+        super().remove_from_board(token)
+
+    def borders(self, rect):
+        borders = []
+        if rect.topleft[0] <= 0:
+            borders.append("left")
+        if rect.topleft[1] + rect.height >= self.height:
+            borders.append("bottom")
+        if rect.topleft[0] + rect.width >= self.width:
+            borders.append("right")
+        if rect.topleft[1] <= 0:
+            borders.append("top")
+        return borders
+
+    def get_touching_borders(self, rect) -> list:
+        borders = []
+        if rect.topleft[0] <= 0:
+            borders.append("left")
+        if rect.topleft[1] + rect.height >= self.height:
+            borders.append("bottom")
+        if rect.topleft[0] + rect.width >= self.width:
+            borders.append("right")
+        if rect.topleft[1] <= 0:
+            borders.append("top")
+        return borders
+
+    def update(self):
+        super().update()
