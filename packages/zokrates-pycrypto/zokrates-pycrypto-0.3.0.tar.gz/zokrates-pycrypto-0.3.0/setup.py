fb7239ba15bb3c3fab4e83db51b44cffd7eae12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:51 +0000

    Add package zokrates-pycrypto/0.3.0/zokrates-pycrypto-0.3.0.tar.gz

---
+++ packages/zokrates-pycrypto/zokrates-pycrypto-0.3.0.tar.gz/zokrates-pycrypto-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['zokrates_pycrypto', 'zokrates_pycrypto.gadgets']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bitstring>=3.1,<4.0']
+
+setup_kwargs = {
+    'name': 'zokrates-pycrypto',
+    'version': '0.3.0',
+    'description': 'Accompanying crypto application code for the zkSNARKs toolbox ZoKrates',
+    'long_description': None,
+    'author': 'sdeml',
+    'author_email': 'stefandeml@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
