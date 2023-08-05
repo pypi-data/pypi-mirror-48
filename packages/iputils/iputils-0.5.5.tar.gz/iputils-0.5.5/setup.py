e7c1288167ca4c9d06007b22d97c1c5d43ce04ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package iputils/0.5.5/iputils-0.5.5.tar.gz

---
+++ packages/iputils/iputils-0.5.5.tar.gz/iputils-0.5.5/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# date:        2019/6/6
+# author:      he.zhiming
+#
+
+from __future__ import (absolute_import, unicode_literals)
+
+from setuptools import setup
+import codecs
+
+setup(
+    name="iputils",
+    version="0.5.5",
+    platforms="any",
+    url="https://github.com/hezhiming/iputils",
+    license="MIT",
+    author="he.zhiming",
+    author_email="he.zhiming@foxmail.com",
+    description="wrapper of ipaddress.py, easy to use",
+    long_description=codecs.open("README.rst", encoding="utf-8", mode="r").read(),
+    keywords="IPv4, IPv6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
