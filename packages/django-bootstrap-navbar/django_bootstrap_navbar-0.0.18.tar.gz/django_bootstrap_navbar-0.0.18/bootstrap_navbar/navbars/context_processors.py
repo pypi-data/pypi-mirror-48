5dc38ec7af891ca6cf5d1cbf851f1b5a9591cfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:11 +0000

    Add package django-bootstrap-navbar/0.0.18/django_bootstrap_navbar-0.0.18.tar.gz

---
+++ packages/django-bootstrap-navbar/django_bootstrap_navbar-0.0.18.tar.gz/django_bootstrap_navbar-0.0.18/bootstrap_navbar/navbars/context_processors.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+
+import importlib
+
+
+def navbar(request):
+    """Add the navbar specified in the settings to the context."""
+
+    if settings.BOOTSTRAP_NAVBAR:
+        module_path, class_name = settings.BOOTSTRAP_NAVBAR.split(":")
+        navbar_module = importlib.import_module(module_path)
+
+        NavBar = getattr(navbar_module, class_name, None)
+        if NavBar is not None:
+            navbar = NavBar()
+            navbar.set_active_navitem(request)
+        else:
+            navbar = None
+
+        return {"navbar": navbar}
+
+    return {}
