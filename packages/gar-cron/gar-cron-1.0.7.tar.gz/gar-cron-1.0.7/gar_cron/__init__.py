c63f5c5b2e3426f6f3e4b69a674a4ecf58828b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:17:02 +0000

    Add package gar-cron/1.0.7/gar-cron-1.0.7.tar.gz

---
+++ packages/gar-cron/gar-cron-1.0.7.tar.gz/gar-cron-1.0.7/gar_cron/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Prahlad Yeri"
+__email__ = "prahladyeri@yahoo.com"
+__copyright__ = "(c) 2019 Prahlad Yeri"
+
+__license__ = "MIT"
+__version__ = "1.0.7"
+__title__ = "github activity reminder cron"
+__description__ = ''
