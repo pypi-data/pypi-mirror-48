eb9d6f3451272925d431544629430a4fd70fd1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:30 +0000

    Add package genomedashboard/0.0.25/genomedashboard-0.0.25.tar.gz

---
+++ packages/genomedashboard/genomedashboard-0.0.25.tar.gz/genomedashboard-0.0.25/genomedashboard/cli.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for genomedashboard."""
+
+import sys
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for genomedashboard."""
+    click.echo("Replace this message by putting your code into "
+               "genomedashboard.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+    return 0
+
+
+# if __name__ == "__main__":
+#     sys.exit(main())  # pragma: no cover
+
+
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+              help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    for x in range(count):
+        click.echo('Hello %s!' % name)
+
+
+if __name__ == '__main__':
+    hello()
