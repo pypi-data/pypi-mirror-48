72fbd69c4dc16d49ec4e0c894acd3a93d4bf3d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:02 +0000

    Add package mofe-distributions/0.1/mofe_distributions-0.1.tar.gz

---
+++ packages/mofe-distributions/mofe_distributions-0.1.tar.gz/mofe_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mofe_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mofe_distributions'],
+      zip_safe=False)
