774efa01db2b5f461aca9377d9c7588cc524b9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:02 +0000

    Add package ivis/1.1.4/ivis-1.1.4.tar.gz

---
+++ packages/ivis/ivis-1.1.4.tar.gz/ivis-1.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='ivis',
+      version='1.1.4',
+      description='Artificial neural network-driven visualization of high-dimensional data using triplets.',
+      url='http://github.com/beringresearch/ivis',
+      author='Benjamin Szubert, Ignat Drozdov',
+      author_email='bszubert@beringresearch.com, idrozdov@beringresearch.com',
+      license='GNU General Public License v2.0',
+      packages=find_packages(),
+      install_requires=[
+          'tensorflow',
+          'keras',
+          'numpy',
+          'scikit-learn',
+          'annoy>=1.15.2',
+          'tqdm'
+      ],
+      extras_require={
+          'tests': ['pytest']
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
