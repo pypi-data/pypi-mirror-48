a856e6b73ef8caa9179a0bc9634c3b68d36f9611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:53 +0000

    Add package hiddenAlphabet/0.0.4/hiddenAlphabet-0.0.4.tar.gz

---
+++ packages/hiddenAlphabet/hiddenAlphabet-0.0.4.tar.gz/hiddenAlphabet-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hiddenAlphabet",
+    version="0.0.4",
+    author="Mac Scheffer, Donaldo Celaj",
+    author_email="author@example.com",
+    description="Early testing.",
+    long_description='long description',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
