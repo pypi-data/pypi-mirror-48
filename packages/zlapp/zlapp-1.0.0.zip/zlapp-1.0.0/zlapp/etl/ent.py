beaa75071075dd5778a8c502bcdd1ec355e2c693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:08 +0000

    Add package zlapp/1.0.0/zlapp-1.0.0.zip

---
+++ packages/zlapp/zlapp-1.0.0.zip/zlapp-1.0.0/zlapp/etl/ent.py
@@ -0,0 +1,7 @@
+from lmf.dbv2 import db_query ,db_command ,db_write 
+from lmf.bigdata import pg2pg
+
+from zlapp.zz.qiyezz import df2df 
+##qyzz表来源于jianzhu.gg_html 
+
+
---
+++ packages/zlapp/zlapp-1.0.0.zip/zlapp-1.0.0/zlapp/etl/qyzz.py
@@ -0,0 +1,11 @@
+from lmf.dbv2 import db_query ,db_command ,db_write 
+from lmf.bigdata import pg2pg
+
+from zlapp.zz.qiyezz import df2df 
+
+
+# sql="select * from jianzhu.gg_html limit 1000"
+
+# conp=["postgres","since2015","192.168.4.188","bid","jianzhu"]
+
+# pg2pg(sql,'qyzz',conp,conp,f=df2df,chunksize=1000)
