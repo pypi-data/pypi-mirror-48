7704c8aa5cc1d9251c2b7f18791f7e5f87d7cde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:00 +0000

    Add package django-urls/1.0.8/django_urls-1.0.8.tar.gz

---
+++ packages/django-urls/django_urls-1.0.8.tar.gz/django_urls-1.0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='django_urls',
+    version='1.0.8',
+    packages=['django_urls'],
+    url='https://github.com/isik-kaplan/django_urls',
+    description="URL decorator for django views",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='APGL-3.0',
+    author='isik-kaplan',
+    author_email='',
+    python_requires=">=3.5",
+    install_requires=['django>=2.0']
+)
