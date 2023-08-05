53c4d42ad9bbb3af5b0e96823182ed90ba188009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:43 +0000

    Add package flaskeztest3/0.3.3/flaskeztest3-0.3.3.tar.gz

---
+++ packages/flaskeztest3/flaskeztest3-0.3.3.tar.gz/flaskeztest3-0.3.3/flaskeztest3/__init__.py
@@ -0,0 +1,19 @@
+"""The flaskeztest package"""
+
+import capybara
+
+# Passing imports along so user's can import commonly used objects from root package
+from .eztest import EZTest
+from .eztestcase import EZTestCase
+from .eztestsuite import EZTestSuite
+
+
+@capybara.register_driver("selenium")
+def init_selenium_driver(app):
+
+    from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+    capabilities = DesiredCapabilities.PHANTOMJS.copy()
+
+    from capybara.selenium.driver import Driver
+
+    return Driver(app, browser="phantomjs", desired_capabilities=capabilities)
