52c0bcf87649537cb185592222e8088ab5ed3d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:16:51 +0000

    Add package coding-test-foo/1.6.1/coding-test-foo-1.6.1.tar.gz

---
+++ packages/coding-test-foo/coding-test-foo-1.6.1.tar.gz/coding-test-foo-1.6.1/bar/speaker.py
@@ -0,0 +1,13 @@
+def hello_world():
+    print("hello world")
+
+
+def say(words):
+    print("you just said: " + words)
+
+
+def multiplication_table():
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            print("%d * %d = %2d" % (i, j, i * j), end="\t")
+        print("")
