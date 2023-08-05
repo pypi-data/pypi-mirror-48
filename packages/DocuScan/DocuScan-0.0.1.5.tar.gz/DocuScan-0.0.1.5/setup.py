2ba897d8d385e24fb16761f6d578765b57fa69a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:10 +0000

    Add package DocuScan/0.0.1.5/DocuScan-0.0.1.5.tar.gz

---
+++ packages/DocuScan/DocuScan-0.0.1.5.tar.gz/DocuScan-0.0.1.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DocuScan",
+    version="0.0.1.5",
+    author="Alex Scotland",
+    author_email="rocketleaguemarket@gmail.com",
+    description="A light-weight package that can open document files (docx, doc, pdf)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mutster/DocuScan-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
