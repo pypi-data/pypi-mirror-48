ed8ddcb09b1f6f20be1e5ee3ad0f49fe55a92d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:13:26 +0000

    Add package codePost-api/1.0.13/codePost-api-1.0.13.tar.gz

---
+++ packages/codePost-api/codePost-api-1.0.13.tar.gz/codePost-api-1.0.13/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# The text of the README file
+README = open("README.md").read()
+
+# This call to setup() does all the work
+setup(
+    name="codePost-api",
+    version="1.0.13",
+    description="Python bindings for the codePost API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codepost-io/codePost-api-python",
+    author="codePost",
+    author_email="team@codepost.io",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "PyYAML"
+    ],
+    include_package_data=True,
+)
