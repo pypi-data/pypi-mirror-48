693e80f066dd96411633b25e4651c4e86f421625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:11 +0000

    Add package django-chunk-upload/2.0.0/django-chunk-upload-2.0.0.tar.gz

---
+++ packages/django-chunk-upload/django-chunk-upload-2.0.0.tar.gz/django-chunk-upload-2.0.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('VERSION.txt', 'r') as v:
+    version = v.read().strip()
+
+with open('README.rst', 'r') as r:
+    readme = r.read()
+
+download_url = (
+    'https://github.com/jerinpetergeorge/django-chunk-upload/tarball/%s'
+)
+
+setup(
+    name='django-chunk-upload',
+    packages=['django_chunk_upload'],
+    version=version,
+    description=('Upload large files to Django in multiple chunks, with the '
+                 'ability to resume if the upload is interrupted.'),
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Jerin Peter George',
+    author_email='jerinpetergeorge@gmail.com',
+    url='https://github.com/jerinpetergeorge/django-chunk-upload',
+    download_url=download_url % version,
+    install_requires=[],
+    license='MIT-Zero'
+)
