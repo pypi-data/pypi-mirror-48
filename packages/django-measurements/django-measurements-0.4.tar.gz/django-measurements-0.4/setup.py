e741f6d7f9217e832164df486ee91c5e263b21ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package django-measurements/0.4/django-measurements-0.4.tar.gz

---
+++ packages/django-measurements/django-measurements-0.4.tar.gz/django-measurements-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-measurements',
+    version='0.4',
+    packages=['measurements'],
+    url='',
+    license='GPL3',
+    author='Stefano Menegon',
+    author_email='stefano.menegon@cnr.it',
+    description='', install_requires=['django', 'numpy', 'pandas', 'urllib3', 'requests', 'pycryptodome']
+)
