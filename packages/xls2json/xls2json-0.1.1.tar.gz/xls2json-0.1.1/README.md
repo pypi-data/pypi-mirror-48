f7fd9517037423af7007feb7f30358db38669c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:51 +0000

    Add package xls2json/0.1.1/xls2json-0.1.1.tar.gz

---
+++ packages/xls2json/xls2json-0.1.1.tar.gz/xls2json-0.1.1/README.md
@@ -0,0 +1,13 @@
+## xls2json
+
+This tool supports 3 modes:
+
+- **--perentry** Read XLS file and write a JSON file per entry of XLS file.
+- **--persheet** Read XLS file and write a JSON file per sheet of XLS file.
+- **default** Read XLS file and write to single JSON file.
+
+## TODO
+
+- auto detect cell type and write accordingly.
+- handle XLS file with no table headers.
+- handle non unique sheet names.
---
+++ packages/xls2json/xls2json-0.1.1.tar.gz/xls2json-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['xls2json']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['xlrd>=1.2,<2.0']
+
+entry_points = \
+{'console_scripts': ['xls2json = xls2json.command_line:main']}
+
+setup_kwargs = {
+    'name': 'xls2json',
+    'version': '0.1.1',
+    'description': 'Convert XLS file to JSON.',
+    'long_description': '## xls2json\n\nThis tool supports 3 modes:\n\n- **--perentry** Read XLS file and write a JSON file per entry of XLS file.\n- **--persheet** Read XLS file and write a JSON file per sheet of XLS file.\n- **default** Read XLS file and write to single JSON file.\n\n## TODO\n\n- auto detect cell type and write accordingly.\n- handle XLS file with no table headers.\n- handle non unique sheet names.\n',
+    'author': 'Reza Handzalah',
+    'author_email': 'rezahandzalah@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
