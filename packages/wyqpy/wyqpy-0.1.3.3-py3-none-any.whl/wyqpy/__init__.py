054cdc5bb60145bcafa2527e645d8bd8d1e6981e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:36 +0000

    Add package wyqpy/0.1.3.3/wyqpy-0.1.3.3-py3-none-any.whl

---
+++ packages/wyqpy/wyqpy-0.1.3.3-py3-none-any.whl/wyqpy/__init__.py
@@ -0,0 +1,14 @@
+"""
+Created on 2017年7月12日
+
+:author: WYQ
+"""
+
+packages = ['wyqpy', 'wyqpy.base', 'wyqpy.io', 'wyqpy.math', 'wyqpy.office', 'wyqpy.ui', 'wyqpy.net']
+
+from . import base
+from . import io
+from . import math
+from . import net
+from . import office
+from . import ui
---
+++ packages/wyqpy/wyqpy-0.1.3.3-py3-none-any.whl/wyqpy/io/__init__.py
@@ -0,0 +1,7 @@
+"""
+Created on 2017年7月12日
+
+:author: WYQ
+"""
+
+from . import file,directory
