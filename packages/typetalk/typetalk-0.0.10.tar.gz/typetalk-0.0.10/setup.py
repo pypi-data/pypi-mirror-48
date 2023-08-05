3d84408d65f3bc569036b1514aa0ae96894530fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package typetalk/0.0.10/typetalk-0.0.10.tar.gz

---
+++ packages/typetalk/typetalk-0.0.10.tar.gz/typetalk-0.0.10/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="typetalk",
+    version="0.0.10",
+    packages=['typetalk'],
+    description="Typetalk API client",
+    url="https://github.com/is2ei/typetalk-py",
+    license="MIT"
+)
