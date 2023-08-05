4d5fefe3c784608f40e47f76de1d198d5bcd0ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:21 +0000

    Add package pysubs2/0.2.4/pysubs2-0.2.4.tar.gz

---
+++ packages/pysubs2/pysubs2-0.2.4.tar.gz/pysubs2-0.2.4/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from textwrap import dedent
+from pysubs2 import VERSION
+
+setup(
+    name = "pysubs2",
+    packages = ["pysubs2"],
+    version = VERSION,
+    author = "Tomas Karabela",
+    author_email = "tkarabela@seznam.cz",
+    url = "https://github.com/tkarabela/pysubs2",
+    keywords = "SubStation SubRip MicroDVD ass srt sub subtitles",
+    description = "A library for editing subtitle files",
+    long_description = dedent(r"""\
+        pysubs2 is a Python library for editing subtitle files.
+        It’s based on *SubStation Alpha*, the native format of
+        `Aegisub <http://www.aegisub.org/>`_; it also supports *SubRip (SRT)*,
+        *MicroDVD*, *MPL2* and *TMP* formats. There is a small CLI tool for batch conversion
+        and retiming.
+
+        ::
+
+            import pysubs2
+            subs = pysubs2.load("my_subtitles.ass", encoding="utf-8")
+            subs.shift(s=2.5)
+            for line in subs:
+                line.text = "{\\be1}" + line.text
+            subs.save("my_subtitles_edited.ass")
+
+        """),
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Markup",
+        "Topic :: Multimedia :: Video",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+        ],
+    entry_points={'console_scripts': ['pysubs2 = pysubs2.cli:__main__']}
+    )
