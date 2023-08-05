50d8e628d9096d27447b58778bcf3e415196ab2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:24 +0000

    Add package index-generator/0.2/index-generator-0.2.tar.gz

---
+++ packages/index-generator/index-generator-0.2.tar.gz/index-generator-0.2/index_generator/models/exceptions.py
@@ -0,0 +1,9 @@
+class IndexGeneratorBaseException(BaseException):
+    def __init__(self, message, hint='Something went wrong.'):
+        super().__init__(message)
+        self.hint = hint
+
+
+class IndexGeneratorTemplateNotFound(IndexGeneratorBaseException):
+    def __init__(self, message):
+        super().__init__(message, hint='Template folder does not exists or template file is not found.')
