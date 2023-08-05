eadf8cfb0648c8fa11a9b3a9c2154a1e03498eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:48 +0000

    Add package aws-cdk.aws-kinesis/0.35.0/aws_cdk.aws_kinesis-0.35.0-py3-none-any.whl

---
+++ packages/aws-cdk.aws-kinesis/aws_cdk.aws_kinesis-0.35.0-py3-none-any.whl/aws_cdk/aws_kinesis/_jsii/__init__.py
@@ -0,0 +1,18 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_logs
+import aws_cdk.cdk
+__all__ = []
+
+publication.publish()
