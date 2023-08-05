6d4bc68fae97072119c06aaa90a7ad0cbd2dee7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:23 +0000

    Add package ncluster/0.1.56/ncluster-0.1.56.tar.gz

---
+++ packages/ncluster/ncluster-0.1.56.tar.gz/ncluster-0.1.56/ncluster/__init__.py
@@ -0,0 +1,37 @@
+import os
+
+from . import aws_backend
+from . import aws_util
+from . import util
+from . import local_backend
+from . import backend  # TODO: remove?
+
+from .ncluster import get_backend
+from .ncluster import set_backend
+from .ncluster import running_locally
+
+from .ncluster import use_aws
+from .ncluster import use_local
+
+from .ncluster import make_task
+from .ncluster import make_job
+from .ncluster import make_run
+from .ncluster import get_zone
+from .ncluster import get_region
+from .ncluster import set_logdir_root
+from .ncluster import get_logdir_root
+from .ncluster import add_authorized_keys
+
+# set default backend from environment
+if 'NCLUSTER_BACKEND' in os.environ:
+  set_backend(os.environ['NCLUSTER_BACKEND'])
+else:
+  set_backend('aws')
+
+# print custom settings
+for v in os.environ:
+  if v.startswith('NCLUSTER'):
+    print(f"ncluster env setting {v}={os.environ[v]}")
+
+if not util.is_set('NCLUSTER_DISABLE_PDB_HANDLER'):
+  util.install_pdb_handler()  # CTRL+\ drops into pdb
