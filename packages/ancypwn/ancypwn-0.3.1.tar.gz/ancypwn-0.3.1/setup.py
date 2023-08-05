7b73de301e37765a31182e53a72d07280e9e02aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package ancypwn/0.3.1/ancypwn-0.3.1.tar.gz

---
+++ packages/ancypwn/ancypwn-0.3.1.tar.gz/ancypwn-0.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'docker',
+    'daemonize',
+]
+
+import sys
+if sys.platform == 'darwin':
+    install_requires.append('osascript') # apple script support
+
+setup(
+    name='ancypwn',
+    version='0.3.1',
+    description='Ancypwn docker image manipulation script, to simplify docker pwn environment management',
+    url='https://github.com/Escapingbug/ancypwn',
+    author='Anciety',
+    author_email='anciety512@gmail.com',
+    packages=['ancypwn', 'ancypwn.notify'],
+    package_dir={'ancypwn': 'src'},
+    entry_points={
+        'console_scripts': ['ancypwn=ancypwn.ancypwn:main']
+    },
+    install_requires=install_requires,
+)
