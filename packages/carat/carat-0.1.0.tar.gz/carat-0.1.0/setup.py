6a778fc7b4012844e00559ea66df1b9e49e8e804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package carat/0.1.0/carat-0.1.0.tar.gz

---
+++ packages/carat/carat-0.1.0.tar.gz/carat-0.1.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from importlib.machinery import SourceFileLoader
+import sys
+
+
+version = SourceFileLoader('carat.version',
+                               'carat/version.py').load_module()
+
+with open('README.md', 'r') as fdesc:
+    long_description = fdesc.read()
+
+setup(
+    name='carat',
+    version=version.version,
+    description='Computer-Aided Rhythm Analysis Toolbox',
+    author='Martín Rocamora',
+    author_email='rocamora@fing.edu.uy',
+    url='http://github.com/mrocamora/carat',
+    download_url='http://github.com/mrocamora/carat/releases',
+    packages=find_packages(),
+    package_data={'': ['example_data/*']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='audio music sound',
+    license='MIT',
+    install_requires=[
+        'audioread >= 2.0.0',
+        'numpy >= 1.8.0',
+        'scipy >= 1.0.0',
+        'scikit-learn >= 0.14.0',
+        'soundfile >= 0.9.0',
+    ],
+    extras_require={
+        'docs': ['numpydoc',
+                 'matplotlib >= 2.0.0'],
+        'tests': ['matplotlib >= 2.1'],
+        'display': ['matplotlib >= 1.5'],
+    }
+)
