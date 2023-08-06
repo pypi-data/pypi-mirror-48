21e02522ed2410a3c7a26b688f40c15bc5b2b519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:04 +0000

    Add package ipfml/0.4.0/ipfml-0.4.0.tar.gz

---
+++ packages/ipfml/ipfml-0.4.0.tar.gz/ipfml-0.4.0/README.rst
@@ -0,0 +1,51 @@
+Image Processing For Machine Learning
+=====================================
+
+This is a package developed during a thesis project.
+
+Installation
+------------
+
+.. code:: bash
+
+   pip install ipfml
+
+How to use ?
+------------
+
+To use, simply do :
+
+.. code:: python
+
+   from PIL import Image
+   from ipfml import processing
+   img = Image.open('path/to/image.png')
+   s = processing.get_LAB_L_SVD_s(img)
+
+
+Modules
+-------
+
+This project contains modules.
+
+- **processing** : *Image processing of images*
+- **metrics** : *Metrics computation of PIL or 2D, 3D numpy images*
+- **filters** : *Filters implemented such as noise filters*
+- **utils** : *All utils functions developed for the package*
+- **exceptions** : *All customized exceptions*
+
+All these modules will be enhanced during development of the package.
+
+Documentation
+-------------
+
+For more information about package, documentation_ is available.
+
+.. _documentation: https://prise-3d.github.io/IPFML/
+
+Contributing
+------------
+
+Please refer to the guidelines_ file if you want to contribute!
+
+.. _guidelines: https://github.com/prise-3d/IPFML/blob/master/CONTRIBUTING.md 
---
+++ packages/ipfml/ipfml-0.4.0.tar.gz/ipfml-0.4.0/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup
+import setuptools.command.build_py
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+class BuildTestCommand(setuptools.command.build_py.build_py):
+    """Custom build command."""
+
+    def run(self):
+
+        # run tests using doctest
+        import doctest
+        from ipfml import processing
+        from ipfml import metrics
+        from ipfml.filters import noise as noise_filters
+        from ipfml.iqa import fr as fr_iqa
+        from ipfml import utils
+
+        print("==============================")
+        print("Runs test command...")
+        doctest.testmod(processing)
+        doctest.testmod(metrics)
+        doctest.testmod(noise_filters)
+        doctest.testmod(fr_iqa)
+        doctest.testmod(utils)
+
+        setuptools.command.build_py.build_py.run(self)
+
+
+setup(
+    name='ipfml',
+    version='0.4.0',
+    description='Image Processing For Machine Learning',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    url='https://github.com/jbuisine/IPFML',
+    author='Jérôme BUISINE',
+    author_email='jerome.buisine@univ-littoral.fr',
+    license='MIT',
+    packages=['ipfml', 'ipfml/filters', 'ipfml/iqa'],
+    install_requires=[
+        'numpy',
+        'Pillow',
+        'sklearn',
+        'scikit-image',
+        'scipy',
+        'opencv-python',
+        'scipy',
+    ],
+    cmdclass={
+        'build_py': BuildTestCommand,
+    },
+    zip_safe=False)
