f4b0abc4bfde6b9dd2bb26df6c289c6de2b8f576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package geojson2imdf/0.0.1.4/geojson2imdf-0.0.1.4-py2.py3-none-any.whl

---
+++ packages/geojson2imdf/geojson2imdf-0.0.1.4-py2.py3-none-any.whl/geojson2imdf/app.py
@@ -0,0 +1,28 @@
+import os
+
+
+def run():
+    print(f" geojson2imdf is running in {os.getcwd()}")
+    p = os.path.join(os.getcwd(), "venue")
+    if(os.path.exists(p)):
+        print(f"👍 Found a folder named 'venue' in {p}")
+        v = os.path.join(p, "venue.geojson")
+        if(os.path.exists(v)):
+            print(f"found venue.geojson in {p}")
+        else:
+            print(f"no venue.geojson found in {p}")
+
+        subfolders = [f.name for f in os.scandir(p) if f.is_dir()]
+        for folder in subfolders:
+            print(f"found a folder called {folder} in {p}")
+
+    else:
+        print(f"😢 No folder named 'venue' found in {p}")
+
+
+def version():
+    print("version")
+
+
+if __name__ == '__main__':
+    run()
