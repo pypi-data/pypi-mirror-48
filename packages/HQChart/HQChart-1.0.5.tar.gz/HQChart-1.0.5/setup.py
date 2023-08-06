6f6f0b568acaab2a743139c864baf8b1d36f3d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:01 +0000

    Add package HQChart/1.0.5/HQChart-1.0.5.tar.gz

---
+++ packages/HQChart/HQChart-1.0.5.tar.gz/HQChart-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup 
+setup ( 
+name='HQChart',     # 这个是最终打包的文件名 
+version='1.0.5', 
+author='jones2000',
+url='https://github.com/jones2000/HQChart',
+install_requires=['requests'],
+#打包.py文件， 
+py_modules=[  
+    '__init__',              
+    'umychart_complier_data',
+    'umychart_complier_help',
+    'umychart_complier_job',
+    'umychart_complier_jsalgorithm',
+    'umychart_complier_jscomplier',
+    'umychart_complier_jsexecute',
+    'umychart_complier_jsparser',
+    'umychart_complier_jssymboldata',
+    'umychart_complier_scanner',
+    'umychart_complier_testcase',
+    'umychart_complier_util'
+    ], 
+license='MIT',
+)
