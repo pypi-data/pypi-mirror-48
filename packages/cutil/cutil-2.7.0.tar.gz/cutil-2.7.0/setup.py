bfb47e8c0544ed238c3b91754f5199cb78eca8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:32 +0000

    Add package cutil/2.7.0/cutil-2.7.0.tar.gz

---
+++ packages/cutil/cutil-2.7.0.tar.gz/cutil-2.7.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='cutil',
+    packages=['cutil'],
+    version='2.7.0',
+    description='A collection of useful functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Eddy Hintze',
+    author_email="eddy@hintze.co",
+    url="https://github.com/xtream1101/cutil",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['hashids',
+                      'psycopg2',
+                      'pytz',
+                      ]
+)
