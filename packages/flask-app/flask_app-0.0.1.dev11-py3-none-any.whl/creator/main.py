391c473d2e771ad15866a53cb94e2d8570316f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:09 +0000

    Add package flask-app/0.0.1.dev11/flask_app-0.0.1.dev11-py3-none-any.whl

---
+++ packages/flask-app/flask_app-0.0.1.dev11-py3-none-any.whl/creator/main.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import shutil
+import sys
+
+
+cwd = os.getcwd()
+script_dir = cwd+'/flask_app'
+
+
+def program(argv):
+    parser = argparse.ArgumentParser(
+        description='My flask  app boilerplate code')
+    parser.add_argument('appname', help='The application name')
+    parser.add_argument('-d', '--destination', help='Folder to copy code to')
+    args = parser.parse_args()
+
+    appname = args.appname
+    fullpath = os.path.join(cwd, appname)
+    destination = args.destination
+    shutil.copytree(os.path.join(script_dir, destination), fullpath)
+
+    # create .env file
+    shutil.copy(fullpath+'/.env.sample', fullpath+'/.env')
+
+
+def main():
+    program(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
