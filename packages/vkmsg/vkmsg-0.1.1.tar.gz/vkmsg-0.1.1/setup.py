a6a571fd32e47842caf695e58508bed878ab7e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:10 +0000

    Add package vkmsg/0.1.1/vkmsg-0.1.1.tar.gz

---
+++ packages/vkmsg/vkmsg-0.1.1.tar.gz/vkmsg-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vkmsg",
+    version="0.1.1",
+    author="Appvelox LLC",
+    author_email="team@appvelox.ru",
+    description="Simple but yet functional library for building VK bots",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AppVelox/vkmsg",
+    packages=setuptools.find_packages(exclude=['tests*', 'examples*']),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests'],
+    python_requires='>=3.6',
+)
