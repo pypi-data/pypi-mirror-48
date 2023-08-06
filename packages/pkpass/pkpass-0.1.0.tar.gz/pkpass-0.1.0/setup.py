661c59b8ab33e24f97ddda93c0a9bc0d0bbd7de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:24 +0000

    Add package pkpass/0.1.0/pkpass-0.1.0.tar.gz

---
+++ packages/pkpass/pkpass-0.1.0.tar.gz/pkpass-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+setup(name='pkpass',
+      version='0.1.0',
+      description='General-purpose .pkpass file reader, includes airline and train specific parsers',
+      url='http://github.com/konradit/pkpass',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Konrad Iturbe',
+      author_email='mail@chernowii.com',
+      license='MIT',
+      zip_safe=False)
+
