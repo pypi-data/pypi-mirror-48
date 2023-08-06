720395089b9ede715d4dc29a956b527defd1f74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:00 +0000

    Add package angrcli/1.0rc2/angrcli-1.0rc2-py3-none-any.whl

---
+++ packages/angrcli/angrcli-1.0rc2-py3-none-any.whl/angrcli/plugins/watches.py
@@ -0,0 +1,38 @@
+from angr import SimStatePlugin
+
+
+class Watches(SimStatePlugin):
+    def __init__(self, watches={}):
+        """
+        :param watches: a list of lambdas that map a state to some AST to evaluate an arbitrary expression inside the state and keep track in the view
+        """
+        super(Watches, self).__init__()
+        self._watches = watches
+
+    def set_state(self, state):
+        super(Watches, self).set_state(state)
+
+    def add_watch(self, watch, name):
+        self._watches[name] = watch
+
+    def watch_bv(self, bv, cast_to=None):
+        w = lambda state: state.solver.eval(bv, cast_to=cast_to)
+        self.add_watch(w, bv.args[0].split('_')[0])
+        return w
+
+    def __getitem__(self, key):
+        return self._watches[key](self.state)
+
+    @SimStatePlugin.memo
+    def copy(self, memo):
+        return Watches(watches=self._watches)
+
+    @property
+    def eval(self):
+        return [(name, w(self.state)) for name, w in self._watches.items()]
+
+
+
+
+
+Watches.register_default("watches")
