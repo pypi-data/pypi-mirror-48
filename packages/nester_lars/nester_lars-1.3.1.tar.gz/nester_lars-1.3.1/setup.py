9e4498d9d15636f8d42831b3b0d39c11bd7ad556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:26 +0000

    Add package nester_lars/1.3.1/nester_lars-1.3.1.tar.gz

---
+++ packages/nester_lars/nester_lars-1.3.1.tar.gz/nester_lars-1.3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name            = 'nester_lars',
+    version         = '1.3.1',
+    py_modules      = ['nester'],
+    author          = 'StarLars',
+    author_email    = 'larsamels@hotmail.com',
+    description     = 'A simple printer of nested lists',
+)
