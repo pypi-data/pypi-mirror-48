59e3ff62d7d149001147b2abe98c48f07eca8b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:54 +0000

    Add package linesample/0.1.1/linesample-0.1.1.tar.gz

---
+++ packages/linesample/linesample-0.1.1.tar.gz/linesample-0.1.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linesample",
+    version="0.1.1",
+    author="Steve Matsumoto",
+    author_email="stephanos.matsumoto@sporic.me",
+    entry_points={
+        'console_scripts': ['linesample=linesample.__main__:main']
+    },
+    description="Utility to randomly sample lines from a file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/syclops/linesample",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    python_requires=">=3",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
