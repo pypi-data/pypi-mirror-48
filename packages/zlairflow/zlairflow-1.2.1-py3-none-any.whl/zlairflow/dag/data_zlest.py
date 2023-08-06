227b22e46d882211842989b80d70ff29c9dcda69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:21 +0000

    Add package zlairflow/1.2.1/zlairflow-1.2.1-py3-none-any.whl

---
+++ packages/zlairflow/zlairflow-1.2.1-py3-none-any.whl/zlairflow/dag/data_zlest.py
@@ -0,0 +1,24 @@
+para=[
+['zlest', 'bc_ezhoushi', 'minutes=480', ''],
+['zlest', 'bc_guizhousheng', 'minutes=480', ''],
+['zlest', 'bc_qinghaisheng', 'minutes=480', ''],
+['zlest', 'bc_shaoyangxian', 'minutes=480', ''],
+['zlest', 'bc_shenzhenshi', 'minutes=480', ''],
+['zlest', 'bc_anhuisheng', 'minutes=480', ''],
+['zlest', 'bc_guangdongsheng', 'minutes=480', ''],
+['zlest', 'bc_guangxisheng', 'minutes=480', ''],
+['zlest', 'bc_henansheng', 'minutes=480', ''],
+['zlest', 'bc_hunansheng', 'minutes=480', ''],
+['zlest', 'bc_jiangxisheng', 'minutes=480', ''],
+['zlest', 'bc_qg_ggzy', 'minutes=480', ''],
+['zlest', 'bc_wuxishi', 'minutes=480', ''],
+['zlest', 'bc_zhejiangsheng', 'minutes=480', ''],
+['zlest', 'bc_anhuisheng1', 'minutes=480', ''],
+['zlest', 'bc_enshizhou', 'minutes=480', ''],
+['zlest', 'bc_qg_zfcg', 'minutes=480', ''],
+['zlest', 'bc_hubeisheng', 'minutes=480', ''],
+['zlest', 'bc_www_bidding_citic', 'minutes=480', ''],
+    
+]
+
+
