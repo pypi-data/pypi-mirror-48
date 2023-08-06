754fc62dd7dbab3249212d827eb0f52bcdbbbabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:08 +0000

    Add package notify-server/0.1.0/notify-server-0.1.0.tar.gz

---
+++ packages/notify-server/notify-server-0.1.0.tar.gz/notify-server-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='notify-server',
+    version='0.1.0',
+    description='A tool to broadcast notifications across various interfaces',
+    url='https://github.com/matthewscholefield/notify-server',
+    author='Matthew Scholefield',
+    author_email='matthew331199@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='notify server',
+    packages=['notify_server'],
+    install_requires=[
+        'Flask',
+        'flask-resto',
+        'ruko',
+        'flask-ruko',
+        'gunicorn',
+        'prettyparse',
+        'notify-client'
+    ],
+    entry_points={
+        'console_scripts': [
+            'notify-server=notify_server.__main__:main',
+        ],
+    }
+)
