6a9cd82e8efb5280b003aff1d03f7e22dd0946b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package nicetable/0.7.0/nicetable-0.7.0.tar.gz

---
+++ packages/nicetable/nicetable-0.7.0.tar.gz/nicetable-0.7.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import nicetable
+
+with open("README.md", "r", encoding="utf8") as f:
+    long_description = f.read()
+
+setup(
+    name='nicetable',
+    version=nicetable.__version__,
+    python_requires='>=3.6',
+    author='Ofir Manor',
+    author_email='ofir.manor@gmail.com',
+    description='A clean and elegant way to print text tables in Python with minimal boilerplate code',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ofirmanor/nicetable',
+    license='MIT',
+    keywords='table tabular textual display data formatter ascii',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+)
