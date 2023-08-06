d23e08fc86074b81baa1bc3249ec336934d9ed2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:26 +0000

    Add package JsonTest/1.4/JsonTest-1.4-py2.py3-none-any.whl

---
+++ packages/JsonTest/JsonTest-1.4-py2.py3-none-any.whl/jsontest.py
@@ -0,0 +1,28 @@
+import json
+from os import listdir, path
+
+
+class JsonTest(type):
+    def __new__(cls, name, bases, attrs):
+        # Get the JSON files
+        files = listdir(attrs['jsontest_files'])
+        files = [f for f in files if f.endswith('.json')]
+
+        test_prefix = attrs.get('jsontest_prefix', 'test_')
+
+        def gen_test(test_name, filename):
+            def test(self):
+                test_data = json.loads(open(path.join(attrs['jsontest_files'], filename)).read())
+                self.jsontest_function(test_name, test_data)
+
+            return test
+
+        # Loop them and create class methods to call the jsontest_function
+        for filename in files:
+            test_name = filename[:-5]
+
+            # Attach the method
+            method_name = '{0}{1}'.format(test_prefix, test_name)
+            attrs[method_name] = gen_test(test_name, filename)
+
+        return type.__new__(cls, name, bases, attrs)
