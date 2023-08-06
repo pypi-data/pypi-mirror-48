679c34c6bea33116f90576fe27c7af6740d5bac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:13 +0000

    Add package draup-django/1.3.1/draup_django-1.3.1.tar.gz

---
+++ packages/draup-django/draup_django-1.3.1.tar.gz/draup_django-1.3.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="draup_django",
+    version="1.3.1",
+    author="Arpit | Teja | Kashish",
+    author_email="arpit@zinnov.com",
+    description=" Django Utility - Draup Labs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Draup-Zinnov/draup_django",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Framework :: Django",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
