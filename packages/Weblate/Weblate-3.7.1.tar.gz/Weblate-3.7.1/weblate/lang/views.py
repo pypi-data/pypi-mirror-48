787aa7d53e09c04af189ce8e0029a26ec51d20c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:45 +0000

    Add package Weblate/3.7.1/Weblate-3.7.1.tar.gz

---
+++ packages/Weblate/Weblate-3.7.1.tar.gz/Weblate-3.7.1/weblate/lang/views.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2019 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.http import Http404
+from django.shortcuts import redirect, render
+from django.utils.http import urlencode
+from django.utils.translation import ugettext as _
+
+from weblate.lang.models import Language
+from weblate.trans.forms import SiteSearchForm
+from weblate.trans.models import Change
+from weblate.trans.util import sort_objects
+from weblate.utils.stats import GlobalStats, prefetch_stats
+from weblate.utils.views import get_paginator, get_project
+
+
+def show_languages(request):
+    return render(
+        request,
+        'languages.html',
+        {
+            'allow_index': True,
+            'languages': prefetch_stats(
+                sort_objects(
+                    Language.objects.have_translation()
+                )
+            ),
+            'title': _('Languages'),
+            'global_stats': GlobalStats(),
+        }
+    )
+
+
+def show_language(request, lang):
+    try:
+        obj = Language.objects.get(code=lang)
+    except Language.DoesNotExist:
+        obj = Language.objects.fuzzy_get(lang)
+        if isinstance(obj, Language):
+            return redirect(obj)
+        raise Http404('No Language matches the given query.')
+
+    last_changes = Change.objects.last_changes(request.user).filter(
+        translation__language=obj
+    )[:10]
+    projects = request.user.allowed_projects
+    dicts = projects.filter(
+        dictionary__language=obj
+    ).distinct()
+    projects = projects.filter(
+        component__translation__language=obj
+    ).distinct()
+
+    for project in projects:
+        project.language_stats = project.stats.get_single_language_stats(obj)
+
+    return render(
+        request,
+        'language.html',
+        {
+            'allow_index': True,
+            'object': obj,
+            'last_changes': last_changes,
+            'last_changes_url': urlencode({'lang': obj.code}),
+            'dicts': dicts,
+            'projects': projects,
+        }
+    )
+
+
+def show_project(request, lang, project):
+    try:
+        obj = Language.objects.get(code=lang)
+    except Language.DoesNotExist:
+        obj = Language.objects.fuzzy_get(lang)
+        if isinstance(obj, Language):
+            return redirect(obj)
+        raise Http404('No Language matches the given query.')
+
+    pobj = get_project(request, project)
+
+    last_changes = Change.objects.last_changes(request.user).filter(
+        translation__language=obj,
+        component__project=pobj
+    )[:10]
+
+    # Paginate translations.
+    translation_list = obj.translation_set.prefetch().filter(
+        component__project=pobj
+    ).order_by(
+        'component__project__slug', 'component__slug'
+    )
+    translations = get_paginator(request, translation_list)
+
+    return render(
+        request,
+        'language-project.html',
+        {
+            'allow_index': True,
+            'language': obj,
+            'project': pobj,
+            'last_changes': last_changes,
+            'last_changes_url': urlencode(
+                {'lang': obj.code, 'project': pobj.slug}
+            ),
+            'translations': translations,
+            'title': '{0} - {1}'.format(pobj, obj),
+            'search_form': SiteSearchForm(),
+            'licenses': ', '.join(sorted(pobj.get_licenses())),
+        }
+    )
---
+++ packages/Weblate/Weblate-3.7.1.tar.gz/Weblate-3.7.1/weblate/trans/tests/test_sort.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2012 - 2019 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""
+Tests for unicode sorting.
+"""
+
+from unittest import SkipTest
+
+from django.test import TestCase
+
+import weblate.trans.util
+
+
+class SortTest(TestCase):
+    def sort_tester(self):
+        result = weblate.trans.util.sort_choices(
+            ((2, 'zkouška'), (3, 'zkouzka'), (1, 'zkouaka'))
+        )
+        self.assertEqual(
+            [1, 2, 3],
+            [x[0] for x in result]
+        )
+
+    def test_sort_pyuca(self):
+        if not weblate.trans.util.HAS_PYUCA:
+            raise SkipTest('pyuca not installed')
+        self.sort_tester()
+
+    def test_sort_fallback(self):
+        backup = weblate.trans.util.HAS_PYUCA
+        try:
+            weblate.trans.util.HAS_PYUCA = False
+
+            self.sort_tester()
+        finally:
+            weblate.trans.util.HAS_PYUCA = backup
