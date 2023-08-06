f5e82b53d459edef21de764d05c5ac7196d9ff9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:04 +0000

    Add package igloo-python/0.9.10/igloo-python-0.9.10.tar.gz

---
+++ packages/igloo-python/igloo-python-0.9.10.tar.gz/igloo-python-0.9.10/README.md
@@ -0,0 +1,14 @@
+<p align="center">
+  <img src="https://github.com/IglooCloud/igloo.py/blob/master/IglooLogo.png" alt="Igloo logo" width="128"/>
+</p>
+
+<h1 align="center">igloo_python</h1>
+
+igloo_python is the official Igloo library for Python.
+
+## Getting started
+TBD
+
+## License
+
+See the [license](https://raw.githubusercontent.com/IglooCloud/igloo_python/master/LICENSE) file for license rights and limitations.
---
+++ packages/igloo-python/igloo-python-0.9.10.tar.gz/igloo-python-0.9.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='igloo-python',         # How you named your package folder (MyLib)
+    packages=['igloo'],   # Chose the same as "name"
+    version='0.9.10',
+    license='MIT',
+    description='Python client for igloo',
+    author='Igloo Team',
+    author_email='hello@igloo.ooo',
+    url='https://github.com/IglooCloud/igloo_python',
+    keywords=['iot', 'igloo'],
+    install_requires=[
+        'requests', 'asyncio', 'pathlib', 'websockets', 'aiodataloader', 'aiohttp'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development ',
+        'License :: OSI Approved :: MIT License  ',
+        'Programming Language :: Python :: 3',
+    ],
+)
