4100311a6efb0c45706aa82aef7e01dbc2744eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:15 +0000

    Add package slicedimage/3.2.0/slicedimage-3.2.0.tar.gz

---
+++ packages/slicedimage/slicedimage-3.2.0.tar.gz/slicedimage-3.2.0/slicedimage/backends/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from ._base import ChecksumValidationError
+from ._caching import CachingBackend, SIZE_LIMIT
+from ._disk import DiskBackend
+from ._http import HttpBackend
+from ._s3 import S3Backend
+
+
+__all__ = [
+    CachingBackend,
+    ChecksumValidationError,
+    DiskBackend,
+    HttpBackend,
+    S3Backend,
+    SIZE_LIMIT,
+]
