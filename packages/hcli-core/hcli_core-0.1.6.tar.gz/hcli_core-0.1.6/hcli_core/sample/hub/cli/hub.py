2a21d217c7d45c6d20f11c28d1f697f637b719fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:01 +0000

    Add package hcli-core/0.1.6/hcli_core-0.1.6.tar.gz

---
+++ packages/hcli-core/hcli_core-0.1.6.tar.gz/hcli_core-0.1.6/hcli_core/sample/hub/cli/hub.py
@@ -0,0 +1,36 @@
+import json
+import namespace as ns
+import data
+
+class Hub:
+    namespace = None
+
+    def __init__(self):
+        if not data.DAO().exists():
+            self.namespace = []
+            self.namespace.append(ns.Namespace())
+            data.DAO(self).save()
+
+        else:
+            data.DAO().load(self)
+
+    def serialize(self):
+        return data.DAO(self).serialize()   
+    
+    def listNamespaces(self):
+        namespaces = ""
+        for index, i in enumerate(self.namespace):
+            arg = self.namespace[index]
+            namespaces = namespaces + arg['name'] + "\n"
+
+        return namespaces
+
+    def findService(self, name):
+        services = ""
+        for index, i in enumerate(self.namespace):
+            arg = self.namespace[index]
+            for jndex, j in enumerate(arg['service']):
+                if j['name'] == name.replace("'", "").replace("\"", ""):
+                    services = services + arg['name'] + ":" + j['name'] + ":" + j['href'] + "\n"
+
+        return services
