78fb89bbc194388f4ff2cf27b984ae61359955f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package behave-manners/0.9.0/behave_manners-0.9.0-py2.py3-none-any.whl

---
+++ packages/behave-manners/behave_manners-0.9.0-py2.py3-none-any.whl/behave_manners/pagelems/actions.py
@@ -0,0 +1,48 @@
+# -*- coding: UTF-8 -*-
+
+from abc import abstractmethod
+
+""" Pseudo-objects of 'actions' that can be /assigned/ to components
+
+    Since the components and their attributes may be virtual (ie. dynamic
+    attribute that returns its value as scalar), there must be a way to
+    expose the object on the remote side and act on it (rather than just
+    assigning a scalar value to it).
+    
+    Pseudo-objects allow it::
+        
+    >>> from behave_manners.pagelems.actions import click
+    >>> form = page['The form']
+    >>> form.name = 'Some name'
+    >>> assert form.date == '2019-01-01'   # it would be a string
+    >>> form.date = click()   # assign an action, ie. click the field!
+
+"""
+
+class Action(object):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def act_on_element(self, welem):
+        """Defines how to perform this action on some remote element
+        """
+        pass
+
+
+class click(Action):
+    def act_on_element(self, welem):
+        welem.click()
+
+
+class focus(Action):
+    pass
+    # TODO
+
+
+class blur(Action):
+    pass
+    # TODO
+
+
+# eof
