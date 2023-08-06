fef217224e712a43e25510bd5e960f27f6a17102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:57 +0000

    Add package cpbox/1.4.26/cpbox-1.4.26.tar.gz

---
+++ packages/cpbox/cpbox-1.4.26.tar.gz/cpbox-1.4.26/tests/test_dockerutil.py
@@ -0,0 +1,19 @@
+import unittest
+from cpbox.tool import dockerutil
+
+
+class TestDockerUtil(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_all(self):
+        ip = dockerutil.get_docker_network_gw('bridge')
+        print(ip)
+
+
+if __name__ == '__main__':
+    unittest.main()
