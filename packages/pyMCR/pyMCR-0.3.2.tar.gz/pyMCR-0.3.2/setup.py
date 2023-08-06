29095afc6c74467451833d1b6b08ead34c1c6bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:55 +0000

    Add package pyMCR/0.3.2/pyMCR-0.3.2.tar.gz

---
+++ packages/pyMCR/pyMCR-0.3.2.tar.gz/pyMCR-0.3.2/setup.py
@@ -0,0 +1,38 @@
+"""
+Setup for pyMCR
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pyMCR',
+      version = '0.3.2',
+      description = 'Multivariate Curve Resolution in Python',
+      long_description = long_description,
+      url = 'https://github.com/usnistgov/pyMCR',
+      author = 'Charles H. Camp Jr.',
+      author_email = 'charles.camp@nist.gov',
+      license = 'Public Domain',
+      packages = find_packages(),
+      zip_safe = False,
+      include_package_data = True,
+      install_requires=['numpy', 'scipy'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: Science/Research',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 3 :: Only',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Scientific/Engineering :: Bio-Informatics',
+                   'Topic :: Scientific/Engineering :: Chemistry',
+                   'Topic :: Scientific/Engineering :: Information Analysis',
+                   'Topic :: Scientific/Engineering :: Mathematics',
+                   'Topic :: Scientific/Engineering :: Physics'])
