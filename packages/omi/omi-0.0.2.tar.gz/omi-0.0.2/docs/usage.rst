66ae53548183bbb94775ec9ce3b0f5c098ef45bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:11 +0000

    Add package omi/0.0.2/omi-0.0.2.tar.gz

---
+++ packages/omi/omi-0.0.2.tar.gz/omi-0.0.2/docs/usage.rst
@@ -0,0 +1,11 @@
+=====
+Usage
+=====
+
+.. click:: omi.cli:cli
+   :prog: omi
+   :show-nested:
+
+To use omi in a project::
+
+	import omi
---
+++ packages/omi/omi-0.0.2.tar.gz/omi-0.0.2/src/omi/dialects/base/dialect.py
@@ -0,0 +1,28 @@
+from omi.dialects.base.compiler import Compiler
+from omi.dialects.base.parser import Parser
+from omi.dialects.base.renderer import Renderer
+from omi.structure import OEPMetadata
+
+
+class Dialect:
+    _parser = Parser
+    _compiler = Compiler
+    _renderer = Renderer
+
+    def compile(self, obj: OEPMetadata, *args, **kwargs):
+        c = self._compiler()
+        return c.visit(obj, *args, **kwargs)
+
+    def parse(self, string: str, *args, **kwargs) -> OEPMetadata:
+        p = self._parser()
+        return p.parse_from_string(string, *args, **kwargs)
+
+    def render(self, structure, *args, **kwargs) -> str:
+        r = self._renderer()
+        return r.render(structure, *args, **kwargs)
+
+    def compile_and_render(self, obj: OEPMetadata, *args, **kwargs):
+        c = self._compiler()
+        r = self._renderer()
+        compiled = c.visit(obj, *args, **kwargs)
+        return r.render(compiled, *args, **kwargs)
