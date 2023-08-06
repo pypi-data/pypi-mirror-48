fe3e37c35a12926eac70bc0ca2402e6e29169a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:29 +0000

    Add package spirit/2.0.1/spirit-2.0.1-py2.py3-none-win_amd64.whl

---
+++ packages/spirit/spirit-2.0.1-py2.py3-none-win_amd64.whl/spirit/__init__.py
@@ -0,0 +1,13 @@
+"""
+Spirit
+==================================================
+"""
+
+__version__ = "2.0.1"
+__revision__ = "7057e7feb9b89"
+__title__ = "Spirit"
+__description__ = "Optimizations and Dynamics Framework for atomistic Spin systems"
+__uri__ = "https://spirit-code.github.io"
+__license__ = "MIT"
+__author__ = "Gideon Mueller"
+__email__ = "g.mueller@fz-juelich.de"
