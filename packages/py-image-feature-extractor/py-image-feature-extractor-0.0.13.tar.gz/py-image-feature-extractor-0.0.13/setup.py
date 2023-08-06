f3b9d845ad3ba8d3f3984a383f07bdb0d2f99f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:18 +0000

    Add package py-image-feature-extractor/0.0.13/py-image-feature-extractor-0.0.13.tar.gz

---
+++ packages/py-image-feature-extractor/py-image-feature-extractor-0.0.13.tar.gz/py-image-feature-extractor-0.0.13/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='py-image-feature-extractor',
+    version='0.0.13',
+    author='Javier Guzman',
+    author_email='jguzmanfd@gmail.com',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['image_feature_extractor = image_feature_extractor.cli:main']},
+    install_requires=[
+        'click',
+        'numpy',
+        'scikit-image',
+        'tensorflow',
+        'opencv-python==3.4.2.17',
+        'opencv-contrib-python==3.4.2.17',
+        'pandas',
+        'pytest',
+        'matplotlib',
+        'scikit-learn',
+        'scipy',
+    ],
+    url='https://github.com/jgoodman8/py-image-feature-selector',
+)
