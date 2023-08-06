6950dd778bcb2f3590cebe1a5d8bde849ce9d8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:28 +0000

    Add package gumo-core/0.1.0a5/gumo-core-0.1.0a5.tar.gz

---
+++ packages/gumo-core/gumo-core-0.1.0a5.tar.gz/gumo-core-0.1.0a5/gumo/core/presentation/cli/__init__.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import subprocess
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+def gumo_dev_server():
+    from gumo.core.infrastructure import MockAppEngineEnvironment
+
+    if len(sys.argv) == 0:
+        print("Usage: gumo-dev-server <path-to-app.yaml>")
+        exit(1)
+
+    app_yaml = sys.argv[1]
+    if not os.path.exists(app_yaml):
+        print(f'File: {app_yaml} does not found.')
+        exit(1)
+
+    MockAppEngineEnvironment.load_app_yaml(app_yaml_path=app_yaml)
+
+    entry_point = os.path.join(
+        os.path.dirname(app_yaml),
+        'main.py'
+    )
+    subprocess.run(
+        args=['python', entry_point],
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
+
+
+if __name__ == '__main__':
+    cli = os.path.dirname(__file__)
+    presentation = os.path.dirname(cli)
+    core = os.path.dirname(presentation)
+    gumo = os.path.dirname(core)
+    sys.path.insert(0, os.path.dirname(gumo))
+    gumo_dev_server()
