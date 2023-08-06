255dd6e17b21fd5ca44f87dc1171704472ed0339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package thunder-remote/0.6.0/thunder_remote-0.6.0-py2.7.egg

---
+++ packages/thunder-remote/thunder_remote-0.6.0-py2.7.egg/thunder_remote/ControllerMapping.py
@@ -0,0 +1,33 @@
+class ControllerMapping:
+    BTN_SOUTH = []
+    BTN_WEST = []
+    BTN_NORTH = []
+    BTN_EAST = []
+
+    STICK_LEFT_X = []
+    STICK_LEFT_Y = []
+    THUMB_L = []
+
+    STICK_RIGHT_X = []
+    STICK_RIGHT_Y = []
+    THUMB_R = []
+
+    CROSS_X = []
+    CROSS_Y = []
+
+    TRIGGER_R = []
+    SHOULDR_R = []
+
+    TRIGGER_L = []
+    SHOULDR_L = []
+
+    SELECT = []
+    START = []
+
+    WAKE_UP = []
+
+    FORWARD_VALUES = []
+    BACKWARD_VALUES = []
+
+    def __init__(self):
+        pass
