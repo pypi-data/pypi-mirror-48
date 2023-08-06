2d348c4557552f7dc16d61bf9b273a5b079f4804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package reconium/0.3/reconium-0.3.tar.gz

---
+++ packages/reconium/reconium-0.3.tar.gz/reconium-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'reconium',
+  packages = ['reconium'],
+  version = '0.3',
+  description = 'record selenium test video',
+  author = 'yldrmzffr',
+  author_email = 'muzaffer@yldrmzffr.com',
+  url = 'https://github.com/yldrmzffr/reconium',
+  download_url = 'https://github.com/yldrmzffr/reconium/tarball/0.3',
+  keywords = ['selenium', 'video', 'recorder'], 
+  classifiers = [],
+)
