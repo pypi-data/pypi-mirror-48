5384ee8f37959b5f2f032dc6536dca779a3b3c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:02 +0000

    Add package pytelematics-oasa/1.0.0/pytelematics-oasa-1.0.0.tar.gz

---
+++ packages/pytelematics-oasa/pytelematics-oasa-1.0.0.tar.gz/pytelematics-oasa-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytelematics-oasa",
+    version="1.0.0",
+    author="blackrose514",
+    author_email="dmtri3sukuna@gmail.com",
+    license='MIT',
+    description="OASA Telematics API wrapper for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blackrose514/pytelematics-oasa",
+    download_url = "https://github.com/blackrose514/pytelematics-oasa/releases",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    keywords = ['oasa', 'telematics', 'api', 'pytelematics-oasa'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
