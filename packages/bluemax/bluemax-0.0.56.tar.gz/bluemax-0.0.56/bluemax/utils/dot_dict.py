1a5e172baf87dc02300a7af2c35261af2f2adccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package bluemax/0.0.56/bluemax-0.0.56.tar.gz

---
+++ packages/bluemax/bluemax-0.0.56.tar.gz/bluemax-0.0.56/bluemax/utils/dot_dict.py
@@ -0,0 +1,12 @@
+class DotDict(dict):
+    """
+        A dict that allows for object-like property access syntax.
+    """
+
+    def __getattr__(self, name):
+        if name[0] == "_":
+            return dict.__getattr__(self.name)
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
