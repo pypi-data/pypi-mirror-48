d2e01a47674c095914437fb7ab694be7abcc0e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:16 +0000

    Add package giosgapps-bindings/0.0.2/giosgapps_bindings-0.0.2.tar.gz

---
+++ packages/giosgapps-bindings/giosgapps_bindings-0.0.2.tar.gz/giosgapps_bindings-0.0.2/giosgapps_bindings/tests/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from ..models import GiosgAppInstallation
+
+class SampleAppInstallation(GiosgAppInstallation):
+    pass
+
+
+class GiosgAppInstallationTest(TestCase):
+
+    def test_creation(self):
+        m = SampleAppInstallation(
+            installed_org_uuid='f50ec0b7-f960-400d-91f0-c42a6d44e3d0',
+            persistent_bot_token='8ajjx980937h8fshxnba1700dshjfncm',  # from public documentation
+            persistent_token_prefix='Token'
+        )
+        m.save()
+        self.assertTrue(isinstance(m, SampleAppInstallation))
