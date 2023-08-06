7c050def79b04ab2adbd08249e41d920987c5e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:11 +0000

    Add package ofx2xlsmbr/0.7/ofx2xlsmbr-0.7.tar.gz

---
+++ packages/ofx2xlsmbr/ofx2xlsmbr-0.7.tar.gz/ofx2xlsmbr-0.7/ofx2xlsmbr/reader/xml/XMLReaderController.py
@@ -0,0 +1,33 @@
+from ofx2xlsmbr.reader.IReaderController import IReaderController
+
+from ofx2xlsmbr.model.BankStatement import BankStatement
+
+import xml.etree.ElementTree as ET
+from lxml import etree
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class XMLReaderController(IReaderController):
+    def read(self, factory, inputFilename='', file=None) -> BankStatement:
+        data = ''
+        tree = None
+
+        if file is not None:
+            data = self.readFile(file)
+            parser = etree.XMLParser(recover=True)
+            tree = etree.fromstring(data, parser=parser)
+        else:
+            with open(inputFilename, 'r', encoding='iso-8859-1') as inputFile:
+                data = self.readFile(inputFile)
+                tree = ET.fromstring(data)
+        
+        bsReader = factory.createReaderBankStatement()
+        bs = bsReader.read(factory, tree)
+        return bs
+
+    def readFile(self, file):
+        data = str(file.read())
+        data = data[data.find('<OFX>'):]
+        return data
