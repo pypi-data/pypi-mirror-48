2c76764d567a7c617f7d0d5af0ce2ae021c698fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:17 +0000

    Add package synotools/0.3.9/synotools-0.3.9.tar.gz

---
+++ packages/synotools/synotools-0.3.9.tar.gz/synotools-0.3.9/synotools/commands/download.py
@@ -0,0 +1,38 @@
+import logging
+import sys
+
+from fabric import Connection, Config
+
+from synotools.models.config.syno import SynoConfig
+from synotools.models.config.deluge import DelugeConfig
+
+
+def download_torrent_with_deluge(torrent_url):
+    syno_config = SynoConfig()
+    deluge_config = DelugeConfig()
+
+    deluge_connection = f"{deluge_config.ip}:{deluge_config.port}"
+
+    fabric_config = Config(overrides={"sudo": {"password": syno_config.password}})
+
+    connection = Connection(
+        host=syno_config.ip, user=syno_config.username, config=fabric_config
+    )
+
+    try:
+        command = f'.scripts/deluge-download.sh "{deluge_connection}" {deluge_config.username} {deluge_config.password} "{torrent_url}"'
+        result = connection.sudo(command)
+        logging.info(result)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    torrent_url = None
+
+    try:
+        torrent_url = sys.argv[1]
+    except IndexError:
+        pass
+
+    download_torrent_with_deluge(torrent_url)
