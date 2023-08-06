40ab16f33166339bde5bcaa397275c58b077e0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:44 +0000

    Add package xdnn/0.1/xdnn-0.1.tar.gz

---
+++ packages/xdnn/xdnn-0.1.tar.gz/xdnn-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='xdnn',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Prashant K. Sharma',
+      author_email='prashaantsharmaa@gmail.com',
+      license='MIT',
+      packages=['xdnn'],
+      zip_safe=False)
