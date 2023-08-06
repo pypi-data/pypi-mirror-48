ef4ea501c52d07a04543ad7c1b7365b680e00322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:44 +0000

    Add package datasetmaker/0.2.0/datasetmaker-0.2.0.tar.gz

---
+++ packages/datasetmaker/datasetmaker-0.2.0.tar.gz/datasetmaker-0.2.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+requires = ["ddf_utils", "pandas", "lxml", "requests",
+            "frame2package", "html5lib", "bs4"]
+
+setuptools.setup(
+    name="datasetmaker",
+    version="0.2.0",
+    description="Fetch, transform, and package data.",
+    author="Robin Linderborg",
+    author_email="robin@datastory.org",
+    install_requires=requires,
+    packages=setuptools.find_packages(),
+)
