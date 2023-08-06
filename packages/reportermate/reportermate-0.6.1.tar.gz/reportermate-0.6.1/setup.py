a960b3f1da02a871e120ac690ba17acc670e5bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:04 +0000

    Add package reportermate/0.6.1/reportermate-0.6.1.tar.gz

---
+++ packages/reportermate/reportermate-0.6.1.tar.gz/reportermate-0.6.1/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(name='reportermate',
+      version='0.6.1',
+      description='Automated news stories and reports from datasets',
+      long_description=open('README.rst').read(),
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+      ],
+      keywords='data-analysis text data analysis',
+      url='https://github.com/nickjevershed/Reportermate-Lib',
+      author='Nick Evershed',
+      author_email='nick.evershed@gmail.com',
+      license='MIT',
+      entry_points = {
+        "console_scripts":['reportermate = reportermate.command_line:main']
+        },
+      packages=['reportermate'],
+      install_requires=['pandas','pybars3','pydateinfer','simplejson'],
+      zip_safe=False)
