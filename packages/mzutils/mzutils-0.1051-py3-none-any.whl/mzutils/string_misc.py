b43942b73786c601df3121e8fbc6c4022421041a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:26 +0000

    Add package mzutils/0.1051/mzutils-0.1051-py3-none-any.whl

---
+++ packages/mzutils/mzutils-0.1051-py3-none-any.whl/mzutils/string_misc.py
@@ -0,0 +1,55 @@
+import nltk
+import pickle
+import codecs
+import ast
+
+def add_spaces_between_special_characters(InputText):
+    """
+    :param InputText:
+    :return:
+    """
+    regular = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM "
+    b = set(list(InputText))
+    spec = []
+    for i in b:
+        if i not in regular:
+            spec.append(i)
+    for i in spec:
+        InputText = InputText.replace(i, ' ' + i + ' ')
+    return InputText
+
+
+def select_first_sentence(InputText, language='english'):
+    return nltk.tokenize.sent_tokenize(InputText, language)[0]
+
+
+def py_serialize(filepath, itemlist):
+    """
+    serialize python objects using pickle
+    :param filepath:
+    :param itemlist:
+    :return:
+    """
+    with codecs.open(filepath, "wb+") as fp:
+        pickle.dump(itemlist, fp)
+
+
+def py_deserialize(filepath):
+    """
+
+    :param filepath:
+    :return:
+    """
+    itemlist = None
+    with codecs.open(filepath, 'rb') as fp:
+        itemlist = pickle.load(fp)
+    return itemlist
+
+
+def str_rep_to_list(s):
+    """
+    convert a string representation of list to a python list object.
+    :param s:
+    :return:
+    """
+    return ast.literal_eval(s)
