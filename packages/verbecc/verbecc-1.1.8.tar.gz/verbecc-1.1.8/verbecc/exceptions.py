a045ad468016f6e44bb4243399f9f6c088af1f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:09 +0000

    Add package verbecc/1.1.8/verbecc-1.1.8.tar.gz

---
+++ packages/verbecc/verbecc-1.1.8.tar.gz/verbecc-1.1.8/verbecc/exceptions.py
@@ -0,0 +1,24 @@
+
+class ConjugatorError(Exception):
+    pass
+
+class ConjugationsParserError(Exception):
+    pass
+
+class ConjugationTemplateError(Exception):
+    pass
+
+class InvalidMoodError(Exception):
+    pass
+
+class InvalidTenseError(Exception):
+    pass
+
+class TemplateNotFoundError(Exception):
+    pass
+
+class VerbNotFoundError(Exception):
+    pass
+
+class VerbsParserError(Exception):
+    pass
