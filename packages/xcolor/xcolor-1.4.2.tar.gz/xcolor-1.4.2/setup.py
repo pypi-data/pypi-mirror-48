f0babcb53d5353ab71a2a09bacd78765336ed1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:28 +0000

    Add package xcolor/1.4.2/xcolor-1.4.2.tar.gz

---
+++ packages/xcolor/xcolor-1.4.2.tar.gz/xcolor-1.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    author = "Max",
+    author_email = "max@xmail.vip",
+    license = "MIT",
+    name = "xcolor",
+    version = "1.4.2",
+    keywords = "color print ColorPrint python3",
+    url = "https://github.com/artsoftio/xcolor",
+    install_requires = [],
+    packages = ['xcolor'],
+    description = "终端彩色打印",
+    long_description = open("README.md").read(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: POSIX",
+    ]
+)
