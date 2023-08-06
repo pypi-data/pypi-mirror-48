7c4c4acdec0e9faeb91d438e106bcd632ab1a524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:52 +0000

    Add package camkes-deps/0.7.1/camkes-deps-0.7.1.tar.gz

---
+++ packages/camkes-deps/camkes-deps-0.7.1.tar.gz/camkes-deps-0.7.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+#
+# Copyright 2017, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+"""
+Setup script for dependency metapackage.
+
+To add a python dependency, add it to the DEPS list below.
+
+To publish to pypitest:
+python setup.py sdist upload -r pypitest
+
+To publish to pypi:
+python setup.py sdist upload -r pypi
+"""
+
+from setuptools import setup
+
+DEPS = [
+    'aenum',
+    'jinja2',
+    'orderedset',
+    'plyplus',
+    'pyelftools',
+    'sel4-deps',
+    'pycparser',
+    'pyfdt',
+    'concurrencytest',
+    'simpleeval',
+
+    # capDL deps
+    'sortedcontainers',
+    'hypothesis',
+]
+
+setup(
+    name='camkes-deps',
+    version='0.7.1',
+    description='Metapackage for downloading build dependencies for CAmkES',
+    url='https://docs.sel4.systems/CAmkES/',
+    license='BSD2',
+    author='TrustworthySystems',
+    author_email='Stephen.Sherratt@data61.csiro.au',
+    install_requires=DEPS,
+)
