cedb1f355db1c0605a9cc74c2e86f49500c6d15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:23 +0000

    Add package yabc/0.1.0/yabc-0.1.0.tar.gz

---
+++ packages/yabc/yabc-0.1.0.tar.gz/yabc-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+setuptools.setup(
+    version="0.1.0",
+    name="yabc",
+    python_requires=">=3.5,<3.8",
+    author="Seattle Blockchain Solutions",
+    install_requires=["flask==1.0.2", "sqlalchemy", "delorean==1.0.0"],
+    test_suite="tests",
+    description="A tax estimator for cryptocurrencies.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
