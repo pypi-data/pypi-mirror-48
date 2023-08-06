122cbcce1212e84a0a2df34290eff9047b4fe891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:14 +0000

    Add package pydvma/0.4.3/pydvma-0.4.3.tar.gz

---
+++ packages/pydvma/pydvma-0.4.3.tar.gz/pydvma-0.4.3/pydvma/__init__.py
@@ -0,0 +1,13 @@
+from .options import MySettings
+from .file import load_data, save_data, save_fig
+from .oscilloscope import Oscilloscope
+from .acquisition import log_data, stream_snapshot
+from .datastructure import DataSet, TimeData, FreqData, CrossSpecData, TfData, SonoData, MetaData
+from .testdata import create_test_impulse_data, create_test_impulse_ensemble, create_test_noise_data
+from .plotting import PlotData, PlotTimeData, PlotData2
+from .analysis import calculate_fft, calculate_cross_spectrum_matrix, calculate_cross_spectra_averaged
+from .analysis import calculate_tf, calculate_tf_averaged, multiply_by_power_of_iw, best_match
+from .streams import Recorder, Recorder_NI, start_stream, REC
+from .interactive_tools import InteractiveLogging, InteractiveView
+
+
