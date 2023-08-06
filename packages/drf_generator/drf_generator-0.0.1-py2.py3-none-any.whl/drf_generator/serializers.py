652a4eb2cc726f49d25e71d0718b6117fb12872a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:46 +0000

    Add package drf_generator/0.0.1/drf_generator-0.0.1-py2.py3-none-any.whl

---
+++ packages/drf_generator/drf_generator-0.0.1-py2.py3-none-any.whl/drf_generator/serializers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from rest_framework import serializers
+# Default serializer setting
+# A separate serializer maker which also accepts mixins
+# A separate viewset maker which also accepts mixins
+# Expand?
+
+
+def serializer_factory(model, mixins=()):
+    mixins = list(mixins)
+    mixins.append(serializers.HyperlinkedModelSerializer)
+
+    class TheSerializer(mixins[0]):
+        class Meta:
+            fields = "__all__"
+
+    TheSerializer.Meta.model = model
+
+    return TheSerializer
