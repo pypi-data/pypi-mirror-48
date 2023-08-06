60fa983c64546333034d3f1bf5bc3e8da7d9fc8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:34 +0000

    Add package shellcraft/0.11.0/shellcraft-0.11.0-py3-none-any.whl

---
+++ packages/shellcraft/shellcraft-0.11.0-py3-none-any.whl/shellcraft/research.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""Basic CLI for ShellCraft."""
+from shellcraft.core import BaseItem, BaseFactory
+
+
+class ResearchProject(BaseItem):
+    def __repr__(self):
+        return f"%{self.name}%"
+
+
+class ResearchFactory(BaseFactory):
+    FIXTURES = 'research.yaml'
+    ITEM_CLASS = ResearchProject
+
+    def is_available(self, item_name):
+        project = self.get(item_name)
+        return project.name not in self.game.state.research_completed and super(ResearchFactory, self).is_available(project)
