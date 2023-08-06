f252c265341eb4c1c3aa30c519927250c331e993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:26 +0000

    Add package sekg/0.4.7/sekg-0.4.7.tar.gz

---
+++ packages/sekg/sekg-0.4.7.tar.gz/sekg-0.4.7/sekg/constant/constant.py
@@ -0,0 +1,21 @@
+class CodeConstant:
+    LABEL_CODE_ELEMENT = "code_element"
+
+
+class DomainConstant:
+    LABEL_DOMAIN_TERM = "domain term"
+    PRIMARY_PROPERTY_NAME = "term_name"
+
+
+class OperationConstance:
+    LABEL_OPERATION = "operation"
+    PRIMARY_PROPERTY_NAME = "operation_name"
+
+
+class PropertyConstant:
+    COMMENT = "comment"
+    INSIDE_COMMENT = "inside_comment"
+    STRING_LITERAL_EXPR = "string_literal_expr"
+    DESCRIPTION = "short_description"
+    ALIAS = "alias"
+    LEMMA = "lemma"
