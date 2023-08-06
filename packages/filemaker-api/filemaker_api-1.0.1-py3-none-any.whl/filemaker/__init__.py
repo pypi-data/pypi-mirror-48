e4a8d939bc0bdc8094df616f965f5c3b60c4a1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:39 +0000

    Add package filemaker-api/1.0.1/filemaker_api-1.0.1-py3-none-any.whl

---
+++ packages/filemaker-api/filemaker_api-1.0.1-py3-none-any.whl/filemaker/__init__.py
@@ -0,0 +1,12 @@
+from filemaker.filemaker_api import *
+
+__author__ = "Hugo Angulo"
+__copyright__ = "Copyright 2019, CaosLab"
+__credits__ = ["Hugo Angulo", "Nour Al-Zaghloul"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Hugo Angulo"
+__email__ = "hugoanda@andrew.cmu.edu"
+__date__ = "06/25/2019"
+__status__ = "Production"
+
