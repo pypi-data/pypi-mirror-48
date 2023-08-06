3f8deb197f03a602382b74486ca443366aee5a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:00 +0000

    Add package sndata/0.3.2/SNData-0.3.2.tar.gz

---
+++ packages/sndata/SNData-0.3.2.tar.gz/SNData-0.3.2/SNData/sdss/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+# -*- coding: UTF-8 -*-
+
+"""This module provides access to supernova data from the Sloan Digital Sky
+Survey (SDSS). For more information on SDSS see https://www.sdss.org .
+"""
+
+from . import sako18
+
+survey_name = 'Sloan Digital Sky Survey'
+survey_abbrev = 'SDSS'
