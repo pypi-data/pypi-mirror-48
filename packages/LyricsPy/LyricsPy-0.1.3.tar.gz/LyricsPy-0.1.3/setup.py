3fb115eafb67440bfeeefef18cad8064b8ea911e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package LyricsPy/0.1.3/LyricsPy-0.1.3.tar.gz

---
+++ packages/LyricsPy/LyricsPy-0.1.3.tar.gz/LyricsPy-0.1.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='LyricsPy',
+    version='0.1.3',
+    packages=setuptools.find_packages(),
+    url='https://github.com/amanoteam/LyricsPy',
+    author='AmanoTeam',
+    install_requires=['requests', 'bs4', 'google', 'markdownify'],
+    author_email='contact@amanoteam.ml',
+    description='search lyrics on letras.mus',
+    long_description=long_desc,
+    long_description_content_type="text/markdown"
+)
