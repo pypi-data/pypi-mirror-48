52231d61b18c44567ded4208cbf855c4ff6b47e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:47 +0000

    Add package nestedDive/0.3.2/nestedDive-0.3.2-py3-none-any.whl

---
+++ packages/nestedDive/nestedDive-0.3.2-py3-none-any.whl/nestedDive/__init__.py
@@ -0,0 +1,1 @@
+from .getHighest import sumData
---
+++ packages/nestedDive/nestedDive-0.3.2-py3-none-any.whl/nestedDive/getHighest.py
@@ -0,0 +1,87 @@
+
+# def sum(orignalData, layers):
+#     allData = {}
+#     for x in range(layers):
+#         for name, data in orignalData.items():
+#             if x == layers - 1:
+#                 allData[name] = data
+
+#             orignalData = data
+    
+#     print(allData)
+#     for name, data in allData.items():
+#         print(name)
+#         print(data)     
+                
+                
+#     return "werkt de return zoals gemoeten"
+from collections import OrderedDict
+
+def sumData(data, layers):
+    allData = {}
+
+    # Get the latest item
+    for testing in data:
+        for name, value in testing.items():
+            for name, value in value.items():
+                # I have the data here`
+                wijkData = []
+
+                for nameData, data in value.items():
+                    wijkData.append(data)
+                calculatedData = sum(wijkData)
+                allData.update({name : calculatedData})
+            pass
+        pass
+
+    print(allData)    
+    # Sort the array/dict
+    sorted_x = sorted(allData.items(), key=lambda x: x[1], reverse = True)
+    # Change
+    sorted_dict = dict(OrderedDict(sorted_x)) #Force to dictionary for mongodb
+
+    print(sorted_dict)
+
+testData = {
+    "Charlois": {
+            "Tarwewijk" : {
+                "test": {
+                    "Veiligheidsindex" : 73,
+                    "Veiligheidsindex -subjectief" : 100
+                }
+            },
+            "Carnisse" : {
+                "32": {
+                    "Veiligheidsindex" : 50,
+                    "Veiligheidsindex -subjectief" : 102
+                }
+            },
+            "ss" : {
+                "32": {
+                    "Veiligheidsindex" : 60,
+                    "Veiligheidsindex -subjectief" : 60
+                }
+            }
+    },
+    "testing": {
+            "wijk1" : {
+                "123123": {
+                    "Veiligheidsindex" : 73,
+                    "Veiligheidsindex -subjectief" : 50
+                }
+            },
+            "wijk2" : {
+                "123123123": {
+                    "Veiligheidsindex" : 50,
+                    "Veiligheidsindex -subjectief" : 100
+                }
+           },
+            "ss" : {
+                "1212": {
+                    "Veiligheidsindex" : 60,
+                    "Veiligheidsindex -subjectief" : 100
+                }
+            }
+    }
+}
+
