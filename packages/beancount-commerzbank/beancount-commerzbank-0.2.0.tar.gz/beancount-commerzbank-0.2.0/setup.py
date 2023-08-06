41cb845724a1984a55033aae16f2faa80f8410a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:37 +0000

    Add package beancount-commerzbank/0.2.0/beancount-commerzbank-0.2.0.tar.gz

---
+++ packages/beancount-commerzbank/beancount-commerzbank-0.2.0.tar.gz/beancount-commerzbank-0.2.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['beancount_commerzbank']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'beancount-commerzbank',
+    'version': '0.2.0',
+    'description': 'Beancount Importer for Commerzbank (DE) CSV exports',
+    'long_description': None,
+    'author': 'Siddhant Goel',
+    'author_email': 'me@sgoel.org',
+    'url': 'https://github.com/siddhantgoel/beancount-commerzbank',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
