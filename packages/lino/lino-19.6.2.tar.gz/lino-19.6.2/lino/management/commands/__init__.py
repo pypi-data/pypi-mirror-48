04d84d5b190ff579ac8c0d5757a5157833ada853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package lino/19.6.2/lino-19.6.2.tar.gz

---
+++ packages/lino/lino-19.6.2.tar.gz/lino-19.6.2/lino/management/commands/__init__.py
@@ -0,0 +1,19 @@
+"""
+.. toctree::
+
+.. autosummary::
+   :toctree:
+
+    diag
+    dump2py
+    initdb
+    prep
+    mergedata
+    monitor
+    run
+    show
+    linod
+    resetsequences
+    makemigdump
+
+"""
