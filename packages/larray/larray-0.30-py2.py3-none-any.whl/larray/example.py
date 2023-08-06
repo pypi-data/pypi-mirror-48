46365bedd66df2d369569fc55d9563c82fbd27e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:13 +0000

    Add package larray/0.30/larray-0.30-py2.py3-none-any.whl

---
+++ packages/larray/larray-0.30-py2.py3-none-any.whl/larray/example.py
@@ -0,0 +1,79 @@
+import os
+import larray as la
+
+
+EXAMPLE_FILES_DIR = os.path.dirname(__file__) + '/tests/data/'
+AVAILABLE_EXAMPLE_DATA = {
+    'demography': os.path.join(EXAMPLE_FILES_DIR, 'demography.h5')
+}
+AVAILABLE_EXAMPLE_FILES = os.listdir(EXAMPLE_FILES_DIR)
+
+
+def get_example_filepath(fname):
+    """Return absolute path to an example file if exist.
+
+    Parameters
+    ----------
+    fname : str
+        Filename of an existing example file.
+
+    Returns
+    -------
+    Filepath
+        Absolute filepath to an example file if exists.
+
+    Notes
+    -----
+    A ValueError is raised if the provided filename does not represent an existing example file.
+
+    Examples
+    --------
+    >>> fpath = get_example_filepath('examples.xlsx')
+    """
+    fpath = os.path.abspath(os.path.join(EXAMPLE_FILES_DIR, fname))
+    if not os.path.exists(fpath):
+        raise ValueError("Example file {} does not exist. "
+                         "Available example files are: {}".format(fname, AVAILABLE_EXAMPLE_FILES))
+    return fpath
+
+
+def load_example_data(name):
+    """Load arrays used in the tutorial so that all examples in it can be reproduced.
+
+    Parameters
+    ----------
+    name : str
+        Example data to load. Available example datasets are:
+
+        - demography
+
+    Returns
+    -------
+    Session
+        Session containing one or several arrays
+
+    Examples
+    --------
+    >>> demo = load_example_data('demography')
+    >>> demo.pop.info # doctest: +SKIP
+    26 x 3 x 121 x 2 x 2
+     time [26]: 1991 1992 1993 ... 2014 2015 2016
+     geo [3]: 'BruCap' 'Fla' 'Wal'
+     age [121]: 0 1 2 ... 118 119 120
+     sex [2]: 'M' 'F'
+     nat [2]: 'BE' 'FO'
+    >>> demo.qx.info # doctest: +SKIP
+    26 x 3 x 121 x 2 x 2
+     time [26]: 1991 1992 1993 ... 2014 2015 2016
+     geo [3]: 'BruCap' 'Fla' 'Wal'
+     age [121]: 0 1 2 ... 118 119 120
+     sex [2]: 'M' 'F'
+     nat [2]: 'BE' 'FO'
+    """
+    if name is None:
+        name = 'demography'
+    if not isinstance(name, str):
+        raise TypeError("Expected string for argument example_data")
+    if name not in AVAILABLE_EXAMPLE_DATA.keys():
+        raise ValueError("example_data must be chosen from list {}".format(list(AVAILABLE_EXAMPLE_DATA.keys())))
+    return la.Session(AVAILABLE_EXAMPLE_DATA[name])
---
+++ packages/larray/larray-0.30-py2.py3-none-any.whl/larray/tests/test_excel.py
@@ -0,0 +1,258 @@
+from __future__ import absolute_import, division, print_function
+
+import re
+
+import pytest
+import numpy as np
+
+from larray.tests.common import needs_xlwings
+from larray import ndtest, open_excel, aslarray, Axis
+from larray.inout import xw_excel
+
+
+@needs_xlwings
+class TestWorkbook(object):
+    def test_open_excel(self):
+        # not using context manager because we call .quit manually
+        wb1 = open_excel(visible=False)
+        app1 = wb1.app
+        wb1.close()
+        # anything using wb1 will fail
+        with pytest.raises(Exception):
+            wb1.sheet_names()
+        wb2 = open_excel(visible=False)
+        app2 = wb2.app
+        assert app1 == app2 == xw_excel.global_app
+        # this effectively close all workbooks but leaves the instance intact (this is probably due to us keeping a
+        # reference to it).
+        app1.quit()
+        # anything using wb2 will fail
+        with pytest.raises(Exception):
+            wb2.sheet_names()
+
+        # in any case, this should work
+        with open_excel(visible=False) as wb:
+            wb['test'] = 'content'
+
+    def test_repr(self):
+        with open_excel(visible=False) as wb:
+            assert re.match(r'<larray.inout.xw_excel.Workbook \[Book\d+\]>', repr(wb))
+
+    def test_getitem(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+            assert isinstance(sheet, xw_excel.Sheet)
+            # this might not be true on non-English locale
+            assert sheet.name == 'Sheet1'
+
+            # this might not work on non-English locale
+            sheet = wb['Sheet1']
+            assert isinstance(sheet, xw_excel.Sheet)
+            assert sheet.name == 'Sheet1'
+
+            with pytest.raises(KeyError) as e_info:
+                wb['this_sheet_does_not_exist']
+            assert e_info.value.args[0] == "Workbook has no sheet named this_sheet_does_not_exist"
+
+    def test_setitem(self):
+        with open_excel(visible=False) as wb:
+            # sheet did not exist, str value
+            wb['sheet1'] = 'sheet1 content'
+            wb['sheet2'] = 'sheet2 content'
+            assert wb.sheet_names() == ['sheet1', 'sheet2']
+
+            # sheet did exist, str value
+            wb['sheet2'] = 'sheet2 content v2'
+            assert wb.sheet_names() == ['sheet1', 'sheet2']
+            assert wb['sheet2']['A1'].value == 'sheet2 content v2'
+
+            # sheet did not exist, Sheet value
+            wb['sheet3'] = wb['sheet1']
+            assert wb.sheet_names() == ['sheet1', 'sheet2', 'sheet3']
+            assert wb['sheet3']['A1'].value == 'sheet1 content'
+
+            # sheet did exist, Sheet value
+            wb['sheet2'] = wb['sheet1']
+            assert wb.sheet_names() == ['sheet1', 'sheet2', 'sheet3']
+            assert wb['sheet2']['A1'].value == 'sheet1 content'
+
+            with open_excel(visible=False, app="new") as wb2:
+                assert wb.app != wb2.app
+                with pytest.raises(ValueError) as e_info:
+                    wb2['sheet1'] = wb['sheet1']
+                assert e_info.value.args[0] == "cannot copy a sheet from one instance of Excel to another"
+
+            # group key
+            arr = ndtest((3, 3))
+            for label in arr.b:
+                wb[label] = arr[label].dump()
+                assert arr[label].equals(wb[label].load())
+
+    def test_delitem(self):
+        with open_excel(visible=False) as wb:
+            wb['sheet1'] = 'sheet1 content'
+            wb['sheet2'] = 'sheet2 content'
+            del wb['sheet1']
+            assert wb.sheet_names() == ['sheet2']
+
+    def test_rename(self):
+        with open_excel(visible=False) as wb:
+            wb['sheet_name'] = 'sheet content'
+            wb['sheet_name'].name = 'renamed'
+            assert wb.sheet_names() == ['renamed']
+
+
+@needs_xlwings
+class TestSheet(object):
+    def test_get_and_set_item(self):
+        arr = ndtest((2, 3))
+
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+            # set a few values
+            sheet['A1'] = 1.5
+            sheet['A2'] = 2
+            sheet['A3'] = True
+            sheet['A4'] = 'toto'
+            # array without header
+            sheet['A5'] = arr
+            # array with header
+            sheet['A8'] = arr.dump()
+
+            # object array with a *numpy* NaN (dc2019 infamous 65535 bug)
+            obj_arr = ndtest((2, 3)).astype(object)
+            obj_arr['a0', 'b1'] = np.float64('nan')
+
+            assert type(obj_arr['a0', 'b1']) is np.float64
+
+            obj_arr_dump = obj_arr.dump()
+            # [['a\\b', 'b0', 'b1', 'b2'], ['a0', 0, nan, 2], ['a1', 3, 4, 5]]
+
+            # float and *not* np.float64, otherwise it gets converted to 65535 when written to Excel
+            assert type(obj_arr_dump[1][2]) is float
+
+            sheet['A12'] = obj_arr_dump
+
+            # read them back
+            assert sheet['A1'].value == 1.5
+            assert sheet['A2'].value == 2
+            assert sheet['A3'].value == True
+            assert sheet['A4'].value == 'toto'
+            # array without header
+            assert np.array_equal(sheet['A5:C6'].value, arr.data)
+            # array with header
+            assert sheet['A8:D10'].load().equals(arr)
+            assert sheet['A12:D14'].load().equals(obj_arr, nans_equal=True)
+
+    def test_asarray(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            arr1 = ndtest((2, 3))
+            # no header so that we have an uniform dtype for the whole sheet
+            sheet['A1'] = arr1
+            res1 = np.asarray(sheet)
+            assert np.array_equal(res1, arr1.data)
+            assert res1.dtype == arr1.dtype
+
+    def test_array_method(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            # normal test array
+            arr1 = ndtest((2, 3))
+            sheet['A1'] = arr1.dump()
+            res1 = sheet.array('B2:D3', 'A2:A3', 'B1:D1', names=['a', 'b'])
+            assert arr1.equals(res1)
+
+            # array with int labels
+            arr2 = ndtest('0..1;0..2')
+            sheet['A1'] = arr2.dump()
+            res2 = sheet.array('B2:D3', 'A2:A3', 'B1:D1')
+            # larray_equal passes even if the labels are floats...
+            assert arr2.equals(res2)
+            # so we check the dtype explicitly
+            assert res2.axes[0].labels.dtype == arr2.axes[0].labels.dtype
+            assert res2.axes[1].labels.dtype == arr2.axes[1].labels.dtype
+
+    def test_repr(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+            assert re.match(r'<larray.inout.xw_excel.Sheet \[Book\d+\]Sheet1>', repr(sheet))
+
+
+@needs_xlwings
+class TestRange(object):
+    def test_scalar_convert(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+            # set a few values
+            sheet['A1'] = 1
+            rng = sheet['A1']
+            assert int(rng) == 1
+            assert float(rng) == 1.0
+            assert rng.__index__() == 1
+
+            sheet['A2'] = 1.0
+            rng = sheet['A2']
+            assert int(rng) == 1
+            assert float(rng) == 1.0
+            # Excel stores everything as float so we cannot really make the difference between 1 and 1.0
+            assert rng.__index__() == 1
+
+            sheet['A3'] = 1.5
+            rng = sheet['A3']
+            assert int(rng) == 1
+            assert float(rng) == 1.5
+            with pytest.raises(TypeError) as e_info:
+                rng.__index__()
+            assert e_info.value.args[0] == "only integer scalars can be converted to a scalar index"
+
+    def test_asarray(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            arr1 = ndtest((2, 3))
+            # no header so that we have an uniform dtype for the whole sheet
+            sheet['A1'] = arr1
+            res1 = np.asarray(sheet['A1:C2'])
+            assert np.array_equal(res1, arr1.data)
+            assert res1.dtype == arr1.dtype
+
+    def test_aslarray(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            arr1 = ndtest([Axis(2), Axis(3)])
+            # no header so that we have an uniform dtype for the whole sheet
+            sheet['A1'] = arr1
+            res1 = aslarray(sheet['A1:C2'])
+            assert res1.equals(arr1)
+            assert res1.dtype == arr1.dtype
+
+    # this tests Range.__getattr__ with an LArray attribute
+    def test_aggregate(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            arr1 = ndtest((2, 3))
+            # no header so that we have an uniform dtype for the whole sheet
+            sheet['A1'] = arr1
+            res = sheet['A1:C2'].sum()
+            assert res == 15
+
+    def test_repr(self):
+        with open_excel(visible=False) as wb:
+            sheet = wb[0]
+
+            arr1 = ndtest((2, 3))
+            sheet['A1'] = arr1
+            res = repr(sheet['A1:C2'])
+            assert res == """\
+{0}*\\{1}*  0  1  2
+        0  0  1  2
+        1  3  4  5"""
+
+
+if __name__ == "__main__":
+    pytest.main()
