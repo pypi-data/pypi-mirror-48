26870e2d0ebf59feb8bfb2a675ebbc619f47f217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:19 +0000

    Add package pymedphys-labs/0.10.0/pymedphys_labs-0.10.0-py3-none-any.whl

---
+++ packages/pymedphys-labs/pymedphys_labs-0.10.0-py3-none-any.whl/pymedphys_labs/_install_requires.py
@@ -0,0 +1,10 @@
+install_requires = [
+    "Pillow",
+    "matplotlib",
+    "numpy >= 1.12",
+    "pydicom",
+    "pymedphys_utilities >= 0.10.0, < 0.11.0",
+    "scipy",
+    "tqdm",
+    "xarray"
+]
