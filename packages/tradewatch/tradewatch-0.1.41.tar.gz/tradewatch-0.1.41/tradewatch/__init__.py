dfb01ecd1d2a759233c691476a5b952921e22fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:13 +0000

    Add package tradewatch/0.1.41/tradewatch-0.1.41.tar.gz

---
+++ packages/tradewatch/tradewatch-0.1.41.tar.gz/tradewatch-0.1.41/tradewatch/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys, os
+import hy,selectolax.parser	
+from tradewatch.allegro import *
+from tradewatch.tradewatch import *
+from tradewatch.google import *
+from tradewatch.name import *
