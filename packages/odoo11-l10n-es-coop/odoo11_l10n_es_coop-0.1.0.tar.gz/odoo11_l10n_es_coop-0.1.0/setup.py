b6be849aa00f242b16d0af062dd19b4964e489b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:26 +0000

    Add package odoo11-l10n-es-coop/0.1.0/odoo11_l10n_es_coop-0.1.0.tar.gz

---
+++ packages/odoo11-l10n-es-coop/odoo11_l10n_es_coop-0.1.0.tar.gz/odoo11_l10n_es_coop-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="odoo11_l10n_es_coop",
+    version="0.1.0",
+    author="Coopdevs,Acysos",
+    url="https://gitlab.com/coopdevs/odoo11-l10n-es-coop",
+    description="Odoo Spanish Localization for Cooperatives",
+    license="GPL",
+    packages=['odoo/addons/l10n_es_coop'],
+    classifiers=[],
+)
