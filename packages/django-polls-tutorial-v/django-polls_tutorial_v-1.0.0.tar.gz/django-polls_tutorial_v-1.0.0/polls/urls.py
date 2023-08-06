8376d1ca79c0d13992e135bd169208facc80fbd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:56 +0000

    Add package django-polls-tutorial-v/1.0.0/django-polls_tutorial_v-1.0.0.tar.gz

---
+++ packages/django-polls-tutorial-v/django-polls_tutorial_v-1.0.0.tar.gz/django-polls_tutorial_v-1.0.0/polls/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'polls'
+
+urlpatterns = [
+	
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
+	
+    #url(r'^$', views.index, name='indexv2'), 
+    #url(r'^$', views.IndexView.as_view(), name='indexviews'),
+
+	url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
+]
+
