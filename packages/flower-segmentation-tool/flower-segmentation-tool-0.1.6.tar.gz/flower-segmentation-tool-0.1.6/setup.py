e78a9934014c6af3aeabc6163968eb458885e166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:51 +0000

    Add package flower-segmentation-tool/0.1.6/flower-segmentation-tool-0.1.6.tar.gz

---
+++ packages/flower-segmentation-tool/flower-segmentation-tool-0.1.6.tar.gz/flower-segmentation-tool-0.1.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flower-segmentation-tool",
+    version="0.1.6",
+    author="Anna Tenberg",
+    author_email="Anna.Tenberg@saturn.uni-freiburg.de",
+    description="A tool to segment yellow arnica flowers in rgb images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnnaTe/arnica",
+    packages=setuptools.find_packages(exclude=['tests*', 'images*']),
+    python_requires='>=3.5',
+    install_requires=[
+        'PyQt5-sip',
+        'PyQt5',
+        'numpy',
+        'opencv-python',
+        'matplotlib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Science/Research",
+    ],
+)
