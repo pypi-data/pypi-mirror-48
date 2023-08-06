96edbce69ca86a4b84c89681695b5d48f366bb9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:05 +0000

    Add package monobank/0.2.0/monobank-0.2.0.tar.gz

---
+++ packages/monobank/monobank-0.2.0.tar.gz/monobank-0.2.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['monobank']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.22,<3.0']
+
+setup_kwargs = {
+    'name': 'monobank',
+    'version': '0.2.0',
+    'description': 'Monobank.ua API implementation',
+    'long_description': None,
+    'author': 'Vitaliy Kucheriavyi',
+    'author_email': 'ppr.vitaly@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
