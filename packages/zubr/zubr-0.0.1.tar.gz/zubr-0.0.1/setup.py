c0474c9b5114304dd2cc6d028093ef30946ab8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:58 +0000

    Add package zubr/0.0.1/zubr-0.0.1.tar.gz

---
+++ packages/zubr/zubr-0.0.1.tar.gz/zubr-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name="zubr",
+    version="0.0.1",
+    author="ZUBR Exchange",
+    author_email="developer@zubr.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
