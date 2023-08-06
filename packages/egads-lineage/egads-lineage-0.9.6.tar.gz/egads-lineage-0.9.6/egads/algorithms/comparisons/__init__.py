299c51840a09187143aaa440cc830c417d65eaf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:55 +0000

    Add package egads-lineage/0.9.6/egads-lineage-0.9.6.tar.gz

---
+++ packages/egads-lineage/egads-lineage-0.9.6.tar.gz/egads-lineage-0.9.6/egads/algorithms/comparisons/__init__.py
@@ -0,0 +1,14 @@
+"""
+EGADS comparisons algorithms. See EGADS Algorithm Documentation for more info.
+"""
+
+__author__ = "ohenry"
+__date__ = "2018-03-05 11:13"
+__version__ = "1.1"
+
+import logging
+try:
+    from .compare_param_lcss import *
+    logging.info('egads [comparisons] algorithms have been loaded')
+except Exception:
+    logging.exception('an error occurred during the loading of a [comparisons] algorithm')
---
+++ packages/egads-lineage/egads-lineage-0.9.6.tar.gz/egads-lineage-0.9.6/egads/algorithms/radiation/__init__.py
@@ -0,0 +1,20 @@
+"""
+EGADS radiation algorithms. See EGADS Algorithm Documentation for more info.
+"""
+
+__author__ = "ohenry"
+__date__ = "2018-03-05 11:13"
+__version__ = "1.1"
+
+import logging
+try:
+    from .camera_viewing_angles import *
+    from .planck_emission import *
+    from .rotate_solar_vector_to_aircraft_frame import *
+    from .scattering_angles import *
+    from .solar_vector_blanco import *
+    from .solar_vector_reda import *
+    from .temp_blackbody import *
+    logging.info('egads [radiation] algorithms have been loaded')
+except Exception:
+    logging.exception('an error occurred during the loading of a [radiation] algorithm')
