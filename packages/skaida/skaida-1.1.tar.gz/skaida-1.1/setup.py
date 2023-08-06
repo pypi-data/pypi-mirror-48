ea4e9fc512e75c57733790379184332a974c87f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:53 +0000

    Add package skaida/1.1/skaida-1.1.tar.gz

---
+++ packages/skaida/skaida-1.1.tar.gz/skaida-1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='skaida',
+    version='1.1',
+    author='Shu Kougetsu',
+    author_email='zefuirusu@qq.com',
+    url='https://user.qzone.qq.com/2078766287/infocenter',
+    description=u"The name comes from AIDA in Marvel's Agents of SHIELD, short for Artificial Intelligence Digital Assistant.",
+    packages=['skaida'],
+    install_requires=['numpy>=1.16.2','pandas>=0.24.2','python-docx>=0.8.10','xlrd>=1.2.0','xlwt>=1.3.0','openpyxl>=2.6.1','selenium>=3.141.0'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'catatest=skaida:catatest',
+            'samprepare=skaida:samprepare',
+            'projpp=skaida:projpp',
+            'simplegoo=skaida:simplegoo',
+            'opsite=skaida:opsite',
+            'findfile=skaida:findfile'
+            
+        
+        ]
+    }
+)
