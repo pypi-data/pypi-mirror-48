4f9fc89afc3d4304e729317d5f97d30cf31ff35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:39 +0000

    Add package distributions-LelandCurtis2/1.3/distributions_LelandCurtis2-1.3.tar.gz

---
+++ packages/distributions-LelandCurtis2/distributions_LelandCurtis2-1.3.tar.gz/distributions_LelandCurtis2-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_LelandCurtis2',
+      version='1.3',
+      description='Gaussian distributions',
+      packages=['distributions_LelandCurtis2'],
+      author = 'Leland Curtis',
+      author_email = 'lelandcurtis88@gmail.com',      
+      zip_safe=False)
