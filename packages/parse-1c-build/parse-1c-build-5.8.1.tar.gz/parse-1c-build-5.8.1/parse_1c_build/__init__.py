af1b54e9ee4063fb0cd4889dbcb394123bd5d4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:53 +0000

    Add package parse-1c-build/5.8.1/parse-1c-build-5.8.1.tar.gz

---
+++ packages/parse-1c-build/parse-1c-build-5.8.1.tar.gz/parse-1c-build-5.8.1/parse_1c_build/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import logging
+
+# noinspection PyUnresolvedReferences
+from parse_1c_build.__about__ import __version__
+# noinspection PyUnresolvedReferences
+from parse_1c_build.build import Builder
+# noinspection PyUnresolvedReferences
+from parse_1c_build.parse import Parser
+
+# noinspection PyUnresolvedReferences
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
