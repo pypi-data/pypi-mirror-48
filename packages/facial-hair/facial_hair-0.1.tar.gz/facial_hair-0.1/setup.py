1792a841ca6aa7d25f8df5629861864162278162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:26 +0000

    Add package facial-hair/0.1/facial_hair-0.1.tar.gz

---
+++ packages/facial-hair/facial_hair-0.1.tar.gz/facial_hair-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+    name='facial_hair',    # This is the name of your PyPI-package.
+    version='0.1',          # Update the version number for new releases
+    scripts=['facial_hair.py']       # The name of your scipt, and also the command you'll be using for calling it
+)
