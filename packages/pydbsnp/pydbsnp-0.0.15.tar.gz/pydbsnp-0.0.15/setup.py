02098603ec9e70492db2371f581f0d9cce4524c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package pydbsnp/0.0.15/pydbsnp-0.0.15.tar.gz

---
+++ packages/pydbsnp/pydbsnp-0.0.15.tar.gz/pydbsnp-0.0.15/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pydbsnp',
+    version='0.0.15',
+    author='Anthony Aylward',
+    author_email='aaylward@eng.ucsd.edu',
+    description='Interface with dbSNP VCF data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/anthony-aylward/pydbsnp.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    install_requires=['pysam'],
+    entry_points={
+        'console_scripts': [
+            'pydbsnp-download=pydbsnp.download:main',
+            'pydbsnp-index=pydbsnp.index:main',
+            'pydbsnp-query=pydbsnp.query:main'
+        ]
+    },
+    include_package_data=True
+)
