3e10f37bcce139111b8d9d3bcf502e87e9f9e354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:16 +0000

    Add package bcml/1.22/bcml-1.22.tar.gz

---
+++ packages/bcml/bcml-1.22.tar.gz/bcml-1.22/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open("docs/README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name='bcml',
+    version='1.22',
+    author='NiceneNerd',
+    author_email='macadamiadaze@gmail.com',
+    description='A mod manager for The Legend of Zelda: Breath of the Wild on Cemu',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/NiceneNerd/BCML',
+    include_package_data=True,
+    packages=['bcml'],
+    entry_points={
+        'console_scripts': [
+            'bcml = bcml.__init__:main'
+        ],
+        'gui_scripts': [
+            'bcml-gui = bcml.gui:main'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pyYaml',
+        'sarc',
+        'rstb',
+        'wszst_yaz0',
+        'wxPython',
+        'xxhash'
+    ]
+)
