9d6f5173140f9526f165438cf42a20093866724b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package cazoo-logger/0.0.0/cazoo_logger-0.0.0.tar.gz

---
+++ packages/cazoo-logger/cazoo_logger-0.0.0.tar.gz/cazoo_logger-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="cazoo_logger",
+    author="Bob Gregory",
+    author_email="bob.gregory@cazoo.co.uk",
+    description="*Super* opinionated structured logger",
+    packages=["cazoo_logger"],
+)
