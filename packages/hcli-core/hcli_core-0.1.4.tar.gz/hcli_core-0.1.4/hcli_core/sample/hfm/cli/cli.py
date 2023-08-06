10fbcaac863c4f7bb8e238564261f569a9d01161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:09 +0000

    Add package hcli-core/0.1.4/hcli_core-0.1.4.tar.gz

---
+++ packages/hcli-core/hcli_core-0.1.4.tar.gz/hcli_core-0.1.4/hcli_core/sample/hfm/cli/cli.py
@@ -0,0 +1,32 @@
+import json
+import io
+from functools import partial
+
+class CLI:
+    commands = None
+    inputstream = None
+    
+    def __init__(self, commands, inputstream):
+        self.commands = commands
+        self.inputstream = inputstream
+
+    def execute(self):
+
+        if self.inputstream != None and self.commands[2] == '-l':
+            self.upload()
+            return None
+
+        if self.inputstream == None and self.commands[2] == '-r':
+            return self.download()
+
+    def upload(self):
+        unquoted = self.commands[3].replace("'", "").replace("\"", "")
+        with io.open(unquoted, 'wb') as f:
+            for chunk in iter(partial(self.inputstream.read, 16384), b''):
+                f.write(chunk)
+
+        return None
+
+    def download(self):
+        f = open(self.commands[3].replace("'", "").replace("\"", ""), "rb")
+        return io.BytesIO(f.read())
