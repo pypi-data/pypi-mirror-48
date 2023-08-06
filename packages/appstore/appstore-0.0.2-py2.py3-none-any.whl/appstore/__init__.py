874ee5d78b4d758974e564bf8baa999cbe7896d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:10 +0000

    Add package appstore/0.0.2/appstore-0.0.2-py2.py3-none-any.whl

---
+++ packages/appstore/appstore-0.0.2-py2.py3-none-any.whl/appstore/__init__.py
@@ -0,0 +1,14 @@
+"""
+App Store API Reference
+=======================
+
+The :doc:`tutorial` provides a helpful walkthrough of the app store.
+
+"""
+
+__title__ = 'App Store'
+__version__ = '0.0.2'
+__build__ = 0x000002
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = '2019, Grant Jenks'
