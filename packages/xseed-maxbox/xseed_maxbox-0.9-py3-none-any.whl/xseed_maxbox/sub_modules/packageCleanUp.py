a00df4a7f5a62c42686fe2ba5120a640a1c94e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:14 +0000

    Add package xseed-maxbox/0.9/xseed_maxbox-0.9-py3-none-any.whl

---
+++ packages/xseed-maxbox/xseed_maxbox-0.9-py3-none-any.whl/xseed_maxbox/sub_modules/packageCleanUp.py
@@ -0,0 +1,41 @@
+  # Package Removal
+
+import os
+import subprocess as sp
+
+def removePackages(x,y):
+    try:
+        null = open("log.txt", "a")
+        print('Removing package %s ' % x)
+        os.system('%s' % y)
+        print("Package %s removed" % x)
+        null.close()
+
+    except OSError:
+        print (e.message)
+        print ("Exiting the process because of above error")
+        exit();
+
+def packageCleanUp ():
+    packages = {
+       "aisleriot": "sudo apt-get remove aisleriot -y",
+       "Browser": "sudo apt purge webbrowser-app && sudo apt autoremove --purge -y",
+       "Libreoffice": "sudo apt-get autoremove -y libreoffice-\*",
+       "Sudoku": "sudo apt-get remove sudoku -y",
+       "Thunderbird": "sudo apt-get purge thunderbird* -y",
+       "Transmission": "sudo apt-get purge transmission* -y && sudo apt-get autoremove -"
+    }
+    for x, y in packages.items():
+        removePackages(x,y)
+
+def bashAndBrowserCleanUp():
+    # Clear bash history
+    os.system('sudo sh -c \'echo "" > .bash_history \'')
+
+    # Clear browser history
+    os.system('sudo rm ~/.config/google-chrome/Default/')
+    os.system('sudo rm ~/.cache/google-chrome')
+
+def cleanUp():
+    packageCleanUp()
+    bashAndBrowserCleanUp()
