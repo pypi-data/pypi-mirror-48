3df1464f2cc3341daa5bbc2d1d169e2c8e61f7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:51 +0000

    Add package dscleaner/0.1.8.post2/dscleaner-0.1.8.post2-py3-none-any.whl

---
+++ packages/dscleaner/dscleaner-0.1.8.post2-py3-none-any.whl/dscleaner/__init__.py
@@ -0,0 +1,6 @@
+from .ifileinfo import IFileInfo
+from .fileinfo import FileInfo
+from .fileutil import FileUtil
+from .filewriter import FileWriter
+from .csvfileinfo import CsvFileInfo
+from .merger import Merger
