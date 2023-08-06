8a50fe04d37bf6cf2340545fa04c6a78044ecec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:38 +0000

    Add package Jimpass/0.0.3/Jimpass-0.0.3.tar.gz

---
+++ packages/Jimpass/Jimpass-0.0.3.tar.gz/Jimpass-0.0.3/tests/test_parser.py
@@ -0,0 +1,38 @@
+from jimpass.parser import Parser
+
+template_str = "account <b>{name}</b> with user {username}"
+mapping = {
+    'name': 'name',
+    'username': 'login.username'
+}
+
+parser = Parser(
+    template_str,
+    mapping
+)
+
+dummy_item = {
+    "name": "Bob",
+    "login": {
+        "username": "sponge.bob"
+    }
+}
+dummy_str = "account <b>Bob</b> with user sponge.bob"
+
+dummy_incomplete_item = {
+    "name": "Bob"
+}
+dummy_incomplete_str = "account <b>Bob</b> with user ?"  # intentionally missing
+
+
+def test_dump():
+    assert(parser.dumps(dummy_item) == dummy_str)
+
+
+def test_load():
+    loaded_item = parser.loads(dummy_str)
+    assert('name' in loaded_item and 'login' in loaded_item)
+    assert(loaded_item['name'] == dummy_item['name'])
+    assert(loaded_item['login']['username'] == dummy_item['login']['username'])
+    loaded_incomplete_item = parser.loads(dummy_incomplete_str)
+    assert(loaded_incomplete_item['name'] == dummy_incomplete_item['name'])
