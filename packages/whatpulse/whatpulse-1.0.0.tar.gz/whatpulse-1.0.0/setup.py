a3de4b33c1e0167e4d7130972bb16fcb3caed032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:46 +0000

    Add package whatpulse/1.0.0/whatpulse-1.0.0.tar.gz

---
+++ packages/whatpulse/whatpulse-1.0.0.tar.gz/whatpulse-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='whatpulse',
+      version='1.0.0',
+      description='Object Oriented WhatPulse API Wrapper',
+      long_description='A python object oriented WhatPulse API wrapper.',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://github.com/sl4vkek/python-whatpulse',
+      packages=['whatpulse'],
+      install_requires=['requests'],
+      license="WTFPL"
+)
