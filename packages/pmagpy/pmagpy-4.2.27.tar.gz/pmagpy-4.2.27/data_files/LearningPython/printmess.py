492ea2ffcfa49d85cc628efed48b483caef5c651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package pmagpy/4.2.27/pmagpy-4.2.27.tar.gz

---
+++ packages/pmagpy/pmagpy-4.2.27.tar.gz/pmagpy-4.2.27/data_files/LearningPython/printmess.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# simple Python test program (printmess.py)
+from __future__ import print_function
+print('test message')
---
+++ packages/pmagpy/pmagpy-4.2.27.tar.gz/pmagpy-4.2.27/data_files/LearningPython/printmess2.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# simple Python test program 2 (printmess2.py)
+from __future__ import print_function
+print("The pump don't work 'cuz the vandals took the handles")
+print('She said "I know what it\'s like to be dead"')
