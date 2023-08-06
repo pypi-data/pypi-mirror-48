3e453fdeee1b398c74e2a393971fedc7f38648c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:37 +0000

    Add package odoo-env/0.8.18/odoo-env-0.8.18.tar.gz

---
+++ packages/odoo-env/odoo-env-0.8.18.tar.gz/odoo-env-0.8.18/odoo_env/options.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from odoo_env.messages import Msg
+
+
+def get_param(args, param):
+    if param == 'client':
+        if args.client:
+            return args.client[0]
+        else:
+            Msg().err('Need -c option (client name). Process aborted')
+
+    if param == 'database':
+        if args.database:
+            return args.database[0]
+        else:
+            Msg().err('Need -d option (database name). Process aborted')
+
+    if param == 'module':
+        if args.module:
+            return args.module
+        else:
+            Msg().err('Need -m option (module(s) name). Process aborted')
+
+    if param == 'backup_file':
+        if args.backup_file:
+            return args.backup_file[0]
+        else:
+            Msg().err('Need -f option (backup file). Process aborted')
