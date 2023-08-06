004eff40dd3dc3083f0202d55ab2328dd545a8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:00 +0000

    Add package aiompesa/0.0.4/aiompesa-0.0.4.tar.gz

---
+++ packages/aiompesa/aiompesa-0.0.4.tar.gz/aiompesa-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aiompesa",
+    version="0.0.4",
+    description="A thin async wrapper of MPESA daraja API.",
+    url="http://github.com/musale/aiompesa",
+    author="Martin Musale",
+    author_email="martinmshale@gmail.com",
+    license="MIT",
+    zip_safe=False,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
