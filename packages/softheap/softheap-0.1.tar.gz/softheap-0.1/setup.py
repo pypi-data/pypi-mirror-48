ddb196785564b087d6e5e7a018cf908005f798c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:24 +0000

    Add package softheap/0.1/softheap-0.1.tar.gz

---
+++ packages/softheap/softheap-0.1.tar.gz/softheap-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='softheap',
+    version='0.1',
+    url='https://github.com/alex-michael17/softheap',
+    author="Alex Michael",
+    author_email="mr17state@gmail.com",
+    description="Python Soft Heap Implementation",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+ )
