a59e5ba2543cdb70be7129dcd2d8a84bb5184f93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:20 +0000

    Add package aisc/0.1.0/aisc-0.1.0.tar.gz

---
+++ packages/aisc/aisc-0.1.0.tar.gz/aisc-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['aisc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.22,<3.0']
+
+setup_kwargs = {
+    'name': 'aisc',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'MB',
+    'author_email': 'mb@m1k.pw',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
