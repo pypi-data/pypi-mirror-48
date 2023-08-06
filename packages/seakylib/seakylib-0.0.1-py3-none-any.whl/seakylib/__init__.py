007b4bfb52a182cec3c325343a97e11886d7dca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:25 +0000

    Add package seakylib/0.0.1/seakylib-0.0.1-py3-none-any.whl

---
+++ packages/seakylib/seakylib-0.0.1-py3-none-any.whl/seakylib/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Seaky
+# @Date:   2019/6/25 15:12
+
+
+__all__ = ['test']
+__title__ = 'seakylib'
+__author__ = 'Seaky'
+__version__ = '0.0.1'
+__copyright__ = 'Copyright (c) 2019 Seaky'
