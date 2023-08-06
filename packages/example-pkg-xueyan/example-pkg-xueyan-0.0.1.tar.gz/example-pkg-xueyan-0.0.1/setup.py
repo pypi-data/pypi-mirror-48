e441da154aea0f8ea2c4e7760f6e42ba925b4281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:41:50 +0000

    Add package example-pkg-xueyan/0.0.1/example-pkg-xueyan-0.0.1.tar.gz

---
+++ packages/example-pkg-xueyan/example-pkg-xueyan-0.0.1.tar.gz/example-pkg-xueyan-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+
+setuptools. setup(
+    name='example-pkg-xueyan',
+    version='0.0.1',
+    author='xueyan',
+    author_email='xueyan0407@outlook.com',
+    description='A small example package',
+    long_description='#A long desc',
+    long_descripthin_content_type='text/markdown',
+    url='',
+    packages=setuptools.find_packages(),
+    classifiers=[],
+)
