7ac65f982a8945ce8e4a5d85fa138c2d6d2132e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:22 +0000

    Add package overly/0.1.8/overly-0.1.8.tar.gz

---
+++ packages/overly/overly-0.1.8.tar.gz/overly-0.1.8/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from sys import version_info
+
+
+setup(
+    name='overly',
+    description='overly - http client testing for hoomans',
+    long_description='overly is for testing your http client, from the balan to the bananas.',
+    license='MIT',
+    version='0.1.8',
+    python_requires=">=3.7",
+    author='Mark Jameson - aka theelous3',
+    url='https://github.com/theelous3/overly',
+    packages=['overly'],
+    include_package_data=True,
+    install_requires=['h11', 'sansio_multipart'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
