0fb7ade65ddd56963c806bf63fc82b2d6018f433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:43 +0000

    Add package propargs/0.0.8/propargs-0.0.8.tar.gz

---
+++ packages/propargs/propargs-0.0.8.tar.gz/propargs-0.0.8/propargs/constants.py
@@ -0,0 +1,27 @@
+
+SWITCH = '-'
+
+OS = "OS"
+
+PROPS_DIR = 'props_dir'
+
+UTYPE = "user_type"
+# user types (DUMMY type is only for automated test)
+TERMINAL = "terminal"
+IPYTHON = "iPython"
+IPYTHON_NB = "iPython Notebook"
+WEB = "Web browser"
+DUMMY = "dummy"
+
+VALUE = "val"
+QUESTION = "question"
+ATYPE = "atype"
+HIVAL = "hival"
+LOWVAL = "lowval"
+
+BOOL = 'BOOL'
+INT = 'INT'
+FLT = 'DBL'
+STR = 'STR'
+CMPLX = 'CMPLX'
+type_dict = {BOOL: bool, INT: int, FLT: float, CMPLX: complex, STR: str}
