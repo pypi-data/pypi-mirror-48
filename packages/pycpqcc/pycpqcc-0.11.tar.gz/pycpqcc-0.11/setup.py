393c7ab223782e3fc8a9d83181f6f19282a15adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:58 +0000

    Add package pycpqcc/0.11/pycpqcc-0.11.tar.gz

---
+++ packages/pycpqcc/pycpqcc-0.11.tar.gz/pycpqcc-0.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pycpqcc',
+    version='0.11',
+    description='Python package for CPQCC',
+    author='Daniel Helkey',
+    author_email='dhelkey@stanford.edu',
+    license='MIT',
+    packages=['pycpqcc'],
+    zip_safe = False,
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose'])
