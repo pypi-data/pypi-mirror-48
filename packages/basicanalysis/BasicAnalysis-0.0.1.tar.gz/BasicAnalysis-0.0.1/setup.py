86cb624f5334cff7bd3160cfcc6b422c9bb75057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:18 +0000

    Add package basicanalysis/0.0.1/BasicAnalysis-0.0.1.tar.gz

---
+++ packages/basicanalysis/BasicAnalysis-0.0.1.tar.gz/BasicAnalysis-0.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(
+    name='BasicAnalysis',
+    version='0.0.1',
+    description='It is a fast tool to see the best supervised learning method for your dataset',
+    license='MIT',
+    packages=['BasicAnalysis'],
+    author='Niral J Shah',
+    author_email='niraljshah@outlook.com',
+    keywords=['Basic', 'Analysis', 'Supervised', 'Learning', 'Metrics']
+)
+
