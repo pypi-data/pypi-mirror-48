65be626aac451f63a51795f8ca5614cc8aed96b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:58 +0000

    Add package concepts/0.9/concepts-0.9.zip

---
+++ packages/concepts/concepts-0.9.zip/concepts-0.9/tests/test_tools.py
@@ -0,0 +1,31 @@
+# test_tools.py
+
+import pytest
+
+from concepts import tools
+
+
+def test_maximal():
+    items = map(frozenset, ((1,), (2,), (3,), (1, 2)))
+    assert set(tools.maximal(items)) == {frozenset([3]), frozenset([1, 2])}
+
+
+def test_dump_json_invalid_path():
+    with pytest.raises(TypeError, match=r'path_or_fileobj'):
+        tools.dump_json({}, object())
+
+
+def test_load_json_invalid_path():
+    with pytest.raises(TypeError, match=r'path_or_fileobj'):
+        tools.load_json(object())
+
+
+def test_dump_load(path_or_fileobj, encoding, obj={u'sp\xe4m': 'eggs'}):
+    is_fileobj = hasattr(path_or_fileobj, 'seek')
+    kwargs = {'encoding': encoding} if encoding is not None else {}
+
+    tools.dump_json(obj, path_or_fileobj, **kwargs)
+    if is_fileobj:
+        path_or_fileobj.seek(0)
+
+    assert tools.load_json(path_or_fileobj, **kwargs) == obj
