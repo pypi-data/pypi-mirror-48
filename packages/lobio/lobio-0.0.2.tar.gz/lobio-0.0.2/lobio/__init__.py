898e7120761016c2b3e15e13ee44bd67a36f25f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:10 +0000

    Add package lobio/0.0.2/lobio-0.0.2.tar.gz

---
+++ packages/lobio/lobio-0.0.2.tar.gz/lobio-0.0.2/lobio/__init__.py
@@ -0,0 +1,5 @@
+from ._version import __version__, __title__, __author__, __homepage__, __repo__
+
+from lobio.models import ImmutableSeqRecord, ImmutableSeqFeature, ImmutableException
+from . import SeqUtils
+from . import SeqIOUtils
