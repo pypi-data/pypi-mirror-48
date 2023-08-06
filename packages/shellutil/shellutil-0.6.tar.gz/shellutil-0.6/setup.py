29eb71a3975a6667639160d309b7ba398826aa17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:06 +0000

    Add package shellutil/0.6/shellutil-0.6.tar.gz

---
+++ packages/shellutil/shellutil-0.6.tar.gz/shellutil-0.6/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='shellutil',
+      version='0.6',
+      description='A shell',
+      url='https://github.com/ryank262/pyBuild',
+      author='Ryan Kirkpatrick',
+      author_email='ryan.Kirkpatrick2@resmed.com',
+      classifiers=['Programming Language :: Python :: 3.3'],
+      license='MIT',
+      packages=['shellutil'],
+      install_requires=['future', 'six'],
+      zip_safe=False)
