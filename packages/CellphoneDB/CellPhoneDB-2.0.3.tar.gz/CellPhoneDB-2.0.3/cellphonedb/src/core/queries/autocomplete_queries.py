c4ceaf5719cbed6db5c2bda1090abc5ded20604d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:45 +0000

    Add package CellphoneDB/2.0.3/CellPhoneDB-2.0.3.tar.gz

---
+++ packages/CellphoneDB/CellPhoneDB-2.0.3.tar.gz/CellPhoneDB-2.0.3/cellphonedb/src/core/queries/autocomplete_queries.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import re
+
+
+def autocomplete_query(genes: pd.DataFrame, multidatas: pd.DataFrame, partial_element: pd.DataFrame) -> pd.DataFrame:
+    values = genes[genes['ensembl'].str.contains(partial_element, flags=re.IGNORECASE)]['ensembl']
+    values = values.append(
+        genes[genes['protein_name'].str.contains(partial_element, flags=re.IGNORECASE)]['protein_name'],
+        ignore_index=True)
+    values = values.append(
+        genes[genes['gene_name'].str.contains(partial_element, flags=re.IGNORECASE)]['gene_name'],
+        ignore_index=True)
+    values = values.append(
+        genes[genes['hgnc_symbol'].str.contains(partial_element, flags=re.IGNORECASE)]['hgnc_symbol'],
+        ignore_index=True)
+    values = values.append(
+        multidatas[multidatas['name'].str.contains(partial_element, flags=re.IGNORECASE)]['name'],
+        ignore_index=True)
+    result = pd.DataFrame(data=values, columns=['value'])
+
+    return result
