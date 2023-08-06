43c1275f47277878e010cd8509237f22e0623bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:11 +0000

    Add package rocket-vocab/0.0.2/rocket-vocab-0.0.2.tar.gz

---
+++ packages/rocket-vocab/rocket-vocab-0.0.2.tar.gz/rocket-vocab-0.0.2/README.md
@@ -0,0 +1,10 @@
+# rocket-vocab
+terminal package for learning vocabulary
+
+## TODO://
+
+0. Intergation with FireBase
+1. Make package on Pypi
+2. Classify by know-point
+3. Random by classify
+4. More color with `TrueColor`
---
+++ packages/rocket-vocab/rocket-vocab-0.0.2.tar.gz/rocket-vocab-0.0.2/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#!-*-coding:utf8-*-
+"""File setup.py"""
+from setuptools import setup, find_packages
+import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+setup(
+    name='rocket-vocab',
+    version='0.0.2',
+    packages=find_packages(
+        '.',
+        exclude=[
+            '*.tests', '*.tests.*', 'tests.*', 'tests'
+        ]
+    ),
+    license='MIT',
+    description='learing vocabulary on terminal',
+    author='huongnhd',
+    author_email = 'huong.nhdh@gmail.com',
+    url = 'https://github.com/huongnhdh/rocket-vocab',
+    download_url = 'https://github.com/huongnhdh/rocket-vocab/archive/v001.tar.gz',
+    keywords = ['vocab', 'terminal', 'true color'],
+    install_requires=[
+    ],
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Education',
+    'Topic :: System :: System Shells',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+    entry_points="""
+    [console_scripts]
+    rocket=app.rocket:main
+    """
+)
