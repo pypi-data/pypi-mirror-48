bf9b9b090dcaf25e0893d47dbb832c8fba654683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:43 +0000

    Add package enable/4.8.0/enable-4.8.0.tar.gz

---
+++ packages/enable/enable-4.8.0.tar.gz/enable-4.8.0/enable/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM ENABLE SETUP.PY
+version = '4.8.0'
+full_version = '4.8.0'
+git_revision = 'd6ebe4f'
+is_released = True
+
+if not is_released:
+    version = full_version
