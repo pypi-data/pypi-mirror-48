df5ac69d179702c984b3e17ad1c4ea921a6148e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:01 +0000

    Add package lstar/0.2.1/lstar-0.2.1.tar.gz

---
+++ packages/lstar/lstar-0.2.1.tar.gz/lstar-0.2.1/lstar/learn.py
@@ -0,0 +1,36 @@
+import funcy as fn
+from dfa import DFA
+
+from lstar.classification_tree import ClassificationTree
+
+
+def learn_dfa(alphabet, membership, find_counter_example,
+              lazy=False, outputs=None) -> DFA:
+    hypotheses = _learn_dfa(
+        alphabet, membership, find_counter_example, lazy, outputs
+    )
+    return fn.last(hypotheses)
+
+
+def _learn_dfa(alphabet, membership, find_counter_example,
+               lazy=False, outputs=None):
+    tree = ClassificationTree(
+        alphabet=alphabet,
+        labeler=membership,
+        outputs={True, False} if outputs is None else outputs,
+    )
+
+    while True:
+        hypothesis = tree.extract_dfa()
+        if not lazy:
+            # DFA is lazily implemented.
+            # Need to actually run through the DFA once to remove dependence
+            # on membership oracle.
+            hypothesis.states()
+
+        yield hypothesis
+        ce = find_counter_example(hypothesis)
+        if ce is None:
+            return
+
+        tree.update_tree(ce, hypothesis)
