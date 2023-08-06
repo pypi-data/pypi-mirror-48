ee62caf63adc6af93fe037c3963b1c4aeb8f815a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:25 +0000

    Add package colab-env/0.0.3/colab-env-0.0.3.tar.gz

---
+++ packages/colab-env/colab-env-0.0.3.tar.gz/colab-env-0.0.3/colab_env/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+
+__version__ = "0.0.3"
+
+from colab_env.handler import ColabEnvHandler
+
+envvar_handler = ColabEnvHandler()
+
+
+def RELOAD():
+    """
+    RELOAD - reload the environment variable handler, envvar_handler
+    """
+    global envvar_handler
+    envvar_handler = ColabEnvHandler()
