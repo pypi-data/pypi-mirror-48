91109cf54f1ebec3d2f307b6131cee795553cd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:50 +0000

    Add package cpsk/0.1.7/cpsk-0.1.7.tar.gz

---
+++ packages/cpsk/cpsk-0.1.7.tar.gz/cpsk-0.1.7/README.md
@@ -0,0 +1,14 @@
+python-cpsk-api ![travis](https://travis-ci.org/Adman/python-cpsk-api.svg?branch=master)
+===============
+
+Simple python api for grabbing data from cp.sk
+
+Installation
+------------
+
+Available on [pypi](https://pypi.org/project/cpsk/)
+
+Running `$ pip install cpsk` will get the package installed
+
+
+*Beware that by using this you might be violating cp.sk ToS*
---
+++ packages/cpsk/cpsk-0.1.7.tar.gz/cpsk-0.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='cpsk',
+
+    description='an unofficial api for grabbing data from cp.sk',
+    author='Adrian Matejov',
+    author_email='a.matejov@centrum.sk',
+    version='0.1.7',
+
+    url='https://github.com/Adman/python-cpsk-api',
+
+    install_requires=['requests', 'lxml'],
+    include_package_data=True,
+    packages=['cpsk'],
+
+    license="The MIT License (MIT)",
+    keywords=['travel', 'train', 'bus']
+)
