72c668790e56e283902a32d39ed0036e3d34493a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:41 +0000

    Add package pyanoboard/0.1.1/pyanoboard-0.1.1.tar.gz

---
+++ packages/pyanoboard/pyanoboard-0.1.1.tar.gz/pyanoboard-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pyanoboard',
+      version='0.1.1',
+      description='Simple piano to keyboard adaptor. Works with any MIDI enabled device.',
+      url='https://github.com/QFSW/Pyanoboard',
+      author='QFSW',
+      author_email='support@qfsw.co.uk',
+      license='MIT',
+      packages=['pyanoboard'],
+      include_package_data=True,
+      install_requires=[
+            'pygame',
+      ],
+      entry_points={
+            "console_scripts": [
+                  "pyanoboard=pyanoboard.__main__:main",
+            ]
+      })
