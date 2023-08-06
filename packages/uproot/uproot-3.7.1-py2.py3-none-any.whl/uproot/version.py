f293066b5d8126d833688b16883f8b81ac395035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package uproot/3.7.1/uproot-3.7.1-py2.py3-none-any.whl

---
+++ packages/uproot/uproot-3.7.1-py2.py3-none-any.whl/uproot/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+
+import re
+
+__version__ = "3.7.1"
+version = __version__
+version_info = tuple(re.split(r"[-\.]", __version__))
+
+del re
