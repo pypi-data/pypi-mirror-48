ceae782bf7790cdab36628b9c2bbf0e9641bc211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:34 +0000

    Add package invidia/0.2/invidia-0.2.tar.gz

---
+++ packages/invidia/invidia-0.2.tar.gz/invidia-0.2/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='invidia',    # This is the name of your PyPI-package.
+    version='0.2',                          # Update the version number for new releases
+    scripts=['invidia']                  # The name of your scipt, and also the command you'll be using for calling it
+)
