9b1259807eaafb236d4b1957871ca4a97e2764b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:22 +0000

    Add package pyanoboard/1.0.2/pyanoboard-1.0.2.tar.gz

---
+++ packages/pyanoboard/pyanoboard-1.0.2.tar.gz/pyanoboard-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='pyanoboard',
+      version='1.0.2',
+      description='Pyanoboard is a simple piano to keyboard emulator, allowing you to use any MIDI enabled device as a virtual keyboard.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/QFSW/Pyanoboard',
+      author='QFSW',
+      author_email='support@qfsw.co.uk',
+      license='MIT',
+      packages=['pyanoboard'],
+      install_requires=[
+            'pygame',
+      ])
