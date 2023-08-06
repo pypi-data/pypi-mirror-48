64c1ad95471edca247aa08f44fce022c058c423b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:54 +0000

    Add package bert-api/0.0.2/bert-api-0.0.2.tar.gz

---
+++ packages/bert-api/bert-api-0.0.2.tar.gz/bert-api-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+#reference from
+#http://xiaoh.me/2015/12/11/python-egg/
+#https://blog.csdn.net/m0_38088359/article/details/83656872
+def setupMain():
+    setup(
+        name = "bert-api",
+        version = "0.0.2",
+        keywords = ("pip", "bert","api",'wrapper'),
+        description = "a wrapper of bert-tensorflow",
+        long_description = "a wrapper of bert-tensorflow of Google for python",
+        license = "Apache Licene 2.0",
+
+        url = "https://github.com/zhc0757/bert-api",
+        author = "zhc",
+        author_email = "ak4777@live.cn",
+
+        packages = find_packages(),
+        include_package_data = True,
+        platforms = "any",
+        install_requires = ['bert-tensorflow>=1.0.1']
+    )
+
+if __name__=='__main__':
+    setupMain()
