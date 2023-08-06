30f4683233e41a46a8c2929846c4370a18a0d0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package akhmadulin/0.0.1/akhmadulin-0.0.1.tar.gz

---
+++ packages/akhmadulin/akhmadulin-0.0.1.tar.gz/akhmadulin-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="akhmadulin",
+    version="0.0.1",
+    author="Example Author",
+    author_email="timur.akhmadulin@spglobal.com",
+    description="A small example package",
+)
