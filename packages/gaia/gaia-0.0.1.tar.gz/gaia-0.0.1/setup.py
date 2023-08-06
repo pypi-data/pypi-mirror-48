129a124e05e289c716f8d3f002cdb10d455598d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package gaia/0.0.1/gaia-0.0.1.tar.gz

---
+++ packages/gaia/gaia-0.0.1.tar.gz/gaia-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import setuptools
+from distutils.core import setup
+
+with open("README.md", 'r') as readme:
+	long_description = readme.read()
+
+setup(
+	name='gaia',
+	version='0.0.1',
+	packages=['gaia'],
+	author='Ryan Spangler',
+	author_email='ryan.spangler@gmail.com',
+	url='https://github.com/prismofeverything/gaia',
+	license='MIT',
+	install_requires=['requests', 'pyyaml', 'confluent-kafka'],
+	long_description=long_description,
+	long_description_content_type='text/markdown')
