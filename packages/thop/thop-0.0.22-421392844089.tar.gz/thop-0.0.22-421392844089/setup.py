1c696bafbd65c8558812df3b598a5bf4049bb2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:28 +0000

    Add package thop/0.0.22-421392844089/thop-0.0.22-421392844089.tar.gz

---
+++ packages/thop/thop-0.0.22-421392844089.tar.gz/thop-0.0.22-421392844089/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+readme = open('README.md').read()
+
+VERSION = '0.0.22'
+
+requirements = [
+    'torch',
+]
+
+import subprocess
+commit_hash = subprocess.check_output("git rev-parse HEAD", shell=True).decode('UTF-8').rstrip()
+VERSION += "_" + str(int(commit_hash, 16))[:12]
+print(VERSION)
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+setup(
+    # Metadata
+    name='thop',
+    version=VERSION,
+    author='Ligeng Zhu',
+    author_email='lykensyu+github@gmail.com',
+    url='https://github.com/Lyken17/pytorch-OpCounter/',
+    description='A tool to count the FLOPs of PyTorch model.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+
+    # Package info
+    packages=find_packages(exclude=('*test*',)),
+
+    #
+    zip_safe=True,
+    install_requires=requirements,
+
+    # Classifiers
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
