fc196d87a0fd9f6855937dba729bfd05107f283c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package xclearx/1.1/xclearx-1.1.tar.gz

---
+++ packages/xclearx/xclearx-1.1.tar.gz/xclearx-1.1/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup 
+
+setup( 
+	name = 'xclearx',
+	version = '1.1', 
+	py_modules = {'xclearx'}, 
+	description = "A function to clear screen on your python shell(cls in windows or clear in linux)",
+) 
