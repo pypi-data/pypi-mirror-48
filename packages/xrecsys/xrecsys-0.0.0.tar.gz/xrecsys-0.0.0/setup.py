55080be21b5cb2e7e376e2e51e92848055370671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:19 +0000

    Add package xrecsys/0.0.0/xrecsys-0.0.0.tar.gz

---
+++ packages/xrecsys/xrecsys-0.0.0.tar.gz/xrecsys-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xrecsys',
+      version='0.0.0',
+      description='Dummy package for xrecsys package name usage',
+      url='',
+      author='mtaifi',
+      author_email='moutai10@gmail.com',
+      license='GPL-3',
+      packages=['xrecsys'],
+      zip_safe=False)
