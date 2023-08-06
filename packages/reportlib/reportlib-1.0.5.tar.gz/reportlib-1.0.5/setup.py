4952ddc1cc59601f6357b1788259c71868ccdb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:02 +0000

    Add package reportlib/1.0.5/reportlib-1.0.5.tar.gz

---
+++ packages/reportlib/reportlib-1.0.5.tar.gz/reportlib-1.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='reportlib',
+    version='1.0.5',
+    author="nhat.nv",
+    author_email="nhat.nv@teko.vn",
+    description="Generator HTML from pandas via Jinja2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.teko.vn/data/libs/reportlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+)
