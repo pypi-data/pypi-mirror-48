22656ea04f4d8940c83c56794cb4df3c12014938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:21 +0000

    Add package monobankua/0.0.2/monobankua-0.0.2.tar.gz

---
+++ packages/monobankua/monobankua-0.0.2.tar.gz/monobankua-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='monobankua',
+    version='0.0.2',
+    author='Sergey Shlyapugin',
+    author_email='shlyapugin@gmail.com',
+    description='Monobank.ua API client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/inbalboa/python-monobankua',
+    packages=setuptools.find_packages(),
+    install_requires=['requests>=2.21,<3.0'],
+    python_requires='>=3.6,<4.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
