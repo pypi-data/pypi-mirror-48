e1975b5b43ecde493d24d9ecc01ac1bd5e35dbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:16 +0000

    Add package odoo10-addon-donation-bank-statement/10.0.1.0.0.99.dev9/odoo10_addon_donation_bank_statement-10.0.1.0.0.99.dev9-py2-none-any.whl

---
+++ packages/odoo10-addon-donation-bank-statement/odoo10_addon_donation_bank_statement-10.0.1.0.0.99.dev9-py2-none-any.whl/odoo/addons/donation_bank_statement/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2014-2016 Barroux Abbey (http://www.barroux.org)
+# © 2014-2016 Akretion France (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Donation Bank Statement',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Creates donation from unreconciled bank statement lines',
+    'author': 'Barroux Abbey, Akretion, Odoo Community Association (OCA)',
+    'website': 'http://www.barroux.org',
+    'depends': ['donation'],
+    'data': [
+        'views/account_config_settings.xml',
+        'views/bank_statement.xml',
+        'views/donation.xml',
+        'security/ir.model.access.csv',
+        ],
+    'demo': ['demo/demo.xml'],
+    'installable': True,
+}
