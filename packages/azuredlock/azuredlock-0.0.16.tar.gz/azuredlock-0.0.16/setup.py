8766fe0e63283d36e06dae1998df0e74ce09de02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:52 +0000

    Add package azuredlock/0.0.16/azuredlock-0.0.16.tar.gz

---
+++ packages/azuredlock/azuredlock-0.0.16.tar.gz/azuredlock-0.0.16/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='azuredlock',
+    py_modules=['azuredlock', 'role'],
+    version='0.0.16',
+    install_requires=['click', 'requests'],
+    python_requires='~=3.5',
+    url='',
+    license='MIT',
+    author='cloudcraeft',
+    author_email='cloudcraeft@outlook.com',
+    description='azure + redlock',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'azuredlock = azuredlock:cli'
+        ]
+    }
+)
