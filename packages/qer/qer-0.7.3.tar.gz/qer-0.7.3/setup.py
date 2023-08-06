f55c7baa3f3a9ccc4dd6fc217d949af27c08e297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:14 +0000

    Add package qer/0.7.3/qer-0.7.3.tar.gz

---
+++ packages/qer/qer-0.7.3.tar.gz/qer-0.7.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qer',
+    version='0.7.3',
+    author='Spencer Putt',
+    author_email='sputt@alumni.iu.edu',
+    description='Python requirements compiler',
+    long_description=open('README.rst').read(),
+    url='https://github.com/sputt/qer',
+    install_requires=open('requirements.txt').readlines(),
+    packages=find_packages(include=['qer*']),
+    license='MIT License',
+    entry_points={
+        'console_scripts': [
+            'req-compile = qer.cmdline:compile_main',
+            'req-hash = qer.hash:hash_main',
+            'req-candidates = qer.candidates:candidates_main',
+        ],
+    },
+    extra_requires={
+        'test': open('test-requirements.txt').readlines()
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Software Development',
+        'Intended Audience :: Developers',
+    ],
+)
