15d3d34e6455386889839eddf52015fdd154ca2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package birdhouse-toolbox/3.2.0/birdhouse-toolbox-3.2.0.tar.gz

---
+++ packages/birdhouse-toolbox/birdhouse-toolbox-3.2.0.tar.gz/birdhouse-toolbox-3.2.0/src/birdhouse_toolbox/cli/main.py
@@ -0,0 +1,18 @@
+import click
+
+from .options import Options
+from .wordpress.main import main as wordpress_group
+from .analytics.main import main as analytics_group
+from ..settings import DEFAULT_REQUEST_TIMEOUT
+
+
+@click.group()
+@click.option("--url")
+@click.option("--timeout", default=DEFAULT_REQUEST_TIMEOUT, required=False)
+@click.pass_context
+def main(ctx, url, timeout):
+    ctx.obj = Options(url, timeout)
+
+
+main.add_command(wordpress_group)
+main.add_command(analytics_group)
