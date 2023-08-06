a19f32b42f6052044bca5a70c41b29386220fb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:37 +0000

    Add package mlstacking/0.3.0/mlstacking-0.3.0.tar.gz

---
+++ packages/mlstacking/mlstacking-0.3.0.tar.gz/mlstacking-0.3.0/setup.py
@@ -0,0 +1,66 @@
+import codecs
+import os
+import sys
+  
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+  
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+  
+  
+  
+NAME = "mlstacking"
+
+PACKAGES = ["mlstacking",]
+  
+DESCRIPTION = "mlstacking is a Python module for sklearn-API friendly multi-layer stacking"
+  
+LONG_DESCRIPTION = read("README.md")
+  
+KEYWORDS = "stacking multi-layer ensemble-learning"
+  
+AUTHOR = "HaoWang"
+  
+AUTHOR_EMAIL = "hao.wang2@tendcloud.com"
+  
+URL = "https://github.com/TalkingData/MultiLayerStacking"
+  
+VERSION = "0.3.0"
+  
+LICENSE = "BSD 3-Clause License"
+
+INSTALL_REQUIRES = [
+    'pandas >= 0.23.0', 
+    'xgboost >= 0.81', 
+    'lightgbm >= 2.1.2', 
+    'matplotlib >= 2.2.0', 
+    'hyperopt >= 0.1.2',
+    'numpy >= 1.11.0',
+    'scikit-learn >= 0.20.3',
+]
+  
+setup(
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    classifiers = [
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords = KEYWORDS,
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = URL,
+    license = LICENSE,
+    packages = PACKAGES,
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=INSTALL_REQUIRES,
+)
