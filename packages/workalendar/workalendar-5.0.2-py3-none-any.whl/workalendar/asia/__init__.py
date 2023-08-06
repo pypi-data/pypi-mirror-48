c45af510a325d90254a3c867cf883c22508ce0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package workalendar/5.0.2/workalendar-5.0.2-py3-none-any.whl

---
+++ packages/workalendar/workalendar-5.0.2-py3-none-any.whl/workalendar/asia/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from .china import China
+from .hong_kong import HongKong
+from .japan import Japan
+from .malaysia import Malaysia
+from .qatar import Qatar
+from .singapore import Singapore
+from .south_korea import SouthKorea
+from .taiwan import Taiwan
+from .israel import Israel
+
+
+__all__ = (
+    'China',
+    'HongKong',
+    'Japan',
+    'Malaysia',
+    'Qatar',
+    'Singapore',
+    'SouthKorea',
+    'Taiwan',
+    'Israel',
+)
