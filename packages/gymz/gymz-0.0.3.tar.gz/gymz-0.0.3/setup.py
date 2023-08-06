30a8da12cfad2dc62842a5741416e39e6940fbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:27 +0000

    Add package gymz/0.0.3/gymz-0.0.3.tar.gz

---
+++ packages/gymz/gymz-0.0.3.tar.gz/gymz-0.0.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import re
+from setuptools import setup
+
+# determine version from __init__.py without importing it
+with open('./gymz/__init__.py', 'r') as f:
+    for l in f:
+        if '__version__' in l:
+            try:
+                version = re.compile('[0-9]+.[0-9]+.[0-9]+').search(l).group()
+            except AttributeError:
+                raise ValueError('Could not determine package version.')
+            else:
+                break
+
+setup(
+    name='gymz',
+    version=version,
+    author='Jakob Jordan, Philipp Weidel',
+    author_email='jakobjordan@posteo.de',
+    description=('A light-weight ZeroMQ wrapper for the OpenAI Gym.'),
+    license='MIT',
+    keywords='openai-gym reinforcement-learning zeromq zmq',
+    url='https://github.com/INM-6/python-gymz',
+    packages=['gymz'],
+    package_data={
+        'gymz': ['DefaultConfig.json']
+    },
+    scripts=['gymz-controller'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering',
+    ],
+    install_requires=[
+        'docopt',
+        'gym>=0.8.1',
+        'numpy',
+        'pyzmq',
+    ]
+)
