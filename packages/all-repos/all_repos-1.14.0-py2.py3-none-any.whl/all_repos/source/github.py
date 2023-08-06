3c53a969da0802275e8133e1fde68461d8ca5508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:45 +0000

    Add package all-repos/1.14.0/all_repos-1.14.0-py2.py3-none-any.whl

---
+++ packages/all-repos/all_repos-1.14.0-py2.py3-none-any.whl/all_repos/source/github.py
@@ -0,0 +1,27 @@
+from typing import Dict
+from typing import NamedTuple
+
+from all_repos import github_api
+
+
+class Settings(NamedTuple):
+    api_key: str
+    username: str
+    collaborator: bool = False
+    forks: bool = False
+    private: bool = False
+    archived: bool = False
+
+
+def list_repos(settings: Settings) -> Dict[str, str]:
+    repos = github_api.get_all(
+        'https://api.github.com/user/repos?per_page=100',
+        headers={'Authorization': f'token {settings.api_key}'},
+    )
+    return github_api.filter_repos(
+        repos,
+        forks=settings.forks,
+        private=settings.private,
+        collaborator=settings.collaborator,
+        archived=settings.archived,
+    )
