297362ec85f99b2b09d776d03e14456b03c1f31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:26 +0000

    Add package RegimeDetect/0.0.5/RegimeDetect-0.0.5-py3-none-any.whl

---
+++ packages/RegimeDetect/RegimeDetect-0.0.5-py3-none-any.whl/RegimeDetect/Happynow.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 14 09:27:51 2019
+
+@author: Noven
+"""
+
+#Rigime shift detection library
+import numpy as np
+def hap():
+	print("I am so confused because Vivek and Arthur know so many strange words!")
