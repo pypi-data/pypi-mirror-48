9e3e604f75f445cfc6825d634e15043060fb4036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:01 +0000

    Add package edc-permissions/0.1.33/edc_permissions-0.1.33-py3-none-any.whl

---
+++ packages/edc-permissions/edc_permissions-0.1.33-py3-none-any.whl/edc_permissions/__init__.py
@@ -0,0 +1,15 @@
+from .constants import (
+    ACCOUNT_MANAGER,
+    ADMINISTRATION,
+    AUDITOR,
+    CLINIC,
+    DATA_MANAGER,
+    EVERYONE,
+    EXPORT,
+    LAB,
+    LAB_VIEW,
+    PHARMACY,
+    PII,
+    PII_VIEW,
+    REVIEW,
+)
