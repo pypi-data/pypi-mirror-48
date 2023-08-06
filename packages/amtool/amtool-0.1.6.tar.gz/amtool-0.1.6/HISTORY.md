23121e0d93e1b7687cc0c22dd995bd77ba2b7807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:59 +0000

    Add package amtool/0.1.6/amtool-0.1.6.tar.gz

---
+++ packages/amtool/amtool-0.1.6.tar.gz/amtool-0.1.6/HISTORY.md
@@ -0,0 +1,30 @@
+# History
+
+
+## 0.1.6 (2019-06-24)
+
+* Debugging automated deployment to pypi.
+
+## 0.1.5 (2019-06-24)
+
+* Added uid command.
+
+## 0.1.4 (2019-06-06)
+
+* Debugging build issues.
+
+## 0.1.3 (2019-05-12)
+
+* Testing release automation.
+
+## 0.1.2 (2019-05-12)
+
+* Adding code coverage.
+
+## 0.1.1 (2019-05-12)
+
+* Automated deployment from travis.
+
+## 0.1.0 (2019-04-23)
+
+* First release on PyPI.
---
+++ packages/amtool/amtool-0.1.6.tar.gz/amtool-0.1.6/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0',
+                'click_log>=0.3.2',
+                'click_plugins>=1.1.1',
+                'PyYAML>=5.1']
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Kenneth E. Bellock",
+    author_email='ken@bellock.net',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="The Artifacts Management Tool is meant to be a generic means of storing and manipulating artifact data in a human readable text format ideal for colaborative work.",
+    entry_points={
+        'console_scripts': [
+            'amt=amt.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='amtool',
+    name='amtool',
+    packages=find_packages(include=['amtool']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/bellockk/amtool',
+    version='0.1.6',
+    zip_safe=False,
+)
