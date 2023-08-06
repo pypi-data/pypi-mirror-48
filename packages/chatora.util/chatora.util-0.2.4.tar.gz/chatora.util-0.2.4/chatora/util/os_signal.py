dd85d3e866ad91706e5d9242907678a4aeae6447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:30 +0000

    Add package chatora.util/0.2.4/chatora.util-0.2.4.tar.gz

---
+++ packages/chatora.util/chatora.util-0.2.4.tar.gz/chatora.util-0.2.4/chatora/util/os_signal.py
@@ -0,0 +1,27 @@
+__all__ = (
+    'signal_handler_ctx',
+)
+
+import contextlib
+import signal
+import typing
+
+
+@contextlib.contextmanager
+def signal_handler_ctx(sig_handler_map: typing.Mapping):
+    original_signal_handler_map = {
+        sig_num: signal.getsignal(sig_num)
+        for sig_num in sig_handler_map.keys()
+    }
+    for sig_num, sig_handler in sig_handler_map.items():
+        signal.signal(sig_num, sig_handler)
+    del sig_handler_map
+
+    try:
+        yield
+    except:
+        raise
+    finally:
+        for sig_num, sig_handler in original_signal_handler_map.items():
+            signal.signal(sig_num, sig_handler)
+    return
