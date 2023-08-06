5a711f7dd659b329ced321c8c4c1d3255324557c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:48 +0000

    Add package pulsarbat/0.0.2/pulsarbat-0.0.2-py2.py3-none-any.whl

---
+++ packages/pulsarbat/pulsarbat-0.0.2-py2.py3-none-any.whl/pulsarbat/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for pulsarbat."""
+
+__author__ = """Nikhil Mahajan"""
+__email__ = 'nikhilm92@gmail.com'
+__version__ = '0.0.2'
+__all__ = []
+
+from . import core
+from . import predictor
+
+__all__.extend(core.__all__)
+__all__.extend(predictor.__all__)
