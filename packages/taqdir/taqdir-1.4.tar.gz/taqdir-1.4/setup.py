b052139fa41eb2691072cc3a7e5b4711c23b88ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:22 +0000

    Add package taqdir/1.4/taqdir-1.4.tar.gz

---
+++ packages/taqdir/taqdir-1.4.tar.gz/taqdir-1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+from எண்ணிக்கை import உரைக்கு as உ
+
+with open('تقدیر/تبدیل.txt', 'r', encoding='utf8') as تبدیل_کی_دستاویز:
+    تبدیل = உ(تبدیل_کی_دستاویز.read().strip(), 'latin', 'ار')
+
+setup(
+    name='taqdir',
+    version=تبدیل,
+    packages=find_packages(),
+    url='https://taqdir.readthedocs.io',
+    download_url='https://github.com/julienmalard/taqdir',
+    license='GNU 3',
+    author='ژولئیں ژاں ملاغ (Julien Jean Malard)',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='',
+    long_description='',
+    install_requires=['numpy', 'pandas', 'ennikkai', 'selenium', 'pcse', 'chardet', 'tradssat', 'requests', 'pykrige',
+                      'scipy'],
+    setup_requires=['ennikkai'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3',
+    ],
+    package_data={
+        '': ['**/*.txt', '**/*.csv'],
+        'تقدیر': ['*.txt', '*.csv']
+    },
+)
