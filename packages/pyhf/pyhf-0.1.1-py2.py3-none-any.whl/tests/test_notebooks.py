cef37ad037150f92b4383c93a3a4476fad4f3b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:15 +0000

    Add package pyhf/0.1.1/pyhf-0.1.1-py2.py3-none-any.whl

---
+++ packages/pyhf/pyhf-0.1.1-py2.py3-none-any.whl/tests/test_notebooks.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import papermill as pm
+import scrapbook as sb
+
+
+def test_notebooks(tmpdir):
+    outputnb = tmpdir.join('output.ipynb')
+    common_kwargs = {
+        'output_path': str(outputnb),
+        'kernel_name': 'python{}'.format(sys.version_info.major),
+    }
+
+    pm.execute_notebook('docs/examples/notebooks/hello-world.ipynb', **common_kwargs)
+
+    pm.execute_notebook(
+        'docs/examples/notebooks/XML_ImportExport.ipynb', **common_kwargs
+    )
+
+    if sys.version_info.major > 2:
+        # The Binder example uses specific relative paths
+        cwd = os.getcwd()
+        os.chdir(os.path.join(cwd, 'docs/examples/notebooks/binderexample'))
+        pm.execute_notebook('StatisticalAnalysis.ipynb', **common_kwargs)
+        os.chdir(cwd)
+
+    pm.execute_notebook(
+        'docs/examples/notebooks/learn/InterpolationCodes.ipynb', **common_kwargs
+    )
+
+    pm.execute_notebook('docs/examples/notebooks/ShapeFactor.ipynb', **common_kwargs)
+    pm.execute_notebook(
+        'docs/examples/notebooks/multichannel-coupled-histo.ipynb',
+        parameters={'validation_datadir': 'validation/data'},
+        **common_kwargs
+    )
+    pm.execute_notebook(
+        'docs/examples/notebooks/multiBinPois.ipynb',
+        parameters={'validation_datadir': 'validation/data'},
+        **common_kwargs
+    )
+
+    nb = sb.read_notebook(str(outputnb))
+    assert nb.scraps['number_2d_successpoints'].data > 200
