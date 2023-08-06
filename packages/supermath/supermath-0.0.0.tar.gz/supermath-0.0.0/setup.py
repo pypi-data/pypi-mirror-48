947a5d9268cabbf9c822a7d159df0b3d60455265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:39 +0000

    Add package supermath/0.0.0/supermath-0.0.0.tar.gz

---
+++ packages/supermath/supermath-0.0.0.tar.gz/supermath-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="supermath",
+    vision='1.0',
+    description='数学模块',
+    author='song',
+    auther_email='18233599177@qq.com',
+    py_modules=['supermath.demo1','supermath.demo2']
+)
---
+++ packages/supermath/supermath-0.0.0.tar.gz/supermath-0.0.0/supermath/demo2.py
@@ -0,0 +1,2 @@
+def hello(name):
+    print("demo2:{0}".format(name))
