48f55ef3fcad4fb5bba34f4fa03793482197c3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:56 +0000

    Add package tablepyxl/0.6.1/tablepyxl-0.6.1.tar.gz

---
+++ packages/tablepyxl/tablepyxl-0.6.1.tar.gz/tablepyxl-0.6.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tablepyxl',
+    version='0.6.1',
+    description='Generate Excel documents from html tables',
+    url='https://github.com/martsberger/tablepyxl',
+    download_url='https://github.com/martsberger/tablepyxl/archive/0.6.1.tar.gz',
+    author='Brad Martsberger, Asma Mehjabeen, Brian Davis',
+    author_email='bmarts@lumere.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    install_requires=['openpyxl', 'premailer', 'requests', 'lxml']
+)
