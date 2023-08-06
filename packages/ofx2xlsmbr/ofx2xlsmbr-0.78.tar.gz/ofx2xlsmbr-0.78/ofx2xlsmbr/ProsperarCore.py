0efbf2cbe798dee20ad2d41edde15522be5b8466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:34 +0000

    Add package ofx2xlsmbr/0.78/ofx2xlsmbr-0.78.tar.gz

---
+++ packages/ofx2xlsmbr/ofx2xlsmbr-0.78.tar.gz/ofx2xlsmbr-0.78/ofx2xlsmbr/ProsperarCore.py
@@ -0,0 +1,23 @@
+from .factory.OFXReaderFactory import OFXReaderFactory
+from .factory.XLSMWriterFactory import XLSMWriterFactory
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ProsperarCore():
+    def run(self, files):
+        #ler o caminho do arquivo e verificar se ele existe
+        if (files is None):
+            logger.info('No files specified.')
+            return
+
+        #chamar o leitor ofx
+        factoryOFX = OFXReaderFactory()
+        readerController = factoryOFX.createReaderController()
+        bankStmts = readerController.read(factoryOFX, files=[files])
+
+        #chamar o escritor xlsm
+        factory = XLSMWriterFactory()
+        writerController = factory.createWriterController()
+        return writerController.write(bankStmts, factory)
