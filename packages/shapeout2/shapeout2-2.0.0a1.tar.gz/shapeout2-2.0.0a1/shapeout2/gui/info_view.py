adc52df1572ccd420cc9789a2c8519aa24f9dfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:19 +0000

    Add package shapeout2/2.0.0a1/shapeout2-2.0.0a1.tar.gz

---
+++ packages/shapeout2/shapeout2-2.0.0a1.tar.gz/shapeout2-2.0.0a1/shapeout2/gui/info_view.py
@@ -0,0 +1,16 @@
+import pkg_resources
+
+from PyQt5 import uic, QtWidgets
+import pyqtgraph as pg
+
+
+class InfoView(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self)
+        path_ui = pkg_resources.resource_filename(
+            "shapeout2.gui", "info_view.ui")
+        uic.loadUi(path_ui, self)
+        self.setWindowTitle("Info View")
+
+    def show(self, path):
+        pass
