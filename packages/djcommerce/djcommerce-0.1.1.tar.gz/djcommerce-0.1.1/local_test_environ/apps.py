40c3f51620c6bd417e8b90f9aa67d31cae6acfc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:06 +0000

    Add package djcommerce/0.1.1/djcommerce-0.1.1.tar.gz

---
+++ packages/djcommerce/djcommerce-0.1.1.tar.gz/djcommerce-0.1.1/local_test_environ/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LocalTestEnvironConfig(AppConfig):
+    name = 'local_test_environ'
---
+++ packages/djcommerce/djcommerce-0.1.1.tar.gz/djcommerce-0.1.1/local_test_environ/models.py
@@ -0,0 +1,8 @@
+from djcommerce.models.order import Order
+from djcommerce.models.address import Address
+
+class CustomOrder(Order):
+    pass
+
+class CustomAddress(Address):
+    pass
