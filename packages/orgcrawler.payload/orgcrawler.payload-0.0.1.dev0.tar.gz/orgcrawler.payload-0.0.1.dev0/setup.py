1ba89b6286758cb1049cf2c7f01ece96642cee5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:14 +0000

    Add package orgcrawler.payload/0.0.1.dev0/orgcrawler.payload-0.0.1.dev0.tar.gz

---
+++ packages/orgcrawler.payload/orgcrawler.payload-0.0.1.dev0.tar.gz/orgcrawler.payload-0.0.1.dev0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_namespace_packages
+
+
+VERSION = '0.0.1.dev0'
+LONGDESC = '''
+OrgCrawler Payloads
+===================
+
+A library of currated orgcrawler payload functions.
+
+'''
+
+setup(
+    name='orgcrawler.payload',
+    version=VERSION,
+    description='A library of orgcrawler payload functions',
+    long_description=LONGDESC,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/ucopacme/orgcrawler-payload',
+    keywords='aws organizations boto3 orgcrawler',
+    author='Ashley Gould - University of California Office of the President',
+    author_email='agould@ucop.edu',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'botocore',
+        'boto3',
+        'orgcrawler',
+    ],
+    packages=find_namespace_packages(include=['orgcrawler.*']),
+    zip_safe=False,
+
+)
