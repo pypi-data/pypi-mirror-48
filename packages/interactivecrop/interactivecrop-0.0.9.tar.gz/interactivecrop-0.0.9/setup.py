5710ebb82fca6ea246900566bc3224f39c883182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package interactivecrop/0.0.9/interactivecrop-0.0.9.tar.gz

---
+++ packages/interactivecrop/interactivecrop-0.0.9.tar.gz/interactivecrop-0.0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='interactivecrop',  
+     version='0.0.9',
+     author="Sam Jett",
+     author_email="samjett247@gmail.com",
+     description="An interactive image cropping tool for Jupyter Notebooks in Python",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/samjett247/interactive_crop",
+     packages=setuptools.find_packages(),
+     include_package_data=True,
+     install_requires=[
+        'pandas',
+        'numpy',
+        'pillow',
+        'ipywidgets',
+        'ipython'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+         "Operating System :: OS Independent",
+     ],
+ )
