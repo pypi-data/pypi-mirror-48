3326c898766a2e3b1b8f9ace516eeccc3664e4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:48 +0000

    Add package splot/1.1.0/splot-1.1.0.tar.gz

---
+++ packages/splot/splot-1.1.0.tar.gz/splot-1.1.0/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+
+def _get_requirements_from_files(groups_files):
+    groups_reqlist = {}
+
+    for k,v in groups_files.items():
+        with open(v, 'r') as f:
+            pkg_list = f.read().splitlines()
+        groups_reqlist[k] = pkg_list
+
+    return groups_reqlist
+
+_groups_files = {
+    'base': 'requirements.txt', #basic requirements
+    'dev': 'requirements_dev.txt', #requirements for dev, doc, test
+}
+
+reqs = _get_requirements_from_files(_groups_files)
+install_reqs = reqs.pop('base')
+extras_reqs = reqs
+    
+setup(name='splot', #name of package
+      version='1.1.0',
+      description= 'Visual analytics for spatial analysis with PySAL.',
+      url= 'https://github.com/pysal/splot',
+      maintainer= 'Serge Rey, Stefanie Lumnitz',
+      maintainer_email= 'sjsrey@gmail.com, stefanie.lumnitz@gmail.com',
+      test_suite = 'nose.collector',
+      tests_require=['nose'],
+      keywords='spatial statistics visualization',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+        ],
+      license='3-Clause BSD',
+      packages=['splot'],
+      include_package_data=True,
+      install_requires=install_reqs,
+      extras_require=extras_reqs,
+      zip_safe=False)
