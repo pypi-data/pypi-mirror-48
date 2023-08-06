eb72815182fc10cdda8093c7a2eeb9e369186f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:05 +0000

    Add package ux/0.1.3/ux-0.1.3.tar.gz

---
+++ packages/ux/ux-0.1.3.tar.gz/ux-0.1.3/ux/interfaces/actions/i_action_sequence.py
@@ -0,0 +1,59 @@
+from typing import List
+
+from ux.interfaces.actions.i_user_action import IUserAction
+
+
+class IActionSequence(object):
+
+    @property
+    def user_actions(self):
+        """
+        :rtype: List[IUserAction]
+        """
+        raise NotImplementedError
+
+    def action_templates(self):
+        """
+        :rtype: List[IActionTemplate]
+        """
+        raise NotImplementedError
+
+    @property
+    def meta(self):
+        """
+        :rtype: dict
+        """
+        raise NotImplementedError
+
+    def duration(self):
+        """
+        :rtype: timedelta
+        """
+        raise NotImplementedError
+
+    def unordered_completion_rate(self, task):
+        """
+        :type task: ITask
+        :rtype: float
+        """
+        raise NotImplementedError
+
+    def ordered_completion_rate(self, task):
+        """
+        :type task: ITask
+        :rtype: float
+        """
+        raise NotImplementedError
+
+    def intersects_task(self, task):
+        """
+        :type task: ITask
+        :rtype: bool
+        """
+        raise NotImplementedError
+
+    def __len__(self):
+        """
+        should return the  number of user actions
+        """
+        raise NotImplementedError
