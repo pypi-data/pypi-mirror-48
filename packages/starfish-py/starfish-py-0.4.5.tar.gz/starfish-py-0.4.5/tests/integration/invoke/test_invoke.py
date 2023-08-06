c26386f0a9cf4392e6ae8e812c837dd2d4aaa51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:16 +0000

    Add package starfish-py/0.4.5/starfish-py-0.4.5.tar.gz

---
+++ packages/starfish-py/starfish-py-0.4.5.tar.gz/starfish-py-0.4.5/tests/integration/invoke/test_invoke.py
@@ -0,0 +1,43 @@
+"""
+    Test invoke agent
+
+"""
+
+import pathlib
+import json
+import logging
+import time
+
+from starfish import (
+    Ocean,
+    logger
+)
+from starfish.agent.invoke_agent import InvokeAgent
+
+from starfish.logging import setup_logging
+
+setup_logging(level=logging.DEBUG)
+
+
+# koi is not working with the current version of keeper 0.9.0
+
+def _test_invoke():
+
+    agent = InvokeAgent()
+    assert agent
+
+    result = agent.get_operations()
+    assert 'hashing_did' == result['hashing']
+    assert 'echo_did' == result['echo']
+
+    operation = agent.get_operation('echo_did')
+    assert operation
+
+    schema = operation.get_schema()
+    assert 1 == len(schema)
+    assert schema['firstparam'] == 'string'
+
+    result = operation.invoke(firstparam='ocean')
+    assert result['firstparam'] == 'hello ocean'
+
+
