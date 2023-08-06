2a3e74368d3753361676d545c34498f5d5da5353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:41 +0000

    Add package OnlySnarf/1.1.6/OnlySnarf-1.1.6.tar.gz

---
+++ packages/OnlySnarf/OnlySnarf-1.1.6.tar.gz/OnlySnarf-1.1.6/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="OnlySnarf",
+    version="1.1.6",
+    author="Skeetzo",
+    author_email="WebmasterSkeetzo@gmail.com",
+    url = 'https://github.com/skeetzo/onlysnarf',
+    download_url = 'https://github.com/skeetzo/onlysnarf/archive/v1.1.6.tar.gz',
+    keywords = ['OnlyFans', 'Content', 'OnlySnarf'],
+    description="OnlyFans Content Distribution Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'selenium',
+        'pydrive',
+        'pathlib',
+        'chromedriver-binary',
+        'moviepy',
+        'apiclient',
+        'google-api-python-client',
+        'httplib2',
+        'python-crontab'
+        ],
+    entry_points={
+        'console_scripts' : [
+            'onlysnarf = OnlySnarf.menu:main_other',
+            'onlysnarf-config = OnlySnarf.config:main'
+        ]
+    },
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: System :: Shells',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    "Operating System :: OS Independent",
+  ]
+)
