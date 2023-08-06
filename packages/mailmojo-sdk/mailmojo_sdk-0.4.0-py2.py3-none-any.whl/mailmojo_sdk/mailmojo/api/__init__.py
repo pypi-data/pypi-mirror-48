10ebd9743402b709701c78708718c953e53166b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:33 +0000

    Add package mailmojo-sdk/0.4.0/mailmojo_sdk-0.4.0-py2.py3-none-any.whl

---
+++ packages/mailmojo-sdk/mailmojo_sdk-0.4.0-py2.py3-none-any.whl/mailmojo_sdk/mailmojo/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from mailmojo.api.account_api import AccountApi
+from mailmojo.api.automation_api import AutomationApi
+from mailmojo.api.contact_api import ContactApi
+from mailmojo.api.embed_api import EmbedApi
+from mailmojo.api.list_api import ListApi
+from mailmojo.api.newsletter_api import NewsletterApi
