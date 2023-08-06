86749ef1615c38fc3e074b298cf04f36f49d9ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:41 +0000

    Add package nexoclom/1.2.0/nexoclom-1.2.0.tar.gz

---
+++ packages/nexoclom/nexoclom-1.2.0.tar.gz/nexoclom-1.2.0/nexoclom/__init__.py
@@ -0,0 +1,20 @@
+from .Input import Input
+from .Output import Output
+from .modeldriver import modeldriver
+from .LOSResult import LOSResult
+from .produce_image import ModelImage
+from .configure_model import configure_model
+from .IDLout import IDLout
+
+
+name = 'nexoclom'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.2.0'
+database = 'thesolarsystemmb'
+
+
+try:
+    configure_model(force=False)
+except:
+    print('Database not set up')
