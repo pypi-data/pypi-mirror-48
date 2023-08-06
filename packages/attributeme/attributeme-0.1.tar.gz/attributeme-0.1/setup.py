6ab42eeeac77e59e68e734e896d3a855d1c76f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:40 +0000

    Add package attributeme/0.1/attributeme-0.1.tar.gz

---
+++ packages/attributeme/attributeme-0.1.tar.gz/attributeme-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+name='attributeme',
+version='0.1',
+description='a pip-installable package example',
+license='MIT',
+packages=['attributeme'],
+author='Aakash Chugh',
+author_email='aakashchugh27@gmail.com',
+keywords=['example'],
+url='https://github.com/chugha/attributeme'
+)
