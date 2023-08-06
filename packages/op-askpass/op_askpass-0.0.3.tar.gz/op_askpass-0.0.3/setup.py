157b26d95de86cf4167d7cd70b8e4d196327df3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:06 +0000

    Add package op-askpass/0.0.3/op_askpass-0.0.3.tar.gz

---
+++ packages/op-askpass/op_askpass-0.0.3.tar.gz/op_askpass-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+with open("dev_requirements.txt", "r") as fh:
+    dev_requirements = fh.read().splitlines()
+
+setup(
+    name="op_askpass",
+    version="0.0.3",
+    author="Maciej Gol",
+    author_email="1kroolik1@gmail.com",
+    description="Add password-protected ssh keys promptless using 1Password.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=["op_askpass"],
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Environment :: Console",
+        "Typing :: Typed",
+    ],
+    entry_points={
+        "console_scripts": [
+            "op-askpass=op_askpass.cli.main:main",
+            "op-askpass-get-password=op_askpass.cli.get_password:main",
+        ]
+    },
+)
