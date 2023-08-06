b9ec54bb5b1a6a61c10cf9e2a6a916a9a801c168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:45 +0000

    Add package UpSetPlot/0.3.0.post1/UpSetPlot-0.3.0.post1.tar.gz

---
+++ packages/UpSetPlot/UpSetPlot-0.3.0.post1.tar.gz/UpSetPlot-0.3.0.post1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup
+
+from upsetplot import __version__ as version
+
+
+def setup_package():
+    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    try:
+        # See setup.cfg
+        setup(name='UpSetPlot',
+              version=version,
+              packages=["upsetplot"],
+              setup_requires=['pytest-runner'],
+              tests_require=['pytest>=2.7', 'pytest-cov<2.6'],
+              # TODO: check versions
+              install_requires=['numpy', 'pandas', 'matplotlib>=2.0'])
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+    return
+
+
+if __name__ == '__main__':
+    setup_package()
