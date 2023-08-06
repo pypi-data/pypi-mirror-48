745e2fa29e460cc8e7a996fb0f16723d7ce28d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:45 +0000

    Add package sphinx-revealjs/0.5.1/sphinx_revealjs-0.5.1-py3-none-any.whl

---
+++ packages/sphinx-revealjs/sphinx_revealjs-0.5.1-py3-none-any.whl/sphinx_revealjs/nodes.py
@@ -0,0 +1,20 @@
+from docutils import nodes
+
+
+class FlagAttribute(object):
+    pass
+
+
+class revealjs_section(nodes.Structural, nodes.Element):
+    def attributes_str(self):
+        pair = []
+        for k, v in self.attributes.items():
+            if isinstance(v, FlagAttribute):
+                pair.append(k)
+                continue
+            pair.append(f'{k}="{v}"')
+        return ' '.join(pair)
+
+
+class revealjs_slide(nodes.Structural, nodes.Element):
+    pass
