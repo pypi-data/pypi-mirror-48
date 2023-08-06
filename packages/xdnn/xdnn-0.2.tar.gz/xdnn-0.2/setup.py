07c4259c00e10fcf8f8e0f16315c4a7db71a008a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package xdnn/0.2/xdnn-0.2.tar.gz

---
+++ packages/xdnn/xdnn-0.2.tar.gz/xdnn-0.2/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup
+
+setup(name='xdnn',
+      version='0.2',
+      description='Explaining deep neural networks',
+      author='Prashant K. Sharma',
+      author_email='prashaantsharmaa@gmail.com',
+      license='MIT',
+      packages=['xdnn'],
+      zip_safe=False)
