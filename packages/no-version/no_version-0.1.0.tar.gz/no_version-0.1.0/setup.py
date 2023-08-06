4cdf2d132754ca0bd6cfaad6156fb65aebd7042d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package no-version/0.1.0/no_version-0.1.0.tar.gz

---
+++ packages/no-version/no_version-0.1.0.tar.gz/no_version-0.1.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='no_version',
+    version='0.1.0',
+    description='Package without __version__ number.',
+    author='no_version',
+    author_email='dieter@werthmuller.org',
+    url='https://github.com/prisae/no_version',
+    download_url='https://github.com/prisae/no_version/tarball/v0.1.0',
+    license='CC0 1.0 Universal (CC0 1.0)',
+    packages=['no_version'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
