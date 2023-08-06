fe45566d9c87cd6f45c2dbcf0c76d5f7842aba77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:42 +0000

    Add package irl2019/0.4/irl2019-0.4.tar.gz

---
+++ packages/irl2019/irl2019-0.4.tar.gz/irl2019-0.4/README.md
@@ -0,0 +1,5 @@
+Utilities for spaCy IRL 2019
+----------------------------
+
+This is a temporary package with some code tools useful for the spaCy IRL
+2019 trainings.
---
+++ packages/irl2019/irl2019-0.4.tar.gz/irl2019-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='irl2019',
+    version='0.4',
+    description='Temporary package to support an event.',
+    url='',
+    author='Matthew Honnibal',
+    author_email='matt@explosion.ai',
+    license='MIT',
+    packages=['irl2019'],
+    zip_safe=False
+)
