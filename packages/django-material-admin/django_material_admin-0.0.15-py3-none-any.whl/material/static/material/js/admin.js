91fccda11569a0d310afebb88739dbb4acbad7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:56 +0000

    Add package django-material-admin/0.0.15/django_material_admin-0.0.15-py3-none-any.whl

---
+++ packages/django-material-admin/django_material_admin-0.0.15-py3-none-any.whl/material/static/material/js/admin.js
@@ -0,0 +1,39 @@
+(function($) {
+    const toasts = ['success', 'warning']
+
+    $('#side-bar, #mobile-demo').mouseenter(
+        function() {
+        $('.scroll-pane').jScrollPane();
+    }
+    ).mouseleave(
+        function() {
+            const jsp = $('.scroll-pane').data('jsp')
+            if (jsp) {
+                jsp.destroy();
+            }
+        }
+    );
+    $('.collapsible-body > .active').closest('.scrollspy').addClass('active');
+
+    window.addEventListener('load', function() {
+        var collapsible = document.querySelectorAll('.collapsible');
+         M.Collapsible.init(collapsible, {
+            accordion: false
+        });
+
+        var sidenav = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(sidenav);
+
+        var elems = document.querySelectorAll('.dropdown-trigger');
+        M.Dropdown.init(elems);
+        $('select').not('.empty-form select, .selector-available select, .selector-chosen select').formSelect();
+
+        for(const toast of toasts) {
+            const messages = $(`.messagelist > .${toast}`);
+            for(let message of messages) {
+                M.toast({html: message.innerText, classes: `rounded ${toast}-toast`});
+            }
+        }
+
+    });
+})(jQuery);
