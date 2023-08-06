792ad9c666809a8e1ede9d1b2066f3a5185074ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:23 +0000

    Add package p4tools/0.5.2/p4tools-0.5.2.tar.gz

---
+++ packages/p4tools/p4tools-0.5.2.tar.gz/p4tools-0.5.2/p4tools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Planet Four Tools."""
+
+__author__ = """K.-Michael Aye"""
+__email__ = 'kmichael.aye@gmail.com'
+__version__ = '0.5.2'
+
+from .p4tools import *
