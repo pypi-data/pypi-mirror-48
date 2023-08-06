f5c5cb3e315e349c40052e5eb589f2c40b9c9c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:43 +0000

    Add package CredentialHolder/0.0.1/CredentialHolder-0.0.1.tar.gz

---
+++ packages/CredentialHolder/CredentialHolder-0.0.1.tar.gz/CredentialHolder-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CredentialHolder",
+    version="0.0.1",
+    author="brando",
+    author_email="snoopy20704@capitalone.com",
+    description="simple class for serializable and secure credential holder",
+    install_requires=["pycryptodome"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bmpang/PythonCredentialHolderClass",
+    packages=["credential_holder"],
+    classifiers=["Programming Language :: Python :: 3"],
+)
