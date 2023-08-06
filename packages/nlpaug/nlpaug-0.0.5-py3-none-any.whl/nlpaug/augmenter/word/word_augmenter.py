a0e4738f6dbb24456b8341df98a2eb534f348e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:57 +0000

    Add package nlpaug/0.0.5/nlpaug-0.0.5-py3-none-any.whl

---
+++ packages/nlpaug/nlpaug-0.0.5-py3-none-any.whl/nlpaug/augmenter/word/word_augmenter.py
@@ -0,0 +1,47 @@
+from nlpaug.util import Method
+from nlpaug import Augmenter
+
+
+class WordAugmenter(Augmenter):
+    def __init__(self, action, name='Word_Aug', aug_min=1, aug_p=0.3, tokenizer=None, stopwords=[], verbose=0):
+        super(WordAugmenter, self).__init__(
+            name=name, method=Method.WORD, action=action, aug_min=aug_min, verbose=verbose)
+        self.aug_p = aug_p
+        if tokenizer is not None:
+            self.tokenizer = tokenizer
+        self.stopwords = stopwords
+        
+    def tokenizer(self, text):
+        return text.split(' ')
+
+    def reverse_tokenizer(self, tokens):
+        return ' '.join(tokens)
+
+    def skip_aug(self, token_idxes, tokens):
+        return token_idxes
+
+    def align_capitalization(self, src_token, dest_token):
+        """
+            Simulate capitalized string
+        """
+        if src_token[0].isupper() and len(src_token) > 1 and src_token[1].isupper():
+            return dest_token.upper()
+        elif src_token[0].isupper():
+            return dest_token.capitalize()
+        else:
+            return dest_token
+
+    def _get_aug_idxes(self, tokens):
+        aug_cnt = self.generate_aug_cnt(len(tokens))
+        word_idxes = [i for i, t in enumerate(tokens) if t not in self.stopwords]
+        word_idxes = self.skip_aug(word_idxes, tokens)
+        if len(word_idxes) == 0:
+            if self.verbose > 0:
+                exception = Warning(name=WarningName.OUT_OF_VOCABULARY,
+                                    code=WarningCode.WARNING_CODE_002, msg=WarningMessage.NO_WORD)
+                exception.output()
+            return None
+        if len(word_idxes) < aug_cnt:
+            aug_cnt = len(word_idxes)
+        aug_idexes = self.sample(word_idxes, aug_cnt)
+        return aug_idexes
---
+++ packages/nlpaug/nlpaug-0.0.5-py3-none-any.whl/nlpaug/augmenter/word/wordnet.py
@@ -0,0 +1,78 @@
+import nltk
+from nltk.corpus import wordnet
+
+from nlpaug.augmenter.word import WordAugmenter
+from nlpaug.util import Action, PartOfSpeech
+
+
+class WordNetAug(WordAugmenter):
+    def __init__(self, name='WordNet_Aug', aug_min=1, aug_p=0.3, lang='eng', tokenizer=None, stopwords=[], verbose=0):
+        super(WordNetAug, self).__init__(
+            action=Action.SUBSTITUTE, name=name, aug_p=aug_p, aug_min=aug_min, tokenizer=tokenizer, stopwords=stopwords,
+            verbose=verbose)
+
+        self.model = self.get_model()
+        self.lang=lang
+
+    def skip_aug(self, token_idxes, tokens):
+        results = []
+        for token_idx in token_idxes:
+            # Some word does not come with synonym. It will be excluded in lucky draw.
+            if tokens[token_idx][1] not in ['DT']:
+                results.append(token_idx)
+
+        return results
+
+    def substitute(self, text):
+        results = []
+
+        tokens = self.tokenizer(text)
+
+        pos = nltk.pos_tag(tokens)
+
+        aug_idexes = self._get_aug_idxes(pos)
+
+        for i, token in enumerate(tokens):
+            # Skip if no augment for word
+            if i not in aug_idexes:
+                results.append(token)
+                continue
+
+            word_poses = PartOfSpeech.pos2wn(pos[i][1])
+            synets = []
+            if word_poses is None or len(word_poses) == 0:
+                # Use every possible words as the mapping does not defined correctly
+                synets.extend(self.model.synsets(pos[i][0], lang=self.lang))
+            else:
+                for word_pos in word_poses:
+                    synets.extend(self.model.synsets(pos[i][0], pos=word_pos, lang=self.lang))
+
+            augmented_data = []
+            for synet in synets:
+                for candidate in synet.lemma_names(lang=self.lang):
+                    if candidate.lower() != token.lower():
+                        augmented_data.append(candidate)
+
+            if len(augmented_data) == 0:
+                results.append(token)
+            else:
+                candidate = self.sample(augmented_data, 1)[0]
+                candidate = candidate.replace("_", " ").replace("-", " ").lower()
+                results.append(self.align_capitalization(token, candidate))
+
+        return self.reverse_tokenizer(results)
+
+    def get_model(self):
+        try:
+            # Check whether wordnet package is downloaded
+            wordnet.synsets('computer')
+        except Exception:
+            nltk.download('wordnet')
+
+        try:
+            # Check whether POS package is downloaded
+            nltk.pos_tag('computer')
+        except Exception:
+            nltk.download('averaged_perceptron_tagger')
+
+        return wordnet
