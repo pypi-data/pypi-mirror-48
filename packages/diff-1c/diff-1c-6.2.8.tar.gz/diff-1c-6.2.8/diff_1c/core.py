3de2e9192fa307623c227895ab13135d2d8d839f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:55 +0000

    Add package diff-1c/6.2.8/diff-1c-6.2.8.tar.gz

---
+++ packages/diff-1c/diff-1c-6.2.8.tar.gz/diff-1c-6.2.8/diff_1c/core.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from cjk_commons.logging_ import add_loggers
+from diff_1c import logger as main_logger
+from diff_1c.cli import get_argparser
+
+
+def run() -> None:
+    argparser = get_argparser()
+    args = argparser.parse_args(sys.argv[1:])
+    add_loggers(args, main_logger, argparser.prog)
+    args.func(args)
