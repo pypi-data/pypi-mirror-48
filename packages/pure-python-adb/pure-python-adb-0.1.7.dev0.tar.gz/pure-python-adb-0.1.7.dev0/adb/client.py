b8af5710d5c437675ca654ab7ba4324c3e920b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:36 +0000

    Add package pure-python-adb/0.1.7.dev0/pure-python-adb-0.1.7.dev0.tar.gz

---
+++ packages/pure-python-adb/pure-python-adb-0.1.7.dev0.tar.gz/pure-python-adb-0.1.7.dev0/adb/client.py
@@ -0,0 +1,24 @@
+from adb.command.host import Host
+from adb.connection import Connection
+from adb.utils.logger import AdbLogging
+
+logger = AdbLogging.get_logger(__name__)
+
+class Client(Host):
+    def __init__(self, host='127.0.0.1', port=5037):
+        self.host = host
+        self.port = port
+
+    def create_connection(self, timeout=None):
+        conn = Connection(self.host, self.port, timeout)
+        conn.connect()
+        return conn
+
+    def device(self, serial):
+        devices = self.devices()
+
+        for device in devices:
+            if device.serial == serial:
+                return device
+
+        return None
