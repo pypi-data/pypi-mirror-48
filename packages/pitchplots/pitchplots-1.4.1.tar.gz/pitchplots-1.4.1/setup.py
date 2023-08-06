88be3d2b59a6f844c1c9e6fa725366bf436b056e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:08 +0000

    Add package pitchplots/1.4.1/pitchplots-1.4.1.tar.gz

---
+++ packages/pitchplots/pitchplots-1.4.1.tar.gz/pitchplots-1.4.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pitchplots",
+    version="1.4.1",
+    author="Fabian Moss",
+    author_email="fabian.moss@epfl.ch",
+    description="A package containing representation tools for musical purposes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DCMLab/pitchplots",
+    packages=setuptools.find_packages(),
+    package_data={
+            'pitchplots': ['data/data_example.mxl'],
+    },
+    install_requires=['matplotlib>=3.0.1',
+                      'pandas>=0.23.4',
+                      'numpy>=1.15.3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
