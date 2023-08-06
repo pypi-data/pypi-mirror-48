9b03465b23579c8acd8f17d86aee7c536b26ef08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:41 +0000

    Add package plasticity/0.0.13/plasticity-0.0.13.tar.gz

---
+++ packages/plasticity/plasticity-0.0.13.tar.gz/plasticity-0.0.13/plasticity/__init__.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+
+class Plasticity(object):
+    """A Plasticity class that holds the user's API token and
+    location (url) of the API to be used.
+
+    Attributes:
+        token: An API token to authenticate with the API
+        url: A local or remote Plasticity API url to use
+    """
+
+    def __init__(self, token=None, url=None, environment=None):
+        """Initializes a new Plasticity object."""
+        environment = environment or os.environ
+        self.url = url or 'https://api.plasticity.ai/'
+        self.token = token or environment.get('PLASTICITY_API_KEY')
+
+        # Services
+        self._sapien = None
+        self._cortex = None
+
+    @property
+    def sapien(self):
+        if self._sapien is None:
+            from plasticity.sapien import Sapien
+            self._sapien = Sapien(self)
+        return self._sapien
+
+    @property
+    def cortex(self):
+        if self._cortex is None:
+            from plasticity.cortex import Cortex
+            self._cortex = Cortex(self)
+        return self._cortex
---
+++ packages/plasticity/plasticity-0.0.13.tar.gz/plasticity-0.0.13/plasticity/sapien/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from plasticity.base.service import Service
+
+
+class Sapien(Service):
+    """Sapien is the API service for language. Its functions are
+    described here: https://www.plasticity.ai/api/docs/#sapien
+    """
+
+    def __init__(self, plasticity):
+        """Initializes a new Sapien Service."""
+        super(Sapien, self).__init__(plasticity)
+        self.url = self.plasticity.url + 'sapien/'
+
+        # Endpoints
+        self._core = None
+        self._transform = None
+        self._names = None
+
+    @property
+    def core(self):
+        if self._core is None:
+            from plasticity.sapien.core import Core
+            self._core = Core(self.plasticity)
+        return self._core
+
+    @property
+    def transform(self):
+        if self._transform is None:
+            from plasticity.sapien.transform import Transform
+            self._transform = Transform(self.plasticity)
+        return self._transform
+
+    @property
+    def names(self):
+        if self._names is None:
+            from plasticity.sapien.names import Names
+            self._names = Names(self.plasticity)
+        return self._names
