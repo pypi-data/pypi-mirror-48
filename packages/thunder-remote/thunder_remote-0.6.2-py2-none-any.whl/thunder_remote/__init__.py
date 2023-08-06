d59561c0cca9321bbb3a2e9730ff8e626d80deba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package thunder-remote/0.6.2/thunder_remote-0.6.2-py2-none-any.whl

---
+++ packages/thunder-remote/thunder_remote-0.6.2-py2-none-any.whl/thunder_remote/__init__.py
@@ -0,0 +1,8 @@
+"""The thunder-remote package.
+
+Package for usage with the 'Raspberry Pi Wireless USB Gamepad'
+from the official 'PiBorg' shop.
+"""
+
+__name__ = "thunder-remote"
+__version__ = '0.6.2'
