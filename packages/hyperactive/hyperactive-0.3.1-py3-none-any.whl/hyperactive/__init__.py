0a502f0f8dc0e51c21aef135b9ad192504942245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:18 +0000

    Add package hyperactive/0.3.1/hyperactive-0.3.1-py3-none-any.whl

---
+++ packages/hyperactive/hyperactive-0.3.1-py3-none-any.whl/hyperactive/__init__.py
@@ -0,0 +1,18 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+from .random_search import RandomSearchOptimizer
+from .simulated_annealing import SimulatedAnnealingOptimizer
+from .particle_swarm_optimization import ParticleSwarmOptimizer
+from .evolution_strategy import EvolutionStrategyOptimizer
+
+__version__ = "0.3.0"
+__license__ = "MIT"
+
+__all__ = [
+    "RandomSearchOptimizer",
+    "SimulatedAnnealingOptimizer",
+    "ParticleSwarmOptimizer",
+    "EvolutionStrategyOptimizer",
+]
