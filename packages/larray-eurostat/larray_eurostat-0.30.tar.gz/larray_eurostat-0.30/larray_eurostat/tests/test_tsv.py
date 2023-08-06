db00c2760d7362458b02afc6756bcf4620173098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package larray-eurostat/0.30/larray_eurostat-0.30.tar.gz

---
+++ packages/larray-eurostat/larray_eurostat-0.30.tar.gz/larray_eurostat-0.30/larray_eurostat/tests/test_tsv.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+
+import pytest
+from larray_eurostat.tsv import *
+
+
+def test_eurostat_get():
+    dataset = 'nama_aux_cra'
+
+    msg = "Not a gzipped file (b'<!')"
+    if sys.version_info[0] >= 3:
+        msg += "\nCan't open file {}{}.tsv.gz".format(EUROSTAT_BASEURL, dataset)
+    type_err = IOError if sys.version_info[0] < 3 else OSError
+
+    with pytest.raises(type_err, match=msg):
+        eurostat_get(dataset)
