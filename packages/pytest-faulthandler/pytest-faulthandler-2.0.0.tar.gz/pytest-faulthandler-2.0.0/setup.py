ec9fe7703fc2f4863557a1d355a4f5cc4e5f7479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:16 +0000

    Add package pytest-faulthandler/2.0.0/pytest-faulthandler-2.0.0.tar.gz

---
+++ packages/pytest-faulthandler/pytest-faulthandler-2.0.0.tar.gz/pytest-faulthandler-2.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='pytest-faulthandler',
+    version="2.0.0",
+    url='https://github.com/pytest-dev/pytest-faulthandler',
+    license='MIT',
+    install_requires=['pytest>=5.0'],
+    author='Bruno Oliveira',
+    author_email='nicoddemus@gmail.com',
+    description='py.test plugin that activates the fault handler module for tests (dummy package)',
+    long_description=long_description,
+    keywords='pytest faulthandler',
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing',
+    ]
+)
