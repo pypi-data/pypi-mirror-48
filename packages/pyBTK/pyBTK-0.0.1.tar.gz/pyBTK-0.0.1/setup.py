e776892bb05b0b84b4a2d9fc02bc65f3dbb7d5df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:47 +0000

    Add package pyBTK/0.0.1/pyBTK-0.0.1.tar.gz

---
+++ packages/pyBTK/pyBTK-0.0.1.tar.gz/pyBTK-0.0.1/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup,find_packages
+
+#------------------------- INSTALL--------------------------------------------
+setup(name = 'pyBTK',
+    version = "0.0.1",
+    author = 'Fabien Leboeuf',
+    author_email = 'fabien.leboeuf@gmail.com',
+    description = "python3 wrapping of the Biomechanical toolkit",
+    keywords = 'c3d, mocap',
+    packages=find_packages(),
+	include_package_data=True,
+    license='BSD License',
+    classifiers=['Programming Language :: Python',
+                 'Operating System :: Microsoft :: Windows',
+                 'Natural Language :: English'],
+    )
