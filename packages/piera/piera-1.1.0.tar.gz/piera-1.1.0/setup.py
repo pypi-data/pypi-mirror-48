3cb39f759b2b27e35a226501b054c8dafd58cb22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:09 +0000

    Add package piera/1.1.0/piera-1.1.0.tar.gz

---
+++ packages/piera/piera-1.1.0.tar.gz/piera-1.1.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os, sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'piera',
+]
+
+requires = [
+    'pyyaml'
+]
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='piera',
+    version='1.1.0',
+    description='a python hiera parser',
+    # long_description=readme + '\n\n',
+    author='Andrei Zbikowski',
+    author_email='andrei.zbikowski@gmail.com',
+    url='http://github.com/b1naryth1ef/piera',
+    packages=packages,
+    package_data={"": ["README.md"]},
+    package_dir={'piera': 'piera'},
+    include_package_data=True,
+    install_requires=requires,
+    license='Apache 2.0',
+    zip_safe=False,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
