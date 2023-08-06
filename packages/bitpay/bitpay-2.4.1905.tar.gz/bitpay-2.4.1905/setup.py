745cb9da6419bbe801e8fc163a85120a03af4653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package bitpay/2.4.1905/bitpay-2.4.1905.tar.gz

---
+++ packages/bitpay/bitpay-2.4.1905.tar.gz/bitpay-2.4.1905/setup.py
@@ -0,0 +1,40 @@
+# chardet's setup.py
+import setuptools
+setuptools.setup(
+    name="bitpay",
+    packages=["bitpay"],
+    version="2.4.1905",
+    description="Accept bitcoin with BitPay",
+    author="BitPay Integrations Team",
+    author_email="sales-engineering@bitpay.com",
+    url="https://github.com/bitpay/bitpay-python",
+    download_url="https://github.com/bitpay/bitpay-python/tarball/v2.4.1905",
+    keywords=["bitcoin", "payments", "crypto"],
+    install_requires=["requests", "ecdsa"],
+    classifiers=[
+"Programming Language :: Python :: 3.7",
+"Programming Language :: Python :: 3 :: Only",
+"Development Status :: 5 - Production/Stable",
+"Environment :: Web Environment",
+"Intended Audience :: Developers",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+"Topic :: Software Development :: Libraries :: Python Modules",
+"Topic :: Office/Business :: Financial"
+],
+    long_description="""\
+Python Library for integrating with BitPay
+------------------------------------------
+
+This library is a simple way to integrate your application with
+BitPay for taking Bitcoin payments. It exposes three basic 
+functions, authenticating with BitPay, creating invoices, 
+and retrieving invoices. It is not meant as a replacement for 
+the entire BitPay API. However, the key_utils module contains
+all of the tools you need to use the BitPay API for other
+purposes.
+
+This version requires Python 2.7 or later.
+""",
+    long_description_content_type='text/markdown'
+)
