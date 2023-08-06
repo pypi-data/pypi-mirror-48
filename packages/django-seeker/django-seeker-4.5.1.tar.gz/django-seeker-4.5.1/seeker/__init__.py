7e8804a18f792869d1d51b090a969b0422eeed6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:28 +0000

    Add package django-seeker/4.5.1/django-seeker-4.5.1.tar.gz

---
+++ packages/django-seeker/django-seeker-4.5.1.tar.gz/django-seeker-4.5.1/seeker/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '4.5.1'
+
+from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet
+from .mapping import (
+    build_mapping, deep_field_factory, DEFAULT_ANALYZER, document_field, document_from_model, Indexable, ModelIndex,
+    RawMultiString, RawString)
+from .registry import app_documents, documents, model_documents, register
+from .utils import delete, index, search
+from .views import AdvancedColumn, AdvancedSavedSearchView, AdvancedSeekerView, Column, SeekerView
+
+
+default_app_config = 'seeker.apps.SeekerConfig'
