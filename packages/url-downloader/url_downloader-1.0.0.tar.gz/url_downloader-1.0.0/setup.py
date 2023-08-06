991d098c350779361ea16b9c595ba2f1d67daa1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:32 +0000

    Add package url-downloader/1.0.0/url_downloader-1.0.0.tar.gz

---
+++ packages/url-downloader/url_downloader-1.0.0.tar.gz/url_downloader-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from distutils.core import setup
+from url_downloader import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='url_downloader',
+    version=__version__,
+    description='Download url',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='ChsHub',
+    author_email='christian1193@web.com',
+    packages=['url_downloader'],
+    license='MIT License',
+    classifiers=['Programming Language :: Python :: 3.7']
+)
+# C:\Python37\python.exe -m pip install . --upgrade
+# C:\Python37\python.exe setup.py sdist bdist_wheel
+# C:\Python37\python.exe -m twine upload dist/*
