2fa9d2f653a676c07010111a84d5ca53ac3e796a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:30 +0000

    Add package django-requestlogs/0.1.5/django_requestlogs-0.1.5-py3-none-any.whl

---
+++ packages/django-requestlogs/django_requestlogs-0.1.5-py3-none-any.whl/requestlogs/storages.py
@@ -0,0 +1,46 @@
+import json
+import logging
+
+from rest_framework import serializers
+
+
+logger = logging.getLogger('requestlogs')
+
+
+class JsonDumpField(serializers.Field):
+    def to_representation(self, value):
+        return json.dumps(value)
+
+
+class BaseEntrySerializer(serializers.Serializer):
+    class RequestSerializer(serializers.Serializer):
+        method = serializers.CharField(read_only=True)
+        full_path = serializers.CharField(read_only=True)
+        data = JsonDumpField(read_only=True)
+        query_params = JsonDumpField(read_only=True)
+
+    class ResponseSerializer(serializers.Serializer):
+        status_code = serializers.IntegerField(read_only=True)
+        data = JsonDumpField(read_only=True)
+
+    class UserSerializer(serializers.Serializer):
+        id = serializers.IntegerField()
+        username = serializers.CharField()
+
+    action_name = serializers.CharField(read_only=True)
+    execution_time = serializers.DurationField(read_only=True)
+    timestamp = serializers.DateTimeField(read_only=True)
+    ip_address = serializers.CharField(read_only=True)
+    request = RequestSerializer(read_only=True)
+    response = ResponseSerializer(read_only=True)
+    user = UserSerializer()
+
+
+class LoggingStorage(object):
+    serializer_class = BaseEntrySerializer
+
+    def store(self, entry):
+        logger.info(self.prepare(entry))
+
+    def prepare(self, entry):
+        return self.serializer_class(entry).data
