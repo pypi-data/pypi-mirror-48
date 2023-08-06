80aabc3ff6af4900b1a68e65ce4b4bd0d7a72463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package api-nichotined/1.0.6/api-nichotined-1.0.6.tar.gz

---
+++ packages/api-nichotined/api-nichotined-1.0.6.tar.gz/api-nichotined-1.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="api-nichotined",
+    version="1.0.6",
+    author="Nicholas Frederick",
+    author_email="nicholas.frederich.lagaunne@gmail.com",
+    description="API package for testing purpose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nichotined/api",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'pytest',
+        'redis',
+        'curlify'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
