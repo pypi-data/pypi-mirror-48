15cd67e965990ccf032d748929b56d2aebd11689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:47 +0000

    Add package python3-http-logstash/0.4.74/python3-http-logstash-0.4.74.tar.gz

---
+++ packages/python3-http-logstash/python3-http-logstash-0.4.74.tar.gz/python3-http-logstash-0.4.74/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='python3-http-logstash',
+    packages=['logstash'],
+    version='0.4.74',
+    description='Python logging handler for Logstash.',
+    #long_description=open('README.md').read(),
+    author='Israel Flores',
+    author_email='frauniki@sinoa.jp',
+    url='https://github.com/frauniki/python3-logstash',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ]
+)
