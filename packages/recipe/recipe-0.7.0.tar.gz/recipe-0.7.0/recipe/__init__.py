c4a464a19d183cf4b175a0209f33ea3f5a11a030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:35 +0000

    Add package recipe/0.7.0/recipe-0.7.0.tar.gz

---
+++ packages/recipe/recipe-0.7.0.tar.gz/recipe-0.7.0/recipe/__init__.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Recipe
+~~~~~~~~~~~~~~~~~~~~~
+"""
+import logging
+
+from flapjack_stack import FlapjackStack
+
+from recipe import default_settings
+from recipe.core import Recipe
+from recipe.exceptions import BadIngredient, BadRecipe
+from recipe.extensions import (
+    Anonymize, AutomaticFilters, BlendRecipe, CompareRecipe, RecipeExtension,
+    SummarizeOver
+)
+from recipe.ingredients import (
+    Dimension, DivideMetric, Filter, Having, IdValueDimension, Ingredient,
+    LookupDimension, Metric, WtdAvgMetric
+)
+from recipe.oven import get_oven
+from recipe.shelf import AutomaticShelf, Shelf
+from recipe.utils import FakerAnonymizer
+
+SETTINGS = FlapjackStack()
+SETTINGS.add_layer(default_settings)
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+
+    class NullHandler(logging.Handler):
+
+        def emit(self, record):
+            pass
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
+__version__ = '0.7.0'
+
+__all__ = [
+    'BadIngredient',
+    'BadRecipe',
+    'Ingredient',
+    'Dimension',
+    'LookupDimension',
+    'IdValueDimension',
+    'Metric',
+    'DivideMetric',
+    'WtdAvgMetric',
+    'Filter',
+    'Having',
+    'Recipe',
+    'Shelf',
+    'AutomaticShelf',
+    'SETTINGS',
+    'get_oven',
+    'Anonymize',
+    'AutomaticFilters',
+    'BlendRecipe',
+    'CompareRecipe',
+    'RecipeExtension',
+    'SummarizeOver',
+    'FakerAnonymizer',
+]
