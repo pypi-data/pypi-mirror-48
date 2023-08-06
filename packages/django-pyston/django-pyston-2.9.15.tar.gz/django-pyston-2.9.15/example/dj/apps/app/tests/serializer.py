bb352fbfb30ba6f50d3fc53d84f59d721e3adeb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:00 +0000

    Add package django-pyston/2.9.15/django-pyston-2.9.15.tar.gz

---
+++ packages/django-pyston/django-pyston-2.9.15.tar.gz/django-pyston-2.9.15/example/dj/apps/app/tests/serializer.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+import json
+import xml.dom.minidom
+
+from germanium.tools import assert_true, assert_equal
+
+from unittest.case import TestCase
+
+from app.models import User
+
+from pyston.serializer import serialize
+
+
+class DirectSerializationTestCase(TestCase):
+
+    def test_serialization(self):
+        for i in range(10):
+            User.objects.create(is_superuser=True, email='test{}@test.cz'.format(i))
+        assert_true(isinstance(json.loads((serialize(User.objects.all()))), list))
+        assert_true(isinstance(json.loads((serialize(User.objects.first()))), dict))
+
+        assert_equal(
+            set(json.loads((serialize(User.objects.first()))).keys()),
+            {'id', 'created_at', '_obj_name', 'email', 'contract', 'solving_issue', 'first_name', 'last_name',
+             'manual_created_date'}
+        )
+        assert_equal(
+            set(json.loads((serialize(User.objects.first(), ('id',)))).keys()),
+            {'id'}
+        )
+
+        xml.dom.minidom.parseString(serialize(User.objects.first(), converter_name='xml'))
