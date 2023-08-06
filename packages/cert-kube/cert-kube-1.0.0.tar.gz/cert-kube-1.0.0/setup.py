9c93d09338ea4115cf9510ce087c0f1d8b495ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:48 +0000

    Add package cert-kube/1.0.0/cert-kube-1.0.0.tar.gz

---
+++ packages/cert-kube/cert-kube-1.0.0.tar.gz/cert-kube-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cert-kube',
+    packages=['cert_kube'],
+    description="Certbot plugin for Oracle Cloud Kubernetes",
+    long_description="Certbot plugin for Oracle Cloud Kubernetes",
+    author="Dilan Tharaka",
+    author_email='tharakamd6@gmail.com',
+    include_package_data=True,
+    version='1.0.0',
+    install_requires=[
+        'certbot',
+        'zope.interface',
+    ],
+    entry_points={
+        'certbot.plugins': [
+            'kube_authenticator = cert_kube.kube:Authenticator'
+        ],
+    },
+)
