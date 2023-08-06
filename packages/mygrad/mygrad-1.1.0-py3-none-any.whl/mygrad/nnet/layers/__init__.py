43198b7b27c9502560a8c2a10efa3a8da9fc7b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:03 +0000

    Add package mygrad/1.1.0/mygrad-1.1.0-py3-none-any.whl

---
+++ packages/mygrad/mygrad-1.1.0-py3-none-any.whl/mygrad/nnet/layers/__init__.py
@@ -0,0 +1,14 @@
+from .batchnorm import batchnorm
+from .conv import conv_nd
+from .pooling import max_pool
+
+__all__ = ["conv_nd", "max_pool", "batchnorm"]
+
+
+try:
+    from .gru import gru
+    from .recurrent import simple_RNN
+
+    __all__ += ["gru", "simple_RNN"]
+except ImportError:
+    pass
