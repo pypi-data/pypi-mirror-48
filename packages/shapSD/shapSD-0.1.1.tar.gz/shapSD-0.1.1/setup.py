5098811cc77a6e99a460dc7e3f08bc44fa1da330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:54 +0000

    Add package shapSD/0.1.1/shapSD-0.1.1.tar.gz

---
+++ packages/shapSD/shapSD-0.1.1.tar.gz/shapSD-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='shapSD',
+    version='0.1.1',
+    packages=['shapSD', 'shapSD.test', 'pysubgroup'],
+    package_dir={'shapSD': 'shapSD', 'shapSD.test': "shapSD.test",
+                 'pysubgroup': 'pysubgroup/pysubgroup',
+                 'pysubgroup.tests': 'pysubgroup/pysubgroup.tests', },
+    url='https://github.com/XiaoqiMa/shapSD',
+    download_url='https://github.com/XiaoqiMa/shapSD/archive/V0.1-alpha.tar.gz',
+    license='MIT License (MIT)',
+    author='xiaoqi',
+    author_email='xiaoqima2013@gmail.com',
+    description='Explain variable influence with shapley values in black box model through pattern mining',
+    install_requires=[
+        'pandas', 'scipy', 'numpy', 'matplotlib', 'eli5', 'shap',
+        'scikit-learn', 'lightgbm', 'graphviz'
+    ],
+    python_requires='>=3.6'
+)
