384a0450d77c4118c4dc3b9aa3d7359ec11a9419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:51 +0000

    Add package steimatzky/1.0.0/steimatzky-1.0.0.tar.gz

---
+++ packages/steimatzky/steimatzky-1.0.0.tar.gz/steimatzky-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='steimatzky',
+      version='1.0.0',
+      description='steimatzky.co.il Web Scraping library',
+      long_description='A Python library that allows developers to scrape data from steimatzky.co.il',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://github.com/sl4vkek/python-steimatzky',
+      packages=['steimatzky'],
+      install_requires=['requests'],
+      license="WTFPL"
+)
