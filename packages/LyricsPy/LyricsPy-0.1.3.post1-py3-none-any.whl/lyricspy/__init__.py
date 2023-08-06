579a94a10b7ebbd90e9a84ab703e4c9eb3fed680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:07 +0000

    Add package LyricsPy/0.1.3.post1/LyricsPy-0.1.3.post1-py3-none-any.whl

---
+++ packages/LyricsPy/LyricsPy-0.1.3.post1-py3-none-any.whl/lyricspy/__init__.py
@@ -0,0 +1,47 @@
+import requests
+from markdownify import markdownify as md
+from bs4 import BeautifulSoup
+import duckpy
+
+ddg = duckpy.Client()
+
+
+def letra(query,limit=4):
+    tr = 'a'
+    query = query.replace('www.letras', 'm.letras')
+    r = requests.get(query)
+    soup = BeautifulSoup(r.text, "html.parser")
+    a = soup.find('div', "lyric-cnt g-1")
+    if a is None:
+        a = soup.find('div', "lyric-tra_l")
+        tr = soup.find('div', "lyric-tra_r")
+    b = ''
+    for i in a.find_all('p'):
+        b += md(str(i))
+    c = soup.find("div", "lyric-title g-1")
+    musica = c.find('h1').get_text()
+    autor = c.find('a').get_text()
+    ret = {'autor': autor, 'musica': musica, 'letra': b.replace('\n\n\n','\n\n'), 'link': r.url}
+    if 'a' not in tr:
+        b = ''
+        for i in tr.find_all('p'):
+            b += md(str(i))
+        ret['traducao'] = b.replace('\n\n\n','\n\n')
+
+    return ret
+
+
+def auto(query,limit=4):
+    result = []
+    n = 0
+    for i in ddg.search('site:letras.mus.br ' + query):
+        try:
+            a = letra(i['url'])
+            result.append(a)
+            n += 1
+        except:
+            pass
+        if n == limit:
+            break
+
+    return result
