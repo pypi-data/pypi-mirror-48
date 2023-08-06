d9a0125b0a75e7b7d2e257a591154c4181b4359e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:09 +0000

    Add package sndata/0.3.1/SNData-0.3.1.tar.gz

---
+++ packages/sndata/SNData-0.3.1.tar.gz/SNData-0.3.1/SNData/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.7
+# -*- coding: UTF-8 -*-
+
+"""This package provides access to supernova light-curve data from various
+surveys.
+"""
+
+from . import csp, des, sdss
+from ._integrations import parse_snoopy_data
+from ._integrations import query_ned_coords
+from ._integrations import query_osc
+from ._integrations import query_osc_photometry
+from ._integrations import query_osc_spectra
+
+__version__ = '0.3.1'
+__author__ = 'Daniel Perrefort'
+__license__ = 'MIT'
