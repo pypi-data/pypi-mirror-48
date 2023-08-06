41fc8809d97c3c555766f22de74765c3449f230e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:05 +0000

    Add package awswrangler/0.0b10/awswrangler-0.0b10.tar.gz

---
+++ packages/awswrangler/awswrangler-0.0b10.tar.gz/awswrangler-0.0b10/awswrangler/exceptions.py
@@ -0,0 +1,22 @@
+class UnsupportedType(Exception):
+    pass
+
+
+class UnsupportedFileFormat(Exception):
+    pass
+
+
+class UnsupportedWriteMode(Exception):
+    pass
+
+
+class InvalidArguments(Exception):
+    pass
+
+
+class InvalidDataframeType(Exception):
+    pass
+
+
+class RedshiftLoadError(Exception):
+    pass
