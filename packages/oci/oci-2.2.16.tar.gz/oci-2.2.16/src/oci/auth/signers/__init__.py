e98cd3bd6aabfacca374451ee39214d5a241df67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:32 +0000

    Add package oci/2.2.16/oci-2.2.16.tar.gz

---
+++ packages/oci/oci-2.2.16.tar.gz/oci-2.2.16/src/oci/auth/signers/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+
+from .security_token_signer import SecurityTokenSigner, X509FederationClientBasedSecurityTokenSigner  # noqa: F401
+from .instance_principals_security_token_signer import InstancePrincipalsSecurityTokenSigner  # noqa: F401
+from .instance_principals_delegation_token_signer import InstancePrincipalsDelegationTokenSigner  # noqa: F401
