90ea7e1612f04a185d52a9f1315260356561a50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:04 +0000

    Add package igloo-python/0.9.6/igloo-python-0.9.6.zip

---
+++ packages/igloo-python/igloo-python-0.9.6.zip/igloo-python-0.9.6/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name='igloo-python',         # How you named your package folder (MyLib)
+    packages=['igloo'],   # Chose the same as "name"
+    version='0.9.6',
+    license='MIT',
+    description='Python client for igloo',
+    author='Igloo Team',
+    author_email='hello@igloo.ooo',
+    url='https://github.com/IglooCloud/igloo_python',
+    download_url='https://github.com/IglooCloud/igloo_python/archive/v_0.9.4.tar.gz',
+    keywords=['iot', 'igloo'],
+    install_requires=[
+        'requests', 'asyncio', 'pathlib', 'websockets', 'aiodataloader', 'aiohttp'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development ',
+        'License :: OSI Approved :: MIT License  ',
+        'Programming Language :: Python :: 3',
+    ],
+)
