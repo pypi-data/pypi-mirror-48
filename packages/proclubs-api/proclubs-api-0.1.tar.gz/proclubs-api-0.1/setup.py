7599ed4ae918714f943314bb3f063cdabcafbaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package proclubs-api/0.1/proclubs-api-0.1.tar.gz

---
+++ packages/proclubs-api/proclubs-api-0.1.tar.gz/proclubs-api-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='proclubs-api',
+    version='0.1',
+    description='An API do FIFA Pro Clubs',
+    url='',
+    author='Marcelo Augusto',
+    author_email='marcelotk_15@hotmail.com',
+    license='MIT',
+    packages=['proclubsapi'],
+    zip_safe=False
+    )
