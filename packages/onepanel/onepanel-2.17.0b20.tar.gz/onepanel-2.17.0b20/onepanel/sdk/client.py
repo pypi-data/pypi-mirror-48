23704ccfbe027aa88c1452d89dcfada65c0ce545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:56 +0000

    Add package onepanel/2.17.0b20/onepanel-2.17.0b20.tar.gz

---
+++ packages/onepanel/onepanel-2.17.0b20.tar.gz/onepanel-2.17.0b20/onepanel/sdk/client.py
@@ -0,0 +1,24 @@
+import onepanel
+from onepanel.utilities.login_helper import login_helper
+from onepanel.utilities.original_connection import Connection
+
+
+from onepanel.sdk.jobs import Jobs
+from onepanel.sdk.machine_types import MachineTypes
+from onepanel.sdk.volume_types import VolumeTypes
+from onepanel.sdk.environments import Environments
+
+class Client():
+    def __init__(self, username, password, token):
+        conn = Connection()
+        if username != "" and password != "" or token != "":
+            data = login_helper(conn, "", username, password, token)
+            if data is not None:
+                conn.set_credentials(data['sessions'][0]['token'],data['account']['uid'])
+        else:
+            conn.load_credentials()
+
+        self.jobs = Jobs(conn)
+        self.machine_types = MachineTypes(conn)
+        self.volume_types = VolumeTypes(conn)
+        self.environments = Environments(conn)
