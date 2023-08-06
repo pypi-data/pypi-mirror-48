224d5793ffebb8b4b88391b686c4712c445a7e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:29 +0000

    Add package pyApp-Redis/1.0/pyApp_Redis-1.0-py3-none-any.whl

---
+++ packages/pyApp-Redis/pyApp_Redis-1.0-py3-none-any.whl/pyapp_ext/redis/__init__.py
@@ -0,0 +1,15 @@
+"""
+PyApp - Redis Extension
+
+This extension provides a factory method for connecting to a Redis instance.
+
+"""
+from .factory import *
+
+
+class Extension:
+    """
+    pyApp Redis Extension
+    """
+    default_settings = ".default_settings"
+    checks = ".checks"
