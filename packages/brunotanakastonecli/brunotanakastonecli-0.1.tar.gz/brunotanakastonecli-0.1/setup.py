73bbfd86943b1275ee43727d978995fcf9a0ef22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:05 +0000

    Add package brunotanakastonecli/0.1/brunotanakastonecli-0.1.tar.gz

---
+++ packages/brunotanakastonecli/brunotanakastonecli-0.1.tar.gz/brunotanakastonecli-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='brunotanakastonecli',  
+     version='0.1',
+     scripts=['stone_cli'] ,
+     author="Bruno Tanaka",
+     author_email="bruno_tanaka100@ghotmail.com",
+     description="Criar a infraestrutura automatizada de forma automatica",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/brunotanaka/stone_cli",
+     packages=setuptools.find_packages(),
+     classifiers=
+     [
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
