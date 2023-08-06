005425a6d32d9ae71c26e8076e76619fa4bbd903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:08 +0000

    Add package coci/0.1.0/coci-0.1.0.tar.gz

---
+++ packages/coci/coci-0.1.0.tar.gz/coci-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="coci",
+    version="0.1.0",
+    author="Koki Fujiwara",
+    author_email="koki.fujiwara@exwzd.com",
+    description="Collective Observation on Causal Inference",
+    install_requires=["numpy", "seaborn"],
+)
