b483f3f9b0504b6e8971885247ff345b0aaf91b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package webcandy-client/0.0.3/webcandy-client-0.0.3.tar.gz

---
+++ packages/webcandy-client/webcandy-client-0.0.3.tar.gz/webcandy-client-0.0.3/opclib/__init__.py
@@ -0,0 +1,12 @@
+# TODO: Separate opclib, make it a separate library and repository
+from . import patterns
+from .fcserver import FadecandyServer
+
+pattern_names = patterns.__all__
+modifier_names = patterns.modifiers.__all__
+
+__all__ = [
+    'pattern_names',
+    'modifier_names',
+    'FadecandyServer'
+]
---
+++ packages/webcandy-client/webcandy-client-0.0.3.tar.gz/webcandy-client-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='webcandy-client',
+    version='0.0.3',
+    author='Graham Preston',
+    author_email='graham.preston@gmail.com',
+    description='Client-side code for communicating with a Webcandy server.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gcpreston/webcandy-client',
+    packages=find_packages(),
+    package_dir={'opclib': 'opclib'},
+    package_data={'opclib': ['bin/fcserver*']},
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'websockets'
+    ],
+    entry_points={
+        'console_scripts': [
+            'wc-client = webcandy_client.client:main',
+            'wc-controller = webcandy_client.controller:main'
+        ]
+    }
+)
