666d614fd652f01bbb0b734a30e586f709b8a700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:41 +0000

    Add package gxredis/0.3.0/gxredis-0.3.0-py3-none-any.whl

---
+++ packages/gxredis/gxredis-0.3.0-py3-none-any.whl/gxredis/__init__.py
@@ -0,0 +1,6 @@
+from .types import (
+    RedisType, RedisString, RedisList, RedisSet, RedisHash, RedisSortedSet,
+    RedisHyperLogLog,
+)
+from .dao import RedisDao
+from .decorator import ignore_redis_error
