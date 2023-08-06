62d4a807fae4070661c93f970de185480fe23616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:07 +0000

    Add package seetaas-beta-helper/0.1.9/seetaas-beta-helper-0.1.9.tar.gz

---
+++ packages/seetaas-beta-helper/seetaas-beta-helper-0.1.9.tar.gz/seetaas-beta-helper-0.1.9/seetaas_helper/result.py
@@ -0,0 +1,29 @@
+from seetaas_helper.base import _send_result, _ResultType, OPEN
+
+
+def send_scalar_result(**results):
+    """
+    发送单数值测试结果
+    eg: send_scalar_result(top1=0.92, top2=0.98)
+    """
+    if not OPEN:
+        return
+    for k, v in results.items():
+        _send_result(_ResultType.SCALAR_RESULT, k, v)
+
+
+def send_curve_result(title, x_name, x_points, y_name, y_points):
+    """
+    发送测试协议输出为曲线的测试结果
+    eg: send_curve_result("Recall and Precision", "recall", [0.0, 0.2, 0.4, 0.6, 0.8, 1.0], "precision",[0.0, 0.8, 0.89, 0.94, 0.96, 1.0])
+    """
+    if not OPEN:
+        return
+    _send_result(_ResultType.CURVE_RESULT, title, {x_name: x_points, y_name: y_points})
+
+
+if __name__ == '__main__':
+    send_scalar_result(top1=0.92, top2=0.98)
+    send_curve_result("Recall and Precision",
+                      "recall", [0.0, 0.2, 0.4, 0.6, 0.8, 1.0],
+                      "precision", [0.0, 0.8, 0.89, 0.94, 0.96, 1.0])
