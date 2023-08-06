344f5e27cfaa59a210908655de50dd43cdd07a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:20 +0000

    Add package dharma-callback-fuzzer/1.2.4/dharma-callback-fuzzer-1.2.4.tar.gz

---
+++ packages/dharma-callback-fuzzer/dharma-callback-fuzzer-1.2.4.tar.gz/dharma-callback-fuzzer-1.2.4/dharma/__version__.py
@@ -0,0 +1,14 @@
+__title__ = 'dharma-callback-fuzzer'
+__version__ = '1.2.4'
+__license__ = 'MPL 2.0'
+__description__ = 'Generation-based, context-free grammar fuzzer.'
+__keywords__ = 'fuzzer fuzzing security testing generator'
+__author__ = 'Jiajun Peng'
+__author_email__ = 'jiajunp@fb.com'
+__maintainer__ = 'Mozilla Fuzzing Team'
+__maintainer_email__ = 'fuzzing@mozilla.com'
+__url__ = 'https://github.com/MozillaSecurity/dharma'
+__project_urls__ = {
+    'Bug Tracker': 'https://github.com/MozillaSecurity/dharma/issues'
+}
+__download_url__ = "https://github.com/MozillaSecurity/dharma/tarball/" + __version__
