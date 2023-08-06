8190d96a2d1e7dc10a69000bd28cf3dfa10c5fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package dbapi/0.0.14/dbapi-0.0.14.tar.gz

---
+++ packages/dbapi/dbapi-0.0.14.tar.gz/dbapi-0.0.14/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Authors. All Rights Reserved.
+# License: MIT.
+# Author: acrazing <joking.young@gmail.com>.
+# File: setup.
+from setuptools import setup
+
+with open('./README.md', encoding='utf8') as f:
+    desc = f.read()
+
+with open('./requirements.txt') as f:
+    requires = [r.split('=', 1)[0] for r in f.read().split('\n') if r]
+
+setup(
+    name='dbapi',
+    version='0.0.14',
+    description='基于爬虫的豆瓣API SDK',
+    url='https://github.com/acrazing/dbapi',
+    author='acrazing',
+    author_email='joking.young@gmail.com',
+    license='MIT',
+    keywords='豆瓣 douban 豆瓣小组 api',
+    long_description=desc,
+    long_description_content_type='text/markdown',
+    install_requires=requires,
+    packages=['dbapi'],
+    entry_points={
+        'console_scripts': ['dbapi=dbapi.cli:main'],
+    },
+)
