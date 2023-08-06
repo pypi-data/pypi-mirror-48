16bed1b7f8679ce51fd14c05d2b1de73454bec70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:34 +0000

    Add package irl2019/0.8/irl2019-0.8.tar.gz

---
+++ packages/irl2019/irl2019-0.8.tar.gz/irl2019-0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='irl2019',
+    version='0.8',
+    description='Temporary package to support an event.',
+    url='',
+    author='Matthew Honnibal',
+    author_email='matt@explosion.ai',
+    license='MIT',
+    packages=['irl2019'],
+    install_requires=["spacy>=2.1.4", "google_cloud_storage"],
+    zip_safe=False
+)
