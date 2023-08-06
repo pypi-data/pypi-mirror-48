8c5b0e09402cde6925b38ad4411ae1505d2f6958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:34 +0000

    Add package csvql/0.0.3/csvql-0.0.3.tar.gz

---
+++ packages/csvql/csvql-0.0.3.tar.gz/csvql-0.0.3/README.md
@@ -0,0 +1,32 @@
+# csvql
+csvql is an easy to use, open source command line tool to
+work with CSV files
+
+csvql has no requirements or dependencies other than Python 3
+
+# Features
+- Load one or more CSV files into an SQLite database as tables
+- Query and modify the data using SQL
+- Create new CSV files from the results of SQL queries
+
+## Installation
+
+You can install, upgrade, and uninstall ``csvql.py`` with these commands:
+
+```sh
+  $ pip3 install csvql
+  $ pip3 install --upgrade csvql
+  $ pip3 uninstall csvql
+```
+
+## Help
+
+To get help use:
+
+```sh
+  $ csvql -h
+```
+
+# Examples
+
+See ``examples`` folder for common and advanced use cases
---
+++ packages/csvql/csvql-0.0.3.tar.gz/csvql-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csvql",
+    version="0.0.3",
+    author="Edgar Nova",
+    author_email="ragnarok540@gmail.com",
+    description="csvql command line tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="csv, sql, sqlite, csvql, query",
+    url="https://github.com/Ragnarok540/csvql",
+    py_modules=['csvql'],
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'csvql = csvql:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
