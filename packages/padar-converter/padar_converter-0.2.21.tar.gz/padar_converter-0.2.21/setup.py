ea4694dc780ab7b46b5fa7602c37ddc12ef6e788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:20 +0000

    Add package padar-converter/0.2.21/padar_converter-0.2.21.tar.gz

---
+++ packages/padar-converter/padar_converter-0.2.21.tar.gz/padar_converter-0.2.21/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='padar_converter',
+    version='0.2.21',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Extension of converters for sensor data used in padar package',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "gpxpy>=1.3.2",
+        "pandas>=0.23.0",
+        "chardet>=3.0.4"
+    ],
+)
