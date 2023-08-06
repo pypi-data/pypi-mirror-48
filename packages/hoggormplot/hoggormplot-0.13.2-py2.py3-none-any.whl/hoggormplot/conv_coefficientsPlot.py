c2d6a329017def4aa67cae99d45c11e2a05cb688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:43 +0000

    Add package hoggormplot/0.13.2/hoggormplot-0.13.2-py2.py3-none-any.whl

---
+++ packages/hoggormplot/hoggormplot-0.13.2-py2.py3-none-any.whl/hoggormplot/conv_coefficientsPlot.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import hoggorm
+from .main_plot import plot
+
+
+def coefficients(model, comp=[1], figsize=None):
+    """
+    This is a convenience plot function which generates coefficients plots of 
+    hoggorm models.
+    
+    
+    PARAMETERS
+    ----------
+    model : nipalsPCR/nipalsPLSR1/nipalsPLSR2 class object computed in Hoggorm
+    
+    comp : list, optional
+        The list contains components to be displayed. Defaults to [1].
+    
+    figsize : tuple, optional 
+        Sets figure width and height in inches
+    
+    RETURNS
+    -------
+    A coefficients plot based on the input Hoggorm model.
+    
+    
+    EXAMPLES
+    --------
+    >>> import hoggorm as ho
+    >>> import hoggormplot as hopl
+    >>> myModel = ho.nipalsPLS2(arrX=my_X_data, arrY=my_Y_data, cvType=["loo"])
+    >>> hopl.coefficients(myModel, comp=[1, 2, 3])
+    >>> hopl.coefficients(myModel)
+    >>> hopl.coefficients(myModel, comp=[2])
+    """
+    plot(model=model, plots=[5], comp=comp, figsize=figsize)
+
+
+def coeffs(model, comp=[1], figsize=None):
+    """
+    This is a convenience plot function which generates coefficients plots of 
+    hoggorm models. Note that this convenience function is identical to 
+    ``hoggorm.resPlotting.coefficients``.
+    
+    PARAMETERS
+    ----------
+    model : nipalsPCR/nipalsPLSR1/nipalsPLSR2 class object computed in Hoggorm
+    
+    comp : list, optional
+        The list contains components to be displayed. Defaults to [1].
+    
+    figsize : tuple, optional 
+        Sets figure width and height in inches
+    
+    RETURNS
+    -------
+    A coefficients plot based on the input Hoggorm model.
+    
+    
+    EXAMPLES
+    --------
+    >>> import hoggorm as ho
+    >>> import hoggormplot as hopl
+    >>> myModel = ho.nipalsPLS2(arrX=my_X_data, arrY=my_Y_data, cvType=["loo"])
+    >>> hopl.coeffs(myModel, comp=[1, 2, 3])
+    >>> hopl.coeffs(myModel)
+    >>> hopl.coeffs(myModel, comp=[2])
+    """
+    plot(model=model, plots=[5], comp=comp, figsize=figsize)
+
+
---
+++ packages/hoggormplot/hoggormplot-0.13.2-py2.py3-none-any.whl/hoggormplot/conv_predictPlot.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+import hoggorm
+from .main_plot import plot
+
+
+def predict(model, comp=[1,2], 
+                 objNames=[], newX=[], newY=[], newObjNames=[], figsize=None):
+    """
+    This is a convenience function that generates plots of predicted vs. 
+    original values of hoggorm models.
+    
+    
+    PARAMETERS
+    ----------
+    model : nipalsPCR/nipalsPLSR1/nipalsPLSR2 class object computed in Hoggorm 
+        The statistical results of the submitted model will be visualized.
+    
+    comp : list, optional
+        The list contains components to be displayed. Defaults to [1,2].
+    
+    newX : list, optional
+        The list contains one array of new measurement data X.
+    
+    newY : list, optional
+        The list contains one array of new measurements of Y.
+    
+    newObjNames : list, optional
+        The list contains object names of new measurement data in X or Y.
+    
+    figsize : tuple, optional 
+        Sets figure width and height in inches
+    
+    RETURNS
+    -------
+    A predicted vs. measured plot.
+    
+    
+    EXAMPLES
+    --------
+    >>> import hoggorm as ho
+    >>> import hoggormplot as hopl
+    >>> myModel = ho.nipalsPLS2(arrX=my_X_data, arrY=my_Y_data, cvType=["loo"])
+    >>> hopl.predict(myModel)
+    >>> hopl.predict(myModel, comp=[3, 4])
+    """
+    plot(model, comp=comp, plots=[7], 
+                 objNames=objNames, newX=newX, newY=newY, newObjNames=newObjNames,
+                 figsize=figsize)
+
