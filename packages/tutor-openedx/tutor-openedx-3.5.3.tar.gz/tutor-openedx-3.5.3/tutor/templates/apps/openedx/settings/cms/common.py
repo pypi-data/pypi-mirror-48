a56bb6cc4263c411c120a0bffd8e2f49742ab184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:10 +0000

    Add package tutor-openedx/3.5.3/tutor-openedx-3.5.3.tar.gz

---
+++ packages/tutor-openedx/tutor-openedx-3.5.3.tar.gz/tutor-openedx-3.5.3/tutor/templates/apps/openedx/settings/cms/common.py
@@ -0,0 +1,33 @@
+# Load module store settings from config files
+update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
+
+# Set uploaded media file path
+MEDIA_ROOT = "/openedx/data/uploads/"
+
+# Video settings
+VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+
+# Change syslog-based loggers which don't work inside docker containers
+LOGGING["handlers"]["local"] = {
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "all.log"),
+    "formatter": "standard",
+}
+LOGGING["handlers"]["tracking"] = {
+    "level": "DEBUG",
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "tracking.log"),
+    "formatter": "standard",
+}
+LOGGING["loggers"]["tracking"]["handlers"] = ["console", "local", "tracking"]
+
+LOCALE_PATHS.append("/openedx/locale")
+
+# Create folders if necessary
+for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+{{ patch("openedx-common-settings") }}
+{{ patch("openedx-cms-common-settings") }}
