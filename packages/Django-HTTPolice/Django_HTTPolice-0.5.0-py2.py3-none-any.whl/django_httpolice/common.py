8f3a3f535874d7bacb213bad8434f8fdbac23a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:23 +0000

    Add package Django-HTTPolice/0.5.0/Django_HTTPolice-0.5.0-py2.py3-none-any.whl

---
+++ packages/Django-HTTPolice/Django_HTTPolice-0.5.0-py2.py3-none-any.whl/django_httpolice/common.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.utils import six
+
+
+DEFAULT_SETTINGS = {
+    'HTTPOLICE_ENABLE': False,
+    'HTTPOLICE_BACKLOG': 20,
+    'HTTPOLICE_SILENCE': [1070, 1110],    # duplicated in docs
+    'HTTPOLICE_RAISE': None,
+}
+
+
+def get_setting(name):
+    name = 'HTTPOLICE_' + name
+    return getattr(settings, name, DEFAULT_SETTINGS[name])
+
+
+class ProtocolError(Exception):
+
+    """A protocol problem in a response produced by the application."""
+
+    def __init__(self, exchange):
+        # Importing `httpolice` can execute a lot of code,
+        # so we only do it when it's really time for action.
+        import httpolice
+        buf = six.BytesIO()
+        httpolice.text_report([exchange], buf)
+        super(ProtocolError, self).__init__(
+            u'HTTPolice found problems in this response:\n' +
+            buf.getvalue().decode('utf-8')
+        )
