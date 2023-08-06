f8fb06146d2f899c2c6c30db01e4db2fb7215d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:59 +0000

    Add package python-clu/0.1.1/python-clu-0.1.1.tar.gz

---
+++ packages/python-clu/python-clu-0.1.1.tar.gz/python-clu-0.1.1/clu/constants/consts.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os
+import socket
+import sys
+import sysconfig
+
+# Possible names for builtin modules:
+BUILTINS = ('__builtins__', '__builtin__', 'builtins', 'builtin')
+
+# Are we debuggin out?
+DEBUG = bool(int(os.environ.get('DEBUG', '0'), base=10))
+
+# Flag for deleting temporary files:
+DELETE_FLAG = getattr(os, 'O_TEMPORARY', 0)
+
+# A prefix to use when creating new modules programmatically:
+DYNAMIC_MODULE_PREFIX = sys.intern('__dynamic_modules__')
+
+# On non-macOS platforms this may be awry:
+ENCODING = sys.intern(sys.getfilesystemencoding().upper()) # 'UTF-8'
+
+# Possible names for file arguments (used for introspection):
+FILE_ARGUMENT_NAMES = ('path', 'pth', 'file')
+
+# The hostname for this computer:
+HOSTNAME = sys.intern(socket.gethostname())
+
+# The __name__ of a lambda function:
+lam = lambda: None
+LAMBDA = sys.intern(getattr(lam, '__qualname__',
+                    getattr(lam, '__name__', None)))
+
+# N.B. this may or may not be a PY2/PY3 thing:
+MAXINT = getattr(sys, 'maxint',
+         getattr(sys, 'maxsize', (2 ** 64) / 2))
+
+# The PATH environment variable, with a sensible default:
+PYTHON_BIN = os.path.join(sysconfig.get_path('data'), 'bin')
+DEFAULT_PATH = os.pathsep.join(filter(os.path.exists, (PYTHON_BIN,
+                                                       "/usr/local/bin",
+                                                       "/bin",  "/usr/bin",
+                                                       "/sbin", "/usr/sbin")))
+PATH = os.getenv("PATH", DEFAULT_PATH)
+
+# The name of this project
+PROJECT_NAME = sys.intern('clu')
+
+# Determine if our Python is three’d up:
+PY3 = sys.version_info.major > 2
+
+# Determine if we’re on PyPy:
+PYPY = hasattr(sys, 'pypy_version_info')
+
+# Qualifier for qualified-name operations:
+QUALIFIER = sys.intern(os.extsep)
+
+# List of Python’s built-in singleton types:
+NoneType = type(None)
+EllipsisType = type(Ellipsis)
+NotImplementedType = type(NotImplemented)
+
+SINGLETON_TYPES = (bool, NoneType, EllipsisType, NotImplementedType)
+
+# Determine if we’re in TextMate:
+TEXTMATE = 'TM_PYTHON' in os.environ
+
+# For terminal-printing:
+if TEXTMATE:
+    SEPARATOR_WIDTH = 100
+else:
+    from .terminalsize import get_terminal_size
+    SEPARATOR_WIDTH = get_terminal_size(default=(100, 25))[0]
+
+# WTF HAX:
+TOKEN = sys.intern(' -')
+
+# Stuff we want to keep out of namespaces:
+pytuple = lambda *attrs: tuple('__%s__' % str(atx) for atx in attrs)
+VERBOTEN = pytuple('all', 'cached', 'loader', 'file', 'spec')
+VERBOTEN += BUILTINS
+VERBOTEN += ('Namespace', 'SimpleNamespace')
+
+# XDG_RUNTIME_DIR support:
+basedir = "/usr/local/var/run/xdg"
+symlink = os.path.join(basedir, 'CURRENT')
+
+XDG_RUNTIME_BASE = basedir
+XDG_RUNTIME_DIR = symlink
+XDG_RUNTIME_MODE = 0o700
+
+class NoDefault(object):
+    """ A singleton object to signify a lack of an argument. """
+    __slots__ = tuple()
+    def __new__(cls, *a, **k):
+        return cls
+
+__all__ = ('BUILTINS',
+           'DEBUG',
+           'DELETE_FLAG',
+           'DYNAMIC_MODULE_PREFIX',
+           'ENCODING',
+           'FILE_ARGUMENT_NAMES',
+           'HOSTNAME',
+           'LAMBDA',
+           'MAXINT',
+           'PATH',
+           'PROJECT_NAME',
+           'PY3', 'PYPY',
+           'QUALIFIER',
+           'SEPARATOR_WIDTH',
+           'SINGLETON_TYPES',
+           'TEXTMATE',
+           'TOKEN',
+           'VERBOTEN',
+           'XDG_RUNTIME_BASE', 'XDG_RUNTIME_DIR',
+                               'XDG_RUNTIME_MODE',
+           'NoDefault')
+
+__dir__ = lambda: list(__all__)
---
+++ packages/python-clu/python-clu-0.1.1.tar.gz/python-clu-0.1.1/clu/repl/enums.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from enum import EnumMeta
+
+class alias(object):
+    
+    """ Alias one Enum class instance to another. To wit:
+        
+            @unique
+            class Numbers(Enum):
+                ONE = 1
+                TWO = 2
+                THREE = 3
+                UNO = alias(ONE)
+                DOS = alias(TWO)
+                TRÉS = alias(THREE)
+        
+        … unlike the enum documentation, which would have you
+        create another set enum member-instances called UNO,
+        DOS and TRÉS with the same values, respectively, as
+        ONE, TWO, and THREE – using the alias(…) descriptor
+        is a true aliasing operation, as accessing any alias
+        instances e.g. `Numbers.DOS` actually gives you the
+        actual aliased-to instance `Numbers.TWO` – which, as
+        the documentation points out, they are all singletons,
+        so `Numbers.TWO is Numbers.DOS` evaluates to `True`.
+        
+        Note that “@unique” has been used on this enum – alias
+        members don’t count as new member-instances, so this
+        enum class still contains three unique members, plus
+        the three alias descriptors, which that is considered
+        OK in the eyes of the @unique decorator.
+        
+        ISN’T ALL OF THAT FUCKING AWESOME?!?!? I think so. Yes!
+    """
+    
+    def __init__(self, instance, name=None, cls=None):
+        """ Set up the alias, passing an enum instance.
+            
+            N.B. Use the “name” and “cls” arguments under Python 2
+            or Python 3 < 3.6 – as they don’t yet support the
+            “__set_name__” method (q.v. definition sub.)
+            
+        """
+        self.aliased = instance
+        if name is not None and cls is not None:
+            self.name = name
+            self.register(cls)
+    
+    def __get__(self, instance=None, cls=None):
+        """ Return the aliased enum instance. """
+        if cls is None:
+            cls = type(instance)
+        for thing in cls:
+            if thing.value == self.aliased:
+                return thing
+        return self.aliased
+    
+    def __set_name__(self, cls, name):
+        """ Register the alias within the __aliases__ dict in
+            the class (if it has one).
+            
+            N.B. This only gets called on Python 3.6+
+        """
+        self.name = name
+        self.register(cls)
+    
+    def register(self, cls):
+        """ Register the alias member with the parent class,
+            if it supports such things (that is to say, if it
+            has a dictionary attribute “__aliases__”).
+        """
+        if hasattr(cls, '__aliases__'):
+            if self.name in cls.__aliases__:
+                message = "Enum already contains an alias named %s" % self.name
+                raise AttributeError(message)
+            cls.__aliases__[self.name] = self.aliased
+
+class AliasingEnumMeta(EnumMeta):
+    
+    def __new__(metacls, name, bases, attributes, **kwargs):
+        """ Ensure __aliases__ is a dictionary attribute on the new class.
+            
+            It is not strictly necessary to make this class an ancestor to
+            your enums in order to use the `alias(…)` descriptor function,
+            as defined above – if you do, the enums you define with it will
+            conveniently furnish the __aliases__ dictionary, which is like
+            the normal enum __members__ directory only with aliases. Yes.
+        """
+        
+        if '__aliases__' not in attributes:
+            attributes['__aliases__'] = {}
+        
+        return super(AliasingEnumMeta, metacls).__new__(metacls, name,
+                                                                 bases,
+                                                                 attributes,
+                                                               **kwargs)
+
+__all__ = ('alias', 'AliasingEnumMeta')
+__dir__ = lambda: list(__all__)
