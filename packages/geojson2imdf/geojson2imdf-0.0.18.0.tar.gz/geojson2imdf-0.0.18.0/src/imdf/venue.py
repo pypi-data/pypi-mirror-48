42b9a269659a53cbdcec8b65379f7a3f894e7ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:20 +0000

    Add package geojson2imdf/0.0.18.0/geojson2imdf-0.0.18.0.tar.gz

---
+++ packages/geojson2imdf/geojson2imdf-0.0.18.0.tar.gz/geojson2imdf-0.0.18.0/src/imdf/venue.py
@@ -0,0 +1,11 @@
+
+class Venue:
+
+    def __init__(self):
+        print("yo")
+
+    def venue_function(self, msg):
+        print(f"function: {msg}")
+
+    def get_stuff(self):
+        return ('stuff')
