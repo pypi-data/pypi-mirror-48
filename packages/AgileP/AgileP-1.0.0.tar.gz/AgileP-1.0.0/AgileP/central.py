d3fbfddea0a0de871ca3f1c4b5e8ebc3d018621b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package AgileP/1.0.0/AgileP-1.0.0.tar.gz

---
+++ packages/AgileP/AgileP-1.0.0.tar.gz/AgileP-1.0.0/AgileP/central.py
@@ -0,0 +1,39 @@
+import copy
+from .rule import Descriptor
+
+
+class CheckedMeta(type):
+    def __new__(cls, cls_name, cls_bases, cls_methods):
+        for m_name, m_instance in cls_methods.items():
+            if isinstance(m_instance, Descriptor):
+                m_instance.name = m_name
+        return type.__new__(cls, cls_name, cls_bases, cls_methods)
+
+
+class Agp(metaclass=CheckedMeta):
+    def __init__(self, *args, **kwargs):
+        self._fields = [x for x in self.__dir__() if not x.startswith("_")]
+
+        # 字段基本校验。校验传入和定义是否一致
+        if len(args) != len(self._fields):
+            raise TypeError(f'Expected {len(self._fields)} arguments')
+
+        # 动态为描述起赋值，调用对应 set 方法
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+        # 动态为描述起赋值（声明式）
+        extra_args = kwargs.keys() - self._fields
+        for name in extra_args:
+            setattr(self, name, kwargs.pop(name))
+
+        # 字段基本校验
+        if kwargs:
+            raise TypeError(f'parameter {kwargs} beyond range')
+
+        instance_dict = self.__dict__
+        result = copy.deepcopy(instance_dict)
+        for name in instance_dict:
+            if name.startswith("_"):
+                result.pop(name)
+        self.values = result
---
+++ packages/AgileP/AgileP-1.0.0.tar.gz/AgileP-1.0.0/README.md
@@ -0,0 +1,49 @@
+# AgileP
+
+### 简介
+AgileP 是一个灵活的参数校验工具，支持绝大多数参数校验。
+
+
+
+### 安装
+
+```
+pip install 
+```
+
+
+
+
+
+### 示例
+
+```python
+from AgileP.central import Agp
+from AgileP.model.extension import SizedString, UnsignedInteger, UnsignedFloat, Email, Bool
+
+
+class Params(Agp):
+    """
+    定义数据校验类
+    """
+    name = SizedString(size=8)
+    age = UnsignedInteger(min=100)
+    salary = UnsignedFloat(max=10000)
+    admin = Bool()
+    email = Email()
+
+params = Params("zhang", 100, 9800.1, True, "1234567@qq.com")
+print(params.values)
+
+```
+
+
+
+### 规划
+
++ [x] 去除字段类型中的名称 
++ [x] 支持更多类型 
++ [ ] 支持更多参数传入方式 
++ [ ] 更新优化错误提示 
+
+
