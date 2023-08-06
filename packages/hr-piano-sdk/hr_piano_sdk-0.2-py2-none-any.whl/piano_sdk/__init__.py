dc4ace32653c465fd67e8470c0583e550ab859dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:33 +0000

    Add package hr-piano-sdk/0.2/hr_piano_sdk-0.2-py2-none-any.whl

---
+++ packages/hr-piano-sdk/hr_piano_sdk-0.2-py2-none-any.whl/piano_sdk/__init__.py
@@ -0,0 +1,28 @@
+# Configuration variables
+
+aid = ''
+api_token = ''
+api_base = 'https://api.tinypass.com/api/v3'
+
+name = "Piano SDK"
+
+# Resources
+from .resources import (  # noqa
+    Access,
+    App,
+    Conversion,
+    Export,
+    Inquiry,
+    Offer,
+    Promotion,
+    Resource,
+    Subscription,
+    OfferTemplate,
+    Term,
+    User,
+    Webhook,
+)
+
+from .security_utils import SecurityUtils  # noqa
+from .enums import WebhookEventTypes  # noqa
+
