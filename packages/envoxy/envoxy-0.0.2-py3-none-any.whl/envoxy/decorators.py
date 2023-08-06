dd090b4534317c8facf8b93f7b0c9146a6aea8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:37 +0000

    Add package envoxy/0.0.2/envoxy-0.0.2-py3-none-any.whl

---
+++ packages/envoxy/envoxy-0.0.2-py3-none-any.whl/envoxy/decorators.py
@@ -0,0 +1,44 @@
+from .views.containers import Response
+from functools import wraps
+from .auth.backends import AuthBackendMixin
+from .utils.logs import Log
+
+def event_wrapper(object_):
+    return Response(object_).to_flask()
+
+def on(**kwargs):
+
+    def _decorate(klass):
+
+        class Meta:
+            pass
+
+        klass.__metaclass__ = Meta
+
+        for _key, _value in kwargs.items():
+            setattr(klass.__metaclass__, _key, _value)
+
+        return klass
+
+    return _decorate
+
+
+class auth_required(object):
+
+    def __init__(self, roles=None):
+        self.role_list = roles
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def wrapped_func(view, request, *args, **kwargs):
+            Log.warning(self.role_list)
+            if self.role_list:
+                kwargs.update({
+                    'roles_needed': self.role_list
+                })
+            Log.warning(kwargs)
+            AuthBackendMixin().authenticate(request, *args, **kwargs)
+
+            return func(view, request, *args, **kwargs)
+        return wrapped_func
