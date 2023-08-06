44142cdc2636669ffec5047cd815afe282e525c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:36 +0000

    Add package setopati-reader/2.0.0/setopati-reader-2.0.0.tar.gz

---
+++ packages/setopati-reader/setopati-reader-2.0.0.tar.gz/setopati-reader-2.0.0/setup.py
@@ -0,0 +1,34 @@
+"""Setup script for setopati-reader"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="setopati-reader",
+    version="2.0.0",
+    description="Read the latest news from setopati",
+    long_description=README,
+    url="https://github.com/jitenpu/setopati-reader",
+    author="Jitendra",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[
+        "feedparser", "html2text", "importlib_resources", "typing"
+    ],
+    entry_points={"console_scripts": ["rss_package=reader.__main__:main"]},
+)
