30799ba74f77293a4ce8ce2b34fdec1359173b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:59 +0000

    Add package pyApp-SQLAlchemy/1.0/pyApp_SQLAlchemy-1.0-py3-none-any.whl

---
+++ packages/pyApp-SQLAlchemy/pyApp_SQLAlchemy-1.0-py3-none-any.whl/pyapp_ext/sqlalchemy/default_settings.py
@@ -0,0 +1,16 @@
+DATABASE_ENGINES = None
+"""
+SQLAlchemy engine connection settings
+
+Example settings::
+
+    DATABASE_ENGINES = {
+        "default": {
+            "url": "driver://user:password@host/database",
+            "connect_args": {
+                # Engine specific args
+            }
+        }
+    }
+
+"""
