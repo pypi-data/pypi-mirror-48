419ef481f4e50f2c3b238bd609f8a348a26376d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:23 +0000

    Add package tox-direct/0.3.4/tox-direct-0.3.4.tar.gz

---
+++ packages/tox-direct/tox-direct-0.3.4.tar.gz/tox-direct-0.3.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tox-direct",
+    description="plugin for tox - run everything directly (tox creates no virtual env)",
+    version="0.3.4",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    entry_points={"tox": ["direct = tox_direct.hookimpls"]},
+    install_requires=["tox>=3.12,<4", "py", "pathlib2"],
+    extras_require={"test": ["pytest"], "publish": ["twine"]},
+)
