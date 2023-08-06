74db578ca65c6c1369e415e74674287d481b00ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:00 +0000

    Add package msmbps/5.0.0.8/msmbps-5.0.0.8.tar.gz

---
+++ packages/msmbps/msmbps-5.0.0.8.tar.gz/msmbps-5.0.0.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='msmbps',
+    version='5.0.0.8',
+    description='Test latency(ms) and download(MBps) of many cloud service providers. Source code of this program is available at https://github.com/msmbps/msmbps-python',
+    scripts=['msmbps'],
+)
