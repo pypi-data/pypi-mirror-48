3517fe1dfac40266cf989b365016f3090c3b70fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:47 +0000

    Add package aiovk/3.0.0/aiovk-3.0.0.tar.gz

---
+++ packages/aiovk/aiovk-3.0.0.tar.gz/aiovk-3.0.0/aiovk/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '3.0.0'
+
+from aiovk.sessions import ImplicitSession, TokenSession, AuthorizationCodeSession
+from aiovk.api import API
+from aiovk.longpoll import LongPoll
---
+++ packages/aiovk/aiovk-3.0.0.tar.gz/aiovk-3.0.0/setup.py
@@ -0,0 +1,42 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
+        __file__)), 'aiovk', '__init__.py'), 'r', 'latin1') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+setup(
+    name='aiovk',
+    version=version,
+
+    author='Fahreev Eldar',
+    author_email='fahreeve@yandex.ru',
+
+    url='https://github.com/Fahreeve/aiovk',
+    description='vk.com API python wrapper for asyncio',
+    long_description=readme,
+
+    packages=find_packages(),
+    install_requires='aiohttp>=3.2.0',
+
+    license='MIT License',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='vk.com api vk wrappper asyncio',
+)
