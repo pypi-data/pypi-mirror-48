57da062059df5267a3d4a6d88c02a02fb9de0744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:06 +0000

    Add package test_library/0.0.1/test_library-0.0.1.tar.gz

---
+++ packages/test_library/test_library-0.0.1.tar.gz/test_library-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import  setup,find_packages
+
+setup(
+    name='test_library',
+    version='0.0.1',
+    keywords='just a test',
+    description='just a test',
+    license='',
+    url='',
+    author='wujiating',
+    author_email='wujiating@mesalab.cn',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
