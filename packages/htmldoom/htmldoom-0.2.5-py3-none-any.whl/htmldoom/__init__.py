cd0bc380034285ccfc42cbad4b5bf9057cb92d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:59 +0000

    Add package htmldoom/0.2.5/htmldoom-0.2.5-py3-none-any.whl

---
+++ packages/htmldoom/htmldoom-0.2.5-py3-none-any.whl/htmldoom/__init__.py
@@ -0,0 +1,17 @@
+__author__ = "Arijit Basu"
+__email__ = "sayanarijit@gmail.com"
+__homepage__ = "https://github.com/sayanarijit/htmldoom"
+__description__ = "Write safer and cleaner HTML using Python"
+__version__ = "v0.2.5"
+__license__ = "MIT"
+__all__ = [
+    "__author__",
+    "__email__",
+    "__homepage__",
+    "__description__",
+    "__version__",
+    "__license__",
+    "elements",
+    "layouts",
+    "functions",
+]
