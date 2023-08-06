5ca84603856f794738f47028cc588d3f7d3442fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:28 +0000

    Add package codewars/0.0.4/codewars-0.0.4-py2.7.egg

---
+++ packages/codewars/codewars-0.0.4-py2.7.egg/codewars/wifi/__init__.py
@@ -0,0 +1,75 @@
+from scapy.all import *
+
+class Wifi(object):
+    """
+    Note
+    ----
+    In order to use this module u need to provide the python application the right
+    permissions. Most likly sudo. This is because `scapy` makes use of your network
+    card. Therefor it needs permissions.
+
+    Warning
+    -------
+    The error `AttributeError: 'L2ListenSocket' object has no attribute 'ins'`
+    is a reaction to the note above. Not the right permissions.
+    """
+    def __init__(self, iAddr="enp0s31f6"):
+        """You can use `ifconfig` to find the network name.
+        Something like `mon0`.
+        
+        Parameters
+        ----------
+        iAddr
+            Type: String
+            Default: 'enp0s31f6'
+            Description: The network interface to sniff on
+        """
+        conf.iface = iAddr
+        self.__listener = []
+    
+    def sniff(self, totalResults = 10):
+        """Sniff Network packages on the selected `iAddr`
+
+        Parameters
+        ----------
+        totalResults
+            Type: Int
+            Default: 10
+            Description: The sniff amount.
+
+        Returns
+        -------
+        List
+            Type: List
+            Content: `scapy.plist.PacketList`
+            Description: A list of network packages adresses that are discovered.
+            Limited to the amount of the `totalResults` parameter.
+        """
+        try:
+            return [pk for pk in sniff(count=totalResults)]
+        except PermissionError:
+            raise OSError("Run as admin or provide the right permissions to this application in order to use the codewars wifi module")
+
+    def info(self, packages = []):
+        """Look for information about a package
+
+        Parameters
+        ----------
+        packages
+            Type: `scapy.Packet`
+            Default: Empty List
+            Description: A list with packages founnd with the `sniff` method.
+
+        Returns
+        -------
+        List
+            Type: List
+            Content: Objects
+            Description: Information of each package
+        """
+        try:
+            if packages:
+                return [pk.summary() for pk in packages]
+            else: raise AttributeError("The list you want to sniff info from is empty!")
+        except PermissionError:
+            raise OSError("Run as admin or provide the right permissions to this application in order to use the codewars wifi module")
