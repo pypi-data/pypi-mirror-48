d345d96f22f261088fae8b5f9a4e370557189829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:17 +0000

    Add package swutil/1.4.2/swutil-1.4.2.tar.gz

---
+++ packages/swutil/swutil-1.4.2.tar.gz/swutil-1.4.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='swutil',
+      version='1.4.2',
+      python_requires='>=3.6',
+      description='Provide common code for validation, multiprocessing, logging, decorator, configuration, plotting, etc.',
+      long_description=open('README.rst').read(),
+      author='Soeren Wolfers',
+      author_email='soeren.wolfers@gmail.com',
+      url = 'https://github.com/soerenwolfers/swutil',
+      packages=find_packages(exclude=['*tests']),#,'examples*']),
+      install_requires=['numpy','matplotlib','matplotlib2tikz','memory_profiler','pathos','dill','PyPDF2','readline']
+)
