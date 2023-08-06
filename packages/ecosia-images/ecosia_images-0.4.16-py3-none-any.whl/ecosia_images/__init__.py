c42e7f1a600d15b65d68bfcb84667766ea7f8a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:30 +0000

    Add package ecosia-images/0.4.16/ecosia_images-0.4.16-py3-none-any.whl

---
+++ packages/ecosia-images/ecosia_images-0.4.16-py3-none-any.whl/ecosia_images/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+browser_options = [
+    'CHROME',
+    'FIREFOX'
+]
+
+from .ecosia_search import crawler
+from .ecosia_search import download_options
+from .ecosia_search import naming_options
