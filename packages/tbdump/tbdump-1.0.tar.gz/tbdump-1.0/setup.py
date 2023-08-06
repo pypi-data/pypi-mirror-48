686dc47eab56aafdacc06747015580f61a2d033a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:34 +0000

    Add package tbdump/1.0/tbdump-1.0.tar.gz

---
+++ packages/tbdump/tbdump-1.0.tar.gz/tbdump-1.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tbdump",
+    version="1.0",
+    author="Hiroyuki Ohsaki",
+    author_email="ohsaki@lsnl.jp",
+    description=
+    "Display helpful information for debbugging in case of errors/exceptions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h-ohsaki/tbdump",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
