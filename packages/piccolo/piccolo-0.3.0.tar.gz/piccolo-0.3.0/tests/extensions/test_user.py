5902f76e7e3d19587471ec9e346b55d5d0f56f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:51 +0000

    Add package piccolo/0.3.0/piccolo-0.3.0.tar.gz

---
+++ packages/piccolo/piccolo-0.3.0.tar.gz/piccolo-0.3.0/tests/extensions/test_user.py
@@ -0,0 +1,69 @@
+import asyncio
+from unittest import TestCase
+
+from piccolo.extensions.user import User
+from ..example_project.tables import DB
+
+
+class _User(User):
+    class Meta():
+        tablename = 'a_user'
+        db = DB
+
+
+class TestCreateUserTable(TestCase):
+
+    def test_create_user_table(self):
+        """
+        Make sure the table can be created.
+        """
+        exception = None
+        try:
+            _User.create.run_sync()
+        except Exception as e:
+            exception = e
+        else:
+            _User.drop.run_sync()
+
+        if exception:
+            raise exception
+
+        self.assertFalse(exception)
+
+
+class TestHashPassword(TestCase):
+
+    def test_hash_password(self):
+        pass
+
+
+class TestLogin(TestCase):
+
+    def setUp(self):
+        _User.create.run_sync()
+
+    def tearDown(self):
+        _User.drop.run_sync()
+
+    def test_login(self):
+        username = "bob"
+        password = "Bob123$$$"
+        email = "bob@bob.com"
+
+        save_query = _User(
+            username=username,
+            password=password,
+            email=email
+        ).save
+
+        save_query.run_sync()
+
+        authenticated = asyncio.run(
+            _User.login(username, password)
+        )
+        self.assertTrue(authenticated is not None)
+
+        authenticated = asyncio.run(
+            _User.login(username, 'blablabla')
+        )
+        self.assertTrue(not authenticated)
