87f7578b0a8cb8766512e1d5f2027a1fe1dde6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:55 +0000

    Add package widgetastic.patternfly4/0.9.4/widgetastic.patternfly4-0.9.4-py3-none-any.whl

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.9.4-py3-none-any.whl/widgetastic_patternfly4/dropdown.py
@@ -0,0 +1,188 @@
+from contextlib import contextmanager
+
+from widgetastic.exceptions import NoSuchElementException, UnexpectedAlertPresentException
+from widgetastic.utils import ParametrizedLocator
+from widgetastic.xpath import quote
+from widgetastic.widget import Widget
+
+
+class DropdownDisabled(Exception):
+    pass
+
+
+class DropdownItemDisabled(Exception):
+    pass
+
+
+class DropdownItemNotFound(Exception):
+    pass
+
+
+class Dropdown(Widget):
+    """Represents the Patternfly dropdown.
+
+    https://www.patternfly.org/v4/documentation/react/components/dropdown
+
+    Args:
+        text: Text of the button, can be the inner text or the title attribute.
+
+    """
+    ROOT = ParametrizedLocator("{@locator}")
+    BUTTON_LOCATOR = (
+        ".//button[contains(@class, 'pf-c-options-menu__toggle-button') or "
+        "contains(@class, 'pf-c-dropdown__toggle')]"
+    )
+    ITEMS_LOCATOR = ".//ul[@class='pf-c-dropdown__menu']/li"
+    ITEM_LOCATOR = (
+        ".//*[self::a or self::button][contains(@class, 'pf-c-dropdown__menu-item')"
+        " and normalize-space(.)={}]"
+    )
+
+    def __init__(self, parent, text=None, locator=None, logger=None):
+        Widget.__init__(self, parent, logger=logger)
+        if locator and text:
+            raise ValueError("Either text or locator should be provided")
+        if text:
+            self.locator = (
+                './/div[contains(@class, "pf-c-dropdown") and '
+                "child::button[normalize-space(.)={}]]"
+            ).format(quote(text))
+        elif locator:
+            self.locator = locator
+        else:
+            self.locator = './/div[contains(@class, "pf-c-dropdown")][1]'
+
+    @contextmanager
+    def opened(self):
+        self.open()
+        yield
+        self.close()
+
+    @property
+    def is_enabled(self):
+        """Returns if the dropdown itself is enabled and therefore interactive."""
+        return "disabled" not in self.browser.classes(self.BUTTON_LOCATOR)
+
+    def _verify_enabled(self):
+        if not self.is_enabled:
+            raise DropdownDisabled(
+                '{} "{}" is not enabled'.format(
+                    type(self).__name__, getattr(self, "text", self.locator)
+                )
+            )
+
+    @property
+    def is_open(self):
+        return "pf-m-expanded" in self.browser.classes(self)
+
+    def open(self):
+        self._verify_enabled()
+        if not self.is_open:
+            self.browser.click(self.BUTTON_LOCATOR)
+
+    def close(self, ignore_nonpresent=False):
+        """Close the dropdown
+
+        Args:
+            ignore_nonpresent: Will ignore exceptions due to disabled or missing dropdown
+        """
+        try:
+            self._verify_enabled()
+            if self.is_open:
+                self.browser.click(self)
+        except (NoSuchElementException, DropdownDisabled):
+            if ignore_nonpresent:
+                self.logger.info("%r hid so it was not possible to close it. But ignoring.", self)
+            else:
+                raise
+
+    @property
+    def items(self):
+        """Returns a list of all dropdown items as strings."""
+        with self.opened():
+            result = [self.browser.text(el) for el in self.browser.elements(self.ITEMS_LOCATOR)]
+        return result
+
+    def has_item(self, item):
+        """Returns whether the items exists.
+
+        Args:
+            item: item name
+
+        Returns:
+            Boolean - True if enabled, False if not.
+        """
+        return item in self.items
+
+    def item_element(self, item, close=True):
+        """Returns a WebElement for given item name."""
+        try:
+            self.open()
+            result = self.browser.element(self.ITEM_LOCATOR.format(quote(item)))
+            if close:
+                self.close()
+            return result
+        except NoSuchElementException:
+            try:
+                items = self.items
+            except NoSuchElementException:
+                items = []
+            if items:
+                items_string = "These items are present: {}".format("; ".join(items))
+            else:
+                items_string = "The dropdown is probably not present"
+            raise DropdownItemNotFound("Item {!r} not found. {}".format(item, items_string))
+
+    def item_enabled(self, item, close=True):
+        """Returns whether the given item is enabled.
+
+        Args:
+            item: Name of the item.
+
+        Returns:
+            Boolean - True if enabled, False if not.
+        """
+        self._verify_enabled()
+        el = self.item_element(item, close=False)
+        is_el_enabled = "pf-m-disabled" not in self.browser.classes(el)
+        if close:
+            self.close()
+        return is_el_enabled
+
+    def item_select(self, item, handle_alert=None):
+        """Opens the dropdown and selects the desired item.
+
+        Args:
+            item: Item to be selected
+            handle_alert: How to handle alerts. None - no handling, True - confirm, False - dismiss.
+
+        Raises:
+            DropdownItemDisabled
+        """
+        self.logger.info("Selecting %r", item)
+        try:
+            if not self.item_enabled(item, close=False):
+                raise DropdownItemDisabled(
+                    'Item "{}" of {} "{}" is disabled\n'
+                    "The following items are available: {}".format(
+                        item,
+                        type(self).__name__.lower(),
+                        getattr(self, "text", None) or self.locator,
+                        ";".join(self.items),
+                    )
+                )
+            self.browser.click(
+                self.item_element(item, close=False), ignore_ajax=handle_alert is not None
+            )
+            if handle_alert is not None:
+                self.browser.handle_alert(cancel=not handle_alert, wait=10.0)
+                self.browser.plugin.ensure_page_safe()
+        finally:
+            try:
+                self.close(ignore_nonpresent=True)
+            except UnexpectedAlertPresentException:
+                self.logger.warning("There is an unexpected alert present.")
+                pass
+
+    def __repr__(self):
+        return "{}({!r})".format(type(self).__name__, getattr(self, "text", None) or self.locator)
---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.9.4-py3-none-any.whl/widgetastic_patternfly4/navigation.py
@@ -0,0 +1,98 @@
+from collections import OrderedDict
+
+from widgetastic.exceptions import NoSuchElementException
+from widgetastic.utils import ParametrizedLocator
+from widgetastic.xpath import quote
+from widgetastic.widget import Widget
+
+
+class Navigation(Widget):
+    """The Patternfly navigation.
+
+    https://www.patternfly.org/v4/documentation/react/components/nav
+    """
+
+    LOCATOR_START = './/nav[@class="pf-c-nav"{}]'
+    ROOT = ParametrizedLocator("{@locator}")
+    CURRENTLY_SELECTED = (
+        './/a[contains(@class, "pf-m-current") or ' 'parent::li[contains(@class, "pf-m-current")]]'
+    )
+    ITEMS = "./ul/li/a"
+    SUB_ITEMS_ROOT = "./section"
+    ITEM_MATCHING = "./ul/li[.//a[normalize-space(.)={}]]"
+
+    def __init__(self, parent, label=None, id=None, locator=None, logger=None):
+        Widget.__init__(self, parent, logger=logger)
+
+        quoted_label = quote(label) if label else ""
+        if label:
+            label_part = " and @label={} or @aria-label={}".format(quoted_label, quoted_label)
+        else:
+            label_part = ""
+
+        id_part = " and @id={}".format(quote(id)) if id else ""
+        if locator is not None:
+            self.locator = locator
+        elif label_part or id_part:
+            self.locator = self.LOCATOR_START.format(label_part + id_part)
+        else:
+            raise TypeError("You need to specify either, id, label or locator for Navigation")
+
+    def read(self):
+        return self.currently_selected
+
+    def nav_links(self, *levels):
+        if not levels:
+            return [self.browser.text(el) for el in self.browser.elements(self.ITEMS)]
+        current_item = self
+        for i, level in enumerate(levels):
+            li = self.browser.element(self.ITEM_MATCHING.format(quote(level)), parent=current_item)
+
+            try:
+                current_item = self.browser.element(self.SUB_ITEMS_ROOT, parent=li)
+            except NoSuchElementException:
+                if i == len(levels) - 1:
+                    return []
+                else:
+                    raise
+
+        return [
+            self.browser.text(el) for el in self.browser.elements(self.ITEMS, parent=current_item)
+        ]
+
+    def nav_item_tree(self, start=None):
+        start = start or []
+        result = OrderedDict()
+        for item in self.nav_links(*start):
+            sub_items = self.nav_item_tree(start=start + [item])
+            result[item] = sub_items or None
+        if result and all(value is None for value in result.values()):
+            # If there are no child nodes, then just make it a list
+            result = list(result.keys())
+        return result
+
+    @property
+    def currently_selected(self):
+        return [self.browser.text(el) for el in self.browser.elements(self.CURRENTLY_SELECTED)]
+
+    def select(self, *levels, **kwargs):
+        """Select an item in the navigation.
+
+        Args:
+            *levels: Items to be clicked in the navigation.
+            force: Force navigation to happen, defaults to False.
+        """
+        self.logger.info("Selecting %r in navigation", levels)
+        force = kwargs.get("force", False)
+        if not force and list(levels) == self.currently_selected:
+            return
+        current_item = self
+        for i, level in enumerate(levels, 1):
+            li = self.browser.element(self.ITEM_MATCHING.format(quote(level)), parent=current_item)
+            self.browser.click(li)
+            if i == len(levels):
+                return
+            current_item = self.browser.element(self.SUB_ITEMS_ROOT, parent=li)
+
+    def __repr__(self):
+        return "{}({!r})".format(type(self).__name__, self.ROOT)
