bba8cbaff13fddcca334eda004e8c082585913ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:22 +0000

    Add package snptoolkit/2.0.6/snpToolkit-2.0.6.tar.gz

---
+++ packages/snptoolkit/snpToolkit-2.0.6.tar.gz/snpToolkit-2.0.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='snpToolkit',
+      version='2.0.6',
+      description='SNPs filtering, annotation and combining',
+      url='https://github.com/Amine-Namouchi/snpToolkit',
+      author='Amine Namouchi',
+      author_email='amine.namouchi@gmail.com',
+      long_description='SNPs annotation, filtering and combining from vcf files',
+      license='GPLv3+',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent',
+      ],
+      packages=['modules'],
+      scripts=['snpToolkit'],
+      keywords='SNPs filtering annotation variants calling',
+      install_requires=['pysam', 'pandas', 'tqdm', 'coloredlogs', 'Biopython'],
+      )
