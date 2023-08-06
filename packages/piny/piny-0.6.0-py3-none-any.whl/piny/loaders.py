9f81577223c5d6a5e1b914f3a092ef7cdde0679c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:41 +0000

    Add package piny/0.6.0/piny-0.6.0-py3-none-any.whl

---
+++ packages/piny/piny-0.6.0-py3-none-any.whl/piny/loaders.py
@@ -0,0 +1,98 @@
+from typing import IO, Any, Type, Union
+
+import yaml
+
+from .errors import LoadingError
+from .matchers import Matcher, MatcherWithDefaults
+from .validators import Validator
+
+#
+# Loader
+#
+
+
+class YamlLoader:
+    """
+    YAML configuration file loader
+    """
+
+    def __init__(
+        self,
+        path: str,
+        *,
+        matcher: Type[Matcher] = MatcherWithDefaults,
+        validator: Type[Validator] = None,
+        schema: Any = None,
+        **schema_params,
+    ) -> None:
+        """
+        Initialize YAML loader
+
+        :param path: string with path to YAML-file
+        :param matcher: matcher class
+        :param validator: validator class for one of the supported validation libraries
+        :param schema: validation schema for the validator of choice
+        :param schema_params: named arguments used as optional validation schema params
+        """
+        self.path = path
+        self.matcher = matcher
+        self.validator = validator
+        self.schema = schema
+        self.schema_params = schema_params
+
+    def _init_resolvers(self):
+        self.matcher.add_implicit_resolver("!env", self.matcher.matcher, None)
+        self.matcher.add_constructor("!env", self.matcher.constructor)
+
+    def load(self, **params) -> Any:
+        """
+        Return Python object loaded (optionally validated) from the YAML-file
+
+        :param params: named arguments used as optional loading params in validation
+        """
+        self._init_resolvers()
+        try:
+            with open(self.path) as fh:
+                load = yaml.load(fh, Loader=self.matcher)
+        except (yaml.YAMLError, FileNotFoundError) as e:
+            raise LoadingError(origin=e, reason=str(e))
+
+        if (self.validator is not None) and (self.schema is not None):
+            return self.validator(self.schema, **self.schema_params).load(
+                data=load, **params
+            )
+        return load
+
+
+class YamlStreamLoader(YamlLoader):
+    """
+    YAML configuration loader for IO streams, e.g. file objects or stdin
+    """
+
+    def __init__(
+        self,
+        stream: Union[str, IO[str]],
+        *,
+        matcher: Type[Matcher] = MatcherWithDefaults,
+        validator: Type[Validator] = None,
+        schema: Any = None,
+        **schema_params,
+    ) -> None:
+        self.stream = stream
+        self.matcher = matcher
+        self.validator = validator
+        self.schema = schema
+        self.schema_params = schema_params
+
+    def load(self, **params) -> Any:
+        self._init_resolvers()
+        try:
+            load = yaml.load(self.stream, Loader=self.matcher)
+        except yaml.YAMLError as e:
+            raise LoadingError(origin=e, reason=str(e))
+
+        if (self.validator is not None) and (self.schema is not None):
+            return self.validator(self.schema, **self.schema_params).load(
+                data=load, **params
+            )
+        return load
---
+++ packages/piny/piny-0.6.0-py3-none-any.whl/piny/validators.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List, Union
+
+from .errors import ValidationError
+
+LoadedData = Union[Dict[str, Any], List[Any]]
+
+
+class Validator(ABC):
+    """
+    Abstract base class for optional validator classes
+
+    Use only to derive new child classes, implement all abstract methods
+    """
+
+    def __init__(self, schema: Any, **params):
+        self.schema = schema
+        self.schema_params = params
+
+    @abstractmethod
+    def load(self, data: LoadedData, **params):
+        """
+        Load data, return validated data or raise en error
+        """
+        pass  # pragma: no cover
+
+
+class PydanticValidator(Validator):
+    """
+    Validator class for Pydantic library
+    """
+
+    def load(self, data: LoadedData, **params):
+        try:
+            return self.schema(**data).dict()
+        except Exception as e:
+            raise ValidationError(origin=e, reason=str(e))
+
+
+class MarshmallowValidator(Validator):
+    """
+    Validator class for Marshmallow library
+    """
+
+    def load(self, data: LoadedData, **params):
+        try:
+            return self.schema(**self.schema_params).load(data, **params).data
+        except Exception as e:
+            raise ValidationError(origin=e, reason=str(e))
+
+
+class TrafaretValidator(Validator):
+    """
+    Validator class for Trafaret library
+    """
+
+    def load(self, data: LoadedData, **params):
+        try:
+            return self.schema.check(data)
+        except Exception as e:
+            raise ValidationError(origin=e, reason=str(e))
