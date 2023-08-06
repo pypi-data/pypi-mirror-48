fb9f649658e1dec3c9a4c866a13a16767dd9b984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:12 +0000

    Add package xlines/0.6.10/xlines-0.6.10.tar.gz

---
+++ packages/xlines/xlines-0.6.10.tar.gz/xlines-0.6.10/xlines/variables.py
@@ -0,0 +1,53 @@
+"""
+    Static assignments for universally used colors and variables
+
+"""
+from xlines.statics import local_config
+from xlines.colors import Colors
+
+try:
+
+    from xlines.oscodes_unix import exit_codes
+    os_type = 'Linux'
+
+    # special colors - linux
+    acct = Colors.ORANGE
+    text = Colors.BRIGHT_PURPLE
+    TITLE = Colors.WHITE + Colors.BOLD
+
+except Exception:
+    from xlines.oscodes_win import exit_codes    # non-specific os-safe codes
+    os_type = 'Windows'
+
+    # special colors - windows
+    acct = Colors.CYAN
+    text = Colors.LT2GRAY
+    TITLE = Colors.WHITE + Colors.BOLD
+
+
+# universal colors
+rd = Colors.RED + Colors.BOLD
+yl = Colors.YELLOW + Colors.BOLD
+fs = Colors.GOLD3
+bd = Colors.BOLD
+gn = Colors.BRIGHT_GREEN
+title = Colors.BRIGHT_WHITE + Colors.BOLD
+bbc = bd + Colors.BRIGHT_CYAN
+bbl = bd + Colors.BRIGHT_BLUE
+highlight = bd + Colors.BRIGHT_YELLOW2
+frame = gn + bd
+btext = text + Colors.BOLD
+bwt = Colors.BRIGHT_WHITE
+bdwt = Colors.BOLD + Colors.BRIGHT_WHITE
+ub = Colors.UNBOLD
+rst = Colors.RESET
+
+# globals
+expath = local_config['EXCLUSIONS']['EX_EXT_PATH']
+exdirpath = local_config['EXCLUSIONS']['EX_DIR_PATH']
+config_dir = local_config['CONFIG']['CONFIG_PATH']
+div = text + '/' + rst
+div_len = 2
+horiz = text + '-' + rst
+arrow = bwt + '-> ' + rst
+BUFFER = local_config['OUTPUT']['BUFFER']
