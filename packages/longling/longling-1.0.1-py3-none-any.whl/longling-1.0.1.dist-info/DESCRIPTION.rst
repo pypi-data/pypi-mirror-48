ea56047e158b1f5b89c283f5af99b49f85ad6172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:37 +0000

    Add package longling/1.0.1/longling-1.0.1-py3-none-any.whl

---
+++ packages/longling/longling-1.0.1-py3-none-any.whl/longling-1.0.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Tutorial and full documentation can be found in https://longling.readthedocs.io/zh/latest/index.html.
+
+
---
+++ packages/longling/longling-1.0.1-py3-none-any.whl/longling/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# created by tongshiwei on 17-12-17
+
+from __future__ import absolute_import
+
+from longling.base import *
+
+from longling.lib.stream import *
+
+from longling.lib import clock
+from longling.lib import process_pattern
+from longling.lib import stream
+from longling.lib import utilog
+from longling.lib import candylib
+
+__version__ = '1.0.1'
