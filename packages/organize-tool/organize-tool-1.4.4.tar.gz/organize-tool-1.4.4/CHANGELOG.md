a0eb0bdf1753dab945459ca38fae2ad50ef6415d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package organize-tool/1.4.4/organize-tool-1.4.4.tar.gz

---
+++ packages/organize-tool/organize-tool-1.4.4.tar.gz/organize-tool-1.4.4/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Changelog
+
+## v1.4.4 (2019-07-02)
+- Fixes issues #36 with umlauts in config file on windows
+
+## v1.4.3 (2019-06-05)
+- Use safe YAML loader to fix a deprecation warning. (Thanks mope1!)
+- Better error message if a folder does not exist. (Again thanks mope1!)
+- Fix example code in documentation for LastModified filter.
+- Custom config file locations (given by cmd line argument or environment variable).
+- `config --debug` now shows the full path to the config file.
+
+## v1.4.2 (2018-11-14)
+- Fixes a bug with command line arguments in the ``$EDITOR`` environment
+  variable.
+- Fixes a bug where an empty config wouldn't show the correct error message.
+- Fix binary wheel creation in setup.py by using environment markers
+
+## v1.4.1 (2018-10-05)
+- A custom separator ``counter_separator`` can now be set in the actions Move,
+  Copy and Rename.
+
+## v1.4 (2018-09-21)
+- Fixes a bug where glob wildcards are not detected correctly
+- Adds support for excluding folders and files via glob syntax.
+- Makes sure that files are only handled once per rule.
+
+## v1.3 (2018-07-06)
+- Glob support in folder configuration.
+- New variable {relative_path} is now available in actions.
+
+## v1.2 (2018-03-19)
+- Shows the relative path to files in subfolders.
+
+## v1.1 (2018-03-13)
+- Removes the colon from extension filter output so `{extension.lower}` now
+  returns `'png'` instead of `'.png'`.
+
+## v1.0 (2018-03-13)
+- Initial release.
---
+++ packages/organize-tool/organize-tool-1.4.4.tar.gz/organize-tool-1.4.4/organize/actions/python.py
@@ -0,0 +1,78 @@
+import logging
+from organize.utils import Path
+from .action import Action
+
+
+class Python(Action):
+
+    """
+    Execute python code in your config file.
+
+    :param str code: The python code to execute
+
+    Examples:
+      - A basic example that shows how to get the current file path and do some
+        printing in a for loop. The ``|`` is yaml syntax for defining a string
+        literal spanning multiple lines.
+
+        .. code-block:: yaml
+            :caption: config.yaml
+
+            rules:
+            - folders: '~/Desktop'
+              actions:
+                - Python: |
+                    print('The path of the current file is %s' % path)
+                    for _ in range(5):
+                        print('Heyho, its me from the loop')
+
+      - You can access filter data:
+
+        .. code-block:: yaml
+            :caption: config.yaml
+
+            rules:
+            - folders: ~/Desktop
+              filters:
+                - Regex: '^(?P<name>.*)\.(?P<extension>.*)$'
+              actions:
+                - Python: |
+                    print('Name: %s' % regex.name)
+                    print('Extension: %s' % regex.extension)
+
+      - You have access to all the python magic -- do a google search for each
+        filename starting with an underscore:
+
+        .. code-block:: yaml
+            :caption: config.yaml
+
+            rules:
+            - folders: ~/Desktop
+              filters:
+                - Filename:
+                    startswith: '_'
+              actions:
+                - Python: |
+                    import webbrowser
+                    webbrowser.open('https://www.google.com/search?q=%s' % path.stem)
+    """
+
+    def __init__(self, code):
+        self.code = code
+        self.log = logging.getLogger(__name__)
+
+    def run(self, attrs: dict, simulate: bool):
+        if simulate:
+            self.print('Code not run in simulation')
+        else:
+            path = attrs['path']
+            self.log.info(
+                'Executing python script:\n"""\n%s""" with path="%s", args=%s',
+                self.code, path, attrs)
+            # local variables for inline function
+            locals_ = attrs.copy()
+            locals_['simulate'] = simulate
+            # replace default print function
+            globals_ = globals().copy()
+            globals_['print'] = self.print
+            exec(self.code, globals_, locals_)
