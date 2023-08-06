1c0913c609dccd4d65a0c0a6aa3fdc01765f97c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package jdcloud-sdk/1.6.2/jdcloud_sdk-1.6.2.tar.gz

---
+++ packages/jdcloud-sdk/jdcloud_sdk-1.6.2.tar.gz/jdcloud_sdk-1.6.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from os import path
+from codecs import open
+
+setup(
+    name='jdcloud_sdk',
+    version="1.6.2",
+    author='JDCloud API Gateway Team',
+    url='https://github.com/jdcloud-api/jdcloud-sdk-python',
+    scripts=[],
+    packages=find_packages(),
+    install_requires=['requests'],
+    license="Apache License V2.0"
+)
