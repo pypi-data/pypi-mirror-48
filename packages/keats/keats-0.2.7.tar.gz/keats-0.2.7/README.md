42dc55931cfa9eb80d55d90b9db1ffa47f107037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:33 +0000

    Add package keats/0.2.7/keats-0.2.7.tar.gz

---
+++ packages/keats/keats-0.2.7.tar.gz/keats-0.2.7/README.md
@@ -0,0 +1,34 @@
+# Keats
+
+![John Keats](assets/keats.jpg)
+
+Keats is an Python build, installation, and workflow manager.
+
+Features include forced change logs, release scripts, and package version control.
+
+## Usage
+
+```
+keats bump <VERSION>
+```
+
+```
+# get the package version number
+keats version
+```
+
+## Installation
+
+
+```
+# globally install keats
+pip install keats
+```
+
+```
+# locally install keats
+poetry add --dev keats
+
+# run keats
+poetry run keats
+```
---
+++ packages/keats/keats-0.2.7.tar.gz/keats-0.2.7/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['keat_scripts', 'keats']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fire>=0.1.3,<0.2.0', 'termcolor>=1.1,<2.0', 'toml>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['keats = keats:main']}
+
+setup_kwargs = {
+    'name': 'keats',
+    'version': '0.2.7',
+    'description': 'Utilities for managing version, changelogs, and project releases.',
+    'long_description': '# Keats\n\n![John Keats](assets/keats.jpg)\n\nKeats is an Python build, installation, and workflow manager.\n\nFeatures include forced change logs, release scripts, and package version control.\n\n## Usage\n\n```\nkeats bump <VERSION>\n```\n\n```\n# get the package version number\nkeats version\n```\n\n## Installation\n\n\n```\n# globally install keats\npip install keats\n```\n\n```\n# locally install keats\npoetry add --dev keats\n\n# run keats\npoetry run keats\n```',
+    'author': 'jvrana',
+    'author_email': 'justin.vrana@gmail.com',
+    'url': 'https://github.com/jvrana/keats',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
