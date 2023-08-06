1570572e3f525030f32e5b9b27fe61c88169edf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:08 +0000

    Add package import-linter/1.0b4/import-linter-1.0b4.tar.gz

---
+++ packages/import-linter/import-linter-1.0b4.tar.gz/import-linter-1.0b4/src/importlinter/application/ports/building.py
@@ -0,0 +1,11 @@
+import abc
+
+from importlinter.domain.ports.graph import ImportGraph
+
+
+class GraphBuilder(abc.ABC):
+    @abc.abstractmethod
+    def build(
+        self, root_package_name: str, include_external_packages: bool = False
+    ) -> ImportGraph:
+        raise NotImplementedError
