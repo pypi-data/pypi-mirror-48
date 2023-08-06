369ddb5bf72de0353aea8008952f0305528825e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:15 +0000

    Add package sosw/0.7.13/sosw-0.7.13.tar.gz

---
+++ packages/sosw/sosw-0.7.13.tar.gz/sosw-0.7.13/sosw/test/unit/test_worker.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+from unittest.mock import patch, Mock
+
+from sosw.worker import Worker
+
+
+os.environ["STAGE"] = "test"
+os.environ["autotest"] = "True"
+
+
+class worker_UnitTestCase(unittest.TestCase):
+    TEST_CONFIG = {'test': True}
+
+
+    def setUp(self):
+        self.patcher = patch("sosw.app.get_config")
+        self.get_config_patch = self.patcher.start()
+
+
+    def tearDown(self):
+        self.patcher.stop()
+
+        try:
+            del (os.environ['AWS_LAMBDA_FUNCTION_NAME'])
+        except Exception:
+            pass
+
+
+    def test_close_task__called(self):
+        p = Worker(custom_config=self.TEST_CONFIG)
+        p.mark_task_as_completed = Mock(return_value=None)
+        p({'task_id': '123'})
+        p.mark_task_as_completed.assert_called_once_with('123')
