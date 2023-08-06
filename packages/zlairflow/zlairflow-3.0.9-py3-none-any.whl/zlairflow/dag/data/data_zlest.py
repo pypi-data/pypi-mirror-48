ce3146bd662c9191ca2ff5f74f515bb3df41aff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:06 +0000

    Add package zlairflow/3.0.9/zlairflow-3.0.9-py3-none-any.whl

---
+++ packages/zlairflow/zlairflow-3.0.9-py3-none-any.whl/zlairflow/dag/data/data_zlest.py
@@ -0,0 +1,29 @@
+para=[
+['zlest', 'ezhoushi', 'minutes=480', ''],
+['zlest', 'guizhousheng', 'minutes=480', ''],
+['zlest', 'qinghaisheng', 'minutes=480', ''],
+['zlest', 'shaoyangxian', 'minutes=480', ''],
+['zlest', 'shenzhenshi', 'minutes=480', ''],
+['zlest', 'anhuisheng', 'minutes=480', ''],
+['zlest', 'guangdongsheng', 'minutes=480', ''],
+['zlest', 'guangxisheng', 'minutes=480', ''],
+['zlest', 'henansheng', 'minutes=480', ''],
+['zlest', 'hunansheng', 'minutes=480', ''],
+['zlest', 'jiangxisheng', 'minutes=480', ''],
+
+['zlest', 'wuxishi', 'minutes=480', ''],
+['zlest', 'zhejiangsheng', 'minutes=480', ''],
+['zlest', 'anhuisheng1', 'minutes=480', ''],
+['zlest', 'enshizhou', 'minutes=480', ''],
+
+['zlest', 'hubeisheng', 'minutes=480', ''],
+['zlest', "sichuansheng",'minutes=480', ''],
+['zlest', "qinghaisheng1",'minutes=480', ''],
+['zlest', "neimenggu", 'minutes=480', ''],
+
+
+
+
+]
+
+
