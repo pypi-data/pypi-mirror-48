525524b37268dfe1b9a6e73024c3bc01573eb882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:48 +0000

    Add package django-awesomplete/0.1.5/django-awesomplete-0.1.5.tar.gz

---
+++ packages/django-awesomplete/django-awesomplete-0.1.5.tar.gz/django-awesomplete-0.1.5/tests/app/admin.py
@@ -0,0 +1,57 @@
+from django import forms
+from django.contrib import admin
+from awesomplete.widgets import AwesompleteWidget
+from .models import City, CityLanguage
+
+
+def get_country_suggestions():
+    return City.objects.values_list(
+        'country', flat=True
+    ).order_by('country').distinct()
+
+
+def get_language_suggestions():
+    return CityLanguage.objects.values_list(
+        'language', flat=True
+    ).order_by('language').distinct()
+
+
+class CityLanguageInlineForm(forms.ModelForm):
+    class Meta:
+        model = CityLanguage
+        fields = forms.ALL_FIELDS
+        widgets = {
+            'language': AwesompleteWidget(
+                suggestions=get_language_suggestions
+            )
+        }
+
+
+class CityLanguageInline(admin.TabularInline):
+    model = CityLanguage
+    form = CityLanguageInlineForm
+    extra = 0
+
+
+class CityLanguageStackedInline(admin.StackedInline):
+    model = CityLanguage
+    form = CityLanguageInlineForm
+    extra = 0
+
+
+class CityAdminForm(forms.ModelForm):
+    class Meta:
+        model = City
+        fields = forms.ALL_FIELDS
+        widgets = {
+            'country': AwesompleteWidget(
+                suggestions=get_country_suggestions
+            )
+        }
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    form = CityAdminForm
+    inlines = (CityLanguageInline, CityLanguageStackedInline)
+    list_display = ('name', 'country')
