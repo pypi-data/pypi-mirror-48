f53013f27487ba1e6a5ff33e3414136d274e70a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:26 +0000

    Add package seqdataloader/0.126/seqdataloader-0.126.tar.gz

---
+++ packages/seqdataloader/seqdataloader-0.126.tar.gz/seqdataloader-0.126/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+config = {
+    'include_package_data': True,
+    'author': 'Anna Shcherbina',
+    'author_email': 'annashch@stanford.edu',
+    'url': 'https://github.com/kundajelab/seqdataloader',
+    'description': 'Generate genome-wide classification and regression labels for DNA accessibility data.',
+    'version': '0.126',
+    'packages': ['seqdataloader'],
+    'setup_requires': [],
+    'install_requires': ['numpy>=1.15','pandas>=0.23.4','cython>=0.27.3','deeptools>=3.0.1','pybedtools>=0.7','pyBigWig>=0.3.2', 'pyfaidx'],
+    'scripts': [],
+    'entry_points': {'console_scripts': ['genomewide_labels=seqdataloader.__init__:main']},
+    'name': 'seqdataloader'
+}
+
+if __name__== '__main__':
+    setup(**config)
