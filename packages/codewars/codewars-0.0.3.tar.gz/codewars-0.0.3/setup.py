26c79756686299ef49d8bc70f3b7d23ec6243fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:03 +0000

    Add package codewars/0.0.3/codewars-0.0.3.tar.gz

---
+++ packages/codewars/codewars-0.0.3.tar.gz/codewars-0.0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.MD", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="codewars", # codewars when updating to pip
+    version="0.0.3",
+    author="Annihilator708",
+    author_email="",
+    description="A toolbox for daily use. This is just a toolbox to make my life easier. Maybe yours too..",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning"
+    ],
+    install_requires=[
+        'PyBluez',
+    ],
+    python_requires='>=3.6.*',
+    include_package_data=True,
+    zip_safe=False
+)
