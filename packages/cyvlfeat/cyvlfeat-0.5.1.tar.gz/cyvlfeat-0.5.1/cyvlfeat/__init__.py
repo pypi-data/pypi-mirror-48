67094047762eda9cef4bee537e02ea23e61181b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:53 +0000

    Add package cyvlfeat/0.5.1/cyvlfeat-0.5.1.tar.gz

---
+++ packages/cyvlfeat/cyvlfeat-0.5.1.tar.gz/cyvlfeat-0.5.1/cyvlfeat/__init__.py
@@ -0,0 +1,11 @@
+import cyvlfeat.fisher
+import cyvlfeat.generic
+import cyvlfeat.gmm
+import cyvlfeat.hog
+import cyvlfeat.kmeans
+import cyvlfeat.sift
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
