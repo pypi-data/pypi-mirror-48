026fc99660f9bbd3a6b2443c5bd216ea22146e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:18 +0000

    Add package distributions-lly/0.1/distributions_lly-0.1.tar.gz

---
+++ packages/distributions-lly/distributions_lly-0.1.tar.gz/distributions_lly-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_lly',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_lly'],
+      zip_safe=False)
