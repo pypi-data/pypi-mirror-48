a461800865da494b1369ab868d6c620a92fa181d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:17 +0000

    Add package eric-ide/19.7/eric_ide-19.7-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-19.7-py3-none-any.whl/eric6/UI/Info.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2019 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining some informational strings.
+"""
+
+from __future__ import unicode_literals
+
+Program = 'eric6'
+Version = '19.7 (rev. 03b771405f5d)'
+VersionOnly = '19.7'
+Copyright = 'Copyright (c) 2002 - 2019 Detlev Offenbach' \
+            ' <detlev@die-offenbachs.de>'
+CopyrightShort = '(c) 2002 - 2019 Detlev Offenbach'
+BugAddress = 'eric-bugs@eric-ide.python-projects.org'
+FeatureAddress = 'eric-featurerequest@eric-ide.python-projects.org'
+Homepage = "https://eric-ide.python-projects.org/index.html"
