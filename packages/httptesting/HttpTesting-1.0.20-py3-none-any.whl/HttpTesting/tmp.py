5d32733998e89374c2a982567b9d250a34f118f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:29 +0000

    Add package httptesting/1.0.20/HttpTesting-1.0.20-py3-none-any.whl

---
+++ packages/httptesting/HttpTesting-1.0.20-py3-none-any.whl/HttpTesting/tmp.py
@@ -0,0 +1,17 @@
+
+import logging
+
+class LOG:
+    """The log module.
+    """
+    @staticmethod
+    def console_info(msg):
+        """
+        The log level is Info.
+        """
+        logging.basicConfig(
+            level = logging.INFO, 
+            format = '[%(levelname)s] - %(asctime)s - %(message)s'
+        )
+
+        logging.info(msg)
