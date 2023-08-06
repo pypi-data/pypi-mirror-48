746edbbb6f0675f87162a2da17153b357405dfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package saphires/0.1.6/saphires-0.1.6.tar.gz

---
+++ packages/saphires/saphires-0.1.6.tar.gz/saphires-0.1.6/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(name='saphires',
+      version='0.1.6',
+      description='Stellar Analysis in Python for HIgh-REsolution Spectroscopy',
+      author='Ben Tofflemire',
+      author_email='tofflemire@utexas.edu',
+      url='https://github.com/tofflemire/saphires',
+      license='BSD/MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          ],
+      packages=['saphires','saphires.extras'],
+      requires=['numpy',
+                'astropy',
+                'scipy',
+                'matplotlib',
+                'pickle',
+                'PyQt5',
+                'pdyl'])
