db3564c13b393f26467337f963b944c575b60a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:26 +0000

    Add package juujian/0.0.5/juujian-0.0.5.tar.gz

---
+++ packages/juujian/juujian-0.0.5.tar.gz/juujian-0.0.5/README.md
@@ -0,0 +1,7 @@
+# juujian
+
+Various web scraping and data science related tools. 
+
+Provides commonly used tools for web scraping and data science. Some of these tools might be superfluous. But the 
+purpose is to provide them through import, rather than requiring a separate file to go with a jupyter notebook, to allow 
+for more elegant project structure (ideally only the notebook).
---
+++ packages/juujian/juujian-0.0.5.tar.gz/juujian-0.0.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(name='juujian',
+                 version='0.0.5',
+                 description='Various web scraping and data science related tools. ',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 author='Julian Barg',
+                 author_email='barg.julian@gmail.com',
+                 packages=['juujian'],
+                 install_requires=[]
+                 )
