077e7c18cadcd2e1ad39aacab328f6fe42e2da41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:28 +0000

    Add package fsx/1.0.5/fsx-1.0.5.tar.gz

---
+++ packages/fsx/fsx-1.0.5.tar.gz/fsx-1.0.5/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+description = r'''A low-level and easily mockable file system wrapper.'''
+
+setuptools.setup(
+    name='fsx',
+    packages = setuptools.find_packages(exclude=['tests*']),
+    version="1.0.5",
+    description=description,
+    author = 'Henry Weickert',
+    author_email = 'henryweickert@gmail.com',
+    url = 'https://github.com/hweickert/fsx',
+    keywords = [],
+    entry_points={},
+    install_requires=['fstree==1.0.3']
+)
