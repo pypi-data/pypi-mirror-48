2b22063399c9d267f46c29b66bb2444b93c996b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:39 +0000

    Add package s3-iterator-kypkalorian/0.0.5/s3-iterator-kypkalorian-0.0.5.tar.gz

---
+++ packages/s3-iterator-kypkalorian/s3-iterator-kypkalorian-0.0.5.tar.gz/s3-iterator-kypkalorian-0.0.5/s3_iterator/iterator.py
@@ -0,0 +1,33 @@
+from .list_obj import list_obj, list_obj_marker, list_prefix, list_prefix_marker
+
+import json
+
+def get_next_marker(response):
+    if 'NextMarker' in response:
+        return response['NextMarker']
+    else:
+        return None
+
+def get_contents(response):
+    if 'Contents' in response:
+        return response['Contents']
+    else:
+        return None
+
+def iterator(bucket, nextMarker=None, prefix=None):
+
+    if nextMarker == None:
+
+        if prefix != None:
+            response = list_prefix(bucket, prefix)
+        else:
+            response = list_obj(bucket)
+
+    else:
+
+        if dir != None:
+            response = list_prefix_marker(bucket, nextMarker, prefix)
+        else:
+            response = list_obj_marker(bucket, nextMarker)
+
+    return get_contents(response), get_next_marker(response)
