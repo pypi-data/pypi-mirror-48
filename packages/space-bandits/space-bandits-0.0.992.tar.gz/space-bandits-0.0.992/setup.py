ff7791d4cc3ba14c5d227dc07ab540b4a44ed234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:26 +0000

    Add package space-bandits/0.0.992/space-bandits-0.0.992.tar.gz

---
+++ packages/space-bandits/space-bandits-0.0.992.tar.gz/space-bandits-0.0.992/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+import os
+
+long_desc = """
+A practical library for building contextual bandits models with deep Bayesian approximation.
+Supports both online learning and offline training of models as well as novel methods for cross-validating CB models on historic data.
+"""
+
+reqs= [
+    'torch',
+    'numpy',
+    'scipy',
+    'pandas',
+    'cython',
+    'scikit-learn'
+]
+version = '0.0.992'
+
+setup(
+    name='space-bandits',
+    version=f'{version}',
+    description='Deep Bayesian Contextual Bandits Library',
+    long_description=long_desc,
+    author='Michael Klear',
+    author_email='michael@fellowship.ai',
+    url='https://github.com/fellowship/space-bandits',
+    download_url=f'https://github.com/fellowship/space-bandits/archive/v{version}.tar.gz',
+    install_requires=reqs,
+    packages=['space_bandits']
+)
