daf8a0b02ffc7d50b4151b761113b58c47450fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:06 +0000

    Add package zipapi.us/1.0.3/zipapi.us-1.0.3.tar.gz

---
+++ packages/zipapi.us/zipapi.us-1.0.3.tar.gz/zipapi.us-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='zipapi.us',
+      version='1.0.3',
+      description='Unoffical zipapi.us wrapper.',
+      long_description='An unoffical zipapi.us object oriented wrapper.',
+      author='sl4v',
+      author_email='iamsl4v@protonmail.com',
+      url='https://github.com/sl4vkek/zipapi.us',
+      packages=['zipapi'],
+      install_requires=['requests'],
+      license="WTFPL"
+)
