cd110c3e0c8fc1b3111b018a4e92e20a1c56ab3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:28 +0000

    Add package japana/0.3/japana-0.3.tar.gz

---
+++ packages/japana/japana-0.3.tar.gz/japana-0.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+
+setuptools.setup(
+    name='japana',
+    version='0.3',
+    author="reem-codes | Reem Ali Alghamdi",
+    author_email="reem.brain@gmail.com",
+    description="Japanese text analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reem-codes/japana",
+    packages=['japana'],
+    license='MIT',
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
