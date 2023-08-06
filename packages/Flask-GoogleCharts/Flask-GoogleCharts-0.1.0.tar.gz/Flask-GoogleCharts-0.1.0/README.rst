76dd239ac79300e8b5bba38e0dc18f36d8a83865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:36 +0000

    Add package Flask-GoogleCharts/0.1.0/Flask-GoogleCharts-0.1.0.tar.gz

---
+++ packages/Flask-GoogleCharts/Flask-GoogleCharts-0.1.0.tar.gz/Flask-GoogleCharts-0.1.0/README.rst
@@ -0,0 +1,34 @@
+Flask-GoogleCharts
+===================
+Flask-GoogleCharts simplifies the process of integrating Google Charts into Flask applications.
+
+
+Installation
+------------
+Flask-GoogleCharts can be installed using pip:
+
+    pip install Flask-GoogleCharts
+
+You may also download the GitHub repository and install Flask-GoogleCharts manually:
+
+    git clone https://github.com/wikkiewikkie/flask-googlecharts.git
+    cd flask-googlecharts
+    python setup.py install
+
+
+Examples
+--------
+There is a basic example with two charts in the */example* folder.  The first demonstrates how to load data into the
+chart when it is created, and the second shows how to configure the chart to pull from a JSON data source using AJAX.
+
+
+Documentation
+-------------
+Documentation for Flask-GoogleCharts can be found at `http://pythonhosted.org/Flask-GoogleCharts/ <http://pythonhosted.org/Flask-GoogleCharts/>`_.
+
+Support
+-------
+Flask-GoogleCharts is tested against Python 2.7 and 3.4 - 3.7.
+
+.. image:: https://travis-ci.org/wikkiewikkie/flask-googlecharts.svg?branch=master
+    :target: https://travis-ci.org/wikkiewikkie/flask-googlecharts
---
+++ packages/Flask-GoogleCharts/Flask-GoogleCharts-0.1.0.tar.gz/Flask-GoogleCharts-0.1.0/setup.py
@@ -0,0 +1,51 @@
+"""
+Flask-GoogleCharts
+==================
+
+Adds Google Charts API support to Flask.
+
+Links
+=====
+
+* `Source Code <https://github.com/wikkiewikkie/flask-googlecharts>`_
+* `Issues <https://github.com/wikkiewikkie/flask-googlecharts/issues>`_
+
+"""
+from setuptools import setup
+
+extra = {}
+install_requires = ['Flask>=0.10.1']
+setup(
+    name='Flask-GoogleCharts',
+    version='0.1.0',
+    url='http://github.com/wikkiewikkie/flask-googlecharts/',
+    license='MIT',
+    author='Kevin Schellenberg',
+    author_email='wikkiewikkie@gmail.com',
+    description='Google Charts API support for Flask',
+    long_description=__doc__,
+    packages=['flask_googlecharts'],
+    package_data={
+        'flask_googlecharts': ['templates/*', 'static/*'], },
+    py_modules=['flask_googlecharts'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=install_requires,
+    test_suite='test_googlecharts',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: JavaScript',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    **extra)
