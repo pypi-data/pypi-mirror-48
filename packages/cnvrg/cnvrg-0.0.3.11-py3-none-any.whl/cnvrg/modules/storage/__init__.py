ffbcf4e9c735e1c45d500e79a812ae386a0ad926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package cnvrg/0.0.3.11/cnvrg-0.0.3.11-py3-none-any.whl

---
+++ packages/cnvrg/cnvrg-0.0.3.11-py3-none-any.whl/cnvrg/modules/storage/__init__.py
@@ -0,0 +1,15 @@
+from cnvrg.modules.storage.storage import Storage
+from cnvrg.modules.storage.s3_storage import S3Storage
+from cnvrg.modules.project import Project
+from cnvrg.helpers.apis_helper import post as apis_post, get as apis_get, download_file
+import os
+
+
+STORAGE_TYPE_GCP = "gcp"
+STORAGE_TYPE_S3 = "s3"
+STORAGE_TYPE_MINIO = "minio"
+
+def storage_factory(element: Project, working_dir=os.curdir):
+    storage_resp = apis_get(element.get_storage_url()).get("client")
+    if storage_resp.get("storage") == STORAGE_TYPE_S3:
+        return S3Storage(element, working_dir, storage_resp)
