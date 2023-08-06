8815a0d538faeea41b7b5cd9bbd9abe31cfa57ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:32 +0000

    Add package hideout/0.1.5/hideout-0.1.5.tar.gz

---
+++ packages/hideout/hideout-0.1.5.tar.gz/hideout-0.1.5/hideout/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+import pickle
+import contextlib
+from hideout import env, log
+from hideout.log import logger
+
+
+@contextlib.contextmanager
+def resume(file_name):
+    file_path = "{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+    target = None
+    if os.path.exists(file_path) and not env.HIDEOUT_FORCE_CACHE:
+        logger.error("found {}".format(file_path))
+        with open(file_path, mode='rb') as f:
+            target = pickle.load(f)
+    yield target
+    if target is None:
+        freeze(target, file_name)
+
+
+def freeze(target, file_name):
+    file_path="{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+    if (not os.path.exists(file_path)) or env.HIDEOUT_FORCE_CACHE:
+        logger.info("saving {}".format(file_path))
+        with open(file_path, mode='wb') as f:
+            return pickle.dump(target, f)
