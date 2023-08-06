3362927e0f46c7d4fcb0caa44f66263fc64c44f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:56 +0000

    Add package angrcli/1.0rc1/angrcli-1.0rc1.tar.gz

---
+++ packages/angrcli/angrcli-1.0rc1.tar.gz/angrcli-1.0rc1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='angrcli',
+    version='1.0rc1',
+    packages=find_packages(exclude=['tests.*', 'tests', 'example.*', 'example']),
+    include_package_data=True,
+    license='MIT',
+    long_description='none',
+    python_requires='>=3.6',
+    url='https://github.com/fmagin/angr-cli'
+)
