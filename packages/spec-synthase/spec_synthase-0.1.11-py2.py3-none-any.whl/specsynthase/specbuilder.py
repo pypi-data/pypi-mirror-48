bd329f2704c9e7a40c92c2b6455cdc668670f249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:05 +0000

    Add package spec-synthase/0.1.11/spec_synthase-0.1.11-py2.py3-none-any.whl

---
+++ packages/spec-synthase/spec_synthase-0.1.11-py2.py3-none-any.whl/specsynthase/specbuilder.py
@@ -0,0 +1,56 @@
+import yaml
+from collections import OrderedDict
+from swagger_spec_validator.validator20 import validate_spec
+
+
+class SpecBuilder(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        OrderedDict.__init__(self, *args, **kwargs)
+        self._mergeable_keys = ["definitions", "parameters", "paths", "responses", "securityDefinitions"]
+
+    def _load_spec(self, file_name):
+        with open(file_name, 'r') as spec_file:
+            return yaml.load(spec_file, Loader=yaml.FullLoader)
+
+    def _merge_part(self, key, part_dict):
+        if part_dict:
+            if key not in self:
+                self[key] = {}
+            self._merge_dict(self[key], part_dict)
+
+    def _merge_dict(self, main_dict, part_dict):
+        self._test_merge(main_dict, part_dict)
+        main_dict.update(part_dict)
+
+    def _test_merge(self, main_dict, part_dict):
+        for k, v in part_dict.items():
+            if k in main_dict:
+                raise ValueError("Key {} already exists in spec file.".format(k))
+
+    def _cut_part(self, spec, key):
+        part = {}
+        if key in spec:
+            part = spec[key]
+            del spec[key]
+        return part
+
+    def _cut_parts(self, spec):
+        parts = []
+        for merg_key in self._mergeable_keys:
+            parts.append((merg_key, self._cut_part(spec, merg_key)))
+        return parts
+
+    def add_spec(self, file_name):
+        spec = self._load_spec(file_name)
+        parts = self._cut_parts(spec)
+        self._merge_dict(self, spec)
+        for merg_key, spec_part in parts:
+            self._merge_part(merg_key, spec_part)
+        return self
+
+    def dump(self, *args, **kwargs):
+        return yaml.safe_dump(dict(**self), *args, **kwargs)
+
+    def validate(self):
+        validate_spec(self)
+        return self
---
+++ packages/spec-synthase/spec_synthase-0.1.11-py2.py3-none-any.whl/tests/test_specsynthase.py
@@ -0,0 +1,60 @@
+import pytest
+import yaml
+
+from os import path
+from swagger_spec_validator import SwaggerValidationError
+from specsynthase.specbuilder import SpecBuilder
+
+
+def _get_base_dir():
+    return path.join(path.abspath(path.dirname(__file__)), "fixtures", "petstore")
+
+
+def _check_dicts(l, r):
+    for k, v in l.items():
+        assert (k in r)
+        if isinstance(v, dict):
+            _check_dicts(v, r[k])
+
+
+def test_build_fluent():
+    base_dir = _get_base_dir()
+    full = yaml.load(
+        open(path.join(base_dir, "full.yml")).read(),
+        Loader=yaml.FullLoader)
+    spec = SpecBuilder().add_spec(path.join(base_dir, "security.yml"))\
+                        .add_spec(path.join(base_dir, "base.yml"))\
+                        .add_spec(path.join(base_dir, "definitions.yml"))\
+                        .add_spec(path.join(base_dir, "paths.yml"))
+    _check_dicts(full, spec)
+    _check_dicts(spec, full)
+
+
+def test_build_existing_key():
+    base_dir = _get_base_dir()
+    with pytest.raises(ValueError):
+        SpecBuilder().add_spec(path.join(base_dir, "base.yml"))\
+                     .add_spec(path.join(base_dir, "definitions.yml"))\
+                     .add_spec(path.join(base_dir, "definitions.yml"))
+
+
+def test_dump():
+    base_dir = _get_base_dir()
+    parts = ["base.yml", "definitions.yml", "paths.yml", "security.yml"]
+    full = yaml.load(
+        open(path.join(base_dir, "full.yml")).read(),
+        Loader=yaml.FullLoader)
+
+    spec = SpecBuilder()
+    for p in parts:
+        spec.add_spec(path.join(base_dir, p))
+
+    # Dumping and reloading the yaml is slow, but we can't compare dumped
+    # strings because of potential key ordering differences.
+    assert full == yaml.load(spec.dump(), Loader=yaml.FullLoader)
+
+
+def test_validation_fail():
+    base_dir = _get_base_dir()
+    with pytest.raises(SwaggerValidationError):
+        SpecBuilder().add_spec(path.join(base_dir, "base.yml")).validate()
