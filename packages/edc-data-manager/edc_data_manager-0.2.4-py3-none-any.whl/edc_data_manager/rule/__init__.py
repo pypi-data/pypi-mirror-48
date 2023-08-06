e0399765576b8cf594e2daad56066c3e1edcaebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:41 +0000

    Add package edc-data-manager/0.2.4/edc_data_manager-0.2.4-py3-none-any.whl

---
+++ packages/edc-data-manager/edc_data_manager-0.2.4-py3-none-any.whl/edc_data_manager/rule/__init__.py
@@ -0,0 +1,4 @@
+from .rule_wrapper import RuleWrapper
+from .rule_runner import RuleRunner, MetaDataInspector
+from .handlers import SimpleHandler, ModelHandler
+from .update import update_crf_query_rules
