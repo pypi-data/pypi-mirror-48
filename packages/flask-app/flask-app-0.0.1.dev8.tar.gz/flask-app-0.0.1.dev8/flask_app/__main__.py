aee36f93fa4c9173cccfef6a5932e64bf8bc1c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:15 +0000

    Add package flask-app/0.0.1.dev8/flask-app-0.0.1.dev8.tar.gz

---
+++ packages/flask-app/flask-app-0.0.1.dev8.tar.gz/flask-app-0.0.1.dev8/flask_app/__main__.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import shutil
+import sys
+
+
+cwd = os.getcwd()
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def program(argv):
+    parser = argparse.ArgumentParser(
+        description='My flask  app boilerplate code')
+    parser.add_argument('appname', help='The application name')
+    parser.add_argument('-d', '--destination', help='Folder to copy code to')
+    args = parser.parse_args()
+
+    appname = args.appname
+    fullpath = os.path.join(cwd, appname)
+    destination = args.destination
+    shutil.copytree(os.path.join(script_dir, destination), fullpath)
+    os.remove(fullpath+'/__main__.py')
+    # create .env file
+    import pdb; pdb.set_trace()
+    shutil.copy(fullpath+'/.env.sample', fullpath+'/.env')
+
+
+def main():
+    program(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
