9eb5f8b5c56a3fb51d08ec1c54842c0a2923e3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:04 +0000

    Add package exif2pandas/1.0/exif2pandas-1.0.tar.gz

---
+++ packages/exif2pandas/exif2pandas-1.0.tar.gz/exif2pandas-1.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='exif2pandas',
+    version='1.0',
+    description="Extract EXIF to pandas / SQL / Excel / Feather",
+    author="Visgean",
+    author_email='visgean@gmail.com',
+    url='https://github.com/visgean/exif2pandas',
+    packages=[
+        'exif2pandas',
+    ],
+    package_dir={'exif2pandas': 'exif2pandas'},
+    license="MIT",
+    keywords='exif sql',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'exifread',
+        'pandas',
+        'python-slugify',
+    ],
+    entry_points={
+        'console_scripts': [
+            'exif2pandas = exif2pandas.main:main'
+        ]
+    },
+)
