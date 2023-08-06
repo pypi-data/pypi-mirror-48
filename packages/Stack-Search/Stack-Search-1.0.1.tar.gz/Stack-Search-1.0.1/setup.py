f030c583a5cbeecfb35b7bf8beff3228fc456465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:29 +0000

    Add package Stack-Search/1.0.1/Stack-Search-1.0.1.tar.gz

---
+++ packages/Stack-Search/Stack-Search-1.0.1.tar.gz/Stack-Search-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+                 name="Stack-Search",
+                 version="1.0.1",
+                 author="Saran Goda",
+                 author_email="godasaran@gmail.com",
+                 description="An advanced exception handler that searches your exception in Stack Overflow",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/pypa/sampleproject",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                              "Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: MIT License",
+                              "Operating System :: OS Independent",
+                              ],
+                 )
