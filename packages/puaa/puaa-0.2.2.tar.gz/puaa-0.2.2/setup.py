0292d5767e9fd14743e7b91a8bb5dae1c222ac0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:25 +0000

    Add package puaa/0.2.2/puaa-0.2.2.tar.gz

---
+++ packages/puaa/puaa-0.2.2.tar.gz/puaa-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name = 'puaa',
+    version = '0.2.2',
+    description = 'Python UAA test server',
+    url = 'https://gitlab.com/romk/puaa',
+    license = 'MIT',
+    author = 'romk',
+    author_email = 'r0mk@gmx.net',
+    install_requires = [
+        'Authlib',
+        'Flask',
+        'gunicorn[gevent]',
+        'PyYAML>=3.12',
+    ],
+    packages = ['puaa'],
+    python_requires = '>=3',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing :: Mocking',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ]
+)
