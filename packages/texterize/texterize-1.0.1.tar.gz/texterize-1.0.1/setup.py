f9fb322784e5a643163ad1d5eb3b040c68cd1bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package texterize/1.0.1/texterize-1.0.1.tar.gz

---
+++ packages/texterize/texterize-1.0.1.tar.gz/texterize-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="texterize",
+    version="1.0.1",
+    author="Cooper Barth",
+    author_email="cooperfbarth@gmail.com",
+    description="Making pictures worth a thousand words.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/cooperbarth/texterize",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'python-docx',
+        'pillow'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+'''
+PACKAGING:
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload dist/* --skip-existing
+'''
