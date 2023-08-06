4eb0c5d0c3bfe6da6dff152f2e4d882dedeb0f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:18 +0000

    Add package normal-binomial-distributions/20190707/normal_binomial_distributions-20190707.tar.gz

---
+++ packages/normal-binomial-distributions/normal_binomial_distributions-20190707.tar.gz/normal_binomial_distributions-20190707/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'normal_binomial_distributions',
+      version = 'v20190707',
+      description = 'normal_binomial_distributions',
+      package = ['normal_binomial_distributions'],
+      author = 'Yedan Zhang',
+      author_email = 'yedan.zhang.projects@gmail.com',
+      zip_safe = False)
