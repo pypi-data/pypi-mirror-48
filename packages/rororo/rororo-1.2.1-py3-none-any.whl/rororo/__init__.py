ec847aee2b29f91bde699378bbb262e185974dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:29 +0000

    Add package rororo/1.2.1/rororo-1.2.1-py3-none-any.whl

---
+++ packages/rororo/rororo-1.2.1-py3-none-any.whl/rororo/__init__.py
@@ -0,0 +1,14 @@
+"""
+======
+rororo
+======
+
+Collection of utilities, helpers, and principles for building Python backend
+applications. Supports `aiohttp.web <http://aiohttp.readthedocs.org/>`_,
+`Flask <http://flask.pocoo.org/>`_, and your web-framework.
+
+"""
+
+__author__ = "Igor Davydenko"
+__license__ = "BSD-3-Clause"
+__version__ = "1.2.1"
