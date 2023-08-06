e983541f833e8219cac12575164837bf94495beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package pylibra/0.0.2/pylibra-0.0.2.tar.gz

---
+++ packages/pylibra/pylibra-0.0.2.tar.gz/pylibra-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+REQUIREMENTS = [
+    line.strip()
+    for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
+]
+
+
+setup(
+    name="pylibra",
+    version="0.0.2",
+    license="MIT",
+    author="Band Protocol",
+    author_email="opensource@bandprotocol.com",
+    description="A Python client for Libra network",
+    url="https://github.com/bandprotocol/pylibra",
+    packages=["pylibra"],
+    keywords=["libra", "client", "cryptocurrency", "blockchain"],
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
