5998c9c4d4eba4bfc5f87cb5ced72b172b82747a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:23 +0000

    Add package pysubway/0.0.1/pysubway-0.0.1.tar.gz

---
+++ packages/pysubway/pysubway-0.0.1.tar.gz/pysubway-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='pysubway',
+    version='0.0.1',
+    author='kougazhang',
+    author_email='kougazhang@gmail.com',
+    url='https://github.com/kougazhang',
+    description='build on flask',
+    packages=find_packages(),
+    install_requires=[]
+)
+
+
