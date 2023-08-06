37fad4a16d9c7e31e1f19c2b5051461902134b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:28 +0000

    Add package goenrichment/0.0.9a3/goenrichment-0.0.9a3.tar.gz

---
+++ packages/goenrichment/goenrichment-0.0.9a3.tar.gz/goenrichment-0.0.9a3/bin/goenrich.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import pickle
+import argparse
+
+from goenrichment.enrichment import calculate
+from goenrichment.go import load_goenrichdb
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Calculate GO enrichment from a list of genes. Default database organism: human')
+
+    parser.add_argument('-i', help='Input list of gene names', required=True)
+    parser.add_argument('-o', help='TSV file with all results', required=True)
+    parser.add_argument('--goenrichDB', help='Gene2GO pickle file created with '
+                                             '"goenrichDB.py". If not provided '
+                                             'the database is loaded from:'
+                                             '', required=False)
+    parser.add_argument('--min_category_depth', help='Min GO term graph depth '
+                                                     'to include in the report. '
+                                                     'Default: 4', required=False)
+    parser.add_argument('--min_category_size', help='Min number of gene in a GO '
+                                                    'term to include in the report. '
+                                                    'Default: 3', required=False)
+    parser.add_argument('--max_category_size', help='Max number of gene in a GO '
+                                                    ''
+                                                    'term to include in the report. Default: 500', required=False)
+    parser.add_argument('--alpha', help='Alpha value for p-value correction. '
+                                        'Default: 0.05', required=False)
+
+    args = parser.parse_args()
+
+    output = args.o
+    input = args.i
+
+    goenrichDB = "ftp://ftp.ncbi.nlm.nih.gov/pub/goenrichment/goenrichDB_human.pickle"
+    if args.goenrichDB:
+        goenrichDB = args.goenrichDB
+
+    godb = load_goenrichdb(goenrichDB)
+
+    alpha = 0.05
+    if args.alpha:
+        alpha = float(args.alpha)
+
+    min_category_depth = 4
+    if args.min_category_depth:
+        min_category_depth = int(args.min_category_depth)
+
+    min_category_size = 3
+    if args.min_category_size:
+        min_category_size = int(args.min_category_size)
+
+    max_category_size = 500
+    if args.max_category_size:
+        max_category_size = int(args.max_category_size)
+
+    query = set()
+    with open(input) as fin:
+        for l in fin:
+            query.add(l.strip())
+
+    print('There are %d alternative ids in database' % (len(godb['alt_id'])))
+    print('There are %d GO terms' % (len(godb['graph'].node)))
+    print('There are %d genes in database' % (godb['M']))
+    print('Query size: %d genes' % len(query))
+
+    df = calculate(godb, query, alpha, min_category_depth, min_category_size, max_category_size)
+    print('GO terms with q less than %.2f: %d' % (alpha, len(df[df['q'] <= alpha])))
+    df.to_csv(output, sep='\t', index=None)
---
+++ packages/goenrichment/goenrichment-0.0.9a3.tar.gz/goenrichment-0.0.9a3/bin/goenrichDB.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+import sys
+import pickle
+import argparse
+
+from goenrichment.go import create_go_graph, update_go_graph
+from goenrichment.parsers import load_ncbi_gene, load_tsv_gene, load_uniprot_goa_gene
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Creates pickle data structure used by "goenrich.py"')
+    parser.add_argument('--gene_info', help='NCBI gene_info file', required=False)
+    parser.add_argument('--gene2go', help='NCBI gene2go file', required=False)
+    parser.add_argument('--tsv', help='TSV file with at least two columns: Gene_name<tab>GO terms', required=False)
+    parser.add_argument('--goenrichDB', help='Previous created goenrich pickle file. '
+                                   'The new genes will be added to this database', required=False)
+    parser.add_argument('--goa_uniprot', help='Uniprot GOA file GAF format', required=False)
+    parser.add_argument('--gobo', help='UGO Obo file from Gene Ontology', required=False)
+    parser.add_argument('--taxid', help='Process genes for tax id if it is possible', required=False)
+    parser.add_argument('-o', help='Pickle output file name', required=True)
+
+    args = parser.parse_args()
+    pickle_file = args.o
+    tax_id = None
+    if args.taxid:
+        tax_id = args.taxid
+
+    geneGoDB = None
+    if args.gobo:
+        print('Creating geneGODB data structure')
+        geneGoDB = create_go_graph(args.gobo)
+
+    if args.goenrichDB:
+        print('Loading pickle file')
+        geneGoDB = pickle.load(open(args.goenrichDB, "rb"))
+
+    if not geneGoDB:
+        print('Should use one of the options --gobo or --goenrichDB')
+        sys.exit(-1)
+
+    print('There are %d alternative ids' % (len(geneGoDB['alt_id'])))
+    print('There are %d GO terms' % (len(geneGoDB['graph'].node)))
+
+    if args.gene_info:
+        gene_info_file = args.gene_info
+    else:
+        gene_info_file = None
+
+    if args.gene2go:
+        gene2go_file = args.gene2go
+    else:
+        gene2go_file = None
+
+    if gene_info_file and gene2go_file:
+        print('Loaging NCBI gene data')
+        values = load_ncbi_gene(geneGoDB, gene_info_file, gene2go_file, tax_id)
+        geneGoDB = update_go_graph(geneGoDB, values, 'genes')
+        print('There are %d genes' % (geneGoDB['M']))
+
+    if args.tsv:
+        print('Loaging TSV gene data')
+        values = load_tsv_gene(geneGoDB, args.tsv)
+        geneGoDB = update_go_graph(geneGoDB, values, 'genes')
+        print('There are %d genes' % (geneGoDB['M']))
+
+    if args.goa_uniprot:
+        print('Loaging Uniprot gene data')
+        values = load_uniprot_goa_gene(geneGoDB, args.goa_uniprot, tax_id)
+        geneGoDB = update_go_graph(geneGoDB, values, 'genes')
+        print('There are %d genes' % (geneGoDB['M']))
+
+    print('There are %d alternative ids' % (len(geneGoDB['alt_id'])))
+    print('There are %d GO terms' % (len(geneGoDB['graph'].node)))
+    print('There are %d genes' % (geneGoDB['M']))
+    pickle.dump(geneGoDB, open(pickle_file, "wb"))
