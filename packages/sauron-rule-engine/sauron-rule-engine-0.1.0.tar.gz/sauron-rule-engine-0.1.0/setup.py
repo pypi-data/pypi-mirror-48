c74327799058ba80e1dca971e43ce9ee00ab3e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:34 +0000

    Add package sauron-rule-engine/0.1.0/sauron-rule-engine-0.1.0.tar.gz

---
+++ packages/sauron-rule-engine/sauron-rule-engine-0.1.0.tar.gz/sauron-rule-engine-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['sauron_rule_engine']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sauron-rule-engine',
+    'version': '0.1.0',
+    'description': 'A simple rule engine implemented in python',
+    'long_description': None,
+    'author': 'jlugao',
+    'author_email': 'joaolhullier@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
