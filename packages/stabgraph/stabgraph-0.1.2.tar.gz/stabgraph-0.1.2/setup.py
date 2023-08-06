c9bbbf1fdc8f7f6131fe9f2648c26feb4cbd9b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:30 +0000

    Add package stabgraph/0.1.2/stabgraph-0.1.2.tar.gz

---
+++ packages/stabgraph/stabgraph-0.1.2.tar.gz/stabgraph-0.1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import io
+from setuptools import setup, find_packages
+
+long_description=io.open("README.md",encoding='utf-8').read()
+
+setup(name='stabgraph',
+      version='0.1.2',
+      description='Transforms stabilizer state into graph state',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      keywords='stabilizer graph local clifford',
+      author='David Amaro',
+      author_email='davamaro@hotmail.es',
+      license='Apache 2',
+      packages=find_packages(),
+      download_url='https://github.com/davamaro/stabgraph/archive/0.1.zip',
+      python_requires='>=3.5',     
+      install_requires=[
+          'numpy',
+      ],
+      include_package_data=True)
