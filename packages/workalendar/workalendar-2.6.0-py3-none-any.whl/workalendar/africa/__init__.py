18429b3c62bd0ca849f8445bde13d917933b956d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:47 +0000

    Add package workalendar/2.6.0/workalendar-2.6.0-py3-none-any.whl

---
+++ packages/workalendar/workalendar-2.6.0-py3-none-any.whl/workalendar/africa/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+from .algeria import Algeria
+from .benin import Benin
+from .ivory_coast import IvoryCoast
+from .madagascar import Madagascar
+from .sao_tome import SaoTomeAndPrincipe
+from .south_africa import SouthAfrica
+from .angola import Angola
+
+
+__all__ = (
+    'Algeria',
+    'Benin',
+    'IvoryCoast',
+    'Madagascar',
+    'SaoTomeAndPrincipe',
+    'SouthAfrica',
+    'Angola',
+)
