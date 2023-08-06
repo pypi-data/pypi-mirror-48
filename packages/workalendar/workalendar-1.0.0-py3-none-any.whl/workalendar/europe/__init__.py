1c8af299129829e4037ef7fe8635616342d91275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:42 +0000

    Add package workalendar/1.0.0/workalendar-1.0.0-py3-none-any.whl

---
+++ packages/workalendar/workalendar-1.0.0-py3-none-any.whl/workalendar/europe/__init__.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from .austria import Austria
+from .belgium import Belgium
+from .bulgaria import Bulgaria
+from .croatia import Croatia
+from .czech_republic import CzechRepublic
+from .estonia import Estonia
+from .european_central_bank import EuropeanCentralBank
+from .denmark import Denmark
+from .finland import Finland
+from .france import France, FranceAlsaceMoselle
+from .greece import Greece
+from .hungary import Hungary
+from .iceland import Iceland
+from .ireland import Ireland
+from .italy import Italy
+from .luxembourg import Luxembourg
+from .netherlands import Netherlands
+from .norway import Norway
+from .poland import Poland
+from .portugal import Portugal
+from .slovakia import Slovakia
+from .slovenia import Slovenia
+from .spain import Spain, Catalonia
+from .sweden import Sweden
+from .switzerland import Switzerland
+from .united_kingdom import UnitedKingdom, UnitedKingdomNorthernIreland
+
+# Germany
+from .germany import (
+    Germany, BadenWurttemberg, Bavaria, Berlin, Brandenburg, Bremen,
+    Hamburg, Hesse, MecklenburgVorpommern, LowerSaxony,
+    NorthRhineWestphalia, RhinelandPalatinate, Saarland, Saxony,
+    SaxonyAnhalt, SchleswigHolstein, Thuringia
+)
+
+
+__all__ = (
+    Austria,
+    Belgium,
+    Bulgaria,
+    Catalonia,
+    Croatia,
+    CzechRepublic,
+    Denmark,
+    Estonia,
+    EuropeanCentralBank,
+    Finland,
+    France,
+    FranceAlsaceMoselle,
+    Greece,
+    Hungary,
+    Iceland,
+    Ireland,
+    Italy,
+    Luxembourg,
+    Netherlands,
+    Norway,
+    Poland,
+    Portugal,
+    Slovakia,
+    Slovenia,
+    Spain,
+    Sweden,
+    Switzerland,
+    UnitedKingdom,
+    UnitedKingdomNorthernIreland,
+
+    # Germany
+    Germany, BadenWurttemberg, Bavaria, Berlin, Brandenburg, Bremen,
+    Hamburg, Hesse, MecklenburgVorpommern, LowerSaxony,
+    NorthRhineWestphalia, RhinelandPalatinate, Saarland, Saxony,
+    SaxonyAnhalt, SchleswigHolstein, Thuringia,
+)
