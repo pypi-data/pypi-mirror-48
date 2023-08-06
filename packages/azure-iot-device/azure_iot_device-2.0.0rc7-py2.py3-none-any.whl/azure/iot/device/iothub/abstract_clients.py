7002f5987ad3f30fe5aa430cde3f4a2334533d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:20 +0000

    Add package azure-iot-device/2.0.0rc7/azure_iot_device-2.0.0rc7-py2.py3-none-any.whl

---
+++ packages/azure-iot-device/azure_iot_device-2.0.0rc7-py2.py3-none-any.whl/azure/iot/device/iothub/abstract_clients.py
@@ -0,0 +1,166 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""This module contains abstract classes for the various clients of the Azure IoT Hub Device SDK
+"""
+
+import six
+import abc
+import logging
+import os
+from . import auth
+from .pipeline import IoTHubPipeline
+
+
+logger = logging.getLogger(__name__)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractIoTHubClient(object):
+    """A superclass representing a generic client. This class needs to be extended for specific clients."""
+
+    def __init__(self, pipeline):
+        """Initializer for a generic client.
+
+        :param pipeline: The pipeline that the client will use.
+        """
+        # TODO: Refactor this to be an iothub_pipeline, and instantiate here instead of
+        # in the factory methods
+        self._pipeline = pipeline
+
+    @classmethod
+    def create_from_connection_string(cls, connection_string, trusted_certificate_chain=None):
+        """
+        Instantiate the client from a IoTHub device or module connection string.
+
+        :param str connection_string: The connection string for the IoTHub you wish to connect to.
+        :param str trusted_certificate_chain: The trusted certificate chain. Necessary when using a
+        connection string with a GatewayHostName parameter. DEFAULT: None
+
+        :raises: ValueError if given an invalid connection_string.
+        """
+        # TODO: Make this device/module specific and reject non-matching connection strings.
+        # This will require refactoring of the auth package to use common objects (e.g. ConnectionString)
+        # in order to differentiate types of connection strings.
+        authentication_provider = auth.SymmetricKeyAuthenticationProvider.parse(connection_string)
+        authentication_provider.ca_cert = (
+            trusted_certificate_chain
+        )  # TODO: make this part of the instantiation
+        pipeline = IoTHubPipeline(authentication_provider)
+        return cls(pipeline)
+
+    @classmethod
+    def create_from_shared_access_signature(cls, sas_token):
+        """
+        Instantiate the client from a Shared Access Signature (SAS) token.
+
+        This method of instantiation is not recommended for general usage.
+
+        :param str sas_token: The string representation of a SAS token.
+
+        :raises: ValueError if given an invalid sas_token
+        """
+        authentication_provider = auth.SharedAccessSignatureAuthenticationProvider.parse(sas_token)
+        pipeline = IoTHubPipeline(authentication_provider)
+        return cls(pipeline)
+
+    @abc.abstractmethod
+    def connect(self):
+        pass
+
+    @abc.abstractmethod
+    def disconnect(self):
+        pass
+
+    @abc.abstractmethod
+    def send_d2c_message(self, message):
+        pass
+
+    @abc.abstractmethod
+    def receive_method_request(self, method_name=None):
+        pass
+
+    @abc.abstractmethod
+    def send_method_response(self, method_request, payload, status):
+        pass
+
+    @abc.abstractmethod
+    def get_twin(self):
+        pass
+
+    @abc.abstractmethod
+    def patch_twin_reported_properties(self, reported_properties_patch):
+        pass
+
+    @abc.abstractmethod
+    def receive_twin_desired_properties_patch(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractIoTHubDeviceClient(AbstractIoTHubClient):
+    @classmethod
+    def create_from_x509_certificate(cls, hostname, device_id, x509):
+        """
+        Instantiate a client which using X509 certificate authentication.
+        :param hostname: Host running the IotHub. Can be found in the Azure portal in the Overview tab as the string hostname.
+        :param device_id: The ID is used to uniquely identify a device in the IoTHub
+        :param x509: The complete x509 certificate object, To use the certificate the enrollment object needs to contain cert (either the root certificate or one of the intermediate CA certificates).
+        If the cert comes from a CER file, it needs to be base64 encoded.
+        :type x509: X509
+        :return: A IoTHubClient which can use X509 authentication.
+        """
+        authentication_provider = auth.X509AuthenticationProvider(
+            hostname=hostname, device_id=device_id, x509=x509
+        )
+        pipeline = IoTHubPipeline(authentication_provider)
+        return cls(pipeline)
+
+    @abc.abstractmethod
+    def receive_c2d_message(self):
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractIoTHubModuleClient(AbstractIoTHubClient):
+    @classmethod
+    def create_from_edge_environment(cls):
+        """
+        Instantiate the client from the IoT Edge environment.
+
+        This method can only be run from inside an IoT Edge container.
+
+        :raises: IoTEdgeError if the IoT Edge container is not configured correctly.
+        """
+        try:
+            hostname = os.environ["IOTEDGE_IOTHUBHOSTNAME"]
+            device_id = os.environ["IOTEDGE_DEVICEID"]
+            module_id = os.environ["IOTEDGE_MODULEID"]
+            gateway_hostname = os.environ["IOTEDGE_GATEWAYHOSTNAME"]
+            module_generation_id = os.environ["IOTEDGE_MODULEGENERATIONID"]
+            workload_uri = os.environ["IOTEDGE_WORKLOADURI"]
+            api_version = os.environ["IOTEDGE_APIVERSION"]
+        except KeyError:
+            raise auth.IoTEdgeError("IoT Edge container not configured correctly")
+
+        authentication_provider = auth.IoTEdgeAuthenticationProvider(
+            hostname=hostname,
+            device_id=device_id,
+            module_id=module_id,
+            gateway_hostname=gateway_hostname,
+            module_generation_id=module_generation_id,
+            workload_uri=workload_uri,
+            api_version=api_version,
+        )
+        pipeline = IoTHubPipeline(authentication_provider)
+        return cls(pipeline)
+
+    @abc.abstractmethod
+    def send_to_output(self, message, output_name):
+        pass
+
+    @abc.abstractmethod
+    def receive_input_message(self, input_name):
+        pass
---
+++ packages/azure-iot-device/azure_iot_device-2.0.0rc7-py2.py3-none-any.whl/azure/iot/device/iothub/transport/mqtt/mqtt_provider.py
@@ -0,0 +1,183 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import paho.mqtt.client as mqtt
+import logging
+import ssl
+import traceback
+
+logger = logging.getLogger(__name__)
+
+
+class MQTTProvider(object):
+    """
+    A wrapper over the actual implementation of mqtt message broker which will eventually connect to an mqtt broker
+    to publish/subscribe messages.
+    """
+
+    def __init__(self, client_id, hostname, username, ca_cert=None):
+        """
+        Constructor to instantiate a mqtt provider.
+        :param client_id: The id of the client connecting to the broker.
+        :param hostname: hostname or IP address of the remote broker.
+        :param ca_cert: Certificate which can be used to validate a server-side TLS connection.
+        """
+        self._client_id = client_id
+        self._hostname = hostname
+        self._username = username
+        self._mqtt_client = None
+        self._ca_cert = ca_cert
+
+        self.on_mqtt_connected = None
+        self.on_mqtt_disconnected = None
+        self.on_mqtt_published = None
+        self.on_mqtt_subscribed = None
+        self.on_mqtt_unsubscribed = None
+        self.on_mqtt_message_received = None
+
+        self._create_mqtt_client()
+
+    def _create_mqtt_client(self):
+        """
+        Create the MQTT client object and assign all necessary callbacks.
+        """
+        logger.info("creating mqtt client")
+
+        self._mqtt_client = mqtt.Client(self._client_id, False, protocol=mqtt.MQTTv311)
+
+        def on_connect_callback(client, userdata, flags, result_code):
+            logger.info("connected with result code: %s", str(result_code))
+            # TODO: how to do failed connection?
+            try:
+                self.on_mqtt_connected()
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_connected")
+                logger.error(traceback.format_exc())
+
+        def on_disconnect_callback(client, userdata, result_code):
+            logger.info("disconnected with result code: %s", str(result_code))
+            try:
+                self.on_mqtt_disconnected()
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_disconnected")
+                logger.error(traceback.format_exc())
+
+        def on_publish_callback(client, userdata, mid):
+            logger.info("payload published for %s", str(mid))
+            # TODO: how to do failed publish
+            try:
+                self.on_mqtt_published(mid)
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_published")
+                logger.error(traceback.format_exc())
+
+        def on_subscribe_callback(client, userdata, mid, granted_qos):
+            logger.info("suback received for %s", str(mid))
+            # TODO: how to do failure?
+            try:
+                self.on_mqtt_subscribed(mid)
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_subscribed")
+                logger.error(traceback.format_exc())
+
+        def on_message_callback(client, userdata, mqtt_message):
+            logger.info("message received on %s", mqtt_message.topic)
+            try:
+                self.on_mqtt_message_received(mqtt_message._topic, mqtt_message.payload)
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_message_received")
+                logger.error(traceback.format_exc())
+
+        def on_unsubscribe_callback(client, userdata, mid):
+            logger.info("UNSUBACK received for %s", str(mid))
+            # TODO: how to do failure?
+            try:
+                self.on_mqtt_unsubscribed(mid)
+            except:  # noqa: E722 do not use bare 'except'
+                logger.error("Unexpected error calling on_mqtt_unsubscribed")
+                logger.error(traceback.format_exc())
+
+        self._mqtt_client.on_connect = on_connect_callback
+        self._mqtt_client.on_disconnect = on_disconnect_callback
+        self._mqtt_client.on_publish = on_publish_callback
+        self._mqtt_client.on_subscribe = on_subscribe_callback
+        self._mqtt_client.on_message = on_message_callback
+        self._mqtt_client.on_unsubscribe = on_unsubscribe_callback
+
+        logger.info("Created MQTT provider, assigned callbacks")
+
+    def connect(self, password):
+        """
+        This method connects the upper transport layer to the mqtt broker.
+        This method should be called as an entry point before sending any telemetry.
+        """
+        logger.info("connecting to mqtt broker")
+
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        if self._ca_cert:
+            ssl_context.load_verify_locations(cadata=self._ca_cert)
+        else:
+            ssl_context.load_default_certs()
+        ssl_context.verify_mode = ssl.CERT_REQUIRED
+        ssl_context.check_hostname = True
+        self._mqtt_client.tls_set_context(ssl_context)
+        self._mqtt_client.tls_insecure_set(False)
+        self._mqtt_client.username_pw_set(username=self._username, password=password)
+
+        self._mqtt_client.connect(host=self._hostname, port=8883)
+        self._mqtt_client.loop_start()
+
+    def reconnect(self, password):
+        """
+        This method reconnects the mqtt broker, possibly because of a password (sas) change
+        Connect should have previously been called.
+        """
+        logger.info("reconnecting transport")
+        self._mqtt_client.username_pw_set(username=self._username, password=password)
+        self._mqtt_client.reconnect()
+
+    def disconnect(self):
+        """
+        This method disconnects the mqtt provider. This should be called from the upper transport
+        when it wants to disconnect from the mqtt provider.
+        """
+        logger.info("disconnecting transport")
+        self._mqtt_client.disconnect()
+
+    def publish(self, topic, message_payload):
+        """
+        This method enables the transport to send a message to the message broker.
+        By default the the quality of service level to use is set to 1.
+        :param topic: topic: The topic that the message should be published on.
+        :param message_payload: The actual message to send.
+        :return message ID for the publish request.
+        """
+        logger.info("sending")
+        message_info = self._mqtt_client.publish(topic=topic, payload=message_payload, qos=1)
+        return message_info.mid
+
+    def subscribe(self, topic, qos=0):
+        """
+        This method subscribes the client to one topic.
+        :param topic: a single string specifying the subscription topic to subscribe to
+        :param qos: the desired quality of service level for the subscription. Defaults to 0.
+        :return: message ID for the subscribe request
+        Raises a ValueError if qos is not 0, 1 or 2, or if topic is None or has zero string length,
+        """
+        logger.info("subscribing to %s with qos %s", topic, str(qos))
+        (result, mid) = self._mqtt_client.subscribe(topic, qos)
+        return mid
+
+    def unsubscribe(self, topic):
+        """
+        Unsubscribe the client from one topic.
+        :param topic: a single string which is the subscription topic to unsubscribe from.
+        :return: mid the message ID for the unsubscribe request.
+        Raises a ValueError if topic is None or has zero string length, or is not a string.
+        """
+        logger.info("unsubscribing from %s", topic)
+        (result, mid) = self._mqtt_client.unsubscribe(topic)
+        return mid
