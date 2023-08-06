0703f08f2eca3ec4f6ad41f9ec52413d52f8a11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:50 +0000

    Add package pyucum/0.1.3/pyucum-0.1.3.tar.gz

---
+++ packages/pyucum/pyucum-0.1.3.tar.gz/pyucum-0.1.3/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyucum",
+    version="0.1.3",
+    author="Sam Tomioka",
+    author_email="stomioka@gmail.com",
+    description="Python library for using UCUM APIs to verify CDISC SDTM.LB and ADaM.ADLB",
+    keywords='SDTM,ADaM, LB, ADLB, UCUM',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='LICENSE',
+    include_package_data=True,
+    install_requires=[
+    'numpy>=1.15.0',
+    'pandas>=0.23.0',
+    'tqdm>=4.32.2',
+    'seaborn>=0.9.0',
+    'matplotlib>=3.1.1'],
+    url='https://github.com/stomioka/ucum',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering"
+    ],
+)
