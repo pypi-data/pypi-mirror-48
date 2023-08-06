c45edc7ff32930e13bdddb77483847490deb659a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:47 +0000

    Add package gpam-logging/241455362/gpam-logging-241455362.tar.gz

---
+++ packages/gpam-logging/gpam-logging-241455362.tar.gz/gpam-logging-241455362/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
+
+setup(name='gpam-logging',
+      version=version,
+      description='Logger handler with GPAM integration',
+      url='http://gpam.unb.br',
+      author='GPAM',
+      author_email='gpamunb@gmail.com',
+      license='MIT',
+      install_requires=[
+      ],
+      packages=[
+          'gpam-logging',
+      ],
+      zip_safe=False)
