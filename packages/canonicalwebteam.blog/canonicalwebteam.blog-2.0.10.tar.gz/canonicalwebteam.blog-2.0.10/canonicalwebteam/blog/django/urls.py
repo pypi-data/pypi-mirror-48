d86eee4f748c0012ecc162df1485c2caea863553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package canonicalwebteam.blog/2.0.10/canonicalwebteam.blog-2.0.10.tar.gz

---
+++ packages/canonicalwebteam.blog/canonicalwebteam.blog-2.0.10.tar.gz/canonicalwebteam.blog-2.0.10/canonicalwebteam/blog/django/urls.py
@@ -0,0 +1,62 @@
+from django.urls import path, register_converter
+from canonicalwebteam.blog.django.views import (
+    archives,
+    article_redirect,
+    article,
+    author,
+    feed,
+    index,
+    latest_news,
+    tag,
+    upcoming,
+)
+
+
+class FourDigitYearConverter:
+    regex = "[0-9]{4}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return "%04d" % value
+
+
+class TwoDigitMonthConverter:
+    regex = "[0-9]{2}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return "%02d" % value
+
+
+class TwoDigitDayConverter:
+    regex = "[0-9]{2}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return "%02d" % value
+
+
+register_converter(FourDigitYearConverter, "yyyy")
+register_converter(TwoDigitMonthConverter, "mm")
+register_converter(TwoDigitDayConverter, "dd")
+
+
+urlpatterns = [
+    path(r"/<yyyy:year>/<mm:month>/<dd:day>/<str:slug>", article_redirect),
+    path(r"/<yyyy:year>/<mm:month>/<str:slug>", article_redirect),
+    path(r"/<yyyy:year>/<str:slug>", article_redirect),
+    path(r"/archives", archives),
+    path(r"/author/<str:username>", author),
+    path(r"/tag/<str:slug>", tag),
+    path(r"/feed", feed),
+    path(r"/upcoming", upcoming),
+    path(r"/latest-news", latest_news),
+    path(r"/<str:slug>", article, name="article"),
+    path(r"", index),
+]
