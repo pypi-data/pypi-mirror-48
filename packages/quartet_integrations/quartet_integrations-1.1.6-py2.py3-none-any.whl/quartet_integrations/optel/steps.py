7dc1aa21190c8954c91659ac798400b92cae663a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:32 +0000

    Add package quartet_integrations/1.1.6/quartet_integrations-1.1.6-py2.py3-none-any.whl

---
+++ packages/quartet_integrations/quartet_integrations-1.1.6-py2.py3-none-any.whl/quartet_integrations/optel/steps.py
@@ -0,0 +1,96 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2019 SerialLab Corp.  All rights reserved.
+import io
+
+from quartet_capture import models
+from quartet_integrations.sap.steps import SAPParsingStep
+from quartet_integrations.optel.parsing import OptelEPCISLegacyParser, \
+    ConsolidationParser
+from quartet_integrations.optel.epcpyyes import get_default_environment
+from quartet_output import steps
+from gs123.conversion import URNConverter
+from quartet_output.steps import EPCPyYesOutputStep as OutputStep, ContextKeys
+
+class AddCommissioningDataStep(steps.AddCommissioningDataStep):
+    def process_events(self, events: list):
+        """
+        Changes the default template and environment for the EPCPyYes
+        object events.
+        """
+        env = get_default_environment()
+        for event in events:
+            for epc in event.epc_list:
+                if ':sscc:' in epc:
+                    parsed_sscc = URNConverter(epc)
+                    event.company_prefix = parsed_sscc._company_prefix
+                    event.extension_digit = parsed_sscc._extension_digit
+                    break
+            event.template = env.get_template(
+                'optel/object_event.xml')
+            event._env = env
+
+        return events
+
+
+class OptelLineParsingStep(SAPParsingStep):
+    """
+    A QU4RTET parsing step that can parse SAP XML data that contains
+    custom event data.
+    """
+
+    def __init__(self, db_task: models.Task, **kwargs):
+        super().__init__(db_task, **kwargs)
+        self.replace_timezone = self.get_boolean_parameter('Replace Timezone',
+                                                           False)
+
+    def _parse(self, data):
+        return OptelEPCISLegacyParser(data).parse(
+            replace_timezone=self.replace_timezone
+        )
+
+    @property
+    def declared_parameters(self):
+        params = super().declared_parameters()
+        params['Replace Timezone'] = 'Whether or not to replace explicit ' \
+                                     'timezone declarations in event times ' \
+                                     'with the timezone offset in the event.'
+
+
+class ConsolidationParsingStep(OptelLineParsingStep):
+    """
+    Uses the consolidation parser to handle any bloated optel messages.
+    """
+
+    def _parse(self, data):
+        return ConsolidationParser(data).parse(self.replace_timezone)
+
+
+class EPCPyYesOutputStep(OutputStep):
+    '''
+    Overrides the standard output step in order to supply a different
+    output template for the header of the generated EPCIS document.
+    '''
+
+    def get_epcis_document_class(self, all_events):
+        """
+        Replaces the default document template with the optel one.
+        :param all_events: The events that will be rendered to XML.
+        :return: The document class with a new template specified.
+        """
+        document = super().get_epcis_document_class(all_events)
+        env = get_default_environment()
+        template = env.get_template('optel/epcis_events_document.xml')
+        document.template = template
+        return document
