289d0a416dc36abd9d41680522578e42007f77a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:03 +0000

    Add package kgtools/0.1.4/kgtools-0.1.4.tar.gz

---
+++ packages/kgtools/kgtools-0.1.4.tar.gz/kgtools-0.1.4/kgtools/symbol.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class HTML:
+    CODE = "-CODE-"
+    TAB = "-TAB-"
+    IMG = "-IMG-"
+    URL = "-URL-"
+    PRE = "-PRE-"
+    QUOTE = "-QUOTE-"
