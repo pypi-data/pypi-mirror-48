47741fe59b279da6bc5ea381b50f50fecf370dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:47 +0000

    Add package QOASecurityUtil/1.1.5/QOASecurityUtil-1.1.5-py2.py3-none-any.whl

---
+++ packages/QOASecurityUtil/QOASecurityUtil-1.1.5-py2.py3-none-any.whl/QOASecurityUtil/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+import time
+import binascii
+from pyDes import des, CBC, PAD_PKCS5
+
+
+def des_encrypt(content, password):
+    return binascii.b2a_hex(
+        des(password, CBC, password, pad=None, padmode=PAD_PKCS5)
+        .encrypt(content, padmode=PAD_PKCS5)).upper()
+
+
+def make_token(appcode, password):
+    return 'token__' + des_encrypt('%s__%s' % (appcode, str(
+            int(time.time() * 1000))), password)
