75d010c744d888281345a1b96cf0dc5426f5d164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:46 +0000

    Add package bace/1.0.0/bace-1.0.0.tar.gz

---
+++ packages/bace/bace-1.0.0.tar.gz/bace-1.0.0/tests/test_unb.py
@@ -0,0 +1,15 @@
+import unittest
+from bace import UniversalSetNB
+from tests.data_feed import get_data
+
+class TestCNB(unittest.TestCase):
+
+    def test_cnb(self):
+        cnb = UniversalSetNB()
+        X_train, y_train, X_test, y_test  =  get_data()
+        score = cnb.fit(X_train, y_train).accuracy_score(X_test, y_test)
+        assert score > 0.80
+
+if __name__ == '__main__':
+    unittest.main()
+
