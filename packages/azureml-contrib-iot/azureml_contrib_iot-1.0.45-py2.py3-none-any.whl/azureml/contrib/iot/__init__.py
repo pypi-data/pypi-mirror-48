9e8c6c90003e32ceaae639760dc59129476a7333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:45 +0000

    Add package azureml-contrib-iot/1.0.45/azureml_contrib_iot-1.0.45-py2.py3-none-any.whl

---
+++ packages/azureml-contrib-iot/azureml_contrib_iot-1.0.45-py2.py3-none-any.whl/azureml/contrib/iot/__init__.py
@@ -0,0 +1,8 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Accelerated inferencing on the edge."""
+from .iot_image import IotContainerImage
+
+
+__all__ = ["IotContainerImage"]
