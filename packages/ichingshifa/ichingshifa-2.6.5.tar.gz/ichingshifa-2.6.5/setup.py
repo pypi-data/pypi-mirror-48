05e066ba4ade279cb0e930933f27c433b09d27e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:16 +0000

    Add package ichingshifa/2.6.5/ichingshifa-2.6.5.tar.gz

---
+++ packages/ichingshifa/ichingshifa-2.6.5.tar.gz/ichingshifa-2.6.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+   
+setuptools.setup(
+    name="ichingshifa",
+    version="2.6.5",
+    author="Ken Tang",
+    author_email="kinyeah@gmail.com",
+    description="A package of iching stalk divination (texts written in Traditional Chinese)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kentang2017/iching_shifa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
