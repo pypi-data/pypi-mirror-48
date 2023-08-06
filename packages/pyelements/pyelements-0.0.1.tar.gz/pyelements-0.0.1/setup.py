3240ff6f79714fbecc516b3ef9e2a9d984b8d54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:56 +0000

    Add package pyelements/0.0.1/pyelements-0.0.1.tar.gz

---
+++ packages/pyelements/pyelements-0.0.1.tar.gz/pyelements-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='pyelements',
+    version='0.0.1',
+    description='Tools for running experiments fast.',
+    url='http://github.com/danijar/elements',
+    install_requires=[],
+    packages=['elements'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
