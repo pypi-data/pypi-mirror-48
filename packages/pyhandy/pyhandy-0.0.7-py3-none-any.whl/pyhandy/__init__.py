2d19d3850d74a2f6e5fe5d3276afa9f538743d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:12 +0000

    Add package pyhandy/0.0.7/pyhandy-0.0.7-py3-none-any.whl

---
+++ packages/pyhandy/pyhandy-0.0.7-py3-none-any.whl/pyhandy/__init__.py
@@ -0,0 +1,12 @@
+from . import pyhandy
+
+name = "pyhandy"
+
+__author__ = "callmexss"
+__email__ = "callmexss@126.com"
+__copyright__ = "(c) 2019 callmexss"
+
+__version__ = "0.0.7"
+__license__ = "MIT"
+__title__ = 'pyhandy'
+__description__ = "A collection of python tools to make my life easier."
