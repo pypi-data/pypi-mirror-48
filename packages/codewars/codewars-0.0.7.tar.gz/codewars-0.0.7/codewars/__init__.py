966e344c6e457e036ec28576514d771255b08d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package codewars/0.0.7/codewars-0.0.7.tar.gz

---
+++ packages/codewars/codewars-0.0.7.tar.gz/codewars-0.0.7/codewars/__init__.py
@@ -0,0 +1,23 @@
+name = "codewars"
+__created_by__ = "codewars.nl"
+
+# try:
+#     from pip._internal import main
+# except ImportError:
+#     from pip import main
+
+# def import_or_install(package):
+#     try:
+#         __import__(package)
+#     except ImportError:
+#         main(['install', package])
+        
+# packages = [
+#     'aiohttp',
+#     'beautifulsoup4',
+#     'ipython',
+#     'lxml',
+#     'PyBluez',
+#     'scapy'
+# ]
+# for item in packages: import_or_install(item)
