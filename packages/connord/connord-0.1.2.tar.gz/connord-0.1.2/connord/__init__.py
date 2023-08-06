4a7040c14f14de4b50101bd060038238485a7330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:16 +0000

    Add package connord/0.1.2/connord-0.1.2.tar.gz

---
+++ packages/connord/connord-0.1.2.tar.gz/connord-0.1.2/connord/__init__.py
@@ -0,0 +1,33 @@
+# vim: set fileencoding=utf-8 :
+
+# connord - connect to nordvpn servers
+# Copyright (C) 2019  Mael Stor <maelstor@posteo.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Init file
+"""
+
+
+class ConnordError(Exception):
+    """Main Exception class for connord module"""
+
+
+# TODO: Introduce ValidationError to be used verifying command-line arguments
+# in filters
+
+
+__version__ = "0.1.2"
+__license__ = "GNU General Public License v3 or later (GPLv3+)"
