5879671255b712a9814ee9f1f78f9c08057d46d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package threadlocal-aws/0.6/threadlocal-aws-0.6.tar.gz

---
+++ packages/threadlocal-aws/threadlocal-aws-0.6.tar.gz/threadlocal-aws-0.6/threadlocal_aws/resources.py
@@ -0,0 +1,9 @@
+import sys
+from threadlocal_aws import _get_local_resource, session, PY37
+from threadlocal_aws.pep562 import Pep562
+
+def __getattr__(name):
+    return lambda **kwargs: _get_local_resource(name, **kwargs)
+
+if not PY37:
+    Pep562(__name__)
