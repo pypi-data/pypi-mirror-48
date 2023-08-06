0c222c2bb5eb4c210d5bd6141849ff945c99e894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package jupyterhub-kerberosauthenticator/0.2.0/jupyterhub_kerberosauthenticator-0.2.0-py2.py3-none-any.whl

---
+++ packages/jupyterhub-kerberosauthenticator/jupyterhub_kerberosauthenticator-0.2.0-py2.py3-none-any.whl/kerberosauthenticator/auth.py
@@ -0,0 +1,147 @@
+import os
+
+import kerberos
+from jinja2 import ChoiceLoader, FileSystemLoader
+from jupyterhub.auth import Authenticator, LocalAuthenticator
+from jupyterhub.handlers import BaseHandler
+from jupyterhub.utils import url_path_join
+from tornado import web
+from traitlets import Unicode
+
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
+
+
+class KerberosLoginHandler(BaseHandler):
+    _loaded = False
+
+    def __init__(self, *args, **kwargs):
+        if KerberosLoginHandler._loaded:
+            return
+        super().__init__(*args, **kwargs)
+
+        self.log.debug('Adding %s to template path', TEMPLATE_DIR)
+        loader = FileSystemLoader([TEMPLATE_DIR])
+        env = self.settings['jinja2_env']
+        previous_loader = env.loader
+        env.loader = ChoiceLoader([previous_loader, loader])
+        self._loaded = True
+
+    def raise_auth_required(self):
+        self.set_status(401)
+        data = self.render_template(
+            'kerberos_login_error.html',
+            login_url=self.settings['login_url']
+        )
+        self.write(data)
+        self.set_header("WWW-Authenticate", "Negotiate")
+        raise web.Finish()
+
+    async def get(self):
+        auth_header = self.request.headers.get('Authorization')
+        if not auth_header:
+            self.raise_auth_required()
+
+        auth_type, auth_key = auth_header.split(" ", 1)
+        if auth_type != 'Negotiate':
+            self.raise_auth_required()
+
+        # Headers are of the proper form, initialize login routine
+        user = await self.login_user()
+        if user is None:
+            raise web.HTTPError(403)
+        else:
+            # Logged in, redirect to next url
+            self.redirect(self.get_next_url(user))
+
+
+class KerberosAuthenticator(Authenticator):
+    """
+    Kerberos Authenticator for JupyterHub
+    """
+
+    service_name = Unicode(
+        "HTTP",
+        help="""The service's kerberos principal name.
+
+        This is almost always "HTTP" (the default)""",
+        config=True
+    )
+
+    keytab = Unicode(
+        "HTTP.keytab",
+        help="The path to the keytab file",
+        config=True
+    )
+
+    login_service = "kerberos"
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        os.environ['KRB5_KTNAME'] = self.keytab
+
+    def get_handlers(self, app):
+        return [('/kerberos_login', KerberosLoginHandler)]
+
+    def login_url(self, base_url):
+        return url_path_join(base_url, 'kerberos_login')
+
+    async def authenticate(self, handler, data):
+        auth_header = handler.request.headers.get('Authorization')
+        if not auth_header:  # pragma: nocover
+            self.log.error("authenticate hit with no kerberos credentials, "
+                           "this code path should never occur")
+            return None
+
+        auth_type, auth_key = auth_header.split(" ", 1)
+        if auth_type != 'Negotiate':  # pragma: nocover
+            self.log.error("authenticate hit with no kerberos credentials, "
+                           "this code path should never occur")
+            return None
+
+        gss_context = None
+        try:
+            # Initialize kerberos context
+            rc, gss_context = kerberos.authGSSServerInit(self.service_name)
+
+            # NOTE: Per the pykerberos documentation, the return code should be
+            # checked after each step. However, after reading the pykerberos
+            # code no method used here will ever return anything but
+            # AUTH_GSS_COMPLETE (all other cases will raise an exception).  We
+            # keep these checks in just in case pykerberos changes its behavior
+            # to match its docs, but they likely never will trigger.
+            if rc != kerberos.AUTH_GSS_COMPLETE:  # pragma: nocover
+                self.log.error("GSS server init failed, return code = %r", rc)
+                return None
+
+            # Challenge step
+            rc = kerberos.authGSSServerStep(gss_context, auth_key)
+            if rc != kerberos.AUTH_GSS_COMPLETE:  # pragma: nocover
+                # Only warn here, since this happens for any user failing login
+                self.log.warn("GSS server step failed, return code = %r", rc)
+                return None
+            gss_key = kerberos.authGSSServerResponse(gss_context)
+
+            # Retrieve user name
+            fulluser = kerberos.authGSSServerUserName(gss_context)
+            user = fulluser.split("@", 1)[0]
+
+            # Complete the protocol by responding with the Negotiate header
+            handler.set_header('WWW-Authenticate', "Negotiate %s" % gss_key)
+            return user
+        except kerberos.GSSError as err:  # pragma: nocover
+            self.log.error("Error occurred during kerberos authentication",
+                           exc_info=err)
+            return None
+        finally:
+            if gss_context is not None:
+                kerberos.authGSSServerClean(gss_context)
+
+
+class KerberosLocalAuthenticator(LocalAuthenticator, KerberosAuthenticator):
+    """
+    Kerberos local authenticator for JupyterHub
+
+    Checks for local users, and can attempt to create them if they exist.
+    """
+    pass
---
+++ packages/jupyterhub-kerberosauthenticator/jupyterhub_kerberosauthenticator-0.2.0-py2.py3-none-any.whl/kerberosauthenticator/tests/test_authenticator.py
@@ -0,0 +1,49 @@
+from urllib.parse import urlparse
+
+import pytest
+from jupyterhub.tests.mocking import public_url
+from jupyterhub.tests.test_api import add_user
+from jupyterhub.tests.utils import async_requests
+from requests_kerberos import HTTPKerberosAuth
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('auto_login', [True, False])
+async def test_integration(app, auto_login, logged_in):
+    app.authenticator.auto_login = auto_login
+
+    # Create a user
+    add_user(app.db, app, name="alice")
+
+    if auto_login:
+        url = public_url(app, path="/hub/login")
+        resp = await async_requests.get(url)
+        # Sends back 401 requesting authentication
+        assert resp.status_code == 401
+        # 401 page is formatted nicely
+        assert "Failed to login with Kerberos." in resp.text
+        assert resp.text.count("/hub/login") >= 2
+        # Before that was a redirect to the auth handler
+        assert resp.history[0].status_code == 302
+        # Now use the redirected url with auth enabled
+        location = resp.history[0].headers['location']
+        netloc = urlparse(app.bind_url).netloc
+        url = 'http://%s%s' % (netloc, location)
+    else:
+        url = public_url(app, path="/hub/kerberos_login")
+
+    # Go through the login procedure
+    resp = await async_requests.get(
+        url,
+        auth=HTTPKerberosAuth(hostname_override="address.example.com")
+    )
+
+    if logged_in:
+        # Successful
+        resp.raise_for_status()
+
+        # At user notebook, login successful
+        assert resp.url.startswith(public_url(app, path="/user/alice"))
+    else:
+        # Unsuccessful
+        assert resp.status_code == 401
