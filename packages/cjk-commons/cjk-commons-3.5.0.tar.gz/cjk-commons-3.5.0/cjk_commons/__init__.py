d5f4b496ab776ae2159461f2a3177512357d4f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package cjk-commons/3.5.0/cjk-commons-3.5.0.tar.gz

---
+++ packages/cjk-commons/cjk-commons-3.5.0.tar.gz/cjk-commons-3.5.0/cjk_commons/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# noinspection PyUnresolvedReferences
+from pathlib import Path
+import re
+
+here = Path(__file__).parent.parent
+
+__version__ = '0.0.0'
+with Path(here, 'pyproject.toml').open() as f:
+    version_match = re.search(r'version = "(?P<version>\d+\.\d+\.\d+)"', f.read())
+    if version_match:
+        __version__ = version_match.group('version')
