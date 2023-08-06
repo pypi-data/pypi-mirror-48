c4317998684388aec4a6466211e5a08bf801b249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:21 +0000

    Add package nvchecker/1.4.4/nvchecker-1.4.4.tar.gz

---
+++ packages/nvchecker/nvchecker-1.4.4.tar.gz/nvchecker-1.4.4/tests/test_archpkg.py
@@ -0,0 +1,14 @@
+# MIT licensed
+# Copyright (c) 2013-2017 lilydjwg <lilydjwg@gmail.com>, et al.
+
+from flaky import flaky
+import pytest
+pytestmark = pytest.mark.asyncio
+
+@flaky
+async def test_archpkg(get_version):
+    assert await get_version("ipw2100-fw", {"archpkg": None}) == "1.3-9"
+
+@flaky
+async def test_archpkg_strip_release(get_version):
+    assert await get_version("ipw2100-fw", {"archpkg": None, "strip-release": 1}) == "1.3"
---
+++ packages/nvchecker/nvchecker-1.4.4.tar.gz/nvchecker-1.4.4/tests/test_regex.py
@@ -0,0 +1,31 @@
+# MIT licensed
+# Copyright (c) 2013-2017 lilydjwg <lilydjwg@gmail.com>, et al.
+
+import pytest
+pytestmark = pytest.mark.asyncio
+
+async def test_regex_httpbin_default_user_agent(get_version):
+    assert await get_version("example", {
+        "url": "https://httpbin.org/get",
+        "regex": r'"User-Agent": "([^"]+)"',
+    }) == "lilydjwg/nvchecker"
+
+async def test_regex_httpbin_user_agent(get_version):
+    assert await get_version("example", {
+        "url": "https://httpbin.org/get",
+        "regex": r'"User-Agent": "(\w+)"',
+        "user_agent": "Meow",
+    }) == "Meow"
+
+async def test_regex(get_version):
+    assert await get_version("example", {
+        "url": "http://example.net/",
+        "regex": r'for (\w+) examples',
+    }) == "illustrative"
+
+async def test_missing_ok(get_version):
+    assert await get_version("example", {
+        "url": "http://example.net/",
+        "regex": "foobar",
+        "missing_ok": True,
+    }) is None
