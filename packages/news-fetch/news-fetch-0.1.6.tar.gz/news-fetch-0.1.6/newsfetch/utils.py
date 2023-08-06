854996f10631c7ca42919adb8fc63fd073443f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package news-fetch/0.1.6/news-fetch-0.1.6.tar.gz

---
+++ packages/news-fetch/news-fetch-0.1.6.tar.gz/news-fetch-0.1.6/newsfetch/utils.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+from newsplease import NewsPlease
+from selenium import webdriver
+from newspaper import Article
+from bs4 import BeautifulSoup
+import chromedriver_binary
+from requests import get
+import requests
+import time
+import json
+import sys
+import re
