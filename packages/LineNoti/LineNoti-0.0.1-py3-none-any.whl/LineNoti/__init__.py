a6d33d7085cb17499b41bc4eca3dc5b0dda3a943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:21 +0000

    Add package LineNoti/0.0.1/LineNoti-0.0.1-py3-none-any.whl

---
+++ packages/LineNoti/LineNoti-0.0.1-py3-none-any.whl/LineNoti/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Donggeun Kwon (donggeun.kwon@gmail.com)
+
+Cryptographic Algorithm Lab.
+Graduate School of Information Security, Korea University
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# from .custom_setup import __setup__
+# __setup__()
+
+from .main import Notify
+
+__version__ = '0.0.1'
+__doc__ = 'https://notify-bot.line.me/doc/en/'
+__name__ = 'LineNoti'
+
+__all__ = ['__version__', 
+		   '__doc__', 
+		   '__name__',
+		   'Notify']
