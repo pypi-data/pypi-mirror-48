53eecf333f6424d3000ee5b16a82249e2f02d47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:50 +0000

    Add package the-collector/0.8.2/the_collector-0.8.2.tar.gz

---
+++ packages/the-collector/the_collector-0.8.2.tar.gz/the_collector-0.8.2/the_collector/__init__.py
@@ -0,0 +1,10 @@
+from the_collector.circular_buffer import CircularBuffer
+from the_collector.utils import array_unpack, array_pack
+from the_collector.utils import file_size, rm
+from the_collector.bagit import BagIt
+from the_collector.protocols import Json, MsgPack, Pickle
+from the_collector.data import Data
+
+__license__ = "MIT"
+__author__ = "Kevin J. Walchko"
+__version__ = '0.8.2'
