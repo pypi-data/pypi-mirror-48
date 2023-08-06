4cf2bb2a31fd5168bb3fe5de1aaa01a50218f426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:20 +0000

    Add package tbzuploader/2019.3.0/tbzuploader-2019.3.0.tar.gz

---
+++ packages/tbzuploader/tbzuploader-2019.3.0.tar.gz/tbzuploader-2019.3.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name='tbzuploader',
+    version='2019.3.0',
+    license='Apache Software License 2.0',
+    long_description=open('README.rst').read(),
+    url='https://github.com/guettli/tbzuploader',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'future',
+    ],
+
+    include_package_data=True,
+
+    author='Thomas Guettler',
+    author_email='info.reprec@thomas-guettler.de',
+
+    entry_points={
+        'console_scripts': [
+            'tbzuploader=tbzuploader.console_scripts:main',
+        ],
+    }
+)
