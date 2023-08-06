9d5a7bf91e72e73e64dbbee83391228ccd803171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:43 +0000

    Add package PyCMeKG/0.2.0/PyCMeKG-0.2.0.tar.gz

---
+++ packages/PyCMeKG/PyCMeKG-0.2.0.tar.gz/PyCMeKG-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyCMeKG",
+    version="0.2.0",
+    author="Yunfei Yang",
+    author_email="yfy@pku.edu.cn",
+    description="Chinese Medical Knowledge Graph",
+    long_description="Chinese Medical Knowledge Graph",
+    license="MIT",
+    url="https://github.com/king-yyf/PyCMeKG",
+    packages=find_packages(),
+
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
