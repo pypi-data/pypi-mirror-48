d932f81d683058062535998779e5891f84ef71fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:01 +0000

    Add package django-useraudit/1.7.1/django-useraudit-1.7.1.tar.gz

---
+++ packages/django-useraudit/django-useraudit-1.7.1.tar.gz/django-useraudit-1.7.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-useraudit',
+    version='1.7.1',
+    description='Django user audit utilities like logging user log in, disabling access when password expires or user is inactive',
+    long_description='Django user audit utilities like logging user log in, disabling access when password expires or user is inactive',
+    author='CCG, Murdoch University',
+    author_email='devops@ccg.murdoch.edu.au',
+    url='https://github.com/muccg/django-useraudit',
+    download_url='https://github.com/muccg/django-useraudit/releases',
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+    zip_safe=True,
+    packages=[
+        'useraudit',
+        'useraudit.migrations',
+    ],
+    include_package_date=True,
+)
