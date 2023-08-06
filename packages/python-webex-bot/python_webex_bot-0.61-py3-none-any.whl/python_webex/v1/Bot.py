9fc7e135b51e046febcbb9f3f0a5085e50beb652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:34 +0000

    Add package python-webex-bot/0.61/python_webex_bot-0.61-py3-none-any.whl

---
+++ packages/python-webex-bot/python_webex_bot-0.61-py3-none-any.whl/python_webex/v1/Bot.py
@@ -0,0 +1,41 @@
+from python_webex.v1.People import People
+from python_webex.v1.Room import Room
+from python_webex.v1.Webhook import Webhook
+from python_webex.v1.Message import Message
+
+import os
+import sys
+
+
+class Bot(People, Room, Webhook, Message):
+
+    def __init__(self):
+
+        # declare headers and how the token will be gotten from the system
+        self.URL = "https://api.ciscospark.com/"
+        self.auth_token = os.getenv("auth_token")
+
+        if self.auth_token == None:
+            sys.exit("'auth_token' not set in the environment variables")
+        
+        self.headers = {
+            "Authorization": "Bearer " + self.auth_token,
+            "Content-Type": "application/json"
+        }
+        
+        # self.hears to function maps when a specific word is heard to a function
+        # for example, when one says 'hi' and you want to map it to say_hi() function
+        self.hears_to_function = {
+
+        }
+        self.attach_function = None
+
+    """
+    decorator meant to do a specific action when called
+    """
+    def on_hears(self, message_text):
+        def hear_decorator(f):
+            self.hears_to_function[message_text] = f
+
+        return hear_decorator
+
