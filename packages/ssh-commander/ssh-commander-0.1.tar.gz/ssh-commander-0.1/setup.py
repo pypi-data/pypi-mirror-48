f6352d92fcf786a0007fa8ae84351426a429d52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:48 +0000

    Add package ssh-commander/0.1/ssh-commander-0.1.tar.gz

---
+++ packages/ssh-commander/ssh-commander-0.1.tar.gz/ssh-commander-0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ssh-commander',
+    version='0.1',
+    description='Excecute remote commands on several hosts, with SSH',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/tuxedoar/ssh-commander',
+    author='tuxedoar',
+    author_email='tuxedoar@gmail.com',
+    packages=['ssh_commander'],
+    python_requires='>=3.4',
+    scripts=["ssh_commander/_version.py"],
+    entry_points={
+        "console_scripts": [
+        "ssh-commander = ssh_commander.ssh_commander:main",
+        ],
+    },
+    install_requires=[
+    'paramiko'
+    ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: System Administrators",
+        "Environment :: Console",
+        ],
+)
