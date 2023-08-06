114b203c8f33c85163c7ad0af28bc706c8fa9076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package pm4py/1.1.19/pm4py-1.1.19-py3-none-any.whl

---
+++ packages/pm4py/pm4py-1.1.19-py3-none-any.whl/pm4py/objects/log/exporter/parquet/versions/pyarrow.py
@@ -0,0 +1,26 @@
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+
+def apply(df, path, parameters=None):
+    """
+    Exports a dataframe to a Parquet file
+
+    Parameters
+    ------------
+    df
+        Dataframe
+    path
+        Path
+    parameters
+        Possible parameters of the algorithm
+    """
+    if parameters is None:
+        parameters = {}
+
+    compression = parameters["compression"] if "compression" in parameters else "snappy"
+
+    df.columns = [x.replace(":", "AAA") for x in df.columns]
+    df = pa.Table.from_pandas(df)
+    pq.write_table(df, path, compression=compression)
+
