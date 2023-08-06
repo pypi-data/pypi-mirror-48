19d0c646b4f72ed42eabb53cd79589b894b57f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:13 +0000

    Add package SeqWORDS/0.0.2/SeqWORDS-0.0.2.tar.gz

---
+++ packages/SeqWORDS/SeqWORDS-0.0.2.tar.gz/SeqWORDS-0.0.2/setup.py
@@ -0,0 +1,18 @@
+
+
+from setuptools import setup, find_packages
+
+setup(name='SeqWORDS',
+      version='0.0.2',
+      keywords = ("EM algorithm", "unsupervised method", "WDM", "Chinese word segmentation"),
+      description='An unsupervised Chinese word segmentation',
+      author='Harry Wu',
+      author_email='tf00604520@gmail.com',
+      license='MIT Licence',
+      url='https://github.com/kkuanhui/SeqWORDS',
+      packages= find_packages(),
+      include_package_data = True,
+      install_requires=[
+          'numpy',
+      ],
+      )
