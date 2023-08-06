449ccf90140ef38c86ce26d8eafda7b18e80802a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:19 +0000

    Add package wasdi/0.1.8/wasdi-0.1.8.tar.gz

---
+++ packages/wasdi/wasdi-0.1.8.tar.gz/wasdi-0.1.8/setup.py
@@ -0,0 +1,29 @@
+"""
+Waspy - WASDI Python Library
+
+Created on 11 Jun 2018
+
+@author: p.campanella - FadeOut Software
+"""
+import setuptools
+
+with open("README.md", "r") as oFh:
+    sLong_description = oFh.read()
+
+
+setuptools.setup(
+    name="wasdi",
+    version="0.1.8",
+    author="FadeOut Software",
+    author_email="info@fadeout.biz",
+    description="The Wasdi Python library",
+    long_description=sLong_description,
+    long_description_content_type="text/markdown",
+    url="http://www.wasdi.net",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
