03bce878e9b4dcf2c6712f539be3e51e2fd4c049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:16 +0000

    Add package aioscrapy/0.1.4/aioscrapy-0.1.4-py3-none-any.whl

---
+++ packages/aioscrapy/aioscrapy-0.1.4-py3-none-any.whl/aioscrapy/__init__.py
@@ -0,0 +1,33 @@
+from .client import (
+    WebTextClient,
+    WebByteClient,
+    Client,
+    CacheClient,
+    RetryClient
+)
+
+from .cache import (
+    Cache,
+    FileCache
+)
+
+from .html import (
+    select_one,
+    select_text_one,
+    select_all
+)
+
+from .session import (
+    ProxyPool,
+    SessionPool,
+    SingleSessionPool,
+    ProxySessionPool
+)
+
+from .worker import (
+    Dispatcher,
+    Worker,
+    Master,
+    CrawlerWorker,
+    SimpleWorker
+)
