1a13fb9802973c6f2f52148a77e31df2c1da79f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:53 +0000

    Add package jslog4kube/1.0.4/jslog4kube-1.0.4.tar.gz

---
+++ packages/jslog4kube/jslog4kube-1.0.4.tar.gz/jslog4kube-1.0.4/jslog4kube/__init__.py
@@ -0,0 +1,19 @@
+# pylint: disable=invalid-name, missing-docstring, too-few-public-methods
+
+import time
+from pythonjsonlogger.jsonlogger import JsonFormatter
+from .bootstrap import LOG_ADDS, format_str
+from .kube.log_config import LOGGING
+
+JsonFormatter.converter = time.gmtime
+
+
+try:
+    from .gunicorn.dictconfig_logger import GunicornLogger
+
+    HAS_GUNICORN = True
+except ImportError:
+    HAS_GUNICORN = False
+
+
+__all__ = ["LOG_ADDS", "format_str", "LOGGING"]
