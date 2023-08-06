63ed700fb2af7efaca5f001da3181edd193ee2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:07 +0000

    Add package openimis-be-core/0.0.9/openimis_be_core-0.0.9-py3-none-any.whl

---
+++ packages/openimis-be-core/openimis_be_core-0.0.9-py3-none-any.whl/core/utils.py
@@ -0,0 +1,18 @@
+def full_class_name(o):
+    module = o.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return o.__class__.__name__
+    return module + '.' + o.__class__.__name__
+
+def comparable(cls):
+    """ Class decorator providing generic comparison functionality """
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    cls.__eq__ = __eq__
+    cls.__ne__ = __ne__
+    return cls
