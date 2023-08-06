fdf1e1d2a229fd86a700e53787e7ab943ac1fb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package eljur-distributions/0.5/eljur_distributions-0.5.tar.gz

---
+++ packages/eljur-distributions/eljur_distributions-0.5.tar.gz/eljur_distributions-0.5/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eljur_distributions',
+      version='0.5',
+      description='Gaussian and Binomial distributions',
+      packages=['eljur_distributions'],
+      zip_safe=False)
