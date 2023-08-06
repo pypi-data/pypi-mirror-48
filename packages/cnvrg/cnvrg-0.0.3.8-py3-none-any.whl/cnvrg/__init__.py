bbf23e528101125f8edb60f07b37eb04e2d61445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:54 +0000

    Add package cnvrg/0.0.3.8/cnvrg-0.0.3.8-py3-none-any.whl

---
+++ packages/cnvrg/cnvrg-0.0.3.8-py3-none-any.whl/cnvrg/__init__.py
@@ -0,0 +1,7 @@
+from cnvrg.modules.library import Library
+from cnvrg.modules.experiment import Experiment
+from cnvrg.version import VERSION
+import cnvrg.subcommands as subcommands
+
+
+__version__ = VERSION
