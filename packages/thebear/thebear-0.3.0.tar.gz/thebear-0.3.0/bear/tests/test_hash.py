910bd5baa992a74256b706ed7a7819fd4b18fe3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package thebear/0.3.0/thebear-0.3.0.tar.gz

---
+++ packages/thebear/thebear-0.3.0.tar.gz/thebear-0.3.0/bear/tests/test_hash.py
@@ -0,0 +1,39 @@
+"""
+Test hashing of specific file contents.
+"""
+
+from unittest import TestCase, main
+from tempfile import mkstemp
+from os import remove
+from bear.hashing import hash_text, hash_file
+
+
+class HashCase(TestCase):
+    """
+    Test hashing functions.
+    """
+
+    def test_hash_word(self):
+        """
+        Test hashing a string.
+        """
+        self.assertEqual(
+            '098f6bcd4621d373cade4e832627b4f6',
+            hash_text('test'.encode('utf-8'))
+        )
+
+    def test_hash_file(self):
+        """
+        Test hashing bytes of a file.
+        """
+        base = b'\xecO\xda\xad\x12\x85\xa1\xe1\xbd\xb7\xf1'
+        (desc, path) = mkstemp(text=False)
+        with open(desc, 'wb') as file:
+            file.write(base)
+        hashed = hash_file(path)
+        remove(path)
+        self.assertEqual('7972892f41d1b98a71d9583b83267d8b', hashed)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/thebear/thebear-0.3.0.tar.gz/thebear-0.3.0/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+"""
+Basic setup.py
+"""
+
+from os.path import abspath, dirname, join, relpath
+from setuptools import setup, find_packages
+from bear import VERSION, NAME
+
+
+ROOT = abspath(dirname(__file__))
+REPO = 'https://github.com/KeyWeeUsr/Bear'
+PKG = join(ROOT, NAME)
+
+
+with open(join(ROOT, "README.md")) as fd:
+    README = fd.read()
+
+
+DATA = [
+    relpath('README.md', ROOT)
+]
+
+
+setup(
+    name='thebear',
+    version=VERSION,
+    description='Bear - the decluttering deduplicator',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    author='Peter Badida',
+    author_email='keyweeusr@gmail.com',
+    url='https://github.com/KeyWeeUsr/Bear',
+    download_url=f'https://github.com/KeyWeeUsr/Bear/tarball/{VERSION}',
+    entry_points={
+        'console_scripts': [f'{NAME} = {NAME}.__main__:run']
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: '
+        'GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Typing :: Typed'
+    ],
+    install_requires=['ensure'],
+    extras_require={
+        'dev': ['pycodestyle', 'pylint', 'coverage'],
+        'ci': ['coveralls'],
+        'release': [
+            'setuptools', 'wheel',
+            'pycodestyle', 'pylint',
+            'coverage', 'coveralls',
+            'twine'
+        ]
+    },
+    include_package_data=True,
+    data_files=[(NAME, DATA)]
+)
