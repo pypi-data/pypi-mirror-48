eaf9dc7980c86434fbc18a82aac67a5fdbfefbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:24 +0000

    Add package aws-cdk.aws-dlm/0.37.0/aws-cdk.aws-dlm-0.37.0.tar.gz

---
+++ packages/aws-cdk.aws-dlm/aws-cdk.aws-dlm-0.37.0.tar.gz/aws-cdk.aws-dlm-0.37.0/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-dlm",
+    "version": "0.37.0",
+    "description": "The CDK Construct Library for AWS::DLM",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_dlm",
+        "aws_cdk.aws_dlm._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_dlm._jsii": [
+            "aws-dlm@0.37.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_dlm": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.13.3",
+        "publication>=0.0.3",
+        "aws-cdk.core~=0.37.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
