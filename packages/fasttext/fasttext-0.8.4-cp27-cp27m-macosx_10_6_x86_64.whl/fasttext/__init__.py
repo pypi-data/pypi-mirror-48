14c1b28644d75ad4514c3b85226d613bbb6bddb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:23 +0000

    Add package fasttext/0.8.4/fasttext-0.8.4-cp27-cp27m-macosx_10_6_x86_64.whl

---
+++ packages/fasttext/fasttext-0.8.4-cp27-cp27m-macosx_10_6_x86_64.whl/fasttext/__init__.py
@@ -0,0 +1,9 @@
+from .fasttext import skipgram
+from .fasttext import cbow
+from .fasttext import load_model
+from .fasttext import supervised
+from .fasttext import print_version
+
+import os
+
+__VERSION__ = '0.8.3'
