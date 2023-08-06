4741318cad6557bc993ae3fcadc5120aeeca84e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:32 +0000

    Add package aws-cdk.aws-transfer/0.37.0/aws-cdk.aws-transfer-0.37.0.tar.gz

---
+++ packages/aws-cdk.aws-transfer/aws-cdk.aws-transfer-0.37.0.tar.gz/aws-cdk.aws-transfer-0.37.0/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-transfer",
+    "version": "0.37.0",
+    "description": "The CDK Construct Library for AWS::Transfer",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_transfer",
+        "aws_cdk.aws_transfer._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_transfer._jsii": [
+            "aws-transfer@0.37.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_transfer": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.13.3",
+        "publication>=0.0.3",
+        "aws-cdk.core~=0.37.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)
