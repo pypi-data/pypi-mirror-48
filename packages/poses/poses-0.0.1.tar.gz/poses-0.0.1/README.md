6fdc279ec081750a3b8c570906e7567c4280ddfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:30 +0000

    Add package poses/0.0.1/poses-0.0.1.tar.gz

---
+++ packages/poses/poses-0.0.1.tar.gz/poses-0.0.1/README.md
@@ -0,0 +1,3 @@
+# PoSeS 
+
+PoSeS package.
---
+++ packages/poses/poses-0.0.1.tar.gz/poses-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='poses',
+    version='0.0.1',
+    author='Marcos Duarte',
+    author_email='duartexyz@gmail.com',
+    description='poses package',
+    long_description='',
+    url='https://github.com/demotu',
+    packages=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",'Development Status :: 1 - Planning'
+    ],
+)
