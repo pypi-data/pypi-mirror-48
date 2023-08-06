7640342b6cae8e2c19330977a6fdec8b238bfd11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:52 +0000

    Add package data-pre/0.1.6/data-pre-0.1.6.tar.gz

---
+++ packages/data-pre/data-pre-0.1.6.tar.gz/data-pre-0.1.6/setup.py
@@ -0,0 +1,27 @@
+"""
+@author: magician
+@file: setup.py
+@date: 2019/06/25
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-pre",
+    version="0.1.6",
+    author="magician",
+    author_email="ftconan@163.com",
+    description="pandas data preprocessing tool",
+    long_description=long_description,
+    long_description_content_type="markdown",
+    url="https://github.com/ftconan/data_pre.git",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'pandas',
+        'numpy'
+    ]
+)
