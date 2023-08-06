ea08397fdb6154a18865bc3a7f884c295f4af8bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:49 +0000

    Add package TicketReminderPlugin/0.2.1/TicketReminderPlugin-0.2.1.tar.gz

---
+++ packages/TicketReminderPlugin/TicketReminderPlugin-0.2.1.tar.gz/TicketReminderPlugin-0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+VERSION = '0.2.1'
+PACKAGE = 'ticketreminder'
+
+setup(
+    name = 'TicketReminderPlugin',
+    version = VERSION,
+    description = "Allows to configure reminders for tickets in Trac.",
+    author = 'Mitar',
+    author_email = 'mitar.trac@tnode.com',
+    url = 'https://github.com/trac-hacks/trac-ticketreminder',
+    keywords = 'trac plugin',
+    license = "AGPLv3",
+    packages = [PACKAGE],
+    include_package_data = True,
+    package_data = {
+        PACKAGE: [
+            'htdocs/css/*.css',
+            'htdocs/js/*.js',
+            'templates/*.html',
+        ],
+    },
+    install_requires = [],
+    zip_safe = False,
+    entry_points = {
+        'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
+    },
+)
