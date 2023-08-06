b1b1d74bfc556bc04fafd366f9fd02d3dd1a235c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:28 +0000

    Add package django-tst-cms/1.4.0/django_tst_cms-1.4.0.tar.gz

---
+++ packages/django-tst-cms/django_tst_cms-1.4.0.tar.gz/django_tst_cms-1.4.0/django_tst_cms/admin.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django import forms
+
+
+#from searchableselect.widgets import SearchableSelect
+
+#from django.template.response import TemplateResponse
+#from django.http import HttpResponse
+#try:
+#    from django.utils.encoding import force_text
+#except ImportError:
+#    from django.utils.encoding import force_unicode as force_text
+#from import_export.forms import ConfirmImportForm
