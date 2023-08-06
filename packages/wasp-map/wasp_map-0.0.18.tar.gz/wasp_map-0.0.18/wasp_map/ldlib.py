304444261835df64d1d650861129da54b469fb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:39 +0000

    Add package wasp-map/0.0.18/wasp_map-0.0.18.tar.gz

---
+++ packages/wasp-map/wasp_map-0.0.18.tar.gz/wasp_map-0.0.18/wasp_map/ldlib.py
@@ -0,0 +1,28 @@
+#===============================================================================
+# ldlib.py
+#===============================================================================
+
+# Imports ======================================================================
+
+import os
+import os.path
+
+from subprocess import run
+
+from wasp_map.env import ANACONDA_DIR
+
+
+
+
+# Functions ====================================================================
+
+def main():
+    command = (
+        'export '
+        'LD_LIBRARY_PATH='
+        f"{os.path.join(ANACONDA_DIR, 'lib')}:$LD_LIBRARY_PATH"
+    )
+    print(
+        'Run the following `export` command, or append it to your .bashrc, '
+        f'.profile, or .bash_profile:\n{command}'
+    )
