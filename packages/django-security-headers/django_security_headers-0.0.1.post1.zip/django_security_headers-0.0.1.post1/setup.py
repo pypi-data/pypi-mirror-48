d71d391d316491a6e4bcfeba7f6a2dfd02540929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:40 +0000

    Add package django-security-headers/0.0.1.post1/django_security_headers-0.0.1.post1.zip

---
+++ packages/django-security-headers/django_security_headers-0.0.1.post1.zip/django_security_headers-0.0.1.post1/setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django_security_headers",
+    version="0.0.1.post1",
+    packages=find_packages(),
+    include_package_data=True,
+    license="BSD License",
+    description="A simple app to add configurable security headers to Django "
+    "responses.",
+    long_description=README,
+    url="https://bitbucket.org/scivero/django-security-headers/src/master/",
+    author="Scivero",
+    author_email="",
+    install_requires=["django>=1.11,<2", "django-csp"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
