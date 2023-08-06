357ee4951e7b70e3fd68c43a3c9a5fed158ed023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:42 +0000

    Add package tmnd/0.0.3/tmnd-0.0.3-py3-none-any.whl

---
+++ packages/tmnd/tmnd-0.0.3-py3-none-any.whl/tmnd/__init__.py
@@ -0,0 +1,9 @@
+import logging
+
+__version__ = '0.0.3'
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
+logger = logging.getLogger('tmnd')
+logger.addHandler(handler)
+logger.setLevel('CRITICAL')
