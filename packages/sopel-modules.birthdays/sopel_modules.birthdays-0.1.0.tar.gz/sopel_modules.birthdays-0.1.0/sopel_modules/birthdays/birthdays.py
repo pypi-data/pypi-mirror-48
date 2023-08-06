f6d3957b838d6293b28b07eaa1474fe7b77936a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:24 +0000

    Add package sopel-modules.birthdays/0.1.0/sopel_modules.birthdays-0.1.0.tar.gz

---
+++ packages/sopel-modules.birthdays/sopel_modules.birthdays-0.1.0.tar.gz/sopel_modules.birthdays-0.1.0/sopel_modules/birthdays/birthdays.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import itertools
+import random
+import textwrap
+
+import sopel.module
+import sopel.formatting
+import requests
+
+
+def configure(config):
+    pass
+
+
+def setup(bot):
+    pass
+
+
+def get_names(j):
+    """ Get names from json response """
+    return [i['text'].split(',')[0] for i in j['data']['Births']]
+
+
+def apply_colours(days, colours):
+    """ Apply given colours to list of names """
+    cdays = [sopel.formatting.color(i, j)
+             for i, j in zip(days, itertools.cycle(colours))]
+
+    return cdays
+
+
+def split_msg(days):
+    msg = ', '.join(i.replace(' ', '\x1f') for i in days)
+    lines = [i.replace('\x1f', ' ') for i in textwrap.wrap(msg, 400)]
+
+    return lines
+
+
+@sopel.module.commands('bdays')
+def bdays(bot, trigger):
+    r = requests.get('https://history.muffinlabs.com/date')
+    rj = r.json()
+
+    names = get_names(rj)
+    colours = random.sample(['01', '02', '03', '04', '06', '08', '11'], 7)
+    cnames = apply_colours(names, colours)
+
+    for msg in split_msg(cnames):
+        bot.say('{}'.format(msg))
---
+++ packages/sopel-modules.birthdays/sopel_modules.birthdays-0.1.0.tar.gz/sopel_modules.birthdays-0.1.0/tests/test_birthdays.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import unittest
+import pytest
+
+from sopel_modules.birthdays import birthdays
+
+
+def test_get_names():
+    j = {'data': {'Births': [{'text': 'person1, description'}]}}
+    expected = ['person1']
+
+    names = birthdays.get_names(j)
+
+    assert expected == names
+
+
+def test_apply_colours():
+    cdays = birthdays.apply_colours(['abc', 'def', 'ghi'], [1, 2])
+
+    assert cdays == ['\x0301abc\x03', '\x0302def\x03', '\x0301ghi\x03']
+
+
+def test_split_messages():
+    days = ['test 1', 'test 2'] * 26
+    output_lines = birthdays.split_msg(days)
+
+    assert output_lines[-1] == 'test 1, test 2'
