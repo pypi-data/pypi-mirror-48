e695dd1ec1e09f2d148618a84f590aff0c38ecd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:30 +0000

    Add package kedro-viz/1.0.1/kedro-viz-1.0.1.tar.gz

---
+++ packages/kedro-viz/kedro-viz-1.0.1.tar.gz/kedro-viz-1.0.1/kedro_viz/html/precache-manifest.336466d6fb49cfa051349ec40d8bdf8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4888f17e810d35fd0ae862d03a272f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d06f37c8b5c7d0f69289",
+    "url": "./static/css/2.d438ea0b.chunk.css"
+  },
+  {
+    "revision": "afd965705b12b3f64616",
+    "url": "./static/css/main.1e0bf853.chunk.css"
+  },
+  {
+    "revision": "d06f37c8b5c7d0f69289",
+    "url": "./static/js/2.ad4db969.chunk.js"
+  },
+  {
+    "revision": "afd965705b12b3f64616",
+    "url": "./static/js/main.95f6455e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7498697619bed8bcf479fc4d8648b5b4",
+    "url": "./static/media/delete.74986976.svg"
+  }
+]);
