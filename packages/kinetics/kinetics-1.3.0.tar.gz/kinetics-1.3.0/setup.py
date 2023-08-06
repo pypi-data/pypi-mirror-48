12aa47a4fe9c5102bf91239c4774dcf41c082df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:10 +0000

    Add package kinetics/1.3.0/kinetics-1.3.0.tar.gz

---
+++ packages/kinetics/kinetics-1.3.0.tar.gz/kinetics-1.3.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import kinetics
+
+setup(
+  name = 'kinetics',
+  packages = ['kinetics'],
+  version = kinetics.__version__,
+  license='MIT',
+  description = 'Python code to run kinetic models of enzyme reactions',
+  author = 'William Finnigan',
+  author_email = 'wjafinnigan@gmail.com',
+  url = 'https://github.com/willfinnigan/kinetics',
+  download_url = 'https://github.com/willfinnigan/kinetics/archive/1.3.0.tar.gz',
+  keywords = ['enzyme', 'kinetics', 'modelling'],
+  install_requires=['scipy', 'numpy', 'SALib', 'tqdm', 'matplotlib', 'pandas', 'deap'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'],
+)
