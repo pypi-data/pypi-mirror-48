b1a4c6cec783bc83f1531bc0992c09bba05eb759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:18 +0000

    Add package edge-pydb/0.1.3/edge_pydb-0.1.3-py3-none-any.whl

---
+++ packages/edge-pydb/edge_pydb-0.1.3-py3-none-any.whl/edge_pydb/__init__.py
@@ -0,0 +1,61 @@
+# find _configuration _file
+import os
+import json
+
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+_filepath = os.path.join(_ROOT, 'config.json')
+# print(_ROOT)
+# print(_filepath)
+try:
+    _fp = open(_filepath, 'r+')
+    _config = json.load(_fp)
+
+except:
+    _fp = open(_filepath, 'w') 
+    _config = {}
+
+if not _config:
+    # print(os.listdir(_ROOT))
+    for _root, _dirs, _files in os.walk(_ROOT):
+        # print(_files)
+        for _file in _files:
+            if _file.endswith('.csv') or _file.endswith('.hdf5'):
+                # print(_file)
+                # print(os.path.join(_ROOT, _file))
+                # print(_dirs)
+                _config[_file] = os.path.join(_root, _file)
+
+json.dump(_config, _fp)
+_fp.close()
+
+def listfiles(file_type=None):
+    suffix = ''
+    if file_type:
+        suffix = file_type
+
+    _files = []
+    for key in _config:
+        if key.endswith(suffix):
+            print(key)
+            _files.append(key)
+    return _files
+
+def getfiles(names):
+    if isinstance(names, list):
+        retval = []
+        for name in names:
+            if name not in _config:
+                raise Exception("Cannot find the specified file: %s" % name)
+            retval.append(_config[name])
+        return retval
+    else:
+        if names not in _config:
+            raise Exception("Cannot find the specified file: %s" % names)
+        return _config[names]
+
+
+from edge_pydb import edge_conv, fitsextract
+
+# update the _files
