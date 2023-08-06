6a7de1e8a5f006b430551fcfc034b4669d665a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:41 +0000

    Add package buildbot-macports-custom-views/0.0.1/buildbot-macports-custom-views-0.0.1.tar.gz

---
+++ packages/buildbot-macports-custom-views/buildbot-macports-custom-views-0.0.1.tar.gz/buildbot-macports-custom-views-0.0.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+try:
+    from buildbot_pkg import setup_www_plugin
+except ImportError:
+    import sys
+    print("Please install buildbot_pkg module in order to install that package, or use the pre-build .whl modules available on pypi", file=sys.stderr)
+    sys.exit(1)
+
+setup_www_plugin(
+    name='buildbot-macports-custom-views',
+    description='MacPorts Custom Views Plugin for Buildbot',
+    version='0.0.1',
+    author=u'Rajdeep Bharati',
+    author_email=u'rajdeepbharati13@gmail.com',
+    url='https://github.com/macports/buildbot-macports-custom-views',
+    packages=['buildbot_macports_custom_views'],
+    package_data={
+        '': [
+            'VERSION',
+            'static/*'
+        ]
+    },
+    entry_points="""
+        [buildbot.www]
+        buildbot_macports_custom_views = buildbot_macports_custom_views:ep
+    """,
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ],
+)
