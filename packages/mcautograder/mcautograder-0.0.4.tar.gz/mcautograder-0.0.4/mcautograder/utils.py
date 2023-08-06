4df81935e0907c7c7405fa7084027d7da9497b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:40 +0000

    Add package mcautograder/0.0.4/mcautograder-0.0.4.tar.gz

---
+++ packages/mcautograder/mcautograder-0.0.4.tar.gz/mcautograder-0.0.4/mcautograder/utils.py
@@ -0,0 +1,13 @@
+##############################################
+##### Utilities for mcautograder library #####
+##############################################
+
+def repeat(x, n):
+	"""
+	Returns a list of a given value repeated a given number of times
+
+	Args:
+		x - value to repeat
+		n - number of repetitions
+	"""
+	return [x for _ in range(n)]
