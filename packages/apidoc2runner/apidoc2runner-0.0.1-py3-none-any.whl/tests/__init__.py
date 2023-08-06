07055a76ff2f3981f7a8f6c8dd082735aa552511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:10 +0000

    Add package apidoc2runner/0.0.1/apidoc2runner-0.0.1-py3-none-any.whl

---
+++ packages/apidoc2runner/apidoc2runner-0.0.1-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time : 2019-06-26 15:03
+# @Author : GerryWen
+# @Contact : 15160333779
+# @Email: blog@gerrywen.com
+# @Site : 
+# @File : __init__.py.py
+# @Software: PyCharm
