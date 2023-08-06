809ef38782d063eac884fe4b2364aa7b349c587a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:19 +0000

    Add package django-talar/0.1.7/django_talar-0.1.7-py3-none-any.whl

---
+++ packages/django-talar/django_talar-0.1.7-py3-none-any.whl/talar/views.py
@@ -0,0 +1,27 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from . import signals
+from .serializers import NotifySerializer
+
+
+class NotifyView(APIView):
+    @method_decorator(csrf_exempt)
+    def post(self, request, format=None):
+        serializer = NotifySerializer(data=request.data)
+        if serializer.is_valid():
+            self._emit_signal(
+                serializer.data.name, serializer.data
+            )
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def _emit_signal(name, data):
+        signals.notification_received.send(
+            sender=None,
+            name=name, data=data
+        )
