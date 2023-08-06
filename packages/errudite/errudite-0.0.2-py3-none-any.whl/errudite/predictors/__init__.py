555981c0087b256029ddc5dfc463bd5630412850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:04 +0000

    Add package errudite/0.0.2/errudite-0.0.2-py3-none-any.whl

---
+++ packages/errudite/errudite-0.0.2-py3-none-any.whl/errudite/predictors/__init__.py
@@ -0,0 +1,22 @@
+from .predictor import Predictor
+from .qa.predictor_qa import PredictorQA
+from .nli.predictor_nli import PredictorNLI
+from .sentiment_analysis.predictor_sentiment_analysis import PredictorSA
+
+
+try:
+    from .qa.predictor_bidaf import PredictorBiDAF
+except:
+    pass
+
+try:
+    from .nli.predictor_decompose_att import PredictorDecomposeAtt
+except:
+    pass
+
+#from .vqa.predictor_vqa import PredictorVQA
+
+try:
+    from .sentiment_analysis.predictor_bcn import PredictorBCN
+except:
+    pass
