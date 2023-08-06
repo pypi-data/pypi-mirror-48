1ae0dd38624aeca4d2e02fe30758cc63e1e4224c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:08 +0000

    Add package instpector/0.1.2/instpector-0.1.2.tar.gz

---
+++ packages/instpector/instpector-0.1.2.tar.gz/instpector-0.1.2/instpector/apis/instagram/__init__.py
@@ -0,0 +1,8 @@
+from .authenticate import Authenticate
+from .followers import Followers
+from .following import Following
+from .parser import Parser
+from .profile import Profile
+from .timeline import Timeline
+
+__all__ = ["Authenticate", "Followers", "Parser", "Following", "Profile", "Timeline"]
