40cd8eeecb051d0ccf8189759666afbc1e79f149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:33 +0000

    Add package ormantic/0.0.29/ormantic-0.0.29-py2.py3-none-any.whl

---
+++ packages/ormantic/ormantic-0.0.29-py2.py3-none-any.whl/ormantic/__init__.py
@@ -0,0 +1,35 @@
+from ormantic.exceptions import MultipleMatches, NoMatch
+from ormantic.fields import (
+    Boolean,
+    Date,
+    DateTime,
+    Enum,
+    Float,
+    ForeignKey,
+    Integer,
+    JSON,
+    String,
+    StringArray,
+    Text,
+    Time,
+)
+from ormantic.models import Model
+
+__version__ = "0.0.29"
+__all__ = [
+    "NoMatch",
+    "MultipleMatches",
+    "Boolean",
+    "Date",
+    "Enum",
+    "DateTime",
+    "Float",
+    "ForeignKey",
+    "Integer",
+    "JSON",
+    "String",
+    "StringArray",
+    "Text",
+    "Time",
+    "Model",
+]
