069fafb169740abc01c64591f4fc4325d232628d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:50 +0000

    Add package git-hooks-1c/9.2.2/git-hooks-1c-9.2.2.tar.gz

---
+++ packages/git-hooks-1c/git-hooks-1c-9.2.2.tar.gz/git-hooks-1c-9.2.2/git_hooks_1c/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import logging
+
+# noinspection PyUnresolvedReferences
+from git_hooks_1c.__about__ import __version__
+
+# noinspection PyUnresolvedReferences
+logging.getLogger().setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
