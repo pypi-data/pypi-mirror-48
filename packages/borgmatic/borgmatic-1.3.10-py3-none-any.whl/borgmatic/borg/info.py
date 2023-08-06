5af198dc6384b0cd5eafb2dca19ebc04349e9f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:34 +0000

    Add package borgmatic/1.3.10/borgmatic-1.3.10-py3-none-any.whl

---
+++ packages/borgmatic/borgmatic-1.3.10-py3-none-any.whl/borgmatic/borg/info.py
@@ -0,0 +1,26 @@
+import logging
+
+from borgmatic.execute import execute_command
+
+logger = logging.getLogger(__name__)
+
+
+def display_archives_info(
+    repository, storage_config, local_path='borg', remote_path=None, json=False
+):
+    '''
+    Given a local or remote repository path, and a storage config dict, display summary information
+    for Borg archives in the repository or return JSON summary information.
+    '''
+    lock_wait = storage_config.get('lock_wait', None)
+
+    full_command = (
+        (local_path, 'info', repository)
+        + (('--remote-path', remote_path) if remote_path else ())
+        + (('--lock-wait', str(lock_wait)) if lock_wait else ())
+        + (('--info',) if logger.getEffectiveLevel() == logging.INFO and not json else ())
+        + (('--debug', '--show-rc') if logger.isEnabledFor(logging.DEBUG) and not json else ())
+        + (('--json',) if json else ())
+    )
+
+    return execute_command(full_command, output_log_level=None if json else logging.WARNING)
