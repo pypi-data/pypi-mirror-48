b81bb12ac9642d431d3a9e8c1e6bbd853f0fc828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:12 +0000

    Add package pibot/34/pibot-34.tar.gz

---
+++ packages/pibot/pibot-34.tar.gz/pibot-34/pibot/buttons.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+from pibot import constants as c
+
+from time import sleep
+
+
+def init_buttons():
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(c.BUTTON_LEFT_PIN, GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(c.BUTTON_MID_PIN, GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(c.BUTTON_RIGHT_PIN, GPIO.IN, GPIO.PUD_UP)
+
+
+def wait_for_button_press(button):
+    while not is_pressed(button):
+        sleep(0.001)
+
+
+def wait_for_button_release(button):
+    while is_pressed(button):
+        sleep(0.001)
+
+
+def wait_for_button(button):
+    wait_for_button_press(button)
+    wait_for_button_release(button)
+
+
+def is_pressed(button):
+    if button == c.BUTTON_LEFT:
+        pin = c.BUTTON_LEFT_PIN
+    elif button == c.BUTTON_MID:
+        pin = c.BUTTON_MID_PIN
+    elif button == c.BUTTON_RIGHT:
+        pin = c.BUTTON_RIGHT_PIN
+    else:
+        pin = button
+
+    if pin is not None:
+        if GPIO.input(pin):
+            return True
+    return False
