f5eb377ff7e136e6adc0ece87929204373d52949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:07 +0000

    Add package parse-diqu/1.0.27/parse_diqu-1.0.27.tar.gz

---
+++ packages/parse-diqu/parse_diqu-1.0.27.tar.gz/parse_diqu-1.0.27/parse_diqu/__init__.py
@@ -0,0 +1,3 @@
+from .parse_diqu import parseDiqu as parseDiqu1
+from .parse_diquxx import parseDiqu as parseDiqu2
+from .parse_diquxxx import parseDiqu as parseDiqu3
---
+++ packages/parse-diqu/parse_diqu-1.0.27.tar.gz/parse_diqu-1.0.27/setup.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="parse_diqu",
+
+    version="1.0.27",
+
+    author="lanmengfei",
+    author_email="865377886@qq.com",
+    description="深圳市筑龙科技的工作-解析地区编号",
+    long_description=open("README.txt", encoding="utf8").read(),
+
+    url="https://github.com/lanmengfei/testdm",
+
+    packages=find_packages(),
+
+    package_data={  # "zhulong.hunan":["profile"]
+        "parse_diqu": ["xzqh_key_word.json", "公共区域", 'list.json'],
+    },
+
+    install_requires=[
+        "jieba",
+        "beautifulsoup4>=4.6.3",
+        "lmfscrap>=1.1.0",
+    ],
+
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ],
+)
