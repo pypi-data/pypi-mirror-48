0c04ac36ef1ecbf2a50bc8819028c4bc67491b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:39 +0000

    Add package attributeme/0.1/attributeme-0.1-py3-none-any.whl

---
+++ packages/attributeme/attributeme-0.1-py3-none-any.whl/attributeme/__init__.py
@@ -0,0 +1,6 @@
+def get_time_line(df, col1, col2):
+    df[col2] = df[col2].astype('datetime64[ns]')
+    df['timedelta_hours'] = df.groupby(col1)[col2].apply(lambda x: x.shift(-1) - x)/np.timedelta64(1, 'h')
+    df['timedelta_sec'] = df.groupby(col1)[col2].apply(lambda x: x.shift(-1) - x)/np.timedelta64(1, 's')
+    df['timedelta_days'] = df.groupby(col1)[col2].apply(lambda x: x.shift(-1) - x)/np.timedelta64(1, 'D')
+    return df
