957c672a1424db7e9596424f8f536659ad63de26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:39 +0000

    Add package royalnet/5.0a25/royalnet-5.0a25.tar.gz

---
+++ packages/royalnet/royalnet-5.0a25.tar.gz/royalnet-5.0a25/royalnet/web/royalprints/__init__.py
@@ -0,0 +1,10 @@
+"""Some Royalprints that can be used with the Royalnet Flask server."""
+
+from .home import rp as rp_home
+from .wikiview import rp as rp_wikiview
+from .tglogin import rp as rp_tglogin
+from .docs import rp as rp_docs
+from .wikiedit import rp as rp_wikiedit
+from .mcstatus import rp as rp_mcstatus
+
+__all__ = ["rp_home", "rp_wikiview", "rp_tglogin", "rp_docs", "rp_wikiedit", "rp_mcstatus"]
