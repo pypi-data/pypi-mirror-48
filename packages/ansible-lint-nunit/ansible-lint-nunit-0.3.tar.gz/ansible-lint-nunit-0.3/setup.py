575e7f4209575514218b45b59cfea3d6039dbcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:04 +0000

    Add package ansible-lint-nunit/0.3/ansible-lint-nunit-0.3.tar.gz

---
+++ packages/ansible-lint-nunit/ansible-lint-nunit-0.3.tar.gz/ansible-lint-nunit-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+from bootstrap import bootstrap
+
+version = bootstrap.version()
+
+setup(
+    name='ansible-lint-nunit',
+    packages=['bootstrap'],
+    version=version,
+    description='ansible-lint to NUnit converter.',
+    author='wasil',
+    author_email='ferhat.yildiz@turingts.com',
+    url='https://github.com/ferhaty/ansible-lint-nunit',
+    download_url='https://github.com/ferhaty/ansible-lint-nunit/archive/%s.tar.gz' % (version),
+    keywords=['ansible', 'nunit'],
+    classifiers=[],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ['ansible-lint-nunit = bootstrap.bootstrap:main']
+    },
+    install_requires=[
+        'ansible-lint',
+    ],
+)
