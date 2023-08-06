34250d9189de268336cdcaf9214b4ea892ef1cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package gzvoc/0.0.1/gzvoc-0.0.1.tar.gz

---
+++ packages/gzvoc/gzvoc-0.0.1.tar.gz/gzvoc-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name='gzvoc',
+    version='0.0.1',
+    author='sxd',
+    author_email='sxd95@qq.com',
+    url='https://weibo.com/xds95',
+    description=u'整理voc数据集',
+    packages=['gzvoc'],
+    install_requires=[],
+    platforms='any',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
