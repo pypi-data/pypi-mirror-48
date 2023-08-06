7a246bf05e70d3174ed71152d06dfd6d5fd2bf76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package pwclip/1.6.1/pwclip-1.6.1.linux-x86_64.tar.gz

---
+++ packages/pwclip/pwclip-1.6.1.linux-x86_64.tar.gz/usr/local/lib/python3.6/dist-packages/pwclip/lib/net/__init__.py
@@ -0,0 +1,5 @@
+"""net lib for pwclip"""
+# to only import all necessarry modules/libs define the calling executable
+from net.dns import \
+    fqdn, askdns, raflookup
+from net.ssh import SecureSHell, SSHException
