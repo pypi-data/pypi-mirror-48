807de56857ef518a2193254aaee622f73418feca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:28 +0000

    Add package xseed-maxbox/1.1/xseed_maxbox-1.1-py3-none-any.whl

---
+++ packages/xseed-maxbox/xseed_maxbox-1.1-py3-none-any.whl/xseed_maxbox/sub_modules/dockerCompose.py
@@ -0,0 +1,22 @@
+import subprocess as sp
+import os
+import sys
+
+def createEnvVariable(miniCampus, dbusername, dbpassword, buildEnv):
+    os.system('echo "MINICAMPUS_CODE=%s\
+    MONGO_INITDB_ROOT_USERNAME=%s\
+    MONGO_INITDB_ROOT_PASSWORD=%s \
+    BUILD_ENV=%s"|sudo tee /var/www/.env' %(miniCampus, dbusername, dbpassword, buildEnv))
+
+def updatingDockerComposeFile():
+    pkgdir = sys.modules['xseed_maxbox'].__path__[0]
+    fullpath = os.path.join(pkgdir,"data/docker-compose.yml" )
+    os.system('sudo cp %s /var/www/' %fullpath)
+
+def dockerLogin(userName, passWord):
+    os.system("docker login --username %s --password %s" %(userName, passWord))
+
+def configureDocker(miniCampus, dbusername, dbpassword, dockerusername, dockerpassword, buildEnv):
+    createEnvVariable(miniCampus, dbusername, dbpassword, buildEnv)
+    updatingDockerComposeFile()
+    dockerLogin(dockerusername, dockerpassword)
