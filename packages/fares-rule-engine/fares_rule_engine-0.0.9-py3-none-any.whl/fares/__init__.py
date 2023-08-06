ad6201af77e52f40a0b44d65d788ca8c9145c69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:25 +0000

    Add package fares-rule-engine/0.0.9/fares_rule_engine-0.0.9-py3-none-any.whl

---
+++ packages/fares-rule-engine/fares_rule_engine-0.0.9-py3-none-any.whl/fares/__init__.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+from fares.dimension import (
+    resolve_user_dimensions,
+    zone_resolver,
+    inter_cluster_resolver,
+)
+from fares.subscription_offering import SubscriptionOffering
+
+
+def _get_usage_resolver(subscription_offering):
+    if subscription_offering.applicability_type == "ZONE_WIDE":
+        return zone_resolver
+    elif subscription_offering.applicability_type == "INTER_CLUSTER":
+        return inter_cluster_resolver
+    else:
+        raise TypeError("Invalid Applicability Type")
+
+
+def is_subscription_offering_applicable_for_usage(
+    subscription_offering: SubscriptionOffering, slot: Dict
+):
+    rule_set = subscription_offering.usage_applicability_rules()
+    resolver = _get_usage_resolver(subscription_offering)
+    return rule_set.evaluate(resolver(slot))
+
+
+def is_subscription_offering_applicable_for_purchase(
+    subscription_offering: SubscriptionOffering, user: Dict
+):
+    rule_set = subscription_offering.purchase_applicability_rules()
+    context = resolve_user_dimensions(user)
+    return rule_set.evaluate(context)
