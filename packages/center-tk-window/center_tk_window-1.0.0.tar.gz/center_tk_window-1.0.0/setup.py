bb040204094c6e92f21b4e375d0abbb9a5c27fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:50 +0000

    Add package center-tk-window/1.0.0/center_tk_window-1.0.0.tar.gz

---
+++ packages/center-tk-window/center_tk_window-1.0.0.tar.gz/center_tk_window-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="center_tk_window",
+    version="1.0.0",
+    author="Jarik Marwede",
+    author_email="jarikmarwede@zoho.eu",
+    description="Functions for centering tkinter windows on parent or on screen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jarikmarwede/center-tk-window",
+    packages=setuptools.find_packages(),
+    py_modules=["center_tk_window"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
