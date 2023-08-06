13c32de3a2b79f3d81178cf96f92bca453388418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package mpl-colors/0.1.0/mpl_colors-0.1.0-py3-none-any.whl

---
+++ packages/mpl-colors/mpl_colors-0.1.0-py3-none-any.whl/mpl_colors/__init__.py
@@ -0,0 +1,6 @@
+from .generated import BaseColor, TableauColor, XkcdColor, Css4Color, Color
+
+__version__ = "0.1.0"
+__version_info__ = tuple(int(n) for n in __version__.split("."))
+
+__all__ = ["BaseColor", "TableauColor", "XkcdColor", "Css4Color", "Color"]
