74921b1ef767dc4a513c642aa39d93e5576e789f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:39 +0000

    Add package jupyterlab-email/0.2.0/jupyterlab_email-0.2.0.tar.gz

---
+++ packages/jupyterlab-email/jupyterlab_email-0.2.0.tar.gz/jupyterlab_email-0.2.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().split()
+
+setup(
+    name='jupyterlab_email',
+    version='0.2.0',
+    description='Sending emails from JupyterLab',
+    url='https://github.com/timkpaine/jupyterlab_email',
+    author='Tim Paine',
+    author_email='t.paine154@gmail.com',
+    license='Apache 2.0',
+    install_requires=requires,
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='jupyter jupyterlab',
+
+    packages=find_packages(exclude=['tests', ]),
+    include_package_data=True,
+    zip_safe=False,
+)
