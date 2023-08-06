d2fe05f94a545e710df4fdcc16e4d295e52c6fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:46 +0000

    Add package django-howl/1.0.4/django-howl-1.0.4.tar.gz

---
+++ packages/django-howl/django-howl-1.0.4.tar.gz/django-howl-1.0.4/howl/settings.py
@@ -0,0 +1,18 @@
+DEBUG = True
+
+SECRET_KEY = 'dev'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+
+    'howl',
+]
