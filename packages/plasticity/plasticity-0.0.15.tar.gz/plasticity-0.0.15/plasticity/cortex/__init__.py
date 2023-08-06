13ba35035f6a296ed4ee0145c59c91e468474cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:44 +0000

    Add package plasticity/0.0.15/plasticity-0.0.15.tar.gz

---
+++ packages/plasticity/plasticity-0.0.15.tar.gz/plasticity-0.0.15/plasticity/cortex/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from plasticity.base.service import Service
+
+
+class Cortex(Service):
+    """Cortex is the API service for knowledge. Its functions are described
+    here: https://www.plasticity.ai/api/docs/#cortex
+    """
+
+    def __init__(self, plasticity):
+        """Initializes a new Cortex Service."""
+        super(Cortex, self).__init__(plasticity)
+        self.url = self.plasticity.url + 'cortex/'
