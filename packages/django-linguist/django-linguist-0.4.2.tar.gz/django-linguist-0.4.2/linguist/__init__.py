8b695d169c0409c1a75e9fc2b370c7fc2859ffad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package django-linguist/0.4.2/django-linguist-0.4.2.tar.gz

---
+++ packages/django-linguist/django-linguist-0.4.2.tar.gz/django-linguist-0.4.2/linguist/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+version = (0, 4, 2)
+
+__version__ = ".".join(map(str, version))
+
+default_app_config = "linguist.apps.LinguistConfig"
+
+__all__ = ["default_app_config", "version"]
