206da3ce8fdd65aa461222842d39d16af615aa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:12 +0000

    Add package Toaster-Secure-Scaffold-Beta/0.6/Toaster%20Secure%20Scaffold%20Beta-0.6.tar.gz

---
+++ packages/Toaster-Secure-Scaffold-Beta/Toaster%20Secure%20Scaffold%20Beta-0.6.tar.gz/Toaster Secure Scaffold Beta-0.6/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import setuptools
+
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Toaster Secure Scaffold Beta",
+    version='0.6',
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'firestore': ['google-cloud-firestore==1.2.0'],
+        'datastore': ['google-cloud-datastore==1.8.0'],
+        'tasks': ['google-cloud-tasks==1.1.0'],
+    },
+    author="Toaster Ltd.",
+    author_email="developers@toaster.co",
+    description="Secure Scaffold for Google App Engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toasterco/gae-secure-scaffold-python",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
