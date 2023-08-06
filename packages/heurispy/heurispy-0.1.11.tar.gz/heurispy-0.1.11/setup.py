3ca1898f8a4b8f5646ab1096b45121ef77e650f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:07 +0000

    Add package heurispy/0.1.11/heurispy-0.1.11.tar.gz

---
+++ packages/heurispy/heurispy-0.1.11.tar.gz/heurispy-0.1.11/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='heurispy',
+      version='0.1.11',
+      description='Framework para exploración de heurísticas de búsqueda local en problemas de optimización discreta',
+      url='https://gitlab.com/escamilla.een/heurispy',
+      author='Esteban Escamilla Navarro',
+      author_email='escamilla.een@gmail.com',
+      license='',
+      packages=['heurispy', 'heurispy.ejemplos', 'heurispy.heuristicas'],
+      install_requires=['pathos', 'tqdm', 'numpy', 'pandas', 'fpdf'],
+      include_package_data=True,
+      zip_safe=False)
