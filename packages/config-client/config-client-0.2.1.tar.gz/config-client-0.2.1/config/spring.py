71e8a60e567ee4fe44bb14ab2d3986c6b449f2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:07 +0000

    Add package config-client/0.2.1/config-client-0.2.1.tar.gz

---
+++ packages/config-client/config-client-0.2.1.tar.gz/config-client-0.2.1/config/spring.py
@@ -0,0 +1,93 @@
+"""Module for retrieve application's config from Spring Cloud Config."""
+import logging
+import os
+import sys
+from functools import wraps
+
+from config.core import singleton
+
+import requests
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+@singleton
+class ConfigServer:
+    """ConfigServer client."""
+
+    _config = {}
+
+    def __init__(self):
+        """Create a instance to retrieve config from Spring Cloud Config."""
+        self.__load_config_from_configserver()
+
+    def __load_config_from_configserver(self):
+        """Retrieve configuration information."""
+        try:
+            request_url = self.__format_configserver_url()
+            response = requests.get(request_url)
+
+            logging.debug(f"Response status code: {response.status_code}")
+
+            if response.status_code == 200:
+                self._config = response.json()
+
+        except requests.exceptions.ConnectionError:
+            logging.error(
+                'Failed to establish connection with configserver.')
+            sys.exit(1)
+
+    def __format_configserver_url(self):
+        """Format ConfigServer URL."""
+        logging.debug('Building request URL')
+
+        address = os.getenv('CONFIGSERVER_ADDRESS') or 'http://localhost:8888/configuration'
+        branch = os.getenv('BRANCH') or 'master'
+        profile = os.getenv('PROFILE') or 'development'
+        context_name = os.getenv('APP_NAME')
+
+        logging.debug(f"ConfigServer address: {address}")
+        logging.debug(f"Branch configuration: {branch}")
+        logging.debug(f"Profile set: {profile}")
+        logging.debug(f"Configuration file base name: {context_name}")
+        logging.debug(
+            f"URL formatted: {address}/{branch}/{context_name}-{profile}.json")
+
+        return f"{address}/{branch}/{context_name}-{profile}.json"
+
+    @property
+    def config(self):
+        """Getter from configurations retrieved from configserver."""
+        return self._config
+
+    def get_attribute(self, attr):
+        """Get attribute from configurations.
+
+        Use <dot> to define a path on a key tree.
+        """
+        key_list = attr.split('.')
+        logging.debug(f'Key attribute: {key_list}')
+
+        attribute_content = self.config.get(key_list[0])
+        logging.debug(f'Attribute content: {attribute_content}')
+
+        for key in key_list[1:]:
+            attribute_content = attribute_content.get(key)
+
+        logging.debug(f"Configuration getted: {attribute_content}")
+
+        return attribute_content
+
+    def get_keys(self):
+        """List all keys from configuration retrieved."""
+        return self.config.keys()
+
+
+def config_client(func):
+    """Spring config client decorator."""
+    @wraps(func)
+    def enable_config():
+        """Inner function to create ConfigServer instance."""
+        return func(ConfigServer())
+    return enable_config
