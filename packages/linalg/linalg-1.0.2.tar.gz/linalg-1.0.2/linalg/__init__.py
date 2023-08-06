fd4244edc14bc7ccaa2da59fbcec13b710f44636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:34 +0000

    Add package linalg/1.0.2/linalg-1.0.2.tar.gz

---
+++ packages/linalg/linalg-1.0.2.tar.gz/linalg-1.0.2/linalg/__init__.py
@@ -0,0 +1,8 @@
+"""Main functions and classes"""
+
+from linalg.types import Matrix
+from linalg.create import *
+from linalg.decompose import *
+from linalg.solve import *
+from linalg.unary import *
+from linalg.data import *
