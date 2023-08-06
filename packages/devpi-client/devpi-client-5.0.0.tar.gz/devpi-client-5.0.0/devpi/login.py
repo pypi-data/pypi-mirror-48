ee6c0e6124bfce4ce938468f38d134b0ef1a5a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:27 +0000

    Add package devpi-client/5.0.0/devpi-client-5.0.0.tar.gz

---
+++ packages/devpi-client/devpi-client-5.0.0.tar.gz/devpi-client-5.0.0/devpi/login.py
@@ -0,0 +1,34 @@
+from devpi.main import hookimpl
+import getpass
+
+
+def main(hub, args):
+    user = args.username
+    if user is None:
+        user = hub.raw_input("user: ")
+    password = args.password
+    if password is None:
+        password = hub.hook.devpiclient_get_password(
+            url=hub.current.root_url.url, username=user)
+    input = dict(user=user, password=password)
+    r = hub.http_api("post", hub.current.login, input, quiet=False)
+    hub.current.set_auth(user, r.result["password"])
+    hours = r.result["expiration"] / (60*60.0)
+    hub.info("logged in %r at %r, credentials valid for %.2f hours" %
+             (user, hub.current.index, hours))
+    #else:
+    #    hub.error("server refused %r login, code=%s" %(user, r.status_code))
+    #    return 1
+
+
+@hookimpl(trylast=True)
+def devpiclient_get_password(url, username):
+    return getpass.getpass("password for user %s: " % username)
+
+
+def logoff(hub, args):
+    if hub.current.get_auth():
+        hub.current.del_auth()
+        hub.info("login information deleted")
+    else:
+        hub.error("not logged in")
---
+++ packages/devpi-client/devpi-client-5.0.0.tar.gz/devpi-client-5.0.0/setup.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+import io
+import os, re
+
+from setuptools import setup, find_packages
+
+
+def get_changelog():
+    text = io.open(os.path.join(here, 'CHANGELOG'), encoding='utf-8').read()
+    header_matches = list(re.finditer('^=+$', text, re.MULTILINE))
+    # until fifth header
+    text = text[:header_matches[5].start()]
+    # all lines without fifth release number
+    lines = text.splitlines()[:-1]
+    return "=========\nChangelog\n=========\n\n" + "\n".join(lines)
+
+
+if __name__ == "__main__":
+    here = os.path.abspath(".")
+    README = io.open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+    CHANGELOG = get_changelog()
+
+    install_requires=["tox>=3.1.0",
+                      "devpi_common<4,>=3.4.0",
+                      "pkginfo>=1.4.2",
+                      "check-manifest>=0.28",
+                      "pluggy>=0.6.0,<1.0",
+                      "py>=1.4.31"]
+
+    extras_require = {}
+
+    setup(
+      name="devpi-client",
+      description="devpi upload/install/... workflow commands for Python "
+                  "developers",
+      long_description="\n\n".join([README, CHANGELOG]),
+      version='5.0.0',
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require=extras_require,
+      url="https://github.com/devpi/devpi",
+      maintainer="Holger Krekel",
+      maintainer_email="holger@merlinux.eu",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        ] + [
+            ("Programming Language :: Python :: %s" % x) for x in
+                "2.7 3.4 3.5 3.6".split()],
+      entry_points = {
+        'console_scripts': [
+          "devpi = devpi.main:main"],
+        'devpi_client': [
+          "devpi-client-login = devpi.login",
+          "devpi-client-subcommands = devpi.main"]}
+      )
