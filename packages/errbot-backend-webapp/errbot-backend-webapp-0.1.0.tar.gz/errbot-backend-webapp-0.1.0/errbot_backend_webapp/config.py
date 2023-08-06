62a0704688eb91420d1239bd4db3f0c8295e5d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package errbot-backend-webapp/0.1.0/errbot-backend-webapp-0.1.0.tar.gz

---
+++ packages/errbot-backend-webapp/errbot-backend-webapp-0.1.0.tar.gz/errbot-backend-webapp-0.1.0/errbot_backend_webapp/config.py
@@ -0,0 +1,14 @@
+class WebappConfig(object):
+    """Webapp server configuration
+    """
+    def __init__(self, bot_config):
+        conf = getattr(bot_config, 'BOT_IDENTITY', {})
+        self.host: str = conf.get('host', 'localhost')
+        """Listen host"""
+        self.port: int = conf.get('port', 8080)
+        """Listen port"""
+        self.use_commands: bool = conf.get('use_commands', False)
+        """Use local commands"""
+
+
+DEFAULT_CLIENT_USERNAME = 'anonymous'
