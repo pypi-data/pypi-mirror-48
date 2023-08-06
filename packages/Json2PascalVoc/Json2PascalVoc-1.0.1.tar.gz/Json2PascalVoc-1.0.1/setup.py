bdb3c333409b3b0cec6670b77cb2699db9003162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:26 +0000

    Add package Json2PascalVoc/1.0.1/Json2PascalVoc-1.0.1.tar.gz

---
+++ packages/Json2PascalVoc/Json2PascalVoc-1.0.1.tar.gz/Json2PascalVoc-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name="Json2PascalVoc",
+	version='1.0.1',
+	author='Caner Karagüler',
+	author_email='caner.karaguler@gmail.com',
+	long_description = long_description,
+	long_description_content_type="text/markdown",
+	description = 'Package To Convert Json Files To PascalVOC XML files',
+	py_modules=["Converter"],
+	package_dir={'':'Json2PascalVoc'},
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+	)
