906c61252d76cd55718a1788be6a7875b797d0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:24 +0000

    Add package db-utils/0.4.5/db_utils-0.4.5.tar.gz

---
+++ packages/db-utils/db_utils-0.4.5.tar.gz/db_utils-0.4.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="db_utils",
+    version="0.4.5",
+    author="Komodo Technologies, LLC",
+    description="Helper class to connect to Redshift, Snowflake, DynamoDB and S3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ktechboston/db_utils",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'boto3',
+        'psycopg2',
+        'numpy',
+        'pandas',
+        'sqlparse',
+        'awscli',
+        'snowflake-connector-python',
+        'mysql-connector-python',
+        'pyodbc',
+        'jinja2'
+    ],
+)
