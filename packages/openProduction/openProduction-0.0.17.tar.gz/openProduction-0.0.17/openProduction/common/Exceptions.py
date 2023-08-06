25b4a922fb2d3355722006ba8f56510d6ab30ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:03 +0000

    Add package openProduction/0.0.17/openProduction-0.0.17.tar.gz

---
+++ packages/openProduction/openProduction-0.0.17.tar.gz/openProduction-0.0.17/openProduction/common/Exceptions.py
@@ -0,0 +1,13 @@
+# define Python user-defined exceptions
+class OpenProductionAssertionError(Exception):
+   """Base class for other exceptions"""
+   def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super(OpenProductionAssertionError, self).__init__(message)
+
+
+class OpenProductionAbortError(Exception):
+   """Base class for other exceptions"""
+   def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super(OpenProductionAbortError, self).__init__(message)
