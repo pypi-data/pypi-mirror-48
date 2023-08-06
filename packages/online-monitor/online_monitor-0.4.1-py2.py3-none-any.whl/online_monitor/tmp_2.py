bccbf013fbb2ad15c870d9da2beb915aa2a4e843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:09 +0000

    Add package online-monitor/0.4.1/online_monitor-0.4.1-py2.py3-none-any.whl

---
+++ packages/online-monitor/online_monitor-0.4.1-py2.py3-none-any.whl/online_monitor/tmp_2.py
@@ -0,0 +1,15 @@
+# import subprocess
+# import os
+# 
+# # import sys
+# # sys.path.append("/home/davidlp/miniconda")
+# 
+# print os.environ["PATH"]
+# subprocess.Popen(["python", "--version"])
+# subprocess.Popen("env")
+
+
+import numpy as np
+
+a = np.ones((10, 10), dtype=np.uint64)
+b = np.ones((10, 10), dtype=np.uint64)
