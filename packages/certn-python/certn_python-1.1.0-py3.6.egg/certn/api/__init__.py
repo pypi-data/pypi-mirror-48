4dedb7b461fcbd0c542ea5936564504ac05cb1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:41 +0000

    Add package certn-python/1.1.0/certn_python-1.1.0-py3.6.egg

---
+++ packages/certn-python/certn_python-1.1.0-py3.6.egg/certn/api/__init__.py
@@ -0,0 +1,9 @@
+from .auth import Auth
+from .properties import Properties
+from .listings import Listings
+from .applicants import Applicants
+from .applications import Applications
+from .reports import Reports
+
+
+__all__ = ['Auth', 'Applicants', 'Applications', 'Listings', 'Properties', 'Reports']
---
+++ packages/certn-python/certn_python-1.1.0-py3.6.egg/certn/api/auth.py
@@ -0,0 +1,24 @@
+from certn.api.api import API
+
+
+class Auth(API):
+
+    path = '/api/v1'
+
+    def login(self):
+        '''basic authentication returns the user id and token'''
+        response = self.client.post(
+            path=f'{self.path}/authenticate/', data={'expires': None}, is_authenticated=False
+        )
+
+        return response.get('user_id'), response.get('token')
+
+    def list(self):
+        '''List all logged in sessions'''
+        return self.client.get(path=f'{self.path}/authenticate/', is_authenticated=False)
+
+    def logout(self):
+        self.client.post(path=f'{self.path}/logout/', data=None, is_json=False)
+
+    def logout_all(self):
+        self.client.post(path=f'{self.path}/logoutall/', data=None, is_json=False)
