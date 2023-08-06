aa009aeb2f0bf9fd70e92910c324e8d03d46b7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:56 +0000

    Add package xseed-maxbox/2.5/xseed_maxbox-2.5-py3-none-any.whl

---
+++ packages/xseed-maxbox/xseed_maxbox-2.5-py3-none-any.whl/xseed_maxbox/sub_modules/dockerCompose.py
@@ -0,0 +1,35 @@
+import subprocess as sp
+import os
+import sys
+
+def createEnvVariable(miniCampus, dbusername, dbpassword, buildEnv):
+    os.system('sudo chown -R xseed:xseed /var/www/')
+    file = open("/var/www/.env", "w")
+    file.write("MINICAMPUS_CODE=%s\
+    MONGO_INITDB_ROOT_USERNAME=%s\
+    MONGO_INITDB_ROOT_PASSWORD=%s \
+    NODE_ENV=%s" %(miniCampus, dbusername, dbpassword, buildEnv))
+    file.close()
+
+    # os.system('echo "MINICAMPUS_CODE=%s\
+    # MONGO_INITDB_ROOT_USERNAME=%s\
+    # MONGO_INITDB_ROOT_PASSWORD=%s \
+    # BUILD_ENV=%s"| sudo tee /var/www/.env' %(miniCampus, dbusername, dbpassword, buildEnv))
+
+def updatingDockerComposeFile():
+    pkgdir = sys.modules['xseed_maxbox'].__path__[0]
+    fullpath = os.path.join(pkgdir,"data/docker-compose.yml" )
+    os.system('sudo cp %s /var/www/' %fullpath)
+
+def dockerLogin(userName, passWord):
+    os.system("echo %s |sudo docker login --username %s --password-stdin" %(passWord, userName))
+
+def configureDocker(miniCampus, dbusername, dbpassword, dockerusername, dockerpassword, buildEnv):
+    try:
+        createEnvVariable(miniCampus, dbusername, dbpassword, buildEnv)
+        updatingDockerComposeFile()
+        dockerLogin(dockerusername, dockerpassword)
+    except OSError:
+        print (e.message)
+        print ("Exiting the process because of above error")
+        exit();
