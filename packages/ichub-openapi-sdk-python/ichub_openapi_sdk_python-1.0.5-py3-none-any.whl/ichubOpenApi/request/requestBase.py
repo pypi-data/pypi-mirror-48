76cf120898e34a9ceacab8b2998184acbd4bd918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:01 +0000

    Add package ichub-openapi-sdk-python/1.0.5/ichub_openapi_sdk_python-1.0.5-py3-none-any.whl

---
+++ packages/ichub-openapi-sdk-python/ichub_openapi_sdk_python-1.0.5-py3-none-any.whl/ichubOpenApi/request/requestBase.py
@@ -0,0 +1,35 @@
+import requests
+from ichubOpenApi import const
+from ..sign.Passport import Identify
+import json
+
+
+class requestBase:
+
+    def __init__(self, host=None, v='1.0.0', app_id='', sign_type='', key='', public_key='', private_key=''):
+        self.v = v
+        self.verify = Identify(sign_type, key, public_key, private_key)
+        self.app_id = app_id
+        self.sign_type = sign_type
+        self.host = host if host is not None else const.HOST
+
+    def send_request(self, params=None, filter_param=None):
+        params['timestamp'] = const.TIMESTAMP
+        params['v'] = self.v
+        params['app_id'] = self.app_id
+        params['sign_type'] = self.sign_type
+        build_params = dict.copy(params)
+
+        if filter_param is not None:
+            for i in filter_param:
+                del build_params[i]
+
+        sign = self.verify.buildRequestMysign(build_params)
+        params['sign'] = sign
+        try:
+            r = requests.post(self.host, data=params)
+            r = r.text
+            result = json.loads(r)
+            return result
+        except Exception as e:
+            return False
