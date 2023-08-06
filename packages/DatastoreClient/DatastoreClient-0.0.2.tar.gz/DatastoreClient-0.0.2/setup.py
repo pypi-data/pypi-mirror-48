5f843ae2f7ebe5bd298a2b1b666e5f0726c8ef90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:57 +0000

    Add package DatastoreClient/0.0.2/DatastoreClient-0.0.2.tar.gz

---
+++ packages/DatastoreClient/DatastoreClient-0.0.2.tar.gz/DatastoreClient-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DatastoreClient",
+    version="0.0.2",
+    author="TaylorHere",
+    author_email="liyanhong@hz-health.cn",
+    description="Python Client for Datastore APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
