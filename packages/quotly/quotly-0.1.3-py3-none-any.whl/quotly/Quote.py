e67d7d87d60831524985fa839e45e1ab6ea07ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package quotly/0.1.3/quotly-0.1.3-py3-none-any.whl

---
+++ packages/quotly/quotly-0.1.3-py3-none-any.whl/quotly/Quote.py
@@ -0,0 +1,23 @@
+class Quote:
+    def __init__(self, quote, *targets):
+        self.quote = quote
+        self.targets = []
+
+        for t in targets:
+            if len(t) > 0:
+                self.targets.append(t[0])
+
+    def has_target(self, target):
+        return target in self.targets
+
+    @staticmethod
+    def filter_by_target(seq, target):
+        for elem in seq:
+            if target in elem.targets:
+                yield elem
+
+    @staticmethod
+    def filter_by_targets(seq, targets):
+        for elem in seq:
+            if targets in elem.targets:
+                yield elem
