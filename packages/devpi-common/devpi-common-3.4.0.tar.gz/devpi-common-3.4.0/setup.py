f8d754e68ea67dc7903070070818ddd5636f4399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:15 +0000

    Add package devpi-common/3.4.0/devpi-common-3.4.0.tar.gz

---
+++ packages/devpi-common/devpi-common-3.4.0.tar.gz/devpi-common-3.4.0/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import io
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def get_changelog():
+    text = io.open(os.path.join(here, 'CHANGELOG'), encoding='utf-8').read()
+    header_matches = list(re.finditer('^=+$', text, re.MULTILINE))
+    # until fifth header
+    text = text[:header_matches[5].start()]
+    # all lines without fifth release number
+    lines = text.splitlines()[:-1]
+    return "=========\nChangelog\n=========\n\n" + "\n".join(lines)
+
+
+if __name__ == "__main__":
+    here = os.path.abspath(".")
+    README = io.open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+    CHANGELOG = get_changelog()
+
+    setup(
+      name="devpi-common",
+      description="utilities jointly used by devpi-server and devpi-client",
+      long_description="\n\n".join([README, CHANGELOG]),
+      version='3.4.0',
+      packages=find_packages(),
+      install_requires=["requests>=2.3.0", "py>=1.4.20"],
+      url="https://github.com/devpi/devpi",
+      maintainer="Holger Krekel",
+      maintainer_email="holger@merlinux.eu",
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        ],
+      )
