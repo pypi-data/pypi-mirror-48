86e37e810292a12b0c9e8d66782e31cad959222f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:31 +0000

    Add package autocti/0.11.1/autocti-0.11.1.tar.gz

---
+++ packages/autocti/autocti-0.11.1.tar.gz/autocti-0.11.1/autocti/exc.py
@@ -0,0 +1,20 @@
+from autofit import exc
+
+class CIPatternException(Exception):
+    pass
+
+
+class RegionException(Exception):
+    pass
+
+
+class PlottingException(Exception):
+    pass
+
+
+class FittingException(Exception):
+    pass
+
+
+class PriorException(exc.FitException):
+    pass
