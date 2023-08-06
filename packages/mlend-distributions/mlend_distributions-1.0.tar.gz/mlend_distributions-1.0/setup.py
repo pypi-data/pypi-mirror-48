b6e78667d88c734b1eb17c0676881e1a53b87ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:57 +0000

    Add package mlend-distributions/1.0/mlend_distributions-1.0.tar.gz

---
+++ packages/mlend-distributions/mlend_distributions-1.0.tar.gz/mlend_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mlend_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['mlend_distributions'],
+      author = 'Dhruv Patel',
+      zip_safe=False)
