1e3f1ece2921f7712be9ed1fc512561e9e43d251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:01 +0000

    Add package exsc/1.2/exsc-1.2.tar.gz

---
+++ packages/exsc/exsc-1.2.tar.gz/exsc-1.2/exsc/__init__.py
@@ -0,0 +1,3 @@
+from exsc.ExtendedSubCipher import ExtendedSubCipher
+
+__version__ = "1.2.0"
---
+++ packages/exsc/exsc-1.2.tar.gz/exsc-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="exsc",
+	  version="1.2",
+	  description="Extended substitution cipher",
+	  url="https://github.com/aman-roy/Extended-Substitution-Cipher",
+	  author="Aman Roy",
+	  author_email="royaman8757@gmail.com",
+	  license='MIT',
+	  packages=["exsc"],
+	  long_description="Implementation of Extended Substitution Cipher.",
+	  zip_safe=False)
