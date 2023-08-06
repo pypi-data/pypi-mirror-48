9a9a9c5d7caba3b69ee4cbeef74ea6ba3136a3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:36 +0000

    Add package ogc/0.1.1/ogc-0.1.1.tar.gz

---
+++ packages/ogc/ogc-0.1.1.tar.gz/ogc-0.1.1/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['ogc', 'ogc.api', 'ogc.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'jinja2>=2.10,<3.0',
+ 'juju-wait==2.7.0',
+ 'juju>=0.11.7,<0.12.0',
+ 'kv>=0.3.0,<0.4.0',
+ 'launchpadlib==1.10.6',
+ 'melddict>=1.0,<2.0',
+ 'pyyaml==3.13',
+ 'semver>=2.8,<3.0',
+ 'sh>=1.12,<2.0',
+ 'toml>=0.10.0,<0.11.0']
+
+entry_points = \
+{'console_scripts': ['ogc = ogc:app.start']}
+
+setup_kwargs = {
+    'name': 'ogc',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Adam Stokes',
+    'author_email': 'battlemidget@users.noreply.github.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
