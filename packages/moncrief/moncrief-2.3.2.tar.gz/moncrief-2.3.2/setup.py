5597e8826ba61e6a40c7da6438d3b9b7ee46ba03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package moncrief/2.3.2/moncrief-2.3.2.tar.gz

---
+++ packages/moncrief/moncrief-2.3.2.tar.gz/moncrief-2.3.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+setuptools.setup(
+    name='moncrief',
+    version='2.3.2',
+    url='https://github.com/AlgernonSolutions/algernon',
+    license='GNU Affero General Public License v3.0',
+    author='algernon_solutions/jcubeta',
+    author_email='jcubeta@algernon.solutions',
+    description='This library contains the basic units of functionality and infrastructure needed to effectively run '
+                'operations and applications in a distributed and severless fashion.',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'aws-xray-sdk',
+        'boto3',
+        'botocore',
+        'certifi',
+        'chardet',
+        'docutils',
+        'future',
+        'idna',
+        'jmespath',
+        'jsonpickle',
+        'jsonref',
+        'python-dateutil',
+        'python-rapidjson',
+        'pytz',
+        'requests',
+        's3transfer',
+        'six',
+        'urllib3',
+        'wrapt'
+    ],
+)
