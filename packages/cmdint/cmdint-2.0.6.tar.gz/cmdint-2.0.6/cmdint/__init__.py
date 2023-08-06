31d8ea467203c5fce13549b500b26eaa87e3b084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:24 +0000

    Add package cmdint/2.0.6/cmdint-2.0.6.tar.gz

---
+++ packages/cmdint/cmdint-2.0.6.tar.gz/cmdint-2.0.6/cmdint/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2.0.6"
+__copyright__ = "Copyright 2018, German Cancer Research Center (DKFZ), Division of Medical Image Computing"
+
+from cmdint.CmdInterface import CmdInterface
+from cmdint import Utils
