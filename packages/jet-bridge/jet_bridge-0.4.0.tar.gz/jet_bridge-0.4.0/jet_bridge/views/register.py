de4f9a9523b5dabbec3f265ee7dc4335ba269f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:35 +0000

    Add package jet-bridge/0.4.0/jet_bridge-0.4.0.tar.gz

---
+++ packages/jet-bridge/jet_bridge-0.4.0.tar.gz/jet_bridge-0.4.0/jet_bridge/views/register.py
@@ -0,0 +1,23 @@
+from six.moves.urllib_parse import quote
+
+from jet_bridge import settings
+from jet_bridge.views.base.api import APIView
+
+
+class RegisterHandler(APIView):
+
+    def get(self, *args, **kwargs):
+        token = self.get_argument('token', '')
+
+        if not token:
+            return
+
+        if settings.WEB_BASE_URL.startswith('https') and not self.request.full_url().startswith('https'):
+            web_base_url = 'http{}'.format(settings.WEB_BASE_URL[5:])
+        else:
+            web_base_url = settings.WEB_BASE_URL
+
+        url = '{}/projects/register/{}'.format(web_base_url, token)
+        query_string = 'referrer={}'.format(quote(self.request.full_url().encode('utf8')))
+
+        self.redirect('%s?%s' % (url, query_string))
