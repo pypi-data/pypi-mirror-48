4fd3de382d6b70585a60885adc5057d1397702d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:56 +0000

    Add package pytorchtrainer/0.1.0rc1/pytorchtrainer-0.1.0rc1.tar.gz

---
+++ packages/pytorchtrainer/pytorchtrainer-0.1.0rc1.tar.gz/pytorchtrainer-0.1.0rc1/test/test_callback.py
@@ -0,0 +1,89 @@
+import os
+import shutil
+import unittest
+import torch
+from torch.utils.data import DataLoader
+
+from pytorchtrainer import create_default_trainer
+from pytorchtrainer.callback import ValidationCallback, SaveBestCheckpointCallback, checkpoint, file_writer
+from pytorchtrainer.metric import Loss
+
+from test.common import XorModule, XorDataset
+
+
+class MyValidationCallback(ValidationCallback):
+    def __init__(self, dataset_loader, metric, validate_every):
+        super().__init__(dataset_loader, metric, validate_every=validate_every)
+        self.has_been_called = False
+
+    def __call__(self, trainer):
+        super().__call__(trainer)
+        self.has_been_called = True
+
+
+class MySaveBestCheckpointCallback(SaveBestCheckpointCallback):
+    def __init__(self, state_metric_name, saves_to_keep):
+        super().__init__(state_metric_name, saves_to_keep=saves_to_keep)
+        self.has_been_called = False
+
+    def __call__(self, trainer):
+        super().__call__(trainer)
+        self.has_been_called = True
+
+
+class TestCallback(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        torch.manual_seed(42)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+        self.model = XorModule()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-2)
+        self.criterion = torch.nn.MSELoss()
+
+        self.train_loader = DataLoader(XorDataset(),
+                                       batch_size=1,
+                                       num_workers=1
+                                       )
+
+    def tearDown(self):
+        super().tearDown()
+        del self.model, self.optimizer, self.criterion, self.train_loader
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(os.path.join(checkpoint.default_save_diretory), ignore_errors=True)
+        shutil.rmtree(os.path.join(file_writer.default_save_directory), ignore_errors=True)
+
+    def test_validation(self):
+        validation_callback = MyValidationCallback(self.train_loader, Loss(self.criterion), validate_every=1)
+
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.register_post_iteration_callback(validation_callback)
+        trainer.register_post_iteration_callback(file_writer.CsvWriter(save_every=1,
+                                                                       extra_header=[validation_callback.state_attribute_name],
+                                                                       callback=lambda state: [state.get(validation_callback.state_attribute_name)]))
+        trainer.register_post_epoch_callback(validation_callback)
+        trainer.train(self.train_loader, max_epochs=5)
+
+        self.assertTrue(validation_callback.has_been_called)
+        self.assertTrue(trainer.state.last_validation_loss != float('inf'))
+
+    def test_save_best(self):
+        validation_callback = ValidationCallback(self.train_loader, Loss(self.criterion), validate_every=1)
+        callback = MySaveBestCheckpointCallback(validation_callback.state_attribute_name, saves_to_keep=1)
+
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.register_post_epoch_callback(validation_callback)
+        trainer.register_post_epoch_callback(callback)
+        trainer.train(self.train_loader, max_epochs=10)
+
+        self.assertTrue(callback.has_been_called)
+
---
+++ packages/pytorchtrainer/pytorchtrainer-0.1.0rc1.tar.gz/pytorchtrainer-0.1.0rc1/test/test_trainer.py
@@ -0,0 +1,128 @@
+import os
+import shutil
+import unittest
+import torch
+from torch.utils.data import DataLoader
+
+from pytorchtrainer import create_default_trainer, ModuleTrainer
+from pytorchtrainer.callback import checkpoint, file_writer, MetricCallback, ValidationCallback
+from pytorchtrainer.metric import Accuracy, Loss
+
+from test.common import XorModule, XorDataset
+
+
+class TestTrainer(unittest.TestCase):
+
+    def setUp(self):
+        super().setUp()
+        torch.manual_seed(42)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+        self.model = XorModule()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-2)
+        self.criterion = torch.nn.MSELoss()
+
+        self.train_loader = DataLoader(XorDataset(),
+                                       batch_size=1,
+                                       num_workers=1
+                                       )
+
+        self.prediction_transform = lambda x: x.round()
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(os.path.join(checkpoint.default_save_diretory), ignore_errors=True)
+        shutil.rmtree(os.path.join(file_writer.default_save_directory), ignore_errors=True)
+        del self.model, self.optimizer, self.criterion, self.train_loader
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+    def _evaluate_model(self, model, train_loader):
+        # evaluate trained model
+        model.eval()
+        with torch.no_grad():
+            for i, batch in enumerate(train_loader, 0):
+                x, y = batch[0], batch[1]
+                y_pred = model(x)
+                print("x=%s, y=%s" % (x, y_pred.item()))
+                self.assertEqual(self.prediction_transform(y_pred), int(y.item()))
+
+    def test_xor(self):
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.train(self.train_loader, max_epochs=100)
+
+        self._evaluate_model(self.model, self.train_loader)
+
+    def test_dtype(self):
+        for dtype in [torch.float32, torch.float64]:
+            trainer = create_default_trainer(self.model, self.optimizer, self.criterion, dtype=dtype)
+            trainer.train(self.train_loader, max_epochs=5)
+            del trainer
+
+    def test_checkpoint_save(self):
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.register_post_epoch_callback(checkpoint.SaveCheckpointCallback(save_every=1))
+        trainer.train(self.train_loader, max_epochs=1)
+
+        self.assertTrue(os.path.exists(os.path.join(checkpoint.default_save_diretory, checkpoint.default_filename)))
+
+    def test_checkpoint_load(self):
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.register_post_epoch_callback(checkpoint.SaveCheckpointCallback(save_every=1))
+        trainer.train(self.train_loader, max_epochs=1)
+
+        self.assertTrue(os.path.exists(os.path.join(checkpoint.default_save_diretory, checkpoint.default_filename)))
+
+        del trainer
+
+        global has_callback_been_called
+        has_callback_been_called = False
+
+        # load from checkpoint
+        def _callback(trainer: ModuleTrainer):
+            from pytorchtrainer.stop_condition import EarlyStopping
+            trainer.stop_condition = EarlyStopping()
+            trainer.optimizer.lr_scheduler = 1e-3
+
+            global has_callback_been_called
+            has_callback_been_called = True
+
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion,
+                                         init_callback=checkpoint.LoadCheckpointCallback(callback=_callback))
+
+        self.assertTrue(has_callback_been_called)
+
+        trainer.train(self.train_loader, max_epochs=2)
+
+        self.assertEqual(2, trainer.state.current_epoch)
+
+    def test_log_save(self):
+        writer = file_writer.CsvWriter(save_every=1, extra_header=['test'], callback=lambda trainer: [42])
+
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+        trainer.register_post_epoch_callback(writer)
+        trainer.train(self.train_loader, max_epochs=10)
+
+        self.assertTrue(os.path.exists(writer.log_file_path))
+
+    def test_progressbar_extra_metric(self):
+        trainer = create_default_trainer(self.model, self.optimizer, self.criterion)
+
+        # Validation callback
+        trainer.register_post_epoch_callback(ValidationCallback(self.train_loader, metric=Loss(self.criterion), validate_every=1))
+
+        # Accuracy
+        trainer.register_post_iteration_callback(MetricCallback(
+            metric=Accuracy(prediction_transform=self.prediction_transform),
+            frequency=1))
+
+        trainer.add_progressbar_metric("validation loss %.4f | accuracy %.2f", ["last_validation_loss", "accuracy"])
+
+        trainer.train(self.train_loader, max_epochs=10)
