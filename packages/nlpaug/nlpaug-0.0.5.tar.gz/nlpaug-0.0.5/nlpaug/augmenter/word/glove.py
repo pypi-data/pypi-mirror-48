6030a2bb4d80c68c6d55dcdae4ed655ec73ad510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:57 +0000

    Add package nlpaug/0.0.5/nlpaug-0.0.5.tar.gz

---
+++ packages/nlpaug/nlpaug-0.0.5.tar.gz/nlpaug-0.0.5/nlpaug/augmenter/word/glove.py
@@ -0,0 +1,42 @@
+import re, os
+import numpy as np
+from random import randint
+
+from nlpaug.augmenter.word import WordEmbsAugmenter
+from nlpaug.util import Action
+import nlpaug.model.word_embs as nmw
+
+GLOVE_MODEL = {}
+
+
+def init_glove_model(model_path, force_reload=False):
+    """
+        Load model once at runtime
+    """
+    global GLOVE_MODEL
+    if GLOVE_MODEL and not force_reload:
+        return GLOVE_MODEL
+
+    glove = nmw.GloVe()
+    glove.read(model_path)
+    GLOVE_MODEL = glove
+
+    return GLOVE_MODEL
+
+
+class GloVeAug(WordEmbsAugmenter):
+    def __init__(self, model_path='.', model=None, action=Action.SUBSTITUTE,
+                 name='GloVe_Aug', aug_min=1, aug_p=0.3, aug_n=5, tokenizer=None, stopwords=[], force_reload=False,
+                 verbose=0):
+        super(GloVeAug, self).__init__(
+            model_path=model_path, aug_n=aug_n,
+            action=action, name=name, aug_p=aug_p, aug_min=aug_min, tokenizer=tokenizer, stopwords=stopwords,
+            verbose=verbose)
+
+        if model is None:
+            self.model = self.get_model(force_reload=force_reload)
+        else:
+            self.model = model
+
+    def get_model(self, force_reload=False):
+        return init_glove_model(self.model_path, force_reload)
---
+++ packages/nlpaug/nlpaug-0.0.5.tar.gz/nlpaug-0.0.5/nlpaug/model/word_stats/word_statistics.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn import preprocessing
+
+
+class WordStatistics:
+    def __init__(self, cache=True):
+        self.cache = cache
+
+    def train(self, data):
+        raise NotYetImplemented()
+
+    def predict(self, data):
+        raise NotYetImplemented()
