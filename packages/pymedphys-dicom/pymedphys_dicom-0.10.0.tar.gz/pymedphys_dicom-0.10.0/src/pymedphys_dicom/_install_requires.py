6ffd9fafacee5f72fcae18fdcceb2e6464f0fec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:20 +0000

    Add package pymedphys-dicom/0.10.0/pymedphys_dicom-0.10.0.tar.gz

---
+++ packages/pymedphys-dicom/pymedphys_dicom-0.10.0.tar.gz/pymedphys_dicom-0.10.0/src/pymedphys_dicom/_install_requires.py
@@ -0,0 +1,8 @@
+install_requires = [
+    "matplotlib",
+    "numpy >= 1.12",
+    "packaging",
+    "pydicom",
+    "pymedphys_base >= 0.10.0, < 0.11.0",
+    "pymedphys_utilities >= 0.10.0, < 0.11.0"
+]
