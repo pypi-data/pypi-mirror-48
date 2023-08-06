ff158434b6ac0eb8551d81ffc6660b79e814bf6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:53 +0000

    Add package buglib/0.4.0/buglib-0.4.0.tar.gz

---
+++ packages/buglib/buglib-0.4.0.tar.gz/buglib-0.4.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='buglib',
+    version='0.4.0',
+    author='zhangligang',
+    author_email='394452216@qq.com',
+    url='http://www.tdace.com',
+    description=u'Collect traceback to file or server',
+    packages=['buglib'],
+    install_requires=[],
+    entry_points={
+    }
+)
