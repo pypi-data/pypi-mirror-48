a64b8baf56f764114d8a05154f0f1ecf353024f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:36 +0000

    Add package dataclasses-jsonschema/2.4.0/dataclasses-jsonschema-2.4.0.tar.gz

---
+++ packages/dataclasses-jsonschema/dataclasses-jsonschema-2.4.0.tar.gz/dataclasses-jsonschema-2.4.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+requires = [
+    'python-dateutil',
+    'jsonschema',
+    'mypy_extensions',
+    'typing_extensions',
+    'dataclasses;python_version<"3.7"'
+]
+
+
+def read(f):
+    return open(f, encoding='utf-8').read()
+
+
+setup(
+    name='dataclasses-jsonschema',
+    description='JSON schema generation from dataclasses',
+    long_description=read('README.rst'),
+    packages=['dataclasses_jsonschema'],
+    package_data={'dataclasses_jsonschema': ["py.typed"]},
+    author='Simon Knibbs',
+    author_email='simon.knibbs@gmail.com',
+    url='https://github.com/s-knibbs/dataclasses-jsonschema',
+    install_requires=requires,
+    extras_require={
+        'fast-validation': ["PyValico>=0.0.1"]
+    },
+    setup_requires=['pytest-runner', 'setuptools_scm'],
+    tests_require=['pytest', 'flake8', 'mypy'],
+    license='MIT',
+    use_scm_version=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
