6b014d99de6aed7be92032e52ab1527c847c483f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:02 +0000

    Add package multiverseML/0.1.7a0/multiverseML-0.1.7a0.tar.gz

---
+++ packages/multiverseML/multiverseML-0.1.7a0.tar.gz/multiverseML-0.1.7a0/multiverseml/__init__.py
@@ -0,0 +1,1 @@
+from .multiverseml import *
---
+++ packages/multiverseML/multiverseML-0.1.7a0.tar.gz/multiverseML-0.1.7a0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='multiverseML',
+    version='0.1.7-Alpha',
+    url='https://github.com/cccadet/multiverseML/',
+    license='GNU General Public License v3.0',
+    author='Cristian Carlos dos Santos',
+    author_email='perestra.ccds@gmail.com',
+    keywords='multiverseML, machine-learning, pipeline',
+    description=u'Aplicação para facilitar a criação de pipelines para desenvovimento de algoritmos de Machine Learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['multiverseml'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'flask'
+    ],
+)
