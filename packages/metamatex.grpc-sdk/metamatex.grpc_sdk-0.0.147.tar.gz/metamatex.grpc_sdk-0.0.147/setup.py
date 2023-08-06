9dfd29ec53507df30efaa840656c632685b3ce5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:27 +0000

    Add package metamatex.grpc-sdk/0.0.147/metamatex.grpc_sdk-0.0.147.tar.gz

---
+++ packages/metamatex.grpc-sdk/metamatex.grpc_sdk-0.0.147.tar.gz/metamatex.grpc_sdk-0.0.147/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='metamatex.grpc_sdk',
+    packages=['metamatex.grpc_sdk'],
+    version='0.0.147',
+    description='',
+    author='',
+    author_email='',
+    include_package_data=True,
+    url='https://github.com/metamatex/grpc-sdk',
+    download_url='',
+    keywords=[],
+    package_data={'metamatex.grpc_sdk':['*', '*/*', '*/*/*']},
+    install_requires=[
+        'googleapis-common-protos',
+        'grpcio-tools'
+    ],
+)
