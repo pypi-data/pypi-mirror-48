c5e02fc56d2d3fa84933f70b1cec33bfdd9ea8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package pydrobert-pytorch/0.0.2/pydrobert-pytorch-0.0.2.tar.gz

---
+++ packages/pydrobert-pytorch/pydrobert-pytorch-0.0.2.tar.gz/pydrobert-pytorch-0.0.2/pydrobert/torch/util.py
@@ -0,0 +1,60 @@
+# Copyright 2019 Sean Robertson
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+'''Utility functions'''
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+
+__author__ = "Sean Robertson"
+__email__ = "sdrobert@cs.toronto.edu"
+__license__ = "Apache 2.0"
+__copyright__ = "Copyright 2019 Sean Robertson"
+__all__ = [
+    'optimizer_to',
+]
+
+
+def optimizer_to(optimizer, to):
+    '''Move tensors in an optimizer to another device
+
+    This function traverses the state dictionary of an `optimizer` and pushes
+    any tensors it finds to the device implied by `to` (as per the semantics
+    of ``torch.tensor.to``)
+    '''
+    state_dict = optimizer.state_dict()
+    key_stack = [(x,) for x in state_dict.keys()]
+    new_device = False
+    while key_stack:
+        last_dict = None
+        val = state_dict
+        keys = key_stack.pop()
+        for key in keys:
+            last_dict = val
+            val = val[key]
+        if isinstance(val, torch.Tensor):
+            try:
+                last_dict[keys[-1]] = val.to(to)
+                if last_dict[keys[-1]].device != val.device:
+                    new_device = True
+            except Exception as e:
+                print(e)
+                pass
+        elif hasattr(val, 'keys'):
+            key_stack += [keys + (x,) for x in val.keys()]
+    if new_device:
+        optimizer.load_state_dict(state_dict)
