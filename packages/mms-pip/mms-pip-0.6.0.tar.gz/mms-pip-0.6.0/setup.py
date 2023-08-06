31f36bbb917caabf0633abb0d716db07661b0b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:23 +0000

    Add package mms-pip/0.6.0/mms-pip-0.6.0.tar.gz

---
+++ packages/mms-pip/mms-pip-0.6.0.tar.gz/mms-pip-0.6.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+required_packages=[
+    'google-cloud-logging>=1.10.0',
+    'redis>=3.0.1',
+]
+
+setuptools.setup(
+    name="mms-pip",
+    version="0.6.0",
+    author="Josef Goppold",
+    author_email="goppold@mediamarktsaturn.com",
+    description="A custom MMS/GDWH module for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://source.cloud.google.com/v135-5683-alice-ksk-explore/mms-pip",
+    packages=setuptools.find_packages(),
+    install_requires=required_packages,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
