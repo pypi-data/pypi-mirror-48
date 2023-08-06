ba79d7ea7ced862d0a1a9db70ea273be04d8529a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:54 +0000

    Add package numerai-reports/0.2.2/numerai_reports-0.2.2-py3-none-any.whl

---
+++ packages/numerai-reports/numerai_reports-0.2.2-py3-none-any.whl/numerai_reports/test.py
@@ -0,0 +1,14 @@
+import data
+import reports
+
+
+lb = data.fetch_leaderboard(56, 100)
+print(lb[lb.username == "uuazed2"].iloc[0])
+
+print(reports.payments(lb, ['uuazed2']))
+print(reports.payments(lb, ['uuazed', 'uuazed2', 'uuazed3']))
+print(reports.payments(lb, ['anna1', 'anna2', 'anna3']))
+print(reports.payments(lb, ['uuazed', 'uuazed2', 'uuazed3', 'anna1', 'anna2', 'anna3']))
+
+
+print(reports.reputation_bonus(162))
