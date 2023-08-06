c9dd93f5b03f747cc30e2dd92edbcb0dad42972c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package pyevents21/0.1/pyevents21-0.1.tar.gz

---
+++ packages/pyevents21/pyevents21-0.1.tar.gz/pyevents21-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyevents21',
+    version='0.1',
+    description='Pre Handle Events System',
+    url='http://sweet_smoke21.github.io/pyevents21',
+    author='Anosov Igor',
+    author_email='AnosovIgor404@gmail.com',
+
+    long_description='Read \\examples',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires='',
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
