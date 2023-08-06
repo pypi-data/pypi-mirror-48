2fc585fa720656c47c41d9b78d9b285502cb5f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:54 +0000

    Add package pwnedapi/1.0.2/pwnedapi-1.0.2-py2.py3-none-any.whl

---
+++ packages/pwnedapi/pwnedapi-1.0.2-py2.py3-none-any.whl/pwnedapi/Password.py
@@ -0,0 +1,89 @@
+"""Password"""
+
+import requests
+import hashlib
+
+from pwnedapi.__version__ import __version__
+from pwnedapi.exceptions.PasswordException import PasswordException
+from pwnedapi.exceptions.RequestException import RequestException
+from typing import Dict
+
+
+class Password:
+    """This class represents a password.
+    It does all the work of talking to the
+    Pwned Passwords API to find out if the
+    password has been pwned."""
+
+    # Endpoint for the HIBP API v2
+    API_URL = "https://api.pwnedpasswords.com/range/"
+
+    # Currently the API desires first 5 characters
+    HASH_PREFIX_LENGTH = 5
+
+    # Hashed need to be encoded before sending
+    HASH_ENCODING = "utf-8"
+
+    USER_AGENT = "pwnedapi v{}".format(__version__)
+
+    DEFAULT_REQUEST_HEADERS = {"User-Agent": USER_AGENT}
+
+    def __init__(self,
+                 password: str,
+                 request_headers: Dict[str, str] = {},
+                 read_timeout: int = 10) -> None:
+
+        if not isinstance(password, str):
+            raise PasswordException("Password must be a string.")
+
+        self.password = password
+        self.DEFAULT_REQUEST_HEADERS.update(request_headers)
+        self.request_headers = self.DEFAULT_REQUEST_HEADERS
+        self.read_timeout = read_timeout
+
+        self.hashed_password = self.hash_password()
+        self.pwned_count = 0
+
+    def get_value(self) -> str:
+        return self.password
+
+    def hash_password(self) -> str:
+        h = hashlib.sha1()
+        h.update(self.password.encode(self.HASH_ENCODING))
+
+        return h.hexdigest().upper()
+
+    def is_pwned(self) -> bool:
+        self.pwned_count = self.fetch_pwned_count()
+
+        return self.pwned_count > 0
+
+    def fetch_pwned_count(self) -> int:
+        hash_list = self.make_request().text.split("\r\n")
+
+        for hash in hash_list:
+            if hash.startswith(self.hashed_password_suffix()):
+                return int(hash.split(":")[1])
+
+        return 0
+
+    def make_request(self) -> requests.Response:
+        url = self.API_URL + self.hashed_password_prefix()
+
+        try:
+            response = requests.get(
+                url, headers=self.request_headers, timeout=self.read_timeout)
+        except requests.exceptions.Timeout:
+            raise RequestException("API request timed out.")
+        except requests.exceptions.ConnectionError:
+            raise RequestException("API request failed.")
+        if response.status_code != 200 or not response.content:
+            raise RequestException("API request failed.")
+
+        return response
+
+    def hashed_password_prefix(self) -> str:
+        return self.hashed_password[:self.HASH_PREFIX_LENGTH]
+
+    def hashed_password_suffix(self) -> str:
+        return self.hashed_password[self.HASH_PREFIX_LENGTH:]
---
+++ packages/pwnedapi/pwnedapi-1.0.2-py2.py3-none-any.whl/pwnedapi/__main__.py
@@ -0,0 +1,32 @@
+import click
+
+from .Password import Password
+from .utils import Scanner
+
+
+@click.group()
+def cli() -> None:
+    """Program cli."""
+    pass  # pragma: no cover
+
+
+@cli.command()
+@click.password_option(help="Password, which will be checked.")
+def check(password: str) -> None:
+    "Checks a single password if it has been pwned."
+    password_inst = Password(password)
+    if password_inst.is_pwned():
+        print("Your password has been pwned {} times.".format(
+            password_inst.pwned_count))
+        if password_inst.pwned_count == 0:
+            print("Your password is safe.")
+
+
+@cli.command()
+@click.argument('INPUT_FILE', type=click.File('rb'))
+@click.option('-o', '--output-format', help='Output data format.', default='csv')
+def scan(input_file: click.File, output_format: str = 'csv') -> None:
+    "Scan a file for pwned passwords."
+    scanner = Scanner()
+    scanner.scan(input_file.name)
+    print(scanner.export(output_format))
