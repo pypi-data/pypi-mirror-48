080a7669fa711beab1a6dd0fecd95749cfcd8a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:47 +0000

    Add package DocuScan/0.0.1.7.2/DocuScan-0.0.1.7.2.tar.gz

---
+++ packages/DocuScan/DocuScan-0.0.1.7.2.tar.gz/DocuScan-0.0.1.7.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DocuScan",
+    version="0.0.1.7.2",
+    author="Alex Scotland",
+    author_email="rocketleaguemarket@gmail.com",
+    description="Added Requirements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mutster/DocuScan-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
