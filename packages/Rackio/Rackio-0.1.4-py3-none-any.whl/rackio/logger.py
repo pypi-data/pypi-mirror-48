17bf28fe4196e900bb5d828f8a95c43b7dd98399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package Rackio/0.1.4/Rackio-0.1.4-py3-none-any.whl

---
+++ packages/Rackio/Rackio-0.1.4-py3-none-any.whl/rackio/logger.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""rackio/logger.py
+
+This module implements a sqlite database logger for the CVT instance, 
+will create a time-serie for each tag in a short memory sqlite data base.
+"""
+
+import threading
+import pickledb
+
+from datetime import datetime
+
+from ._singleton import Singleton
+
+
+class TagLogger:
+
+    """CVT Tag Logger class.
+
+    This class is intended to be an API for tags 
+    settings and tags logged access.
+
+    # Example
+    
+    ```python
+    >>> from rackio.logger import TagLogger
+    >>> _logger = TagLogger()
+    ```
+    
+    """
+    
+    def __init__(self, dbfile="tags.db", memory=None):
+
+        self._dbfile = dbfile
+        self._memory = memory
+
+        self._period = None
+
+    def set_dbfile(self, dbfile):
+
+        self._dbfile = dbfile
+
+    def set_db(self):
+
+        self._db = pickledb.load(self._dbfile, False) 
+
+    def set_tag(self, tag):
+
+        # self._db.set(tag, list())
+        
+        waveform = dict()
+        waveform["values"] = list()
+        waveform["dt"] = self._period
+        waveform["t0"] = None
+
+        # self._db.set(tag, list())
+        self._db.set(tag, waveform)
+        self._db.dump()
+
+    def write_tag(self, tag, value):
+
+        # values = self._db.get(tag)
+        # values.append(value)
+        # self._db.set(tag, values)
+        # self._db.dump()
+
+        waveform = self._db.get(tag)
+
+        values = waveform["values"]
+
+        if not values:
+            
+            dt = self._period
+            t0 = datetime.now()
+            
+            waveform["dt"] = dt
+            waveform["t0"] = t0
+
+        values.append(value)
+
+        waveform["values"] = values
+
+        self._db.set(tag, waveform)
+        self._db.dump()
+
+    def read_tag(self, tag):
+
+        result = self._db.get(tag)
+        
+        return result
+
+
+class LoggerEngine(Singleton):
+    """Logger Engine class for Tag thread-safe database logging.
+
+    This class is intended hold in memory tag based values and 
+    observers for those required tags, it is implemented as a singleton
+    so each sub-thread within the Rackio application can access tags
+    in a thread-safe mechanism.
+
+    # Example
+    
+    ```python
+    >>> from rackio.engine import CVTEngine
+    >>> tag_egine = CVTEngine()
+    >>> tag_engine.write_tag("TAG1", 40.43)
+    >>> value = tag_engine.read_tag("TAG1")
+    >>> print(value)
+    40.43
+    ```
+
+    """
+
+    def __init__(self, period=0.5):
+
+        super(LoggerEngine, self).__init__()
+
+        self._logger = TagLogger()
+        self._logging_tags = list()
+
+        self._logger._period = period
+
+        self._request_lock = threading.Lock()
+        self._response_lock = threading.Lock()
+
+        self._response = None
+
+        self._response_lock.acquire()
+
+    def set_db(self, dbfile):
+
+        self._logger.set_dbfile(dbfile)
+
+    def add_tag(self, tag):
+
+        self._logging_tags.append(tag)
+
+    def write_tag(self, tag, value):
+
+        _query = dict()
+        _query["action"] = "write_tag"
+
+        _query["parameters"] = dict()
+        _query["parameters"]["tag"] = tag
+        _query["parameters"]["value"] = value
+
+        self.request(_query)
+        result = self.response()
+
+        return result
+
+    def read_tag(self, tag):
+
+        _query = dict()
+        _query["action"] = "read_tag"
+
+        _query["parameters"] = dict()
+        _query["parameters"]["tag"] = tag
+
+        self.request(_query)
+        result = self.response()
+
+        if result["result"]:
+            return result["response"]
+
+    def request(self, _query):
+
+        self._request_lock.acquire()
+
+        action = _query["action"]
+
+        if action == "write_tag":
+
+            try:
+                parameters = _query["parameters"]
+
+                tag = parameters["tag"]
+                value = parameters["value"]
+
+                self._logger.write_tag(tag, value)
+
+                self._response = {
+                    "result": True
+                }
+            except:
+                self._response = {
+                    "result": False
+                }
+        
+        elif action == "read_tag":
+
+            try:
+
+                parameters = _query["parameters"]
+
+                tag = parameters["tag"]
+
+                result = self._logger.read_tag(tag)
+
+                self._response = {
+                    "result": True,
+                    "response": result
+                }
+            except:
+                self._response = {
+                    "result": False,
+                    "response": None
+                }
+
+        self._response_lock.release()
+
+    def response(self):
+
+        self._response_lock.acquire()
+
+        result = self._response
+
+        self._request_lock.release()
+
+        return result
+        
---
+++ packages/Rackio/Rackio-0.1.4-py3-none-any.whl/rackio/workers.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""rackio/workers.py
+
+This module implements all thread classes for workers.
+"""
+import time
+from threading import Thread
+from wsgiref.simple_server import make_server
+
+from .controls import ControlManager
+from .engine import CVTEngine
+
+class BaseWorker(Thread):
+
+    def  __init__(self):
+
+        super(BaseWorker, self).__init__()
+
+
+class ThreadWorker(BaseWorker):
+
+    def  __init__(self, worker_function, period):
+
+        super(ThreadWorker, self).__init__()
+        self._period = period
+        self._worker_function = worker_function
+
+    def run(self):
+
+        while True:
+
+            time.sleep(self._period)
+
+            self._worker_function()
+
+
+class ControlWorker(BaseWorker):
+
+    def __init__(self, manager):
+
+        super(ControlWorker, self).__init__()
+        
+        self._manager = manager
+        self._manager.attach_all()
+
+    def run(self):
+
+        _queue = self._manager._tag_queue
+
+        while True:
+            
+            time.sleep(0.1)
+
+            if not _queue.empty():
+                item = _queue.get()
+                
+                _tag = item["tag"]
+
+                self._manager.execute(_tag)
+
+
+class AlarmWorker(BaseWorker):
+
+    def __init__(self, manager):
+
+        super(AlarmWorker, self).__init__()
+        
+        self._manager = manager
+        self._manager.attach_all()
+
+    def run(self):
+
+        _queue = self._manager._tag_queue
+
+        while True:
+            
+            time.sleep(0.1)
+
+            if not _queue.empty():
+                item = _queue.get()
+                
+                _tag = item["tag"]
+
+                self._manager.execute(_tag)
+
+
+class _ContinousWorker:
+
+    def __init__(self, f, period=0.5, pause_tag=None, stop_tag=None):
+
+        self._f = f
+        self._period = period
+        self._pause_tag = pause_tag
+        self._stop_tag = stop_tag
+
+        from .core import Rackio
+
+        rackio = Rackio()
+        rackio._continous_functions.append(self)
+    
+    def __call__(self, *args):
+
+        _cvt = CVTEngine()
+
+        time.sleep(self._period)
+
+        while True:
+
+            now = time.time()
+
+            if self._pause_tag:
+                stop = _cvt.read_tag(self._stop_tag)
+
+                if stop:
+                    return
+
+            if self._pause_tag:
+                pause = _cvt.read_tag(self._pause_tag)
+
+                if not pause:
+                    
+                    self._f()
+            else:
+                self._f()
+
+            elapsed = time.time() - now
+
+            time.sleep(self._period - elapsed)
+            
+
+class APIWorker(BaseWorker):
+
+    def __init__(self, app, port=8000):
+
+        super(APIWorker, self).__init__()
+
+        self._api_app = app
+        self._port = port
+
+    def run(self):
+
+        with make_server('', self._port, self._api_app) as httpd:
+            print('Serving on port {}...'.format(self._port))
+
+            # Serve until process is killed
+            httpd.serve_forever()
+
+
+class LoggerWorker(BaseWorker):
+
+    def __init__(self, manager, period=0.5):
+
+        super(LoggerWorker, self).__init__()
+        
+        self._manager = manager
+        self._period = period
+
+    def run(self):
+
+        for _tag in self._manager._logging_tags:
+            self._manager._logger.set_tag(_tag)
+
+        _cvt = CVTEngine()
+
+        time.sleep(self._period)
+
+        while True:
+
+            now = time.time()
+
+            for _tag in self._manager._logging_tags:
+                value = _cvt.read_tag(_tag)
+                self._manager.write_tag(_tag, value)
+            
+            elapsed = time.time() - now
+
+            time.sleep(self._period - elapsed)
