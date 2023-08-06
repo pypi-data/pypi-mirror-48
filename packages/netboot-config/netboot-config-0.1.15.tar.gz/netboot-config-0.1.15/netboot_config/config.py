f521b0258a1fa95dc957e9878a79c2ace87e18d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package netboot-config/0.1.15/netboot-config-0.1.15.tar.gz

---
+++ packages/netboot-config/netboot-config-0.1.15.tar.gz/netboot-config-0.1.15/netboot_config/config.py
@@ -0,0 +1,59 @@
+import ipaddress
+
+import yaml
+
+from .network import HostGroup, Host, SpecificHost
+
+
+class Config(object):
+
+    def __init__(self, config_file: str):
+
+        self.hosts = []
+        self.static_hosts = []
+        self.netboot_host = None
+
+        with open(config_file, 'r') as stream:
+            data_loaded = yaml.safe_load(stream)
+            self.netboot_host = data_loaded['netboot']['host']
+
+            for host_group_entry in data_loaded['hostgroups']:
+                host_group = HostGroup(host_group_entry['prefix'], host_group_entry['cidr'])
+
+                for host_entry in host_group_entry['hosts']:
+                    self.map_entry(host_entry, host_group)
+
+                self.hosts += host_group.hosts()
+
+            for host_group_entry in data_loaded['hosts']:
+                host_group = HostGroup(host_group_entry['prefix'], host_group_entry['cidr'])
+
+                for host_entry in host_group_entry['hosts']:
+                    self.map_entry(host_entry, host_group)
+
+                self.static_hosts += host_group.hosts()
+
+    def map_entry(self, host_entry, host_group):
+        offset_ = host_entry['offset']
+        count_ = host_entry['count'] if 'count' in host_entry else 1
+
+        if 'alias' in host_entry:
+            aliases_ = (host_entry['alias'],)
+        elif 'aliases' in host_entry:
+            aliases_ = tuple(host_entry['aliases'])
+        else:
+            aliases_ = None
+
+        if 'config' in host_entry:
+            config_ = [ (config_entry['source'], config_entry['target']) for config_entry in host_entry['config']]
+        else:
+            config_ = None
+
+        image_type_ = host_entry['image_type'] if 'image_type' in host_entry else None
+
+        host_group.add_hosts(offset_, count_, aliases_, image_type_, config_)
+
+
+    @property
+    def all_hosts(self):
+        return self.hosts + self.static_hosts
---
+++ packages/netboot-config/netboot-config-0.1.15.tar.gz/netboot-config-0.1.15/netboot_config/network.py
@@ -0,0 +1,135 @@
+import ipaddress
+from abc import ABCMeta, abstractmethod
+from typing import List, Tuple, Optional
+
+
+class Host(metaclass=ABCMeta):
+
+    @property
+    @abstractmethod
+    def image_type(self):
+        pass
+
+    @property
+    @abstractmethod
+    def host_name(self):
+        pass
+
+    @property
+    @abstractmethod
+    def ipv4_address(self):
+        pass
+
+    @property
+    @abstractmethod
+    def ipv4_address_hex(self):
+        pass
+
+    @property
+    @abstractmethod
+    def aliases(self):
+        pass
+
+    @property
+    @abstractmethod
+    def entry(self):
+        pass
+
+
+class DefaultHost(Host):
+
+    def __init__(self, image_type):
+        self._image_type = image_type
+
+    @property
+    def image_type(self):
+        return self._image_type
+
+    @property
+    def host_name(self):
+        return None
+
+    @property
+    def ipv4_address(self):
+        return None
+
+    @property
+    def ipv4_address_hex(self):
+        return "default"
+
+    @property
+    def aliases(self):
+        return []
+
+    @property
+    def entry(self):
+        return ""
+
+
+class SpecificHost(Host):
+
+    def __init__(self, name_prefix: str, address: ipaddress.IPv4Address, image_type: Optional[str] = None,
+                 aliases: Optional[List[str]] = None, config: Optional[List[Tuple[str]]] = None, address_digits=2):
+        self.host_name_template = name_prefix + "{:0" + str(address_digits) + "}"
+
+        value = address._ip
+        first = value % 256
+        value //= 256
+        second = value % 256
+        value //= 256
+        third = value % 256
+        value //= 256
+        fourth = value % 256
+        self._address = (fourth, third, second, first)
+
+        self._image_type = image_type
+        self._aliases = tuple(aliases) if aliases is not None else ()
+        self._config = tuple(config) if config is not None else ()
+
+    @property
+    def ipv4_address(self) -> str:
+        return ".".join((str(x) for x in self._address))
+
+    @property
+    def ipv4_address_hex(self) -> str:
+        return "".join(('{:02X}'.format(x) for x in self._address))
+
+    @property
+    def host_name(self):
+        return self.host_name_template.format(self._address[-1])
+
+    @property
+    def image_type(self) -> str:
+        return self._image_type
+
+    @property
+    def aliases(self) -> Tuple[str]:
+        return self._aliases
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def entry(self) -> str:
+        return "{}\t{}".format(self.ipv4_address, " ".join((self.host_name,) + self.aliases))
+
+    def __str__(self):
+        return str(self._address)
+
+
+class HostGroup(object):
+
+    def __init__(self, name_prefix: str, cidr_string: str):
+        self.network = ipaddress.ip_network(cidr_string)
+        self.name_prefix = name_prefix
+        self._hosts = []
+
+    def add_hosts(self, machine_offset: int, machine_count: int, aliases: Optional[List[str]] = None,
+                  image_type: Optional[str] = None, config: Optional[Tuple[str]] = None) -> None:
+        self._hosts += (SpecificHost(self.name_prefix, x, image_type, aliases, config) for i, x in
+                        enumerate(self.network.hosts())
+                        if machine_offset <= i + 1 < machine_offset + machine_count)
+
+    def hosts(self) -> List[Host]:
+        return self._hosts
