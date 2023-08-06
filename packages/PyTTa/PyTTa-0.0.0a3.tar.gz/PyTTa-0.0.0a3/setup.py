ce00f6fc51521d5937f746c73178a5095a99fc0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:45 +0000

    Add package PyTTa/0.0.0a3/PyTTa-0.0.0a3.tar.gz

---
+++ packages/PyTTa/PyTTa-0.0.0a3.tar.gz/PyTTa-0.0.0a3/setup.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: utf-8 -*-
+"""
+PyTTa setup file
+=================
+
+@Autor:
+- João Vitor Gutkoski Paes, joao.paes@eac.ufsm.br
+
+"""
+
+#%%
+from setuptools import setup
+
+settings = {
+    'name': 'PyTTa',
+    'version': '0.0.0a3',
+    'description': 'Signal processing tools for acoustics and vibrations in python.',
+    'url': 'http://github.com/PyTTAmaster/PyTTa/tree/v0.0.0a2',
+    'author': 'Marcos Reis, Matheus Lazarin, João Vitor Paes',
+    'packages': ['pytta'],
+    'author_email': 'joao.paes@eac.ufsm.br',
+    'license': 'MIT',
+    'install_requires': ['numpy','scipy', 'matplotlib','sounddevice'],
+}
+setup(**settings)
