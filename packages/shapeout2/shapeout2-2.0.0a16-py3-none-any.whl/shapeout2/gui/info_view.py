c56cd02190200c59ef287c264ab1221eb660a8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package shapeout2/2.0.0a16/shapeout2-2.0.0a16-py3-none-any.whl

---
+++ packages/shapeout2/shapeout2-2.0.0a16-py3-none-any.whl/shapeout2/gui/info_view.py
@@ -0,0 +1,15 @@
+import pkg_resources
+
+from PyQt5 import uic, QtWidgets
+
+
+class InfoView(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self)
+        path_ui = pkg_resources.resource_filename(
+            "shapeout2.gui", "info_view.ui")
+        uic.loadUi(path_ui, self)
+        self.setWindowTitle("Info View")
+
+    def show(self, path):
+        pass
