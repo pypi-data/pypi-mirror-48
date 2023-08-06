17f1f856d94ba12be2edf7fb887c4a14cad305ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:15 +0000

    Add package jangli/1.1.0/jangli-1.1.0.tar.gz

---
+++ packages/jangli/jangli-1.1.0.tar.gz/jangli-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    description = readme.read()
+
+with open("LICENSE", "r") as readme:
+    license_x = readme.read()
+
+setup(
+    name='jangli',
+    version='1.1.0',
+    packages=['jangli'],
+    url='https://github.com/AbhimanyuHK/Json_Object_Conv',
+    license='MIT License',
+    author='Abimanyu H K',
+    author_email='manyu1994@hotmail.com',
+    description='Convert json to python object and vice versa ',
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
