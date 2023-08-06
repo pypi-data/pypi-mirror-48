e2b27ec2938bd43f8f35aacf03c2d2a1098ba0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:52 +0000

    Add package dagflow/0.0.11/dagflow-0.0.11.tar.gz

---
+++ packages/dagflow/dagflow-0.0.11.tar.gz/dagflow-0.0.11/dagflow/exceptions.py
@@ -0,0 +1,17 @@
+__author__ = 'godq'
+
+
+class PlugInHasExisted(Exception):
+    pass
+
+
+class DagNotFoundInRepo(Exception):
+    pass
+
+
+class StepFailed(Exception):
+    pass
+
+
+class DagHasExisted(Exception):
+    pass
