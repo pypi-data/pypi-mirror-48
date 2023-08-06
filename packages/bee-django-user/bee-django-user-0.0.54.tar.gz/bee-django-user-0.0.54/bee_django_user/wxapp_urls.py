f23c8ffade05afd1aa92e6c82e6a3e663dc9ff7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package bee-django-user/0.0.54/bee-django-user-0.0.54.tar.gz

---
+++ packages/bee-django-user/bee-django-user-0.0.54.tar.gz/bee-django-user-0.0.54/bee_django_user/wxapp_urls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'bee'
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
+
+from . import wxapp
+
+
+urlpatterns = [
+    url(r'^login/$', wxapp.login),
+    url(r'^user/bind$', wxapp.user_bind),
+]
