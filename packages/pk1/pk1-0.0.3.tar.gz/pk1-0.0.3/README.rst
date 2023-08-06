0d341691eb5e871f7d430e6a3ca0ffdae7c4b564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:40:00 +0000

    Add package pk1/0.0.3/pk1-0.0.3.tar.gz

---
+++ packages/pk1/pk1-0.0.3.tar.gz/pk1-0.0.3/README.rst
@@ -0,0 +1,28 @@
+.. image:: pk1/static/logo-large.png
+    :width: 200
+    :alt: Logo
+
+===================
+Introduction
+===================
+PackOne, inspired by the "serverless" trend in cloud computing and big data processing, has the ambitions to bridge the IaaS to Apache Ambari seamlessly and coordinate Ambari Services into a high-level workspace. 
+
+===================
+Install
+===================
+pip install pk1
+
+===================
+Run
+===================
+pk1 start [--listening 127.0.0.1:11001]
+
+===================
+Stop
+===================
+pk1 stop
+
+===================
+Acknowledge
+===================
+National key Research Program of China: Scientific Big Data Management System (No.2016YFB1000600)
---
+++ packages/pk1/pk1-0.0.3.tar.gz/pk1-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pk1",
+    version = "0.0.3",
+    keywords = ("pip", "packone"),
+    description = "PackOne: Pack clouds, engines and data services into one light stack.",
+    long_description = open('README.rst').read(),
+    license = "Apache-2.0 Licence",
+    url = "https://github.com/cas-bigdatalab/packone",
+    author = "Excel Wang",
+    author_email = "wanghj@cnic.com",
+    packages = find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = ['uwsgi','pk1-remedy','django-admin-auto','djangorestframework','djangorestframework-simplejwt', 'django-filter', 'psycopg2-binary', 'coreapi', 'paramiko', 'scp', 'django-cors-headers', 'python-novaclient', 'python-cinderclient', 'django-autocomplete-light'],
+    entry_points = {
+        'console_scripts': [
+            'pk1 = pk1.server:main',
+        ]
+    }
+)
