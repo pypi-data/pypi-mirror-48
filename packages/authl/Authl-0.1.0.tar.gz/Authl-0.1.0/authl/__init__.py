bd954383e07f3a021db95ad22c2b8ac502c92680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:29 +0000

    Add package authl/0.1.0/Authl-0.1.0.tar.gz

---
+++ packages/authl/Authl-0.1.0.tar.gz/Authl-0.1.0/authl/__init__.py
@@ -0,0 +1,81 @@
+""" Authl: A wrapper library to simplify the implementation of federated identity """
+
+
+import requests
+
+
+class Authl:
+    """ Authentication wrapper """
+
+    def __init__(self, handlers=None):
+        """ Initialize an Authl library instance.
+
+        handlers -- a collection of handlers for different authentication
+            mechanisms
+
+        """
+        self._handlers = handlers or []
+
+    def add_handler(self, handler):
+        """ Add another handler to the configured handler list. It will be
+        given the lowest priority. """
+        self._handlers.append(handler)
+
+    def get_handler_for_url(self, url):
+        """ Get the appropriate handler for the specified identity URL.
+        Returns a tuple of (handler, id). """
+        for pos, handler in enumerate(self._handlers):
+            if handler.handles_url(url):
+                return handler, pos
+
+        request = requests.get(url)
+        for pos, handler in enumerate(self._handlers):
+            if handler.handles_page(request.headers, request.text):
+                return handler, pos
+
+        return None, -1
+
+    def get_handler_by_id(self, handler_id):
+        """ Get the handler with the given ID """
+        return self._handlers[handler_id]
+
+    @property
+    def handlers(self):
+        """ get all of the registered handlers, for UX purposes """
+        return [*self._handlers]
+
+
+def from_config(config, secret_key):
+    """ Generate an AUthl handler set from provided configuration directives.
+
+    Arguments:
+
+    config -- a configuration dictionary. See the individual handlers'
+        from_config functions to see possible configuration values.
+    secret_key -- a signing key to use for the handlers which need one
+
+    Handlers will be enabled based on truthy values of the following keys
+
+        TEST_ENABLED -- enable the TestHandler handler
+        EMAIL_FROM -- enable the EmailAddress handler
+        INDIELOGIN_CLIENT_ID -- enable the IndieLogin handler
+
+    """
+
+    handlers = []
+    if config.get('TEST_ENABLED'):
+        from .handlers import test_handler
+
+        handlers.append(test_handler.TestHandler())
+
+    if config.get('EMAIL_FROM'):
+        from .handlers import email_addr
+
+        handlers.append(email_addr.from_config(config, secret_key))
+
+    if config.get('INDIELOGIN_CLIENT_ID'):
+        from .handlers import indielogin
+
+        handlers.append(indielogin.from_config(config))
+
+    return Authl(handlers)
---
+++ packages/authl/Authl-0.1.0.tar.gz/Authl-0.1.0/setup.py
@@ -0,0 +1,63 @@
+"""Setup for AUthl packaging"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+from distutils.util import convert_path
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+main_ns = {}
+ver_path = convert_path('authl/__version__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setup(
+    name='Authl',
+    version=main_ns['__version__'],
+    description='Genericized multi-protocol authentication wrapper',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/PlaidWeb/Authl',
+    author='fluffy',
+    author_email='fluffy@beesbuzz.biz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+    ],
+    keywords='authentication openid indieauth login',
+    packages=['authl'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'validate_email',
+        'ska',
+        'expiringdict',
+    ],
+    extras_require={'dev': [
+        'pylint',
+        'twine',
+        'flake8',
+        'flask',
+        'autopep8'
+    ]},
+    project_urls={
+        'Bug Reports': 'https://github.com/PlaidWeb/Authl/issues',
+        'Source': 'https://github.com/PlaidWeb/Authl/',
+        'Discord': 'https://beesbuzz.biz/discord',
+        'Funding': 'https://liberapay.com/fluffy',
+    },
+    python_requires='>=3.5',
+)
