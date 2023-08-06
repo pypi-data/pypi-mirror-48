c2f324a388ff471c8cf9ab104f4bf97e60780687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:19 +0000

    Add package smart-integration-utils/1.1/smart-integration-utils-1.1.tar.gz

---
+++ packages/smart-integration-utils/smart-integration-utils-1.1.tar.gz/smart-integration-utils-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='smart-integration-utils',
+    version='1.1',
+    install_requires = [
+        'Django>=2.0.7',
+            'djangorestframework>=3.8.2',
+            'psycopg2-binary>=2.7.4',
+            'pytz==2018.4',
+            'requests>=2.18.2',
+            'six==1.11.0',
+            'eventmonitoring-client',
+            'config_field',
+    ],
+)
