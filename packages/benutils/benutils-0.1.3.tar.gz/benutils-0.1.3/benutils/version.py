c279fd82a7fe29f348478cbd6781066f5b3f0e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:11 +0000

    Add package benutils/0.1.3/benutils-0.1.3.tar.gz

---
+++ packages/benutils/benutils-0.1.3.tar.gz/benutils-0.1.3/benutils/version.py
@@ -0,0 +1,7 @@
+""" benutils/version.py """
+__version__ = "0.1.3"
+
+# 0.1.0a0 (20/02/2018): Initial version
+# 0.1.1   (27/02/2018): Add classes of handler for data logging mechanism
+# 0.1.2   (13/03/2019): Add initial condition to 'sdr files'
+# 0.1.3   (19/03/2019): Moves to PyQt5
