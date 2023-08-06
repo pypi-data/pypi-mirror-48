5e589398f7c41480190cb2ff32f7806a1bc4a84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:01 +0000

    Add package partd/1.0.0/partd-1.0.0.tar.gz

---
+++ packages/partd/partd-1.0.0.tar.gz/partd-1.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='partd',
+      version='1.0.0',
+      description='Appendable key-value storage',
+      url='http://github.com/dask/partd/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='',
+      packages=['partd'],
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      extras_require={'complete': [
+          'numpy >= 1.9.0',
+          'pandas >=0.19.0',
+          'pyzmq',
+          'blosc',
+      ]},
+      zip_safe=False)
