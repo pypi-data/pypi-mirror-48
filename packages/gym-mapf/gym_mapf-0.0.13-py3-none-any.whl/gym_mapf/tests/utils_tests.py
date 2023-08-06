de63b085d6c436a5f64fd1e555717c57c54f6491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:49 +0000

    Add package gym-mapf/0.0.13/gym_mapf-0.0.13-py3-none-any.whl

---
+++ packages/gym-mapf/gym_mapf-0.0.13-py3-none-any.whl/gym_mapf/tests/utils_tests.py
@@ -0,0 +1,53 @@
+import unittest
+
+from gym_mapf.envs.utils import create_mapf_env
+from gym_mapf.envs import integer_to_vector
+from gym_mapf.envs.mapf_env import ACTIONS, UP, RIGHT, DOWN, LEFT, STAY
+
+
+class UtilsTest(unittest.TestCase):
+    def test_create_mapf_env(self):
+        empty_8_8_1 = create_mapf_env(map_name='empty-8-8',
+                                      scen_id=1,
+                                      n_agents=2,
+                                      right_fail=0.1,
+                                      left_fail=0.1,
+                                      reward_of_clash=-1000.0,
+                                      reward_of_goal=100.0,
+                                      reward_of_living=0.0)
+
+        self.assertEqual(empty_8_8_1.s, ((0, 0), (5, 3)))
+
+        empty_48_48_16 = create_mapf_env(map_name='empty-48-48',
+                                         scen_id=16,
+                                         n_agents=2,
+                                         right_fail=0.1,
+                                         left_fail=0.1,
+                                         reward_of_clash=-1000.0,
+                                         reward_of_goal=100.0,
+                                         reward_of_living=0.0)
+
+        self.assertEqual(empty_48_48_16.s, ((40, 42), (17, 2)))
+
+    def test_integer_to_vector(self):
+        # state in a 4x4 grid for a single agent.
+        self.assertEqual(integer_to_vector(10, 4, 2, lambda n: n), (2, 2))
+
+        # action for 3 agents
+        self.assertEqual(integer_to_vector(28, 5, 3, lambda n: ACTIONS[n]), (LEFT, UP, RIGHT))
+
+        # state in a 4x3 grid for two agents.
+        self.assertEqual(integer_to_vector(10, 4 * 3, 2, lambda n: (int(n / 3), n % 3)),
+                         ((3, 1), (0, 0)))
+        self.assertEqual(integer_to_vector(13, 4 * 3, 2, lambda n: (int(n / 3), n % 3)),
+                         ((0, 1), (0, 1)))
+        self.assertEqual(integer_to_vector(14, 4 * 3, 2, lambda n: (int(n / 3), n % 3)),
+                         ((0, 2), (0, 1)))
+        self.assertEqual(integer_to_vector(23, 4 * 3, 2, lambda n: (int(n / 3), n % 3)),
+                         ((3, 2), (0, 1)))
+        self.assertEqual(integer_to_vector(143, 4 * 3, 2, lambda n: (int(n / 3), n % 3)),
+                         ((3, 2), (3, 2)))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
