09550ee28c8981f5deaf5f1362ba873bde8e3056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:02 +0000

    Add package ipygee/0.0.6/ipygee-0.0.6.tar.gz

---
+++ packages/ipygee/ipygee-0.0.6.tar.gz/ipygee-0.0.6/ipygee/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+""" A set of tools for working with Google Earth Engine Python API in Jupyter
+notebooks """
+
+from ._version import __version__
+from .map import Map
+from .assets import AssetManager
+from .tasks import TaskManager
+from . import chart
+from .eprint import eprint, set_eprint_async, getInfo
