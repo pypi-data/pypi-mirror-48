f6a27d057acdfa7847b292fc402f0af8c5ca478a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:38 +0000

    Add package atomtools/1.5.6/atomtools-1.5.6-py3.6.egg

---
+++ packages/atomtools/atomtools-1.5.6-py3.6.egg/atomtools/__init__.py
@@ -0,0 +1,16 @@
+"""
+independent chemical symbols
+"""
+
+
+__version__ = '1.5.6'
+def version():
+    return __version__
+
+from . import name, unit, geo
+from . import file, string, system
+
+from .status import Status
+
+
+
