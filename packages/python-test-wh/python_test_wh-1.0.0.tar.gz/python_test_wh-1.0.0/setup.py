8b558ca9a1c17def8c1ce9a3b87bef885d3308a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:53 +0000

    Add package python-test-wh/1.0.0/python_test_wh-1.0.0.tar.gz

---
+++ packages/python-test-wh/python_test_wh-1.0.0.tar.gz/python_test_wh-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+
+setup(
+        name  = "python_test_wh",
+        version   =  "1.0.0",
+        py_modules =['python_test_wh'],
+        author ='wangheng',
+        author_email = 'a2397017b@163.com',
+        url   =  'http://www.headfirstlabs.com',
+        description = 'nothing',
+
+    )
