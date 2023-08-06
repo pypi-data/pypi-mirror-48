bd310d807dab19f539d45a25ab3b8b24d3bd8820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:43 +0000

    Add package xskillscore/0.0.4/xskillscore-0.0.4.tar.gz

---
+++ packages/xskillscore/xskillscore-0.0.4.tar.gz/xskillscore-0.0.4/xskillscore/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+from .core.deterministic import mae, mse, pearson_r, pearson_r_p_value, rmse
+from .core.probabilistic import xr_crps_ensemble as crps_ensemble
+from .core.probabilistic import xr_crps_gaussian as crps_gaussian
+from .core.probabilistic import \
+    xr_threshold_brier_score as threshold_brier_score
+from .core.accessor import XSkillScoreAccessor
