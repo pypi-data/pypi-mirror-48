c217e6a247e7ebf66b5bce9030e789b588302f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:55 +0000

    Add package wilson/1.8/wilson-1.8.tar.gz

---
+++ packages/wilson/wilson-1.8.tar.gz/wilson-1.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+with open('wilson/_version.py') as f:
+    exec(f.read())
+
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(name='wilson',
+      version=__version__,
+      author='Jason Aebischer, David M. Straub',
+      author_email='jason.aebischer@tum.de, david.straub@tum.de',
+      url='https://github.com/wilson-eft/wilson',
+      description='Toolkit for effective field theories beyond the Standard Model',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      license='MIT',
+      packages=find_packages(),
+      package_data={
+      'wilson': ['run/smeft/tests/data/*.*',
+                 'data/tests/*.*',
+                 'data/*.*',
+                 ],
+      },
+      install_requires=['scipy>=1.0', 'numpy', 'pylha>=0.2', 'pyyaml',
+                        'ckmutil>=0.3', 'wcxf>=1.6.1', 'rundec>=0.5',
+                        'voluptuous'],
+      extras_require={
+            'testing': ['nose',],
+      },
+    )
---
+++ packages/wilson/wilson-1.8.tar.gz/wilson-1.8/wilson/translate/__init__.py
@@ -0,0 +1,136 @@
+"""Provides basis translators for SMEFT and and WET that can be used with the
+`wcxf` Python package."""
+
+
+from . import smeft
+from . import wet
+import wcxf
+
+
+@wcxf.translator('SMEFT', 'Warsaw', 'Warsaw mass')
+def warsaw_to_warsawmass(C, scale, parameters, sectors=None):
+    return smeft.warsaw_to_warsawmass(C, sectors)
+
+
+@wcxf.translator('SMEFT', 'Warsaw', 'Warsaw up')
+def warsaw_to_warsaw_up(C, scale, parameters, sectors=None):
+    return smeft.warsaw_to_warsaw_up(C, sectors)
+
+
+@wcxf.translator('SMEFT', 'Warsaw up', 'Warsaw')
+def warsaw_up_to_warsaw(C, scale, parameters, sectors=None):
+    return smeft.warsaw_up_to_warsaw(C, sectors)
+
+
+@wcxf.translator('WET', 'flavio', 'JMS')
+def flavio_to_JMS(C, scale, parameters, sectors=None):
+    return wet.flavio_to_JMS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-4', 'flavio', 'JMS')
+def flavio_to_JMS_wet4(C, scale, parameters, sectors=None):
+    return wet.flavio_to_JMS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-3', 'flavio', 'JMS')
+def flavio_to_JMS_wet3(C, scale, parameters, sectors=None):
+    return wet.flavio_to_JMS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'JMS', 'flavio')
+def JMS_to_flavio(C, scale, parameters, sectors=None):
+    return wet.JMS_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-4', 'JMS', 'flavio')
+def JMS_to_flavio_wet4(C, scale, parameters, sectors=None):
+    return wet.JMS_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-3', 'JMS', 'flavio')
+def JMS_to_flavio_wet3(C, scale, parameters, sectors=None):
+    return wet.JMS_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'Bern', 'flavio')
+def Bern_to_flavio(C, scale, parameters, sectors=None):
+    return wet.Bern_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'flavio', 'Bern')
+def flavio_to_Bern(C, scale, parameters, sectors=None):
+    return wet.flavio_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-4', 'Bern', 'flavio')
+def Bern_to_flavio_wet4(C, scale, parameters, sectors=None):
+    return wet.Bern_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-4', 'flavio', 'Bern')
+def flavio_to_Bern_wet4(C, scale, parameters, sectors=None):
+    return wet.flavio_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-3', 'Bern', 'flavio')
+def Bern_to_flavio_wet3(C, scale, parameters, sectors=None):
+    return wet.Bern_to_flavio(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-3', 'flavio', 'Bern')
+def flavio_to_Bern_wet3(C, scale, parameters, sectors=None):
+    return wet.flavio_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'JMS', 'EOS')
+def JMS_to_EOS(C, scale, parameters, sectors=None):
+    return wet.JMS_to_EOS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'JMS', 'Bern')
+def JMS_to_Bern(C, scale, parameters, sectors=None):
+    return wet.JMS_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-4', 'JMS', 'Bern')
+def JMS_to_Bern_wet4(C, scale, parameters, sectors=None):
+    return wet.JMS_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET-3', 'JMS', 'Bern')
+def JMS_to_Bern_wet3(C, scale, parameters, sectors=None):
+    return wet.JMS_to_Bern(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'Bern', 'JMS')
+def Bern_to_JMS(C, scale, parameters, sectors=None):
+    return wet.Bern_to_JMS(C, scale, parameters, sectors)
+
+@wcxf.translator('WET-4', 'Bern', 'JMS')
+def Bern_to_JMS_wet4(C, scale, parameters, sectors=None):
+    return wet.Bern_to_JMS(C, scale, parameters, sectors)
+
+@wcxf.translator('WET-3', 'Bern', 'JMS')
+def Bern_to_JMS_wet3(C, scale, parameters, sectors=None):
+    return wet.Bern_to_JMS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'JMS', 'formflavor')
+def JMS_to_FormFlavor(C, scale, parameters, sectors=None):
+    return wet.JMS_to_FormFlavor(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'FlavorKit', 'JMS')
+def FlavorKit_to_JMS(C, scale, parameters, sectors=None):
+    return wet.FlavorKit_to_JMS(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'JMS', 'FlavorKit')
+def JMS_to_FlavorKit(C, scale, parameters, sectors=None):
+    return wet.JMS_to_FlavorKit(C, scale, parameters, sectors)
+
+
+@wcxf.translator('WET', 'FlavorKit', 'flavio')
+def FlavorKit_to_flavio(C, scale, parameters, sectors=None):
+    C_JMS = wet.FlavorKit_to_JMS(C, scale, parameters, sectors)
+    return wet.JMS_to_flavio(C_JMS, scale, parameters, sectors)
