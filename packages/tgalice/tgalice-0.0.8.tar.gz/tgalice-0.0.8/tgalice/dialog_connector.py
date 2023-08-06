b40bcea55bdcfe70f7d546707f0e3e9243f8fbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:43:03 +0000

    Add package tgalice/0.0.8/tgalice-0.0.8.tar.gz

---
+++ packages/tgalice/tgalice-0.0.8.tar.gz/tgalice-0.0.8/tgalice/dialog_connector.py
@@ -0,0 +1,107 @@
+import copy
+import telebot
+
+from .session_storage import BaseStorage
+
+
+class SOURCES:
+    TELEGRAM = 'telegram'
+    ALICE = 'alice'
+    TEXT = 'text'
+    unknown_source_error_message = 'Source must be on of {"telegram", "alice", "text"}'
+
+
+class DialogConnector:
+    COMMAND_EXIT = 'exit'
+    """ This class provides unified interface for both Telegram and Alice applications """
+    def __init__(self, dialog_manager, storage=None, default_source='telegram', tg_suggests_cols=1):
+        self.dialog_manager = dialog_manager
+        self.default_source = default_source
+        self.storage = storage or BaseStorage()
+        self.tg_suggests_cols = tg_suggests_cols
+
+    def respond(self, message, source=None):
+        # todo: support different triggers - not only messages, but calendar events as well
+        if source is None:
+            source = self.default_source
+        user_id, message_text, metadata = self.standardize_input(source, message)
+        user_object = self.get_user_object(user_id)
+        old_user_object = copy.deepcopy(user_object)
+        updated_user_object, response_text, suggests, response_commands = self.dialog_manager.respond(
+            user_object, message_text, metadata
+        )
+        # todo: execute response_commands
+        if updated_user_object != old_user_object:
+            self.set_user_object(user_id, updated_user_object)
+        response = self.standardize_output(source, message, response_text, response_commands, suggests)
+        return response
+
+    def get_user_object(self, user_id):
+        if self.storage is None:
+            return {}
+        return self.storage.get(user_id)
+
+    def set_user_object(self, user_id, user_object):
+        if self.storage is None:
+            raise NotImplementedError()
+        self.storage.set(user_id, user_object)
+
+    def standardize_input(self, source, message):
+        metadata = {}
+        if source == SOURCES.TELEGRAM:
+            user_id = source + '__' + str(message.from_user.id)
+            message_text = message.text
+        elif source == SOURCES.ALICE:
+            user_id = source + '__' + message['session']['user_id']
+            message_text = message['request']['original_utterance']
+            metadata['new_session'] = message.get('session', {}).get('new', False)
+        elif source == SOURCES.TEXT:
+            user_id = '0'
+            message_text = message
+        else:
+            raise ValueError(SOURCES.unknown_source_error_message)
+        return user_id, message_text, metadata
+
+    def standardize_output(self, source, original_message, response_text, response_commands=None, suggests=None):
+        has_exit_command = False
+        if response_commands:
+            for command in response_commands:
+                if command == self.COMMAND_EXIT:
+                    has_exit_command = True
+                else:
+                    raise NotImplementedError('Command "{}" is not implemented'.format(command))
+        else:
+            response_commands = []
+        if source == SOURCES.TELEGRAM:
+            response = {
+                'text': response_text
+            }
+            if suggests:
+                # todo: do smarter row width calculation
+                row_width = min(self.tg_suggests_cols, len(suggests))
+                response['reply_markup'] = telebot.types.ReplyKeyboardMarkup(row_width=row_width)
+                response['reply_markup'].add(*[telebot.types.KeyboardButton(t) for t in suggests])
+            else:
+                response['reply_markup'] = telebot.types.ReplyKeyboardRemove(selective=False)
+            return response
+        elif source == SOURCES.ALICE:
+            response = {
+                "version": original_message['version'],
+                "session": original_message['session'],
+                "response": {
+                    "end_session": has_exit_command,
+                    "text": response_text
+                }
+            }
+            if suggests:
+                response['response']['buttons'] = [{'title': suggest, 'hide': True} for suggest in suggests]
+            return response
+        elif source == SOURCES.TEXT:
+            response = response_text
+            if suggests is not None and len(suggests) > 0:
+                response = response + '\n' + ', '.join(['[{}]'.format(s) for s in suggests])
+            if response_commands is not None and len(response_commands) > 0:
+                response = response + '\n' + ', '.join(['{{{}}}'.format(c) for c in response_commands])
+            return response, has_exit_command
+        else:
+            raise ValueError(SOURCES.unknown_source_error_message)
---
+++ packages/tgalice/tgalice-0.0.8.tar.gz/tgalice-0.0.8/tgalice/dialog_manager/faq.py
@@ -0,0 +1,70 @@
+import random
+import yaml
+
+from collections import Iterable
+
+from ..nlu import basic_nlu
+from ..nlu.matchers import make_matcher
+from .base import CascadableDialogManager
+
+
+class FAQDialogManager(CascadableDialogManager):
+    """ This dialog manager tries to match the input message with one of the questions from its config,
+    and if successful, gives the corresponding answer. """
+    def __init__(self, config, matcher='tf-idf', *args, **kwargs):
+        super(FAQDialogManager, self).__init__(*args, **kwargs)
+        if isinstance(config, str):
+            with open(config, 'r', encoding='utf-8') as f:
+                self._cfg = yaml.load(f)
+        elif isinstance(config, Iterable):
+            self._cfg = config
+        else:
+            raise ValueError('Config must be a filename or a list.')
+        if isinstance(matcher, str):
+            matcher = make_matcher(matcher)
+        self.matcher = matcher
+        self._q2i = {}
+        self._i2a = {}
+        self._i2s = {}
+        question_keys = []
+        question_labels = []
+        for i, pair in enumerate(self._cfg):
+            questions = self._extract_string_or_strings(pair, key='q')
+            for q in questions:
+                q2 = self._normalize(q)
+                self._q2i[q2] = i
+                question_keys.append(q2)
+                question_labels.append(i)
+            self._i2a[i] = self._extract_string_or_strings(pair, key='a')
+            self._i2s[i] = self._extract_string_or_strings(pair, key='s', allow_empty=True)
+        self.matcher.fit(question_keys, question_labels)
+
+    def try_to_respond(self, user_object, message_text, metadata):
+        text = self._normalize(message_text)
+        index, score = self.matcher.match(text)
+        if index is None:
+            return None
+        commands = []
+        response = random.choice(self._i2a[index])
+        suggests = self._i2s.get(index, [])
+        return user_object, response, suggests, commands
+
+    @staticmethod
+    def _extract_string_or_strings(data, key, allow_empty=False):
+        if key not in data:
+            if allow_empty:
+                return []
+            raise ValueError('The question "{}" has no "{}" key.'.format(data, key))
+        inputs = data[key]
+        if isinstance(inputs, str):
+            result = [inputs]
+        elif isinstance(inputs, Iterable):
+            if not all(isinstance(i, str) for i in inputs):
+                raise ValueError('The list "{}" does not consist of strings.'.format(inputs))
+            result = inputs
+        else:
+            raise ValueError('The question "{}" is not a string or list.'.format(data))
+        return result
+
+    def _normalize(self, text):
+        return basic_nlu.fast_normalize(text)
