0f1a61cd6d82a39ce8b098983e33513229b7350d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:53 +0000

    Add package baseadmin/1.0.1/baseadmin-1.0.1-py3-none-any.whl

---
+++ packages/baseadmin/baseadmin-1.0.1-py3-none-any.whl/baseadmin/components/system/wifi/__init__.py
@@ -0,0 +1,29 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import os
+from shutil import copyfile
+
+def run(script, *args):
+  script = os.path.join(os.path.dirname(__file__), script)
+  os.system("{0} {1}".format(script, " ".join(args)))
+
+def generate_wpa_supplicant(wifi):
+  try:
+    # make a copy of the initial file as a base for all  updates
+    # this also always keeps the initial/mother network
+    if not os.path.exists("/etc/wpa_supplicant/wpa_supplicant.conf.org"):
+      copyfile(
+        "/etc/wpa_supplicant/wpa_supplicant.conf",
+        "/etc/wpa_supplicant/wpa_supplicant.conf.org"
+      )
+
+    # start with fresh/original file and add all wifi entries
+    copyfile(
+      "/etc/wpa_supplicant/wpa_supplicant.conf.org",
+      "/etc/wpa_supplicant/wpa_supplicant.conf"
+    )
+    for ssid, psk in wifi.items():
+      run("add-wifi.sh", ssid, psk, ssid)
+  except FileNotFoundError:
+    logger.error("file not found...")
