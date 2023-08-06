dbd1e693805f436073399e0c498d2ac3973c2661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:19 +0000

    Add package protowhat/1.9.0/protowhat-1.9.0.tar.gz

---
+++ packages/protowhat/protowhat-1.9.0.tar.gz/protowhat-1.9.0/protowhat/Feedback.py
@@ -0,0 +1,41 @@
+class Feedback:
+    def __init__(self, message, state=None):
+        self.message = message
+        self.highlight = None
+        self.highlighting_disabled = False
+        if state is not None:
+            self.highlight = getattr(state, "highlight", None)
+            self.highlighting_disabled = state.highlighting_disabled
+
+    def _highlight_data(self):
+        if hasattr(self.highlight, "get_position"):
+            return self.highlight.get_position()
+        elif hasattr(self.highlight, "first_token") and hasattr(
+            self.highlight, "last_token"
+        ):
+            # used by pythonwhat
+            # a plugin+register system would be better
+            # if many different AST interfaces exist
+            return {
+                "line_start": self.highlight.first_token.start[0],
+                "column_start": self.highlight.first_token.start[1],
+                "line_end": self.highlight.last_token.end[0],
+                "column_end": self.highlight.last_token.end[1],
+            }
+
+    def get_highlight_data(self):
+        result = None
+        try:
+            if self.highlight is not None and not self.highlighting_disabled:
+                result = self._highlight_data()
+        except:
+            pass
+
+        return result or {}
+
+    def __repr__(self):
+        return "<{} {}>".format(self.__class__.__name__, repr(vars(self)))
+
+
+class InstructorError(Exception):
+    pass
---
+++ packages/protowhat/protowhat-1.9.0.tar.gz/protowhat-1.9.0/protowhat/checks/check_files.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+
+def check_file(
+    state,
+    path,
+    missing_msg="Did you create the file `{}`?",
+    is_dir_msg="Want to check the file `{}`, but found a directory.",
+    parse=True,
+    solution_code=None,
+):
+    """Test whether file exists, and make its contents the student code.
+
+    Note: this SCT fails if the file is a directory.
+    """
+
+    path_obj = Path(path)
+    if not path_obj.exists():
+        state.report(missing_msg.format(path))  # test file exists
+    if path_obj.is_dir():
+        state.report(is_dir_msg.format(path))  # test its not a dir
+
+    code = get_file_content(path_obj)
+
+    sol_kwargs = {"solution_code": solution_code, "solution_ast": None}
+    if solution_code:
+        sol_kwargs["solution_ast"] = (
+            state.parse(solution_code, test=False) if parse else False
+        )
+
+    return state.to_child(
+        append_message="We checked the file `{}`. ".format(path),
+        student_code=code,
+        student_ast=state.parse(code) if parse else False,
+        **sol_kwargs
+    )
+
+
+def has_dir(state, path, incorrect_msg="Did you create a directory `{}`?"):
+    """Test whether a directory exists."""
+    if not Path(path).is_dir():
+        state.report(incorrect_msg.format(path))
+
+    return state
+
+
+# helper functions
+def load_file(relative_path, prefix=""):
+    # the prefix can be partialed
+    # so it's not needed to copy the common part of paths
+    path = Path(prefix, relative_path)
+
+    return get_file_content(path)
+
+
+def get_file_content(path):
+    if not isinstance(path, Path):
+        path = Path(path)
+
+    try:
+        content = path.read_text(encoding="utf-8")
+    except:
+        content = None
+
+    return content
