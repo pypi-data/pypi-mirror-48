e34c79650ed82a86d81f97623e21a48906ec2dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:39:59 +0000

    Add package influx-client/1.9.0/influx-client-1.9.0.tar.gz

---
+++ packages/influx-client/influx-client-1.9.0.tar.gz/influx-client-1.9.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    try:
+        with open('README.md', 'r') as doc:
+            return doc.read()
+    except IOError:
+        return """\
+# influx-client
+
+See [GitHub](https://github.com/AxiomExergy/influx-client) for documentation.
+"""
+
+
+# Used for installing test dependencies directly
+tests_require = [
+    'mock',
+    'nose',
+    'flake8',
+    'coverage<4.1'
+]
+
+setup(
+    name='influx-client',
+    version='1.9.0',
+    description="InfluxDB client",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license="Apache License v2.0",
+    platforms=['any'],
+    author="Jacob Alheid",
+    author_email="shakefu@gmail.com",
+    packages=find_packages(exclude=['test', 'test_*', 'fixtures']),
+    install_requires=[
+        'pytool',
+        'requests',
+        'simplejson',
+        ],
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    # For installing test dependencies directly
+    extras_require={'test': tests_require},
+    keywords=['influx-client', 'database', 'influx', 'influxdb', 'client'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Topic :: Database',
+        ],
+    url="https://github.com/AxiomExergy/influx-client/",
+    project_urls={
+        "Bug Tracker": "https://github.com/AxiomExergy/influx-client/issues",
+        "Documentation": "https://github.com/AxiomExergy/influx-client/",
+        "Source Code": "https://github.com/AxiomExergy/influx-client/",
+    }
+)
