e18f1200314914a4c295039363ec2470aee27f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:51 +0000

    Add package flower-segmentation-tool/0.1.5/flower-segmentation-tool-0.1.5.tar.gz

---
+++ packages/flower-segmentation-tool/flower-segmentation-tool-0.1.5.tar.gz/flower-segmentation-tool-0.1.5/README.md
@@ -0,0 +1,16 @@
+# arnica filter
+
+This is an software application in construction.
+
+Dies ist eine Anwendungssoftware zum Monitoring von Arnika Blumen durch die Auswertung von Luftbildern von Drohnen
+
+todos:
+
+- close option in menu
+- Anmerkungen von Weinacker einarbeiten (v.A. Parameter aus Bilddaten vorschlagen)
+- Modulvorraussetzungen und Fehlermeldungen programmieren
+
+- Softwaretests -> Debugging
+
+- Kommentierung 
+- PEP 8 -> pylind
---
+++ packages/flower-segmentation-tool/flower-segmentation-tool-0.1.5.tar.gz/flower-segmentation-tool-0.1.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flower-segmentation-tool",
+    version="0.1.5",
+    author="Anna Tenberg",
+    author_email="Anna.Tenberg@saturn.uni-freiburg.de",
+    description="A tool to segment yellow flowers in rgb images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AnnaTe/arnica",
+    packages=setuptools.find_packages(exclude=['tests*', 'scripts', 'src', '*.git']),
+    python_requires='>=3.0',
+    install_requires=[
+        'PyQt5.sip',
+        'PyQt5-sip',
+        'PyQt5',
+        'numpy',
+        'opencv-python',
+        'matplotlib',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Intended Audience :: Science/Research",
+    ],
+)
