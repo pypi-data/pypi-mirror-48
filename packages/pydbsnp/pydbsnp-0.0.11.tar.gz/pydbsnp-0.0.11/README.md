e2d362660a7fe9178aacefc8071974f3c72c7ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package pydbsnp/0.0.11/pydbsnp-0.0.11.tar.gz

---
+++ packages/pydbsnp/pydbsnp-0.0.11.tar.gz/pydbsnp-0.0.11/README.md
@@ -0,0 +1,40 @@
+# pydbsnp
+
+Interface with dbSNP VCF data
+
+## Installation
+
+First install the python package via `pip3`
+
+```sh
+pip3 install pydbsnp
+```
+or
+```sh
+pip3 install --user pydbsnp
+```
+
+Once the python package is installed, download and index the dbSBP VCF data:
+
+```sh
+pydbsnp-download
+pydbsnp-index
+```
+
+For hg19/GRCh37 coordinates:
+
+```sh
+pydbsnp-download --reference-build GRCh37
+pydbsnp-index
+```
+
+## Command line usage
+
+```sh
+pydbsnp-query -h
+```
+
+```sh
+pydbsnp-query rs689
+pydbsnp-query --reference-build GRCh37 rs689
+```
---
+++ packages/pydbsnp/pydbsnp-0.0.11.tar.gz/pydbsnp-0.0.11/pydbsnp/query.py
@@ -0,0 +1,70 @@
+#===============================================================================
+# query.py
+#===============================================================================
+
+"""query the dbSNP VCF data"""
+
+
+
+
+# Imports ======================================================================
+
+import re
+
+from argparse import ArgumentParser
+from pysam import TabixFile, VariantFile
+
+from pydbsnp.env import BUILD_TO_VCF, BUILD_TO_RSID
+
+
+
+
+# Constants ====================================================================
+
+COORD_REGEX = re.compile('.+:[0-9]+$')
+RSID_REGEX = re.compile('rs[1-9][0-9]+$')
+
+
+
+
+# Functions ====================================================================
+
+def parse_arguments():
+    parser = ArgumentParser(description='query dbSNP VCF data')
+    parser.add_argument(
+        'variants',
+        nargs='+',
+        metavar='<rsid or chr:pos>',
+        help='variant for which to query database'
+    )
+    parser.add_argument(
+        '-r',
+        '--reference-build',
+        choices=('GRCh37', 'hg19', 'GRCh38', 'hg38'),
+        default='GRCh38',
+        help='reference build for coordinates'
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    print(VariantFile(BUILD_TO_VCF[args.reference_build]).header)
+    vcf = TabixFile(BUILD_TO_VCF[args.reference_build])
+    rsid = TabixFile(BUILD_TO_RSID[args.reference_build])
+    def rsid_to_coordinates(rsid, variant):
+        rs_number = variant.replace('rs', '')
+        for row in rsid.fetch('rs', rs_number):
+            yield row[2], int(row[3])
+    for variant in args.variants:
+        if COORD_REGEX.match(variant):
+            chrom, pos = variant.split(':')
+            pos = int(pos)
+            for row in vcf.fetch(chrom, pos, pos):
+                print(str(row))
+        elif RSID_REGEX.match(variant):
+            for chrom, pos in rsid_to_coordinates(rsid, variant):
+                for row in vcf.fetch(chrom, pos, pos):
+                    print(str(row))
+        else:
+            raise RuntimeError('Improperly formatted query')
