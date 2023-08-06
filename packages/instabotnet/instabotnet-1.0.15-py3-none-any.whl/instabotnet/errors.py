6262e5ffd79c88232895b3b118e57031bf472ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:14 +0000

    Add package instabotnet/1.0.15/instabotnet-1.0.15-py3-none-any.whl

---
+++ packages/instabotnet/instabotnet-1.0.15-py3-none-any.whl/instabotnet/errors.py
@@ -0,0 +1,6 @@
+from .api.instagram_private_api.errors import (
+    ClientError, ClientLoginError, ClientLoginRequiredError,
+    ClientCookieExpiredError, ClientThrottledError, ClientConnectionError,
+    ClientCheckpointRequiredError, ClientChallengeRequiredError,
+    ClientSentryBlockError, ClientReqHeadersTooLargeError,
+)
