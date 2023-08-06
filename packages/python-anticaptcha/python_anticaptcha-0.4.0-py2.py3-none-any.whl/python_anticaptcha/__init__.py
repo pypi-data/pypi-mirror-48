0867ea99411052db22118d8688101c1a7b11056f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:23 +0000

    Add package python-anticaptcha/0.4.0/python_anticaptcha-0.4.0-py2.py3-none-any.whl

---
+++ packages/python-anticaptcha/python_anticaptcha-0.4.0-py2.py3-none-any.whl/python_anticaptcha/__init__.py
@@ -0,0 +1,11 @@
+from .base import AnticaptchaClient
+from .tasks import (
+    NoCaptchaTask, NoCaptchaTaskProxylessTask, ImageToTextTask,
+    FunCaptchaTask, RecaptchaV3TaskProxyless
+)
+from .exceptions import AnticaptchaException
+from .fields import (
+    SimpleText, Image, WebLink, TextInput, Textarea, Checkbox, Select,
+    Radio, ImageUpload
+)
+AnticatpchaException = AnticaptchaException
