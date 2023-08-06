9854986721760b02938c9ab5e716e63545825b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:02 +0000

    Add package cora-ui/0.0.1/cora_ui-0.0.1-py3-none-any.whl

---
+++ packages/cora-ui/cora_ui-0.0.1-py3-none-any.whl/cora_ui/__about__.py
@@ -0,0 +1,8 @@
+"""Package metadata."""
+
+__name__ = 'cora-ui'
+__version__ = '0.0.1'
+__author__ = 'Stephen Vondenstein'
+__email__ = 'stephen@vondenstein.com'
+__description__ = 'Touchscreen interface for Cora virtual assistant'
+__url__ = 'https://heycora.com'
