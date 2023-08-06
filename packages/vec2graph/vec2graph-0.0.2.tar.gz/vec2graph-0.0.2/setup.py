db38cdded50bcc846d60620385a624a9f19c1aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:47 +0000

    Add package vec2graph/0.0.2/vec2graph-0.0.2.tar.gz

---
+++ packages/vec2graph/vec2graph-0.0.2.tar.gz/vec2graph-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vec2graph",
+    version="0.0.2",
+    author="Nadezda Katricheva, Alyaxey Yaskevich, Anastasiya Lisitsina, Tamara Zhordaniya, "
+           "Andrey Kutuzov, Elizaveta Kuzmenko",
+    author_email="andreku@ifi.uio.no",
+    description="Mini-library for producing graph visualizations from embedding models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lizaku/vec2graph",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Utilities"
+    ],
+)
