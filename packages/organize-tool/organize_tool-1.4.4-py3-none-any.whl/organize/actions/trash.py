05ca984165decc904d9d7dfda513e375b918baec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:02 +0000

    Add package organize-tool/1.4.4/organize_tool-1.4.4-py3-none-any.whl

---
+++ packages/organize-tool/organize_tool-1.4.4-py3-none-any.whl/organize/actions/trash.py
@@ -0,0 +1,39 @@
+import logging
+
+from .action import Action
+
+
+class Trash(Action):
+
+    """
+    Move a file into the trash.
+
+    Example:
+        - Move all JPGs and PNGs on the desktop which are older than one year
+          into the trash:
+
+          .. code-block:: yaml
+            :caption: config.yaml
+
+            rules:
+              - folders: '~/Desktop'
+              - filters:
+                  - LastModified:
+                      - days: 365
+                  - Extension:
+                      - png
+                      - jpg
+              - actions:
+                  - Trash
+    """
+
+    def __init__(self):
+        self.log = logging.getLogger(__name__)
+
+    def run(self, attrs: dict, simulate: bool):
+        path = attrs['path']
+        from send2trash import send2trash
+        self.print('Trash "%s"' % path)
+        if not simulate:
+            self.log.info('Moving file %s into trash.', path)
+            send2trash(str(path))
