1329ba7ca5517170a2a1e18e97d2d0c3a3ad9245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:03 +0000

    Add package sdc-api/0.2.1/sdc_api-0.2.1.tar.gz

---
+++ packages/sdc-api/sdc_api-0.2.1.tar.gz/sdc_api-0.2.1/sdc_api/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+SDC API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic async wrapper for the SDC API.
+"""
+
+__title__ = 'sdc_api'
+__author__ = 'Tomori Project'
+__version__ = '0.2.1'
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel='beta', serial=0)
