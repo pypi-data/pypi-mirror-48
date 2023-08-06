f8d9ca191d12440c1aecb9ec2bf95ed50ba5ded8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package gzvoc/0.0.3/gzvoc-0.0.3-py3-none-any.whl

---
+++ packages/gzvoc/gzvoc-0.0.3-py3-none-any.whl/gzvoc/gzvoc.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding=utf-8
+name = "gzvoc"
+
+
+def get(src, dst):
+    pass
+
+
+def check():
+    pass
+
+
+def gen(train=0.8, val=0):
+    pass
+
+
+def printoho():
+    print("oho, we have done!")
