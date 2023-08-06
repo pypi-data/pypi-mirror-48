d4cac4a1271a16e21fb6575a2fb2e81b8a94a782
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:27 +0000

    Add package pyApp-SQLAlchemy/1.0.1/pyApp-SQLAlchemy-1.0.1.tar.gz

---
+++ packages/pyApp-SQLAlchemy/pyApp-SQLAlchemy-1.0.1.tar.gz/pyApp-SQLAlchemy-1.0.1/pyapp_ext/sqlalchemy/__init__.py
@@ -0,0 +1,15 @@
+"""
+pyApp - SQLAlchemy Extension
+
+This extension provides a factory method for connecting to a SQL RDBMS.
+
+"""
+from .factory import *
+
+
+class Extension:
+    """
+    pyApp SQLAlchemy Extension
+    """
+    default_settings = ".default_settings"
+    checks = ".checks"
