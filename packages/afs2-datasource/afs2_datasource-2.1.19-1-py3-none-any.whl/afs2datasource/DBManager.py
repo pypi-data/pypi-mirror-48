3aac79d9456418f52365f33abdb86dcb247d19cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:27 +0000

    Add package afs2-datasource/2.1.19/afs2_datasource-2.1.19-1-py3-none-any.whl

---
+++ packages/afs2-datasource/afs2_datasource-2.1.19-1-py3-none-any.whl/afs2datasource/DBManager.py
@@ -0,0 +1,245 @@
+# Copyright 2019 WISE-PaaS/AFS
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License. 
+
+import os
+import json
+import pandas as pd
+import afs2datasource.constant as const
+import afs2datasource.utils as utils
+
+class DBManager:
+  def __init__(self, **config):
+    if config:
+      self._get_credential_from_config(config)
+    dataDir = os.getenv('PAI_DATA_DIR', None)
+    if dataDir:
+      self._get_credential_from_env(dataDir)
+    else:
+      raise ValueError('No DB config.')
+    self._status = const.DB_STATUS['DISCONNECTED']
+    self._helper = self._create_helper(self._dbType)
+
+  def _get_credential_from_config(self, config):
+    dataDir = {}
+    db_type = config.get('db_type', None)
+    if db_type == const.DB_TYPE['S3']:
+      endpoint = config.get('endpoint', None)
+      access_key = config.get('access_key', None)
+      secret_key = config.get('secret_key', None)
+      bucket_name = config.get('bucket_name', None)
+      blobList = config.get('blob_list', None)
+      dataDir = {
+        'type': db_type,
+        'data': {
+          'blobList': blobList,
+          'bucketName': bucket_name,
+          'credential': {
+            'accessKey': access_key,
+            'endpoint': endpoint,
+            'secretKey': secret_key
+          }
+        }
+      }
+    elif db_type == const.DB_TYPE['APM']:
+      username = config.get('username', None)
+      password = config.get('password', None)
+      apmUrl = config.get('apmUrl', None)
+      machineIdList = config.get('machineIdList', None)
+      parameterList = config.get('parameterList', None)
+      mongouri = config.get('mongouri', None)
+      timeRange = config.get('timeRange', None)
+      dataDir = {
+        'type': db_type,
+        'data': {
+          'username': username,
+          'password': password,
+          'apmUrl': apmUrl,
+          'machineIdList': machineIdList,
+          'parameterList': parameterList,
+          'timeRange': timeRange,
+          'credentials': {
+            'uri': mongouri
+          }
+        }
+      }
+    else:
+      username = config.get('username', None)
+      password = config.get('password', None)
+      host = config.get('host', None)
+      port = config.get('port', None)
+      database = config.get('database', None)
+      querySql = config.get('querySql', None)
+      collection = config.get('collection', None)
+      dataDir = {
+        'type': db_type,
+        'data': {
+          'collection': collection,
+          'querySql': querySql,
+          'credential': {
+            'username': username,
+            'password': password,
+            'host': host,
+            'port': port,
+            'database': database
+          }
+        }
+      }
+    os.environ['PAI_DATA_DIR'] = json.dumps(dataDir)
+
+  def _get_credential_from_env(self, dataDir):
+    if type(dataDir) is str:
+      dataDir = json.loads(dataDir)
+    dbType = dataDir.get('type', None)
+    if dbType is None:
+      raise AttributeError('No type in dataDir')
+    if dbType not in const.DB_TYPE.values():
+      raise ValueError('{0} is not support'.format(dbType))
+        
+    self._status = const.DB_STATUS['DISCONNECTED']
+    self._helper = self._create_helper(dbType)
+    self._dbType = dbType
+
+  def _create_helper(self, dbType):
+    dbType = dbType.lower()
+    if dbType == const.DB_TYPE['MONGODB']:
+      import afs2datasource.mongoHelper as mongoHelper
+      return mongoHelper.MongoHelper()
+    elif dbType == const.DB_TYPE['POSTGRES']:
+      import afs2datasource.postgresHelper as postgresHelper
+      return postgresHelper.PostgresHelper()
+    elif dbType == const.DB_TYPE['INFLUXDB']:
+      import afs2datasource.influxHelper as influxHelper
+      return influxHelper.InfluxHelper()
+    elif dbType == const.DB_TYPE['S3']:
+      import afs2datasource.s3Helper as s3Helper
+      return s3Helper.s3Helper()
+    elif dbType == const.DB_TYPE['APM']:
+      import afs2datasource.apmDSHelper as apmDSHelper
+      return apmDSHelper.APMDSHelper()
+    else:
+      return None
+
+  def connect(self):
+    try:
+      if self.is_connected() or self.is_connecting():
+        return
+      self._status = const.DB_STATUS['CONNECTING']
+      self._helper.connect()
+      self._status = const.DB_STATUS['CONNECTED']
+    except Exception as ex:
+      self._status = const.DB_STATUS['DISCONNECTED']
+      raise ex
+
+  def disconnect(self):
+    if self.is_connected():
+      self._helper.disconnect()
+      self._status = const.DB_STATUS['DISCONNECTED']      
+
+  def is_connected(self):
+    return self._status == const.DB_STATUS['CONNECTED']
+  
+  def is_connecting(self):
+    return self._status == const.DB_STATUS['CONNECTING']
+
+  def get_dbtype(self):
+    return self._dbType
+  
+  def execute_query(self):
+    data = utils.get_data_from_dataDir()
+    if not self.is_connected():
+      raise RuntimeError('No connection.')
+    query = None
+    if self._dbType == const.DB_TYPE['S3']:
+      query = data.get('blobList', [])
+    elif self._dbType == const.DB_TYPE['APM']:
+      query = {
+        'machine_list': data.get('machineIdList', []),
+        'parameter_list': data.get('parameterList', []),
+        'time_range': data.get('timeRange', [])
+      }
+    else:
+      query = data.get('querySql', None)
+      if not query and not(type(query) is dict):
+        raise AttributeError('No querySql in dataDir[data]')
+    query = self._helper.check_query(query)
+    return self._helper.execute_query(query)
+
+  def is_table_exist(self, table_name=None):
+    if table_name is None:
+      raise ValueError('table_name is necessary')
+    if not self.is_connected():
+      raise RuntimeError('No connection.')
+    return self._helper.is_table_exist(table_name)
+
+  def is_file_exist(self, table_name='', file_name=''):
+    if self._dbType != const.DB_TYPE['S3']:
+      raise NotImplementedError('{} not implemented.'.format(self._dbType))
+    if not table_name:
+      raise ValueError('table_name is necessary')
+    if not file_name:
+      raise ValueError('file_name is necessary')
+    return self._helper.is_file_exist(table_name, file_name)
+
+  def create_table(self, table_name=None, columns=[]):
+    if table_name is None:
+      raise ValueError('table_name is necessary')
+    if not self.is_connected():
+      raise RuntimeError('No connection.')
+    if self._helper.is_table_exist(table_name):
+      raise ValueError('table_name is exist')
+    columns = list(map(self._check_columns, columns))
+    self._helper.create_table(table_name=table_name, columns=columns)
+
+  def insert(self, table_name=None, columns=(), records=[], source='', destination=''):
+    if not table_name:
+      raise ValueError('table_name is necessary')
+    if not self.is_connected():
+      raise RuntimeError('No connection.')
+    if not self._helper.is_table_exist(table_name) and self._dbType != const.DB_TYPE['INFLUXDB']:
+      raise ValueError('table_name is not exist')
+    if self._dbType == const.DB_TYPE['S3']:
+      if not source or not destination:
+        raise ValueError('source and destination is necessary')
+      if destination.endswith('/'):
+        raise ValueError('destination cannot end with "/"')
+      return self._helper.insert(table_name=table_name, source=source, destination=destination)
+    else:
+      records = [[None if pd.isnull(value) else value for value in record] for record in records]
+      return self._helper.insert(table_name=table_name, columns=columns, records=records)
+
+  # def delete_table(self, table_name=''):
+
+  def delete_file(self, table_name='', file_name=''):
+    if self._dbType != const.DB_TYPE['S3']:
+      raise NotImplementedError('{} not implemented.'.format(self._dbType))
+    if not table_name:
+      raise ValueError('table_name is necessary')
+    if not file_name:
+      raise ValueError('file_name is necessary')
+    if not self.is_file_exist(table_name, file_name):
+      raise FileNotFoundError('{} file is not found'.format(file_name))
+    return self._helper.delete_file(table_name, file_name)
+
+  def _check_columns(self, col):
+    if 'name' not in col:
+      raise ValueError('name is necessary in {}'.format(col))
+    if 'type' not in col:
+      raise ValueError('type is necessary in {}'.format(col))
+    return {
+      'name': col['name'],
+      'type': col['type'],
+      'is_primary': True if 'is_primary' in col and col['is_primary'] else False,
+      'is_not_null': True if 'is_not_null' in col and col['is_not_null'] else False,
+    }
---
+++ packages/afs2-datasource/afs2_datasource-2.1.19-1-py3-none-any.whl/afs2datasource/apmDSHelper.py
@@ -0,0 +1,160 @@
+# Copyright 2019 WISE-PaaS/AFS
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License. 
+
+import json
+import os
+import requests as req
+from urllib.parse import urljoin
+from pymongo import MongoClient, errors, DESCENDING, ASCENDING
+import pandas as pd
+import datetime
+import base64
+
+class APMDSHelper():
+  def __init__(self):
+    self._connection = None
+    self._db = ''
+    dataDir = os.getenv('PAI_DATA_DIR', None)
+    if dataDir is not None:
+      if type(dataDir) is str:
+        dataDir = json.loads(dataDir)
+      self.__login_data = {"userName": dataDir['data']['username'], "password": dataDir['data']['password']}
+      self.apm_url = dataDir['data']['apmUrl']
+      self.machine_list = dataDir['data']['machineIdList']
+      self.parameter_list = dataDir['data']['parameterList']
+      self.__mongo_credentials = dataDir['data']['credentials']['uri']
+      self.time_range = dataDir['data']['timeRange']
+    else:
+      raise AssertionError(
+        "Environment parameters need apm_username={username}, apm_password={password}, apm_url={apmUrl}, machine_id_list={machineIdList}, parameter_list={parameterList}, mongo_uri={mongouri} and time_range={timeRange}".format(
+            username=self.__login_data['userName'], password=self.__login_data['password'], apmUrl=self.apm_url, machineIdList=self.machine_list, parameterList=self.parameter_list, mongouri=self.__mongo_credentials, timeRange=self.time_range)
+      )
+
+  def connect(self):
+    if self._connection is None:
+      self._connection = MongoClient(self.__mongo_credentials)
+      self._db = self.__mongo_credentials.split('/')[-1]
+
+  def disconnect(self):
+    if self._connection:
+      self._connection.close()
+      self._connection = None
+      self._db = ''
+
+  def check_query(self, query):
+    if not query['machine_list']:
+      raise ValueError('machine_list is invalid')
+    if not query['parameter_list']:
+      raise ValueError('parameter_list is invalid')
+    if not query['time_range']:
+      raise ValueError('time_range is invalid')
+    for time in query['time_range']:
+      if time.get('start') is None or time.get('end') is None:
+        raise ValueError('time_range is invalid')
+    return query
+
+  def get_token(self):
+    login_url = urljoin(self.apm_url, '/auth/login')
+    accept_header = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    counts = 0
+    while counts < 5:
+      try:
+        login_information = req.post(login_url, data=json.dumps(self.__login_data), headers=accept_header, timeout=3)
+        if login_information.status_code is 200:
+          return json.loads(login_information.content.decode('UTF-8'))
+      except Exception as e:
+        raise Exception('login failed. error: {}'.format(e))
+    raise Exception('Try SSO Login Failed {} times.'.format(counts))
+
+  def get_machine_detail(self):
+    self.machine_content = []
+    apm_token = self.get_token()
+    get_node_detail_url = urljoin(self.apm_url, '/topo/node/detail/info')
+    header = {'Accept': 'application/json', 'Authorization': 'Bearer ' + apm_token['accessToken']}
+    for mid in self.machine_list:
+      try:
+        machineDetail = req.get(get_node_detail_url, headers=header, params='id='+str(mid))
+        if (machineDetail.status_code == 200):
+          dtInstance = json.loads(machineDetail.text)['dtInstance']
+          self.reorganize_detail(dtInstance)
+        # else: 沒有加進 machine_content有沒有關係?
+      except Exception as e:
+        print('Get Machine {0} detail failed: {1}'.format(min, e))
+
+  def reorganize_detail(self,dtInstance):
+    tags = []
+    compact = []
+    dtFeature = dtInstance['feature']['monitor']
+    dtProperty = dtInstance['property']['iotSense']
+    device = dtProperty['deviceId'].split('@')[-1]
+    for i,e in enumerate(dtFeature):
+      for p in self.parameter_list:
+        if p == e['name']:
+          splitTagList = dtFeature[i]['tag'].split('@')
+          tags.append({'name': dtFeature[i]['name'], 'tag': splitTagList[len(splitTagList)-1]})
+    for t in range(len(tags)):
+      compact.append(device + '\\' + tags[t]['tag'])
+    self.machine_content.append({'tags': tags, 'info': dtProperty, 'deviceCompact': compact})
+
+  async def generate_querySql(self):
+    timeSql = []
+    querySql = []
+    for tr in self.time_range:
+      startTS = datetime.datetime.strptime(tr['start'], '%Y-%m-%d')
+      endTS = datetime.datetime.strptime(tr['end'], '%Y-%m-%d')
+      timeSql.append({'ts': {'$gte': startTS, '$lte': endTS}})
+    for mc in self.machine_content:
+      for d in range(len(mc['deviceCompact'])):
+        querySql.append({'s': mc['info']['groupId'], 't': mc['deviceCompact'][d], '$or': timeSql })
+    return self.execute('scada_HistRawData', querySql)
+
+  async def execute_query(self, query):
+    self.get_machine_detail()
+    return await self.generate_querySql()
+
+  def execute(self, collection, query_sql):
+    data_container = []
+    for sql in range(len(query_sql)):
+      data = list(self._connection[self._db][collection].find(query_sql[sql], {'_id':0, 's':0, 't':0}).sort('ts',ASCENDING))
+      count_data = len(data)
+      data = pd.DataFrame(data=data)
+      if count_data is not 0:
+        data.columns = ['ts', (query_sql[sql]['t'] + '\\' + self.parameter_list[(sql % len(self.parameter_list))])]
+        data_container.append(data)
+    return self.combine_data(data_container)
+
+  def combine_data(self, container):
+    time_stamp_index = None
+    for i, e in enumerate(container):
+      if "Time_Stamp" in e.columns[1] and "Time_Stamp_ms" not in e.columns[1]:
+        self.results = container[i]
+        time_stamp_index = i
+    for contain in range(len(container)):
+      if contain == time_stamp_index:
+        continue
+      self.results = pd.merge(self.results, container[contain], how='left', on='ts')
+    return self.results
+
+  def is_table_exist(self, table_name):
+    raise NotImplementedError('APMDataSource not implement.')
+
+  def is_file_exist(self, table_name, file_name):
+    raise NotImplementedError('APMDataSource not implement.')
+
+  def create_tabe(sef, table_name):
+    raise NotImplementedError('APMDataSource not implement.')
+
+  def insert(table_name, columns, records):
+    raise NotImplementedError('APMDataSource not implement.')
