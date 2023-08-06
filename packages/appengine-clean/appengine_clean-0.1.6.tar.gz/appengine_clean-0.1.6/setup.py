dc61e98c31726e144aa992f48417681624827aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:22 +0000

    Add package appengine-clean/0.1.6/appengine_clean-0.1.6.tar.gz

---
+++ packages/appengine-clean/appengine_clean-0.1.6.tar.gz/appengine_clean-0.1.6/setup.py
@@ -0,0 +1,29 @@
+"""
+Setup script
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.1.6'
+setuptools.setup(
+    name='appengine_clean',
+    version=VERSION,
+    author='David Grant',
+    author_email='davidgrant@gmail.com',
+    description='Clean old AppEngine versions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/poweredbygrow/appengine_clean',
+    packages=['appengine_clean'],
+    classifiers=[
+      'Programming Language :: Python :: 3.7',
+    ],
+    entry_points = {
+      'console_scripts': [
+        'appengine-clean = appengine_clean.appengine_clean:main',                  
+      ],              
+    },
+    keywords=['utility', 'miscellaneous', 'library'],
+)
