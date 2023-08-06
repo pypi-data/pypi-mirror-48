28b4e57d4c5db56744a8b429f01787426577dffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:12 +0000

    Add package redshift-unloader/0.1.4/redshift_unloader-0.1.4-py36-none-any.whl

---
+++ packages/redshift-unloader/redshift_unloader-0.1.4-py36-none-any.whl/redshift_unloader/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="[%(asctime)s / %(name)s] [%(levelname)s] %(message)s"
+)
+
+logger = logging.getLogger("redshift-unloader")
