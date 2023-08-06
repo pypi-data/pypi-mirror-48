c7fd2f7fa00bf2865bbf66025fd38c3db9c1d2b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:37 +0000

    Add package textgraph/1.0.1/textgraph-1.0.1.tar.gz

---
+++ packages/textgraph/textgraph-1.0.1.tar.gz/textgraph-1.0.1/textgraph/__init__.py
@@ -0,0 +1,11 @@
+# coding: UTF-8
+"""
+Python Text Graphing.
+
+Creates text based graphs from integer lists.
+"""
+
+import textgraph.util  # noqa: F401
+from textgraph.spark import spark  # noqa: F401
+from textgraph.horizontal import horizontal  # noqa: F401
+from textgraph.vertical import vertical  # noqa: F401
