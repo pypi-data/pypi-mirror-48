8a84eada1913585d20c8947b242297fe8c39b999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:28 +0000

    Add package open-deployment/0.0.2/open_deployment-0.0.2.tar.gz

---
+++ packages/open-deployment/open_deployment-0.0.2.tar.gz/open_deployment-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name     = 'open_deployment',
+  packages = ['open_deployment'],
+  version  = '0.0.2',
+  license  = 'MIT',
+  description  = 'Open Deployment helps you deploy your ML models by seamlesslly creating APIs, dockerfiles and much more. ',
+  author       = 'Larxel',
+  author_email = 'andrew.mvd@gmail.com',
+  url          = 'https://github.com/Larxel/open_deployment',
+  download_url = 'https://github.com/Larxel/open_deployment/archive/0.0.1.tar.gz',
+  keywords     = ['deployment', 'cloud', 'api'],
+  install_requires = [],
+  classifiers      = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
