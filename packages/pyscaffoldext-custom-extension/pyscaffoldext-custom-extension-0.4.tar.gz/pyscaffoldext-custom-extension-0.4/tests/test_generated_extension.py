96b3b0d4f48b5eee82efe6171bb41ea2b18b2cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:06 +0000

    Add package pyscaffoldext-custom-extension/0.4/pyscaffoldext-custom-extension-0.4.tar.gz

---
+++ packages/pyscaffoldext-custom-extension/pyscaffoldext-custom-extension-0.4.tar.gz/pyscaffoldext-custom-extension-0.4/tests/test_generated_extension.py
@@ -0,0 +1,22 @@
+from os.path import exists as path_exists
+
+from pyscaffold.api import create_project
+from pyscaffold.cli import parse_args, process_opts
+from pyscaffold.utils import chdir
+
+
+def test_generated_extension(tmpfolder, venv_run):
+    args = ["--custom-extension", "pyscaffoldext-some_extension"]
+
+    opts = parse_args(args)
+    opts = process_opts(opts)
+    create_project(opts)
+    with chdir("pyscaffoldext-some_extension"):
+        assert '' == venv_run("flake8")
+        venv_run("python setup.py install")
+
+    venv_run("putup --some-extension the_actual_project")
+    assert path_exists("the_actual_project/setup.cfg")
+
+    with chdir("the_actual_project"):
+        assert '' == venv_run("flake8")
