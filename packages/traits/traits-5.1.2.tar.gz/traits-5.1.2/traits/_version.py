0c62055191b94a744d2188aadf220b0fe6288691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:31 +0000

    Add package traits/5.1.2/traits-5.1.2.tar.gz

---
+++ packages/traits/traits-5.1.2.tar.gz/traits-5.1.2/traits/_version.py
@@ -0,0 +1,8 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '5.1.2'
+full_version = '5.1.2'
+git_revision = 'e62cb0a15195e14f5aa916f941d9a93a488a04fd'
+is_released = True
+
+if not is_released:
+    version = full_version
