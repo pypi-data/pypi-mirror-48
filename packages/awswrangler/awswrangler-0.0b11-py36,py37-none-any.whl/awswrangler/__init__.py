d32ef3233b6941615004631b21b00cabcb689a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:57 +0000

    Add package awswrangler/0.0b11/awswrangler-0.0b11-py36,py37-none-any.whl

---
+++ packages/awswrangler/awswrangler-0.0b11-py36,py37-none-any.whl/awswrangler/__init__.py
@@ -0,0 +1,9 @@
+from awswrangler.__version__ import __title__, __description__, __version__  # noqa
+from awswrangler.session import Session  # noqa
+from awswrangler.pandas import Pandas  # noqa
+from awswrangler.s3 import S3  # noqa
+from awswrangler.athena import Athena  # noqa
+from awswrangler.glue import Glue  # noqa
+from awswrangler.redshift import Redshift  # noqa
+from awswrangler.spark import Spark  # noqa
+import awswrangler.utils  # noqa
