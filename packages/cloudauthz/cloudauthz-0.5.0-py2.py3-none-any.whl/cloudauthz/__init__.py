7684ca7579c0a2d5f9d50c5c8b7958d40eebc791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:11 +0000

    Add package cloudauthz/0.5.0/cloudauthz-0.5.0-py2.py3-none-any.whl

---
+++ packages/cloudauthz/cloudauthz-0.5.0-py2.py3-none-any.whl/cloudauthz/__init__.py
@@ -0,0 +1,27 @@
+"""
+Implements means of delegating access to cloud-based resources.
+"""
+
+__version__ = "0.5.0"
+
+from .providers import aws
+from .providers import azure
+from .providers import gcp
+
+
+class CloudAuthz:
+
+    def __init__(self):
+        pass
+
+    def authorize(self, provider, config):
+        if provider.lower() == "aws":
+            authz = aws.Authorize(config)
+        elif provider.lower() == "azure":
+            authz = azure.Authorize(config)
+        elif provider.lower() == "gcp":
+            authz = gcp.Authorize(config)
+            return authz.get_impersonated_credentials()
+        else:
+            raise NotImplementedError("Authorization flow for the provider `{}` is not implemented.".format(provider))
+        return authz.get_credentials()
