3963731658d4152e2f0b7e0502e116683032432e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package cpsk/0.1.8/cpsk-0.1.8.tar.gz

---
+++ packages/cpsk/cpsk-0.1.8.tar.gz/cpsk-0.1.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='cpsk',
+
+    description='an unofficial api for grabbing data from cp.sk',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Adrian Matejov',
+    author_email='a.matejov@centrum.sk',
+    version='0.1.8',
+
+    url='https://github.com/Adman/python-cpsk-api',
+
+    install_requires=['requests', 'lxml'],
+    include_package_data=True,
+    packages=['cpsk'],
+
+    license="The MIT License (MIT)",
+    keywords=['travel', 'train', 'bus']
+)
