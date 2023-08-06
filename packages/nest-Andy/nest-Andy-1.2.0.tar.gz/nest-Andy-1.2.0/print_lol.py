13a128002d2009d52b1dc6c3c94caa9c53d6fbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:59 +0000

    Add package nest-Andy/1.2.0/nest-Andy-1.2.0.tar.gz

---
+++ packages/nest-Andy/nest-Andy-1.2.0.tar.gz/nest-Andy-1.2.0/print_lol.py
@@ -0,0 +1,14 @@
+"""This is the standard way to include a multiple-line comment in your code."""
+
+def print_lol(the_list, level=0):
+	"""This function takes one positional argument called "the_list", which is any
+	python list(of-possibly-nested lists). Each data item in the provided list is
+	(recursively) printed to the screen on it's own line."""
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level+1)
+		else:
+			for tab_stop in range(level):
+				print('\t', end = '')
+			print(each_item)
+
