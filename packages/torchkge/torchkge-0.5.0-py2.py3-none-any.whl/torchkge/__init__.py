3c0dc7faa984cabacf913eb30a87bb4841cdaab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package torchkge/0.5.0/torchkge-0.5.0-py2.py3-none-any.whl

---
+++ packages/torchkge/torchkge-0.5.0-py2.py3-none-any.whl/torchkge/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for TorchKGE."""
+
+__author__ = """Armand Boschin"""
+__email__ = 'aboschin@enst.fr'
+__version__ = '0.5.0'
+
+
+from .evaluation.Dissimilarities import l1_dissimilarity
+from .evaluation.Dissimilarities import l2_dissimilarity
+from .evaluation.LinkPrediction import LinkPredictionEvaluator
+
+from .models.Losses import MarginLoss
+from .models.TranslationModels import TransEModel
+from .models.TranslationModels import TransHModel
+from .models.TranslationModels import TransRModel
+from .models.TranslationModels import TransDModel
+
+from .data.KnowledgeGraph import KnowledgeGraph
+
+from .utils import Config
+from .exceptions import NotYetEvaluated
