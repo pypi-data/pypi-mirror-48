633c62495868eef027fb0e3e3c30a7614903112f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:36 +0000

    Add package pyvmath/0.1.3/pyvmath-0.1.3.tar.gz

---
+++ packages/pyvmath/pyvmath-0.1.3.tar.gz/pyvmath-0.1.3/README.md
@@ -0,0 +1,5 @@
+pyvmath
+
+C++ extension Vector Mathematics Package for 3D and 2D games.
+
+
---
+++ packages/pyvmath/pyvmath-0.1.3.tar.gz/pyvmath-0.1.3/vmath_doc_en.h
@@ -0,0 +1,58 @@
+
+PyDoc_STRVAR(vec2_doc, "A 2-D vector in array-of-structures format");
+PyDoc_STRVAR(vec3_doc, "A 3-D vector in array-of-structures format");
+PyDoc_STRVAR(vec4_doc, "A 4-D vector in array-of-structures format");
+PyDoc_STRVAR(quat_doc, "A quaternion in array-of-structures format");
+PyDoc_STRVAR(mat22_doc, "A 2x2 matrix in array-of-structures format");
+PyDoc_STRVAR(mat33_doc, "A 3x3 matrix in array-of-structures format");
+PyDoc_STRVAR(mat44_doc, "A 4x4 matrix in array-of-structures format");
+
+PyDoc_STRVAR(add_doc, "Add two vectors or matrices\n");
+PyDoc_STRVAR(sub_doc, "Sub two vectors or matrices\n");
+
+
+/*
+mul
+div
+recip
+sqrt
+rsqrt
+abs
+max
+maxElem
+min
+minElem
+sum
+dot
+lengthSqr
+length
+normalize
+cross
+lerp
+slerp
+quat_rotation
+quat_rotationX
+quat_rotationY
+quat_rotationZ
+conj
+squad
+rotate
+mat_rotation
+mat_rotationX
+mat_rotationY
+mat_rotationZ
+quat_rotationZYX
+mat_identity
+transpose
+inverse
+orthoInverse
+determinant
+scale
+appendScale
+prependScale
+translation
+lookAt
+perspective
+frustum
+orthographic
+*/
