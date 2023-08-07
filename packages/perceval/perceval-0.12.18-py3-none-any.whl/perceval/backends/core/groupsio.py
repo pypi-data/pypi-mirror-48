d8b94a55b8b347419f64a1b7d5d8c6677ee1a195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:52 +0000

    Add package perceval/0.12.18/perceval-0.12.18-py3-none-any.whl

---
+++ packages/perceval/perceval-0.12.18-py3-none-any.whl/perceval/backends/core/groupsio.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2019 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+#
+# Authors:
+#     Valerio Cosentino <valcos@bitergia.com>
+#
+
+import logging
+import os
+import requests
+from requests.auth import HTTPBasicAuth
+
+from grimoirelab_toolkit.uris import urijoin
+
+from .mbox import MBox, MailingList, CATEGORY_MESSAGE
+from ...backend import (BackendCommand,
+                        BackendCommandArgumentParser)
+from ...errors import BackendError
+from ...utils import DEFAULT_DATETIME
+
+MBOX_FILE = 'messages.zip'
+
+GROUPSIO_URL = 'https://groups.io/'
+GROUPSIO_API_URL = 'https://api.groups.io/v1/'
+
+PER_PAGE = 100
+
+
+logger = logging.getLogger(__name__)
+
+
+class Groupsio(MBox):
+    """Groups.io backend.
+
+    This class allows the fetch the messages of a Groups.io group.
+    Initialize this class passing the name of the group and the
+    directory path where the mbox files will be fetched and
+    stored. The origin of the data will be set to the url of the
+    group on Groups.io.
+
+    In order to get an `api_token`, you should first subscribe to
+    a group via the Groups.io website and then execute the
+    following command:
+    `curl "https://api.groups.io/v1/login"
+          -u 123456:
+          -d "email=<your email address>&password=<your password>"`
+
+    In order to know the group names where your token is subscribed,
+    you can use the following script:
+    https://gist.github.com/valeriocos/676d90c58c56e2b17b882f2603283d39
+
+    :param group_name: Name of the group
+    :param dirpath: directory path where the mboxes are stored
+    :param api_token: Groupsio auth token to access the API
+    :param verify: allows to disable SSL verification
+    :param tag: label used to mark the data
+    :param archive: archive to store/retrieve items
+    """
+    version = '0.1.5'
+
+    CATEGORIES = [CATEGORY_MESSAGE]
+
+    def __init__(self, group_name, dirpath, api_token, verify=True, tag=None, archive=None):
+        url = urijoin(GROUPSIO_URL, 'g', group_name)
+        super().__init__(url, dirpath, tag=tag, archive=archive)
+        self.group_name = group_name
+        self.api_token = api_token
+        self.verify = verify
+
+    def fetch(self, category=CATEGORY_MESSAGE, from_date=DEFAULT_DATETIME):
+        """Fetch the messages from a Groups.io group.
+
+        The method fetches the mbox files from a remote Groups.io group
+        and retrieves the messages stored on them.
+
+        :param category: the category of items to fetch
+        :param from_date: obtain messages since this date
+
+        :returns: a generator of messages
+        """
+        items = super().fetch(category, from_date)
+
+        return items
+
+    def fetch_items(self, category, **kwargs):
+        """Fetch the messages
+
+        :param category: the category of items to fetch
+        :param kwargs: backend arguments
+
+        :returns: a generator of items
+        """
+        from_date = kwargs['from_date']
+
+        logger.info("Looking for messages from '%s' since %s",
+                    self.uri, str(from_date))
+
+        mailing_list = GroupsioClient(self.group_name, self.dirpath,
+                                      self.api_token, self.verify)
+        mailing_list.fetch()
+
+        messages = self._fetch_and_parse_messages(mailing_list, from_date)
+
+        for message in messages:
+            yield message
+
+        logger.info("Fetch process completed")
+
+    @classmethod
+    def has_archiving(cls):
+        """Returns whether it supports archiving items on the fetch process.
+
+        :returns: this backend does not support items archive
+        """
+        return False
+
+    @classmethod
+    def has_resuming(cls):
+        """Returns whether it supports to resume the fetch process.
+
+        :returns: this backend supports items resuming
+        """
+        return True
+
+
+class GroupsioClient(MailingList):
+    """Manage mailing list archives stored by Groups.io.
+
+    This class gives access to remote and local mboxes archives
+    from a mailing list stored by Groups.io. This class also allows
+    to keep them in sync.
+
+    :param group_name: Name of the group
+    :param dirpath: directory path where the mboxes are stored
+    :param api_token: Groupsio auth token to access the API
+    :param verify: allows to disable SSL verification
+    """
+
+    DOWNLOAD_ARCHIVES = 'downloadarchives'
+    GET_SUBSCRIPTIONS = 'getsubs'
+
+    def __init__(self, group_name, dirpath, api_token, verify=True):
+        url = urijoin(GROUPSIO_URL, 'g', group_name)
+        super().__init__(url, dirpath)
+        self.group_name = group_name
+        self.api_token = api_token
+        self.auth = HTTPBasicAuth(self.api_token, '')
+        self.verify = verify
+
+    def fetch(self):
+        """Fetch the mbox files from the remote archiver.
+
+        Stores the archives in the path given during the initialization
+        of this object. Those archives which a not valid extension will
+        be ignored.
+
+        Groups.io archives are returned as a .zip file, which contains
+        one file in mbox format.
+
+        :returns: a list of tuples, storing the links and paths of the
+            fetched archives
+        """
+        logger.info("Downloading mboxes from '%s'", self.uri)
+        logger.debug("Storing mboxes in '%s'", self.dirpath)
+
+        if not os.path.exists(self.dirpath):
+            os.makedirs(self.dirpath)
+
+        group_id = self.__find_group_id()
+
+        url = urijoin(GROUPSIO_API_URL, self.DOWNLOAD_ARCHIVES)
+        payload = {'group_id': group_id}
+        filepath = os.path.join(self.dirpath, MBOX_FILE)
+        success = self._download_archive(url, payload, filepath)
+
+        return success
+
+    def subscriptions(self, per_page=PER_PAGE):
+        """Fetch the groupsio paginated subscriptions for a given token
+
+        :param per_page: number of subscriptions per page
+
+        :returns: an iterator of subscriptions
+        """
+        url = urijoin(GROUPSIO_API_URL, self.GET_SUBSCRIPTIONS)
+        logger.debug("Get groupsio paginated subscriptions from " + url)
+
+        keep_fetching = True
+        payload = {
+            "limit": per_page
+        }
+
+        while keep_fetching:
+            r = self.__fetch(url, payload)
+            response_raw = r.json()
+            subscriptions = response_raw['data']
+            yield subscriptions
+
+            total_subscriptions = response_raw['total_count']
+            logger.debug("Subscriptions: %i/%i" % (response_raw['end_item'], total_subscriptions))
+
+            payload['page_token'] = response_raw['next_page_token']
+            keep_fetching = response_raw['has_more']
+
+    def _download_archive(self, url, payload, filepath):
+        r = requests.get(url, params=payload, auth=self.auth, stream=True, verify=self.verify)
+        try:
+            r.raise_for_status()
+            self._write_archive(r, filepath)
+        except requests.exceptions.HTTPError as e:
+            raise e
+        except OSError as e:
+            logger.warning("Ignoring %s archive due to: %s", self.uri, str(e))
+            return False
+
+        logger.debug("%s archive downloaded and stored in %s", self.uri, filepath)
+
+        return True
+
+    @staticmethod
+    def _write_archive(r, filepath):
+        with open(filepath, 'wb') as fd:
+            fd.write(r.raw.read())
+
+    def __find_group_id(self):
+        """Find the id of a group given its name by iterating on the list of subscriptions"""
+
+        group_subscriptions = self.subscriptions()
+
+        for subscriptions in group_subscriptions:
+            for sub in subscriptions:
+                if sub['group_name'] == self.group_name:
+                    return sub['group_id']
+
+        msg = "Group id not found for group name %s" % self.group_name
+        raise BackendError(cause=msg)
+
+    def __fetch(self, url, payload):
+        """Fetch requests from groupsio API"""
+
+        r = requests.get(url, params=payload, auth=self.auth, verify=self.verify)
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            raise e
+
+        return r
+
+
+class GroupsioCommand(BackendCommand):
+    """Class to run Groupsio backend from the command line."""
+
+    BACKEND = Groupsio
+
+    def _pre_init(self):
+        """Initialize mailing lists directory path"""
+
+        if not self.parsed_args.mboxes_path:
+            base_path = os.path.expanduser('~/.perceval/mailinglists/')
+            dirpath = os.path.join(base_path, GROUPSIO_URL, 'g', self.parsed_args.group_name)
+        else:
+            dirpath = self.parsed_args.mboxes_path
+
+        setattr(self.parsed_args, 'dirpath', dirpath)
+
+    @classmethod
+    def setup_cmd_parser(cls):
+        """Returns the Groupsio argument parser."""
+
+        parser = BackendCommandArgumentParser(cls.BACKEND.CATEGORIES,
+                                              from_date=True,
+                                              token_auth=True)
+
+        # Backend token is required
+        action = parser.parser._option_string_actions['--api-token']
+        action.required = True
+
+        # Optional arguments
+        group = parser.parser.add_argument_group('Groupsio arguments')
+        group.add_argument('--mboxes-path', dest='mboxes_path',
+                           help="Path where mbox files will be stored")
+        group.add_argument('--no-verify', dest='verify',
+                           action='store_false',
+                           help="Value 'True' enable SSL verification")
+
+        # Required arguments
+        parser.parser.add_argument('group_name',
+                                   help="Name of the group on Groups.io")
+
+        return parser
---
+++ packages/perceval/perceval-0.12.18-py3-none-any.whl/perceval/backends/core/jenkins.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2019 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA.
+#
+# Authors:
+#     Alvaro del Castillo <acs@bitergia.com>
+#
+
+import json
+import logging
+
+import requests
+
+from grimoirelab_toolkit.uris import urijoin
+
+from ...backend import (Backend,
+                        BackendCommand,
+                        BackendCommandArgumentParser)
+from ...client import HttpClient
+
+CATEGORY_BUILD = "build"
+SLEEP_TIME = 10
+DETAIL_DEPTH = 1
+
+logger = logging.getLogger(__name__)
+
+
+class Jenkins(Backend):
+    """Jenkins backend for Perceval.
+
+    This class retrieves the builds from a Jenkins site.
+    To initialize this class the URL must be provided.
+    The `url` will be set as the origin of the data.
+
+    :param url: Jenkins url
+    :param tag: label used to mark the data
+    :param archive: archive to store/retrieve items
+    :param blacklist_jobs: exclude the jobs of this list while fetching
+    :param detail_depth: control the detail level of the data returned by the API
+    :param sleep_time: time (in seconds) to sleep in case
+        of connection problems
+    :param archive: collect builds already retrieved from an archive
+    """
+    version = '0.11.2'
+
+    CATEGORIES = [CATEGORY_BUILD]
+
+    def __init__(self, url, tag=None, archive=None,
+                 blacklist_jobs=None, detail_depth=DETAIL_DEPTH, sleep_time=SLEEP_TIME):
+        origin = url
+
+        super().__init__(origin, tag=tag, archive=archive)
+        self.url = url
+        self.sleep_time = sleep_time
+        self.blacklist_jobs = blacklist_jobs
+        self.detail_depth = detail_depth
+
+        self.client = None
+
+    def fetch(self, category=CATEGORY_BUILD):
+        """Fetch the builds from the url.
+
+        The method retrieves, from a Jenkins url, the
+        builds updated since the given date.
+
+        :param category: the category of items to fetch
+
+        :returns: a generator of builds
+        """
+
+        kwargs = {}
+        items = super().fetch(category, **kwargs)
+
+        return items
+
+    def fetch_items(self, category, **kwargs):
+        """Fetch the contents
+
+        :param category: the category of items to fetch
+        :param kwargs: backend arguments
+
+        :returns: a generator of items
+        """
+        logger.info("Looking for projects at url '%s'", self.url)
+
+        nbuilds = 0  # number of builds processed
+        njobs = 0  # number of jobs processed
+
+        projects = json.loads(self.client.get_jobs())
+        jobs = projects['jobs']
+
+        for job in jobs:
+            logger.debug("Adding builds from %s (%i/%i)",
+                         job['url'], njobs, len(jobs))
+
+            try:
+                raw_builds = self.client.get_builds(job['name'])
+            except requests.exceptions.HTTPError as e:
+                if e.response.status_code == 500:
+                    logger.warning(e)
+                    logger.warning("Unable to fetch builds from job %s; skipping",
+                                   job['url'])
+                    continue
+                else:
+                    raise e
+
+            if not raw_builds:
+                continue
+
+            try:
+                builds = json.loads(raw_builds)
+            except ValueError:
+                logger.warning("Unable to parse builds from job %s; skipping",
+                               job['url'])
+                continue
+
+            builds = builds['builds']
+            for build in builds:
+                yield build
+                nbuilds += 1
+
+            njobs += 1
+
+        logger.info("Total number of jobs: %i/%i", njobs, len(jobs))
+        logger.info("Total number of builds: %i", nbuilds)
+
+    @classmethod
+    def has_archiving(cls):
+        """Returns whether it supports archiving items on the fetch process.
+
+        :returns: this backend supports items archiving
+        """
+        return True
+
+    @classmethod
+    def has_resuming(cls):
+        """Returns whether it supports to resume the fetch process.
+
+        :returns: this backend does not supports items resuming
+        """
+        return False
+
+    @staticmethod
+    def metadata_id(item):
+        """Extracts the identifier from a Build item."""
+        return str(item['url'])
+
+    @staticmethod
+    def metadata_updated_on(item):
+        """Extracts the update time from a Jenkins item.
+
+        The timestamp is extracted from 'timestamp' field.
+        This date is a UNIX timestamp but needs to be converted to
+        a float value.
+
+        :param item: item generated by the backend
+
+        :returns: a UNIX timestamp
+        """
+        return float(item['timestamp'] / 1000)
+
+    @staticmethod
+    def metadata_category(item):
+        """Extracts the category from a Jenkins item.
+
+        This backend only generates one type of item which is
+        'build'.
+        """
+        return CATEGORY_BUILD
+
+    def _init_client(self, from_archive=False):
+        """Init client"""
+
+        return JenkinsClient(self.url, self.blacklist_jobs, self.detail_depth,
+                             self.sleep_time,
+                             archive=self.archive, from_archive=from_archive)
+
+
+class JenkinsClient(HttpClient):
+    """Jenkins API client.
+
+    This class implements a simple client to retrieve jobs/builds from
+    projects in a Jenkins node. The amount of data returned for each request
+    depends on the detail_depth value selected (minimun and default is 1).
+    Note that increasing the detail_depth may considerably slow down the
+    fetch operation and cause connection broken errors.
+
+    :param url: URL of jenkins node: https://build.opnfv.org/ci
+    :param blacklist_jobs: exclude the jobs of this list while fetching
+    :param detail_depth: set the detail level of the data returned by the API
+    :param sleep_time: time (in seconds) to sleep in case
+        of connection problems
+    :param archive: an archive to store/read fetched data
+    :param from_archive: it tells whether to write/read the archive
+
+    :raises HTTPError: when an error occurs doing the request
+    """
+    EXTRA_STATUS_FORCELIST = [410, 502, 503]
+    MAX_RETRIES = 5
+
+    def __init__(self, url, blacklist_jobs=None, detail_depth=DETAIL_DEPTH, sleep_time=SLEEP_TIME,
+                 archive=None, from_archive=False):
+        super().__init__(url, sleep_time=sleep_time, extra_status_forcelist=self.EXTRA_STATUS_FORCELIST,
+                         archive=archive, from_archive=from_archive)
+        self.blacklist_jobs = blacklist_jobs
+        self.detail_depth = detail_depth
+
+    def get_jobs(self):
+        """ Retrieve all jobs"""
+
+        url_jenkins = urijoin(self.base_url, "api", "json")
+
+        response = self.fetch(url_jenkins)
+        return response.text
+
+    def get_builds(self, job_name):
+        """ Retrieve all builds from a job"""
+
+        if self.blacklist_jobs and job_name in self.blacklist_jobs:
+            logger.warning("Not getting blacklisted job: %s", job_name)
+            return
+
+        payload = {'depth': self.detail_depth}
+        url_build = urijoin(self.base_url, "job", job_name, "api", "json")
+
+        response = self.fetch(url_build, payload=payload)
+        return response.text
+
+
+class JenkinsCommand(BackendCommand):
+    """Class to run Jenkins backend from the command line."""
+
+    BACKEND = Jenkins
+
+    @classmethod
+    def setup_cmd_parser(cls):
+        """Returns the Jenkins argument parser."""
+
+        parser = BackendCommandArgumentParser(cls.BACKEND.CATEGORIES,
+                                              archive=True)
+
+        # Jenkins options
+        group = parser.parser.add_argument_group('Jenkins arguments')
+        group.add_argument('--blacklist-jobs', dest='blacklist_jobs',
+                           nargs='*',
+                           help="Wrong jobs that must not be retrieved.")
+
+        group.add_argument('--detail-depth', dest='detail_depth',
+                           type=int, default=DETAIL_DEPTH,
+                           help="Detail level of the Jenkins data.")
+
+        group.add_argument('--sleep-time', dest='sleep_time',
+                           type=int, default=SLEEP_TIME,
+                           help="Minimun time to wait after a Timeout connection error.")
+
+        # Required arguments
+        parser.parser.add_argument('url',
+                                   help="URL of the Jenkins server")
+
+        return parser
