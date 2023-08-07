8f7e793a779627e5a25674260c7c98210d381c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:51:59 +0000

    Add package gpytorch/0.3.4/gpytorch-0.3.4.tar.gz

---
+++ packages/gpytorch/gpytorch-0.3.4.tar.gz/gpytorch-0.3.4/gpytorch/functions/_matmul.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+from torch.autograd import Function
+from .. import settings
+
+
+class Matmul(Function):
+    def __init__(self, representation_tree):
+        self.representation_tree = representation_tree
+
+    def forward(self, rhs, *matrix_args):
+        orig_rhs = rhs
+
+        if rhs.ndimension() == 1:
+            is_vector = True
+            rhs = rhs.unsqueeze(-1)
+        else:
+            is_vector = False
+
+        lazy_tsr = self.representation_tree(*matrix_args)
+        res = lazy_tsr._matmul(rhs)
+
+        to_save = [orig_rhs] + list(matrix_args)
+        self.save_for_backward(*to_save)
+        if settings.memory_efficient.off():
+            self._lazy_tsr = lazy_tsr
+
+        # Squeeze if necessary
+        if is_vector:
+            res = res.squeeze(-1)
+        return res
+
+    def backward(self, grad_output):
+        rhs = self.saved_tensors[0]
+        matrix_args = self.saved_tensors[1:]
+        rhs_shape = rhs.shape
+
+        rhs_grad = None
+        arg_grads = [None] * len(matrix_args)
+
+        # input_1 gradient
+        if any(self.needs_input_grad[1:]):
+            rhs = rhs.unsqueeze(-1) if (rhs.ndimension() == 1) else rhs
+            grad_output_matrix = grad_output.unsqueeze(-1) if grad_output.ndimension() == 1 else grad_output
+            arg_grads = self.representation_tree(*matrix_args)._quad_form_derivative(grad_output_matrix, rhs)
+
+        # input_2 gradient
+        if self.needs_input_grad[0]:
+            if hasattr(self, "_lazy_tsr"):
+                lazy_tsr = self._lazy_tsr
+            else:
+                lazy_tsr = self.representation_tree(*matrix_args)
+
+            if grad_output.dim() == 1:
+                # Confusing Cublas_Sgemv bug when grad_output is single dimensional on GPU.
+                rhs_grad = lazy_tsr._t_matmul(grad_output.unsqueeze(-1)).squeeze(-1)
+            else:
+                rhs_grad = lazy_tsr._t_matmul(grad_output)
+
+            # For broadcasting
+            if rhs_grad.dim() > len(rhs_shape):
+                rhs_grad = rhs_grad.contiguous().view(-1, *rhs_shape).sum(0)
+
+        return tuple([rhs_grad] + list(arg_grads))
---
+++ packages/gpytorch/gpytorch-0.3.4.tar.gz/gpytorch-0.3.4/gpytorch/functions/_root_decomposition.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+import torch
+from torch.autograd import Function
+from ..utils import lanczos
+from .. import settings
+
+
+class RootDecomposition(Function):
+    def __init__(
+        self,
+        representation_tree,
+        max_iter,
+        dtype,
+        device,
+        matrix_shape,
+        batch_shape,
+        root=True,
+        inverse=False,
+        initial_vectors=None,
+    ):
+        self.representation_tree = representation_tree
+        self.device = device
+        self.dtype = dtype
+        self.matrix_shape = matrix_shape
+        self.max_iter = max_iter
+        self.batch_shape = batch_shape
+        self.root = root
+        self.inverse = inverse
+        self.initial_vectors = initial_vectors
+
+    def forward(self, *matrix_args):
+        """
+        *matrix_args - The arguments representing the symmetric matrix A (or batch of PSD matrices A)
+
+        Returns:
+        - (Tensor) R, such that R R^T \approx A
+        - (Tensor) R_inv, such that R_inv R_inv^T \approx A^{-1} (will only be populated if self.inverse = True)
+        """
+        from ..lazy import lazify
+
+        # Get closure for matmul
+        lazy_tsr = self.representation_tree(*matrix_args)
+        matmul_closure = lazy_tsr._matmul
+        # Do lanczos
+        q_mat, t_mat = lanczos.lanczos_tridiag(
+            matmul_closure,
+            self.max_iter,
+            dtype=self.dtype,
+            device=self.device,
+            matrix_shape=self.matrix_shape,
+            batch_shape=self.batch_shape,
+            init_vecs=self.initial_vectors,
+        )
+
+        if self.batch_shape is None:
+            q_mat = q_mat.unsqueeze(-3)
+            t_mat = t_mat.unsqueeze(-3)
+        if t_mat.ndimension() == 3:  # If we only used one probe vector
+            q_mat = q_mat.unsqueeze(0)
+            t_mat = t_mat.unsqueeze(0)
+        n_probes = t_mat.size(0)
+
+        mins = lazify(t_mat).diag().min(dim=-1, keepdim=True)[0].unsqueeze(-1)
+        jitter_mat = (settings.tridiagonal_jitter.value() * mins) * torch.eye(
+            t_mat.size(-1), device=t_mat.device, dtype=t_mat.dtype
+        ).expand_as(t_mat)
+        eigenvalues, eigenvectors = lanczos.lanczos_tridiag_to_diag(t_mat + jitter_mat)
+
+        # Get orthogonal matrix and eigenvalue roots
+        q_mat = q_mat.matmul(eigenvectors)
+        root_evals = eigenvalues.sqrt()
+
+        # Store q_mat * t_mat_chol
+        # Decide if we're computing the inverse, or the regular root
+        root = torch.empty(0, dtype=q_mat.dtype, device=q_mat.device)
+        inverse = torch.empty(0, dtype=q_mat.dtype, device=q_mat.device)
+        if self.inverse:
+            inverse = q_mat / root_evals.unsqueeze(-2)
+        if self.root:
+            root = q_mat * root_evals.unsqueeze(-2)
+
+        if settings.memory_efficient.off():
+            self._lazy_tsr = lazy_tsr
+
+        if self.batch_shape is None:
+            root = root.squeeze(1) if root.numel() else root
+            q_mat = q_mat.squeeze(1)
+            t_mat = t_mat.squeeze(1)
+            root_evals = root_evals.squeeze(1)
+            inverse = inverse.squeeze(1) if inverse.numel() else inverse
+        if n_probes == 1:
+            root = root.squeeze(0) if root.numel() else root
+            q_mat = q_mat.squeeze(0)
+            t_mat = t_mat.squeeze(0)
+            root_evals = root_evals.squeeze(0)
+            inverse = inverse.squeeze(0) if inverse.numel() else inverse
+
+        to_save = list(matrix_args) + [q_mat, root_evals, inverse]
+        self.save_for_backward(*to_save)
+        return root, inverse
+
+    def backward(self, root_grad_output, inverse_grad_output):
+        # Taken from http://homepages.inf.ed.ac.uk/imurray2/pub/16choldiff/choldiff.pdf
+        if any(self.needs_input_grad):
+            def is_empty(tensor):
+                return tensor.numel() == 0 or (tensor.numel() == 1 and tensor[0] == 0)
+
+            # Fix outputs and gradients
+            if is_empty(root_grad_output):
+                root_grad_output = None
+            if is_empty(inverse_grad_output):
+                inverse_grad_output = None
+
+            # Get saved tensors
+            matrix_args = self.saved_tensors[:-3]
+            q_mat = self.saved_tensors[-3]
+            root_evals = self.saved_tensors[-2]
+            inverse = self.saved_tensors[-1]
+            is_batch = False
+
+            if root_grad_output is not None:
+                if root_grad_output.ndimension() == 2 and q_mat.ndimension() > 2:
+                    root_grad_output = root_grad_output.unsqueeze(0)
+                    is_batch = True
+                if root_grad_output.ndimension() == 3 and q_mat.ndimension() > 3:
+                    root_grad_output = root_grad_output.unsqueeze(0)
+                    is_batch = True
+            if inverse_grad_output is not None:
+                if inverse_grad_output.ndimension() == 2 and q_mat.ndimension() > 2:
+                    inverse_grad_output = inverse_grad_output.unsqueeze(0)
+                    is_batch = True
+                if inverse_grad_output.ndimension() == 3 and q_mat.ndimension() > 3:
+                    inverse_grad_output = inverse_grad_output.unsqueeze(0)
+                    is_batch = True
+
+            # Get closure for matmul
+            if hasattr(self, "_lazy_tsr"):
+                lazy_tsr = self._lazy_tsr
+            else:
+                lazy_tsr = self.representation_tree(*matrix_args)
+
+            # Get root inverse
+            if not self.inverse:
+                inverse = q_mat / root_evals.unsqueeze(-2)
+            # Left factor:
+            left_factor = torch.zeros_like(inverse)
+            if root_grad_output is not None:
+                left_factor.add_(root_grad_output)
+            if inverse_grad_output is not None:
+                # -root^-T grad_output.T root^-T
+                left_factor.sub_(torch.matmul(inverse, inverse_grad_output.transpose(-1, -2)).matmul(inverse))
+
+            # Right factor
+            right_factor = inverse.div(2.0)
+
+            # Fix batches
+            if is_batch:
+                left_factor = left_factor.permute(1, 0, 2, 3).contiguous()
+                left_factor = left_factor.view(inverse.size(1), -1, left_factor.size(-1))
+                right_factor = right_factor.permute(1, 0, 2, 3).contiguous()
+                right_factor = right_factor.view(inverse.size(1), -1, right_factor.size(-1))
+            else:
+                left_factor = left_factor.contiguous()
+                right_factor = right_factor.contiguous()
+            res = lazy_tsr._quad_form_derivative(left_factor, right_factor)
+
+            return tuple(res)
+        else:
+            pass
