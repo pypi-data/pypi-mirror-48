f0949c3df7936e4c3331068938167fe09e9d30ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:41 +0000

    Add package DatastoreClient/0.0.3/DatastoreClient-0.0.3.tar.gz

---
+++ packages/DatastoreClient/DatastoreClient-0.0.3.tar.gz/DatastoreClient-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DatastoreClient",
+    version="0.0.3",
+    author="TaylorHere",
+    author_email="liyanhong@hz-health.cn",
+    description="Python Client for Datastore APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TaylorHere",
+    packages=setuptools.find_packages(),
+    package_data={'DatastoreClient.graphql': ['*.graphql']},
+    include_package_data=True,
+    install_requires=['graphqlclient', 'minio'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
