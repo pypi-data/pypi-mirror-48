32c57fd4a8db8f1a16eb8ead5fc995ea8a9f2ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:33 +0000

    Add package opensitua-core/0.0.1/opensitua_core-0.0.1.tar.gz

---
+++ packages/opensitua-core/opensitua_core-0.0.1.tar.gz/opensitua_core-0.0.1/setup.py
@@ -0,0 +1,38 @@
+import os,re
+import setuptools
+
+PACKAGE_NAME = "opensitua_core"
+
+def get_version():
+    print(os.getcwd())
+    VERSIONFILE = os.path.join(PACKAGE_NAME, '__init__.py')
+    initfile_lines = open(VERSIONFILE, 'rt').readlines()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]\s*"
+    for line in initfile_lines:
+        mo = re.search(VSRE, line, re.M)
+        print(line,mo)
+        print("----")
+        if mo:
+            return mo.group(1)
+    raise RuntimeError('Unable to find version string in %s.' % (VERSIONFILE,))
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=get_version(),
+    author="Valerio Luzzi",
+    author_email="valluzzi@gmail.com",
+    description="core functions package",
+    long_description="core functions package",
+    url="https://github.com/valluzzi/%s.git"%(PACKAGE_NAME),
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=['jinja2',
+                      'six', 'argparse', 'xmljson',
+                      'openpyxl',
+                      'xlrd', 'xlwt', 'xlutils', 'numpy',
+                      'rarfile']
+)
