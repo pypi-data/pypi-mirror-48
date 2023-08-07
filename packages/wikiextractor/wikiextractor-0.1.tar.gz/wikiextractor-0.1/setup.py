b1b4d1b696597a0e877cbd2907db177c46df48d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:15 +0000

    Add package wikiextractor/0.1/wikiextractor-0.1.tar.gz

---
+++ packages/wikiextractor/wikiextractor-0.1.tar.gz/wikiextractor-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='wikiextractor',
+    packages=['wikiextractor'],
+    version='0.1',
+    license='MIT',
+    description='scripts for parsing the wikimedia xml dumps files',
+    author='',
+    author_email='',
+    url='https://github.com/MouhamadAboShokor/wikiextractor',
+    download_url='https://github.com/MouhamadAboShokor/wikiextractor/v_01.tar.gz',
+    keywords=[''],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+)
