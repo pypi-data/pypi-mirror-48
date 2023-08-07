f8ef2244aa25bc7198e20e38242982ba9001767d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:52 +0000

    Add package rotest/7.8.0/rotest-7.8.0-py2.py3-none-any.whl

---
+++ packages/rotest/rotest-7.8.0-py2.py3-none-any.whl/rotest/__init__.py
@@ -0,0 +1,14 @@
+"""Rotest testing framework, based on Python unit-test and Django."""
+# pylint: disable=unused-import
+from __future__ import absolute_import
+from unittest import skip, SkipTest, skipIf as skip_if
+
+import colorama
+
+from .common import config
+from .cli.client import main
+from .common.django_utils import settings
+
+
+# Enable color printing on screen.
+colorama.init()
