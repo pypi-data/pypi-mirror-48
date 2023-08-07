b1d41686d61ba65fef45959a0a4a8d829256ce02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:23 +0000

    Add package pyrolite/0.1.19/pyrolite-0.1.19.tar.gz

---
+++ packages/pyrolite/pyrolite-0.1.19.tar.gz/pyrolite-0.1.19/pyrolite/geochem/__init__.py
@@ -0,0 +1,16 @@
+"""
+Submodule for working with geochemical data.
+"""
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+
+# direct imports for backwards compatibility
+from .ind import *
+from .magma import *
+from .parse import *
+from .transform import *
+from .validate import *
+from .alteration import *
+from .norm import *
