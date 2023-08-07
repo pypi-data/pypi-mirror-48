05782c04814280542e543e46ffc9a27415ff2944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:49 +0000

    Add package cplex/12.9.0.0/cplex-12.9.0.0-cp27-cp27m-win_amd64.whl

---
+++ packages/cplex/cplex-12.9.0.0-cp27-cp27m-win_amd64.whl/cplex-12.9.0.0.data/purelib/cplex/_internal/_pycplex_platform.py
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+# Licensed Materials - Property of IBM
+# 5725-A06 5725-A29 5724-Y48 5724-Y49 5724-Y54 5724-Y55 5655-Y21
+# Copyright IBM Corporation 2008, 2019. All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or
+# disclosure restricted by GSA ADP Schedule Contract with
+# IBM Corp.
+# ------------------------------------------------------------------------------
+"""
+"""
+
+import platform
+
+from sys import version_info
+
+ERROR_STRING = "CPLEX 12.9.0.1 is not compatible with this version of Python."
+
+if platform.system() in ('Darwin', 'Linux', 'AIX', 'Windows', 'Microsoft'):
+    if version_info < (2, 7, 0):
+        raise Exception(ERROR_STRING)
+    elif version_info < (2, 8, 0):
+        from cplex._internal.py27_cplex1290 import *
+    else:
+        raise Exception(ERROR_STRING)
+else:
+    raise Exception("The CPLEX Python API is not supported on this platform.")
