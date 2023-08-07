a0593a9b75b7856f68f9e35fd2acdfc0e60f7056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:13 +0000

    Add package kedro-viz/1.0.2/kedro_viz-1.0.2-py3-none-any.whl

---
+++ packages/kedro-viz/kedro_viz-1.0.2-py3-none-any.whl/kedro_viz/html/precache-manifest.b47b937d78ffe7034e8a5b1f1cc41f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe4c64b04181e424c60f613de12be49",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d06f37c8b5c7d0f69289",
+    "url": "./static/css/2.d438ea0b.chunk.css"
+  },
+  {
+    "revision": "e1a7236e32a12d906f26",
+    "url": "./static/css/main.1e0bf853.chunk.css"
+  },
+  {
+    "revision": "d06f37c8b5c7d0f69289",
+    "url": "./static/js/2.ad4db969.chunk.js"
+  },
+  {
+    "revision": "e1a7236e32a12d906f26",
+    "url": "./static/js/main.86bc5985.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "7498697619bed8bcf479fc4d8648b5b4",
+    "url": "./static/media/delete.74986976.svg"
+  }
+]);
