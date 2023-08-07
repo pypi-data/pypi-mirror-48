5143f42f0bfb57e2a85b7804632091c5a92bf41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:06 +0000

    Add package grimoire-elk/0.58.0/grimoire_elk-0.58.0-py3-none-any.whl

---
+++ packages/grimoire-elk/grimoire_elk-0.58.0-py3-none-any.whl/grimoire_elk/raw/supybot.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2019 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Authors:
+#   Alvaro del Castillo San Felix <acs@bitergia.com>
+#
+
+from .elastic import ElasticOcean
+
+
+class SupybotOcean(ElasticOcean):
+    """MediaWiki Ocean feeder"""
+
+    @classmethod
+    def get_perceval_params_from_url(cls, url):
+        # In the url the uri and the data dir are included
+        params = url.split()
+
+        return params
+
+    @classmethod
+    def get_arthur_params_from_url(cls, url):
+        # In the url the uri and the dirpath are included
+
+        params = url.split()
+        """ Get the arthur params given a URL for the data source """
+        params = {"uri": params[0], "dirpath": params[1]}
+
+        return params
