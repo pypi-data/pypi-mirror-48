44d14c8bb5988174f557b78bb3ba679806411e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:18 +0000

    Add package odut/1.1.2/odut-1.1.2-py2.py3-none-any.whl

---
+++ packages/odut/odut-1.1.2-py2.py3-none-any.whl/odut/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for odut."""
+
+__author__ = """Ming Lyu"""
+__email__ = 'minglyupolimi@gmail.com'
+__version__ = '1.1.2'
+
+import sys
+from pathlib import Path
+sys.path.append(Path(__file__).parent)
