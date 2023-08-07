739911d7eb3750c419124703d7ea4cfdd2109629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:14 +0000

    Add package riboflow/1.1.1/riboflow-1.1.1.tar.gz

---
+++ packages/riboflow/riboflow-1.1.1.tar.gz/riboflow-1.1.1/setup.py
@@ -0,0 +1,23 @@
+import os,glob
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="riboflow",
+    version="1.1.1",
+    author="Keshav Aditya R.P",
+    author_email="keshavaditya26896@gmail.com",
+    description="Classifying Putative Riboswitch Sequences",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KeshavAdityaRP/riboflow",
+    packages=['riboflow'],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)      
