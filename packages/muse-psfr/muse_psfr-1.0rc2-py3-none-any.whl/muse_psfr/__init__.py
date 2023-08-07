653767e5dcf8573134928f52f362b957ce3c0fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:48 +0000

    Add package muse-psfr/1.0rc2/muse_psfr-1.0rc2-py3-none-any.whl

---
+++ packages/muse-psfr/muse_psfr-1.0rc2-py3-none-any.whl/muse_psfr/__init__.py
@@ -0,0 +1,24 @@
+def _setup_logging():
+    from mpdaf.log import setup_logging
+    import sys
+
+    setup_logging(
+        "muse_psfr",
+        fmt="[%(levelname)s] %(message)s",
+        level="INFO",
+        color=True,
+        stream=sys.stdout,
+    )
+
+
+_setup_logging()
+
+from .psfrec import *  # noqa
+
+from pkg_resources import get_distribution, DistributionNotFound  # noqa
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
