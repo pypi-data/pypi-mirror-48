dbc2eab1a85abb524c344ada758036d7d22d0318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:53 +0000

    Add package botbuilder-core/4.5.0b1/botbuilder_core-4.5.0b1-py3-none-any.whl

---
+++ packages/botbuilder-core/botbuilder_core-4.5.0b1-py3-none-any.whl/botbuilder/core/__init__.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+# --------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+from . import conversation_reference_extension
+
+from .about import __version__
+from .activity_handler import ActivityHandler
+from .bot_assert import BotAssert
+from .bot_adapter import BotAdapter
+from .bot_framework_adapter import BotFrameworkAdapter, BotFrameworkAdapterSettings
+from .bot_state import BotState
+from .bot_telemetry_client import BotTelemetryClient
+from .card_factory import CardFactory
+from .conversation_state import ConversationState
+from .invoke_response import InvokeResponse
+from .memory_storage import MemoryStorage
+from .message_factory import MessageFactory
+from .middleware_set import AnonymousReceiveMiddleware, Middleware, MiddlewareSet
+from .null_telemetry_client import NullTelemetryClient
+from .state_property_accessor import StatePropertyAccessor
+from .state_property_info import StatePropertyInfo
+from .storage import Storage, StoreItem, calculate_change_hash
+from .turn_context import TurnContext
+from .user_state import UserState
+from .user_token_provider import UserTokenProvider
+
+__all__ = ['ActivityHandler',
+           'AnonymousReceiveMiddleware',
+           'BotAdapter',
+           'BotAssert',
+           'BotFrameworkAdapter',
+           'BotFrameworkAdapterSettings',
+           'BotState',
+           'BotTelemetryClient',
+           'calculate_change_hash',
+           'CardFactory',
+           'ConversationState',
+           'conversation_reference_extension',
+           'InvokeResponse',
+           'MemoryStorage',
+           'MessageFactory',
+           'Middleware',
+           'MiddlewareSet',
+           'NullTelemetryClient',
+           'StatePropertyAccessor',
+           'StatePropertyInfo',
+           'Storage',
+           'StoreItem',
+           'TurnContext',
+           'UserState',
+           'UserTokenProvider',
+           '__version__']
