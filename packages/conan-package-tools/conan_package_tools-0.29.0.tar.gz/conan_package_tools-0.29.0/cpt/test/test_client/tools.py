5a08ea0385a067a68d67a9fc177bfaf6be9d423c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:36 +0000

    Add package conan-package-tools/0.29.0/conan_package_tools-0.29.0.tar.gz

---
+++ packages/conan-package-tools/conan_package_tools-0.29.0.tar.gz/conan_package_tools-0.29.0/cpt/test/test_client/tools.py
@@ -0,0 +1,39 @@
+import six
+from conans.client.conan_api import Conan
+from conans.model.version import Version
+from cpt import get_client_version
+
+from cpt.packager import ConanMultiPackager
+
+
+def get_patched_multipackager(tc, *args, **kwargs):
+    tc.init_dynamic_vars()
+    client_version = get_client_version()
+    extra_init_kwargs = {}
+    if Version(client_version) >= Version("1.11"):
+        extra_init_kwargs.update({'requester': tc.requester})
+
+    if Version(client_version) < Version("1.12.0"):
+        cache = tc.client_cache
+    else:
+        cache = tc.cache
+
+    conan_api = Conan(cache, tc.user_io, tc.runner, tc.remote_manager, tc.hook_manager,
+                      interactive=False, **extra_init_kwargs)
+
+    class Printer(object):
+
+        def __init__(self, tc):
+            self.tc = tc
+
+        def __call__(self, contents):
+            if six.PY2:
+                contents = unicode(contents)
+            self.tc.user_io.out._buffer.write(contents)
+
+    kwargs["out"] = Printer(tc)
+    kwargs["conan_api"] = conan_api
+    kwargs["cwd"] = tc.current_folder
+
+    mp = ConanMultiPackager(*args, **kwargs)
+    return mp
