24ee765ae7aecc9f05366df6b3b6639caaa1a5f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:05 +0000

    Add package tus.py/1.3.4/tus.py-1.3.4.tar.gz

---
+++ packages/tus.py/tus.py-1.3.4.tar.gz/tus.py-1.3.4/setup.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+from setuptools import setup
+
+setup(
+    name='tus.py',
+    description='tus (resumable file upload protocol) client',
+    version='1.3.4',
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    url='https://github.com/cenk/tus.py',
+    py_modules=['tus'],
+    include_package_data=True,
+    zip_safe=True,
+    platforms='any',
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'tus-upload = tus:_cmd_upload',
+            'tus-resume = tus:_cmd_resume',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
