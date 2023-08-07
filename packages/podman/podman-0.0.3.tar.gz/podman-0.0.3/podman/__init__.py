cfb32c971318d9ad3bb12687d9118090b0501d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:28 +0000

    Add package podman/0.0.3/podman-0.0.3.tar.gz

---
+++ packages/podman/podman-0.0.3.tar.gz/podman-0.0.3/podman/__init__.py
@@ -0,0 +1,31 @@
+"""A client for communicating with a Podman server."""
+import pkg_resources
+
+from .client import Client
+from .libs import FoldedString, datetime_format, datetime_parse
+from .libs.errors import (ContainerNotFound, ErrorOccurred, ImageNotFound,
+                          NoContainerRunning, NoContainersInPod,
+                          PodContainerError, PodmanError, PodNotFound)
+
+from pbr.version import VersionInfo
+
+assert FoldedString
+
+try:
+    __version__ = VersionInfo("podman")
+except Exception:  # pylint: disable=broad-except
+    __version__ = '0.0.0'
+
+__all__ = [
+    'Client',
+    'ContainerNotFound',
+    'datetime_format',
+    'datetime_parse',
+    'ErrorOccurred',
+    'ImageNotFound',
+    'NoContainerRunning',
+    'NoContainersInPod',
+    'PodContainerError',
+    'PodmanError',
+    'PodNotFound',
+]
