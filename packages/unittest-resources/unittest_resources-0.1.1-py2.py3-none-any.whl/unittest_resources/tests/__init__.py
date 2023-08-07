c1a62f8c951d428084054ca7f5d3c59b87f9b908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:22 +0000

    Add package unittest-resources/0.1.1/unittest_resources-0.1.1-py2.py3-none-any.whl

---
+++ packages/unittest-resources/unittest_resources-0.1.1-py2.py3-none-any.whl/unittest_resources/tests/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+:module:`unittest_resources` test suite. Not part of the public API.
+
+More details on project `README.md` and
+`repository <https://gitlab.com/ergoithz/unittest-resources/>`_.
+
+License
+-------
+MIT (see LICENSE file).
+"""
