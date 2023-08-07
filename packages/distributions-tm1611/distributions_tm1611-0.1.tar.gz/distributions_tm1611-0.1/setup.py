18d14ec8137e1bced08b0b3adb0e7196069980ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:44:09 +0000

    Add package distributions-tm1611/0.1/distributions_tm1611-0.1.tar.gz

---
+++ packages/distributions-tm1611/distributions_tm1611-0.1.tar.gz/distributions_tm1611-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_tm1611',
+      version='0.1',
+      description='distributions package',
+      packages=['distributions_tm1611'],
+      author="Timo Meiendresch",
+      author_email="tmeiendresch@gmx.de",
+      zip_safe=False)
