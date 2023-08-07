4c571312c003692c9c5e6525e27202a62a25fb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:53 +0000

    Add package mathematical/0.1.5/mathematical-0.1.5.tar.gz

---
+++ packages/mathematical/mathematical-0.1.5.tar.gz/mathematical-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name="mathematical",
+	version="0.1.5",
+    author='Dominic Davis-Foster',
+	author_email="dominic@davis-foster.co.uk",
+	packages=find_packages(),
+	license="OSI Approved :: GNU General Public License v3 (GPLv3)",
+	url="https://github.com/domdfcoding/mathematical",
+	description='Mathematical tools for python',
+	long_description=long_description,
+	long_description_content_type="text/x-rst",
+	classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+		"Development Status :: 4 - Beta",
+    ],
+	install_requires=[
+		"scipy >= 1.3.0",
+		"numpy >= 1.16.0",
+	],
+)
