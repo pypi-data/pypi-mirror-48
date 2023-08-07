439686fbb1b13c5352b3289fa16d4485ee3238f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:17 +0000

    Add package Kerapu/2.0.2/Kerapu-2.0.2.tar.gz

---
+++ packages/Kerapu/Kerapu-2.0.2.tar.gz/Kerapu-2.0.2/kerapu/application/KerapuApplication.py
@@ -0,0 +1,39 @@
+"""
+Kerapu
+"""
+from typing import List
+
+from cleo import Application, Command
+
+from kerapu.command.ShredderCommand import ShredderCommand
+from kerapu.command.TestsetShredderCommand import TestShredderCommand
+
+
+class KerapuApplication(Application):
+    """
+    The Kerapu application.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def __init__(self):
+        """
+        Object constructor.
+        """
+        Application.__init__(self, 'kerapu', '2.0.2')
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def get_default_commands(self) -> List[Command]:
+        """
+        Returns the default commands of this application.
+
+        :rtype: list[Command]
+        """
+        commands = Application.get_default_commands(self)
+
+        # Kerapu:
+        commands.append(ShredderCommand())
+        commands.append(TestShredderCommand())
+
+        return commands
+
+# ----------------------------------------------------------------------------------------------------------------------
