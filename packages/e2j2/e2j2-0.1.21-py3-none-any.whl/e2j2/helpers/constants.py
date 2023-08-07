4c0b4521ea8499f622e72e19b7df8dbbe63f0e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:42 +0000

    Add package e2j2/0.1.21/e2j2-0.1.21-py3-none-any.whl

---
+++ packages/e2j2/e2j2-0.1.21-py3-none-any.whl/e2j2/helpers/constants.py
@@ -0,0 +1,8 @@
+ERROR = '** ERROR'
+BRIGHT_RED = '\033[1;31m'
+RESET_ALL = '\033[00m'
+YELLOW = '\033[93m'
+GREEN = '\033[0;32m'
+LIGHTGREEN = '\033[1;32m'
+WHITE = '\033[0;37m'
+DESCRIPTION = 'Parse jinja2 templates with enhanced JSON aware environment variables'
