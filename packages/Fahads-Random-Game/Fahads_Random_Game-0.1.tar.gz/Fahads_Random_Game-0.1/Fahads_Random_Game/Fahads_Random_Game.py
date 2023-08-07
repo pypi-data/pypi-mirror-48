764675b6cfc9872877798e82b454c9e7bb107792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:04 +0000

    Add package Fahads-Random-Game/0.1/Fahads_Random_Game-0.1.tar.gz

---
+++ packages/Fahads-Random-Game/Fahads_Random_Game-0.1.tar.gz/Fahads_Random_Game-0.1/Fahads_Random_Game/Fahads_Random_Game.py
@@ -0,0 +1,27 @@
+import random
+
+class RandomGame():
+   
+    
+    def __init__(self, random,user_input):
+                
+        self.random = random1
+        self.user_input = user_input
+        
+   
+    
+                        
+    
+    def play():
+        
+        random1 =random.randint(1,101)
+        print(random1)
+        
+        user_input= int(input("Enter your value: "))
+        if random1 == user_input :
+            print ("you're right")
+        else:
+            print("try again")
+       
+    play()
+   
---
+++ packages/Fahads-Random-Game/Fahads_Random_Game-0.1.tar.gz/Fahads_Random_Game-0.1/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+
+from setuptools import setup
+
+setup(name='Fahads_Random_Game',
+      version='0.1',
+      description='paly with your computer',
+      packages=['Fahads_Random_Game'],
+      autor = "Fahad Alajmi",
+      zip_safe=False)
