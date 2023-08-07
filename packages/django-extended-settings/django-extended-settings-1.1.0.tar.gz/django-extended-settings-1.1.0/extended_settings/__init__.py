3a4f244750c08600dd9ade586a0444baf567dee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:12 +0000

    Add package django-extended-settings/1.1.0/django-extended-settings-1.1.0.tar.gz

---
+++ packages/django-extended-settings/django-extended-settings-1.1.0.tar.gz/django-extended-settings-1.1.0/extended_settings/__init__.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------------------
+# - Django Extends Settings
+# -
+# - MickBad
+# --------------------------------------------------------------------------------------------
+
+# misc
+__version__ = '1.1.0'
+
+# Code that discovers files or modules in INSTALLED_APPS imports this module.
+default_app_config = 'extended_settings.apps.ExtendedSettingsConfig'
