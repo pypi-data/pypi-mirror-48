43007f02586339d91b772796638db9223f9716a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:35 +0000

    Add package AwesomeBuild/2.0.1/AwesomeBuild-2.0.1.tar.gz

---
+++ packages/AwesomeBuild/AwesomeBuild-2.0.1.tar.gz/AwesomeBuild-2.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='AwesomeBuild',
+    description='Awesome build manager to replace Makefiles. It allow very fast building!',
+    version='2.0.1',
+    author='Raphael Jacob',
+    author_email='r.jacob2002@gmail.com',
+    url='https://github.com/ski7777/AwesomeBuild',
+    license='GPLv3',
+    packages=['AwesomeBuild'],
+    entry_points={
+        'console_scripts': [
+            'AwesomeBuild = AwesomeBuild.__main__:main'
+        ]
+    }
+)
